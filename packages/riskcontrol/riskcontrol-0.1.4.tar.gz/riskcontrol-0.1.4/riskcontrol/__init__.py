b849168a5493de1e77cef04135765f7caa031132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:42:19 +0000

    Add package riskcontrol/0.1.4/riskcontrol-0.1.4.tar.gz

---
+++ packages/riskcontrol/riskcontrol-0.1.4.tar.gz/riskcontrol-0.1.4/riskcontrol/__init__.py
@@ -0,0 +1,2 @@
+from . import ks4score
+
---
+++ packages/riskcontrol/riskcontrol-0.1.4.tar.gz/riskcontrol-0.1.4/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='riskcontrol',
+      version='0.1.4',
+      description='risk control modeling',
+      author='SimaShanhe',
+      author_email='hsliu_em@126.com',
+      license='MIT',
+      packages=['riskcontrol'])

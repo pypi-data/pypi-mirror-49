bc78eac604600ce3f2876b5d2ec618bfcc5b4998
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:37 +0000

    Add package capalyzer/2.11.3/capalyzer-2.11.3-py3-none-any.whl

---
+++ packages/capalyzer/capalyzer-2.11.3-py3-none-any.whl/capalyzer/packet_parser/longform_parser.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import gzip
+import csv
+
+
+def parse_longform_taxa(filename, rank='all', strict=512, exclude_ranks=['assembly', 'sequence']):
+    """Return a pandas dataframe."""
+    tbl = {}
+    with gzip.open(filename, 'r') as longform:
+        longform.readline()
+        for line in longform:
+            tkns = list(csv.reader([line.decode('utf-8')]))[0]
+            sample_name, taxa_name, taxa_rank = tkns[0], tkns[1], tkns[3]
+            try:
+                nkmers, nreads = int(tkns[6]), int(tkns[5])
+            except ValueError:
+                print(line)
+                raise
+            if rank and rank != 'all' and rank != taxa_rank:
+                continue
+            if strict and nkmers < strict:
+                continue
+            if taxa_rank in exclude_ranks:
+                continue
+            sample_tbl = tbl.get(sample_name, {})
+            sample_tbl[taxa_name] = nreads
+            tbl[sample_name] = sample_tbl
+    return pd.DataFrame.from_dict(tbl, orient='index').fillna(0)

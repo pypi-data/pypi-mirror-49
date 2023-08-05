66544b3e1806843b752f5227677ec908d535f4f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:49 +0000

    Add package zumidashboard/1.15/zumidashboard-1.15-py3-none-any.whl

---
+++ packages/zumidashboard/zumidashboard-1.15-py3-none-any.whl/zumidashboard/dashboard/precache-manifest.c5ec8779e26c3bac708b3a0d53fecfff.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "da48b7c9501eefc16a33c82a1bdfad00",
+    "url": "/static/media/learn_bg.da48b7c9.png"
+  },
+  {
+    "revision": "d0dcecaf2eb6f1b1840a",
+    "url": "/static/css/main.57bf5e4c.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f91d5e7614351925cd97",
+    "url": "/static/js/2.087529f7.chunk.js"
+  },
+  {
+    "revision": "801dbb9a832a1016a2d090132832ef92",
+    "url": "/static/media/explore_bg.801dbb9a.png"
+  },
+  {
+    "revision": "d0dcecaf2eb6f1b1840a",
+    "url": "/static/js/main.4d523d12.chunk.js"
+  },
+  {
+    "revision": "e05c10dfe41c6e170783c26d99b3b0c7",
+    "url": "/static/media/zumiface_thinking.e05c10df.png"
+  },
+  {
+    "revision": "de69c17f481141c7d327dd6d5c0877bc",
+    "url": "/static/media/zumiface_blanklarge.de69c17f.png"
+  },
+  {
+    "revision": "65b5d8fd6fa98e2d3cd61619de53b0f6",
+    "url": "/static/media/zumiface_neutral.65b5d8fd.png"
+  },
+  {
+    "revision": "f65cac4689448505492afe9a89f1a188",
+    "url": "/static/media/zumiface_happy.f65cac46.png"
+  },
+  {
+    "revision": "a6de6d19da81c1ef525d2be8acb29141",
+    "url": "/index.html"
+  }
+];

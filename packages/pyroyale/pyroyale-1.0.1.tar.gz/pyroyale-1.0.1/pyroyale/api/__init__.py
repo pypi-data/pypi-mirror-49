4546d7b9da2ec5692ad5baf325417498b8f79e94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:47 +0000

    Add package pyroyale/1.0.1/pyroyale-1.0.1.tar.gz

---
+++ packages/pyroyale/pyroyale-1.0.1.tar.gz/pyroyale-1.0.1/pyroyale/api/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from pyroyale.api.cards_api import CardsApi
+from pyroyale.api.clans_api import ClansApi
+from pyroyale.api.locations_api import LocationsApi
+from pyroyale.api.players_api import PlayersApi
+from pyroyale.api.tournaments_api import TournamentsApi

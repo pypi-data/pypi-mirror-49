c0e3e750a052a1016b21b7024f00965a2c67ada4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:14 +0000

    Add package bbcu.reportIlluminaInterop/1.0.0/bbcu.reportIlluminaInterop-1.0.0.tar.gz

---
+++ packages/bbcu.reportIlluminaInterop/bbcu.reportIlluminaInterop-1.0.0.tar.gz/bbcu.reportIlluminaInterop-1.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open('VERSION.txt', 'r') as version_file:
+    version = version_file.read().strip()
+
+requires = ['pandas', 'interop==1.1.4', 'tabulate']
+
+setup(
+    name='bbcu.reportIlluminaInterop',
+    version=version,
+    author='Refael Kohen',
+    author_email='refael.kohen@weizmann.ac.il',
+    packages=find_packages(),
+    scripts=[
+        'scripts/run-sav-stat.py',
+    ],
+    description='Create SAV statistics',
+    long_description=open('README.txt').read(),
+    install_requires=requires,
+    tests_require=requires + ['nose'],
+    include_package_data=True,
+    test_suite='nose.collector',
+)

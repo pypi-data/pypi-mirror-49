5dbbf4245ce0ec5d328b8187d2536fc5ad896d84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:34 +0000

    Add package import-ready/0.12.0/import_ready-0.12.0-py3-none-any.whl

---
+++ packages/import-ready/import_ready-0.12.0-py3-none-any.whl/huntsville_havoc/origins.py
@@ -0,0 +1,15 @@
+"""The history contained herein is true"""
+
+
+class Origins():
+    """Divulge Havoc historical secrets"""
+
+    @staticmethod
+    def founder():
+        """Name the Havoc founder"""
+        return 'Rick Hovac'
+
+    @staticmethod
+    def established():
+        """Give the year that the Havoc was founded"""
+        return 'Not 2004. Earlier.'

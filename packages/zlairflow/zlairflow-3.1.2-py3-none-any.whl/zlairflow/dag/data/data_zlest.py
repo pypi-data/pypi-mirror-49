c76918f5e71912620637ee9697f6032348c7fd7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:21 +0000

    Add package zlairflow/3.1.2/zlairflow-3.1.2-py3-none-any.whl

---
+++ packages/zlairflow/zlairflow-3.1.2-py3-none-any.whl/zlairflow/dag/data/data_zlest.py
@@ -0,0 +1,51 @@
+para=[
+['zlest', 'ezhoushi', 'minutes=480', ''],
+['zlest', 'guizhousheng', 'minutes=480', ''],
+['zlest', 'qinghaisheng', 'minutes=480', ''],
+['zlest', 'shaoyangxian', 'minutes=480', ''],
+['zlest', 'shenzhenshi', 'minutes=480', ''],
+['zlest', 'anhuisheng', 'minutes=480', ''],
+['zlest', 'guangdongsheng', 'minutes=480', ''],
+['zlest', 'guangxisheng', 'minutes=480', ''],
+['zlest', 'henansheng', 'minutes=480', ''],
+['zlest', 'hunansheng', 'minutes=480', ''],
+['zlest', 'jiangxisheng', 'minutes=480', ''],
+
+['zlest', 'wuxishi', 'minutes=480', ''],
+['zlest', 'zhejiangsheng', 'minutes=480', ''],
+['zlest', 'anhuisheng1', 'minutes=480', ''],
+['zlest', 'enshizhou', 'minutes=480', ''],
+
+['zlest', 'hubeisheng', 'minutes=480', ''],
+['zlest', "sichuansheng",'minutes=480', ''],
+['zlest', "qinghaisheng1",'minutes=480', ''],
+['zlest', "neimenggu", 'minutes=480', ''],
+
+
+['zlest','ggzy_gansu_linxia','minutes=180',''],
+['zlest','ggzy_henan_henan','minutes=180',''],
+['zlest','ggzy_hubei_anlu','minutes=180',''],
+['zlest','ggzy_hubei_chibi','minutes=180',''],
+['zlest','ggzy_hubei_daye','minutes=180',''],
+['zlest','ggzy_hubei_ezhou','minutes=180',''],
+['zlest','ggzy_hubei_guangshui','minutes=180',''],
+['zlest','ggzy_hubei_hanchuan','minutes=180',''],
+['zlest','ggzy_hubei_honghu','minutes=180',''],
+['zlest','ggzy_hubei_hubei','minutes=180',''],
+['zlest','ggzy_hubei_jingzhou','minutes=180',''],
+['zlest','ggzy_hubei_macheng','minutes=180',''],
+['zlest','ggzy_hubei_wuxue','minutes=180',''],
+['zlest','ggzy_hubei_xianning','minutes=180',''],
+['zlest','ggzy_hubei_yicheng','minutes=180',''],
+['zlest','ggzy_hubei_yingcheng','minutes=180',''],
+['zlest','ggzy_hubei_zhijiang','minutes=180',''],
+['zlest','ggzy_hubei_zhongxiang','minutes=180',''],
+['zlest','ggzy_hunan_xiangxi','minutes=180',''],
+['zlest','ggzy_tianjin_tianjin','minutes=180',''],
+['zlest','zfcg_xinjiang_bole','minutes=180',''],
+
+
+
+]
+
+

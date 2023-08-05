01e4ddb569caa56e7c15a72edb25fa0f1ec4061b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:21 +0000

    Add package lampylib/0.1.1/lampylib-0.1.1.tar.gz

---
+++ packages/lampylib/lampylib-0.1.1.tar.gz/lampylib-0.1.1/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['lampylib']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['python-rtmidi']
+
+entry_points = \
+{'console_scripts': ['lampy = lampylib.__main__:main']}
+
+setup_kwargs = {
+    'name': 'lampylib',
+    'version': '0.1.1',
+    'description': 'Launchpad Mini python library',
+    'long_description': "|PyPI| |Build Status| |Coverage|\n\n=============================================================\nLampy: **La**\\ unchpad **M**\\ ini **p**\\ ython librar\\ **y**\n=============================================================\n\n.. image:: https://github.com/pkulev/lampy/raw/master/launchpad.jpg\n\nRequirements\n============\n\n* Python >= 3.7\n* python-rtmidi\n* TODO\n* TODO\n\nInstallation\n============\n\nFor usage only\n--------------\n\nUnfortunately name **lampy** was already in use in PyPI.\n\n.. code-block:: console\n\n   # via pipsi\n   $ pipsi install lampylib\n\n   # via pip\n   $ pip install --user lampylib\n\nFor development\n---------------\n\nYou need `poetry <https://poetry.eustace.io/>`__ installed.\n(It's great tool! Check it out, please.)\n\n.. code-block:: console\n\n   $ poetry install --develop .\n\nTesting\n=======\n\n* TODO\n\nDocumentation\n=============\n\n* TODO\n\n.. |Build Status| image:: https://travis-ci.org/pkulev/lampy.svg?branch=master\n   :target: https://travis-ci.org/pkulev/lampy\n.. |Coverage| image:: https://codecov.io/gh/pkulev/lampy/branch/master/graph/badge.svg\n  :target: https://codecov.io/gh/pkulev/lampy\n.. |PyPI| image:: https://badge.fury.io/py/lampylib.svg\n    :target: https://badge.fury.io/py/lampylib\n",
+    'author': 'Pavel Kulyov',
+    'author_email': 'kulyov.pavel@gmail.com',
+    'url': 'https://github.com/pkulev/lampy',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)

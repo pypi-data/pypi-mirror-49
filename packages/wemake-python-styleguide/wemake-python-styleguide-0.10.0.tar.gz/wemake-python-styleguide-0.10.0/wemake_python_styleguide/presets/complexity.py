59929b32cb75728e39400dc2873d4d21b3d06fa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:31 +0000

    Add package wemake-python-styleguide/0.10.0/wemake-python-styleguide-0.10.0.tar.gz

---
+++ packages/wemake-python-styleguide/wemake-python-styleguide-0.10.0.tar.gz/wemake-python-styleguide-0.10.0/wemake_python_styleguide/presets/complexity.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from wemake_python_styleguide.visitors.ast.complexity import (
+    classes,
+    counts,
+    function,
+    jones,
+    nested,
+    offset,
+)
+
+#: Used to store all complexity related visitors to be later passed to checker:
+COMPLEXITY_PRESET = (
+    function.FunctionComplexityVisitor,
+
+    jones.JonesComplexityVisitor,
+
+    nested.NestedComplexityVisitor,
+
+    offset.OffsetVisitor,
+
+    counts.ImportMembersVisitor,
+    counts.ModuleMembersVisitor,
+    counts.MethodMembersVisitor,
+    counts.ConditionsVisitor,
+    counts.ElifVisitor,
+    counts.TryExceptVisitor,
+    counts.YieldTupleVisitor,
+
+    classes.ClassComplexityVisitor,
+)

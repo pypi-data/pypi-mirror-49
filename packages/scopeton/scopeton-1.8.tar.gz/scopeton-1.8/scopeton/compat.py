1f99794fafd22d698f8b2938fde5afd804b0a2b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:19 +0000

    Add package scopeton/1.8/scopeton-1.8.tar.gz

---
+++ packages/scopeton/scopeton-1.8.tar.gz/scopeton-1.8/scopeton/compat.py
@@ -0,0 +1,46 @@
+import inspect
+import sys
+
+from scopeton import constants
+
+
+class _Python2NativeArgs:
+    args = ['self']
+
+def isPython3():
+    return sys.version_info.major >= 3
+
+
+def isClass(obj):
+    if not isPython3():
+        import new
+        return type(obj) is new.classobj or type(obj) is type
+    else:
+        import inspect
+        return inspect.isclass(obj)
+
+def objectIsInstance(obj):
+    if not isPython3():
+        import new
+        return type(obj) is new.instance or (hasattr(obj, "__class__") and hasattr(obj.__class__, "__name__"))
+    else:
+        import inspect
+        return inspect.isclass(obj)
+
+
+def getMethodSignature(method):
+    if isPython3():
+        return inspect.getfullargspec(method)
+    else:
+        try:
+            return inspect.getargspec(method)
+        except TypeError:
+            return _Python2NativeArgs()
+
+
+def hasInject(fn):
+    return hasattr(fn, constants.INJECT_FLAG)
+
+
+def isMethod(fn):
+    return inspect.ismethod(fn)
---
+++ packages/scopeton/scopeton-1.8.tar.gz/scopeton-1.8/scopeton/scope.py
@@ -0,0 +1,74 @@
+import logging
+from threading import RLock
+
+import typing
+
+from scopeton import compat, glob
+from scopeton.objects import Bean
+from scopeton.qualifier_tree import QualifierTree
+from scopeton.scopeTools import getBean_qualifier, callMethodByName, getClassTree, flatten, ScopetonException
+
+T = typing.TypeVar("T")
+
+class Scope(object):
+    '''this is servicelocator pattern implementation'''
+    def __init__(self, lock=False, initMethod="postConstruct", destroyMethod="preDestroy", parent=None):
+        self._singletons = QualifierTree()
+        self._beans = QualifierTree()
+        self.lock = lock or RLock()       # type: RLock
+        self.initMethod = initMethod
+        self.destroyMethod = destroyMethod
+        self.parent = parent    #type: Scope
+
+    def getInstance(self, name: T) -> T:
+        with self.lock:
+            return self._getInstance(getBean_qualifier(name))
+
+    def _getInstance(self, qualifier):
+
+        suitableQualifier = self._beans.find_suitable_qualifier(qualifier)
+
+        if self._singletons.contains(suitableQualifier):
+            return self._singletons.find_by_qualifier_name(suitableQualifier)
+
+        bean = self._beans.find_by_qualifier_name(suitableQualifier)
+        glob.lastScope = self
+        if compat.hasInject(bean.cls.__init__):
+            instance = bean.cls()
+        elif len(compat.getMethodSignature(bean.cls.__init__).args) == 2:
+            instance = bean.cls(self)
+        elif len(compat.getMethodSignature(bean.cls.__init__).args) > 2:
+            raise ScopetonException("Invalid number of parameters for bean constructor, maybe @Inject() decorator forgotten: {}".format(compat.getMethodSignature(bean.cls.__init__).args))
+        else:
+            instance = bean.cls()
+
+        if bean.singleton:
+            self.registerInstance(suitableQualifier, instance)
+
+        return instance
+
+    def registerInstance(self, name, instance):
+        qualifier = getBean_qualifier(name)
+        suitableQualifier = self._beans.find_suitable_qualifier(qualifier)
+        logging.debug("Suitable qualifier for {} is: {}".format(qualifier, suitableQualifier))
+        self._singletons.register(suitableQualifier, instance)
+
+    def registerBean(self, *args):
+        with self.lock:
+            for bean in args:
+                if not isinstance(bean, Bean):
+                    bean = Bean(bean)
+                self._registerBean(bean)
+
+    def _registerBean(self, bean):
+        """
+        :type bean: Bean
+        """
+        for name in bean.qualifier_tree:
+            logging.debug("Registering: {} as {}".format(name, bean))
+            self._beans.register(name, bean)
+
+    def runServices(self):
+        for bean in self._beans.get_all_objects():
+            if bean.service:
+                callMethodByName(self.getInstance(bean), self.initMethod)

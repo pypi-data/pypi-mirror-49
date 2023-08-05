547b9b7c1c6ad32d12cea31cd7da1e88ace1278e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:02 +0000

    Add package zlapp/1.3.6/zlapp-1.3.6.zip

---
+++ packages/zlapp/zlapp-1.3.6.zip/zlapp-1.3.6/zlapp/etl3/app_unit.py
@@ -0,0 +1,27 @@
+from zlapp.etl3 import app_bd 
+from zlapp.etl3 import app_bd_dt
+from zlapp.etl3 import app_t_gg_ent_bridge
+from zlapp.etl3 import app_gg_zhongbiao
+from zlapp.etl3 import app_qy_zhongbiao 
+from zlapp.etl3 import app_app_qy_zz
+from zlapp.etl3 import app_app_qy_zcry
+
+from zlapp.etl3 import app_app_gg_zhongbiao
+from zlapp.etl3 import app_app_gg_qy_query 
+from zlapp.etl3 import app_t_bd_xflv 
+
+
+app_bd.est()
+app_bd_dt.est()
+app_t_gg_ent_bridge.est()
+app_gg_zhongbiao.est()
+app_qy_zhongbiao.est()
+app_app_qy_zz.est()
+app_app_qy_zcry.est()
+app_app_gg_zhongbiao.est()
+app_t_bd_xflv.est()
+
+
+
+
+

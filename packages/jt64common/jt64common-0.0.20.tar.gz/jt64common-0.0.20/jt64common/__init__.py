f783c3ed8925710ab5d66171d2c6b49ff648197b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:36 +0000

    Add package jt64common/0.0.20/jt64common-0.0.20.tar.gz

---
+++ packages/jt64common/jt64common-0.0.20.tar.gz/jt64common-0.0.20/jt64common/__init__.py
@@ -0,0 +1,14 @@
+version_info = (0, 0, 20)
+__title__ = "jt64common"
+__version__ = '.'.join(str(c) for c in version_info)
+__name__ = "jt64common"
+__author__ = "Greg Beam, KI7MT"
+__license__ = "GPLv3"
+__email__ = "ki7mt@yahoo.com"
+
+# Supported QT version List
+__qt_version_list__ = ['5.12.2', '5.12.3', '5.12.4', '5.13.0']
+__qt_version_dict__ = {'5.12.2': 'GCC 7.3.0 x86_64',
+                       '5.12.3': 'GCC 7.3.0 x86_64',
+                       '5.12.4': 'GCC 7.3.0 x86_64',
+                       '5.13.0': 'GCC 7.3.0 x86_64'}

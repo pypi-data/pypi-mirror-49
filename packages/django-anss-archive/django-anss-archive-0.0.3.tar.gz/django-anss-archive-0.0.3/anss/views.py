6d76f87b70b1383cb496e2e0d65c43563e63d728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:35 +0000

    Add package django-anss-archive/0.0.3/django-anss-archive-0.0.3.tar.gz

---
+++ packages/django-anss-archive/django-anss-archive-0.0.3.tar.gz/django-anss-archive-0.0.3/anss/views.py
@@ -0,0 +1,36 @@
+import json
+from anss.models import Feed
+from django.core import serializers
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+
+class BaseJsonView(TemplateView):
+
+    def to_json_response(self, content, **response_kwargs):
+        return HttpResponse(
+            content,
+            content_type='application/json',
+            **response_kwargs
+        )
+
+
+class LatestFeedView(BaseJsonView):
+
+    def get_context_data(self, **kwargs):
+        return Feed.objects.latest()
+
+    def render_to_response(self, context, **response_kwargs):
+        f = json.load(context.content)
+        d = json.dumps(f, indent=4)
+        return self.to_json_response(d)
+
+
+class FeedListView(BaseJsonView):
+
+    def get_context_data(self, **kwargs):
+        return Feed.objects.all()[:100]
+
+    def render_to_response(self, context, **response_kwargs):
+        object_list = serializers.serialize('json', context, indent=4)
+        return self.to_json_response(object_list)

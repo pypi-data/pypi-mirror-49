d3661f82c82748f8e40ae7106e3f43a7cdebc00f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:08 +0000

    Add package pyrchain/0.1.3/pyrchain-0.1.3.tar.gz

---
+++ packages/pyrchain/pyrchain-0.1.3.tar.gz/pyrchain-0.1.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+from pathlib import Path
+
+setuptools.setup(
+    name='pyrchain',
+    version='0.1.3',
+    author='RChain Cooperative',
+    author_email='tomas.virtus@rchain.coop',
+    description='Interface to RChain RNode RPC',
+    long_description=Path('README.md').read_text(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/rchain/pyrchain',
+    packages=setuptools.find_namespace_packages(include=['rchain', 'rchain.*']),
+    package_data={'rchain.vault': ['*.rho.tpl']},
+    install_requires=[
+        'grpcio',
+        'protobuf',
+        'ecdsa',
+        'python-bitcoinlib',
+    ],
+    extras_require={'dev': ['grpcio-tools', 'mypy']}
+)

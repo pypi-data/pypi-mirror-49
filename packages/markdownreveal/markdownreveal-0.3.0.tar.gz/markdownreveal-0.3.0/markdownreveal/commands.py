513f29a2e6a6e2730f9a405af0ef6c29fb192f3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:30 +0000

    Add package markdownreveal/0.3.0/markdownreveal-0.3.0.tar.gz

---
+++ packages/markdownreveal/markdownreveal-0.3.0.tar.gz/markdownreveal-0.3.0/markdownreveal/commands.py
@@ -0,0 +1,157 @@
+import sys
+import shlex
+from subprocess import run
+from subprocess import check_output
+from subprocess import CalledProcessError
+from tempfile import TemporaryDirectory
+from pathlib import Path
+from shutil import copytree
+from shutil import make_archive
+from shutil import rmtree
+
+import click
+from click_default_group import DefaultGroup
+from livereload import Server
+from watchdog.observers import Observer
+
+from .convert import Handler
+from .convert import generate
+from .config import load_config
+
+
+def shell(command):
+    """
+    Execute a shell command and return the output as a list of lines.
+    """
+    return check_output(shlex.split(command)).decode('utf').splitlines()
+
+
+@click.group(cls=DefaultGroup, default='show')
+@click.version_option(
+    prog_name='Markdownreveal',
+    message='%(prog)s %(version)s'
+)
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument('markdown_file')
+@click.option('-h', '--host', type=str, default='localhost',
+              help='Listen on IP (default: localhost).')
+@click.option('-p', '--port', type=int, default=8123,
+              help='Listen on port (default: 8123).')
+def show(markdown_file: Path, host: str='localhost', port: int=8123):
+    """
+    Visualize your presentation (default).
+    """
+    markdown_file = Path(markdown_file)
+
+    observer = Observer()
+    handler = Handler(markdown_file)
+    # Initial generation
+    generate(markdown_file)
+    observer.schedule(handler, '.', recursive=True)
+    observer.start()
+
+    server = Server()
+    config = load_config()
+    server.watch(str(config['output_path'] / '.reload'), delay=0)
+    server.serve(
+        root=str(config['output_path']),
+        restart_delay=0,
+        debug=True,
+        open_url=True,
+        open_url_delay=0,
+        host=host,
+        port=port,
+    )
+
+
+@cli.command()
+@click.argument('markdown_file')
+@click.option('-r', '--remote', type=str, default='origin',
+              help='Choose a specific remote.')
+def upload(markdown_file: Path, remote: str='origin'):
+    """
+    Upload your presentation.
+    """
+    markdown_file = Path(markdown_file)
+
+    try:
+        shell('git status')
+        remote_url = shell('git remote get-url --push ' + remote)[0]
+    except CalledProcessError:
+        return
+
+    if 'github' not in remote_url:
+        error = 'Uploading only supported for GitHub repositories!'
+        sys.stderr.write(error + '\n')
+        return
+
+    # We copy the directory because `git` does not follow symlinks...
+    with TemporaryDirectory() as tmpdir:
+        tmpdir = Path(tmpdir) / 'out'
+        generate(markdown_file)
+        config = load_config()
+        copytree(src=str(config['output_path']), dst=str(tmpdir))
+
+        worktree = '--work-tree=%s' % tmpdir
+        current = shell('git rev-parse --abbrev-ref HEAD')[0]
+        shell('git checkout -B gh-pages')
+        shell('git %s add .' % worktree)
+        shell('git %s commit -m "Markdownreveal live presentation"' % worktree)
+        shell('git %s push -f -u %s gh-pages' % (worktree, remote))
+        shell('git %s checkout %s' % (worktree, current))
+
+    repo = Path(remote_url.split(':')[-1])
+    url = 'https://%s.github.io/%s/' % (repo.parent, repo.stem)
+    sys.stdout.write('Presentation uploaded to:\n\n' + url + '\n\n')
+
+
+@cli.command()
+@click.argument('markdown_file')
+def zip(markdown_file: Path):
+    """
+    Generate a ZIP file with the presentation.
+    """
+    markdown_file = Path(markdown_file)
+
+    # We copy the directory because `make_archive` cannot follow symlinks...
+    with TemporaryDirectory() as tmpdir:
+        tmpdir = Path(tmpdir) / 'out'
+        generate(markdown_file)
+        config = load_config()
+        copytree(src=str(config['output_path']), dst=str(tmpdir))
+        make_archive(markdown_file.stem, format='zip', root_dir=str(tmpdir))
+
+
+@cli.command()
+@click.argument('markdown_file')
+def pdf(markdown_file: str):
+    """
+    Generate a PDF file with the presentation.
+    """
+    if markdown_file.startswith('http'):
+        presentation = markdown_file
+    else:
+        markdown_file = Path(markdown_file)
+        generate(markdown_file)
+        config = load_config()
+        presentation = config['output_path'] / 'index.html'
+
+    name = 'slides.pdf'
+    command = 'decktape reveal {presentation} {name}'.format(
+        presentation=presentation,
+        name=name,
+    )
+    run(shlex.split(command))
+
+
+@cli.command()
+def clean():
+    """
+    Clean local Markdownreveal files.
+    """
+    config = load_config()
+    rmtree(str(config['local_path']), ignore_errors=True)
---
+++ packages/markdownreveal/markdownreveal-0.3.0.tar.gz/markdownreveal-0.3.0/markdownreveal/convert.py
@@ -0,0 +1,164 @@
+from distutils.version import LooseVersion
+from subprocess import check_output
+from sys import platform
+from threading import Timer
+from typing import List
+
+from pypandoc import convert_text
+from pypandoc import get_pandoc_version
+from watchdog.events import FileSystemEventHandler
+
+from .config import load_config
+from .local import initialize_localdir
+from .tweak import tweak_html
+from .typing import Config
+
+
+if platform == 'linux':
+    from watchdog.observers.inotify_buffer import InotifyBuffer
+    # Reduce watchdog default buffer delay for faster response times
+    InotifyBuffer.delay = 0.1
+
+
+def pandoc_extra_to_args(config: Config) -> List[str]:
+    """
+    Transform Pandoc extra configuration options into Pandoc arguments.
+
+    Parameters
+    ----------
+    config
+        Markdownreveal configuration.
+
+    Returns
+    -------
+        A list with all the Pandoc arguments.
+    """
+    arguments = []
+    for key, value in config['pandoc_extra'].items():
+        if isinstance(value, bool):
+            if value:
+                arguments.append('--' + key)
+        else:
+            arguments.append('--{}={}'.format(key, value))
+    return arguments
+
+
+def reveal_extra_to_args(config: Config) -> List[str]:
+    """
+    Transform reveal.js extra configuration options into Pandoc arguments.
+
+    Parameters
+    ----------
+    config
+        Markdownreveal configuration.
+
+    Returns
+    -------
+        A list with all the Pandoc arguments.
+    """
+    arguments = []
+    for key, value in config['reveal_extra'].items():
+        arguments.extend(['-V', '%s=%s' % (key, value)])
+    return arguments
+
+
+def markdown_to_reveal(text: str, config: Config) -> str:
+    """
+    Transform a Markdown input file to an HTML (reveal.js) output string.
+
+    Parameters
+    ----------
+    markdown_text
+        Markdown text to convert to HTML.
+    config
+        Markdownreveal configuration.
+
+    Returns
+    -------
+        The converted string.
+    """
+    extra_args = [
+        '-s',
+        '--slide-level=2',
+        '-V', 'revealjs-url=revealjs',
+    ]
+    if config['katex']:
+        pandoc_version = get_pandoc_version()
+        if LooseVersion(pandoc_version) < LooseVersion('2.0'):
+            extra_args.extend([
+                '--katex=katex/katex.min.js',
+                '--katex-stylesheet=katex/katex.min.css',
+            ])
+        else:
+            extra_args.extend([
+                '--katex=katex/',
+            ])
+    extra_args.extend(pandoc_extra_to_args(config))
+    extra_args.extend(reveal_extra_to_args(config))
+    input_format = 'markdown'
+    if config['emoji_codes']:
+        input_format += '+emoji'
+    output = convert_text(
+        source=text,
+        format=input_format,
+        to='revealjs',
+        extra_args=extra_args,
+    )
+
+    # HTML substitution
+    output = tweak_html(output, config)
+
+    return output
+
+
+def generate(markdown_file):
+    """
+    TODO
+    """
+    # Reload config
+    config = load_config()
+
+    # Initialize localdir
+    initialize_localdir(config)
+
+    # rsync
+    command = [
+        'rsync',
+        '--delete',
+        '--exclude', 'katex',
+        '--exclude', 'revealjs',
+        '--exclude', 'markdownrevealstyle',
+        '--exclude', '.git',
+        '-av',
+        '%s/' % markdown_file.resolve().parent,
+        '%s/' % config['output_path'],
+    ]
+    check_output(command)
+
+    # Convert from markdown
+    output = markdown_to_reveal(markdown_file.read_text(), config)
+
+    # Write index.html
+    index = config['output_path'] / 'index.html'
+    index.write_text(output)
+
+    # Reload view
+    with open(str(config['output_path'] / '.reload'), 'a') as f:
+        f.write('x\n')
+
+
+class Handler(FileSystemEventHandler):
+    def __init__(self, markdown_file, period=0.1):
+        self.markdown_file = markdown_file
+        self.period = period
+        self.timer = None
+        self.set_timer()
+
+    def on_any_event(self, event):
+        if self.timer.is_alive():
+            return
+        self.set_timer()
+        self.timer.start()
+
+    def set_timer(self):
+        self.timer = Timer(self.period, generate, args=(self.markdown_file,))

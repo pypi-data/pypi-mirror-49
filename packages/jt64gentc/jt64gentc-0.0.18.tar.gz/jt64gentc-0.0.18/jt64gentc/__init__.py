e8bcd2eecd1e1027aea64a633a066335a84eaf48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:15 +0000

    Add package jt64gentc/0.0.18/jt64gentc-0.0.18.tar.gz

---
+++ packages/jt64gentc/jt64gentc-0.0.18.tar.gz/jt64gentc-0.0.18/jt64gentc/__init__.py
@@ -0,0 +1,8 @@
+version_info = (0, 0, 18)
+__title__ = "jt64gentc"
+__version__ = '.'.join(str(c) for c in version_info)
+__name__ = "jt64gentc"
+__author__ = "Greg Beam, KI7MT"
+__license__ = "GPLv3"
+__email__ = "ki7mt@yahoo.com"
+__summary__ = "Generates Tool Chain files for each QT version supported"

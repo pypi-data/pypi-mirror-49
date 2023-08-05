dd21f3af5d9d2593ddd71114f435b46e121cacfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:57 +0000

    Add package platypush/0.10.4/platypush-0.10.4.tar.gz

---
+++ packages/platypush/platypush-0.10.4.tar.gz/platypush-0.10.4/platypush/backend/sensor/mcp3008.py
@@ -0,0 +1,22 @@
+from platypush.backend.sensor import SensorBackend
+from platypush.context import get_plugin
+
+
+class SensorMcp3008Backend(SensorBackend):
+    """
+    Backend to poll analog sensor values from an MCP3008 chipset
+    (https://learn.adafruit.com/raspberry-pi-analog-to-digital-converters/mcp3008)
+
+    Requires:
+
+        * ``adafruit-mcp3008`` (``pip install adafruit-mcp3008``)
+        * The :mod:`platypush.plugins.gpio.sensor.mcp3008` plugin configured
+    """
+
+    def get_measurement(self):
+        """ get_measurement implementation """
+        plugin = get_plugin('gpio.sensor.mcp3008')
+        return plugin.get_data().output
+
+
+# vim:sw=4:ts=4:et:
---
+++ packages/platypush/platypush-0.10.4.tar.gz/platypush-0.10.4/platypush/backend/sensor/serial.py
@@ -0,0 +1,21 @@
+from platypush.backend.sensor import SensorBackend
+from platypush.context import get_plugin
+
+
+class SensorSerialBackend(SensorBackend):
+    """
+    This backend listens for new events from sensors connected through a serial
+    interface (like Arduino) acting as a wrapper for the ``serial`` plugin.
+
+    Requires:
+
+        * The :mod:`platypush.plugins.serial` plugin configured
+    """
+
+    def get_measurement(self):
+        """ Implementation of ``get_measurement`` """
+        plugin = get_plugin('serial')
+        return plugin.get_data().output
+
+
+# vim:sw=4:ts=4:et:

a3a4f1d73f500d66d6a60f3eeb75b57c7bf27b86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:41 +0000

    Add package raiden-contracts/0.26.0/raiden_contracts-0.26.0-py3-none-any.whl

---
+++ packages/raiden-contracts/raiden_contracts-0.26.0-py3-none-any.whl/raiden_contracts/utils/versions.py
@@ -0,0 +1,35 @@
+from typing import Optional
+
+from semver import compare
+
+
+def contracts_version_expects_deposit_limits(contracts_version: Optional[str]) -> bool:
+    """ Answers whether TokenNetworkRegistry of the contracts_vesion needs deposit limits """
+    if contracts_version is None:
+        # contracts_version == None means the stock version in development.
+        return True
+    if contracts_version == "0.3._":
+        return False
+    return compare(contracts_version, "0.9.0") > -1
+
+
+def contracts_version_with_max_token_networks(version: Optional[str]) -> bool:
+    if version is None:
+        # contracts_version == None means the stock version in development.
+        return True
+    if version == "0.3._":
+        return False
+    if version == "0.8.0_unlimited":
+        return False
+    return compare(version, "0.9.0") >= 0
+
+
+def contracts_version_provides_services(version: Optional[str]) -> bool:
+    if version is None:
+        # contracts_version == None means the stock version in development.
+        return True
+    if version == "0.3._":
+        return False
+    if version == "0.8.0_unlimited":
+        return True
+    return compare(version, "0.8.0") >= 0

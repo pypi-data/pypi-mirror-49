5f01d8722cae50b830d8338c2ed143831f486e2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:47 +0000

    Add package bmcc/0.2.0/bmcc-0.2.0.tar.gz

---
+++ packages/bmcc/bmcc-0.2.0.tar.gz/bmcc-0.2.0/src/gibbs.c
@@ -0,0 +1,164 @@
+/**
+ * Gibbs Sampler
+ */
+
+#include <Python.h>
+
+#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+#define NO_IMPORT_ARRAY
+#define PY_ARRAY_UNIQUE_SYMBOL BAYESIAN_CLUSTERING_C_ARRAY_API
+#include <numpy/arrayobject.h>
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <mixture.h>
+#include <misc_math.h>
+#include <type_check.h>
+
+#include <stdio.h>
+#include <stdbool.h>
+#include "normal_wishart.h"
+
+/**
+ * Execute gibbs iteration.
+ * @param data data array; row-major
+ * @param assignments assignment vector
+ * @param components vector containing component structs, stored as void *.
+ *      Component methods are responsible for casting to the correct type.
+ * @return true if returned without error
+ */
+bool gibbs_iter(
+    double *data, uint16_t *assignments,
+    struct mixture_model_t *model)
+{
+
+    // Assignment weight vector: exponentially-overallocated
+    double *weights = (double *) malloc(sizeof(double) * BASE_VEC_SIZE);
+    int vec_size = BASE_VEC_SIZE;
+
+    // For each sample:
+    for(int idx = 0; idx < model->size; idx++) {
+
+        double *point = &data[idx * model->dim];
+
+        // Check assignments vector for error
+        if(assignments[idx] >= model->num_clusters) {
+            PyErr_SetString(
+                PyExc_IndexError,
+                "Assignment index greater than the number of clusters.");
+            return false;
+        }
+
+        // Remove from currently assigned cluster
+        model->comp_methods->remove(
+            model->clusters[assignments[idx]], model->comp_params, point);
+
+        // Remove empty components
+        remove_empty(model, assignments);
+
+        // Handle vector resizing
+        if(model->num_clusters + 1 >= vec_size) {
+            free(weights);
+            vec_size *= 2;
+            double *weights_new = (double *) malloc(sizeof(double) * vec_size);
+
+            // Check for error
+            if(weights_new == NULL) {
+                free(weights);
+                PyErr_SetString(
+                    PyExc_MemoryError,
+                    "Could not allocate weight vector in memory.");
+                return false;
+            }
+            else { weights = weights_new; }
+        }
+
+        // Get assignment weights
+        for(int i = 0; i < model->num_clusters; i++) {
+            void *c_i = model->clusters[i];
+            weights[i] = (
+                model->comp_methods->loglik_ratio(
+                    c_i, model->comp_params, point) +
+                model->model_methods->log_coef(
+                    model->model_params,
+                    model->comp_methods->get_size(c_i),
+                    model->num_clusters));
+        }
+
+        // Get new cluster weight
+        weights[model->num_clusters] = (
+            model->model_methods->log_coef_new(
+                model->model_params,
+                model->num_clusters) +
+            model->comp_methods->loglik_new(model->comp_params, point));
+
+        // Sample new
+        int new = sample_log_weighted(weights, model->num_clusters + 1);
+
+        // New cluster?
+        if(new == model->num_clusters) {
+            bool success_new = add_component(model);
+            // Check for allocation failure
+            if(!success_new) {
+                free(weights);
+                return NULL;
+            }
+        }
+
+        // Update component
+        model->comp_methods->add(
+            model->clusters[new], model->comp_params, point);
+        assignments[idx] = new;
+    }
+
+    free(weights);
+
+    return true;
+}
+
+
+/**
+ * Run Gibbs Iteration. See docstring (sourced from gibbs.h) for details on
+ * Python calling.
+ */
+PyObject *gibbs_iter_py(PyObject *self, PyObject *args)
+{
+    // Get args
+    PyArrayObject *data_py;
+    PyArrayObject *assignments_py;
+    PyObject *model_py;
+    bool success = PyArg_ParseTuple(
+        args, "O!O!O",
+        &PyArray_Type, &data_py,
+        &PyArray_Type, &assignments_py,
+        &model_py);
+    if(!success) { return NULL; }
+    if(!type_check(data_py, assignments_py)) { return NULL; }
+
+    // Unpack capsules
+    struct mixture_model_t *model = (
+        (struct mixture_model_t *) PyCapsule_GetPointer(
+            model_py, MIXTURE_MODEL_API));
+
+    // GIL free zone ----------------------------------------------------------    
+    Py_INCREF(data_py);
+    Py_INCREF(assignments_py);
+    Py_INCREF(model_py);
+    // Py_BEGIN_ALLOW_THREADS;
+
+    bool gibbs_success = gibbs_iter(
+        (double *) PyArray_DATA(data_py),
+        (uint16_t *) PyArray_DATA(assignments_py),
+        model);
+
+    // Py_END_ALLOW_THREADS;
+    Py_DECREF(data_py);
+    Py_DECREF(assignments_py);
+    Py_DECREF(model_py);
+    // ------------------------------------------------------------------------
+
+    // Check for exception
+    if(!gibbs_success) { return NULL; }
+    else { Py_RETURN_NONE; }
+}
---
+++ packages/bmcc/bmcc-0.2.0.tar.gz/bmcc-0.2.0/src/mixture.c
@@ -0,0 +1,265 @@
+/**
+ * Core Mixture Structs and Routines
+ */
+
+#include <Python.h>
+
+#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+#define NO_IMPORT_ARRAY
+#define PY_ARRAY_UNIQUE_SYMBOL BAYESIAN_CLUSTERING_C_ARRAY_API
+#include <numpy/arrayobject.h>
+
+#include "type_check.h"
+#include "mixture.h"
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <normal_wishart.h>
+
+
+/**
+ * Create components struct
+ * @return allocated components_t; initialized empty
+ */
+struct mixture_model_t *create_mixture(
+    ComponentMethods *comp_methods,
+    ModelMethods *model_methods,
+    PyObject *params,
+    uint32_t size, uint32_t dim)
+{
+    // Allocate vector
+    struct mixture_model_t *mixture = (
+        (struct mixture_model_t *) malloc(sizeof(struct mixture_model_t)));
+    mixture->mem_size = BASE_VEC_SIZE;
+    mixture->num_clusters = 0;
+    mixture->clusters = (void **) malloc(
+        sizeof(struct mixture_model_t) * mixture->mem_size);
+
+    // Bind methods, params, dim
+    mixture->comp_methods = comp_methods;
+    mixture->model_methods = model_methods;
+
+    mixture->comp_params = comp_methods->params_create(params);
+    if(mixture->comp_params == NULL) {
+        goto error;
+    }
+
+    mixture->model_params = model_methods->create(params);
+    if(mixture->model_params == NULL) {
+        comp_methods->params_destroy(mixture->comp_params);
+        goto error;
+    }
+
+    mixture->size = size;
+    mixture->dim = dim;
+
+    return mixture;
+
+    // Error condition: free all and return NULL
+    error:
+        free(mixture->clusters);
+        free(mixture);
+        return NULL;
+}
+
+
+/**
+ * Destroy mixture model struct
+ * @param model : mixture_model_t struct to destroy
+ */
+void destroy_mixture(PyObject *model_py)
+{
+    struct mixture_model_t *model_tc = (
+        (struct mixture_model_t *) PyCapsule_GetPointer(
+            model_py, MIXTURE_MODEL_API));
+    for(int i = 0; i < model_tc->num_clusters; i++) {
+        model_tc->comp_methods->destroy(model_tc->clusters[i]);
+        free(model_tc->clusters[i]);
+    }
+    free(model_tc);
+}
+
+
+/**
+ * Add Component: allocates new component, and appends to components capsule
+ * @param components: components_t struct containing components
+ * @return true if addition successful
+ */
+bool add_component(struct mixture_model_t *model)
+{
+    // Handle exponential over-allocation
+    if(model->mem_size <= model->num_clusters) {
+        model->mem_size *= 2;
+        void **clusters_new = (void *) realloc(
+            model->clusters, sizeof(void *) * model->mem_size);
+
+        if(clusters_new == NULL) {
+            PyErr_SetString(
+                PyExc_MemoryError, "Failed to allocate new component");
+            return false;
+        }
+        else { model->clusters = clusters_new; }
+    }
+
+    // Allocate new
+    model->clusters[model->num_clusters] = (
+        model->comp_methods->create(model->comp_params));
+    model->num_clusters += 1;
+
+    return true;
+}
+
+
+/**
+ * Remove component from component vector.
+ * @param components : component vector struct
+ * @param idx : index to remove
+ */
+void remove_component(struct mixture_model_t *model, int idx)
+{
+    model->comp_methods->destroy(model->clusters[idx]);
+    free(model->clusters[idx]);
+    for(int i = idx; i < (model->num_clusters - 1); i++) {
+        model->clusters[i] = model->clusters[i + 1];
+    }
+}
+
+
+/**
+ * Remove empty components.
+ * @param components components_t vector to remove from
+ * @param assignments assignment vector; indices in assignments greater than
+ *      the empty vector index are decremented.
+ * @return true if a empty component was removed.
+ */
+bool remove_empty(struct mixture_model_t *model, uint16_t *assignments)
+{
+    // Search for empty
+    for(int i = 0; i < model->num_clusters; i++) {
+        if(model->comp_methods->get_size((model->clusters)[i]) == 0) {
+            // Deallocate component; remove component from vector
+            remove_component(model, i);
+            // Decrement values
+            for(int j = 0; j < model->size; j++) {
+                if(assignments[j] > i) { assignments[j] -= 1; }
+            }
+            model->num_clusters -= 1;
+            return true;
+        }
+    }
+    return false;
+}
+
+
+/**
+ * Initialize model capsules. See docstring (sourced from mixture.h) for more
+ * details.
+ */
+PyObject *init_model_capsules_py(PyObject *self, PyObject *args)
+{
+    // Get args
+    PyArrayObject *data_py;
+    PyArrayObject *assignments_py;
+    PyObject *comp_methods_py;
+    PyObject *model_methods_py;
+    PyObject *params;
+    bool success = PyArg_ParseTuple(
+        args, "O!O!OOO",
+        &PyArray_Type, &data_py,
+        &PyArray_Type, &assignments_py,
+        &comp_methods_py, &model_methods_py,
+        &params);
+    if(!success) { return NULL; }
+    if(!type_check(data_py, assignments_py)) { return NULL; }
+
+    // Unpack capsules
+    ComponentMethods *comp_methods = (
+        (ComponentMethods *) PyCapsule_GetPointer(
+            comp_methods_py, COMPONENT_METHODS_API));
+    ModelMethods *model_methods = (
+        (ModelMethods *) PyCapsule_GetPointer(
+            model_methods_py, MODEL_METHODS_API));
+
+    uint16_t *asn = PyArray_DATA(assignments_py);
+    double *data = PyArray_DATA(data_py);
+    int size = PyArray_DIM(data_py, 0);
+    int dim = PyArray_DIM(data_py, 1);
+
+    // Create mixture_t
+    struct mixture_model_t *mixture = create_mixture(
+        comp_methods, model_methods, params, size, dim);
+    if(mixture == NULL) { return NULL; }
+
+    // Get number of components
+    int max = 0;
+    for(int i = 0; i < size; i++) {
+        if(asn[i] > max) { max = asn[i]; }
+    }
+
+    // Allocate enough components
+    for(int i = 0; i <= max; i++) {
+        bool success_add = add_component(mixture);
+
+        // Check for error
+        if(!success_add) {
+            for(int j = 0; j < i; j++) {
+                comp_methods->destroy(mixture->clusters[j]);
+            }
+            free(mixture->clusters);
+            free(mixture);
+            return NULL;
+        }
+    }
+
+    // Add points
+    for(int i = 0; i < size; i++) {
+        comp_methods->add(
+            mixture->clusters[asn[i]],
+            mixture->comp_params,
+            &(data[i * dim]));
+    }
+
+    return PyCapsule_New(mixture, MIXTURE_MODEL_API, &destroy_mixture);
+}
+
+
+/**
+ * Update mixture model hyperparameters
+ */
+PyObject *update_mixture_py(PyObject *self, PyObject *args)
+{
+    PyObject *mixture;
+    PyObject *update;
+    if(!PyArg_ParseTuple(args, "OO", &mixture, &update)) { return NULL; }
+
+    struct mixture_model_t *mixture_tc = (
+        (struct mixture_model_t *) PyCapsule_GetPointer(
+            mixture, MIXTURE_MODEL_API));
+
+    if(mixture_tc->model_methods->update != NULL) {
+        mixture_tc->model_methods->update(mixture_tc->model_params, update);
+    }
+
+    Py_RETURN_NONE;
+}
+
+
+/**
+ * Update component hyperparameters
+ */
+PyObject *update_components_py(PyObject *self, PyObject *args)
+{
+    PyObject *mixture;
+    PyObject *update;
+    if(!PyArg_ParseTuple(args, "OO", &mixture, &update)) { return NULL; }
+
+    struct mixture_model_t *mixture_tc = (
+        (struct mixture_model_t *) PyCapsule_GetPointer(
+            mixture, MIXTURE_MODEL_API));
+
+    if(mixture_tc->comp_methods->update != NULL) {
+        mixture_tc->comp_methods->update(mixture_tc->comp_params, update);
+    }
+
+    Py_RETURN_NONE;
+}

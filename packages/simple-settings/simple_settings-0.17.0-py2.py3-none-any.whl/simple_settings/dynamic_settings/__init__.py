b21388b136c4a2dd8ef32b2717b57e5dc09670dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:01 +0000

    Add package simple-settings/0.17.0/simple_settings-0.17.0-py2.py3-none-any.whl

---
+++ packages/simple-settings/simple_settings-0.17.0-py2.py3-none-any.whl/simple_settings/dynamic_settings/__init__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import importlib
+
+from simple_settings.constants import (
+    DYNAMIC_SETTINGS_KEY,
+    SPECIAL_SETTINGS_KEY
+)
+
+DYNAMIC_SETTINGS_MAPPING = {
+    'consul': 'simple_settings.dynamic_settings.consul_reader',
+    'database': 'simple_settings.dynamic_settings.database_reader',
+    'memcached': 'simple_settings.dynamic_settings.memcached_reader',
+    'redis': 'simple_settings.dynamic_settings.redis_reader',
+    's3': 'simple_settings.dynamic_settings.s3_reader'
+}
+
+
+def get_dynamic_reader(settings_dict):
+    dynamic_settings_conf = (
+        settings_dict.get(SPECIAL_SETTINGS_KEY, {}).get(DYNAMIC_SETTINGS_KEY)
+    )
+    if not dynamic_settings_conf:
+        return
+
+    reader_backend = dynamic_settings_conf['backend']
+    if reader_backend in DYNAMIC_SETTINGS_MAPPING:
+        reader_backend = DYNAMIC_SETTINGS_MAPPING[reader_backend]
+
+    reader_module = importlib.import_module(reader_backend)
+    reader = reader_module.Reader(dynamic_settings_conf)
+
+    return reader

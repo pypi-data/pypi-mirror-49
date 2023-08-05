753016c8a3a0ef8ab26e8fe44181a018dc79c018
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:49 +0000

    Add package aws-cdk.aws-greengrass/1.0.0/aws-cdk.aws-greengrass-1.0.0.tar.gz

---
+++ packages/aws-cdk.aws-greengrass/aws-cdk.aws-greengrass-1.0.0.tar.gz/aws-cdk.aws-greengrass-1.0.0/setup.py
@@ -0,0 +1,43 @@
+import json
+import setuptools
+
+kwargs = json.loads("""
+{
+    "name": "aws-cdk.aws-greengrass",
+    "version": "1.0.0",
+    "description": "The CDK Construct Library for AWS::Greengrass",
+    "url": "https://github.com/awslabs/aws-cdk",
+    "long_description_content_type": "text/markdown",
+    "author": "Amazon Web Services",
+    "project_urls": {
+        "Source": "https://github.com/awslabs/aws-cdk.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "aws_cdk.aws_greengrass",
+        "aws_cdk.aws_greengrass._jsii"
+    ],
+    "package_data": {
+        "aws_cdk.aws_greengrass._jsii": [
+            "aws-greengrass@1.0.0.jsii.tgz"
+        ],
+        "aws_cdk.aws_greengrass": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "jsii~=0.14.0",
+        "publication>=0.0.3",
+        "aws-cdk.core~=1.0,>=1.0.0"
+    ]
+}
+""")
+
+with open('README.md') as fp:
+    kwargs['long_description'] = fp.read()
+
+
+setuptools.setup(**kwargs)

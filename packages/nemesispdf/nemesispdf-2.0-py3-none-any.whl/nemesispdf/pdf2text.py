643e1c1e3a225d48a18f96f120af784e32c21648
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:16 +0000

    Add package nemesispdf/2.0/nemesispdf-2.0-py3-none-any.whl

---
+++ packages/nemesispdf/nemesispdf-2.0-py3-none-any.whl/nemesispdf/pdf2text.py
@@ -0,0 +1,13 @@
+"""This is a module to change a pdf file to text"""
+
+
+def convert(path):
+    """
+    Parameters:
+    path (str): insert a path of a pdf file.
+
+    Returns:
+    a text file as string.
+    """
+    print("pdf2text")
+    print("version2")

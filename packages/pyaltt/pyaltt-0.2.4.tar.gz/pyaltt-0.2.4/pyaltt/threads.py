799f1918f0eb5e952016c0ae3995c5af43d6c95e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:25 +0000

    Add package pyaltt/0.2.4/pyaltt-0.2.4.tar.gz

---
+++ packages/pyaltt/pyaltt-0.2.4.tar.gz/pyaltt-0.2.4/pyaltt/threads.py
@@ -0,0 +1,40 @@
+__author__ = "Altertech Group, http://www.altertech.com/"
+__copyright__ = "Copyright (C) 2018-2019 Altertech Group"
+__license__ = "Apache License 2.0"
+__version__ = "0.2.4"
+
+import threading
+
+from functools import wraps
+
+
+class LocalProxy(threading.local):
+
+    def get(self, attr, default=None):
+        return getattr(self, attr, default)
+
+    def has(self, attr):
+        return hasattr(self, attr)
+
+    def set(self, attr, value):
+        return setattr(self, attr, value)
+
+    def clear(self, attr):
+        return delattr(self, attr) if hasattr(self, attr) else True
+
+
+def background_job(f, *args, **kwargs):
+
+    @wraps(f)
+    def start_thread(*args, **kw):
+        t = threading.Thread(
+            group=kwargs.get('group'),
+            target=f,
+            name=kwargs.get('name'),
+            args=args,
+            kwargs=kw)
+        if kwargs.get('daemon'): t.setDaemon(True)
+        t.start()
+        return t
+
+    return start_thread

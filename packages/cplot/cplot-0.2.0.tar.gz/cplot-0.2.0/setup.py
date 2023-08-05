33ba041887f5499110fc405bb3edc6c8ed956b90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:00 +0000

    Add package cplot/0.2.0/cplot-0.2.0.tar.gz

---
+++ packages/cplot/cplot-0.2.0.tar.gz/cplot-0.2.0/setup.py
@@ -0,0 +1,40 @@
+import codecs
+import os
+
+from setuptools import find_packages, setup
+
+# https://packaging.python.org/single_source_version/
+base_dir = os.path.abspath(os.path.dirname(__file__))
+about = {}
+with open(os.path.join(base_dir, "cplot", "__about__.py"), "rb") as f:
+    exec(f.read(), about)
+
+
+def read(fname):
+    return codecs.open(os.path.join(base_dir, fname), encoding="utf-8").read()
+
+
+setup(
+    name="cplot",
+    packages=find_packages(),
+    version=about["__version__"],
+    author=about["__author__"],
+    author_email=about["__author_email__"],
+    description="Plotting tools for complex-valued functions",
+    long_description=read("README.md"),
+    url="https://github.com/nschloe/cplot",
+    license=about["__license__"],
+    platforms="any",
+    install_requires=["colorio", "matplotlib", "numpy"],
+    long_description_content_type="text/markdown",
+    python_requires=">=3",
+    classifiers=[
+        about["__status__"],
+        about["__license__"],
+        "Intended Audience :: Science/Research",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering",
+    ],
+)

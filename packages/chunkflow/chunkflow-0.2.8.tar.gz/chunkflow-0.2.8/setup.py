04f7cc48159615ff2c455563742694dd2e09f4ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:01 +0000

    Add package chunkflow/0.2.8/chunkflow-0.2.8.tar.gz

---
+++ packages/chunkflow/chunkflow-0.2.8.tar.gz/chunkflow-0.2.8/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+version = '0.2.8'
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='chunkflow',
+    description='Large Scale 3d Convolution Net Inference',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='Apache License 2.0',
+    version=version,
+    author='Jingpeng Wu',
+    author_email='jingpeng.wu@gmail.com',
+    packages=find_packages(exclude=['chunkflow.test']),
+    url='https://github.com/seung-lab/chunkflow',
+    install_requires=[
+        'click',
+        'numpy',
+        'cloud-volume',
+        'scikit-image',
+        'boto3',
+        'h5py',
+        'tifffile',
+        'neuroglancer',
+        'tinybrain',
+        'zmesh'
+    ],
+    entry_points='''
+        [console_scripts]
+        chunkflow=chunkflow.flow:cli
+    ''',
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+)

9aeaab4dbf580e09b39c45a818e5f2f4e26a3afe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:16 +0000

    Add package nemesispdf/2.0/nemesispdf-2.0.tar.gz

---
+++ packages/nemesispdf/nemesispdf-2.0.tar.gz/nemesispdf-2.0/README.md
@@ -0,0 +1,2 @@
+It is a program to convert PDF files to a text or image file.
+Good Luck
---
+++ packages/nemesispdf/nemesispdf-2.0.tar.gz/nemesispdf-2.0/nemesispdf/pdf2image.py
@@ -0,0 +1,13 @@
+"""This is a module to change a pdf file to image"""
+
+
+def convert(path):
+    """
+    Parameters:
+    path (str): insert a path of a pdf file.
+
+    Returns:
+    an image as a file.
+    """
+    print("pdf2image")
+    print("version2")

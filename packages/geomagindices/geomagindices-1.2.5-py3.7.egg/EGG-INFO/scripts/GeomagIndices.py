aae7ce850f6ce4b842097797afb470a26d6bb557
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:30 +0000

    Add package geomagindices/1.2.5/geomagindices-1.2.5-py3.7.egg

---
+++ packages/geomagindices/geomagindices-1.2.5-py3.7.egg/EGG-INFO/scripts/GeomagIndices.py
@@ -0,0 +1,16 @@
+#!c:\Miniconda3\python.exe
+"""
+simple demo of retrieving common geomagnetic indices by date
+"""
+import geomagindices as gi
+
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    p = ArgumentParser()
+    p.add_argument('date', help='time of observation yyyy-mm-ddTHH:MM:ss')
+    a = p.parse_args()
+
+    inds = gi.get_indices(a.date)
+
+    print(inds)

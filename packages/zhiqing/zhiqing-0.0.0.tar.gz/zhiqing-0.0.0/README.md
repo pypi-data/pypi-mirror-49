ce9d12054bb8a84d000f3910367228e8b4797fc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:31 +0000

    Add package zhiqing/0.0.0/zhiqing-0.0.0.tar.gz

---
+++ packages/zhiqing/zhiqing-0.0.0.tar.gz/zhiqing-0.0.0/README.md
@@ -0,0 +1,4 @@
+zhiqing
+=======================
+
+Zhiqing's personal module.
---
+++ packages/zhiqing/zhiqing-0.0.0.tar.gz/zhiqing-0.0.0/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
+from setuptools import find_packages, setup
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'zhiqing'))
+from version import __version__ as version
+
+setup(
+    name='zhiqing',
+    version=version,
+    description='Zhiqing',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Zhiqing Xiao',
+    author_email='xzq.xiaozhiqing@gmail.com',
+    python_requires='>=3.5.0',
+    url='http://github.com/zhiqingxiao/zhiqingxiao.github.io/',
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python',
+    ],
+)

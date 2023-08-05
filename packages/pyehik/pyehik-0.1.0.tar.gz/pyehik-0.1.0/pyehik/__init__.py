7489a9bce0d3c981f0e870162003dac83d04f7e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:36 +0000

    Add package pyehik/0.1.0/pyehik-0.1.0.tar.gz

---
+++ packages/pyehik/pyehik-0.1.0.tar.gz/pyehik-0.1.0/pyehik/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for pyehik."""
+
+__author__ = """Jovany Leandro G.C"""
+__email__ = 'bit4bit@riseup.net'
+__version__ = '0.1.0'
+import ctypes
+from . import config
+
+def load_sdk(pathname):
+    HC = ctypes.cdll.LoadLibrary(pathname)
+
+    from . import sdk
+    sdk.HC = HC
+    return sdk
+

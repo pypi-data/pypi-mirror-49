5154bc77529ad21e26f7b9a11c6d534ebe7f3b9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:58 +0000

    Add package ezpg/0.1.1b2/ezpg-0.1.1b2.tar.gz

---
+++ packages/ezpg/ezpg-0.1.1b2.tar.gz/ezpg-0.1.1b2/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['ezpg']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['psycopg2-binary>=2.8,<3.0']
+
+setup_kwargs = {
+    'name': 'ezpg',
+    'version': '0.1.1b2',
+    'description': '',
+    'long_description': None,
+    'author': 'Matt Chapman',
+    'author_email': 'Matt@NinjitsuWeb.com',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)

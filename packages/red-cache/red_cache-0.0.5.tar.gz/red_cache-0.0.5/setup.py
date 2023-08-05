111c5e6978c846d2908787653f01c05542b63006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:07 +0000

    Add package red-cache/0.0.5/red_cache-0.0.5.tar.gz

---
+++ packages/red-cache/red_cache-0.0.5.tar.gz/red_cache-0.0.5/setup.py
@@ -0,0 +1,18 @@
+import io
+
+from setuptools import setup, find_packages
+
+__author__ = 'Memory_Leak<irealing@163.com>'
+with io.open("README.md", 'rt', encoding='utf-8') as f:
+    readme = f.read()
+setup(
+    name='red_cache',
+    version='0.0.5',
+    author='Memory_Leak',
+    url="https://gitee.com/irealing/red_cache",
+    author_email='irealing@163.com',
+    packages=find_packages(),
+    install_requires=('redis>=3.0.1',),
+    long_description=readme,
+    long_description_content_type='text/markdown',
+)

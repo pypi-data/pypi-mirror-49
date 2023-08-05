14b50015d7ecdedcd35bef20204b6838cce08f5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:50 +0000

    Add package syncurity-utils/0.2.7/syncurity_utils-0.2.7-py3-none-any.whl

---
+++ packages/syncurity-utils/syncurity_utils-0.2.7-py3-none-any.whl/syncurity_utils/models.py
@@ -0,0 +1,41 @@
+""" syncurity_utls.models
+
+This module contains objects representing API constructs or other common objects/singletons
+
+:copyright: (c) 2019 Syncurity
+:license: Apache 2.0, see LICENSE.txt for more details
+"""
+
+
+class FactGroup(object):
+    """ Everything has a Fact Group"""
+    def __init__(self):
+        pass
+
+
+class Alert(FactGroup):
+    """ Alerts are a 'specialized' fact group """
+    def __init__(self):
+        super(Alert, self).__init__()
+        pass
+
+
+class Step(FactGroup):
+    """ Steps are a 'specialized' fact group """
+    def __init__(self):
+        super(Step, self).__init__()
+        pass
+
+
+class Task(FactGroup):
+    """ Tasks are a 'specialized' fact group """
+    def __init__(self):
+        super(Task, self).__init__()
+        pass
+
+
+class Incident(FactGroup):
+    """ Incidents are a 'specialized' fact group """
+    def __init__(self):
+        super(Incident, self).__init_()
+        pass

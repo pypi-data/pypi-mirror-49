cc63b063c8da529f8beb51415c928167cb5d67c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:38 +0000

    Add package pygalmesh/0.3.6/pygalmesh-0.3.6.tar.gz

---
+++ packages/pygalmesh/pygalmesh-0.3.6.tar.gz/pygalmesh-0.3.6/pygalmesh/__init__.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+#
+# https://github.com/pybind/pybind11/issues/1004
+from _pygalmesh import (
+    DomainBase,
+    Translate,
+    Rotate,
+    Scale,
+    Stretch,
+    Intersection,
+    Union,
+    Difference,
+    Extrude,
+    Ball,
+    Cuboid,
+    Ellipsoid,
+    Tetrahedron,
+    Cone,
+    Cylinder,
+    Torus,
+    HalfSpace,
+    Polygon2D,
+    RingExtrude,
+)
+
+from .main import (
+    generate_mesh,
+    generate_periodic_mesh,
+    generate_surface_mesh,
+    generate_volume_mesh_from_surface_mesh,
+    generate_from_inr,
+)
+
+from .__about__ import (
+    __author__,
+    __author_email__,
+    __copyright__,
+    __license__,
+    __version__,
+    __maintainer__,
+    __status__,
+)
+
+__all__ = [
+    "__author__",
+    "__author_email__",
+    "__copyright__",
+    "__license__",
+    "__version__",
+    "__maintainer__",
+    "__status__",
+    #
+    "DomainBase",
+    "Translate",
+    "Rotate",
+    "Scale",
+    "Stretch",
+    "Intersection",
+    "Union",
+    "Difference",
+    "Extrude",
+    "Ball",
+    "Cuboid",
+    "Ellipsoid",
+    "Tetrahedron",
+    "Cone",
+    "Cylinder",
+    "Torus",
+    "HalfSpace",
+    "Polygon2D",
+    "RingExtrude",
+    #
+    "generate_mesh",
+    "generate_periodic_mesh",
+    "generate_surface_mesh",
+    "generate_volume_mesh_from_surface_mesh",
+    "generate_from_inr",
+]

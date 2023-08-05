ca5f287c3127ddfd23edc8a590f5570af73d44f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:42:50 +0000

    Add package servicelayer/1.5.3/servicelayer-1.5.3.tar.gz

---
+++ packages/servicelayer/servicelayer-1.5.3.tar.gz/servicelayer-1.5.3/servicelayer/settings.py
@@ -0,0 +1,24 @@
+from servicelayer import env
+
+# Redis cache
+REDIS_URL = env.get('REDIS_URL')
+REDIS_SHORT = 84700
+REDIS_LONG = REDIS_SHORT * 200
+REDIS_EXPIRE = env.to_int('REDIS_EXPIRE', REDIS_SHORT * 7)
+
+# Aleph client API settings
+ALEPH_HOST = env.get('MEMORIOUS_ALEPH_HOST')
+ALEPH_HOST = env.get('ALEPH_HOST', ALEPH_HOST)
+
+ALEPH_API_KEY = env.get('MEMORIOUS_ALEPH_API_KEY')
+ALEPH_API_KEY = env.get('ALEPH_API_KEY', ALEPH_API_KEY)
+
+# Amazon client credentials
+AWS_KEY_ID = env.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_KEY = env.get('AWS_SECRET_ACCESS_KEY')
+AWS_REGION = env.get('AWS_REGION', 'eu-west-1')
+
+# Storage type (either 's3', 'gs', or 'file', i.e. local file system):
+ARCHIVE_TYPE = env.get('ARCHIVE_TYPE', 'file')
+ARCHIVE_BUCKET = env.get('ARCHIVE_BUCKET')
+ARCHIVE_PATH = env.get('ARCHIVE_PATH')

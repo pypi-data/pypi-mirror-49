f91f941037c41475e400690ac89392481cada920
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:27 +0000

    Add package pipeloyus/2.1/pipeloyus-2.1.tar.gz

---
+++ packages/pipeloyus/pipeloyus-2.1.tar.gz/pipeloyus-2.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+#from distutils.core import setup
+
+setup(
+    name = 'pipeloyus',
+    packages = ['pipeloyus'],
+    include_package_data=True,
+    version = '2.1',
+    description = 'Librerias creadas para interactuar con los servicios de AWS.',
+    author='Eloy Barahona',
+    author_email="eloy.barahona@gmail.com",
+    license="GPLv3",
+    url="",
+    classifiers = ["Programming Language :: Python :: 3",
+                   "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+                   "Development Status :: 4 - Beta", "Intended Audience :: Developers",
+                   "Operating System :: OS Independent"],
+)

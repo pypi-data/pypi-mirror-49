f712830955b21e71adf4b06cb25e581d6727d687
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:27 +0000

    Add package pvault/1.1.0a0/pvault-1.1.0a0-py3-none-any.whl

---
+++ packages/pvault/pvault-1.1.0a0-py3-none-any.whl/pvault-1.1.0a0.data/scripts/pv.py
@@ -0,0 +1,45 @@
+#!python
+import sys
+import pv_password as password
+from pv_management import create_database, query_db
+from sqlite3 import Error
+from getpass import getpass
+
+
+def auth():
+    p = getpass("Enter master password: ")
+    if p == query_db("master")[2]:
+        return True
+    else:
+        print("Invalid password")
+
+
+# todo: implement help system
+
+
+if __name__ == "__main__":
+    try:
+        query_db("master")
+    except Error:
+        create_database()
+        password.set_master_password()
+
+    if auth():
+        command = sys.argv[1:]
+        if not command:
+            raise Exception("pv.py help for available commands")
+
+        switcher = {
+            "generate": password.generate,
+            "account": password.get,
+            "accounts": password.get,
+            "delete": password.delete,
+            "save": password.save
+        }
+
+        event = switcher.get(command[0], "error")
+
+        if not isinstance(event, str):
+            event(command)
+        else:
+            raise Exception("Invalid argument")

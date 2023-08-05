a400a18c072a13ac698e677b744b9a1fc54f8f2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:40 +0000

    Add package unpack/0.0.8/unpack-0.0.8.tar.gz

---
+++ packages/unpack/unpack-0.0.8.tar.gz/unpack-0.0.8/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="unpack",
+    version="0.0.8",
+    author="Mihir Joshi",
+    author_email="idealab@library.illinois.edu",
+    description="Library for unpacking request object for 3deposit services",
+    packages=["unpack"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

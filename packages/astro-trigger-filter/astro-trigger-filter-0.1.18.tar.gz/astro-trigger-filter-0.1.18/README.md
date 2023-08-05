d2b8c686638232db8684c3d410959cd266f76ca4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:05 +0000

    Add package astro-trigger-filter/0.1.18/astro-trigger-filter-0.1.18.tar.gz

---
+++ packages/astro-trigger-filter/astro-trigger-filter-0.1.18.tar.gz/astro-trigger-filter-0.1.18/README.md
@@ -0,0 +1,14 @@
+# astro-trigger-filter
+A Python package for filtering radio astronomical pulse matched template searches.
+
+
+## Installation
+
+```
+pip install astro-trigger-filter
+```
+
+## Documentation
+
+<https://astro-trigger-filter.readthedocs.io/>
+
---
+++ packages/astro-trigger-filter/astro-trigger-filter-0.1.18.tar.gz/astro-trigger-filter-0.1.18/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    README = fh.read()
+
+# This call to setup() does all the work
+setuptools.setup(
+    name="astro-trigger-filter",
+    version="0.1.18",
+    description="A package for filtering triggers from radio astronomical matched template pulse searches.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    keywords=['astronomy','filter'],
+    url="https://github.com/sitmo/astro-trigger-filter",
+    author="Thijs van den Berg",
+    author_email="thijs@sitmo.com",
+    license="MIT",
+    scripts=['bin/ambertf'],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    #packages=["astro-trigger-cluster"],
+    packages=setuptools.find_packages(exclude=["tests", "examples", "test*", "private"]),
+    include_package_data=True,
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "realpython=reader.__main__:main",
+        ]
+    },
+)

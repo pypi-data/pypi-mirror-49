b91aa1e67ea3c80ccf8d63229bd265477aff679d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:34 +0000

    Add package unitrans-evalir/0.0.1.6/unitrans-evalir-0.0.1.6.tar.gz

---
+++ packages/unitrans-evalir/unitrans-evalir-0.0.1.6.tar.gz/unitrans-evalir-0.0.1.6/universal_translator/inputHandler.py
@@ -0,0 +1,21 @@
+
+class InputHandler:
+
+    def parseInput(self, gInput: str):
+        '''Handles input parsing'''
+        splitInput = gInput.split()
+        if len(splitInput) < 3:
+            print("invalid input: expecting 3 columns.")
+            return
+        parsedInput = []
+        for i in range(0, len(splitInput) - 1, 3):
+            newArr = [splitInput[i],
+                      splitInput[i + 1], splitInput[i + 2]]
+            parsedInput.append(newArr)
+        return parsedInput
+
+    def writeToFile(self, pathname: str, convertedUnits):
+        ''' Writes the results to a file'''
+        osFile = open(pathname, 'w', encoding="utf8")
+        for convertedUnit in convertedUnits:
+            osFile.write(convertedUnit + "\n")

6ab40e81133e21fe9bd9231128d62d8d7b10d7e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:16 +0000

    Add package jt64version/0.0.18/jt64version-0.0.18-py3-none-any.whl

---
+++ packages/jt64version/jt64version-0.0.18-py3-none-any.whl/jt64version/__init__.py
@@ -0,0 +1,9 @@
+version_info = (0, 0, 18)
+__title__ = "jt64version"
+__version__ = '.'.join(str(c) for c in version_info)
+__name__ = "jt64version"
+__author__ = "Greg Beam, KI7MT"
+__license__ = "GPLv3"
+__email__ = "ki7mt@yahoo.com"
+
+__all__ = [__title__, __version__, __name__, __author__, __license__, __email__]

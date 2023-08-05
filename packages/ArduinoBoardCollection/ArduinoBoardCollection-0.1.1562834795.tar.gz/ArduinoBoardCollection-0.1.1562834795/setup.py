d178f94a262c0d16e99bdf7bf1a159997fea8a28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:46 +0000

    Add package ArduinoBoardCollection/0.1.1562834795/ArduinoBoardCollection-0.1.1562834795.tar.gz

---
+++ packages/ArduinoBoardCollection/ArduinoBoardCollection-0.1.1562834795.tar.gz/ArduinoBoardCollection-0.1.1562834795/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setup = dict(
+    name="ArduinoBoardCollection",
+    version="0.1",
+    author="Julian Kimmig",
+    author_email="julian-kimmig@gmx.net",
+    description="Some boards for ArduinoController",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/JulianKimmig/arduino_board_collection",
+    include_package_data=True,
+    packages=setuptools.find_packages(),
+    install_requires=['ArduinoController'],
+classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+if __name__ == "__main__":
+    setuptools.setup(**setup)

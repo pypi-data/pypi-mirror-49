d7ff546f3251c1ceea9323a425b54f05e074c096
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:47 +0000

    Add package taskmonk-sdk/0.1/taskmonk-sdk-0.1.tar.gz

---
+++ packages/taskmonk-sdk/taskmonk-sdk-0.1.tar.gz/taskmonk-sdk-0.1/taskmonk-sdk/TaskMonkClient.py
@@ -0,0 +1,7 @@
+class TaskMonkClient:
+	def __init__(self, server):
+		self.server = server
+
+	def dummy(self):
+		print(self.server)
+	
---
+++ packages/taskmonk-sdk/taskmonk-sdk-0.1.tar.gz/taskmonk-sdk-0.1/taskmonk-sdk/__init__.py
@@ -0,0 +1,1 @@
+from taskmonk-sdk.TaskMonkClient import TaskMonkClient

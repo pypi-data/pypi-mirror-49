84c41eeb710626f1e11bdb2ef8b41c5925f78fe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:11 +0000

    Add package timetoolkit/1.7/timetoolkit-1.7.tar.gz

---
+++ packages/timetoolkit/timetoolkit-1.7.tar.gz/timetoolkit-1.7/setup.py
@@ -0,0 +1,16 @@
+﻿# !usr\bin\env python
+# -*- coding:utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='timetoolkit',
+    version='1.7',
+    author='张乐涛',
+    author_email='1668151593@qq.com',
+    url='https://github.com',
+    description='Some great tools about time.',
+    long_description='''
+Some great tools about time.
+''',
+    packages=['timetoolkit'],
+    install_requires=[])

6e08f3c497fd8bfa3e0b0637a3aadc3f60613281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:38 +0000

    Add package aiidalab-widgets-base/1.0.0a4/aiidalab-widgets-base-1.0.0a4.tar.gz

---
+++ packages/aiidalab-widgets-base/aiidalab-widgets-base-1.0.0a4.tar.gz/aiidalab-widgets-base-1.0.0a4/aiidalab_widgets_base/__init__.py
@@ -0,0 +1,13 @@
+# pylint: disable=unused-import
+from __future__ import absolute_import
+from aiida import load_profile
+load_profile()
+
+from .structures import StructureUploadWidget  # noqa
+from .structures_multi import MultiStructureUploadWidget  # noqa
+from .codes import CodeDropdown, AiiDACodeSetup, extract_aiidacodesetup_arguments  # noqa
+from .computers import SshComputerSetup, extract_sshcomputersetup_arguments # noqa
+from .computers import AiidaComputerSetup, extract_aiidacomputer_arguments # noqa
+from .display import aiidalab_display # noqa
+
+__version__ = "1.0.0a4"

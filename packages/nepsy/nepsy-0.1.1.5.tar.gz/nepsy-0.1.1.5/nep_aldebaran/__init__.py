ae8e5b1d67d9fa535c04c03b6e88d2ef64da1aa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:10 +0000

    Add package nepsy/0.1.1.5/nepsy-0.1.1.5.tar.gz

---
+++ packages/nepsy/nepsy-0.1.1.5.tar.gz/nepsy-0.1.1.5/nep_aldebaran/__init__.py
@@ -0,0 +1,19 @@
+from SayText import*
+from BodyMove import*
+from AutonomusLife import*
+from BodyMove import*
+from BehaviorManager import*
+from Volume import*
+from SpeechRecognition import*
+from Leds import*
+from Audio import*
+from Tablet import*
+
+
+
+
+
+
+
+
+

0070fbc9fe6442bd7aca37b8862810574374cbc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:18 +0000

    Add package atomtools/1.7.5/atomtools-1.7.5-py2.py3-none-any.whl

---
+++ packages/atomtools/atomtools-1.7.5-py2.py3-none-any.whl/atomtools/__init__.py
@@ -0,0 +1,17 @@
+"""
+independent chemical symbols
+"""
+
+
+__version__ = '1.7.5'
+def version():
+    return __version__
+
+from . import name, unit, geo
+from . import file, string, system
+from . import types
+from .filetype import filetype, list_supported_formats, support_multiframe
+from .status import Status
+
+
+

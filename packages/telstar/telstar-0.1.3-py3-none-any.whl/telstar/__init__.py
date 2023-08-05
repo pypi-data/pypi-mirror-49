cc0f38df984e101b6efb24d8a5c5d8052b1fa274
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:19 +0000

    Add package telstar/0.1.3/telstar-0.1.3-py3-none-any.whl

---
+++ packages/telstar/telstar-0.1.3-py3-none-any.whl/telstar/__init__.py
@@ -0,0 +1,19 @@
+"""
+Telstar is a package to write producer and consumers groups against redis streams.
+"""
+__version__ = "0.1.3"
+
+import logging
+
+from .com import StagedMessage
+
+log = logging.getLogger(__package__).addHandler(logging.NullHandler())
+
+
+def stage(topic, data):
+    e = StagedMessage.create(topic=topic, data=data)
+    return e.msg_uid
+
+
+def staged():
+    return [e.to_msg() for e in StagedMessage.unsent()]

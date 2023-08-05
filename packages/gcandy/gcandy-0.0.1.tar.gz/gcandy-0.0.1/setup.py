1ddb6ac024da6288389e511fb87d94b5f9176f9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:25 +0000

    Add package gcandy/0.0.1/gcandy-0.0.1.tar.gz

---
+++ packages/gcandy/gcandy-0.0.1.tar.gz/gcandy-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='gcandy',
+      version='0.0.1',
+      description='Python wrappers for Google REST APIs.',
+      url='https://github.com/Marco-Christiani/drive-candy',
+      author='Marco Christiani',
+      author_email='mchristiani2017@gmail.com',
+      license='GPLv3',
+      packages=['drivecandy'],
+      zip_safe=False)

dd9b37b45eb5d8ad8b86c28f6f70aacea4c26e31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:55 +0000

    Add package NeodroidAgent/0.2.4/NeodroidAgent-0.2.4-py36-none-any.whl

---
+++ packages/NeodroidAgent/NeodroidAgent-0.2.4-py36-none-any.whl/agent/configs/agent_test_configs/pg_test_config.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from agent.architectures import MLP
+
+from .base_test_config import *
+
+__author__ = 'cnheider'
+
+CONFIG_NAME = __name__
+CONFIG_FILE = __file__
+
+CONNECT_TO_RUNNING = False
+RENDER_ENVIRONMENT = True
+
+EVALUATION_FUNCTION = torch.nn.CrossEntropyLoss
+
+ENVIRONMENT_NAME = 'CartPole-v1'
+#ENVIRONMENT_NAME = 'Pendulum-v0'
+MODEL_DIRECTORY = PROJECT_APP_PATH.user_data / ENVIRONMENT_NAME / LOAD_TIME / 'models'
+CONFIG_DIRECTORY = PROJECT_APP_PATH.user_data / ENVIRONMENT_NAME / LOAD_TIME / 'configs'
+LOG_DIRECTORY = PROJECT_APP_PATH.user_log / ENVIRONMENT_NAME / LOAD_TIME
+
+DISCOUNT_FACTOR = 0.95
+PG_ENTROPY_REG = 3e-3
+
+# Architecture
+POLICY_ARCH_SPEC = GDCS(CategoricalMLP, NOD(**{
+  'input_shape':            None,  # Obtain from environment
+  'hidden_layer_activation':torch.relu,
+  'hidden_layers':          None,
+  'output_shape':           None,  # Obtain from environment
+  'use_bias':               True,
+  }))

11de58dd272edd37683e9fce5d2fd75b6062ff30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:09 +0000

    Add package bruno-util/1.4.7/bruno_util-1.4.7.tar.gz

---
+++ packages/bruno-util/bruno_util-1.4.7.tar.gz/bruno_util-1.4.7/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='bruno_util',
+      version='1.4.7',
+      description='Catch-all package for utilities useful to Bruno Beltran',
+      long_description=readme(),
+      author='Bruno Beltran',
+      author_email='brunobeltran0@gmail.com',
+      packages=['bruno_util', 'bruno_util.mittag_leffler'],
+      license='MIT',
+      classifiers=[
+          'Development Status :: 2 - Pre-Alpha',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: MIT License',
+          'Topic :: Utilities'
+      ],
+      keywords='parameter scanning scientific search testing',
+      url='https://github.com/brunobeltran/bruno_util',
+      install_requires=['pandas', 'numpy', 'matplotlib', 'numba', 'seaborn',
+          'pscan', 'multi_locus_analysis'], # include other packages I wrote that are useful
+     )

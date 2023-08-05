c4b63a219a4ae4c4b9c5bd9ea439c5ceb7e7580f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:02 +0000

    Add package vertool/1.0.1/vertool-1.0.1.tar.gz

---
+++ packages/vertool/vertool-1.0.1.tar.gz/vertool-1.0.1/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+from os import environ
+from setuptools import setup, find_packages
+
+
+# Get long description about this python project.
+with open('README.md', encoding='utf-8') as readme_file:
+    long_description = readme_file.read()
+
+
+setup(
+    name='vertool',
+    version=environ.get('BITBUCKET_TAG') or environ['VT_LOCAL_VERSION'],
+    python_requires='>=3.4',
+    url='https://bitbucket.org/celadonteam/vertool',
+    author='Celadon Developers',
+    author_email='opensource@celadon.ae',
+    description='The project versioning controller',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    scripts=['vertool/__main__.py'],
+    entry_points={
+        'console_scripts': [
+            'vertool=vertool.__main__:main',
+        ],
+    },
+    zip_safe=False,
+    classifiers=[
+        'Environment :: Console',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Other Audience',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Debuggers',
+        'Topic :: Software Development',
+    ],
+)

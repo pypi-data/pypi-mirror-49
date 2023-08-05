194f59f2093bd6a7020cbf765049973c697e6f36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:41:29 +0000

    Add package uproot/3.8.0/uproot-3.8.0-py2.py3-none-any.whl

---
+++ packages/uproot/uproot-3.8.0-py2.py3-none-any.whl/uproot/write/objects/TObjString.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+# BSD 3-Clause License; see https://github.com/scikit-hep/uproot/blob/master/LICENSE
+
+from __future__ import absolute_import
+
+import struct
+
+import numpy
+
+import uproot.const
+import uproot.write.compress
+import uproot.write.sink.cursor
+
+class TObjString(object):
+    def __init__(self, string):
+        if isinstance(string, bytes):
+            self.value = string
+        else:
+            self.value = string.encode("utf-8")
+
+    fClassName = b"TObjString"
+    fTitle = b"Collectable string class"
+
+    _format = struct.Struct(">IHHII")
+
+    def write(self, context, cursor, name, compression, key, keycursor):
+        cnt = numpy.int64(self.length(name) - 4) | uproot.const.kByteCountMask
+        vers = 1
+        givenbytes = cursor.return_fields(self._format, cnt, vers, 1, 0, uproot.const.kNotDeleted) + cursor.return_string(self.value)
+        uproot.write.compress.write(context, cursor, givenbytes, compression, key, keycursor)
+
+    def length(self, name):
+        return self._format.size + uproot.write.sink.cursor.Cursor.length_string(self.value)
---
+++ packages/uproot/uproot-3.8.0-py2.py3-none-any.whl/uproot/write/sink/cursor.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+
+# BSD 3-Clause License; see https://github.com/scikit-hep/uproot/blob/master/LICENSE
+
+from __future__ import absolute_import
+
+import struct
+
+class Cursor(object):
+    def __init__(self, index):
+        self.index = index
+
+    def skip(self, numbytes):
+        self.index += numbytes
+
+    def update_fields(self, sink, format, *args):
+        sink.write(format.pack(*args), self.index)
+
+    def write_fields(self, sink, format, *args):
+        self.update_fields(sink, format, *args)
+        self.index += format.size
+
+    def return_fields(self, format, *args):
+        return format.pack(*args)
+
+    @staticmethod
+    def length_string(string):
+        if len(string) < 255:
+            return len(string) + 1
+        else:
+            return len(string) + 5
+
+    @staticmethod
+    def length_strings(strings):
+        return sum(Cursor.length_string(x) for x in strings)
+
+    _format_byte = struct.Struct("B")
+    _format_byteint = struct.Struct(">Bi")
+    def update_string(self, sink, data):
+        if len(data) < 255:
+            sink.write(self._format_byte.pack(len(data)), self.index)
+            sink.write(data, self.index + 1)
+        else:
+            sink.write(self._format_byteint.pack(255, len(data)), self.index)
+            sink.write(data, self.index + 5)
+
+    def return_string(self, data):
+        if len(data) < 255:
+            return self._format_byte.pack(len(data)) + data
+        else:
+            return self._format_byteint.pack(255, len(data)) + data
+
+    def write_string(self, sink, data):
+        self.update_string(sink, data)
+        self.index += self.length_string(data)
+
+    def update_cstring(self, sink, data):
+        sink.write(data, self.index)
+        sink.write(b"\x00")
+
+    def write_cstring(self, sink, data):
+        self.update_cstring(sink, data)
+        self.index += len(data) + 1
+
+    def update_data(self, sink, data):
+        sink.write(data, self.index)
+
+    def write_data(self, sink, data):
+        self.update_data(sink, data)
+        self.index += len(data)
+
+    def return_array(self, data):
+        return data.tostring()
+
+    def update_array(self, sink, data):
+        sink.write(data.tostring(), self.index)
+
+    def write_array(self, sink, data):
+        self.update_array(sink, data)
+        self.index += data.nbytes

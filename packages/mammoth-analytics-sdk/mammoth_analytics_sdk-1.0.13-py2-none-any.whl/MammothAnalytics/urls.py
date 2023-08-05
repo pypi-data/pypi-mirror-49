bf949db42c505b405ad08ae1efb252ffa0926b58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:09 +0000

    Add package mammoth-analytics-sdk/1.0.13/mammoth_analytics_sdk-1.0.13-py2-none-any.whl

---
+++ packages/mammoth-analytics-sdk/mammoth_analytics_sdk-1.0.13-py2-none-any.whl/MammothAnalytics/urls.py
@@ -0,0 +1,12 @@
+from __future__ import unicode_literals
+import logging
+from . import const
+
+log = logging.getLogger(__name__)
+
+
+def get_url(subpath, api_url=None):
+    if not api_url:
+        api_url = const.API
+    return api_url + subpath
+

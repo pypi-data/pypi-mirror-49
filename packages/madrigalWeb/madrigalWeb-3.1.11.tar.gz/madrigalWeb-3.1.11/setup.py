2b815f0cfe8531a37cacf0e04d6433a437af253c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:35 +0000

    Add package madrigalWeb/3.1.11/madrigalWeb-3.1.11.tar.gz

---
+++ packages/madrigalWeb/madrigalWeb-3.1.11.tar.gz/madrigalWeb-3.1.11/setup.py
@@ -0,0 +1,21 @@
+"""set up file for the Python Madrigal Remote API
+
+$Id: setup.py 6548 2018-07-16 16:16:05Z brideout $
+"""
+import os, os.path, sys
+
+from distutils.core import setup
+    
+setup(name="madrigalWeb",
+      version="3.1.11",
+      description="Remote Madrigal Python API",
+      author="Bill Rideout",
+      author_email="brideout@haystack.mit.edu",
+      url="http://cedar.openmadrigal.org",
+      packages=["madrigalWeb"],
+      keywords = ['Madrigal'],
+      scripts=['madrigalWeb/globalIsprint.py', 'madrigalWeb/globalDownload.py',
+               'madrigalWeb/examples/exampleMadrigalWebServices.py']
+      )
+
+    

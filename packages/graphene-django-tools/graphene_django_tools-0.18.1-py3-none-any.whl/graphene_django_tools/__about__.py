8669d93b360655073cddb450eca26752f4ba17d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:13 +0000

    Add package graphene-django-tools/0.18.1/graphene_django_tools-0.18.1-py3-none-any.whl

---
+++ packages/graphene-django-tools/graphene_django_tools-0.18.1-py3-none-any.whl/graphene_django_tools/__about__.py
@@ -0,0 +1,9 @@
+"""Tools for use [graphene-django](https://github.com/graphql-python/graphene-django).  """
+
+__version__ = '0.18.1'
+__author__ = 'NateScarlet@Gmail.com'
+
+# TODO: Add tests
+# TODO: Support other python version
+# TODO: Sort field on nodemodel
+# TODO: Find documentation for MutationContext.root

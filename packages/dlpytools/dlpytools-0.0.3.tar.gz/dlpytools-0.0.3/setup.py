819facc079d1cc68096debc84f17674ce8cc6395
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:16 +0000

    Add package dlpytools/0.0.3/dlpytools-0.0.3.tar.gz

---
+++ packages/dlpytools/dlpytools-0.0.3.tar.gz/dlpytools-0.0.3/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: wxnacy(wxnacy@gmail.com)
+# Description:
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'dlpytools',
+    version = '0.0.3',
+    keywords='tools',
+    description = 'common tools',
+    license = 'MIT License',
+    url = 'https://github.com/GuNanHai/dltools',
+    author = 'guhai',
+    author_email = 'bysideen@gmail.com',
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = 'any',
+)

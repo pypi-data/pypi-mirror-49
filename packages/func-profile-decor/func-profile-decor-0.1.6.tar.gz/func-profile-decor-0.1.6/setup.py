be21ee42082dc5cdb26c6f511184fb1681162f85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:57 +0000

    Add package func-profile-decor/0.1.6/func-profile-decor-0.1.6.tar.gz

---
+++ packages/func-profile-decor/func-profile-decor-0.1.6.tar.gz/func-profile-decor-0.1.6/setup.py
@@ -0,0 +1,28 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.rst").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="func-profile-decor",
+    version="0.1.6",
+    description="Profile a function using a simple decorator",
+    long_description=README,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/asanoryu/function_profile",
+    author="Vladislav Shumanov",
+    author_email="vl.shumanov@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    include_package_data=True,
+    install_requires=["memory_profiler"],
+)

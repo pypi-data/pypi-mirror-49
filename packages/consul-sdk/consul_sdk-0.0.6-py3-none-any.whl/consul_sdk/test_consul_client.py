97099029bad3ecd280ebe7afa8826e4222252af2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:41:09 +0000

    Add package consul-sdk/0.0.6/consul_sdk-0.0.6-py3-none-any.whl

---
+++ packages/consul-sdk/consul_sdk-0.0.6-py3-none-any.whl/consul_sdk/test_consul_client.py
@@ -0,0 +1,43 @@
+import time
+from os import environ
+
+import pytest
+
+from consul_sdk.client import ConsulClient
+
+
+@pytest.fixture
+def client():
+    yield ConsulClient(url=environ.get("CONSUL_ADDR"))
+
+
+def test_create_and_get_session(client):
+    session_id = client.create_session(30)
+    assert session_id == client.get_session(session_id)
+
+
+def test_renew_session(client):
+    session_id = client.create_session(10)
+    assert session_id == client.get_session(session_id)
+
+    time.sleep(9)
+    client.renew_session(session_id)
+
+    time.sleep(2)
+    assert client.get_session(session_id) is not None
+
+
+@pytest.mark.skip("Failing")
+def test_get_session_returns_none_if_session_expired(client):
+    session_id = client.create_session(10)
+    assert session_id == client.get_session(session_id)
+
+    time.sleep(15)
+    assert client.get_session(session_id) is None
+
+
+def test_acquire_and_release_lock(client):
+    session_id = client.create_session(10)
+
+    assert client.acquire_lock("my-key", session_id)
+    assert client.release_lock("my-key", session_id)

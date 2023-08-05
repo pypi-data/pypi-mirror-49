c26b1d47ea27ee0f207636f86fb2ee52723f7d0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:49 +0000

    Add package django-json-ordered/0.2/django-json-ordered-0.2.tar.gz

---
+++ packages/django-json-ordered/django-json-ordered-0.2.tar.gz/django-json-ordered-0.2/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+from io import open
+
+from setuptools import find_packages, setup
+
+from django_json_ordered.meta import VERSION
+
+setup(
+    name='django-json-ordered',
+    version=str(VERSION),
+    description="It's an ordered json field",
+    long_description=open('README.md', encoding='utf-8').read(),
+    author='Rafael Gontijo',
+    author_email='rafaelgontijowinter@gmail.com',
+    url='https://github.com/rafaelgontijo/django-json-ordered',
+    license='MIT',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "django >= 1.8.0",
+    ],
+    include_package_data=True,
+)

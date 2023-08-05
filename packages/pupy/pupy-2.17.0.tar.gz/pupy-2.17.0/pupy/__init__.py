b9ec733656fa7a9393e88efaa5d1f1337cea75a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:49 +0000

    Add package pupy/2.17.0/pupy-2.17.0.tar.gz

---
+++ packages/pupy/pupy-2.17.0.tar.gz/pupy-2.17.0/pupy/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# ~ Jesse K. Rubin ~ Pretty Useful Python
+from pupy.foreign import (
+    chunks,
+    digits_list,
+    dirs_gen,
+    files_gen,
+    int_from_digits,
+    is_permutation,
+    iter_product,
+    rotate,
+    rotations_gen,
+)
+from pupy.savings_n_loads import (
+    ljson,
+    load_jasm,
+    lpak,
+    lstr,
+    lstring,
+    safepath,
+    save_jasm,
+    savings,
+    sjson,
+    spak,
+    sstr,
+    sstring,
+)
+from pupy.sh import cd, cp, ls, mv, parent_dirpath, pwd, rm
+from pupy.utils import timestamp

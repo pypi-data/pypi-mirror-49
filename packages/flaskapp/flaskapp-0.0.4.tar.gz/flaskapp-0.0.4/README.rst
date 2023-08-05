a238f12275d5a8b94a53c251c6d2fc5e68fe07dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:50 +0000

    Add package flaskapp/0.0.4/flaskapp-0.0.4.tar.gz

---
+++ packages/flaskapp/flaskapp-0.0.4.tar.gz/flaskapp-0.0.4/README.rst
@@ -0,0 +1,5 @@
+flaskapp
+============================================================
+| Create a flask project one by one is very troublesome to me.
+| This project can create base files of flask project.
+
---
+++ packages/flaskapp/flaskapp-0.0.4.tar.gz/flaskapp-0.0.4/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="flaskapp",
+    python_requires='>=3.4',
+    version="0.0.4",
+    author="ksg97031",
+    author_email="ksg97031@gmail.com",
+    description="Manage flask project simply",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/ksg97031/flaskapp",
+    packages=setuptools.find_packages(),
+    scripts=['bin/flaskapp'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

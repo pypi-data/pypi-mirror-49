55d584b77cdb83d772438ab95b4caa780d9ae838
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:23 +0000

    Add package os-tools/1.0/os_tools-1.0.tar.gz

---
+++ packages/os-tools/os_tools-1.0.tar.gz/os_tools-1.0/os_tools/Tools.py
@@ -0,0 +1,11 @@
+###########################################################################
+#
+# this module meant to provide general handling to python files
+#
+###########################################################################
+
+
+# will print an array
+def print_arr(arr, divider='\n'):
+    print(divider.join(arr))
+

bdd5ce71f258230db5c378761ec9cf66923c666b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:52 +0000

    Add package quantum-queen/1.0.2/quantum-queen-1.0.2.tar.gz

---
+++ packages/quantum-queen/quantum-queen-1.0.2.tar.gz/quantum-queen-1.0.2/queen/ext/awx/ansible/__init__.py
@@ -0,0 +1,6 @@
+from .inventorygroup import InventoryGroupModule
+from .inventoryhost import InventoryHostModule
+from .jobtemplate import JobTemplateModule
+from .jobtemplatecredential import JobTemplateCredentialModule
+from .schema import SchemaModule
+from .team import TeamModule

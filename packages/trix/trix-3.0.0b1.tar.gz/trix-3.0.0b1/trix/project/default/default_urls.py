8fdceac786444a3dd35ab8c059561614f244ed6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:50 +0000

    Add package trix/3.0.0b1/trix-3.0.0b1.tar.gz

---
+++ packages/trix/trix-3.0.0b1.tar.gz/trix-3.0.0b1/trix/project/default/default_urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+from trix.trix_admin.cradmin import CrAdminInstance
+
+admin.autodiscover()
+
+default_urls = [
+    url(r'^authenticate/', include('trix.trix_auth.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^a/', include(CrAdminInstance.urls())),
+    url(r'^', include('trix.trix_student.urls')),
+]
---
+++ packages/trix/trix-3.0.0b1.tar.gz/trix-3.0.0b1/trix/trix_admin/css_icon_map.py
@@ -0,0 +1,11 @@
+from cradmin_legacy import css_icon_map
+
+#: Font-awesome icon map for the ``cradmin_icon`` template tag.
+# NOTE this is font-awesome v4 syntax, this is because cradmin uses font-awesome 4
+css_icon_map.FONT_AWESOME.update({'arrow-up': 'fa fa-arrow-up',
+                                  'database': 'fa fa-database',
+                                  'link': 'fa fa-link',
+                                  'chart-bar': 'fa fa-chart-bar',
+                                  'home': 'fa fa-home',
+                                  'wrench': 'fa fa-wrench'})
+FONT_AWESOME = css_icon_map.FONT_AWESOME

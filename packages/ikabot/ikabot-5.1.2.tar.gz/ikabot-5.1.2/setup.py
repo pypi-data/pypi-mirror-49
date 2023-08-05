9a99a0d024ce9e2b4cc1649fbe2ec64de727ef8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:11 +0000

    Add package ikabot/5.1.2/ikabot-5.1.2.tar.gz

---
+++ packages/ikabot/ikabot-5.1.2.tar.gz/ikabot-5.1.2/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="ikabot",
+	version="5.1.2",
+	author="physics-sp",
+	description="A bot for ikariam",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/physics-sp/ikabot",
+	include_package_data=True,
+	packages=setuptools.find_packages(),
+	install_requires=[
+		  'requests',
+		  'pycryptodome'
+	],
+	entry_points = {
+		'console_scripts': ['ikabot=ikabot.command_line:main'],
+	},
+	classifiers=(
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: POSIX :: Linux",
+	),
+)

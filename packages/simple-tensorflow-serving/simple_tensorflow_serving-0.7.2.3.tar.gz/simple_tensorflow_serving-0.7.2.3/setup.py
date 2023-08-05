18f12f2edc840f0f3095ff873e64c32ee0990479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:11 +0000

    Add package simple-tensorflow-serving/0.7.2.3/simple_tensorflow_serving-0.7.2.3.tar.gz

---
+++ packages/simple-tensorflow-serving/simple_tensorflow_serving-0.7.2.3.tar.gz/simple_tensorflow_serving-0.7.2.3/setup.py
@@ -0,0 +1,27 @@
+# Update the code and upload the package to pypi
+# 1. python ./setup.py bdist_wheel --universal
+# 2. twine upload dist/simple_tensorflow_serving-x.x.x-py2.py3-none-any.whl
+
+from setuptools import setup, find_packages
+
+setup(
+    name="simple_tensorflow_serving",
+    version="0.7.2.3",
+    author="tobe",
+    author_email="tobeg3oogle@gmail.com",
+    url="https://github.com/tobegit3hub/simple_tensorflow_serving",
+    description=
+    "The simpler and easy-to-use serving service for TensorFlow models",
+    packages=find_packages(),
+    install_requires=[
+        'configparser', 'pandas', 'protobuf', 'flask', 'jinja2', 'flask-cors',
+        'requests', 'pillow', 'uwsgi', 'tensorflow'
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        "console_scripts": [
+            "simple_tensorflow_serving=simple_tensorflow_serving.command:main",
+            "stfs=simple_tensorflow_serving.command:main"
+        ],
+    })

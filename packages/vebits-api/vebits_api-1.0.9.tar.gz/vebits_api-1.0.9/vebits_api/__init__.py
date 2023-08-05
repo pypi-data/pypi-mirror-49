6578273fb04d0f25ef2860010987d8aa85ca1c7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:15 +0000

    Add package vebits-api/1.0.9/vebits_api-1.0.9.tar.gz

---
+++ packages/vebits-api/vebits_api-1.0.9.tar.gz/vebits_api-1.0.9/vebits_api/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+
+
+# from vebits_api.vebits_api import *
+
+from . import bbox_util as bbox_util
+from . import detector_util as detector_util
+from . import im_util as im_util
+from . import labelmap_util as labelmap_util
+from . import others_util as others_util
+from . import vis_util as vis_util
+from . import xml_util as xml_util
+
+__version__ = "1.0.9"

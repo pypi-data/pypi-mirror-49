dca416d36d2364070b107330cd736bc5f8d4df76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:36 +0000

    Add package videocloud/2.5.1/videocloud-2.5.1.tar.gz

---
+++ packages/videocloud/videocloud-2.5.1.tar.gz/videocloud-2.5.1/setup.py
@@ -0,0 +1,51 @@
+import setuptools
+
+long_description = '''
+# VideoCloud
+[![License](https://img.shields.io/github/license/paramt/videocloud.svg)](LICENSE)
+[![PyPI](https://img.shields.io/pypi/v/videocloud.svg)](https://pypi.org/project/VideoCloud/)
+
+Generate word clouds from YouTube video captions
+
+## Installation
+```
+pip install videocloud
+```
+*Note: A C compiler is required for the installation process.*
+
+## Usage
+```
+videocloud <link to youtube video> [filepath] [language code] [font]
+```
+If no filepath is given, the wordcloud will be generated as `videocloud.png`.
+The lanuage code defaults to `en` and the fontpath defaults to [Noto Sans](https://github.com/paramt/videocloud/blob/master/assets/fonts/NotoSans/NotoSans.ttf)
+To use a custom font, provide the HTTP link for its TTF file. Similarly, you can provide an HTTP link to a PNG mask file. The mask defaults to
+[a generic cloud](https://github.com/paramt/videocloud/blob/v2.5/assets/masks/cloud.png).
+
+## Development
+This project is maintained by [paramt](https://github.com/paramt) on [GitHub](https://github.com/paramt/videocloud).
+'''
+
+setuptools.setup(
+    name="videocloud",
+    version="2.5.1",
+    url="https://github.com/paramt/VideoCloud",
+    license="MIT",
+    author="Param Thakkar",
+    author_email="contact@param.me",
+    description="A command line tool that generates word clouds from YouTube video captions",
+    long_description=long_description,
+	long_description_content_type="text/markdown",
+    packages=["videocloud"],
+    install_requires=[
+        "pillow==6.1.0",
+        "wordcloud==1.5.0",
+        "youtube-transcript-api==0.1.5"
+    ],
+    python_requires=">= 3.6",
+	entry_points={
+        'console_scripts': [
+            'videocloud=videocloud.main:main'
+        ]
+    }
+)
---
+++ packages/videocloud/videocloud-2.5.1.tar.gz/videocloud-2.5.1/videocloud/main.py
@@ -0,0 +1,115 @@
+import os
+import sys
+import PIL.Image as Image
+from wordcloud import WordCloud
+from youtube_transcript_api import YouTubeTranscriptApi as ytcc
+import urllib.request
+import numpy as np
+
+def download(url: str, filepath: str) -> str:
+	try:
+		urllib.request.urlretrieve(url, filepath)
+	except:
+		print("Unable to download the needed assets to generate the videocloud")
+		print("Please make sure that you have provided the correct URLs to any fonts/masks")
+		sys.exit(1)
+	else:
+		return filepath
+
+def delete(*files) -> bool:
+	for f in files:
+		try:
+			os.remove(f)
+		except:
+			print(f"Unable to remove {f} while cleaning up")
+
+def get_video_id(url: str) -> str:
+    return(url[-11:])
+
+def get_cc(video_id: str, language: list) -> str:
+	"""Returns captions from a YouTube video in a word-separated list"""
+	try:
+		captions = ytcc.get_transcript(video_id, languages=language)
+	except:
+		captions = ytcc.get_transcript(video_id)
+
+	full_captions = ""
+
+	for caption in captions:
+		full_captions += caption["text"] + " "
+
+	return full_captions.lower()
+
+def wordcloud(words: str, font: str, mask: str) -> Image.Image:
+	"""Generates a word cloud from a list of strings"""
+	font = download(font, "tempfont.ttf")
+	mask = download(mask, "tempmask.png")
+	mask_data = np.array(Image.open(mask))
+
+	try:
+		wordcloud = WordCloud(
+			width = 1000,
+			height = 500,
+			font_path = font,
+			background_color = "#d1d1d1",
+			mask = mask_data,
+		).generate(words)
+
+		image = wordcloud.to_image()
+
+	except Exception:
+		print(f"There was an unknown error generating the videocloud.")
+		sys.exit(1)
+
+	finally:
+		delete(font, mask)
+
+	return image
+
+def videocloud(url: str, language: list, font: str, mask: str):
+	try:
+		video_id = get_video_id(url)
+		captions = get_cc(video_id, language)
+		image = wordcloud(captions, font, mask)
+
+	except ytcc.CouldNotRetrieveTranscript:
+		print("The specified video either doesn't exist or doesn't have captions enabled. For more help visit https://www.param.me/videocloud#usage")
+		sys.exit(1)
+	except IOError:
+		print(f"There was an error saving the videocloud. For more help visit https://www.param.me/videocloud#usage")
+		sys.exit(1)
+
+	return image
+
+def main():
+	try:
+		video_id = sys.argv[1]
+	except:
+		print("Please specify a YouTube video link or video ID. For more help visit https://www.param.me/videocloud#usage")
+		sys.exit(1)
+
+	try:
+		filepath = sys.argv[2]
+	except IndexError:
+		filepath = "videocloud.png"
+
+	try:
+		language = [sys.argv[3]]
+	except IndexError:
+		language = ["en"]
+
+	try:
+		font = sys.argv[4]
+	except IndexError:
+		font = "https://github.com/paramt/videocloud/blob/master/assets/fonts/NotoSans/NotoSans.ttf?raw=true"
+
+	try:
+		mask = sys.argv[5]
+	except IndexError:
+		mask = "https://github.com/paramt/videocloud/blob/master/assets/masks/cloud.png?raw=true"
+
+	wordcloud = videocloud(video_id, language, font, mask)
+	filepath = os.path.abspath(filepath)
+	wordcloud.save(filepath)
+
+	print(f"Videocloud created in {filepath}")

01b971ba0fea85fad59b51409085351b22c09d3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:50 +0000

    Add package wagtail-factories/2.0.0/wagtail_factories-2.0.0.tar.gz

---
+++ packages/wagtail-factories/wagtail_factories-2.0.0.tar.gz/wagtail_factories-2.0.0/setup.py
@@ -0,0 +1,61 @@
+import re
+
+from setuptools import find_packages, setup
+
+install_requires = [
+    'factory-boy>=2.8.0',
+    'wagtail>=2.0',
+]
+
+docs_require = [
+    'sphinx>=1.4.0',
+]
+
+tests_require = [
+    'pytest==5.0.1',
+    'pytest-django==3.5.1',
+    'pytest-cov==2.7.1',
+    'pytest-pythonpath==0.7.3',
+    'psycopg2>=2.3.1',
+    'coverage==4.5.3',
+
+    'isort==4.3.21',
+    'flake8==3.7.8',
+    'flake8-blind-except==0.1.1',
+    'flake8-debugger==3.1.0',
+]
+
+with open('README.rst') as fh:
+    long_description = re.sub(
+        '^.. start-no-pypi.*^.. end-no-pypi', '', fh.read(), flags=re.M | re.S)
+
+setup(
+    name='wagtail_factories',
+    version='2.0.0',
+    description='Factory boy classes for wagtail',
+    long_description=long_description,
+    author="Michael van Tellingen",
+    author_email="michaelvantellingen@gmail.com",
+    url='https://github.com/mvantellingen/wagtail-factories/',
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require={
+        'docs': docs_require,
+        'test': tests_require,
+    },
+    entry_points={},
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    zip_safe=False,
+)

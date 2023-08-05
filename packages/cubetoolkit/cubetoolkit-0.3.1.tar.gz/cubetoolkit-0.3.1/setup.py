544c321c310fd76ef156a032138ababa781a05ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:35 +0000

    Add package cubetoolkit/0.3.1/cubetoolkit-0.3.1.tar.gz

---
+++ packages/cubetoolkit/cubetoolkit-0.3.1.tar.gz/cubetoolkit-0.3.1/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding:Utf-8 -*-
+
+from setuptools import setup
+
+setup(name='cubetoolkit',
+      version='0.3.1',
+      description='toolkit to works on cubicweb cubes',
+      author='Laurent Peuch',
+      long_description=open("README.md", "r").read(),
+      long_description_content_type='text/markdown',
+      author_email='cortex@worlddomination.be',
+      url='https://github.com/Psycojoker/cubetoolkit',
+      install_requires=['argh', 'redbaron', 'requests', 'sphinx'],
+      py_modules=['cubetoolkit'],
+      entry_points={
+          'console_scripts': [
+              'cubetoolkit = cubetoolkit:main',
+              'ctk = cubetoolkit:main',
+          ]
+      },
+      license='gplv3+',
+      keywords='cubicweb',
+      )

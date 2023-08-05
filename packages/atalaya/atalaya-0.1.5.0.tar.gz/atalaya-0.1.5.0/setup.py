889e5a9a1266b878a9ee6510324865a7492f2421
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:05 +0000

    Add package atalaya/0.1.5.0/atalaya-0.1.5.0.tar.gz

---
+++ packages/atalaya/atalaya-0.1.5.0.tar.gz/atalaya-0.1.5.0/setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name='atalaya',
+      version='0.1.5.0',
+      python_requires='>=3.6',
+      description='Atalaya is a logger for pytorch.',
+      url='https://bitbucket.org/dmmlgeneva/frameworks/src/master/atalaya/',
+      author='Joao Ramos',
+      author_email='joao.candido@etu.unige.ch',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=['numpy',
+                        'torch',
+                        'visdom>=0.1.8.8',
+                        'tensorboardX==1.4'
+      ],
+      long_description=read('README.md'),
+      long_description_content_type='text/markdown',
+      zip_safe=False)

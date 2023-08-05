39e24ce0449dc10101e0a4ab2e93f69e0f859541
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:27 +0000

    Add package odoo10-addon-product-default-image/10.0.1.0.0.99.dev8/odoo10_addon_product_default_image-10.0.1.0.0.99.dev8-py2-none-any.whl

---
+++ packages/odoo10-addon-product-default-image/odoo10_addon_product_default_image-10.0.1.0.0.99.dev8-py2-none-any.whl/odoo/addons/product_default_image/hooks.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 LasLabs Inc.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from odoo import api, SUPERUSER_ID
+
+from .image_constants import CUSTOM
+
+
+def find_templates_with_imgs(cr, registry):
+    with cr.savepoint():
+        env = api.Environment(cr, SUPERUSER_ID, {})
+        ProductTemplate = env['product.template']
+        if ProductTemplate._fields['image'].store:
+            tmpls = ProductTemplate.search([
+                ('image', '!=', False),
+            ])
+        else:
+            # some modules (like product_multi_image) could change image to
+            # non-store, so it is necessary to get all product templates and
+            # filter them
+            tmpls = ProductTemplate.search([])
+            tmpls = tmpls.filtered(lambda tmpl: tmpl.image)
+        tmpls.write({
+            'image_type': CUSTOM,
+        })

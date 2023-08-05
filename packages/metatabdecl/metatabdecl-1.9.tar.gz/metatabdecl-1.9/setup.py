d412197ae387781ad74b369ccf7dc2cc19946db0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:16 +0000

    Add package metatabdecl/1.9/metatabdecl-1.9.tar.gz

---
+++ packages/metatabdecl/metatabdecl-1.9.tar.gz/metatabdecl-1.9/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+import sys
+import os
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+setup(
+    name='metatabdecl',
+    version='1.9',
+    packages=['metatabdecl'],
+    package_data={'metatabdecl': ['*.csv','*.json']},
+    url='https://github.com/Metatab/metatab-declarations',
+    license='BSD 2-Clause License',
+    author='Eric Busboom',
+    author_email='eric@busboom.org',
+    description='Declaration files and specifications for the Metatab package',
+    zip_safe=False
+)

805315481633b96928b983765c04f722ae11da88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:07 +0000

    Add package fzutils/0.3.3.8/fzutils-0.3.3.8-py3-none-any.whl

---
+++ packages/fzutils/fzutils-0.3.3.8-py3-none-any.whl/fzutils/spider/fz_driver.py
@@ -0,0 +1,26 @@
+# coding:utf-8
+
+'''
+@author = super_fazai
+@File    : fz_driver.py
+@connect : superonesfazai@gmail.com
+'''
+
+from .fz_phantomjs import (
+    MyPhantomjs,
+    CHROME,
+    PHANTOMJS,
+    FIREFOX,
+    PC,
+    PHONE,
+    ChromiumPuppeteer,
+    PYPPETEER,
+    ChromeSwitchProxyExtensioner,)
+
+__all__ = [
+    'BaseDriver',       # 驱动控制类
+]
+
+class BaseDriver(MyPhantomjs):
+    pass
+

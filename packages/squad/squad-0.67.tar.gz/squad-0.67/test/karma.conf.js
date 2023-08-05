06a12b688bca46fe0b99b0ced72e4b56f4c9a0f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:29 +0000

    Add package squad/0.67/squad-0.67.tar.gz

---
+++ packages/squad/squad-0.67.tar.gz/squad-0.67/test/karma.conf.js
@@ -0,0 +1,30 @@
+module.exports = function(config) {
+    config.set({
+        // root path location that will be used to resolve all relative paths
+        basePath : '../',
+
+        // files to include, ordered by dependencies
+        files : [
+            'squad/frontend/static/angularjs/angular.js',
+            'squad/frontend/static/angularjs/angular-mocks.js',
+            'squad/frontend/static/jquery.js',
+            'squad/frontend/static/lodash.js',
+            'squad/frontend/static/bootstrap/js/bootstrap.js',
+            'squad/frontend/static/floatThead/src/jquery.floatThead.js',
+            'squad/frontend/static/chartjs/Chart.bundle.js',
+            'squad/frontend/static/select2.js/select2.min.js',
+
+            {pattern: 'squad/frontend/static/squad/controllers/*.js', type: 'module'},
+            {pattern: 'test/unit/*.js', type: 'module'},
+        ],
+
+        autoWatch : false,
+        frameworks: ['jasmine'],
+        browsers : ['ChromeHeadless'],
+        reporters: ['progress'],
+
+        plugins : [
+            'karma-chrome-launcher',
+            'karma-jasmine',
+        ]
+})}

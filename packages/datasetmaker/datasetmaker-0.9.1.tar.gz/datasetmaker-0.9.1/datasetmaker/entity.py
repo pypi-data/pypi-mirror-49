e2848139223966b3536b59988244cb56149e4ef0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:08 +0000

    Add package datasetmaker/0.9.1/datasetmaker-0.9.1.tar.gz

---
+++ packages/datasetmaker/datasetmaker-0.9.1.tar.gz/datasetmaker-0.9.1/datasetmaker/entity.py
@@ -0,0 +1,72 @@
+import os
+import pandas as pd
+from datasetmaker.path import DDF_DIR
+
+
+class Country():
+    @classmethod
+    def _read_entity_csv(cls, entity, lang=None):
+        fname = f"ddf--entities--{entity}.csv"
+        if lang:
+            path = os.path.join(DDF_DIR, f"lang/{lang}", fname)
+        else:
+            path = os.path.join(DDF_DIR, fname)
+        df = pd.read_csv(path, sep=',')
+        return df
+
+    @classmethod
+    def _merge_translation(cls, src_df, lang_df, col):
+        src_df = src_df.merge(lang_df, on="country", how="left")
+        src_df[f"{col}_x"] = src_df[f"{col}_y"].fillna(src_df[f"{col}_x"])
+        src_df = src_df.drop(f"{col}_y", axis=1)
+        src_df = src_df.rename(columns={f"{col}_x": col})
+        return src_df
+
+    @classmethod
+    def name_to_id(cls, lang=None):
+        df = cls._read_entity_csv("country")
+        if lang:
+            lang_df = cls._read_entity_csv("country", lang)
+            df = cls._merge_translation(df, lang_df, "name")
+        df['name'] = df['name'].str.lower()
+        map_ = df.set_index("name").country.to_dict()
+        alt_names = df.set_index('alt_name').country.to_dict().items()
+        for alts, id_ in alt_names:
+            if pd.isnull(alts):
+                continue
+            map_.update({k.lower(): id_ for k in alts.split(';')})
+        return map_
+
+    @classmethod
+    def denonym_to_id(cls, lang=None):
+        df = cls._read_entity_csv("country")
+        if lang:
+            lang_df = cls._read_entity_csv("country", lang)
+            df = cls._merge_translation(df, lang_df, "name")
+
+        map_ = {}
+        den_map = df.set_index("denonym").country.to_dict().items()
+        for dens, id_ in den_map:
+            if pd.isnull(dens):
+                continue
+            map_.update({k: id_ for k in dens.split(';')})
+        return map_
+
+    @classmethod
+    def id_to_name(cls, lang=None):
+        df = cls._read_entity_csv("country")
+        if lang:
+            lang_df = cls._read_entity_csv("country", lang)
+            df = cls._merge_translation(df, lang_df, "name")
+        return df.set_index("country").name.to_dict()
+
+    @classmethod
+    def iso3_to_id(cls):
+        df = cls._read_entity_csv("country")
+        df = df.dropna(subset=['iso3'])
+        return df.set_index("iso3").country.to_dict()
+
+    @classmethod
+    def list_ids(cls):
+        df = cls._read_entity_csv("country")
+        return df.country.tolist()

92e31140ac91a85f63deec1a3db9662cba74b082
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:48 +0000

    Add package lbl-repack/1.3.0/lbl-repack-1.3.0.tar.gz

---
+++ packages/lbl-repack/lbl-repack-1.3.0.tar.gz/lbl-repack-1.3.0/repack/VERSION.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2017-2019 Patricio Cubillos and contributors.
+# repack is open-source software under the MIT license (see LICENSE).
+
+# repack version:
+repack_VER =  1  # Major version
+repack_MIN =  3  # Minor version
+repack_REV =  0  # Revision

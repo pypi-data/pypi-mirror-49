ce313531564d0e155499c851a325644eab1cde54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:44 +0000

    Add package odoo12-addon-mail-debrand/12.0.2.0.0.99.dev3/odoo12_addon_mail_debrand-12.0.2.0.0.99.dev3-py3-none-any.whl

---
+++ packages/odoo12-addon-mail-debrand/odoo12_addon_mail_debrand-12.0.2.0.0.99.dev3-py3-none-any.whl/odoo/addons/mail_debrand/__manifest__.py
@@ -0,0 +1,17 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Mail Debrand",
+    "summary": "Remove Odoo branding in sent emails",
+    "version": "12.0.2.0.0",
+    "category": "Social Network",
+    "website": "https://github.com/OCA/social/",
+    "author": "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "mail",
+    ],
+    "development_status": "Stable",
+    "maintainers": ["pedrobaeza"],
+}

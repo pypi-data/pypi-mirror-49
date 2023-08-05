4a9e95fab15444747cc288b5fa714c35f435a18a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:41:42 +0000

    Add package python-smartgadget/0.1.3/python-smartgadget-0.1.3.tar.gz

---
+++ packages/python-smartgadget/python-smartgadget-0.1.3.tar.gz/python-smartgadget-0.1.3/setup.py
@@ -0,0 +1,33 @@
+from distutils.spawn import find_executable
+import os
+
+from setuptools import setup
+
+
+def include_readme():
+    try:
+        import pandoc
+    except ImportError:
+        return ''
+    pandoc.core.PANDOC_PATH = find_executable('pandoc')
+    readme_file = os.path.join(os.path.dirname(__file__), 'README.md')
+    doc = pandoc.Document()
+    with open(readme_file, 'r') as rf:
+        doc.markdown = rf.read()
+    return doc.rst.decode('utf-8')
+
+
+setup(
+    name='python-smartgadget',
+    version='0.1.3',
+    packages=['sensirionbt'],
+    python_requires='>=3.4',
+    install_requires=['bluepy>=1.0.5'],
+    description='Library for reading temperature, humidity, and battery level from a '
+                'SHT31 Smart Gadget Development Kit by Sensirion',
+    long_description=include_readme(),
+    author='Matthias Erll',
+    author_email='matthias@erll.de',
+    url='https://github.com/merll/python-smartgadget',
+    license="MIT",
+)

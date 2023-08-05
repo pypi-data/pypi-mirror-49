7ff11949e9847642565422bfd4eb19db50c258e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:36 +0000

    Add package alva-robot/0.0.2/alva_robot-0.0.2.tar.gz

---
+++ packages/alva-robot/alva_robot-0.0.2.tar.gz/alva_robot-0.0.2/setup.py
@@ -0,0 +1,35 @@
+# coding=utf8
+
+__author__ = 'Alexander.Li'
+
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.0.2'
+
+setup(name='alva_robot',
+      version=version,
+      description="A robot base of AlvaIM",
+      long_description="""\
+Recieve and process messages and make response for robot of AlvaIM""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='AlvaIM, Robot',
+      author='Alexander.Li',
+      author_email='superpowerlee@gmail.com',
+      url='https://alvaim.com',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          # -*- Extra requirements: -*-
+          'pollworker',
+          'secp256k1py',
+          'boto3',
+          'click',
+          'oss2'
+      ],
+      entry_points={
+        'console_scripts': ['alva-robot=alva_robot.main:main'],
+      },
+      )

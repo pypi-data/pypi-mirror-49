e0e16013fb0f18e186f988cc65fabc557805521b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:12 +0000

    Add package seeq/0.0.15/seeq-0.0.15.tar.gz

---
+++ packages/seeq/seeq-0.0.15.tar.gz/seeq-0.0.15/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="seeq",
+    version="0.0.15",
+    author="Seeq Corporation",
+    author_email="support@seeq.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://www.seeq.com",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

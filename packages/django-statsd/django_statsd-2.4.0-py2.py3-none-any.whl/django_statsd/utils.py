dae3ee2740a304879d96c0452712c8f5995e2716
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:55 +0000

    Add package django-statsd/2.4.0/django_statsd-2.4.0-py2.py3-none-any.whl

---
+++ packages/django-statsd/django_statsd-2.4.0-py2.py3-none-any.whl/django_statsd/utils.py
@@ -0,0 +1,30 @@
+import statsd
+from . import settings
+
+
+def get_connection(host=None, port=None, sample_rate=None):
+    if not host:
+        host = settings.STATSD_HOST
+
+    if not port:
+        port = settings.STATSD_PORT
+
+    if not sample_rate:
+        sample_rate = settings.STATSD_SAMPLE_RATE
+
+    return statsd.Connection(host, port, sample_rate)
+
+
+def get_client(name, connection=None, class_=statsd.Client):
+    if not connection:
+        connection = get_connection()
+
+    return class_(name, connection)
+
+
+def get_timer(name, connection=None):
+    return get_client(name, connection, statsd.Timer)
+
+
+def get_counter(name, connection=None):
+    return get_client(name, connection, statsd.Counter)

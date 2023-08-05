f7ee9243eb4cb229b2ac6ce12ea58cdf1d4caa2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:11 +0000

    Add package iotile-support-firm-env-1/1.2.1/iotile_support_firm_env_1-1.2.1.tar.gz

---
+++ packages/iotile-support-firm-env-1/iotile_support_firm_env_1-1.2.1.tar.gz/iotile_support_firm_env_1-1.2.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="iotile_support_firm_env_1",
+    packages=find_packages(include=["iotile_support_firm_env_1.*", "iotile_support_firm_env_1"]),
+    version="1.2.1",
+    install_requires=['iotile-core ~= 5.0'],
+    entry_points={'iotile.proxy': ['env_proxy = iotile_support_firm_env_1.env_proxy'], 'iotile.emulated_tile': ['envbsl_1 = iotile_support_firm_env_1.envbsl_1'], 'iotile.virtual_device': ['dev_envbsl_1 = iotile_support_firm_env_1.dev_envbsl_1']},
+    author="Arch",
+    author_email="info@arch-iot.com"
+)

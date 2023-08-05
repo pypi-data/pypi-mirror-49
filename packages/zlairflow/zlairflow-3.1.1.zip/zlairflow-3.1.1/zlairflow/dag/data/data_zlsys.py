b2ffc427b1abe30c889c93013866f2d800d9b36f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:11 +0000

    Add package zlairflow/3.1.1/zlairflow-3.1.1.zip

---
+++ packages/zlairflow/zlairflow-3.1.1.zip/zlairflow-3.1.1/zlairflow/dag/data/data_zlsys.py
@@ -0,0 +1,22 @@
+para=[
+
+  ['zlsys', 'sichuan_suiningshi', 'minutes=120', '']
+, ['zlsys', 'sichuan_yaanshi', 'minutes=120', '']
+, ['zlsys', 'sichuan_yibinshi', 'minutes=120', '']
+, ['zlsys', 'yunnan_baoshanshi', 'minutes=120', '']
+, ['zlsys', 'yunnan_dalizhou', 'minutes=120', '']
+, ['zlsys', 'yunnan_dehongzhou', 'minutes=120', '']
+, ['zlsys', 'yunnan_diqingzhou', 'minutes=120', '']
+, ['zlsys', 'yunnan_lijiangshi', 'minutes=120', '']
+, ['zlsys', 'yunnan_lincangshi', 'minutes=120', '']
+, ['zlsys', 'yunnan_puershi', 'minutes=120', '']
+, ['zlsys', 'yunnan_qujingshi', 'minutes=120', '']
+, ['zlsys', 'yunnan_tengchongshi', 'minutes=120', '']
+, ['zlsys', 'yunnan_wenshanzhou', 'minutes=120', '']
+, ['zlsys', 'yunnan_xishuangbanna', 'minutes=120', '']
+, ['zlsys', 'yunnan_yunnansheng', 'minutes=120', '']
+, ['zlsys', 'yunnan_yuxishi', 'minutes=120', '']
+, ['zlsys', 'yunnan_zhaotongshi', 'minutes=120', '']
+
+
+]

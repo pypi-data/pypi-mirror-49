d2b804c0edc82c66beb2759dc378381f1d294252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:26 +0000

    Add package pulp-file/0.1.0b1/pulp-file-0.1.0b1.tar.gz

---
+++ packages/pulp-file/pulp-file-0.1.0b1.tar.gz/pulp-file-0.1.0b1/pulp_file/app/models.py
@@ -0,0 +1,53 @@
+from logging import getLogger
+
+from django.db import models
+
+from pulpcore.plugin.models import Content, PublicationDistribution, Remote, Publication
+
+
+log = getLogger(__name__)
+
+
+class FileContent(Content):
+    """
+    The "file" content type.
+
+    Content of this type represents a single file uniquely
+    identified by path and SHA256 digest.
+
+    Fields:
+        relative_path (str): The file relative path.
+        digest (str): The SHA256 HEX digest.
+    """
+
+    TYPE = "file"
+
+    relative_path = models.CharField(max_length=255, null=False)
+    digest = models.CharField(max_length=64, null=False)
+
+    class Meta:
+        unique_together = ("relative_path", "digest")
+
+
+class FileRemote(Remote):
+    """
+    Remote for "file" content.
+    """
+
+    TYPE = "file"
+
+
+class FilePublication(Publication):
+    """
+    Publication for 'file' content.
+    """
+
+    TYPE = "file"
+
+
+class FileDistribution(PublicationDistribution):
+    """
+    Distribution for 'file' content.
+    """
+
+    TYPE = "file"

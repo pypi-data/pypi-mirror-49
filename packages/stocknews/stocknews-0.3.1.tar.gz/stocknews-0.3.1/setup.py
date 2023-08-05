b3329f97dea12108be10b39cfba14b0e9d24014c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:42:57 +0000

    Add package stocknews/0.3.1/stocknews-0.3.1.tar.gz

---
+++ packages/stocknews/stocknews-0.3.1.tar.gz/stocknews-0.3.1/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setuptools.setup(name='stocknews',
+                 version='0.3.1',
+                 description='PoC for scraping Yahoo News with sentiment analysis',
+                 url='http://github.com/primus852/stock-news.git',
+                 author='Torsten Wolter',
+                 long_description=readme(),
+                 keywords='sentiment news rss stock',
+                 long_description_content_type="text/markdown",
+                 author_email='tow.berlin@gmail.com',
+                 packages=setuptools.find_packages(),
+                 classifiers=[
+                     'Development Status :: 3 - Alpha',
+                     'License :: OSI Approved :: MIT License',
+                     'Programming Language :: Python :: 3.6',
+                     'Topic :: Text Processing :: Linguistic',
+                 ],
+                 license='MIT',
+                 include_package_data=True,
+                 install_requires=[
+                     'pandas',
+                     'feedparser',
+                     'nltk',
+                 ],
+                 zip_safe=False,
+                 test_suite='nose.collector',
+                 tests_require=['nose'],
+                 )

9b9815b226e334f3de5ff44d988cccf7a389323b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:39 +0000

    Add package zlapp/1.2.9/zlapp-1.2.9.zip

---
+++ packages/zlapp/zlapp-1.2.9.zip/zlapp-1.2.9/zlapp/etl3/t_gg_ent_bridge.py
@@ -0,0 +1,22 @@
+import os 
+from zlhawq.data import zhulong_diqu_dict ,zl_diqu_dict
+import time 
+from lmf.dbv2 import db_command
+import traceback
+from lmf.bigdata import pg2pg 
+from sqlalchemy.dialects.postgresql import  TEXT,BIGINT,TIMESTAMP,NUMERIC
+from  datetime  import datetime,timedelta
+
+def est1():
+
+    sql="select * from etl.t_gg_ent_bridge"
+    tbname="t_gg_ent_bridge"
+    conp_hawq=["gpadmin","since2015",'192.168.4.179','base_db',"etl"]
+    conp_pg=["postgres","since2015",'192.168.4.188','bid',"cdc"]
+    bg=time.time()
+    datadict={"html_key":BIGINT(),"price":NUMERIC(),"xzqh":TEXT()}
+    pg2pg(sql,tbname,conp_hawq,conp_pg,chunksize=10000,datadict=datadict)
+    ed=time.time()
+    cost=int(ed-bg)
+    print("耗时%s秒"%cost)
+

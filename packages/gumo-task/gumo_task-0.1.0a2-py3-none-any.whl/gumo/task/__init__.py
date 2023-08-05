cdba38294a5b35a57e5878c5275085f982069b50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:49 +0000

    Add package gumo-task/0.1.0a2/gumo_task-0.1.0a2-py3-none-any.whl

---
+++ packages/gumo-task/gumo_task-0.1.0a2-py3-none-any.whl/gumo/task/__init__.py
@@ -0,0 +1,10 @@
+from gumo.task._configuration import configure
+from gumo.task.infrastructure.configuration import TaskConfiguration
+from gumo.task.application import enqueue
+
+
+__all__ = [
+    configure.__name__,
+    TaskConfiguration.__name__,
+    enqueue.__name__,
+]

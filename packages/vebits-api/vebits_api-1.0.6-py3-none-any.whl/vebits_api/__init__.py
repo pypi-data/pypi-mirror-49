971950f54267532453ea7389a8c8f8393ca45d77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:10 +0000

    Add package vebits-api/1.0.6/vebits_api-1.0.6-py3-none-any.whl

---
+++ packages/vebits-api/vebits_api-1.0.6-py3-none-any.whl/vebits_api/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+
+
+# from vebits_api.vebits_api import *
+
+import vebits_api.bbox_util as bbox_util
+import vebits_api.detector_util as detector_util
+import vebits_api.im_util as im_util
+import vebits_api.labelmap_util as labelmap_util
+import vebits_api.others_util as others_util
+import vebits_api.vis_util as vis_util
+import vebits_api.xml_util as xml_util
+
+__version__ = "1.0.6"

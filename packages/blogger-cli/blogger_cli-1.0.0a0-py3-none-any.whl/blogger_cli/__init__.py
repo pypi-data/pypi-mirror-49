b8d82aa4c1a83335b2573b6f33969b5f8ef802b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:13 +0000

    Add package blogger-cli/1.0.0a0/blogger_cli-1.0.0a0-py3-none-any.whl

---
+++ packages/blogger-cli/blogger_cli-1.0.0a0-py3-none-any.whl/blogger_cli/__init__.py
@@ -0,0 +1,7 @@
+import os
+
+ROOT_DIR = os.path.join(os.path.split(__file__)[0])
+RESOURCE_DIR = os.path.join(os.path.split(__file__)[0], 'resources')
+
+HOME = os.path.expanduser('~')
+CONFIG_DIR = os.path.join(HOME, '.blogger')
---
+++ packages/blogger-cli/blogger_cli-1.0.0a0-py3-none-any.whl/blogger_cli/tests/messages.py
@@ -0,0 +1,53 @@
+class BloggerMessage(object):
+
+    main = '''\
+Usage: blogger [OPTIONS] COMMAND [ARGS]...
+
+  A CLI tool to maintain your blogger blog. Sync, convert and upload :).
+
+Options:
+  -v, --verbose  enables verbose command
+  --help         Show this message and exit.
+
+Commands:
+  addblog     Register a new blog
+  config      Change a blog's configurations
+  convert     Convert files to html
+  export      Export default design to your blog
+  info        Show blog's properties
+  rmblog      Remove a blog
+  serve       Serve your blog locally
+  setdefault  Set a blog as default
+  setupblog   Register a new blog
+'''
+
+    addblog_success = '''\
+Blog added succesfully
+'''
+
+    addblog_existing = '''\
+Blog already exists!
+'''
+
+    rmblog_success = '''\
+Blog removed succesfully
+'''
+
+    info_success = '''\
+
+Registered Blogs:
+   test1
+
+Blog:configs [standard] 	Optional:configs [Advanced]
+   google_analytics_id 		 meta_format
+   disqus_username 		 post_extract_list
+   blog_images_dir 		 index_div_name
+   templates_dir 		 filter_post_without_title
+   blog_posts_dir 		 working_dir_timestamp
+   default
+   working_dir
+   blog_dir
+
+Tip: Use blogger info blogname for blog details
+
+'''

04732c15e93b40f6989575065699a80bc95feea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:04 +0000

    Add package realm-cli/0.0.2/realm_cli-0.0.2.tar.gz

---
+++ packages/realm-cli/realm_cli-0.0.2.tar.gz/realm_cli-0.0.2/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="realm_cli",
+    entry_points={
+        'console_scripts': ['realm-cli=realm_cli.main:main']
+    },
+    py_modules=find_packages(),
+    version='0.0.2'
+)

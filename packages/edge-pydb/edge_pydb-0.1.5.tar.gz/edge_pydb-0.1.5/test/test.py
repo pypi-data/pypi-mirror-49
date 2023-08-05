4a3c64922bb344bff3994649c5a12f968e9d62db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:34 +0000

    Add package edge-pydb/0.1.5/edge_pydb-0.1.5.tar.gz

---
+++ packages/edge-pydb/edge_pydb-0.1.5.tar.gz/edge_pydb-0.1.5/test/test.py
@@ -0,0 +1,13 @@
+import edge_pydb
+import h5py
+import os
+
+fp = edge_pydb.getfiles('NGC4047.pipe3d.hdf5')
+h5py.File(fp, 'r')
+
+print(os.path.abspath(__file__))
+edge_pydb.Addfile('../build.sh')
+edge_pydb.listfiles('.sh')
+
+edge_pydb.AddDir('../dist')
+edge_pydb.listfiles('.whl')

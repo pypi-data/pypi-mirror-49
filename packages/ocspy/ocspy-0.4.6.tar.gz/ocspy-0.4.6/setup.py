e62445df809e798e01d39e82d7441e81cc907818
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:32 +0000

    Add package ocspy/0.4.6/ocspy-0.4.6.tar.gz

---
+++ packages/ocspy/ocspy-0.4.6.tar.gz/ocspy-0.4.6/setup.py
@@ -0,0 +1,22 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="ocspy",
+    version="0.4.6",
+    keywords=("pip", "optical communication",
+              "coherent optical", "ocspy", "ocspy"),
+    description="simulate optical communication",
+    long_description="simulate optical communication",
+    license="MIT Licence",
+    packages=find_packages(),
+
+    url="",
+    author="nigulasikaochuan",
+    author_email="nigulasikaochuan@sjtu.edu.cn",
+
+
+    include_package_data=True,
+    platforms="any",
+    install_requires=['pyqtgraph','plotly','numpy']
+)

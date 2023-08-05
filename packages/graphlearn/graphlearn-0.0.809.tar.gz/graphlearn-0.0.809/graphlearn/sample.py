07b7d8aff1788e824868508067e216efaf11d340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:40:47 +0000

    Add package graphlearn/0.0.809/graphlearn-0.0.809.tar.gz

---
+++ packages/graphlearn/graphlearn-0.0.809.tar.gz/graphlearn-0.0.809/graphlearn/sample.py
@@ -0,0 +1,59 @@
+from graphlearn.util import util
+
+def sample_step(object, transformer, grammar, scorer, selector):
+    """
+    Parameters
+    ----------
+    object:
+    transformer: test.test(object) -> graph
+    grammar: trained on graphs, lsgg.propose(graph) -> objects
+    scorer: score.decision_function(object) -> number
+
+    Returns
+    -------
+        object
+    """
+    graph = transformer.encode_single(object)
+    util.valid_gl_graph(graph)
+    proposal_graphs = grammar.propose(graph)
+
+    proposal_objects = transformer.decode(proposal_graphs)
+    scores = scorer.decision_function(proposal_objects)
+    object, score = selector.select(proposal_objects, scores)
+
+    return object, score
+
+
+def sample(graph, transformer=None, grammar=None, scorer=None, selector=None, n_steps=10, return_score=False):
+    for i in range(n_steps):
+        graph, score = sample_step(graph, transformer, grammar, scorer,selector)
+    if return_score:
+        return graph, score
+    return graph
+
+
+def fit():
+    pass
+
+def optimize():
+    pass
+
+
+
+
+def test_sample_step():
+    from graphlearn.score import SimpleDistanceEstimator as SDE
+    import networkx as nx
+    from graphlearn import select
+    import graphlearn.test.transformutil as transformutil
+
+    lsgg = util.test_get_grammar()
+    graph = util._edenize_for_testing(nx.path_graph(4))
+    graph.node[3]['label'] = '5'
+    score_estimator = SDE().fit(util._edenize_for_testing(nx.path_graph(4)))
+    graph,score= sample(graph, transformutil.no_transform(), lsgg, score_estimator, select.SelectMax(), n_steps=2, return_score=True)
+
+    assert (0.000001 > abs(0.319274373045 - score)), score
+    print("sambledestdone")
+
+

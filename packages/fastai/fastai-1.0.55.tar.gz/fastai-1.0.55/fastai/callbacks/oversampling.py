334b6793d60afd3026df3b38d41691e03298832f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:56 +0000

    Add package fastai/1.0.55/fastai-1.0.55.tar.gz

---
+++ packages/fastai/fastai-1.0.55.tar.gz/fastai-1.0.55/fastai/callbacks/oversampling.py
@@ -0,0 +1,20 @@
+from ..torch_core import *
+from ..basic_data import DataBunch
+from ..callback import *
+from ..basic_train import Learner,LearnerCallback
+from torch.utils.data.sampler import WeightedRandomSampler
+
+__all__ = ['OverSamplingCallback']
+
+
+
+class OverSamplingCallback(LearnerCallback):
+    def __init__(self,learn:Learner, weights:torch.Tensor=None):
+        super().__init__(learn)
+        self.labels = self.learn.data.train_dl.dataset.y.items
+        _, counts = np.unique(self.labels,return_counts=True)
+        self.weights = (weights if weights is not None else
+                        torch.DoubleTensor((1/counts)[self.labels]))
+
+    def on_train_begin(self, **kwargs):
+        self.learn.data.train_dl.dl.batch_sampler = BatchSampler(WeightedRandomSampler(self.weights,len(self.learn.data.train_dl.dataset)), self.learn.data.train_dl.batch_size,False)        

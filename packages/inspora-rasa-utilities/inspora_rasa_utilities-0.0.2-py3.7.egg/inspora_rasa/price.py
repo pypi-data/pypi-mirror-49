dc2ec704ee4b4a9b4722e9a00cec7a62ee7b2ac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:17 +0000

    Add package inspora-rasa-utilities/0.0.2/inspora_rasa_utilities-0.0.2-py3.7.egg

---
+++ packages/inspora-rasa-utilities/inspora_rasa_utilities-0.0.2-py3.7.egg/inspora_rasa/price.py
@@ -0,0 +1,39 @@
+import re
+from typing import Any
+
+from rasa.nlu.extractors import EntityExtractor
+from rasa.nlu.training_data import Message
+
+__all__ = ['PriceEntityMapper']
+
+
+class PriceEntityMapper(EntityExtractor):
+    name = 'PriceEntityMapper'
+    requires = ['entities']
+    provides = ['entities']
+
+    def process(self, message: Message, **kwargs: Any) -> None:
+        entities = message.data.get('entities', [])[:]
+        if not entities:
+            return
+
+        for entity in entities:
+            if entity['entity'] == 'price':
+                value = str(entity['value'])
+
+                # price entity includes currency, e.g. "123 euro"
+                price_number = re.findall(r'([0-9]+([.,][0-9]{2})?)', value)
+                if price_number:
+                    price = float(price_number[0][0].replace(',', '.'))
+                    entity['value'] = price
+                    self.add_processor_name(entity)
+
+            if entity['entity'] == 'price-modifier':
+                if entity.get('is_negated'):
+                    self.add_processor_name(entity)
+                    if entity['value'] == 'gt':
+                        entity['value'] = 'lt'
+                    elif entity['value'] == 'lt':
+                        entity['value'] = 'gt'
+
+        message.set('entities', entities, add_to_output=True)

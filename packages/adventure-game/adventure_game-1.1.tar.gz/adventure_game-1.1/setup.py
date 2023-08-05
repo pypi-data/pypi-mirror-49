135b6805587cec6a83a20c2a7686853d7ce24c23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:01 +0000

    Add package adventure-game/1.1/adventure_game-1.1.tar.gz

---
+++ packages/adventure-game/adventure_game-1.1.tar.gz/adventure_game-1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='adventure_game',
+      version='1.1',
+      description='a type of game',
+      author="Paulynn Yu",
+      author_email="paulynnyu@gmail.com",
+      packages=['adventure_game'],
+      zip_safe=False)
+      

cbc99932ab7e3f89327050679ead8254a114486f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:36 +0000

    Add package django-entity-history/1.2.0/django_entity_history-1.2.0-py2.py3-none-any.whl

---
+++ packages/django-entity-history/django_entity_history-1.2.0-py2.py3-none-any.whl/entity_history/migrations/0002_auto_20150406_1605.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('entity_history', '0001_initial'),
+    ]
+
+    operations = []

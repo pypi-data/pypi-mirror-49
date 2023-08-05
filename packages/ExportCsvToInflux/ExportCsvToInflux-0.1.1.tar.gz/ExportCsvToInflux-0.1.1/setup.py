8ec7dbb129799801252f982b85107300c068360e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:01 +0000

    Add package ExportCsvToInflux/0.1.1/ExportCsvToInflux-0.1.1.tar.gz

---
+++ packages/ExportCsvToInflux/ExportCsvToInflux-0.1.1.tar.gz/ExportCsvToInflux-0.1.1/setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup, find_packages
+import os
+import re
+
+CURDIR = os.path.dirname(os.path.abspath(__file__))
+
+with open(os.path.join(CURDIR, 'requirements.txt')) as f:
+    REQUIRES = f.read().splitlines()
+
+with open(os.path.join(CURDIR, 'src', 'ExportCsvToInflux', '__version__.py')) as f:
+    VERSION = re.search("__version__ = '(.*)'", f.read()).group(1)
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
+setup(
+    name='ExportCsvToInflux',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    version=VERSION,
+    license='bsd-3-clause-clear',
+    description='Export',
+    zip_safe=False,
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Bugazelle',
+    author_email='463407426@qq.com',
+    keywords=['python', 'csv', 'influx'],
+    install_requires=REQUIRES,
+    download_url='https://github.com/Bugazelle/export-csv-to-inlfux/archive/v{0}.tar.gz'.format(VERSION),
+    url='https://github.com/Bugazelle/export-csv-to-inlfux',
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ),
+    entry_points={
+        'console_scripts': [
+            'export_csv_to_influx = ExportCsvToInflux.exporter_object:export_csv_to_influx',
+        ],
+    },
+)

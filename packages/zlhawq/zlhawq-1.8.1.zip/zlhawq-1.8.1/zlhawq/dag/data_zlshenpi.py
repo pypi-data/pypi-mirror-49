11061f8c0ec592c78cfe03d34b4b01e78cf8ddb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:42:12 +0000

    Add package zlhawq/1.8.1/zlhawq-1.8.1.zip

---
+++ packages/zlhawq/zlhawq-1.8.1.zip/zlhawq-1.8.1/zlhawq/dag/data_zlshenpi.py
@@ -0,0 +1,34 @@
+para=[
+['zlshenpi', 'anhuisheng', 'minutes=120', '']
+, ['zlshenpi', 'beijingshi', 'minutes=120', '']
+, ['zlshenpi', 'fujiansheng', 'minutes=120', '']
+, ['zlshenpi', 'gansusheng', 'minutes=120', '']
+
+, ['zlshenpi', 'guangdongsheng', 'minutes=120', '']
+, ['zlshenpi', 'hebeisheng', 'minutes=120', '']
+, ['zlshenpi', 'heilongjiangsheng', 'minutes=120', '']
+, ['zlshenpi', 'henansheng', 'minutes=120', '']
+, ['zlshenpi', 'hubeisheng', 'minutes=120', '']
+, ['zlshenpi', 'hunansheng', 'minutes=120', '']
+, ['zlshenpi', 'jiangxisheng', 'minutes=120', '']
+, ['zlshenpi', 'jilinsheng', 'minutes=120', '']
+, ['zlshenpi', 'neimenggusheng', 'minutes=120', '']
+, ['zlshenpi', 'ningxiasheng', 'minutes=120', '']
+, ['zlshenpi', 'qinghaisheng', 'minutes=120', '']
+
+, ['zlshenpi', 'shanxisheng', 'minutes=120', '']
+
+, ['zlshenpi', 'shenxisheng1', 'minutes=120', '']
+
+, ['zlshenpi', 'sichuansheng', 'minutes=120', '']
+
+, ['zlshenpi', 'xiamenshi', 'minutes=120', '']
+
+, ['zlshenpi', 'xinjiangsheng', 'minutes=120', '']
+
+, ['zlshenpi', 'yunnansheng', 'minutes=120', '']
+
+, ['zlshenpi', 'zhejiangsheng', 'minutes=120', '']
+
+
+]
---
+++ packages/zlhawq/zlhawq-1.8.1.zip/zlhawq-1.8.1/zlhawq/zlsys/data.py
@@ -0,0 +1,8 @@
+zl_sys_dict={
+    
+    "zlsys":[ 
+    "zlsys_sichuan_yaanshi"
+    ,"zlsys_sichuan_yibinshi"
+    ,"zlsys_yunnan_qujingshi"
+    ]
+}

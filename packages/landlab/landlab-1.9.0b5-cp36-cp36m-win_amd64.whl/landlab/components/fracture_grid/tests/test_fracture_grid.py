21deb9236b67818ab5f325db063ddf12b05473cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:11 +0000

    Add package landlab/1.9.0b5/landlab-1.9.0b5-cp36-cp36m-win_amd64.whl

---
+++ packages/landlab/landlab-1.9.0b5-cp36-cp36m-win_amd64.whl/landlab/components/fracture_grid/tests/test_fracture_grid.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+test_fracture_grid.py
+
+Unit test for fracture_grid component.
+
+Created on Sun Oct 18 09:47:59 2015
+
+@author: gtucker
+"""
+
+from numpy.testing import assert_array_equal
+
+from landlab.components.fracture_grid import make_frac_grid
+
+
+def test_frac_grid():
+    frac_spacing = 3
+    nrows = 9
+    ncols = 9
+
+    fg = make_frac_grid(frac_spacing, nrows, ncols, seed=1)
+
+    assert_array_equal(
+        fg,
+        [
+            [1, 0, 0, 0, 0, 0, 1, 1, 0],
+            [0, 0, 0, 0, 1, 1, 0, 0, 0],
+            [1, 1, 1, 1, 1, 1, 1, 0, 0],
+            [1, 1, 1, 0, 0, 0, 0, 1, 1],
+            [0, 0, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 1, 1, 0, 0],
+            [0, 0, 0, 0, 0, 1, 1, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0, 1, 1],
+        ],
+    )

8d44cb2a300d24c5305d5ee2c7fa4f8988379be3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:23 +0000

    Add package kimolo/1.0.0/kimolo-1.0.0.tar.gz

---
+++ packages/kimolo/kimolo-1.0.0.tar.gz/kimolo-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name = 'kimolo',
+        version = '1.0.0',
+        py_modules = ['kimolo'],
+        author = 'dankim',
+        author_email = 'dankimist@gmail.com',
+        url = 'http://www.fb.com/kinduberr',
+        description = 'A simple printer of nested lists',
+     )

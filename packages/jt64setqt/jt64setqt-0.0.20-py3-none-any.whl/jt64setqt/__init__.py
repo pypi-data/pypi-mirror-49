89b013afbc9f3f24567ba93c72275f5742b75965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:37 +0000

    Add package jt64setqt/0.0.20/jt64setqt-0.0.20-py3-none-any.whl

---
+++ packages/jt64setqt/jt64setqt-0.0.20-py3-none-any.whl/jt64setqt/__init__.py
@@ -0,0 +1,8 @@
+version_info = (0, 0, 20)
+__title__ = "jt64setqt"
+__version__ = '.'.join(str(c) for c in version_info)
+__name__ = "jt64setqt"
+__author__ = "Greg Beam, KI7MT"
+__license__ = "GPLv3"
+__email__ = "ki7mt@yahoo.com"
+__summary__ = "JTSDK64 Tools Set Qt Version"

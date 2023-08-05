6881e4cdb577ad27858d48a9479e206bf809dcfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:27 +0000

    Add package seeq/0.0.18/seeq-0.0.18-py3-none-any.whl

---
+++ packages/seeq/seeq-0.0.18-py3-none-any.whl/seeq/spy/tests/test_login.py
@@ -0,0 +1,31 @@
+import pytest
+
+from seeq.base import gconfig
+from seeq import spy
+
+from . import test_common
+
+
+@pytest.mark.system
+def test_bad_login():
+    try:
+        spy.login('mark.derbecker@seeq.com', 'DataLab!', url='http://bogus')
+        assert False, 'Exception was not raised with bogus url'
+    except RuntimeError:
+        pass
+
+    # Remove overrides that resulted from spy.login() with bogus URL
+    gconfig.override_global_property('seeq_server_hostname', None)
+    gconfig.override_global_property('seeq_server_port', None)
+    gconfig.override_global_property('seeq_secure_port', None)
+
+    try:
+        spy.login('mark.derbecker@seeq.com', 'DataLab!', auth_provider='bogus')
+        assert False, 'Exception was not raised with bogus auth_provider'
+    except RuntimeError:
+        pass
+
+
+@pytest.mark.system
+def test_good_login():
+    test_common.login()

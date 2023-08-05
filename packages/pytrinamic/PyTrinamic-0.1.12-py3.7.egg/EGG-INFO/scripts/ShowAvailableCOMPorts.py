ea2a4397dcc54677344da7e858694b4f06adb5b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:48 +0000

    Add package pytrinamic/0.1.12/PyTrinamic-0.1.12-py3.7.egg

---
+++ packages/pytrinamic/PyTrinamic-0.1.12-py3.7.egg/EGG-INFO/scripts/ShowAvailableCOMPorts.py
@@ -0,0 +1,18 @@
+#!C:\Users\laborstudent\workspace\python\PyTrinamic\venv\Scripts\python.exe
+'''
+Print out all available USB and Serial ports that are available for connecting.
+
+Created on 29.04.2019
+
+@author: LH
+'''
+
+import PyTrinamic
+
+PyTrinamic.showInfo()
+
+print("USB Ports:")
+PyTrinamic.showAvailableComPorts(USB=True)
+
+print("Serial Ports:")
+PyTrinamic.showAvailableComPorts(Serial=True)

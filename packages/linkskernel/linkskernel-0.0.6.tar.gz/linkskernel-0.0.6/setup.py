b0da6079aba564a24ecd92b1c4b0c795bed11bda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:58 +0000

    Add package linkskernel/0.0.6/linkskernel-0.0.6.tar.gz

---
+++ packages/linkskernel/linkskernel-0.0.6.tar.gz/linkskernel-0.0.6/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+setup(
+    name='linkskernel',
+    version='0.0.6',
+    packages=['links_kernel'],
+    description='Links kernel for Jupyter',
+    long_description=open('README.txt').read(),
+    author='Tom Davey & Simon Fowler',
+    author_email='tomjackdavey@hotmail.com',
+    url='https://github.com/SimonJF/links-server',
+    install_requires=[
+        'jupyter_client', 'IPython', 'ipykernel',
+	'metakernel'
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+	"License :: OSI Approved :: MIT License",
+        'Programming Language :: Python :: 3',
+    ],
+)

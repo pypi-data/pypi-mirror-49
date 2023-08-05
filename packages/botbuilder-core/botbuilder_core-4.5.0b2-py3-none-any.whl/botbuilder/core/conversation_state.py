f72bfdc2e2e1dba7699204e863ef933a43adc985
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:29 +0000

    Add package botbuilder-core/4.5.0b2/botbuilder_core-4.5.0b2-py3-none-any.whl

---
+++ packages/botbuilder-core/botbuilder_core-4.5.0b2-py3-none-any.whl/botbuilder/core/conversation_state.py
@@ -0,0 +1,38 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+from .turn_context import TurnContext
+from .bot_state import BotState
+from .storage import Storage
+
+
+class ConversationState(BotState):
+    """Conversation State
+    Reads and writes conversation state for your bot to storage.
+    """
+
+    no_key_error_message = 'ConversationState: channelId and/or conversation missing from context.activity.'
+
+    def __init__(self, storage: Storage):
+        """Creates a new ConversationState instance.
+        Parameters
+        ----------
+        storage : Storage
+            Where to store 
+        namespace: str
+        """
+
+        super(ConversationState, self).__init__(storage, 'ConversationState')
+
+    def get_storage_key(self, context: TurnContext):
+        channel_id = context.activity.channel_id or self.__raise_type_error("invalid activity-missing channel_id")
+        conversation_id = context.activity.conversation.id or self.__raise_type_error(
+            "invalid activity-missing conversation.id")
+
+        storage_key = None
+        if channel_id and conversation_id:
+            storage_key = "%s/conversations/%s" % (channel_id,conversation_id)
+        return storage_key
+
+    def __raise_type_error(self, err: str = 'NoneType found while expecting value'):
+        raise TypeError(err)

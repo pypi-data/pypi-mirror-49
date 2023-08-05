d0268c327f1869581750c5f02982d987f1fcf43f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:49 +0000

    Add package domdf-wxpython-tools/0.1.14/domdf_wxpython_tools-0.1.14.tar.gz

---
+++ packages/domdf-wxpython-tools/domdf_wxpython_tools-0.1.14.tar.gz/domdf_wxpython_tools-0.1.14/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setup(
+	name="domdf_wxpython_tools",
+	version="0.1.14",
+    author='Dominic Davis-Foster',
+	author_email="dominic@davis-foster.co.uk",
+	packages=find_packages(),
+	license="OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+	url="https://github.com/domdfcoding/domdf_wxpython_tools",
+	description='Tools and widgets for wxPython',
+	long_description=long_description,
+	long_description_content_type="text/x-rst",
+	classifiers=[
+        "Programming Language :: Python :: 3",
+		"License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Operating System :: OS Independent",
+		"Development Status :: 4 - Beta",
+    ],
+	install_requires=[
+		"matplotlib>=3.0.0"
+	#	"wxPython >= 4.0.0",
+	],
+)

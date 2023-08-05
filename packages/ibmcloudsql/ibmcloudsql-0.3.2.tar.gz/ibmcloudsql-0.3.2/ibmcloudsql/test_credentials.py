09ec80bd82db108b9cc683ea7c2cb9317af798eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:08 +0000

    Add package ibmcloudsql/0.3.2/ibmcloudsql-0.3.2.tar.gz

---
+++ packages/ibmcloudsql/ibmcloudsql-0.3.2.tar.gz/ibmcloudsql-0.3.2/ibmcloudsql/test_credentials.py
@@ -0,0 +1,3 @@
+apikey='YNiXREBMTfQsSzKrXYhQJElNXnUFEgpQ7qVVTkDK3_Zr'
+instance_crn='crn%3Av1%3Abluemix%3Apublic%3Asql-query%3Aus-south%3Aa%2Fd86af7367f70fba4f306d3c19c938f2f%3Ad1b2c005-e3d8-48c0-9247-e9726a7ed510%3A%3A'
+result_location='cos://us-south/sqltempregional/'

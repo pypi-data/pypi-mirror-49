5d0db37c50f11c5bc5b83da7f199330b3c218ccd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:53 +0000

    Add package ucloud-sdk-python3/0.1.4/ucloud_sdk_python3-0.1.4-py3-none-any.whl

---
+++ packages/ucloud-sdk-python3/ucloud_sdk_python3-0.1.4-py3-none-any.whl/ucloud/core/typesystem/abstract.py
@@ -0,0 +1,47 @@
+import typing
+
+
+class Field(object):
+    def __init__(
+        self,
+        required: bool = False,
+        default: typing.Any = None,
+        dump_to: str = None,
+        load_from: str = None,
+        **kwargs
+    ):
+        self.required = required
+        self.default = default
+        self.dump_to = dump_to
+        self.load_from = load_from
+        self.options = kwargs
+
+    def dumps(self, value, **kwargs):
+        raise NotImplementedError
+
+    def loads(self, value, **kwargs):
+        raise NotImplementedError
+
+
+class Schema(object):
+    fields = {}
+
+    def __init__(
+        self,
+        required: bool = False,
+        default: typing.Any = None,
+        dump_to: str = None,
+        load_from: str = None,
+        **kwargs
+    ):
+        self.required = required
+        self.default = default
+        self.dump_to = dump_to
+        self.load_from = load_from
+        self.options = kwargs
+
+    def dumps(self, d: dict) -> dict:
+        raise NotImplementedError
+
+    def loads(self, d: dict) -> dict:
+        raise NotImplementedError

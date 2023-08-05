5f7677329a79fcc4c2c4820c57bff0861a6a7746
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:37:27 +0000

    Add package pvault/1.1.0a0/pvault-1.1.0a0.tar.gz

---
+++ packages/pvault/pvault-1.1.0a0.tar.gz/pvault-1.1.0a0/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open('requirements.txt') as f:
+    requirements = f.readlines()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pvault",
+    version="1.1.0a",
+    author="oluwafenyi",
+    author_email="o.enyioma@gmail.com",
+    description="A password manager package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/oluwafenyi/password-vault",
+    license="MIT",
+    package_name=["assets"],
+    scripts=["assets/pv.py", "assets/pv_password.py",
+             "assets/pv_management.py", "assets/pv_generate.py"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+    ],
+    install_requires=requirements
+)

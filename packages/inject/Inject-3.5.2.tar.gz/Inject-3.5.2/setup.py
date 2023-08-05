88b3b0839d5de8f852ddf3f5eae5fb9a0be2df0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:19 +0000

    Add package inject/3.5.2/Inject-3.5.2.tar.gz

---
+++ packages/inject/Inject-3.5.2.tar.gz/Inject-3.5.2/setup.py
@@ -0,0 +1,46 @@
+# encoding: utf-8
+import sys
+
+from setuptools import setup
+
+
+def read_description():
+    with open('README.md', 'r') as f:
+        return f.read()
+
+
+setup(
+    name='Inject',
+    version='3.5.2',
+    url='https://github.com/ivankorobkov/python-inject',
+    license='Apache License 2.0',
+
+    author='Ivan Korobkov',
+    author_email='ivan.korobkov@gmail.com',
+
+    description='Python dependency injection framework',
+    long_description=read_description(),
+    long_description_content_type="text/markdown",
+
+    package_dir={'': 'src'},
+    py_modules=['inject'],
+    data_files=[
+        (
+            'lib/python{}.{}/site-packages'.format(*sys.version_info[:2]),
+            ['src/inject.py']
+        )
+    ],
+
+    install_requires=['typing'],
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules']
+)

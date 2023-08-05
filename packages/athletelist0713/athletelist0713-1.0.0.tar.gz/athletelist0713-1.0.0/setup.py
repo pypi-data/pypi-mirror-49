d1afd6a99786b7997e3bf9c20a3bf06cdb09eee7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:12 +0000

    Add package athletelist0713/1.0.0/athletelist0713-1.0.0.tar.gz

---
+++ packages/athletelist0713/athletelist0713-1.0.0.tar.gz/athletelist0713-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name         = 'athletelist0713',
+    version      ='1.0.0',
+    py_modules   =['athletelist0713'],
+    author       ='zrh-sq',
+    author_email ='1336746693@qq.com',
+    url          ='http://www.headfirstlabs.com',
+    description  ='A class of getting athlete_time',
+    )

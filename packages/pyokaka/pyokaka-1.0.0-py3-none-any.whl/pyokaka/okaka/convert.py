83dfd61095fe3c4db7c8f7d95864284c2a7fbaa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:57 +0000

    Add package pyokaka/1.0.0/pyokaka-1.0.0-py3-none-any.whl

---
+++ packages/pyokaka/pyokaka-1.0.0-py3-none-any.whl/pyokaka/okaka/convert.py
@@ -0,0 +1,78 @@
+from ..util.priority_dict import PriorityDict
+
+import json
+from pathlib import Path
+
+
+_dir = Path(__file__).parent
+
+
+def _make_sokuon_tpls(items):
+    return [
+        ('{}{}'.format(key[0], key), 'っ{}'.format(value))
+        for key, value in items
+    ]
+
+
+def _load_transtable():
+    with (_dir / '../transtable.json').open(encoding='utf-8') as fin:
+        table = json.load(fin)
+
+    update_transtable({
+        key: value for key, value in table.items()
+        if key not in 'あいうえおん'
+    })
+    _convert_dct.update(
+        (key, value) for value, keys in table.items() for key in keys
+        if value in 'あいうえおん'
+    )
+    _convert_dct.update([
+        ('\'', ''), ('-', 'ー')
+    ])
+
+
+#
+#
+def convert(roman_sentence):
+    roman_sentence = roman_sentence.lower()
+
+    def step():
+        for key, value in _convert_dct.items():
+            if roman_sentence.startswith(key):
+                return key, value
+
+        head_ch = roman_sentence[0]
+        return head_ch, head_ch
+
+    ret = []
+    while roman_sentence:
+        frm, to = step()
+
+        ret.append(to)
+        roman_sentence = roman_sentence[len(frm):]
+
+    return ''.join(ret)
+
+
+def update_convert_dct(dct, *, top_priority=False):
+    items = tuple(dct.items())
+    items = {
+        **dct, **dict(_make_sokuon_tpls(items))
+    }
+
+    if top_priority:
+        _convert_dct.update(items, priority_func=lambda _: 0)
+    else:
+        _convert_dct.update(items)
+
+
+def update_transtable(table):
+    update_convert_dct({
+        key: value for value, keys in table.items() for key in keys
+    })
+
+
+#
+#
+_convert_dct = PriorityDict(lambda k: 100-len(k))
+_load_transtable()

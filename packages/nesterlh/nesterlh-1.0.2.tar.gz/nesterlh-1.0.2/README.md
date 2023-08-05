d4695aa8d8b6756cfe43308a56e47073275718e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:28 +0000

    Add package nesterlh/1.0.2/nesterlh-1.0.2.tar.gz

---
+++ packages/nesterlh/nesterlh-1.0.2.tar.gz/nesterlh-1.0.2/README.md
@@ -0,0 +1,1 @@
+A simple printer of nested lists
---
+++ packages/nesterlh/nesterlh-1.0.2.tar.gz/nesterlh-1.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description =fh.read()
+
+setuptools.setup(
+        name        = 'nesterlh',
+        version     = '1.0.2',
+        py_modules  = ['nesterlh'],
+        author      = 'leohuang',
+        author_email= 'leohuang@leohuang.leohuang',
+        url         = 'https://www.leohuang.top',
+        description = 'A simple printer of nested lists',
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        packages=setuptools.find_packages(),
+        license='MIT',
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+)

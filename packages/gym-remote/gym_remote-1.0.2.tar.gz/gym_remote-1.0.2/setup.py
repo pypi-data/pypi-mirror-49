e76941002ecc1140f25001a98ef083c0aa468cd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:51 +0000

    Add package gym-remote/1.0.2/gym_remote-1.0.2.tar.gz

---
+++ packages/gym-remote/gym_remote-1.0.2.tar.gz/gym_remote-1.0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='gym_remote',
+      version='1.0.2',
+      url='https://github.com/wau/gym-remote',
+      license='MIT',
+      install_requires=['gym>=0.2.3', 'requests']
+)

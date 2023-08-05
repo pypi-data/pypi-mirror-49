d38c6c0ae717009f07f1426a34a41b9338b1e36e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:44 +0000

    Add package opencroplib/0.1.4b0/opencroplib-0.1.4b0.tar.gz

---
+++ packages/opencroplib/opencroplib-0.1.4b0.tar.gz/opencroplib-0.1.4b0/opencroplib/__init__.py
@@ -0,0 +1,9 @@
+
+__version__ = "0.1.4b"
+from . import time_utils
+from . import atmophere
+from . import radiation
+from . import aerodynamic
+from . import plant
+
+from . import test_opencroplib
---
+++ packages/opencroplib/opencroplib-0.1.4b0.tar.gz/opencroplib-0.1.4b0/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+import opencroplib
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='opencroplib',
+    version=opencroplib.__version__,
+    author='Jose A. Jimenez-Berni',
+    author_email='berni@ias.csic.es',
+    packages=['opencroplib'],
+    url='https://github.com/OpenAgriTech/opencroplib',
+    license='LICENSE.txt',
+    description='Python Library for simulating physiological processes in plants',
+    long_description=long_description,
+    install_requires=[
+        'numpy', 'pytz', 'pandas'
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Atmospheric Science",
+    ],
+)

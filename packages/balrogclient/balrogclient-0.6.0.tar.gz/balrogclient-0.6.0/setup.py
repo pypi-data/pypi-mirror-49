d6f15edb8f23470b6ed77b92dad1f4cf1a1e928e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:26 +0000

    Add package balrogclient/0.6.0/balrogclient-0.6.0.tar.gz

---
+++ packages/balrogclient/balrogclient-0.6.0.tar.gz/balrogclient-0.6.0/setup.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="balrogclient",
+    version="0.6.0",
+    description="Balrog Admin API Client",
+    author="Mozilla Release Engineers",
+    author_email="release+python@mozilla.com",
+    url="https://github.com/mozilla/balrog",
+    license="MPL-2.0",
+    packages=["balrogclient"],
+    test_suite="balrogclient.test",
+    install_requires=["requests"],
+    include_package_data=True,
+)

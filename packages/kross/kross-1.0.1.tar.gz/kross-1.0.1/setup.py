36288047da837b739a4b4c3a01a1c71da7358b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:42:22 +0000

    Add package kross/1.0.1/kross-1.0.1.tar.gz

---
+++ packages/kross/kross-1.0.1.tar.gz/kross-1.0.1/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+
+# Get requirements
+def get_requirements():
+    with open("requirements.txt") as f:
+        return f.read().splitlines()
+
+
+def get_description():
+    with open("README.md") as f:
+        return f.read()
+
+
+# Setup
+setup(
+    name="kross",
+    version="1.0.1",
+    license="apache-2.0",
+    author="pcorbel",
+    author_email="pierrot.corbel@gmail.com",
+    url="https://github.com/pcorbel/kross",
+    download_url="https://github.com/pcorbel/kross/archive/v1.0.1.tar.gz",
+    description='A simple CLI to "multi-arch all the things"',
+    long_description=get_description(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=get_requirements(),
+    entry_points={"console_scripts": ["kross = kross.main:cli"]},
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Utilities",
+    ],
+)

113b0465dacca8aeacaceff5c4ec552f6d6f5ccb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:59 +0000

    Add package RelStorage/3.0a4/RelStorage-3.0a4-cp35-cp35m-win32.whl

---
+++ packages/RelStorage/RelStorage-3.0a4-cp35-cp35m-win32.whl/relstorage/adapters/postgresql/stats.py
@@ -0,0 +1,30 @@
+##############################################################################
+#
+# Copyright (c) 2009 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""
+Stats implementations
+"""
+
+from __future__ import absolute_import
+
+from ..stats import AbstractStats
+
+
+class PostgreSQLStats(AbstractStats):
+
+    def get_db_size(self):
+        """Returns the approximate size of the database in bytes"""
+        def callback(_conn, cursor):
+            cursor.execute("SELECT pg_database_size(current_database())")
+            return cursor.fetchone()[0]
+        return self.connmanager.open_and_call(callback)

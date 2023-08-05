b4df487dfaf78ab5c2b7282bcca32527f327aca0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:45 +0000

    Add package zhulong5/1.4.5/zhulong5-1.4.5.tar.gz

---
+++ packages/zhulong5/zhulong5-1.4.5.tar.gz/zhulong5-1.4.5/setup.py
@@ -0,0 +1,61 @@
+from __future__ import print_function
+from setuptools import setup, find_packages
+import sys
+
+setup(
+    name="zhulong5",
+
+    version="1.4.5",
+
+    author="lanmengfei",
+    author_email="865377886@qq.com",
+    description="深圳市筑龙科技的工作-特殊项目",
+    long_description=open("README.txt",encoding="utf8").read(),
+
+    url="https://github.com/lanmengfei/testdm",
+
+    packages=find_packages(),
+    package_data={  # "zhulong.hunan":["profile"]
+        "zhulong5.util_qgsp": ["cfg_db"],
+        # "zhulong5.jianzhu_spider": ['rerun.txt','rerun2.txt','rerun3.txt'],
+
+    },
+
+    install_requires=[
+        "pandas >= 0.13",
+        "beautifulsoup4>=4.6.3",
+        "cx-Oracle",
+        "numpy",
+        "psycopg2-binary",
+        "selenium",
+        "xlsxwriter",
+        "xlrd",
+        "requests",
+        "lxml",
+        "sqlalchemy",
+        "pymssql",
+        "jieba",
+        "mysqlclient",
+        "pymssql",
+        "lmf>=2.0.6",
+        "lmfscrap>=1.1.0",
+        "lmfhawq>=1.2.0",
+        "gzqzl>=1.2.8",
+        ],
+
+    classifiers=[
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Text Processing :: Indexing",
+        "Topic :: Utilities",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5"
+    ],
+)
+
+
+
---
+++ packages/zhulong5/zhulong5-1.4.5.tar.gz/zhulong5-1.4.5/zhulong5/__init__.py
@@ -0,0 +1,2 @@
+from zhulong5.util.etl  import est_meta, est_html_cdc
+from zhulong5.util_jianzhu.etl  import est_html, gg_existed

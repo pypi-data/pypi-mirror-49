8ff13ceb40c0c570c5b129ade970f2a16ba46d4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:40:06 +0000

    Add package nester1907/1.6.0/nester1907-1.6.0.tar.gz

---
+++ packages/nester1907/nester1907-1.6.0.tar.gz/nester1907-1.6.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+    name         = 'nester1907',
+    version      ='1.6.0',
+    py_modules   =['nester1907'],
+    author       ='zrh-sq',
+    author_email ='1336746693@qq.com',
+    url          ='http://www.headfirstlabs.com',
+    description  ='Asimple printer of nested lists',
+    )
+    

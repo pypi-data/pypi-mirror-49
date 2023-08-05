d61e6871cc69baf135599a54e409bbfbcea0f9b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:27:41 +0000

    Add package dcma/0.1.0/dcma-0.1.0.tar.gz

---
+++ packages/dcma/dcma-0.1.0.tar.gz/dcma-0.1.0/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="dcma",
+    version="0.1.0",
+    description="Deep Codon Mutation Analyser",
+    license="MIT",
+    author="Jan Justi",
+    author_email='jan.ma.justi@gmail.com',
+    url='https://github.com/janjusti/dppa',
+    packages=find_packages(),
+    install_requires=[
+        "tqdm==4.31.1",
+        "anytree==2.6.0",
+        "pandas==0.24.2",
+        "numpy==1.16.3",
+        "biopython==1.73",
+        "openpyxl==2.6.2",
+        "styleframe==2.0.3"
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+    ],
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'run-dcma = dcma.core:_main'
+        ]
+    }
+)

6f710e00785efd8ba82d97b717a51b2590a701fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:19 +0000

    Add package sqliteorm/1.0.0/sqliteorm-1.0.0.tar.gz

---
+++ packages/sqliteorm/sqliteorm-1.0.0.tar.gz/sqliteorm-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name = "sqliteorm",
+    author = "aaronhua",
+    version = "1.0.0",
+    author_email = "cjhdwyyx@163.com",
+    url = "http://www.xxx.com"
+)

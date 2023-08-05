51c77cdd7f1da8ef041cc3bd69b79c95bd527d07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:02 +0000

    Add package shinkei/0.1.1/shinkei-0.1.1-py3-none-any.whl

---
+++ packages/shinkei/shinkei-0.1.1-py3-none-any.whl/shinkei/exceptions.pyi
@@ -0,0 +1,29 @@
+import aiohttp
+
+class ShinkeiException(Exception): ...
+
+
+class ShinkeiHTTPException(ShinkeiException):
+    request: aiohttp.ClientResponse
+    code: int
+    message: str
+
+    def __init__(self, request: aiohttp.ClientResponse, code: int, message: str) -> None: ...
+
+
+class ShinkeiWSException(ShinkeiException):
+    message: str
+
+    def __init__(self, message: str) -> None: ...
+
+
+class ShinkeiResumeWS(ShinkeiException):
+    message: str
+
+    def __init__(self, message: str) -> None: ...
+
+class ShinkeiWSClosed(ShinkeiException):
+    code: int
+    message: str
+
+    def __init__(self, message: str, code: int) -> None: ...

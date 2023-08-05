680b8bd7455f622a98fb227c6a1b2decb67918fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:17 +0000

    Add package simpleonedrive/0.1/simpleonedrive-0.1.tar.gz

---
+++ packages/simpleonedrive/simpleonedrive-0.1.tar.gz/simpleonedrive-0.1/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+setup(
+    name='simpleonedrive',    # This is the name of your PyPI-package.
+    version='0.1',                          # Update the version number for new releases
+    scripts=['simpleonedrive']                  # The name of your scipt, and also the command you'll be using for calling it
+)

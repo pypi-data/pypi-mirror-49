dfc4c1bea11f5666487f99067904a36f106099f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:28:42 +0000

    Add package pyhiveapi/0.2.18/pyhiveapi-0.2.18.tar.gz

---
+++ packages/pyhiveapi/pyhiveapi-0.2.18.tar.gz/pyhiveapi-0.2.18/README.txt
@@ -0,0 +1,7 @@
+A Python library to interface with the Hive API, mainly to support Home Assistant
+
+Home Assistant version : pyhiveapi version
+0.59.0 : 0.2.5
+0.61.0 : 0.2.10
+0.62.0 : 0.2.11
+0.86.0 : 0.2.17
---
+++ packages/pyhiveapi/pyhiveapi-0.2.18.tar.gz/pyhiveapi-0.2.18/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+setup(
+    name='pyhiveapi',
+    version='0.2.18',
+    description='A Python library to interface with the Hive API',
+    long_description="A Python library to interface with the Hive API",
+    url='https://github.com/Rendili/pyhiveapi',
+
+    author='Rendili',
+    author_email='rendili@outlook.com',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        #        'Topic :: Software Development :: API',
+        'License :: OSI Approved :: MIT License',
+        #        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    python_requires='>=3.5.*',
+    keywords='Hive API Library',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    #    packages=["pyhiveapi"],
+    #    install_requires=[],
+
+    entry_points={
+        'console_scripts': [
+            'pyhiveapi=pyhiveapi.pyhiveapi:Pyhiveapi',
+        ],
+    }
+)

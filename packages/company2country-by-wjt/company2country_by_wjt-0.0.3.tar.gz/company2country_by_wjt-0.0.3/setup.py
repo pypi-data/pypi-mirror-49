8be0dafbf1d78837a7279e9aaa6cd683029ee584
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:43 +0000

    Add package company2country-by-wjt/0.0.3/company2country_by_wjt-0.0.3.tar.gz

---
+++ packages/company2country-by-wjt/company2country_by_wjt-0.0.3.tar.gz/company2country_by_wjt-0.0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import  setup,find_packages
+
+setup(
+    name='company2country_by_wjt',
+    version='0.0.3',
+    keywords='get country by company name ',
+    description='hello pypi',
+    license='',
+    url='',
+    author='wujiating',
+    author_email='wujiating@mesalab.cn',
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    install_requires=[],
+)

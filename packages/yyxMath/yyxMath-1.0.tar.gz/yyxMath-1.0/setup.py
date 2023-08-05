91017fff5ad0f43707a36e54010ffe5acb98667e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:27 +0000

    Add package yyxMath/1.0/yyxMath-1.0.tar.gz

---
+++ packages/yyxMath/yyxMath-1.0.tar.gz/yyxMath-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+name='yyxMath',# 对 外 我 们 模 块 的 名 字
+version='1.0',# 版 本 号
+description='这是第一个对外发布的模块，yyxMath测试哦',# 描 述
+author='yyx',# 作 者
+author_email='171807707@qq.com',
+py_modules=['yyxMath.demo1','yyxMath.demo2']# 要 发 布 的 模 块
+)

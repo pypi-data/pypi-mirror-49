1aa924afe0cb7027f3ffd539fd841c4a470138fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:16 +0000

    Add package logonlabs-python/1.0.1/logonlabs_python-1.0.1-py3-none-any.whl

---
+++ packages/logonlabs-python/logonlabs_python-1.0.1-py3-none-any.whl/logonlabs/allowableValues.py
@@ -0,0 +1,6 @@
+
+identityProviders = ['microsoft','google','facebook','linkedin','slack','twitter','github','quickbooks','onelogin']
+
+eventTypes = ['LocalLogin', 'LocalLogout']
+
+localVallidationValues = ['Pass', 'Fail', 'NotApplicable']

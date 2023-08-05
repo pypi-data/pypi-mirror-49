24305043b8ed6dae7e24a9d5ce874f1b037bfbe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:38 +0000

    Add package Pop-Con/1.0.1/Pop_Con-1.0.1-py3-none-any.whl

---
+++ packages/Pop-Con/Pop_Con-1.0.1-py3-none-any.whl/popcon/util.py
@@ -0,0 +1,15 @@
+from os import path, makedirs
+import errno
+
+
+def mkdir_p(dir_path):
+   try:
+      makedirs(dir_path)
+   except OSError as exc: # Python >2.5
+      if exc.errno == errno.EEXIST and path.isdir(dir_path):
+         pass
+      else:
+         raise
+
+
+

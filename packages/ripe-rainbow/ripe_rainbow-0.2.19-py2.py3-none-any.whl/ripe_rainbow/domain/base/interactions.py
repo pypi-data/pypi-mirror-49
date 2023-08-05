426c3e600d79304f4fc23a8be08b539f57c81ee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:37 +0000

    Add package ripe-rainbow/0.2.19/ripe_rainbow-0.2.19-py2.py3-none-any.whl

---
+++ packages/ripe-rainbow/ripe_rainbow-0.2.19-py2.py3-none-any.whl/ripe_rainbow/domain/base/interactions.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from .. import parts
+
+class InteractionsPart(parts.Part):
+
+    def try_click(self, element, focus = True):
+        return self.driver.click(element, focus = focus)
+
+    def click_when_possible(self, selector, condition = None, scroll = True):
+        """
+        Clicks an element when possible, which happens when that element is both
+        visible and "clickable".
+
+        Optionally the browser viewport is "scrolled" to the visual position
+        of the element, enabling proper "clicking".
+
+        :type selector: String
+        :param selector: The selector for the element to click.
+        :type condition: Function
+        :param condition: The filter the selected element must pass to be "clickable".
+        :type scroll: bool
+        :param scroll: If the browser viewport should be scrolled to the element
+        before the click operation is performed.
+        :rtype Element
+        :return The clicked element if there's any otherwise an invalid value.
+        """
+
+        # waits for the element to be available at the DOM and then
+        # optionally runs the scroll operation to the element
+        element = self.waits.element(selector, condition = condition)
+        if scroll: self.driver.scroll_to(element)
+
+        element = self.waits.is_visible(selector, condition = condition)
+
+        return self.waits.until(
+            lambda d: self.try_click(element),
+            "Element '%s' found but never became clickable" % selector
+        )
---
+++ packages/ripe-rainbow/ripe_rainbow-0.2.19-py2.py3-none-any.whl/ripe_rainbow/interactive/drivers.py
@@ -0,0 +1,223 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys
+
+import appier
+
+from .. import errors
+
+class InteractiveDriver(object):
+
+    def __init__(self, owner):
+        self.owner = owner
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def get(self, url):
+        raise appier.NotImplementedError()
+
+    def find_element(self, selector):
+        raise appier.NotImplementedError()
+
+    def find_elements(self, selector):
+        raise appier.NotImplementedError()
+
+    def find_by_name(self, name):
+        raise appier.NotImplementedError()
+
+    def focus(self, element):
+        raise appier.NotImplementedError()
+
+    def click(self, element, focus = True):
+        raise appier.NotImplementedError()
+
+    def scroll_to(self, element):
+        """
+        Scrolls the element on which it's called into the visible area
+        of the browser window.
+
+        This operation should be performed in a deterministic way.
+
+        :type element: Element
+        :param element: The element to scroll into view.
+        """
+
+        raise appier.NotImplementedError()
+
+    def wrap_outer(self, method, *args, **kwargs):
+        return method(*args, **kwargs)
+
+    def wrap_inner(self, method, *args, **kwargs):
+        return method(*args, **kwargs)
+
+    @property
+    def current_url(self):
+        raise appier.NotImplementedError()
+
+    def _wait(self, timeout = None):
+        raise appier.NotImplementedError()
+
+class SeleniumDriver(InteractiveDriver):
+
+    def __init__(self, owner):
+        InteractiveDriver.__init__(self, owner)
+        self.maximized = appier.conf("SEL_MAXIMIZED", False, cast = bool)
+        self.headless = appier.conf("SEL_HEADLESS", False, cast = bool)
+        self.window_size = appier.conf("SEL_WINDOW_SIZE", "1920x1080")
+
+    def get(self, url):
+        return self.instance.get(url)
+
+    def find_element(self, selector):
+        return self.instance.find_element_by_css_selector(selector)
+
+    def find_elements(self, selector):
+        return self.instance.find_elements_by_css_selector(selector)
+
+    def find_by_name(self, name):
+        return self.instance.find_element_by_name(name)
+
+    def find_element_by_css_selector(self, selector):
+        return self.find_element(selector)
+
+    def find_elements_by_css_selector(self, selector):
+        return self.find_elements(selector)
+
+    def find_element_by_name(self, name):
+        return self.find_by_name(name)
+
+    def focus(self, element):
+        from selenium.webdriver.common.action_chains import ActionChains
+        actions = ActionChains(self.instance)
+        actions.move_to_element(element)
+        actions.perform()
+
+    def click(self, element, focus = True):
+        from selenium.webdriver.common.action_chains import ActionChains
+        from selenium.common.exceptions import ElementClickInterceptedException, ElementNotVisibleException, WebDriverException
+
+        try:
+            if focus:
+                self.scroll_to(element)
+                actions = ActionChains(self.instance)
+                actions.move_to_element(element)
+                actions.click(element)
+                actions.perform()
+            else:
+                element.click()
+
+            return element
+        except (
+            ElementClickInterceptedException,
+            ElementNotVisibleException,
+            WebDriverException
+        ) as exception:
+            self.owner.logger.debug("Element is not \"clickable\" because: %s" % exception)
+            return None
+
+    def scroll_to(self, element):
+        # as Selenium doesn't automatically support automatically scrolling
+        # in elements inside the page, such as when using an iframe or
+        # overflow scroll, therefore we must rely on Web API Element.scrollIntoView()
+        # that allows proper scroll operation into element
+        self.instance.execute_script("arguments[0].scrollIntoView();", element)
+
+    def wrap_outer(self, method, *args, **kwargs):
+        from selenium.common.exceptions import TimeoutException
+        try: return method(*args, **kwargs)
+        except TimeoutException: raise errors.TimeoutError()
+
+    def wrap_inner(self, method, *args, **kwargs):
+        """
+        Wraps the method being waited on to tolerate some exceptions since
+        in most cases these are transitory conditions that shouldn't break
+        the test.
+
+        :type method: Function
+        :param method: The method that is going to be executed in a wrapped
+        fashion to properly handle exceptions.
+        :rtype Function
+        :return: The method wrapped on a try-catch for exceptions.
+        """
+
+        from selenium.common.exceptions import StaleElementReferenceException
+        try: return method(*args, **kwargs)
+        except (StaleElementReferenceException, AssertionError) as exception:
+            self.owner.logger.debug("Got exception while waiting: %s" % exception)
+            return None
+
+    @property
+    def current_url(self):
+        return self.instance.current_url
+
+    @property
+    def instance(self):
+        cls = self.__class__
+        if hasattr(cls, "_instance") and cls._instance:
+            return cls._instance
+
+        import selenium.webdriver
+
+        # creates the underlying instance of the Chrome driver
+        # that is going to be used in the concrete execution
+        cls._instance = selenium.webdriver.Chrome(
+            chrome_options = self._selenium_options()
+        )
+
+        # in case the browser should be started maximized, then
+        # a new argument is added to the list of options
+        if self.maximized:
+            cls._instance.fullscreen_window()
+
+        # in case the browser should have a specific window size
+        # then splits the window size around the specific values
+        if self.window_size:
+            width, height = (int(value) for value in self.window_size.split("x"))
+            cls._instance.set_window_size(width, height)
+
+        self.owner.runner.add_on_finish(self._destroy_instance)
+        return cls._instance
+
+    def _destroy_instance(self):
+        cls = self.__class__
+        if not hasattr(cls, "_instance") or not self._instance:
+            return
+        cls._instance.close()
+        cls._instance = None
+
+    def _selenium_options(self):
+        import selenium.webdriver
+
+        # crates the base object for the options to be used by
+        # the Google Chrome browser
+        options = selenium.webdriver.ChromeOptions()
+
+        # adds some of the default arguments to be used for the
+        # execution of the Google Chrome instance
+        options.add_argument("--disable-extensions")
+
+        # in case the headless mode was selected then an
+        # extra argument is added to the set of options
+        if self.headless:
+            options.add_argument("--headless")
+
+        # fixes issue when running within a docker container
+        # since it typically uses /dev/shm shared memory with
+        # little space making chrome crash when rendering large pages
+        if sys.platform.startswith("linux"):
+            options.add_argument("--disable-dev-shm-usage")
+            options.add_argument("--no-sandbox")
+
+        # returns the options to the calling method as expected
+        # by the current infrastructure
+        return options
+
+    def _wait(self, timeout = None):
+        from selenium.webdriver.support.ui import WebDriverWait
+        if timeout == None: timeout = self.owner.timeout
+        return WebDriverWait(self.instance, timeout)

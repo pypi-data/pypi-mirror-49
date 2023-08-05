650ef4350d402ed35bcd67ded71540f80dada678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:39:04 +0000

    Add package redshift/0.0.1/redshift-0.0.1.tar.gz

---
+++ packages/redshift/redshift-0.0.1.tar.gz/redshift-0.0.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name="redshift",
+    version="0.0.1",
+    author="Zach Christopherson",
+    author_email="zach@christopherson.io",
+    description="",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="https://github.com/zach-data/redshift",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

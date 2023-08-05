1fe916bd391af6f3fe640e154b75725cebc67f72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:05 +0000

    Add package sotabench/0.0.3/sotabench-0.0.3.tar.gz

---
+++ packages/sotabench/sotabench-0.0.3.tar.gz/sotabench-0.0.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+PACKAGE_NAME = "sotabench"
+LICENSE = "Apache 2.0"
+AUTHOR = "rstojnic"
+EMAIL = "hello@sotabench.com"
+URL = "https://sotabench.com"
+DESCRIPTION = "Easily benchmark Machine Learning models on selected tasks and datasets"
+
+
+setup(
+    name=PACKAGE_NAME,
+    maintainer=AUTHOR,
+    version='0.0.3',
+    packages=[PACKAGE_NAME,
+              'sotabench.core',
+              'sotabench.datasets',
+              'sotabench.image_classification',
+              'sotabench.image_generation',
+              'sotabench.object_detection',
+              'sotabench.semantic_segmentation'],
+    include_package_data=True,
+    license=LICENSE,
+    description=DESCRIPTION,
+    long_description=DESCRIPTION,
+    url=URL,
+    install_requires=['torch', 'torchvision'],
+)

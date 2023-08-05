1048cd08b33b6dff358fa385944aa0ee68e0c7d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:38:12 +0000

    Add package pyPheWAS/2.1.0/pyPheWAS-2.1.0.tar.gz

---
+++ packages/pyPheWAS/pyPheWAS-2.1.0.tar.gz/pyPheWAS-2.1.0/setup.py
@@ -0,0 +1,36 @@
+from distutils.core import setup
+setup(
+  name = 'pyPheWAS',
+  packages = ['pyPheWAS'], # this must be the same as the name above
+  version = '2.1.0',
+  description = 'MASI Lab Port of PheWAS into Python',
+  author = 'MASI Lab',
+  author_email = 'bennett.landman@vanderbilt.edu',
+  url = 'https://github.com/MASILab/pyPheWAS', # use the URL to the github repo
+  download_url = 'https://github.com/MASILab/pyPheWAS/tarball/0.1', # I'll explain this in a second
+  keywords = ['PheWAS', 'ICD-9', 'EMR'], # arbitrary keywords
+  classifiers = [],
+  install_requires=['numpy',
+	'matplotlib',
+	'scipy',
+	'pandas',
+	'statsmodels',
+        'numexpr',
+        'bottleneck',
+        'hopcroftkarp',
+        'tqdm'
+	],
+  package_data={
+    '':['resources/*.csv']
+  },
+  scripts=['bin/maximizeControls',
+  'bin/pyPhewasLookup',
+  'bin/pyPhewasModel',
+  'bin/pyPhewasPlot',
+  'bin/pyPhewasPipeline',
+  'bin/generateGroups',
+  'bin/censorData',
+  'bin/convertEventToAge',
+  'bin/createGenotypeFile'
+  ],
+)

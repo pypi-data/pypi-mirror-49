2f197982d9ec077b99653473d59dbf79d4b59ddd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:03:52 +0000

    Add package tshx/1.9.2/tshx-1.9.2.tar.gz

---
+++ packages/tshx/tshx-1.9.2.tar.gz/tshx-1.9.2/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'tshx',
+  packages = ['Tssh'],
+  version = '1.9.2',
+  license='MIT',
+  description = 'hacking shell for termux . by 22bit',
+  author = '22bit',
+  author_email = '22bit@protonmail.com',
+  url = 'https://github.com/22bit/tshx.git',
+  download_url = 'https://github.com/22bit/tshx.git',
+  keywords = ['tsh', 'shell', 'thundershell'],
+  install_requires=[
+          'future',
+          'bs4',
+          'requests',
+          'colorama',
+          'wget==3.2',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Topic :: Software Development :: Build Tools',    'License :: OSI Approved :: MIT License',   # Again, pick a license    'Programming Language :: Python :: 3',    
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)

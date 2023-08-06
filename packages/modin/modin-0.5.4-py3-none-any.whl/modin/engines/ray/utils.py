1f5b80a1089ecf4ca2331af25ee320b82452cdb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:28 +0000

    Add package modin/0.5.4/modin-0.5.4-py3-none-any.whl

---
+++ packages/modin/modin-0.5.4-py3-none-any.whl/modin/engines/ray/utils.py
@@ -0,0 +1,19 @@
+import sys
+
+if sys.version_info[0] == 3:
+    import builtins
+else:
+    import __builtin__ as builtins
+
+
+def handle_ray_task_error(e):
+    for s in e.traceback_str.split("\n")[::-1]:
+        if "Error" in s or "Exception" in s:
+            try:
+                raise getattr(builtins, s.split(":")[0])("".join(s.split(":")[1:]))
+            except AttributeError as att_err:
+                if "module" in str(att_err) and builtins.__name__ in str(att_err):
+                    pass
+                else:
+                    raise att_err
+    raise e

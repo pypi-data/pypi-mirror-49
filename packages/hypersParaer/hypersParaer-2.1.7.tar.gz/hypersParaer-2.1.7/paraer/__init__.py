8a3e166439bdb5e29f5b55135c124b5f258f6bff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:59 +0000

    Add package hypersParaer/2.1.7/hypersParaer-2.1.7.tar.gz

---
+++ packages/hypersParaer/hypersParaer-2.1.7.tar.gz/hypersParaer-2.1.7/paraer/__init__.py
@@ -0,0 +1,8 @@
+__version__ = '2.1.7'
+
+from .datastrctures import Result, Valid, MethodProxy
+from .para import para_ok_or_400, perm_ok_or_403
+from .doc import patch_all
+
+__all__ = ("Result", "MethodProxy", "Valid", "para_ok_or_400", "perm_ok_or_403")
+patch_all()

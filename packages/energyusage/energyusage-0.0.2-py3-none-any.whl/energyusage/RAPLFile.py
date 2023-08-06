fc3db16887996482e5cd4adeead2144472276fa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:41 +0000

    Add package energyusage/0.0.2/energyusage-0.0.2-py3-none-any.whl

---
+++ packages/energyusage/energyusage-0.0.2-py3-none-any.whl/energyusage/RAPLFile.py
@@ -0,0 +1,20 @@
+class RAPLFile:
+    def __init__(self, name, path):
+        self.name = name
+        self.path = path
+        self.baseline = []
+        self.process = []
+        self.recent = 0
+        self.num_process_checks =0
+        self.process_average = 0
+        self.baseline_average = 0
+
+    def set_recent(val):
+        self.recent = val
+
+    def average(baseline_checks):
+        self.process_average = sum(self.process)/self.num_process_checks
+        self.baseline_average = sum(self.baseline)/baseline_checks
+
+    def __repr__():
+        print(name,path,recent)

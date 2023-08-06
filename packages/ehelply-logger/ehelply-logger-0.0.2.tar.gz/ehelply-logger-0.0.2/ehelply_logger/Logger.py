f86cb69818dae67d837752937fdf86943c1e3914
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:58 +0000

    Add package ehelply-logger/0.0.2/ehelply-logger-0.0.2.tar.gz

---
+++ packages/ehelply-logger/ehelply-logger-0.0.2.tar.gz/ehelply-logger-0.0.2/ehelply_logger/Logger.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+import random
+import string
+
+
+class Logger:
+    def __init__(self, prefix: str = None, debug: bool = False):
+        self.prefix: str = prefix
+        self._chosen_prefix: bool = True
+        if not self.prefix:
+            self.prefix = ''.join(random.choices(string.ascii_uppercase + string.digits, k=3))
+            self._chosen_prefix: bool = False
+
+        self.debug: bool = debug
+
+    def _output(self, message: str):
+        print(self.prefix + " " + message, flush=True)
+
+    def info(self, message: str = ""):
+        self._output("[INFO] " + message)
+
+    def warning(self, message: str = ""):
+        self._output("[WARNING] " + message)
+
+    def severe(self, message: str = ""):
+        self._output("[SEVERE] " + message)
+
+    def newline(self):
+        self._output("")
+
+    def debug(self, message: str = "", force: bool = False):
+        if self.debug or force:
+            self._output("[DEBUG] " + message)
+
+    def spinoff(self) -> Logger:
+        prefix = None
+        if self._chosen_prefix:
+            prefix = self.prefix + "!"
+        return Logger(prefix=prefix, debug=self.debug)

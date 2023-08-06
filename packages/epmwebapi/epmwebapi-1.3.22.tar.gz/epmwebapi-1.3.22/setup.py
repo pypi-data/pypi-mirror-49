7b8f58bce1761d41085805b7ef92015c17b6bdd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:20 +0000

    Add package epmwebapi/1.3.22/epmwebapi-1.3.22.tar.gz

---
+++ packages/epmwebapi/epmwebapi-1.3.22.tar.gz/epmwebapi-1.3.22/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
+setup(name = 'epmwebapi', 
+      version = '1.3.22', 
+      author = 'Elipse Software Ltda', 
+      packages = ['epmwebapi'], 
+      include_package_data=True,
+      description = '', 
+      long_description = long_description, 
+      long_description_content_type='text/markdown',
+      url = 'https://github.com/elipsesoftware/epmwebapi',
+      python_requires='>=3.2, <4',
+      install_requires=[
+          'numpy',
+          'requests',
+          'python-dateutil',
+      ],
+      data_files = [("", ["LICENSE.txt"]),("", ["Readme.md"])],
+      classifiers=[
+      'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+      'Intended Audience :: Developers',      # Define that your audience are developers
+      'Topic :: Software Development :: Build Tools',
+      'License :: OSI Approved :: MIT License',   # Again, pick a license
+      'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+      'Programming Language :: Python :: 3.4',
+      'Programming Language :: Python :: 3.5',
+      'Programming Language :: Python :: 3.6',
+      'Programming Language :: Python :: 3.7',],)
+

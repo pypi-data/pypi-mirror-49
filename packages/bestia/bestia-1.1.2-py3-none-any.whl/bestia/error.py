7ac08ae57172ea43000bce554951daf7a88925a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:52 +0000

    Add package bestia/1.1.2/bestia-1.1.2-py3-none-any.whl

---
+++ packages/bestia/bestia-1.1.2-py3-none-any.whl/bestia/error.py
@@ -0,0 +1,14 @@
+class SttyBinMissing(Exception):
+    pass
+
+class SayBinMissing(Exception):
+    pass
+
+class FileBinMissing(Exception):
+    pass
+
+class CurlBinMissing(Exception):
+    pass
+
+class CurlFailed(Exception):
+    pass

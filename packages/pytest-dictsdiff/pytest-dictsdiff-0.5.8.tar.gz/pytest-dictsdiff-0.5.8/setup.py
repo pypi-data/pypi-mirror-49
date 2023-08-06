d998f897dedd2201ff254d0e3b886204d08a186c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:00 +0000

    Add package pytest-dictsdiff/0.5.8/pytest-dictsdiff-0.5.8.tar.gz

---
+++ packages/pytest-dictsdiff/pytest-dictsdiff-0.5.8.tar.gz/pytest-dictsdiff-0.5.8/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="pytest-dictsdiff",
+    version='0.5.8',
+    py_modules=["pytest_dictsdiff"],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Lukasz Balcerzak',
+    author_email='lukaszbalcerzak@gmail.com',
+    entry_points={"pytest11": ["dictsdiff = pytest_dictsdiff"]},
+    license='MIT',
+    classifiers=["Framework :: Pytest"],
+    install_requires=['dictdiffer'],
+)

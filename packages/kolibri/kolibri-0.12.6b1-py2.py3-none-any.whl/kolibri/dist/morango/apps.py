06dcb2ba0b0cbb201fc993fd88c1e9b499fc6905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:18 +0000

    Add package kolibri/0.12.6b1/kolibri-0.12.6b1-py2.py3-none-any.whl

---
+++ packages/kolibri/kolibri-0.12.6b1-py2.py3-none-any.whl/kolibri/dist/morango/apps.py
@@ -0,0 +1,21 @@
+from __future__ import unicode_literals
+
+import logging as logger
+
+from django.apps import AppConfig
+from django.db import connection
+
+from morango.utils.register_models import add_syncable_models
+
+logging = logger.getLogger(__name__)
+
+
+class MorangoConfig(AppConfig):
+    name = "morango"
+    verbose_name = "Morango"
+
+    def ready(self):
+        from .signals import add_to_deleted_models  # noqa: F401
+
+        # add models to be synced by profile
+        add_syncable_models()
---
+++ packages/kolibri/kolibri-0.12.6b1-py2.py3-none-any.whl/kolibri/dist/morango/errors.py
@@ -0,0 +1,66 @@
+class MorangoError(Exception):
+    pass
+
+
+class InvalidMorangoModelConfiguration(MorangoError):
+    pass
+
+
+class UnsupportedFieldType(MorangoError):
+    pass
+
+
+class InvalidSyncableManager(MorangoError):
+    pass
+
+
+class InvalidSyncableQueryset(MorangoError):
+    pass
+
+
+class InvalidMPTTManager(MorangoError):
+    pass
+
+
+class InvalidMPTTQuerySet(MorangoError):
+    pass
+
+
+class MorangoCertificateError(MorangoError):
+    pass
+
+
+class CertificateScopeNotSubset(MorangoCertificateError):
+    pass
+
+
+class CertificateSignatureInvalid(MorangoCertificateError):
+    pass
+
+
+class CertificateIDInvalid(MorangoCertificateError):
+    pass
+
+
+class CertificateProfileInvalid(MorangoCertificateError):
+    pass
+
+
+class CertificateRootScopeInvalid(MorangoCertificateError):
+    pass
+
+
+class MorangoNonceError(MorangoError):
+    pass
+
+
+class NonceDoesNotExist(MorangoNonceError):
+    pass
+
+
+class NonceExpired(MorangoNonceError):
+    pass
+
+
+class MorangoServerDoesNotAllowNewCertPush(MorangoError):
+    pass

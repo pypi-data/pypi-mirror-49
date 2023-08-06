2a0964f05820b8abf9fb56b5f151042bd83205a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:01 +0000

    Add package sqlalchemy_mptt/0.2.5/sqlalchemy_mptt-0.2.5.tar.gz

---
+++ packages/sqlalchemy_mptt/sqlalchemy_mptt-0.2.5.tar.gz/sqlalchemy_mptt-0.2.5/CHANGES.rst
@@ -0,0 +1,45 @@
+0.2.5 (2019-07-23)
+==================
+
+see issue #64
+
+- Added similar `django_mptt` methods `get_siblings` and `get_children`
+
+
+0.2.4 (2018-12-14)
+==================
+
+see PR #61
+
+- Allow to specify ordering of path_to_root
+
+0.2.3 (2018-06-03)
+==================
+
+see issue #57
+
+- Fix rebuild tree
+- Added support node's identifier start from 0
+
+0.2.2 (2017-10-05)
+==================
+
+see issue #56
+
+- Added custom default root level. Support Django style level=0
+
+0.2.1 (2016-01-23)
+==================
+
+see PR #51
+
+- fix of index columns names
+
+0.2.0 (2015-11-13)
+==================
+
+see PR #50
+
+- Changed ``parent_id`` to dynamically match the type of the primary_key
+- exposed drilldown_tree's logic and path_to_root's logic as both instance and
+  class level method
---
+++ packages/sqlalchemy_mptt/sqlalchemy_mptt-0.2.5.tar.gz/sqlalchemy_mptt-0.2.5/setup.py
@@ -0,0 +1,47 @@
+import os
+from setuptools import setup
+
+this = os.path.dirname(os.path.realpath(__file__))
+
+
+def read(name):
+    with open(os.path.join(this, name)) as f:
+        return f.read()
+
+
+setup(
+    name="sqlalchemy_mptt",
+    version="0.2.5",
+    url="http://github.com/uralbash/sqlalchemy_mptt/",
+    author="Svintsov Dmitry",
+    author_email="sacrud@uralbash.ru",
+    packages=["sqlalchemy_mptt"],
+    include_package_data=True,
+    zip_safe=False,
+    test_suite="nose.collector",
+    license="MIT",
+    description="SQLAlchemy MPTT mixins (Nested Sets)",
+    long_description=read("README.rst") + "\n" + read("CHANGES.rst"),
+    install_requires=read("requirements.txt"),
+    tests_require=read("requirements.txt") + read("requirements-test.txt"),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Framework :: Pyramid",
+        "Framework :: Flask",
+        "Topic :: Internet",
+        "Topic :: Database",
+        "License :: OSI Approved :: MIT License",
+    ],
+)

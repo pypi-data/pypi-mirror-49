0a5444c05be88998c53859f7f3521cc8235ce14d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:31 +0000

    Add package jupyter-vscode-server/0.0.10/jupyter-vscode-server-0.0.10.tar.gz

---
+++ packages/jupyter-vscode-server/jupyter-vscode-server-0.0.10.tar.gz/jupyter-vscode-server-0.0.10/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jupyter-vscode-server",
+    version="0.0.10",
+    author="Timothy Liu",
+    author_email="timothyl@nvidia.com",
+    description="A Jupyter extension to launch VS Code",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tlkh/jupyter-vscode-server",
+    py_modules=['nbvscode'],
+    entry_points={
+        'jupyter_serverproxy_servers': [
+            'nbvscode = nbvscode:setup_nbvscode',
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux ",
+    ],
+    install_requires=['jupyter-server-proxy'],
+)

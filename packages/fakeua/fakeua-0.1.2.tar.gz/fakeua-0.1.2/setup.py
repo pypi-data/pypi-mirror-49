dbe8a9718c7e18780f90ff0f91340e1ae008c863
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:00 +0000

    Add package fakeua/0.1.2/fakeua-0.1.2.tar.gz

---
+++ packages/fakeua/fakeua-0.1.2.tar.gz/fakeua-0.1.2/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+from pathlib import Path
+
+readme = Path("README.md").read_text()
+
+
+setup(
+    name="fakeua",
+    packages=["fakeua"],
+    entry_points={
+        "console_scripts": [
+            "fakeua = fakeua.__main__:main"
+        ]
+    },
+    version="0.1.2",
+    license="GPL3",
+    description="Python3 module made as a wrapper of fake-useragent.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="Carlos A. Planchón",
+    author_email="bubbledoloresuruguay2@gmail.com",
+    url="https://github.com/carlosplanchon/fakeua",
+    download_url="https://github.com/carlosplanchon/"
+        "fakeua/archive/v0.1.2.tar.gz",
+    keywords=["fake", "useragent", "networking", "web"],
+    install_requires=[
+        "fake-useragent"
+    ],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)

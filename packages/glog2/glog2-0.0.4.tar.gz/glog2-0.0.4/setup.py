f98d82f27a94dff187e39c7b4e0d05e39466e515
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:29 +0000

    Add package glog2/0.0.4/glog2-0.0.4.tar.gz

---
+++ packages/glog2/glog2-0.0.4.tar.gz/glog2-0.0.4/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import os
+import setuptools
+
+VERSION = "0.0.4"
+
+README = open(os.path.join(os.path.dirname(__file__), "README.rst")).read()
+
+setuptools.setup(
+    name="glog2",
+    author="Paul M",
+    author_email="ppymou@gmail.com",
+    url="https://github.com/moomou/python-glog",
+    install_requires=[
+        "python-gflags>=3.1",
+        "six",  # glog doesn't need six, but gflags 3.1 does and its distutils
+        # "requires" line apparently accomplishes nothing, so ...
+    ],
+    description="Simple Google-style logging wrapper for Python. Forked from benley/python-glog",
+    long_description=README,
+    py_modules=["glog"],
+    license="BSD",
+    test_suite="tests",
+    version=VERSION,
+    classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Topic :: System :: Logging",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    platforms="any",
+)

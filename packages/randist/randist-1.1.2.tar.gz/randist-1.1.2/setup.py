d0bae1055fc1eae4e4989957630401357dac0d58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:13 +0000

    Add package randist/1.1.2/randist-1.1.2.tar.gz

---
+++ packages/randist/randist-1.1.2.tar.gz/randist-1.1.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="randist",
+    version="1.1.2",
+    author="Ningji Wei",
+    author_email="tidues@gmail.com",
+    description="Distance statistics for two random events on a network",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tidues/Distance-Distribution-of-Random-Events",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+

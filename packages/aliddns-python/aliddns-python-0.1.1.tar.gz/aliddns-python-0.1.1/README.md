c497ada3a7b9d10d45bb2343c6545a75756ff850
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:19 +0000

    Add package aliddns-python/0.1.1/aliddns-python-0.1.1.tar.gz

---
+++ packages/aliddns-python/aliddns-python-0.1.1.tar.gz/aliddns-python-0.1.1/README.md
@@ -0,0 +1,83 @@
+# aliddns-python
+
+A simple dynamic DNS(DDNS) tool for alicloud.
+
+[![image](https://img.shields.io/pypi/status/aliddns-python)](https://pypi.org/project/aliddns-python/)
+[![image](https://img.shields.io/pypi/v/aliddns-python.svg)](https://pypi.org/project/aliddns-python/)
+[![image](https://img.shields.io/pypi/l/aliddns-python.svg)](https://pypi.org/project/aliddns-python/)
+[![image](https://img.shields.io/pypi/pyversions/aliddns-python.svg)](https://pypi.org/project/aliddns-python/)
+[![image](https://img.shields.io/github/contributors/cheng10/aliddns-python.svg)](https://github.com/cheng10/aliddns-python/graphs/contributors)
+
+## Use Cases
+
+You have domain names hosted in Alibaba Cloud, and you want to setup DDNS.
+DDNS is pretty usefull when you have public but dynamic IP from your ISP.
+Some advanced home routers support DDNS of many kinds out of box,
+but many cheap or old ones do not. This easy to use script allows you to setup
+up Alicloud DDNS sync on your home PC or any python-enabled devices.
+
+## Installation
+
+```shell
+pip install --upgrade aliddns-python
+```
+
+## Quickstart
+
+```shell
+# update the given domain name DNS record with current public ip
+python -m aliddns.ddns --key=<ACCESS_KEY> --secret=<SECERT_KEY> --record=www.yourdomain.com
+```
+
+## Usage
+
+### Job Scheduling
+
+```shell
+# update dns record every 5 mins using cron job
+# use `crontab -e` to edit the crob job
+# m h  dom mon dow   command
+*/5 * * * python -m aliddns.ddns --key=<ACCESS_KEY> --secret=<SECERT_KEY> --record=www.yourdomain.com
+```
+
+## Documentation
+
+### Get Your Access Keys from Alibaba Cloud
+
+```text
+Getting a key pair is easy, and lets you to use more API features apart from the DNS one.
+
+In order to get one, log into your Alibaba Cloud console and in the top navigation bar, hover with your mouse in your email address and click "accesskeys" as illustrated below.
+
+<https://www.alibabacloud.com/blog/Dynamic-DNS-using-Alibaba-Cloud-DNS-API_459542>
+```
+
+## References
+
+- [Offical Alicloud DNS API](https://help.aliyun.com/document_detail/124923.html)
+- [Dynamic DNS using Alibaba Cloud DNS API](https://www.alibabacloud.com/blog/Dynamic-DNS-using-Alibaba-Cloud-DNS-API_459542)
+- [Alicloud OpenAPI Explorer](https://api.aliyun.com/)
+
+## LICENSE
+
+```text
+Copyright (c) 2019 cheng10
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+```
---
+++ packages/aliddns-python/aliddns-python-0.1.1.tar.gz/aliddns-python-0.1.1/aliddns/ddns.py
@@ -0,0 +1,132 @@
+"""
+Update alicloud dns record.
+
+Usage: ddns.py (--record=<record>) [--ip=<ip>] [--key=<key>] [--secret=<secret>]
+
+Options:
+    -h --help                             show this help message and exit.
+    --version                             show version and exit.
+    --record=<record>                     subdomain name to update, eg: `www.cheng10.cc`
+    --ip=<ip>                             ip, default to current public ip.
+    --key=<key>                           alicloud access key.
+    --secret=<secret>                     alickoud access secret.
+
+Install:
+pip install aliyun-python-sdk-core
+pip install aliyun-python-sdk-alidns
+pip install docopt
+
+Tested on:
+aliyun-python-sdk-alidns==2.0.10
+aliyun-python-sdk-core==2.13.5
+"""
+import json
+import logging
+from logging.config import dictConfig
+
+import requests
+from docopt import docopt
+
+from aliyunsdkcore.client import AcsClient
+from aliyunsdkcore.acs_exception.exceptions import ClientException
+from aliyunsdkcore.acs_exception.exceptions import ServerException
+from aliyunsdkalidns.request.v20150109.DescribeSubDomainRecordsRequest import DescribeSubDomainRecordsRequest
+from aliyunsdkalidns.request.v20150109.UpdateDomainRecordRequest import UpdateDomainRecordRequest
+
+
+QUERY_IP_URL = 'http://cheng10.cc/ip'
+logger = logging.getLogger(__name__)
+dictConfig({
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s[%(levelname)s]%(name)s[%(lineno)s]: %(message)s'  # NOQA E502
+        },
+    },
+    'handlers': {
+        'default': {
+            'level': 'INFO',
+            'formatter': 'standard',
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://sys.stdout',  # Default is stderr
+        },
+    },
+    'loggers': {
+        '': {  # root logger
+            'handlers': ['default'],
+            'level': 'INFO',
+            'propagate': True
+        },
+    }
+})
+
+
+def get_sub_domain_records(client, subdomain_name):
+    """subdomain_name: eg. `www.cheng10.cc`, `home.cheng10.cc`
+    """
+    request = DescribeSubDomainRecordsRequest()
+    request.set_accept_format('json')
+
+    request.set_SubDomain(subdomain_name)
+
+    response = client.do_action_with_exception(request)
+    res = json.loads(str(response, encoding='utf-8'))
+    record_info = res['DomainRecords']['Record'][0]
+    logger.info(f'Domain record found: {record_info}')
+    return record_info
+    # sample response data
+    """
+    {
+        "PageNumber": 1,
+        "TotalCount": 1,
+        "PageSize": 20,
+        "RequestId": "19A7E4EB-A57E-4F02-8E7E-5BB18A0FBD39",
+        "DomainRecords": {
+            "Record": [
+                {
+                    "RR": "home",
+                    "Status": "ENABLE",
+                    "Value": "25x423z923.zicp.vip",
+                    "Weight": 1,
+                    "RecordId": "17847367556740096",
+                    "Type": "CNAME",
+                    "DomainName": "cheng10.cc",
+                    "Locked": false,
+                    "Line": "default",
+                    "TTL": 600
+                }
+            ]
+        }
+    }
+    """
+
+def update_domain_record(client, record_info, ip):
+    request = UpdateDomainRecordRequest()
+    request.set_accept_format('json')
+
+    request.set_RecordId(record_info['RecordId'])
+    request.set_RR(record_info['RR'])
+    request.set_Type("A")
+    request.set_Value(ip)
+
+    response = client.do_action_with_exception(request)
+    print(str(response, encoding='utf-8'))
+
+
+def get_public_ip():
+    res = requests.get(QUERY_IP_URL)
+    ip = str(res.content, encoding='utf-8').strip('\n')
+    logger.info(f'public ip checked: {ip}')
+    return ip
+
+
+if __name__ == '__main__':
+    args = docopt(__doc__, version='1.0.0')
+    logger.debug(args)
+    client = AcsClient(args['--key'], args['--secret'], 'cn-hangzhou')
+    record_info = get_sub_domain_records(client, args['--record'])
+    update_domain_record(
+        client, record_info, args.get('--ip') or get_public_ip())
+    logger.info('Updated domain record:')
+    record_info = get_sub_domain_records(client, args['--record'])

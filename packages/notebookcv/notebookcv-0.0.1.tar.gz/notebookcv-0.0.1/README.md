cfd24dd60c992a0efa8b9942619517aea90f345b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:03:31 +0000

    Add package notebookcv/0.0.1/notebookcv-0.0.1.tar.gz

---
+++ packages/notebookcv/notebookcv-0.0.1.tar.gz/notebookcv-0.0.1/README.md
@@ -0,0 +1,2 @@
+# notebookcv
+Learn how to create an open source Python package with a notebook converter
---
+++ packages/notebookcv/notebookcv-0.0.1.tar.gz/notebookcv-0.0.1/notebookcv/notebookcv.py
@@ -0,0 +1,8 @@
+def convert(my_name):
+    """
+    Print a line about converting a notebook.
+    :param my_name: str: person's name
+    :return: None
+    """
+
+    print(f"I'll convert a notebook for you some day, {my_name}.")

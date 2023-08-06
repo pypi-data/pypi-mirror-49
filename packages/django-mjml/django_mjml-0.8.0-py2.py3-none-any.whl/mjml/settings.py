3ed9ac9502326feb3cb43227c624abee0805a189
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:51 +0000

    Add package django-mjml/0.8.0/django_mjml-0.8.0-py2.py3-none-any.whl

---
+++ packages/django-mjml/django_mjml-0.8.0-py2.py3-none-any.whl/mjml/settings.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+
+MJML_BACKEND_MODE = getattr(settings, 'MJML_BACKEND_MODE', 'cmd')
+assert MJML_BACKEND_MODE in ('cmd', 'tcpserver')
+
+# cmd backend mode configs
+MJML_EXEC_CMD = getattr(settings, 'MJML_EXEC_CMD', 'mjml')
+MJML_CHECK_CMD_ON_STARTUP = getattr(settings, 'MJML_CHECK_CMD_ON_STARTUP', True)
+
+# tcpserver backend mode configs
+MJML_TCPSERVERS = getattr(settings, 'MJML_TCPSERVERS', [('127.0.0.1', 28101)])
+assert isinstance(MJML_TCPSERVERS, (list, tuple))
+for t in MJML_TCPSERVERS:
+    assert isinstance(t, (list, tuple)) and len(t) == 2 and isinstance(t[0], str) and isinstance(t[1], int)

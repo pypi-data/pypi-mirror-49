903a28f288c7c479c2a269d68ecb7ff1c2ece07b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:04 +0000

    Add package pynoorm/1.0.0/pynoorm-1.0.0.tar.gz

---
+++ packages/pynoorm/pynoorm-1.0.0.tar.gz/pynoorm-1.0.0/HISTORY.rst
@@ -0,0 +1,61 @@
+=======
+History
+=======
+
+0.1.0 (2016-02-17)
+------------------
+
+* First release on github.
+
+0.1.1 (2016-02-22)
+------------------
+
+* Registered on PyPI
+
+0.2.0 (2016-04-12)
+------------------
+
+* Added support for Python 3.3+ and MySQL
+
+0.3.0 (2017-09-06)
+------------------
+
+* Added SQL Server support
+* Added Linker class to support object cross-referencing
+
+0.4.0 (2018-07-24)
+------------------
+
+* Updating to Beta status
+* Optimized Linker class
+* Python list => SQL IN (xxx, yyy) functionality on Binder.
+
+0.4.1 (2018-08-07)
+------------------
+
+* adjusted for Python 3 
+
+0.4.2 (2019-01-10)
+------------------
+
+* ran Black for code formatting
+* updated PyYaml to 4.2b4 to fix security vulnerability
+
+
+0.4.3 (2019-01-10)
+------------------
+
+* removed Python 3.7 from tox since that Python version is not supported yet by tox.
+
+
+0.4.4 (2019-01-15)
+------------------
+
+* adjusted list binding variable names from `__xxx_000` to `xxx_000__` because leading underscore are invalid under Oracle.
+  
+0.1.0 (2019-07-15)
+------------------
+
+* added the D3.js Tree Linker example.
+* Code is stable and hasn't had fixes or API changes.
+
---
+++ packages/pynoorm/pynoorm-1.0.0.tar.gz/pynoorm-1.0.0/setup.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import sys
+import os
+import re
+
+
+if sys.version_info < (2, 7):
+    raise Exception("PyNoORM requires Python 2.7 or higher.")
+
+if sys.version_info >= (3,):
+    if sys.version_info < (3, 3):
+        raise Exception("PyNoORM requires Python 3.3 or higher")
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+# with open('README.rst') as readme_file:
+#     readme = readme_file.read()
+
+with open("HISTORY.rst") as history_file:
+    history = history_file.read()
+
+requirements = [
+    # TODO: put package requirements here
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+
+def parse_readme(text):
+    """start on reStructuredText banner and end at software declaration"""
+
+    start = re.compile("~~~~~~~", re.IGNORECASE)
+    end = re.compile("Free software:", re.IGNORECASE)
+    from_ = to_ = description = None
+
+    lines = text.split("\n")
+
+    for lineno, line in enumerate(lines):
+
+        if from_ is None and start.search(line):
+            from_ = lineno - 1
+            description = lines[from_].strip()
+
+        if to_ is None and end.search(line):
+            to_ = lineno
+
+    return description, "\n".join(lines[from_:to_])
+
+
+with open(os.path.join(os.path.dirname(__file__), "README.rst")) as r_file:
+    description, readme = parse_readme(r_file.read())
+
+    assert description.strip()
+    assert readme.strip()
+
+
+setup(
+    name="pynoorm",
+    version="1.0.0",
+    description=description,
+    long_description=readme + "\n\n" + history,
+    author="JL Peyret",
+    author_email="jpeyret@gmail.com",
+    url="https://github.com/jpeyret/pynoorm",
+    packages=["pynoorm"],
+    package_dir={"pynoorm": "pynoorm"},
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT License",
+    zip_safe=False,
+    keywords="sql database multiplatform",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Database :: Front-Ends",
+        "Topic :: Utilities",
+        "Operating System :: OS Independent",
+    ],
+    test_suite="tests",
+    tests_require=test_requirements,
+)

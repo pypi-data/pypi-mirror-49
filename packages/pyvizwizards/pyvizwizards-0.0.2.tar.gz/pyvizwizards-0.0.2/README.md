2fab7885c203144c6b15912712d7c6e3b577a0b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:19 +0000

    Add package pyvizwizards/0.0.2/pyvizwizards-0.0.2.tar.gz

---
+++ packages/pyvizwizards/pyvizwizards-0.0.2.tar.gz/pyvizwizards-0.0.2/README.md
@@ -0,0 +1,5 @@
+# Example Package
+
+This is a simple visualization package.
+
+v 0.0.2 Release update contains 3 new plotting functions and a fix to the name of earlier function
---
+++ packages/pyvizwizards/pyvizwizards-0.0.2.tar.gz/pyvizwizards-0.0.2/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyvizwizards",
+    version="0.0.2",
+    author="Sathwik Vangari",
+    author_email="sathwik.mohan@gmail.com",
+    description="A small visualization package",
+    long_description="long_description",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

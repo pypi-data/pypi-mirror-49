6296c2acb6aeb4370421c24a43c2396eff990780
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:14 +0000

    Add package UsePhysics/1.0.1/UsePhysics-1.0.1-py2-none-any.whl

---
+++ packages/UsePhysics/UsePhysics-1.0.1-py2-none-any.whl/physics/pyphysics.py
@@ -0,0 +1,14 @@
+import math
+g=9.8
+mu=4*math.pi*math.pow(10,-7)
+plank=6.62606876*math.pow(10,-34)
+impedance=376.730313461
+charge=1.602176462*math.pow(10,-19)
+flux=2.067833636*math.pow(10,-15)
+me=9.10938188*math.pow(10,-31)
+mp=1.67262158*math.pow(10,-27)
+boltzman=1.3806503*math.pow(10,-23)
+rydberg=10973731.568549
+c=299792459
+gravitation=6.673*math.pow(10,-11)
+bohr_radius=0.5291772083*math.pow(10,-10)

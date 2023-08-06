7653f7dc8fd54b663f7812ae4e166ce2b2ccdba8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:38 +0000

    Add package tutor-openedxsettings/0.0.2/tutor-openedxsettings-0.0.2.tar.gz

---
+++ packages/tutor-openedxsettings/tutor-openedxsettings-0.0.2.tar.gz/tutor-openedxsettings-0.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+setup(
+    name="tutor-openedxsettings",
+    version="0.0.2",
+    license="AGPLv3",
+    author="eliteu",
+    author_email="code@eliteu.cn",
+    description="A Tutor plugin for openedx settings",
+    packages=["tutoropenedxsettings"],
+    include_package_data=True,
+    python_requires=">=3.5",
+    entry_points={"tutor.plugin.v0": ["openedx_settings = tutoropenedxsettings.plugin"]},
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)

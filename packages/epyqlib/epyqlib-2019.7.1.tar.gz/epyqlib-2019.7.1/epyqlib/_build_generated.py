16a54238f8ecc7377fef745e04182c9908ad4e47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:26 +0000

    Add package epyqlib/2019.7.1/epyqlib-2019.7.1.tar.gz

---
+++ packages/epyqlib/epyqlib-2019.7.1.tar.gz/epyqlib-2019.7.1/epyqlib/_build_generated.py
@@ -0,0 +1,9 @@
+# This file has been generated
+
+
+build_system = 'AppVeyor'
+build_id = '26241728'
+build_number = '339'
+build_version = '1.0.339'
+job_id = '8ivvlj5eqd3hwtta'
+job_url = 'https://ci.appveyor.com/project/KyleAltendorf/stlib/build/job/8ivvlj5eqd3hwtta'
---
+++ packages/epyqlib/epyqlib-2019.7.1.tar.gz/epyqlib-2019.7.1/setup.py
@@ -0,0 +1,71 @@
+import setuptools
+
+import versioneer
+
+
+setuptools.setup(
+    name="epyqlib",
+    author="EPC Power Corp.",
+    classifiers=[
+        ("License :: OSI Approved :: "
+         "GNU General Public License v2 or later (GPLv2+)")
+    ],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    entry_points={
+        'console_scripts': [
+            'outparsecheck = epyqlib.outparsecheck:main [dss]',
+            'collectdevices = epyqlib.collectdevices:main',
+            'contiguouscommits = epyqlib.utils.contiguouscommits:_entry_point [dulwich]',
+            'epyqflash = epyqlib.flash:_entry_point',
+            'patchvenv = epyqlib.patchvenv:main',
+            'cangenmanual = epyqlib.cangenmanual:_entry_point',
+            'updateepc = epyqlib.updateepc:main',
+            'genbuildinfo = epyqlib.genbuildinfo:write_build_file',
+            'versionfile = epyqlib.cli.versionfile:cli',
+            'generateversion = epyqlib.cli.generateversion:cli',
+            'autodevice = epyqlib.autodevice.cli:cli',
+        ],
+        'pytest11': [
+            'epyqlib = epyqlib.tests.pytest_plugin',
+        ]
+    },
+    install_requires=[
+        'arrow',
+        'bitstruct',
+        'canmatrix',
+        'click>=7',
+        'python-dotenv',
+        'natsort',
+        'pint',
+        'pyelftools',
+        'qt5reactor',
+        'gitpython',
+        'graham',
+        'PyQt5',
+        'python-docx',
+        'python-can',
+        'Twisted; platform_system != "Darwin" and platform_system != "Windows"',
+        'Twisted[osx_platform]; platform_system == "Darwin"',
+        'Twisted[windows_platform]; platform_system == "Windows"',
+    ],
+    extras_require={
+        'deploy': [
+            'gitpython',
+            'requests',
+        ],
+        'dss': [
+            'ccstudiodss>=0.2.7',
+        ],
+        'dulwich': [
+            'dulwich',
+        ],
+        'test': [
+            'pytest',
+            'pytest-qt',
+            'pytest-rerunfailures',
+        ],
+    },
+)

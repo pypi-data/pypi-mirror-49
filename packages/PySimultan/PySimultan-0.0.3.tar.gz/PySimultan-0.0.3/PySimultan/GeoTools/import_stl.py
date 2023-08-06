6f952f9f951e99e4b64be663a7df74eb4293bf4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:04 +0000

    Add package PySimultan/0.0.3/PySimultan-0.0.3.tar.gz

---
+++ packages/PySimultan/PySimultan-0.0.3.tar.gz/PySimultan-0.0.3/PySimultan/GeoTools/import_stl.py
@@ -0,0 +1,31 @@
+
+import os
+import trimesh
+
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+
+from vertice import *
+from edge import *
+from edge_loop import *
+from face import *
+from building import *
+from material import *
+
+# mesh = trimesh.load('../models/featuretype.STL')
+
+
+def import_stl(file=None):
+    mesh = trimesh.load(file)
+    pass
+
+
+if __name__ == '__main__':
+
+    # file picker for debugging surposes
+    Tk().withdraw()
+    file = askopenfilename()
+
+    MyBuilding = import_stl(file)
+
+    print('done')

4323e22bf8ffc93d435e98bb0ceded6ed18a1816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:07 +0000

    Add package pydeco/0.3/pydeco-0.3-py3-none-any.whl

---
+++ packages/pydeco/pydeco-0.3-py3-none-any.whl/pydeco/utils.py
@@ -0,0 +1,28 @@
+"""Utils functions."""
+import os
+import sys
+from ast import literal_eval
+from inspect import isclass
+
+
+def python_version():
+    """Return Python version."""
+    if 'TRAVIS' in os.environ and 'TRAVIS_PYTHON_VERSION' in os.environ:
+        # Travis CI
+        py_version = literal_eval(os.environ['TRAVIS_PYTHON_VERSION'])
+    else:
+        # Local
+        version_info = sys.version_info
+        py_version = float(version_info.major + version_info.minor / 10)
+
+    return py_version
+
+
+def is_wrapped(obj):
+    """Return True if input object is wrapped."""
+    cls = obj if isclass(obj) else obj.__class__
+
+    return getattr(cls, '_MC__decorated', False)
+
+
+PYTHON_VERSION = python_version()

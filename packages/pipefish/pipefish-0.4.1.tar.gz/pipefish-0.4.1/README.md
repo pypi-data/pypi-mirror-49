d2ed3584a53399037d1f050e31bc74045d8bc9e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:03 +0000

    Add package pipefish/0.4.1/pipefish-0.4.1.tar.gz

---
+++ packages/pipefish/pipefish-0.4.1.tar.gz/pipefish-0.4.1/README.md
@@ -0,0 +1,7 @@
+# pipefish
+Process JUnit XML and Cobertura coverage XML reports into Markdown
+
+# Additional Documentation:
+* [News](NEWS.rst).
+* [Code of Conduct](CODE_OF_CONDUCT.md).
+* [Contribution Guidelines](CONTRIBUTING.md).
---
+++ packages/pipefish/pipefish-0.4.1.tar.gz/pipefish-0.4.1/setup.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+`setuptools` Distribution for pipefish
+"""
+
+# {{{ Import
+# System  Imports
+import codecs
+import os
+import re
+
+# External Imports
+from setuptools import setup
+
+# }}}
+
+
+PACKAGE_NAME = 'pipefish'
+
+
+def read(fname):
+    """
+    Read the contents of relative file.
+    """
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+def read_version():
+    """
+    Read the contents of relative file.
+    """
+    file_path = os.path.join(
+        os.path.dirname(__file__), PACKAGE_NAME, 'version.py'
+    )
+    regex = re.compile('__version__ = [\'\"]([^\'\"]*)[\'\"]')
+    with codecs.open(file_path, encoding='utf-8') as fobj:
+        for line in fobj:
+            mobj = regex.match(line)
+            if mobj:
+                return mobj.group(1)
+    raise Exception('Failed to read version')
+
+
+setup(
+    name=PACKAGE_NAME,
+    version=read_version(),
+    author='Tim Gates',
+    author_email='tim.gates@iress.com',
+    maintainer='Tim Gates',
+    maintainer_email='tim.gates@iress.com',
+    packages=[PACKAGE_NAME],
+    license='GPLv3+',
+    description=(
+        'Process JUnit XML and Cobertura coverage XML reports into Markdown'
+    ),
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    install_requires=['docopt', 'defusedxml'],
+    url='https://github.com/pytest-buildkite/pipefish',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Pytest',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    ],
+)

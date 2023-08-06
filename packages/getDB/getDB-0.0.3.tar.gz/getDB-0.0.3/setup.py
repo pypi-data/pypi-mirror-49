9e00f81a9beed7522ab89f807641b8e7189776c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:07 +0000

    Add package getDB/0.0.3/getDB-0.0.3.tar.gz

---
+++ packages/getDB/getDB-0.0.3.tar.gz/getDB-0.0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(name='getDB',
+      version='0.0.3',
+      description="This module can be used to download HMDB and KEGG database.",
+      license='MIT',
+      author='Xiaotao Shen',
+      author_email='shenxt1990@163.com',
+      url='https://github.com/jaspershen/getDB',
+      long_description_content_type="text/markdown",
+      packages=find_packages(),
+      install_requires=['requests', 'pandas', 'bs4', 'numpy'],
+      classifiers=[
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7'
+      ]
+      )

3afcea2a17331866df1b317c657b21c6064f7241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:48 +0000

    Add package xtgen/0.0.9/xtgen-0.0.9-py3-none-any.whl

---
+++ packages/xtgen/xtgen-0.0.9-py3-none-any.whl/xtgen/assets/pytorch_assets/snli/README.md
@@ -0,0 +1,7 @@
+# Basic SLNI Example
+
+```bash
+pip install -r requirements.txt
+python train.py
+# CUDA_VISIBLE_DEVICES=2 python main.py  # to specify GPU id to ex. 2
+```
---
+++ packages/xtgen/xtgen-0.0.9-py3-none-any.whl/xtgen/pytorch_gen.py
@@ -0,0 +1,30 @@
+# pytorch_gen.py: code for generating pytorch apps
+import os
+import shutil
+from . import utils
+
+class PytorchGenerator():
+    def __init__(self):
+        pass
+
+    def generate(self, output_dir, dataset, model):
+        mydir = os.path.dirname(__file__) 
+        if dataset == "default" and model == "default":
+            utils.user_error("you must specify the dataset or the model (or both)")
+            
+        if dataset == "mnist":
+            from_dir = mydir + "/assets/pytorch_assets/mnist"
+        elif dataset == "snli":
+            from_dir = mydir + "/assets/pytorch_assets/snli"
+        else:
+            utils.user_error("dataset not yet available for pytorch: {}".format(dataset))
+
+        shutil.copytree(from_dir, output_dir)
+
+        # zap the __pycache__ directory created by pip install for our assets
+        shutil.rmtree(output_dir + "/__pycache__")
+
+        # count files generated (recursively)
+        count = sum([len(files) for r, d, files in os.walk(output_dir]))
+        return count
+        

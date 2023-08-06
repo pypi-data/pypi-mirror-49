32a6f3eecde822ef4d7cf6331328dba2ccc56fb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:07 +0000

    Add package Phygital/0.0.8/phygital-0.0.8.tar.gz

---
+++ packages/Phygital/phygital-0.0.8.tar.gz/phygital-0.0.8/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 29 11:07:52 2019
+
+@author: Innotechway
+"""
+import pathlib
+import setuptools
+
+HERE = pathlib.Path(__file__).parent
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="phygital",
+    version="0.0.8",
+    author="Renuka Angole",
+    author_email="renuka.angole@gmail.com",
+    description="A powerful package to work on IoT projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/RenukaAngole1/phygital/tree/master/PhygitalLib/phygital",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    
+    install_requires=["pyserial","requests"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+   
+   
+)

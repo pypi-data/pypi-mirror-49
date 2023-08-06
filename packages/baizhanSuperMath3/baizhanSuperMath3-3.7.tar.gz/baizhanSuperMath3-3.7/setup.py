e6e9b17e3e1764bde993e7854195f3b9a97980cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:23 +0000

    Add package baizhanSuperMath3/3.7/baizhanSuperMath3-3.7.tar.gz

---
+++ packages/baizhanSuperMath3/baizhanSuperMath3-3.7.tar.gz/baizhanSuperMath3-3.7/setup.py
@@ -0,0 +1,11 @@
+﻿#coding=utf-8
+from distutils.core import setup
+
+setup(
+    name='baizhanSuperMath3',  #对外我们模块的名称
+    version='3.7',  #版本号
+    description='这是第一个对外发布的模块，里面只有数学方法，用于测试',  #描述
+    author='zhanghaixuan',  #作者
+    author_email='316256501@qq.com',
+    py_modules=['baizhanSuperMath3.demo1','baizhanSuperMath3.demo2']   #要发布的模块
+)

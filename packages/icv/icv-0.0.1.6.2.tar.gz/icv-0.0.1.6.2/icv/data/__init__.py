b08674361e17b24d1358d0b767fe687e58f1631f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:57 +0000

    Add package icv/0.0.1.6.2/icv-0.0.1.6.2.tar.gz

---
+++ packages/icv/icv-0.0.1.6.2.tar.gz/icv-0.0.1.6.2/icv/data/__init__.py
@@ -0,0 +1,7 @@
+from .coco import Coco
+from .voc import Voc
+from .dataset import IcvDataSet
+from .labelme import LabelMe
+from .classify import Classify
+
+__all__ = ['Coco', 'Voc', 'IcvDataSet', 'LabelMe', 'Classify']

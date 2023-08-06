45b295e1455d99b92d5b46d6b44698609ea54639
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:13 +0000

    Add package ClassHTTPServer/0.0.1/ClassHTTPServer-0.0.1.tar.gz

---
+++ packages/ClassHTTPServer/ClassHTTPServer-0.0.1.tar.gz/ClassHTTPServer-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+setup(
+    name="ClassHTTPServer",
+    version="0.0.1",
+    author="fjy",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries'
+    ],
+    packages=["ClassHTTPServer"]
+)

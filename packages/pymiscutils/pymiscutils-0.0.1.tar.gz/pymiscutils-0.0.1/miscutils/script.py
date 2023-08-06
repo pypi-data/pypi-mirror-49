cdcfc2c8388489c1517e23cf03c5330f9178d86b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:20 +0000

    Add package pymiscutils/0.0.1/pymiscutils-0.0.1.tar.gz

---
+++ packages/pymiscutils/pymiscutils-0.0.1.tar.gz/pymiscutils-0.0.1/miscutils/script.py
@@ -0,0 +1,110 @@
+from __future__ import annotations
+
+import contextlib
+import functools
+import traceback
+from types import FunctionType
+from typing import Dict, Any, Type, cast, TypeVar, Callable
+import inspect
+
+from maybe import Maybe
+from subtypes import DateTime
+from pathmagic import Dir, File
+
+from .misc import Counter
+from .context import Timer
+from .log import PrintLog
+
+FuncSig = TypeVar("FuncSig", bound=Callable)
+
+
+class ScriptProfiler:
+    def __init__(self, log: PrintLog = None) -> None:
+        self.log, self.stack = log, Counter()
+
+    def __call__(self, func: FuncSig = None) -> FuncSig:
+        @functools.wraps(func)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            timer, context = Timer(), Maybe(self.log).else_(contextlib.nullcontext())
+            positional, keyword = ', '.join([repr(arg) for arg in args[1:]]), ', '.join([f'{name}={repr(val)}' for name, val in kwargs.items()])
+            arguments = f"{positional}{f', ' if positional and keyword else ''}{keyword}"
+
+            with context(to_console=False):
+                print(f"{self.prefix}{func.__name__}({arguments}) starting...")
+
+            self.stack.increment()
+
+            with context(to_console=True):
+                ret = func(*args, **kwargs)
+
+            self.stack.decrement()
+
+            with context(to_console=False):
+                print(f"{self.prefix}{func.__name__} finished in {timer} seconds, returning: {repr(ret)}. {f'State of the script object is now: {args[0]}' if isinstance(args[0], ScriptBase) else ''}")
+
+            return ret
+        return cast(FuncSig, wrapper)
+
+    @property
+    def prefix(self) -> str:
+        return f"{DateTime.now().logformat()} - {'    '*int(Maybe(self.stack).else_(0))}"
+
+
+class ScriptMeta(type):
+    def __new__(mcs, name: str, bases: Any, namespace: dict) -> Type[ScriptBase]:
+        if name == "ScriptBase":
+            return type.__new__(mcs, name, bases, namespace)
+        else:
+            profiler = ScriptProfiler()
+
+            for attr, val in namespace.items():
+                if isinstance(val, FunctionType):
+                    if attr == "__init__":
+                        namespace[attr] = mcs._constructor_wrapper(profiler(val))
+                    else:
+                        namespace[attr] = profiler(val)
+
+            cls = cast(Type[ScriptBase], type.__new__(mcs, name, bases, namespace))
+            cls._profiler = profiler
+
+            return cls
+
+    @staticmethod
+    def _constructor_wrapper(func: FuncSig) -> FuncSig:
+        @functools.wraps(func)
+        def wrapper(self: ScriptBase, run_mode: str = "smart", **arguments: Any) -> None:
+            self.name = File(inspect.getfile(type(self))).prename
+            self.run_mode, self.arguments = run_mode, arguments
+
+            now = DateTime.now()
+            logs_dir = Dir.from_home().d.documents.newdir("Python").newdir("logs")
+            log_path = logs_dir.newdir(now.isoformat_date()).newdir(self.name).newfile(f"[{now.hour}h {now.minute}m {now.second}s {now.microsecond}ms]", "txt")
+            self.log = PrintLog(log_path)
+
+            self._profiler.log = self.log
+
+            exception = None
+
+            try:
+                func(self)
+            except Exception as ex:
+                exception = ex
+                self.log.to_console = False
+                self.log.write(traceback.format_exc())
+
+            self.log.file.newrename(self.log.file.prename, "pkl").contents = self
+
+            if exception is not None:
+                raise exception
+
+        return cast(FuncSig, wrapper)
+
+
+class ScriptBase(metaclass=ScriptMeta):
+    name: str
+    run_mode: str
+    arguments: Dict[str, Any]
+    log: PrintLog
+
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}({', '.join([f'{attr}={repr(val)}' for attr, val in self.__dict__.items() if not attr.startswith('_')])})"
---
+++ packages/pymiscutils/pymiscutils-0.0.1.tar.gz/pymiscutils-0.0.1/miscutils/serializer.py
@@ -0,0 +1,185 @@
+from __future__ import annotations
+
+import base64
+import copy
+import os
+from typing import Any
+import pickle
+
+from cryptography.fernet import Fernet
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+import dill
+
+from pathmagic import Dir, PathLike
+
+from .singleton import Singleton
+
+
+class Lost(Singleton):
+    def __len__(self) -> int:
+        return 0
+
+    def __iter__(self) -> Lost:
+        return self
+
+    def __next__(self) -> Any:
+        raise StopIteration
+
+    def __getattr__(self, name: str) -> Lost:
+        if name.startswith("__") and name.endswith("__"):
+            raise AttributeError(name)
+        else:
+            return self
+
+
+class LostObject:
+    def __init__(self, obj: Any) -> None:
+        self.repr = repr(obj)
+
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}({self.repr})"
+
+    def __len__(self) -> int:
+        return 0
+
+    def __iter__(self) -> Lost:
+        return self
+
+    def __next__(self) -> Any:
+        raise StopIteration
+
+    def __getattr__(self, name: str) -> Lost:
+        if name.startswith("__") and name.endswith("__"):
+            raise AttributeError(name)
+        else:
+            return Lost()
+
+
+class FutureObject:
+    def __init__(self, id: int) -> None:
+        self.id = id
+
+
+class Serializer:
+    def __init__(self, file: os.PathLike) -> None:
+        self.file = file
+
+    def __repr__(self) -> str:
+        return f"{type(self).__name__}({', '.join([f'{attr}={repr(val)}' for attr, val in self.__dict__.items() if not attr.startswith('_')])})"
+
+    def serialize(self, obj: Any, **kwargs: Any) -> None:
+        self.file.path.write_bytes(self.to_bytes(obj=obj, **kwargs))
+
+    def deserialize(self, **kwargs: Any) -> Any:
+        try:
+            return self.from_bytes(text=self.file.path.read_bytes(), **kwargs)
+        except EOFError:
+            return None
+
+    def to_bytes(self, obj: Any, **kwargs: Any) -> None:
+        try:
+            return dill.dumps(obj, **kwargs)
+        except TypeError:
+            cleaned_object = UnpickleableItemHelper(obj).serializable_copy()
+            return dill.dumps(cleaned_object, **kwargs)
+
+    def from_bytes(self, text: bytes, **kwargs: Any) -> Any:
+        return dill.loads(text, **kwargs)
+
+
+class UnpickleableItemHelper:
+    def __init__(self, item: Any) -> None:
+        self.item, self.seen = item, {}
+
+    def serializable_copy(self) -> Any:
+        return self.recursively_strip_invalid(self.item)
+
+    def recursively_strip_invalid(self, obj) -> Any:
+        if self.is_pickleable(obj):
+            self.seen[id(obj)] = obj
+            return obj
+        else:
+            return self.handle_unpickleable(obj)
+
+    def handle_unpickleable(self, obj):
+        if self.is_endpoint(obj):
+            ret = LostObject(obj)
+            self.seen[id(obj)] = ret
+            return ret
+        else:
+            return self.handle_non_endpoint(obj)
+
+    def handle_non_endpoint(self, obj):
+        try:
+            shallow_copy = copy.copy(obj)
+        except TypeError:
+            ret = LostObject(obj)
+            self.seen[id(obj)] = ret
+            return ret
+        else:
+            self.seen[id(obj)] = shallow_copy
+            return self.handle_shallow_copy(shallow_copy)
+
+    def handle_shallow_copy(self, obj):
+        if not hasattr(obj, "__dict__"):
+            return self.handle_iterable(obj)
+        else:
+            return self.handle_namespace_item(obj)
+
+    def handle_iterable(self, obj):
+        for index, val in enumerate(obj):
+            obj[index] = self.handle_member(val)
+
+        return obj
+
+    def handle_namespace_item(self, obj):
+        prior_items = set([id(item) for item in vars(obj).values()])
+        for key, val in vars(obj).items():
+            setattr(obj, key, self.handle_member(val))
+
+        return LostObject(obj) if prior_items == set(id(item) for item in vars(obj).values()) else obj
+
+    def handle_member(self, obj):
+        try:
+            dill.dumps(obj)
+        except pickle.PicklingError:
+            return LostObject(obj)
+        except TypeError:
+            return self.seen[id(obj)] if id(obj) in self.seen else self.recursively_strip_invalid(obj)
+        else:
+            return obj
+
+    @staticmethod
+    def is_pickleable(item: Any) -> bool:
+        try:
+            dill.dumps(item)
+            return True
+        except Exception:
+            return False
+
+    @staticmethod
+    def is_endpoint(item: Any) -> bool:
+        return (not hasattr(item, "__dict__") or type(item).__setattr__ is not object.__setattr__) and (not hasattr(item, "__iter__") or isinstance(item, (str, bytes)))
+
+
+class Secrets:
+    def __init__(self, file: os.PathLike, key_path: PathLike = None, salt: bytes = b"") -> None:
+        self.pw = Dir.from_home().newfile("secrets", "txt") if key_path is None else key_path
+        self.serializer, self.salt = Serializer(file), salt
+        self.fernet = self._generate_fernet()
+
+    def provide_new_password(self, key: str) -> None:
+        self.pw.contents = key
+
+    def encrypt(self, obj: Any) -> None:
+        self.serializer.file.path.write_bytes(self.fernet.encrypt(self.serializer.to_bytes(obj)))
+
+    def decrypt(self) -> Any:
+        return self.serializer.from_bytes(self.fernet.decrypt(self.serializer.file.path.read_bytes()))
+
+    def _generate_fernet(self) -> Fernet:
+        kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=32, salt=self.salt, iterations=100000, backend=default_backend())
+        key = base64.urlsafe_b64encode(kdf.derive(self.pw.contents.encode()))
+        return Fernet(key)

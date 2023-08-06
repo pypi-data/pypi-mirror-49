25589d30365b1c87a7a9aabaf5428fb8aee9cc87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:54 +0000

    Add package mllaunchpad/0.0.5/mllaunchpad-0.0.5.tar.gz

---
+++ packages/mllaunchpad/mllaunchpad-0.0.5.tar.gz/mllaunchpad-0.0.5/docs/usage.rst
@@ -0,0 +1,12 @@
+=====
+Usage
+=====
+
+To use ML Launchpad in a project::
+
+    import mllaunchpad
+
+Have a look at the examples provided in `mllaunchpad's GitHub repository`_
+(`download <https://minhaskamal.github.io/DownGit/#/home?url=https://github.com/schuderer/mllaunchpad/tree/master/examples>`_).
+
+_`mllaunchpad's GitHub repository`: https://github.com/schuderer/mllaunchpad/
---
+++ packages/mllaunchpad/mllaunchpad-0.0.5.tar.gz/mllaunchpad-0.0.5/examples/testserver/start.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo
+echo "Server will be started."
+echo "Config used: '$LAUNCHPAD_CFG'"
+echo "Test URL with HTTPS and Basic Auth (user: muleuser, pw: thepassword):"
+echo "https://localhost:8080/iris/v0/varieties?sepal.length=4.9&sepal.width=2.4&petal.length=3.3&petal.width=1"
+echo
+
+ORIGPATH=$PWD
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+cd "$SCRIPTPATH/.."
+if gunicorn --workers 2 --bind 127.0.0.1:5000 mllaunchpad.wsgi & #>>"$SCRIPTPATH/mllaunchpad.log" 2>&1 &
+then
+  SERVERPID=$!
+  echo Started mllaunchpad gunicorn server in backgroud, pid $SERVERPID
+else
+  echo Failed starting mllaunchpad gunicorn server. Aborting.
+  exit 1
+fi
+
+# mkdir -p "$SCRIPTPATH/temp"
+
+nginx -c "$SCRIPTPATH/nginx.conf" -p "$SCRIPTPATH"
+
+echo Stopping gunicorn...
+kill $SERVERPID
+
+cd "$ORIGPATH"

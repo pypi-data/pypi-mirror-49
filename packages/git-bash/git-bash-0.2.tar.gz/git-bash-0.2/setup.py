31fbe77a9c8d2157f74d13e3ade5deadab7b5a3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:03:44 +0000

    Add package git-bash/0.2/git-bash-0.2.tar.gz

---
+++ packages/git-bash/git-bash-0.2.tar.gz/git-bash-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='git-bash',
+    version='0.2',
+
+    description='Stub that prevents name clash with https://github.com/conda-forge/git-bash-feedstock For Git Bash see https://github.com/kiwi0fruit/git-bash',
+    url='https://github.com/kiwi0fruit/git-bash',
+    author='kiwi0fruit',
+    author_email='peter.zagubisalo@gmail.com',
+    license='MIT',
+)

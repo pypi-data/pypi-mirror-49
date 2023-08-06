2b0fc2af2926d0941f45e206b966031f03455e61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:32 +0000

    Add package mkdocs-redirects/0.0.6/mkdocs-redirects-0.0.6.tar.gz

---
+++ packages/mkdocs-redirects/mkdocs-redirects-0.0.6.tar.gz/mkdocs-redirects-0.0.6/README.md
@@ -0,0 +1,57 @@
+# mkdocs-redirects
+Open source plugin for Mkdocs page redirects
+
+## Installing
+
+> **Note:** This package requires MkDocs version 1.0.4 or higher. 
+
+Install with pip:
+
+```bash
+pip install mkdocs-redirects
+```
+
+Enable the plugin in your `mkdocs.yml`:
+
+```yaml
+plugins:
+    - search
+    - redirects
+```
+
+## Using
+
+In your `mkdocs.yml`, add a `redirects` block that maps the old page location to the new location:
+
+```
+redirects:
+  'old': 'some/new_location'
+  'something/before': 'another/moved/file'
+  'external': 'http://google.com'
+```
+
+Note that the `.html` extension should be omitted (and will be automatically appended).
+
+The plugin will dynamically create `old.html`, `something/before.html`, and `external.html` in your configured `site_dir` with
+HTML that will include a meta redirect to the new page location.
+
+If the new location does not start with `http` or `HTTP` then it will also be appended with `.html` extension and is assumed to be relative to the root of the site.
+
+For nested subfolders, the plugin will automatically create these directories in the `site_dir`.
+
+## Contributing
+
+- Pull requests are welcome.
+- File bugs and suggestions in the Github Issues tracker.
+
+## Releasing
+
+```
+    make release
+```
+
+It will prompt you for your PyPI user and password.
+
+See:
+- https://packaging.python.org/tutorials/packaging-projects/
+- https://packaging.python.org/guides/migrating-to-pypi-org/
---
+++ packages/mkdocs-redirects/mkdocs-redirects-0.0.6.tar.gz/mkdocs-redirects-0.0.6/mkdocs_redirects/plugin.py
@@ -0,0 +1,50 @@
+import logging
+import os
+import textwrap
+
+from mkdocs import utils as mkdocs_utils
+from mkdocs.config import config_options, Config
+from mkdocs.plugins import BasePlugin
+from mkdocs.structure.files import File
+
+log = logging.getLogger(__name__)
+log.addFilter(mkdocs_utils.warning_filter)
+
+
+class RedirectPlugin(BasePlugin):
+
+    def on_post_build(self, config, **kwargs):
+        redirects = config.get('redirects', {})
+
+        for old_page, new_page in redirects.items():
+            old_page_path = os.path.join(config['site_dir'], '{}.html'.format(old_page))
+            if not new_page.startswith(('http','HTTP')):
+                new_page_path = os.path.join(config['site_dir'], '{}.html'.format(new_page))
+                # check that the page being redirected to actually exists
+                if not os.path.exists(os.path.dirname(new_page_path)):
+                    msg = 'Redirect does not exist for path: {}'.format(new_page_path)
+                    if config.get('strict', False):
+                        raise Exception(msg)
+                    else:
+                        log.warn(msg)
+                new_page = '/{}.html'.format(new_page)
+
+            # ensure the folder path exists, recursively for nested directories.
+            if not os.path.exists(os.path.dirname(old_page_path)):
+                os.makedirs(os.path.dirname(old_page_path))
+
+            # write an HTML file in the site_dir with a meta redirect to the new page
+            # note that it will prefix the path with `/` to be relative to the site root.
+            with open(old_page_path, 'w') as f:
+                f.write(textwrap.dedent("""
+                    <!doctype html>
+                    <html lang="en" class="no-js">
+                    <head>
+                        <script>var anchor=window.location.hash.substr(1);location.href="{url}"+(anchor?"#"+anchor:"")</script>
+                        <meta http-equiv="refresh" content="0; url={url}">
+                    </head>
+                    <body>
+                    Redirecting...
+                    </body>
+                    </html>
+                """).format(url=new_page))

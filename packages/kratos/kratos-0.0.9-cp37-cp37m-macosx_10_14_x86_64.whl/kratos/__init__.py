133387d79467ba17f6eb4f8f5b9bdd168001f67b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:49 +0000

    Add package kratos/0.0.9/kratos-0.0.9-cp37-cp37m-macosx_10_14_x86_64.whl

---
+++ packages/kratos/kratos-0.0.9-cp37-cp37m-macosx_10_14_x86_64.whl/kratos/__init__.py
@@ -0,0 +1,25 @@
+from .generator import Generator, PortType, PortDirection, BlockEdgeType, \
+    always, signed, CombinationalCodeBlock, SequentialCodeBlock
+
+from .passes import Attribute, verilog
+from .util import clog2, reduce_add, reduce_and, reduce_mul, reduce_or,\
+    concat, zext
+
+# directly import from the underlying C++ binding
+from _kratos.util import is_valid_verilog
+from _kratos.exception import VarException, StmtException
+from _kratos.passes import IRVisitor as IRVisitor
+from _kratos import PackedStruct, Port, Var, AssignmentType
+from .stmts import IfStmt, SwitchStmt, if_, switch_
+
+__all__ = ["Generator", "PortType", "PortDirection", "BlockEdgeType", "always",
+           "verilog", "signed", "is_valid_verilog", "VarException",
+           "StmtException", "IRVisitor"]
+
+__all__ += ["CombinationalCodeBlock", "SequentialCodeBlock", "SwitchStmt",
+            "PackedStruct", "Port", "Var", "IfStmt", "AssignmentType",
+            "if_", "switch_", "Attribute"]
+
+# utils
+__all__ += ["clog2", "reduce_add", "reduce_and", "reduce_mul", "reduce_or",
+            "concat", "zext"]

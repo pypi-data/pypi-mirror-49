7cab03a0f2c23fc4466c2836f91c51a564e62ff8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:13 +0000

    Add package robojslib/0.1.1/robojslib-0.1.1-py3-none-any.whl

---
+++ packages/robojslib/robojslib-0.1.1-py3-none-any.whl/robojslib/__init__.py
@@ -0,0 +1,10 @@
+from robojslib import robojslib
+from version import VERSION
+
+__author__ = 'Andrea Gubellini'
+__email__ = 'agubellini@yahoo.com'
+__version__ = VERSION
+
+class robojs(robojslib):
+    ROBOT_LIBRARY_SCOPE = "GLOBAL"
+    ROBOT_LIBRARY_DOC_FORMAT = "ROBOT"

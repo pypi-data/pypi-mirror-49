e1b74b1ff95c09b5a4997819a8b0119d11a60346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:07 +0000

    Add package pyls/0.1.6/pyls-0.1.6.tar.gz

---
+++ packages/pyls/pyls-0.1.6.tar.gz/pyls-0.1.6/setup.py
@@ -0,0 +1,31 @@
+import sys
+
+from setuptools import setup
+
+NAME = 'pyls'
+SCRIPT = 'pyls.bat'
+
+
+def main():
+    if 'py2exe' in sys.argv:
+        import py2exe
+    version = __import__(NAME).__version__
+    setup(
+        name=NAME,
+        version=version,
+        packages=['pyls'],
+        entry_points={
+            'console_scripts': [
+                'pyls=pyls.main:main',
+            ],
+        },
+        author='Jonathan Hartley',
+        author_email='tartley@tartley.com',
+        url='https://github.com/tartley/pyls',
+        license='New BSD License',
+    )
+
+
+if __name__ == '__main__':
+    main()
+

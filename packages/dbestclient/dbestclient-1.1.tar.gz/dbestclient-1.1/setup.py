f729b36ca15797be06abdb84a355f6efa386004e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:53 +0000

    Add package dbestclient/1.1/dbestclient-1.1.tar.gz

---
+++ packages/dbestclient/dbestclient-1.1.tar.gz/dbestclient-1.1/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='dbestclient',
+    version='1.1',
+    description='Model-based Approximate Query Processing (AQP) engine.',
+    classifiers=[
+        'Development Status :: 3.0',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Approximate Query Processing :: AQP :: Data Warehouse',
+      ],
+    keywords='Approximate Query Processing AQP',
+    url='https://github.com/qingzma/DBEstClient',
+    author='Qingzhi Ma',
+    author_email='Q.Ma.2@warwick.ac.uk',
+    long_description=readme,
+    license=license,
+    packages=['dbestclient'], #find_packages(exclude=('examples', 'docs')),
+    entry_points = {
+            'console_scripts': ['dbestclient=dbestclient.main:main'],
+        },
+    zip_safe=False,
+    install_requires=[
+          'numpy','sqlparse','pandas','scikit-learn','qregpy', 'scipy'
+      ],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+)

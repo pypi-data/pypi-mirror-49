882587131de0b9263144d3d2e2d884c5bd5f12dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:54 +0000

    Add package miniirc-extras/0.2.7/miniirc_extras-0.2.7.tar.gz

---
+++ packages/miniirc-extras/miniirc_extras-0.2.7.tar.gz/miniirc_extras-0.2.7/miniirc_extras/__init__.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+#
+# miniirc_extras: Adds extendability to miniirc.
+#
+# © 2019 by luk3yx.
+#
+
+import miniirc, os, sys
+assert hasattr(miniirc, 'ver') and miniirc.ver >= (1,4,0), \
+    'miniirc_extras requires miniirc >= 1.4.0.'
+
+from miniirc import CmdHandler, Handler, IRC
+from typing import Any, List
+from ._classes import *
+
+# Version info
+ver     = VersionInfo(0,2,7, 'alpha')
+version = 'miniirc v{}.{}.{} / miniirc_extras v{}.{}.{}'.format(miniirc.ver[0],
+    miniirc.ver[1], miniirc.ver[2], ver[0], ver[1], ver[2])
+
+# The base exception class
+class error(Exception):
+    pass
+
+# The require() code may eventually move into miniirc.
+if hasattr(miniirc, 'Feature'):
+    from miniirc import Feature # type: ignore
+else:
+    from ._require import Feature
+
+AbstractIRC.require = miniirc.IRC.require
+
+# Load features on-the-fly when required
+def _core_feature(name: str) -> None:
+    module = __name__ + '.features.' + name # type: str
+
+    @Feature(name)
+    def _feature(irc: miniirc.IRC) -> Any:
+        if name.startswith('_'):
+            print('WARNING: WIP feature loaded!', file=sys.stderr)
+
+        __import__(module)
+        return irc.require(name)
+
+# Create __all__
+__all__ = ['CmdHandler', 'Feature', 'Handler', 'IRC', 'error'] # type: List[str]
+
+# Load the base classes
+from . import _classes
+__all__.extend(_classes.__all__)
+del _classes
+
+# Add core features
+for f in os.listdir(os.path.dirname(__file__) + os.sep + 'features'):
+    if not f.startswith('__') and f.endswith('.py'):
+        _core_feature(f[:-3])
+
+del f
---
+++ packages/miniirc-extras/miniirc_extras-0.2.7.tar.gz/miniirc_extras-0.2.7/miniirc_extras/_classes.py
@@ -0,0 +1,188 @@
+#!/usr/bin/env python3
+#
+# Base miniirc_extras classes
+#
+
+import abc, collections, io, miniirc, sys, threading
+from typing import Any, Callable, Dict, Iterable, List, Optional, Set, Tuple, \
+    Type, Union
+from deprecated import deprecated # type: ignore
+
+__all__ = ['AbstractIRC', 'DummyIRC', 'Hostmask', 'VersionInfo']
+
+# Remove ._classes from __module__.
+def _fix_name(cls):
+    if cls.__module__.endswith('._classes'):
+        cls.__module__ = cls.__module__.rsplit('.', 1)[0]
+    return cls
+
+# A metaclass for instance checking
+class _HostmaskMetaclass(type):
+    def __instancecheck__(self, other) -> bool:
+        # Make sure the other class is a 3-tuple
+        if not isinstance(other, tuple) or len(other) != 3:
+            return False
+
+        # Make sure all arguments are str instances
+        return all(isinstance(i, str) for i in other)
+
+# Create a hostmask class
+@_fix_name
+class Hostmask(tuple, metaclass = _HostmaskMetaclass):
+    def __new__(cls, nick: str, user: str, host: str) -> Tuple[str, str, str]:
+        res = (nick, user, host) # type: Tuple[str, str, str]
+        if not all(isinstance(i, str) for i in res):
+            raise TypeError('{} is not a valid hostmask!'.format(res))
+        return res
+
+# Backport namedtuple features - This is made public in utils.py
+if sys.version_info >= (3, 7):
+    from collections import namedtuple as _namedtuple
+else:
+    def _namedtuple(typename: str, field_names: Union[str, Iterable[str]], *,
+            rename: bool = False, defaults: Optional[Iterable[Any]] = None,
+            module: Optional[str] = None):
+        # Try and auto-detect the module
+        if module is None:
+            try:
+                module = sys._getframe(1).f_globals.get('__name__', '__main__')
+            except:
+                pass
+
+        if sys.version_info >= (3, 6):
+            res = collections.namedtuple(typename, field_names, # type: ignore
+                rename = rename, module = module)
+        else:
+            res = collections.namedtuple(typename, field_names, # type: ignore
+                rename = rename)
+            if module is not None:
+                res.__module__ = module
+
+        if defaults is not None:
+            res.__new__.__defaults__ = tuple(defaults) # type: ignore
+        return res
+
+# A version info class
+VersionInfo = _fix_name(_namedtuple('VersionInfo', 'major minor micro '
+    'releaselevel serial', defaults = (0,0,0, 'final', 0), module = __name__))
+VersionInfo.patch = VersionInfo.micro # type: ignore
+VersionInfo.__module__ += '.utils'
+
+# Make miniirc.ver a VersionInfo.
+miniirc.ver = VersionInfo(*miniirc.ver) # type: ignore
+
+# An abstract IRC class
+_hostmask = Union[Hostmask, Tuple[str, str, str]]
+@_fix_name
+class AbstractIRC(abc.ABC):
+    connected = None # type: Optional[bool]
+    debug_file = None # type: Optional[Union[io.TextIOWrapper, miniirc._Logfile]]
+    msglen = 512 # type: int
+    _sasl = False # type: bool
+    _unhandled_caps = None # type: Optional[set]
+
+    ip = None # type: str
+    port = None # type: int
+    nick = None # type: str
+    channels = None # type: Set[str]
+    ident = None # type: str
+    realname = None # type: str
+    ssl = None # type: Optional[bool]
+    persist = True # type: bool
+    ircv3_caps = None # type: Set[str]
+    active_caps = None # type: Set[str]
+    isupport = None # type: Dict[str, Union[str, int]]
+    connect_modes = None # type: Optional[str]
+    quit_message = 'I grew sick and died.' # type: str
+    ping_interval = 60 # type: int
+    verify_ssl = True # type: bool
+
+    ns_identity = None # type: Optional[Union[Tuple[str, str], str]]
+
+    # Functions copied from miniirc.IRC.
+    def require(self, feature: str) -> Optional[Callable[[miniirc.IRC], Any]]:
+        ...
+    def debug(self, *args: Any, **kwargs) -> None: ...
+    def quote(self, *msg: str, force: Optional[bool] = None,
+        tags: Optional[Dict[str, Union[str, bool]]] = None) -> None: ...
+    def msg(self, target: str, *msg: str,
+        tags: Optional[Dict[str, Union[str, bool]]] = None) -> None: ...
+    def notice(self, target: str, *msg: str,
+        tags: Optional[Dict[str, Union[str, bool]]] = None) -> None: ...
+    def ctcp(self, target: str, *msg: str, reply: bool = False,
+        tags: Optional[Dict[str, Union[str, bool]]] = None) -> None: ...
+    def me(self, target: str, *msg: str,
+        tags: Optional[Dict[str, Union[str, bool]]] = None) -> None: ...
+
+    # Abstract functions.
+    @abc.abstractmethod
+    def Handler(self, *events: str, colon: bool = False,
+        ircv3: bool = False) -> Callable: ...
+    @abc.abstractmethod
+    def CmdHandler(self, *events: str, colon: bool = False,
+        ircv3: bool = False) -> Callable: ...
+
+    @abc.abstractmethod
+    def connect(self) -> None: ...
+
+    @abc.abstractmethod
+    def disconnect(self, msg: Optional[str] = None, *,
+        auto_reconnect: bool = False) -> None: ...
+
+    @abc.abstractmethod
+    def finish_negotiation(self, cap: str) -> None: ...
+
+    @abc.abstractmethod
+    def change_parser(self, parser: Callable[[str],
+        Tuple[str, _hostmask, Dict[str, Union[str, bool]],
+        List[str]]] = miniirc.ircv3_message_parser) -> None: ...
+
+    @abc.abstractmethod
+    def _handle(self, cmd: str, hostmask: _hostmask,
+        tags: Dict[str, Union[str, bool]], args: List[str]) -> bool: ...
+
+    @abc.abstractmethod
+    def main(self) -> threading.Thread: ...
+
+    @abc.abstractmethod
+    def __init__(self, ip: str, port: int, nick: str,
+        channels: Union[List[str], Set[str]] = None, *,
+        ssl: Optional[bool] = None, ident: Optional[str] = None,
+        realname: Optional[str] = None, persist: bool = True,
+        debug: Union[bool, io.TextIOWrapper, str] = False,
+        ns_identity: Optional[Union[Tuple[str, str], str]] = None,
+        auto_connect: bool = True, ircv3_caps: Union[Set[str], List[str],
+        Tuple[str, ...]] = [], connect_modes: Optional[str] = None,
+        quit_message: str = 'I grew sick and died.', ping_interval: int = 60,
+        verify_ssl: bool = True) -> None: ...
+
+# Replace some functions with ones from miniirc.IRC
+for func in ('debug', 'quote', 'msg', 'notice', 'ctcp', 'me'):
+    setattr(AbstractIRC, func, getattr(miniirc.IRC, func)) # type: ignore
+del func
+
+AbstractIRC.register(miniirc.IRC)
+
+# A dummy IRC class
+# TODO: Move this to miniirc_extras.utils
+@_fix_name
+class _DummyIRC(miniirc.IRC):
+    def connect(self) -> None: raise NotImplementedError
+
+    def quote(self, *msg: str, force: Optional[bool] = None,
+            tags: Optional[Dict[str, Union[str, bool]]] = None) -> None:
+        pass
+
+    def __init__(self, ip: str = '', port: int = 0, nick: str = '',
+            channels = (), **kwargs) -> None:
+        kwargs['auto_connect'] = False
+        super().__init__(ip, port, nick, channels, **kwargs)
+
+_DummyIRC.__qualname__ = _DummyIRC.__name__ = 'DummyIRC'
+_DummyIRC.__module__ += '.utils'
+
+@_fix_name
+@deprecated(version='0.2.6',
+            reason='Use miniirc_extras.utils.DummyIRC instead.')
+class DummyIRC(_DummyIRC):
+    """ Deprecated, use miniirc_extras.utils.DummyIRC instead. """

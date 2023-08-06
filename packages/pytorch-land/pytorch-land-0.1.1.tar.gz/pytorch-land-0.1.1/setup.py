67d37bb335679749d4e2b9a8670ceafa8975135f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:02 +0000

    Add package pytorch-land/0.1.1/pytorch-land-0.1.1.tar.gz

---
+++ packages/pytorch-land/pytorch-land-0.1.1.tar.gz/pytorch-land-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pytorch-land',
+    version='0.1.1',
+    description='pytorch-land for happy deep learning',
+    long_description='',
+    author='dansuh17',
+    author_email='kaehops@gmail.com',
+    url='https://github.com/dansuh17/pytorch-land',
+    license='MIT',
+    packages=find_packages(),
+)

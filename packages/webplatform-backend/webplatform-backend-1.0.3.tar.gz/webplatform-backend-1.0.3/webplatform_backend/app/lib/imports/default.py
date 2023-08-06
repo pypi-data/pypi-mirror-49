115ff1832181ba30cef01f1f9469fe04651d5094
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:41 +0000

    Add package webplatform-backend/1.0.3/webplatform-backend-1.0.3.tar.gz

---
+++ packages/webplatform-backend/webplatform-backend-1.0.3.tar.gz/webplatform-backend-1.0.3/webplatform_backend/app/lib/imports/default.py
@@ -0,0 +1,6 @@
+from bson.objectid import ObjectId
+# from lib.utils.db import Manager
+# from lib.utils.modules import Modules
+# from lib.utils.config import Settings
+from views.lib.responses import *
+import datetime, json

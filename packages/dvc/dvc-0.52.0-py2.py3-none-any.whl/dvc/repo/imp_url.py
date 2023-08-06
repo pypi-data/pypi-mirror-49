d40ee2c23e1a455208338e93ecce2f57d3210156
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:17 +0000

    Add package dvc/0.52.0/dvc-0.52.0-py2.py3-none-any.whl

---
+++ packages/dvc/dvc-0.52.0-py2.py3-none-any.whl/dvc/repo/imp_url.py
@@ -0,0 +1,35 @@
+from dvc.utils.compat import pathlib
+from dvc.repo.scm_context import scm_context
+
+
+@scm_context
+def imp_url(
+    self, url, out=None, resume=False, fname=None, erepo=None, locked=False
+):
+    from dvc.stage import Stage
+
+    out = out or pathlib.PurePath(url).name
+
+    with self.state:
+        stage = Stage.create(
+            repo=self,
+            cmd=None,
+            deps=[url],
+            outs=[out],
+            fname=fname,
+            erepo=erepo,
+        )
+
+    if stage is None:
+        return None
+
+    self.check_dag(self.stages() + [stage])
+
+    with self.state:
+        stage.run(resume=resume)
+
+    stage.locked = locked
+
+    stage.dump()
+
+    return stage

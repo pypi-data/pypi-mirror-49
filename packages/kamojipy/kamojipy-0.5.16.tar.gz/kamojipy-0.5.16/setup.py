ea6f29c511aa6a550439c2921faf581c3a040724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:28 +0000

    Add package kamojipy/0.5.16/kamojipy-0.5.16.tar.gz

---
+++ packages/kamojipy/kamojipy-0.5.16.tar.gz/kamojipy-0.5.16/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(name='kamojipy',
+      version='0.5.16',
+      description='Add kamoji to your python file',
+      url='https://github.com/SafyreLyons/kamojipy',
+      author='Safyre Lyons',
+      author_email='lyons.safyre@gmail.com',
+      license='MIT',
+      py_modules=['kamojipy'],
+      package_dir={'': 'src'},
+      zip_safe=False)

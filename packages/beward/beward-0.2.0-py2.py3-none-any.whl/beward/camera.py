cfa0b2ab3de80646d1a0644546ca2c530bb4fbd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:05 +0000

    Add package beward/0.2.0/beward-0.2.0-py2.py3-none-any.whl

---
+++ packages/beward/beward-0.2.0-py2.py3-none-any.whl/beward/camera.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+
+"""Beward camera controller."""
+
+#
+#  Copyright (c) 2019, Andrey "Limych" Khrolenok <andrey@khrolenok.ru>
+#  Creative Commons BY-NC-SA 4.0 International Public License
+#  (see LICENSE.md or https://creativecommons.org/licenses/by-nc-sa/4.0/)
+#
+
+import logging
+
+from requests import ConnectTimeout
+
+from beward.const import ALARM_MOTION
+from .core import BewardGeneric
+
+_LOGGER = logging.getLogger(__name__)
+
+
+# pylint: disable=R0902,R0904
+class BewardCamera(BewardGeneric):
+    """Beward camera controller class."""
+
+    def __init__(self, host_ip, username, password):
+        super().__init__(host_ip, username, password)
+
+        self.last_motion_timestamp = None
+        self.last_motion_image = None
+
+        self._live_image_url = self.get_url(
+            'images',
+            extra_params={
+                'channel': 0,
+            },
+            # Add authentication data
+            username=self.username,
+            password=self.password,
+        )
+
+        self._rtsp_live_video_url = None
+        try:
+            info = self.get_info('rtsp')
+            url = 'rtsp://%s:%s@%s:%s/av0_0' % (
+                self.username, self.password,
+                self.host, info.get('RtspPort', 554),
+            )
+            self._rtsp_live_video_url = url
+        except ConnectTimeout:
+            pass
+
+    def _handle_alarm(self, timestamp, alarm, state):
+        """Handle alarms from Beward device."""
+
+        super()._handle_alarm(timestamp, alarm, state)
+
+        if alarm == ALARM_MOTION and state == 1:
+            self.last_motion_timestamp = timestamp
+            self.last_motion_image = self.live_image
+
+    @property
+    def live_image(self):
+        """Return bytes of camera image."""
+
+        res = self.query('images', extra_params={'channel': 0})
+
+        if not res.headers.get('Content-Type') in ('image/jpeg', 'image/png'):
+            return None
+
+        return res.content
+
+    @property
+    def live_image_url(self):
+        """Return URL to get live photo from camera."""
+        return self._live_image_url
+
+    @property
+    def rtsp_live_video_url(self):
+        """Return URL to get live video from camera via RTSP protocol."""
+        return self._rtsp_live_video_url

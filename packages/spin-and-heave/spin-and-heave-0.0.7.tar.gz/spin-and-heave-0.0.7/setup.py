c08ddf28ac6138165688d43fbea381ada5c07f37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:24 +0000

    Add package spin-and-heave/0.0.7/spin-and-heave-0.0.7.tar.gz

---
+++ packages/spin-and-heave/spin-and-heave-0.0.7.tar.gz/spin-and-heave-0.0.7/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="spin-and-heave",
+    version="0.0.7",
+    author="Andy Klier",
+    author_email="andyklier@gmail.com",
+    description="package and zip lambdas, upload to s3, then run terraform.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages = ['spinandheave'],
+    include_package_data=True,
+    install_requires= ['setuptools'],
+    entry_points = {
+        'console_scripts': ['spin-and-heave=spinandheave.main:main'],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

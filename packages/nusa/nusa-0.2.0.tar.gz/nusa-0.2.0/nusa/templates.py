598cdfb45f803f3f0b897709041051b3e2287f0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:30 +0000

    Add package nusa/0.2.0/nusa-0.2.0.tar.gz

---
+++ packages/nusa/nusa-0.2.0.tar.gz/nusa-0.2.0/nusa/templates.py
@@ -0,0 +1,38 @@
+# ***********************************
+#  Author: Pedro Jorge De Los Santos     
+#  E-mail: delossantosmfq@gmail.com 
+#  Blog: numython.github.io
+#  License: MIT License
+# ***********************************
+
+TRUSS_SIMPLE_REPORT = """
+==========================
+    NuSA Simple Report
+==========================
+
+Model: {model_name}
+Number of nodes: {nodes}
+Number of elements: {elements}
+
+NODAL DISPLACEMENTS
+{nodal_displacements}
+
+NODAL FORCES
+{nodal_forces}
+
+ELEMENT FORCES
+{element_forces}
+
+ELEMENT STRESSES
+{element_stresses}
+
+
+FINITE-ELEMENT MODEL INFO
+
+NODES:
+{nodes_info}
+
+ELEMENTS:
+{elements_info}
+
+"""
---
+++ packages/nusa/nusa-0.2.0.tar.gz/nusa-0.2.0/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# ***********************************
+#  Author: Pedro Jorge De Los Santos     
+#  E-mail: delossantosmfq@gmail.com 
+#  Web: numython.github.io
+#  License: MIT License
+# ***********************************
+from setuptools import setup
+
+# Current status: pre-alpha
+
+setup(name='nusa',
+      version='0.2.0',
+      description='Numerical Structural Analysis in Python using the FEM',
+      author='Pedro Jorge De Los Santos',
+      author_email='delossantosmfq@gmail.com',
+      license = "MIT",
+      keywords=["Structural Analysis","Finite Element Analysis","Mechanical Engineering"],
+      url='https://github.com/JorgeDeLosSantos/nusa',
+      packages=['nusa'],
+      classifiers=[
+      "Development Status :: 2 - Pre-Alpha",
+      "Intended Audience :: Education",
+      "Intended Audience :: Developers",
+      "License :: OSI Approved :: MIT License",
+      "Operating System :: OS Independent",
+      "Programming Language :: Python",
+      "Programming Language :: Python :: 3",
+      "Programming Language :: Python :: Implementation :: CPython",
+      ]
+      )

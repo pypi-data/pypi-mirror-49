7d05ad8824396a1e66605878a4de7a9ae3a431bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:21 +0000

    Add package pytest-nunit/0.1.0/pytest-nunit-0.1.0.tar.gz

---
+++ packages/pytest-nunit/pytest-nunit-0.1.0.tar.gz/pytest-nunit-0.1.0/README.rst
@@ -0,0 +1,80 @@
+============
+pytest-nunit
+============
+
+.. image:: https://dev.azure.com/AnthonyShaw/pytest-nunit/_apis/build/status/tonybaloney.pytest-nunit?branchName=master
+   :target: https://dev.azure.com/AnthonyShaw/pytest-nunit/_build/latest?definitionId=3?branchName=master
+   :alt: Build status
+
+.. image:: https://img.shields.io/pypi/v/pytest-nunit.svg
+    :target: https://pypi.org/project/pytest-nunit
+    :alt: PyPI version
+
+.. image:: https://img.shields.io/pypi/pyversions/pytest-nunit.svg
+    :target: https://pypi.org/project/pytest-nunit
+    :alt: Python versions
+
+.. image:: https://img.shields.io/pypi/dm/pytest-nunit.svg
+     :target: https://pypi.python.org/pypi/pytest-nunit/
+     :alt: PyPI download month
+
+
+A pytest plugin for generating Nunit3 test result XML output
+
+This plugin is an early beta release!
+
+Configuration
+-------------
+
+--nunit-xml
+~~~~~~~~~~~
+
+Use ``--nunit-xml=output.xml`` to create an Nunit3-compatible file called ``output.xml``
+
+Argument takes a path to the output file, either relative, or absolute.
+
+Fixtures
+--------
+
+The following fixtures are made available by this plugin.
+
+record_nunit_property
+~~~~~~~~~~~~~~~~~~~~~
+
+Calling `record_nunit_property(key: str, value: str)` will result in `Property` tags being added to the `test-case` for the related node. 
+
+.. code-block:: python
+
+    def test_basic(record_nunit_property):
+        record_nunit_property("test", "value")
+        assert 1 == 1
+
+add_nunit_attachment
+~~~~~~~~~~~~~~~~~~~~
+
+Add an attachment to a node test-case by calling the `add_nunit_attachment()` function with the filepath and a description.
+
+.. code-block:: python
+
+    def test_attachment(add_nunit_attachment):
+        pth = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'fixture.gif')
+        add_nunit_attachment(path, "peanut butter jelly time")
+        assert 1 == 1
+
+Usage with Azure Pipelines
+--------------------------
+
+Add the following steps to your build job to publish the results to Azure Pipelines:
+
+.. code-block:: yaml
+
+      - script: "pip install pytest-nunit"
+
+      - script: |
+          python -m pytest tests -v --nunit-xml=test-results-integration.xml
+        continueOnError: true
+
+      - task: PublishTestResults@2
+        inputs:
+          testResultsFormat: NUnit
+          testResultsFiles: '**/test-results*.xml'
---
+++ packages/pytest-nunit/pytest-nunit-0.1.0.tar.gz/pytest-nunit-0.1.0/pytest_nunit/nunit.py
@@ -0,0 +1,184 @@
+import sys
+import os
+import locale
+import platform
+from .models.nunit import (
+    TestRunType,
+    TestResultType,
+    TestCaseElementType,
+    TestSuiteElementType,
+    TestStatusType,
+    TestRunStateType,
+    TestSuiteTypeType,
+    PropertyBagType,
+    PropertyType,
+    EnvironmentType,
+    AssertionStatusType,
+    AssertionsType,
+    AssertionType,
+    AttachmentsType,
+    AttachmentType,
+    ReasonType,
+    FailureType
+)
+from .attrs2xml import AttrsXmlRenderer, CdataComment
+
+FRAMEWORK_VERSION = "3.6.2"  # Nunit version this was based on
+CLR_VERSION = sys.version
+
+
+PYTEST_TO_NUNIT = {
+    "passed": TestStatusType.Passed,
+    "failed": TestStatusType.Failed,
+    "skipped": TestStatusType.Skipped,
+}
+
+
+def _format_assertions(case):
+    # TODO
+    return None
+
+
+def _format_attachments(case):
+    if case['attachments']:
+        return AttachmentsType(
+                attachment=[
+                    AttachmentType(filePath=k, description=v)
+                    for k, v in case["attachments"].items()
+                ]
+            )
+    else:
+        return None
+
+
+def _getlocale():
+    # TODO: Come up with a Python 2/3 friendly way of doing this
+    return 'en-US'
+
+
+class NunitTestRun(object):
+    """
+    Convert a test report into a Nunit Test Run
+    """
+
+    def __init__(self, nunitxml):
+        self.nunitxml = nunitxml
+
+    @property
+    def environment(self):
+        return EnvironmentType(
+            framework_version=FRAMEWORK_VERSION,
+            clr_version=CLR_VERSION,
+            os_version=platform.release(),
+            platform=platform.system(),
+            cwd=os.getcwd(),
+            machine_name=platform.machine(),
+            user="",  # TODO: Get sys user but only with a toggle to hide this
+            user_domain="",  # TODO: Get sys user but only with a toggle to hide this
+            culture=_getlocale(),
+            uiculture=_getlocale(),  # TODO: Get UI? Locale
+            os_architecture=platform.architecture()[0],
+        )
+
+    @property
+    def test_cases(self):
+        return [
+            TestCaseElementType(
+                id_=str(case["idref"]),
+                name=nodeid,
+                fullname=nodeid,
+                methodname=nodeid,
+                properties=PropertyBagType(
+                    property=[
+                        PropertyType(name=k, value=v)
+                        for k, v in case["properties"].items()
+                    ]
+                ),
+                environment=self.environment,
+                settings=None,
+                failure=FailureType(message=CdataComment(text=case['error']), stack_trace=CdataComment(text=case['stack-trace'])),
+                reason=ReasonType(message=CdataComment(text=case['reason'])),
+                output=CdataComment(text=case['reason']),
+                assertions=_format_assertions(case),
+                attachments=_format_attachments(case),
+                classname="",
+                runstate=TestRunStateType.Skipped if case['outcome'] == 'skipped' else TestRunStateType.Runnable,
+                seed=str(sys.flags.hash_randomization),
+                result=PYTEST_TO_NUNIT.get(
+                    case["outcome"], TestStatusType.Inconclusive
+                ),
+                label="",
+                site=None,
+                start_time=case["start"].strftime("%Y-%m-%d %H:%M:%S"),
+                end_time=case["stop"].strftime("%Y-%m-%d %H:%M:%S"),
+                duration=case["duration"],
+                asserts=0,
+            )
+            for nodeid, case in self.nunitxml.cases.items()
+        ]
+
+    @property
+    def test_suites(self):
+        return [
+            TestSuiteElementType(
+                id_="3",  # TODO : Suite numbers
+                name=self.nunitxml.suite_name,
+                fullname="example",
+                methodname="test",
+                classname="testClass",
+                test_suite=None,
+                properties=PropertyBagType(
+                    property=[PropertyType(name="pythony_version", value=sys.version)]
+                ),
+                environment=self.environment,
+                settings=None,
+                failure=None,
+                reason=None,
+                output=None,
+                assertions=None,
+                attachments=None,
+                test_case=self.test_cases,
+                runstate=TestRunStateType.Runnable,
+                type_=TestSuiteTypeType.Assembly,
+                testcasecount=self.nunitxml.stats["total"],
+                result=TestStatusType.Passed,
+                label="",
+                site=None,
+                start_time=self.nunitxml.suite_start_time.strftime("%Y-%m-%d %H:%M:%S"),
+                end_time=self.nunitxml.suite_stop_time.strftime("%Y-%m-%d %H:%M:%S"),
+                duration=self.nunitxml.suite_time_delta,
+                asserts=self.nunitxml.stats["asserts"],
+                total=self.nunitxml.stats["total"],
+                passed=self.nunitxml.stats["passed"],
+                failed=self.nunitxml.stats["failure"],
+                warnings=0,
+                inconclusive=0,
+                skipped=self.nunitxml.stats["skipped"],
+            )
+        ]
+
+    def as_test_run(self):
+        return TestRunType(
+            id_="2",
+            testcasecount=self.nunitxml.stats["total"],
+            result=TestResultType.Passed,
+            start_time=self.nunitxml.suite_start_time.strftime("%Y-%m-%d %H:%M:%S"),
+            end_time=self.nunitxml.suite_stop_time.strftime("%Y-%m-%d %H:%M:%S"),
+            duration=int(self.nunitxml.suite_time_delta),
+            total=self.nunitxml.stats["total"],
+            passed=self.nunitxml.stats["passed"],
+            failed=self.nunitxml.stats["failure"],
+            inconclusive=0,
+            skipped=self.nunitxml.stats["skipped"],
+            asserts=self.nunitxml.stats["asserts"],
+            command_line=" ".join(sys.argv),
+            filter_=None,
+            test_case=None,
+            test_suite=self.test_suites,
+            engine_version=FRAMEWORK_VERSION,
+            clr_version=CLR_VERSION,
+        )
+
+    def generate_xml(self):
+        tr = self.as_test_run()
+        return AttrsXmlRenderer.render(tr, "test-run")

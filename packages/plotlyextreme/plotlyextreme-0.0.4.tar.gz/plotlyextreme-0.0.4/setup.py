2c9d2daa808687ef5d044a496e22a4a75420be78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:12:11 +0000

    Add package plotlyextreme/0.0.4/plotlyextreme-0.0.4.tar.gz

---
+++ packages/plotlyextreme/plotlyextreme-0.0.4.tar.gz/plotlyextreme-0.0.4/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="plotlyextreme",
+    version="0.0.4",
+    author="Jonathan",
+    description="Simple wrapper functions for plotly visualization package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jonathan-marsan/plotlyextreme",
+    packages=setuptools.find_packages(),
+    install_requires=[
+          'pandas',
+          'plotly',
+      ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

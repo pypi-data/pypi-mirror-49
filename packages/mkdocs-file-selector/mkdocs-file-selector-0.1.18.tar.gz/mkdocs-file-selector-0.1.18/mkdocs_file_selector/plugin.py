cd9a260528fb5de679e98cf3b996e023a60e1ca3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:25 +0000

    Add package mkdocs-file-selector/0.1.18/mkdocs-file-selector-0.1.18.tar.gz

---
+++ packages/mkdocs-file-selector/mkdocs-file-selector-0.1.18.tar.gz/mkdocs-file-selector-0.1.18/mkdocs_file_selector/plugin.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import re
+from timeit import default_timer as timer
+from datetime import datetime, timedelta
+
+from mkdocs import utils as mkdocs_utils
+from mkdocs.config import config_options, Config
+from mkdocs.plugins import BasePlugin
+
+class FileSelector(BasePlugin):
+
+    config_scheme = (
+        ('param', config_options.Type(mkdocs_utils.string_types, default='')),
+    )
+
+    def __init__(self):
+        self.enabled = True
+        self.total_time = 0
+
+    def on_page_markdown(self, markdown, page, config, site_navigation=None, **kwargs):
+        selected = re.search('{?(.*)?}', markdown);
+        paths = selected.split(' | ')
+        return "" + str(selected)

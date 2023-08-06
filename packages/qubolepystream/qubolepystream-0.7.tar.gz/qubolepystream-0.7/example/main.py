662a72efa6ad41486be38df2ffce003dbebe55dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:53 +0000

    Add package qubolepystream/0.7/qubolepystream-0.7.tar.gz

---
+++ packages/qubolepystream/qubolepystream-0.7.tar.gz/qubolepystream-0.7/example/main.py
@@ -0,0 +1,12 @@
+from qubolepystream.connector import QuboleConnector
+
+
+if __name__ == "__main__":
+    ''' A simple example of how to use the connector '''
+    con = QuboleConnector(api_token='api_token')
+
+    query = r"""select * from table"""
+    # query = open('query.sql').read() # import from local file
+
+    # Use job_id to retrieve job and verbose=True for streaming output
+    data = con.query_data(sql_query=query, job_id=None, engine='Hive', cluster='Hive_cluster_name', verbose=False)

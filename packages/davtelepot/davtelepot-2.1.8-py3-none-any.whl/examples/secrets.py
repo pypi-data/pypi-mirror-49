456411523f67448bebe942bf1a6dc22a1b9c2b92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:38 +0000

    Add package davtelepot/2.1.8/davtelepot-2.1.8-py3-none-any.whl

---
+++ packages/davtelepot/davtelepot-2.1.8-py3-none-any.whl/examples/secrets.py
@@ -0,0 +1,4 @@
+simple_bot_token = "197190910:AAGzsK6eliocHcgFu2WLl2baKQ8hGz0ORag"
+webhook_bot_token = "279769259:AAFGbTKEAwcuBCOXlXfbwy2iDaRl654wCtM"
+hostname = "davte.it/telegram"
+certificate = ""

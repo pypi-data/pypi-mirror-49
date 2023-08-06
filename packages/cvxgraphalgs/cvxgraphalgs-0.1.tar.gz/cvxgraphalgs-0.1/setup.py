1970662bd2f125547190ff7e8c80b2e597498efd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:43 +0000

    Add package cvxgraphalgs/0.1/cvxgraphalgs-0.1.tar.gz

---
+++ packages/cvxgraphalgs/cvxgraphalgs-0.1.tar.gz/cvxgraphalgs-0.1/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='cvxgraphalgs',
+    version='0.1',
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'cvxpy',
+        'networkx',
+        'itertools',
+        'random'
+    ],
+
+    author='Hermish Mehta',
+    author_email='hermishdm@gmail.com',
+    keywords='graph algorithms theory convex optimization',
+    description='Modern convex optimization-based graph algorithms.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/hermish/cvx-graph-algorithms',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ]
+)

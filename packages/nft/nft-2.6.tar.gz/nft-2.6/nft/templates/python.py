3a3576448c1672526a244570edf6f7e58beb65c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:52 +0000

    Add package nft/2.6/nft-2.6.tar.gz

---
+++ packages/nft/nft-2.6.tar.gz/nft-2.6/nft/templates/python.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''-------------------------------------
+<filename>.py
+<project>
+
+    by <name> (<email>)
+    Copyright © <year> <name>. All rights reserved.
+-------------------------------------- 
+'''
+print("New python3 script file created from template")
---
+++ packages/nft/nft-2.6.tar.gz/nft-2.6/nft/templates/ruby.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+=begin
+<filename>.rb
+<project>
+
+    by <name> (<email>)
+    Copyright © <year> <name>. All rights reserved.
+-------------------------------------- 
+=end
+puts "New ruby script file created from template"

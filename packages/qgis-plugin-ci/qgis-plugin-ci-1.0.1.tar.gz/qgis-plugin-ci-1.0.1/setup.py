bce7086dbdbbc7efffab4ed906ac821787de33aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:05 +0000

    Add package qgis-plugin-ci/1.0.1/qgis-plugin-ci-1.0.1.tar.gz

---
+++ packages/qgis-plugin-ci/qgis-plugin-ci-1.0.1.tar.gz/qgis-plugin-ci-1.0.1/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup
+import sys
+
+python_min_version=(3, 7)
+
+if sys.version_info < python_min_version:
+    sys.exit('qgis-plugin-ci requires at least Python version {vmaj}.{vmin}.\n'
+             'You are currently running this installation with\n\n{curver}'.format(
+        vmaj=python_min_version[0],
+        vmin=python_min_version[1],
+        curver=sys.version))
+
+setup(
+    name='qgis-plugin-ci',
+    packages=[
+        'qgispluginci'
+    ],
+    scripts=[
+        'scripts/qgis-plugin-ci'
+    ],
+    package_data={'': ['plugins.xml.template']},
+    include_package_data=True,  # required for files in MANIFEST.in
+    version='1.0.1',
+    description='Let QGIS-plugin-ci package and release your QGIS plugins for you. Have a tea or go skiing meanwhile.',
+    author='Denis Rouzaud',
+    author_email='denis.rouzaud@gmail.com',
+    url='https://github.com/opengisch/qgis-plugin-ci',
+    download_url='https://github.com/opengisch/qgis-plugin-ci/archive/1.0.1.tar.gz',
+    keywords=['QGIS'],
+    classifiers=[
+        'Topic :: Scientific/Engineering :: GIS',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Information Technology',
+        'Development Status :: 3 - Alpha'
+    ],
+    install_requires=[
+        'python-slugify',
+        'pyyaml',
+        'pytransifex',
+        'gitpython',
+        'PyQt5',
+        'PyGithub'
+    ],
+    python_requires=">={vmaj}.{vmin}".format(vmaj=python_min_version[0], vmin=python_min_version[1]),
+)

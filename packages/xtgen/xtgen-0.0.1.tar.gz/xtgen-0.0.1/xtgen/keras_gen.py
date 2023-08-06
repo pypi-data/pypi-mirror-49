8be801c1618b6299d95208890578952a503ba5c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:47 +0000

    Add package xtgen/0.0.1/xtgen-0.0.1.tar.gz

---
+++ packages/xtgen/xtgen-0.0.1.tar.gz/xtgen-0.0.1/xtgen/keras_gen.py
@@ -0,0 +1,24 @@
+# keras_gen.py: code for generating keras apps
+import os
+import shutil
+from . import utils
+
+class KerasGenerator():
+    def __init__(self):
+        pass
+
+    def generate(self, output_dir, dataset, model):
+        mydir = os.path.dirname(__file__) 
+        if dataset == "default" and model == "default":
+            utils.user_error("you must specify the dataset or the model (or both)")
+            
+        if dataset == "mnist":
+            from_dir = mydir + "/keras_assets/mnist"
+        else:
+            utils.user_error("dataset not yet available for keras: {}".format(dataset))
+
+        count = len(os.listdir(from_dir))
+        shutil.copytree(from_dir, output_dir)
+
+        return count
+        

d36f65918a5a4aa7f7c370254e2151170680afbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:58 +0000

    Add package rozental-as-a-service/0.0.7/rozental_as_a_service-0.0.7.tar.gz

---
+++ packages/rozental-as-a-service/rozental_as_a_service-0.0.7.tar.gz/rozental_as_a_service-0.0.7/rozental_as_a_service/common_types.py
@@ -0,0 +1,23 @@
+from mypy_extensions import TypedDict
+from typing import List, Optional
+
+
+class TypoInfo(TypedDict):
+    original: str
+    possible_options: List[str]
+
+
+class BackendsConfig(TypedDict):
+    speller_chunk_size: int
+    vocabulary_path: Optional[str]
+    db_path: Optional[str]
+
+
+class RozentalOptions(TypedDict):
+    path: str
+    vocabulary_path: str
+    exclude: List[str]
+    db_path: str
+    exit_zero: bool
+    processes_amount: int
+    verbosity: int

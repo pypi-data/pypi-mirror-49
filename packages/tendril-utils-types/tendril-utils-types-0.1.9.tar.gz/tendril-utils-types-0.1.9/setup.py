af5329f5e6f4c07f3e9ece8c74037e85927be97f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:51 +0000

    Add package tendril-utils-types/0.1.9/tendril-utils-types-0.1.9.tar.gz

---
+++ packages/tendril-utils-types/tendril-utils-types-0.1.9.tar.gz/tendril-utils-types-0.1.9/setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup
+    from setuptools import find_packages
+except ImportError:
+    from distutils.core import setup
+    find_packages = None
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+    'tendril-utils-core>=0.1.12',
+    'tendril-config>=0.1.6',
+    'tendril-utils-www>=0.1.5',
+    'six',
+    'cachecontrol[filecache]',
+    'arrow',
+    'num2words',
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='tendril-utils-types',
+    version='0.1.9',
+    description="Type classes for tendril",
+    long_description=readme,
+    author="Chintalagiri Shashank",
+    author_email='shashank@chintal.in',
+    url='https://github.com/chintal/tendril-utils-types',
+    packages=find_packages(),
+    install_requires=requirements,
+    license="AGPLv3",
+    zip_safe=False,
+    keywords='tendril',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+    ],
+    # test_suite='tests',
+    # tests_require=test_requirements
+)

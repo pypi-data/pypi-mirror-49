63749223c2172fe4d09cd3cd202c7cd1fce031f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:01:51 +0000

    Add package glean-parser/0.30.0/glean_parser-0.30.0.tar.gz

---
+++ packages/glean-parser/glean_parser-0.30.0.tar.gz/glean_parser-0.30.0/glean_parser/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+"""Top-level package for Glean parser."""
+
+__author__ = """Michael Droettboom"""
+__email__ = 'mdroettboom@mozilla.com'
+__version__ = '0.30.0'

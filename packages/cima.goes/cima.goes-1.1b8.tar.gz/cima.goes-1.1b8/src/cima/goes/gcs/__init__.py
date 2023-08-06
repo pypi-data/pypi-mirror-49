8e747b29d4b53346c1dadeb876f526003e0de3e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:16 +0000

    Add package cima.goes/1.1b8/cima.goes-1.1b8.tar.gz

---
+++ packages/cima.goes/cima.goes-1.1b8.tar.gz/cima.goes-1.1b8/src/cima/goes/gcs/__init__.py
@@ -0,0 +1,3 @@
+from cima.goes.gcs._google_cloud import set_credentials, mount_colab
+from cima.goes.gcs._google_cloud import band, group_blobs, get_blobs
+from cima.goes.gcs._google_cloud import get_dataset_from_blob, get_datasets, close_datasets, close_dataset

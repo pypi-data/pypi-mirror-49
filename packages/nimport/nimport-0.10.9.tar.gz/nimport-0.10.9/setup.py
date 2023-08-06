ee373ed9b7ab4c91e289651d77ee21745eb04b21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:38 +0000

    Add package nimport/0.10.9/nimport-0.10.9.tar.gz

---
+++ packages/nimport/nimport-0.10.9.tar.gz/nimport-0.10.9/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+NAME = "nimport"
+VERSION = '0.10.9'
+
+INSTALL_REQUIRES = [
+    'PyGithub',
+    'websocket-client',
+    'IPython',
+    'nbformat',
+]
+
+setup(
+    name=NAME,
+    version=VERSION,
+    install_requires=INSTALL_REQUIRES,
+    packages=find_packages(exclude=['tests'])
+)

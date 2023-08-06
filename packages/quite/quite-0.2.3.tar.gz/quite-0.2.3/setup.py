4368e71fc9b06158ab4f97beffdfa035676cef37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:31 +0000

    Add package quite/0.2.3/quite-0.2.3.tar.gz

---
+++ packages/quite/quite-0.2.3.tar.gz/quite-0.2.3/setup.py
@@ -0,0 +1,37 @@
+"""
+QT UI Extension
+
+Author: SF-Zhou
+Date: 2016-08-07
+
+See:
+https://github.com/sf-zhou/quite
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='quite',
+    version='0.2.3',
+    description='QT UI Extension',
+    url='https://github.com/sf-zhou/quite',
+
+    author='SF-Zhou',
+    author_email='sfzhou.scut@gmail.com',
+
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+
+    keywords='qt ui',
+    packages=find_packages(exclude=['docs', 'tests', 'examples']),
+    install_requires=['st', 'prett', 'typing']
+)

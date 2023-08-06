c44f3c2351d270b0c0993576745fc553e0ebedcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:57 +0000

    Add package pycommon/0.3.6/pycommon-0.3.6.tar.gz

---
+++ packages/pycommon/pycommon-0.3.6.tar.gz/pycommon-0.3.6/setup.py
@@ -0,0 +1,23 @@
+"""
+Setup script
+"""
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+VERSION = '0.3.6'
+setuptools.setup(
+    name='pycommon',
+    packages=['pycommon'],
+    version=VERSION,
+    description='Useful functions missing from the standard libraries',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='David Grant',
+    author_email='davidgrant@gmail.com',
+    url='https://github.com/dgrant/pycommon',
+    download_url='https://github.com/dgrant/pycommon/tarball/' + VERSION,
+    keywords=['utility', 'miscellaneous', 'library'],
+    classifiers=[],
+)

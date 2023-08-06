4b228ef172bd927ed60f4644fee5dde3ffc77aac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:46 +0000

    Add package limetr/0.0.4/limetr-0.0.4.tar.gz

---
+++ packages/limetr/limetr-0.0.4.tar.gz/limetr-0.0.4/setup.py
@@ -0,0 +1,20 @@
+from numpy.distutils.core import setup
+from numpy.distutils.core import Extension
+
+# fortran extension module
+ext = Extension(name='limetr.special_mat',
+                sources=['limetr/special_mat.f90'],
+                library_dirs=['/usr/local/lib'],
+                libraries=['lapack', 'blas'])
+
+setup(name='limetr',
+      version='0.0.4',
+      description='linear mixed effects model with trimming',
+      url='https://github.com/zhengp0/limetr',
+      author='Peng Zheng',
+      author_email='zhengp@uw.edu',
+      license='MIT',
+      packages=['limetr'],
+      ext_modules=[ext],
+      install_requires=['numpy', 'scipy', 'ipopt'],
+      zip_safe=False)

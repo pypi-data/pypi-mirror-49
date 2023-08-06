145cb909e20055ebb0f9929642972acc779c9d34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:00 +0000

    Add package rotest/7.8.2/rotest-7.8.2-py2.py3-none-any.whl

---
+++ packages/rotest/rotest-7.8.2-py2.py3-none-any.whl/rotest/cli/server.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import
+
+import pkg_resources
+import django
+from django.core.management import call_command
+
+from rotest.common import core_log
+from rotest.common.config import DJANGO_MANAGER_PORT
+
+
+def start_server():
+    """Run session manager and Django server according to the config file."""
+    django.setup()
+
+    for entry_point in \
+            pkg_resources.iter_entry_points("rotest.cli_server_actions"):
+        core_log.debug("Applying server entry point %s", entry_point.name)
+        extension_action = entry_point.load()
+        extension_action()
+
+    call_command("runserver", "0.0.0.0:{}".format(DJANGO_MANAGER_PORT))

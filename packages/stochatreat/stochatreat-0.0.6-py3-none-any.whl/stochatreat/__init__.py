3816e1c84c90676cb5652cec57c97b24fd7ecc29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:01 +0000

    Add package stochatreat/0.0.6/stochatreat-0.0.6-py3-none-any.whl

---
+++ packages/stochatreat/stochatreat-0.0.6-py3-none-any.whl/stochatreat/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 10 12:16:55 2019
+
+===============================================================================
+@author:    Manuel Martinez
+@project:   stochatreat
+===============================================================================
+"""
+
+__version__ = '0.0.6'
+__author__ = 'Manuel Martinez'
+
+from .stochatreat import stochatreat
+from .utils import *

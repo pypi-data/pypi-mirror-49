9e9925446d0ccdeab0822cedd07ab916ca85c653
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:11:24 +0000

    Add package templateflow/0.4.1/templateflow-0.4.1.tar.gz

---
+++ packages/templateflow/templateflow-0.4.1.tar.gz/templateflow-0.4.1/templateflow/__init__.py
@@ -0,0 +1,13 @@
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+"""
+The Zone of Templates
+=====================
+"""
+from .__about__ import __version__, __copyright__, __credits__
+
+__all__ = [
+    '__version__',
+    '__copyright__',
+    '__credits__',
+]

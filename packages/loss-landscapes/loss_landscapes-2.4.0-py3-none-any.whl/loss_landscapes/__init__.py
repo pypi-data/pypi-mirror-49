2690d5a6de497bfc0f816e7834dcb3ebd16c5651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:57 +0000

    Add package loss-landscapes/2.4.0/loss_landscapes-2.4.0-py3-none-any.whl

---
+++ packages/loss-landscapes/loss_landscapes-2.4.0-py3-none-any.whl/loss_landscapes/__init__.py
@@ -0,0 +1,8 @@
+from loss_landscapes.main import point
+from loss_landscapes.main import linear_interpolation
+from loss_landscapes.main import random_line
+from loss_landscapes.main import planar_interpolation
+from loss_landscapes.main import random_plane
+from loss_landscapes.model_interface.agent_interface import ModelInterface
+from loss_landscapes.model_metrics import Metric, MetricPipeline
+from loss_landscapes.model_metrics import torch as torch_metrics

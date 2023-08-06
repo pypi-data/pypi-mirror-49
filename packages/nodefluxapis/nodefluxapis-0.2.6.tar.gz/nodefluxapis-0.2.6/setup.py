520bef972257c02208cb456a8d1b598e6f863ab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:03:06 +0000

    Add package nodefluxapis/0.2.6/nodefluxapis-0.2.6.tar.gz

---
+++ packages/nodefluxapis/nodefluxapis-0.2.6.tar.gz/nodefluxapis-0.2.6/setup.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
+
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_namespace_packages, setup, Command
+
+NAME = "nodefluxapis"
+DESCRIPTION = "Nodeflux APIs."
+URL = "https://gitlab.com/nodefluxio/nodefluxapis"
+EMAIL = "ilham@nodeflux.io"
+AUTHOR = "Ilham Imaduddin"
+REQUIRES_PYTHON = ">=3.4.0"
+VERSION = "0.2.6"
+REQUIRED = ["protobuf", "grpcio", "googleapis-common-protos"]
+EXTRAS = {}
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+        long_description = "\n" + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
+about = {"__version__": VERSION}
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = "Build and publish the package."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
+        except OSError:
+            pass
+
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
+
+        self.status("Uploading the package to PyPI via Twine…")
+        os.system("twine upload dist/*")
+
+        sys.exit()
+
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    version=about["__version__"],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    package_dir={"": "python"},
+    packages=find_namespace_packages(where="python"),
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license="Apache License 2.0",
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    # $ setup.py publish support.
+    cmdclass={"upload": UploadCommand},
+)

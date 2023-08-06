d87f1f878c9e50e7ce3b44e14c258067222e3c1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:38 +0000

    Add package rita-dsl/0.1.0/rita_dsl-0.1.0-py3-none-any.whl

---
+++ packages/rita-dsl/rita_dsl-0.1.0-py3-none-any.whl/rita/__init__.py
@@ -0,0 +1,20 @@
+import logging
+
+from rita.translate import rules_to_patterns
+from rita.parser import RitaParser
+
+logger = logging.getLogger(__name__)
+
+
+def compile(fname):
+    parser = RitaParser()
+    parser.build()
+    with open(fname, "r") as f:
+        raw = f.read()
+
+    root = parser.parse(raw)
+    logger.debug(root)
+    result = [rule
+              for doc in root if doc
+              for rule in rules_to_patterns(doc())]
+    return result

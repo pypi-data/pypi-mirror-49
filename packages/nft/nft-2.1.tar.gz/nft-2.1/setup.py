b7966489761e492ce7c610aaf21f880af280f438
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:28 +0000

    Add package nft/2.1/nft-2.1.tar.gz

---
+++ packages/nft/nft-2.1.tar.gz/nft-2.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+import io
+__version__ = '2.1'
+
+with io.open('README.rst') as readme:
+    setup(name='nft',
+            version=__version__,
+            description='A tool for quickly creating a New File form a Template',
+            long_description=readme.read(),
+            license='Expat License',
+            author='Daniel Richards',
+            author_email='ddrichar@ucsc.edu',
+            url='https://github.com/dan-rds/NFT',
+            packages=['nft'],
+            package_data={'nft': ['*.txt', 'templates/*']},
+            classifiers=[
+                "Programming Language :: Python :: 3"
+            ],
+            install_requires=['setuptools==40.7.3', 'plumbum==1.6.7', 'PyYAML==5.1.1'],
+            zip_safe=False,
+            scripts=['bin/nft'],
+            keywords='automation, template',
+            )

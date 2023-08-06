5fd7b18dcb0b1e43c0933f6f0cc1f9dfe0f06cb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:14 +0000

    Add package hello-world-lizixi/0.0.3/hello_world_lizixi-0.0.3.tar.gz

---
+++ packages/hello-world-lizixi/hello_world_lizixi-0.0.3.tar.gz/hello_world_lizixi-0.0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+__project__ = "hello_world_lizixi"
+__version__ = "0.0.3"
+__description__ = "a Python module to say hello world"
+__packages__ = ["hello"]
+setup(
+    name = __project__,
+    version = __version__,
+    description = __description__,
+    packages = __packages__,
+)

71d59d1cdaec64b23d72d39517682f1d1d8edc0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:46 +0000

    Add package pydistinct/0.2/pydistinct-0.2.tar.gz

---
+++ packages/pydistinct/pydistinct-0.2.tar.gz/pydistinct-0.2/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pydistinct",
+    version="0.2",
+    author="Edwin Chan",
+    author_email="edwinchan@u.nus.edu",
+    description="Package for estimating distinct values in a population",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chanedwin/pydistinct/",
+    download_url='https://github.com/chanedwin/pydistinct/archive/v0.2.tar.gz', 
+    keywords=['distinct', 'value', 'estimators', 'sample', 'sequences'], 
+    install_requires=[ 
+        'scipy',
+        'statsmodels',
+        'xgboost',
+        'numpy'
+    ],
+
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

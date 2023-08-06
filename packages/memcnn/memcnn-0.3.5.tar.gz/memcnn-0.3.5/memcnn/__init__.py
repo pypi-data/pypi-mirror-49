edd2ca0d929ca78dccb541ad9fe1a30656447f56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:21 +0000

    Add package memcnn/0.3.5/memcnn-0.3.5.tar.gz

---
+++ packages/memcnn/memcnn-0.3.5.tar.gz/memcnn-0.3.5/memcnn/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for MemCNN."""
+
+__author__ = """Sil van de Leemput"""
+__email__ = 'silvandeleemput@gmail.com'
+__version__ = '0.3.5'
+
+
+from memcnn.models.revop import ReversibleBlock
+
+__all__ = [
+    'ReversibleBlock'
+]

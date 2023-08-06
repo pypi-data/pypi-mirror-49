2818b222a315bff9e41081a2e625a37db34e736e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:22 +0000

    Add package Renpy-Tools/0.0.1/Renpy_Tools-0.0.1.tar.gz

---
+++ packages/Renpy-Tools/Renpy_Tools-0.0.1.tar.gz/Renpy_Tools-0.0.1/Renpy_Tools/get_tran_inf.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+import io
+
+def get_file1_tra(file1, M=1):
+    read = io.open(file1, "r", encoding='utf-8')
+    lines = read.readlines()
+    Line = 0
+    Mode = 1
+    Tran = {}
+    Boolean = False
+    if M == 1:
+        for line in lines:
+            Line += 1
+
+            try:
+                if line[0:9] == "translate" or Boolean:
+                    if line[-9:-1] == "strings:":
+                        Mode = 2
+                        Boolean = False
+                    elif Mode == 1 and Boolean == False:
+                        Boolean = True
+                    elif Boolean and Mode == 1:
+                        sline = line.strip()
+                        if sline[0] == "#":
+                            Title = line
+                            Context = lines[Line]
+                            Tran[Title] = Context
+                            Boolean = False
+                if Mode == 2:
+                    siine = line.strip()
+                    if siine[:3] == "old":
+                        Title = line
+                        Context = lines[Line]
+                        Tran[Title] = Context
+
+            except IndexError:
+                pass
+    elif M == 2:
+        for line in lines:
+            Line += 1
+
+            try:
+                if line[0:9] == "translate" or Boolean:
+                    if line[-9:-1] == "strings:":
+                        Mode = 2
+                        Boolean = False
+                    elif Mode == 1 and line[0:9] == "translate":
+                        Title = line
+                        Context = lines[Line + 2]
+                        Tran[Title] = Context
+                        Boolean = False
+                if Mode == 2:
+                    siine = line.strip()
+                    if siine[:3] == "old":
+                        Title = line
+                        Context = lines[Line]
+                        Tran[Title] = Context
+
+            except IndexError:
+                pass
+
+    read.close()
+
+    return Tran
+
---
+++ packages/Renpy-Tools/Renpy_Tools-0.0.1.tar.gz/Renpy_Tools-0.0.1/setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name="Renpy_Tools",
+    version="0.0.1",
+    author="ClodLingxi",
+    author_email="clodlingxi@qq.com",
+    description="操作Renpy的文件",
+    url="https://github.com/clodlingxi",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

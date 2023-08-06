dca546a9c987bc40b8582da083becdc9d41e11bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:17 +0000

    Add package lumes/0.0.4a2/lumes-0.0.4a2.tar.gz

---
+++ packages/lumes/lumes-0.0.4a2.tar.gz/lumes-0.0.4a2/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+from os import path
+
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='lumes',
+    version='0.0.4a2',
+    description='A simple wraper for screenshot',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/juliancoffee/lumes-py/',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Utilities',
+        'Operating System :: POSIX :: Linux',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3, <4',
+    keywords='screenshot',
+    packages=['lumes'],
+    entry_points={
+        'console_scripts': [
+            'lumes=lumes.__main__:main',
+        ],
+    },
+    project_urls={
+        'Bug Reports': 'https://github.com/juliancoffee/lumes-py/issues',
+        'Source': 'https://github.com/juliancoffee/lumes-py/',
+    },
+)

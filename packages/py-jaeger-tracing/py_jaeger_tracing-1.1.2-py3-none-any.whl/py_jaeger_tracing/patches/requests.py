5078a9fa4065a6a00e1e4ed3c4965a6a6d5ab5e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:43 +0000

    Add package py-jaeger-tracing/1.1.2/py_jaeger_tracing-1.1.2-py3-none-any.whl

---
+++ packages/py-jaeger-tracing/py_jaeger_tracing-1.1.2-py3-none-any.whl/py_jaeger_tracing/patches/requests.py
@@ -0,0 +1,28 @@
+import copy
+
+from jaeger_client import Span
+from opentracing import Format
+from opentracing import tags
+
+
+def patch_requests():
+    import requests
+
+    from py_jaeger_tracing.main import TracingEnvironment
+
+    real_impl = requests.request
+
+    def fake_impl(method, url, **kwargs):
+        kwargs = copy.deepcopy(kwargs)
+
+        span: Span = TracingEnvironment.spans[-1]
+        span.set_tag(tags.HTTP_METHOD, method.upper())
+        span.set_tag(tags.HTTP_URL, url)
+        span.set_tag(tags.SPAN_KIND, tags.SPAN_KIND_RPC_CLIENT)
+
+        kwargs['headers'] = kwargs.get('headers', {})
+        TracingEnvironment.tracer.inject(span, Format.HTTP_HEADERS, kwargs['headers'])
+
+        return real_impl(method, url, **kwargs)
+
+    requests.api.request = fake_impl

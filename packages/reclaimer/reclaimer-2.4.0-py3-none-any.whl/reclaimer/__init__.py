7379404bc81d7cf446bb4b0fe45db7ab6b8ce90f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:54 +0000

    Add package reclaimer/2.4.0/reclaimer-2.4.0-py3-none-any.whl

---
+++ packages/reclaimer/reclaimer-2.4.0-py3-none-any.whl/reclaimer/__init__.py
@@ -0,0 +1,13 @@
+# ##############
+#   metadata   #
+# ##############
+__author__ = "Devin Bobadilla"
+#   #           YYYY.MM.DD
+__date__ = "2019.07.05"
+__version__ = (2, 4, 0)
+__all__ = (
+    "h2", "h3", "hek", "os_hek", "os_v3_hek", "os_v4_hek", "stubbs",
+    "meta", "misc", "common_descs", "constants", "data_extraction",
+    "field_type_methods", "field_types", "util", "animation", "bitmaps",
+    "halo_script", "model", "physics", "sounds", "strings"
+    )

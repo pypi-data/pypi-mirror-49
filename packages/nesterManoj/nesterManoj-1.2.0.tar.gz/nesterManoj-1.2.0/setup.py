1b5c7e63329b9bbe279049931781b747c5f5eaa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:06 +0000

    Add package nesterManoj/1.2.0/nesterManoj-1.2.0.tar.gz

---
+++ packages/nesterManoj/nesterManoj-1.2.0.tar.gz/nesterManoj-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+            name                =  'nesterManoj',
+            version              =  '1.2.0',
+            py_modules      =  ['nesterManoj'],
+            author               =  'manoj b',
+            author_email    =  'manojb1@in.ibm.com',
+            url                     = 'http://www.ibm.com',
+            description       = 'A simple printer of nested lists'
+ 
+          )

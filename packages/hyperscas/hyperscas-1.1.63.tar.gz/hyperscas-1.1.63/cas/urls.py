d41eb9632c62cf4ac152c98245e508ce53578267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:26 +0000

    Add package hyperscas/1.1.63/hyperscas-1.1.63.tar.gz

---
+++ packages/hyperscas/hyperscas-1.1.63.tar.gz/hyperscas-1.1.63/cas/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+app_name = "cas"
+
+urlpatterns = [
+    path("login", views.login, name="login"),
+    path("logout", views.logout, name="logout"),
+    path("users/language", views.language, name="language"),
+    path("enviroment", views.enviroment, name="enviroment"),
+    path("profile", views.profile, name="profile"),
+]

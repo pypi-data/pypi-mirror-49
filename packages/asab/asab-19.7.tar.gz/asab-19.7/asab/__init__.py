5b11695cf05f10357a73233b1961a77889b347fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:17 +0000

    Add package asab/19.7/asab-19.7.tar.gz

---
+++ packages/asab/asab-19.7.tar.gz/asab-19.7/asab/__init__.py
@@ -0,0 +1,13 @@
+__version__ = '19.07'
+
+from .log import _StructuredDataLogger, LOG_NOTICE
+from .application import Application
+from .abc.service import Service
+from .abc.module import Module
+from .abc.singleton import Singleton
+from .config import Config, ConfigObject
+from .pubsub import subscribe, PubSub, Subscriber
+from .pdict import PersistentDict
+from .timer import Timer
+
+from .socket import StreamSocketServerService
---
+++ packages/asab/asab-19.7.tar.gz/asab-19.7/setup.py
@@ -0,0 +1,35 @@
+import re
+import pathlib
+from setuptools import setup
+from setuptools import find_packages
+
+here = pathlib.Path(__file__).parent
+txt = (here / 'asab' / '__init__.py').read_text('utf-8')
+try:
+    version = re.findall(r"^__version__ = '([^']+)'\r?$", txt, re.M)[0]
+except IndexError:
+    raise RuntimeError('Unable to determine version.')
+
+setup(
+	name='asab',
+	version=version,
+	description='ASAB simplifies a development of async application servers',
+	long_description=open('README.rst').read(),
+	url='https://github.com/TeskaLabs/asab',
+	author='TeskaLabs Ltd',
+	author_email='info@teskalabs.com',
+	license='BSD License',
+	platforms='any',
+	classifiers=[
+		'Development Status :: 4 - Beta',
+		'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: 3.6',
+		'Programming Language :: Python :: 3.7',
+	],
+	keywords='asyncio',
+	packages=find_packages(exclude=['module_sample']),
+	project_urls={
+		'Source': 'https://github.com/TeskaLabs/asab'
+	},
+)
+

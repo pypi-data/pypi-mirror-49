02031fb03f2d5d9379f6cdb66ec9b1cd0e919282
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:32 +0000

    Add package lowhaio-aws-sigv4-unsigned-payload/0.0.4/lowhaio_aws_sigv4_unsigned_payload-0.0.4.tar.gz

---
+++ packages/lowhaio-aws-sigv4-unsigned-payload/lowhaio_aws_sigv4_unsigned_payload-0.0.4.tar.gz/lowhaio_aws_sigv4_unsigned_payload-0.0.4/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+
+def long_description():
+    with open('README.md', 'r') as file:
+        return file.read()
+
+
+setuptools.setup(
+    name='lowhaio_aws_sigv4_unsigned_payload',
+    version='0.0.4',
+    author='Michal Charemza',
+    author_email='michal@charemza.name',
+    description='AWS Signature Version 4 signing for lowhaio, but with UNSIGNED-PAYLOAD',
+    long_description=long_description(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/michalc/lowhaio-aws-sigv4',
+    py_modules=[
+        'lowhaio_aws_sigv4_unsigned_payload',
+    ],
+    python_requires='>=3.6.0',
+    test_suite='test',
+    tests_require=[
+        'lowhaio~=0.0.61',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Framework :: AsyncIO',
+    ],
+)

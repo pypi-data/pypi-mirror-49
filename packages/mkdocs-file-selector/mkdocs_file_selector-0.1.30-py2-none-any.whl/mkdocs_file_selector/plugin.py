0dbd996248d232bacbec66b071e828b53c083608
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:06 +0000

    Add package mkdocs-file-selector/0.1.30/mkdocs_file_selector-0.1.30-py2-none-any.whl

---
+++ packages/mkdocs-file-selector/mkdocs_file_selector-0.1.30-py2-none-any.whl/mkdocs_file_selector/plugin.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import re
+from pprint import pprint
+
+from timeit import default_timer as timer
+from datetime import datetime, timedelta
+
+from mkdocs import utils as mkdocs_utils
+from mkdocs.config import config_options, Config
+from mkdocs.plugins import BasePlugin
+
+class FileSelector(BasePlugin):
+
+    config_scheme = (
+        ('param', config_options.Type(mkdocs_utils.string_types, default='')),
+    )
+
+    def __init__(self):
+        self.enabled = True
+        self.total_time = 0
+
+    def on_page_markdown(self, markdown, page, config, site_navigation=None, **kwargs):
+        pprint(config['site_name'])
+        return markdown

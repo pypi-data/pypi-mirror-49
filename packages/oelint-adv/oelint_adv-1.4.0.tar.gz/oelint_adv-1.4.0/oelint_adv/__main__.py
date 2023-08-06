cedbacea3eb03c06a7d183f4feb77e4e4ba34733
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:10 +0000

    Add package oelint-adv/1.4.0/oelint_adv-1.4.0.tar.gz

---
+++ packages/oelint-adv/oelint_adv-1.4.0.tar.gz/oelint_adv-1.4.0/oelint_adv/__main__.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import argparse
+import glob
+
+sys.path.append(os.path.abspath(os.path.join(__file__, "..")))
+from oelint_adv.cls_rule import load_rules
+from oelint_adv.cls_stash import Stash
+
+def create_argparser():
+    parser = argparse.ArgumentParser(
+        description='Advanced OELint - Check bitbake recipes against OECore styleguide')
+    parser.add_argument("--suppress", default=[], action="append", help="Rules to suppress")
+    parser.add_argument("--output", default=sys.stderr, help="Where to flush the findings (default: stderr)")
+    parser.add_argument("files", nargs='+', help="File to parse")
+    return parser
+
+if __name__ == '__main__':
+    args = create_argparser().parse_args()
+    rules = [x for x in load_rules() if str(x) not in args.suppress] 
+    print("Loaded rules: {}".format(",".join(sorted([str(x) for x in rules]))))
+    stash = Stash()
+    issues = []
+    for f in args.files:
+        try:
+            stash.AddFile(f)
+        except FileNotFoundError:
+            print("Can't open/read {}".format(f))
+
+    for f in stash.GetRecipes():
+        for r in rules:
+            issues += r.check(f, stash)
+    issues = list(set(issues))
+    if args.output != sys.stderr:
+        args.output = open(args.output, "w")
+    args.output.write("\n".join(sorted(issues)) + "\n")
+    sys.exit(len(issues))

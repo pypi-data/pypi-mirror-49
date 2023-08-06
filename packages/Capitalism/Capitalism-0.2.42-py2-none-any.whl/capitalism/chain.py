633554165b5278290059b4a8efe7a37df45cda12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:47 +0000

    Add package Capitalism/0.2.42/Capitalism-0.2.42-py2-none-any.whl

---
+++ packages/Capitalism/Capitalism-0.2.42-py2-none-any.whl/capitalism/chain.py
@@ -0,0 +1,180 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
+    Chain Class
+
+    Dispatch 'jobs' to factories or workers.
+
+    @author: traitor
+    @github: https://github.com/tbbatbb
+'''
+
+import sys, Queue, time, threading, signal
+import worker, util, factory
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+class Chain:
+    def __init__(self, task_queue, jobs, in_factory=True, size=1,
+        delay=1, debug=False, name=None, exit_on_done=False,
+        timeout=2, split_array=True, int_handler=None, return_to=-1):
+        '''
+            Initialize the job chain.
+
+            @param task_queue:
+                The original task queue.
+            @param jobs:
+                The jobs that need to be done. It's an Array, a serial of jobs should place in execution sequence.
+            @param name:
+                Name for the chain.
+            @param int_handler:
+                The function called when receive keyboard interrupt.
+        '''
+        # Check the parameters
+        if (not isinstance(task_queue, Queue.Queue)
+            and not isinstance(task_queue, list)):
+            self.err('Task Queue Should Be An Instance Of Queue Or Array.')
+            return
+        # If the task_queue is an instance of queue
+        if isinstance(task_queue, list):
+            # Initialize a new queue
+            self.t_q = Queue.Queue()
+            for tk in task_queue:
+                # Put all the tasks in the task queue
+                try:
+                    self.t_q.put(tk, block=False, timeout=timeout)
+                except Queue.Full:
+                    # Output error message
+                    self.err('Task Queue Is Currently Full.')
+                    continue
+        else:
+            self.t_q = task_queue
+        # Jobs should be an array
+        if not isinstance(jobs, list):
+            self.err('Parameter Jobs Should Be A List Of Funtions.')
+            return
+        # Check each jobs
+        for job in jobs:
+            # If the job is not a function
+            if not callable(job):
+                util.err('Each Job Should Be A Function.')
+                return
+        # Set the attributes
+        self.jobs = jobs
+        self.info_out = debug
+        self.name = name
+        self.info_out = debug
+        self.workers = []
+        self.int_handler = int_handler
+        # Initialize the factories of workers
+        if in_factory:
+            # If use factories to do jobs
+            for i in range(len(jobs)):
+                # Initialize a factory
+                self.workers.append(
+                    factory.Factory(
+                        jobs[i],
+                        task_queue=self.t_q if i == 0 else None,
+                        size=size,
+                        result_queue=None,
+                        delay=delay,
+                        debug=debug,
+                        name='F{0}@{1}'.format(i, self.name),
+                        exit_on_done=exit_on_done,
+                        timeout=timeout,
+                        split_array=split_array
+                    )
+                )
+        else:
+            # If use workers to do jobs
+            for i in range(len(jobs)):
+                # Initialize a factory
+                w = worker.Worker(
+                    jobs[i],
+                    task_queue=self.t_q if i == 0 else None,
+                    result_queue=None,
+                    delay=delay,
+                    debug=debug,
+                    name='Worker-{0}@{1}'.format(i, self.name),
+                    exit_on_done=exit_on_done,
+                    timeout=timeout
+                )
+                w.setDaemon(True)
+                self.workers.append(w)
+        # Make workers connected
+        for i in range(len(self.workers) - 1):
+            # Pipe the result to next worker
+            self.workers[i].pipe(self.workers[i+1])
+        # If set return_to
+        if (isinstance(return_to, int)
+            and return_to >= 0
+            and return_to < len(self.workers)):
+            # Set the result queue
+            self.workers[-1].set_result_queue(self.workers[return_to].t_q)
+        # If the callback for keyborad interrupt is given
+        signal.signal(signal.SIGINT, self.int_hdl)
+        signal.signal(signal.SIGTERM, self.int_hdl)
+        # Well, Initialized
+        self.info('Initialized.')
+    
+    def start(self):
+        '''
+            Start all the factories or workers.
+        '''
+        for w in self.workers:
+            w.start()
+        return self
+    
+    def stop(self):
+        '''
+            Stop all the factories or workers.
+        '''
+        for w in self.workers:
+            w.stop()
+        return self
+    
+    def join(self):
+        '''
+            Join all the factories or workers.
+        '''
+        for w in self.workers:
+            w.join()
+        return self
+
+    def info(self, i):
+        '''
+            Output some information.
+
+            @param i:
+                The information which is going to be outputed.
+        '''
+        # If not in the debug mode
+        if not self.info_out:
+            return
+        util.info('[{0}]: {1}'.format(self.name, i))
+    
+    def err(self, e):
+        '''
+            Output some error information.
+
+            @param e:
+                The error information which is going to be outputed.
+        '''
+        # If not in the debug mode
+        if not self.info_out:
+            return
+        util.err('[{0}]: {1}'.format(self.name, e))
+
+    def int_hdl(self, num, frame):
+        '''
+            The callback for keyboard interrupt.
+        '''
+        # Stop all the workers
+        self.stop()
+        self.info('I\'m Exiting...')
+        # If int_handler is callable
+        if callable(self.int_handler):
+            self.int_handler(num, frame)
---
+++ packages/Capitalism/Capitalism-0.2.42-py2-none-any.whl/capitalism/worker.py
@@ -0,0 +1,221 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
+    Worker Class
+
+    Get a task from the 'task_queue',
+    do the job with function 'target',
+    finally put the results into 'result_queue'.
+
+    @author: traitor
+    @github: https://github.com/tbbatbb
+'''
+
+import sys, Queue, time, threading
+import util
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+class Worker(threading.Thread):
+    def __init__(self, target, task_queue=None, result_queue=None, delay=1, debug=False, name=None, exit_on_done=False, timeout=2, split_array=True):
+        '''
+            Initialize the thread
+
+            @param task_queue:
+                Task queue which contains the tasks to be done.
+                Actually, the queue contains parameters for 'target' function.
+            @param target:
+                Actual function to deal with each parameters in 'task_queue'.
+            @param result_queue:
+                A queue for placing the results.
+            @param delay:
+                How much time should the thread to sleep after finishing a job.
+            @param debug:
+                Wether to output the information.
+            @param name:
+                The name of the worker thread.
+            @param exit_on_done:
+                Wether exit the loop when there is no task in the queue.
+            @param timeout:
+                How much time should the thread wait for getting a job or putting results.
+            @param split_array:
+                When the job is done, should I split the result before put it in result queue if it's an array?
+        '''
+        # Check the parameters
+        if not target:
+            return
+        threading.Thread.__init__(self)
+        # Check the parameters
+        if (task_queue
+            and not isinstance(task_queue, Queue.Queue)
+            and not isinstance(task_queue, list)):
+            self.err('Task Queue Should Be An Instance Of Queue Or Array.')
+            return
+        # If the task_queue is an instance of queue
+        if (task_queue
+            and isinstance(task_queue, list)):
+            # Initialize a new queue
+            self.t_q = Queue.Queue()
+            for tk in task_queue:
+                # Put all the tasks in the task queue
+                try:
+                    self.t_q.put(tk, block=False, timeout=timeout)
+                except Queue.Full:
+                    # Output error message
+                    self.err('Task Queue Is Currently Full.')
+                    continue
+        else:
+            self.t_q = task_queue
+        # Set several property
+        self.do = target
+        self.r_q = result_queue
+        self.sleep_time = delay
+        self.info_out = debug
+        self.name = name
+        self.exit_on_done = exit_on_done
+        self.timeout = timeout
+        self.should_stop = False
+        self.split_arr = split_array
+        # Print the information
+        self.info('Initilized.')
+    
+    def run(self):
+        '''
+            Actual loop for doing the job.
+        '''
+        # If task queue is not set
+        if not self.t_q:
+            # Print information
+            self.err('The Task Queue Hasn\'t Been Set.')
+            return
+        while not self.should_stop:
+            try:
+                # Try to get a job
+                param = self.t_q.get(block=False, timeout=self.timeout)
+            except Queue.Empty:
+                # The task queue is empty
+                self.info('Task Queue Is Empty At The Present.')
+                if self.exit_on_done:
+                    # Print some information
+                    self.info('Exit Due To Empty Task Queue.')
+                    break
+                # Sleep for some time
+                time.sleep(self.sleep_time)
+                continue
+            except Exception, e:
+                # Print the error
+                self.err(e)
+                continue
+            # OK, now we got the task, and get the result
+            self.result = self.do(param)
+            # If there is no result queue?
+            if isinstance(self.r_q, Queue.Queue):
+                # Well, we need to put the result into result queue
+                try:
+                    # Try to put the result
+                    if self.split_arr and isinstance(self.result, list):
+                        for i in range(len(self.result)):
+                            self.r_q.put(self.result[i], block=False, timeout=self.timeout)
+                    else:
+                        self.r_q.put(self.result, block=False, timeout=self.timeout)
+                except Queue.Full:
+                    # The result queue is full
+                    self.err('Result Queue Is Full At The Present.')
+                except Exception, e:
+                    # Print the error
+                    self.err(e)
+            self.t_q.task_done()
+            # I'm so tired, can I sleep for a while?
+            time.sleep(self.sleep_time)
+        # WHO KILLED ME!!!!!!!!!
+        self.info('Oops! I Was Killed. Do You Know Who It Is?')
+    
+    def stop(self):
+        '''
+            Stop the thread.
+        '''
+        self.should_stop = True
+        return self
+
+    def set_task_queue(self, target):
+        '''
+            Set my task queue to target
+
+            @param target:
+                Target queue to get tasks
+        '''
+        # If not a valid target
+        if not target:
+            # Print some information
+            self.err('Oops! The New Task Queue Is Invalid.')
+            return
+        # Set the task queue
+        self.t_q = target
+        return self
+
+    def set_result_queue(self, target):
+        '''
+            Set my result queue to target
+
+            @param target:
+                Target queue to put results
+        '''
+        # If not a valid target
+        if not target:
+            # Print some information
+            self.err('Oops! The New Result Queue Is Invalid.')
+            return
+        # Set the result queue
+        self.r_q = target
+        return self
+
+    def pipe(self, poor_guy):
+        '''
+            Pipe the result of me to next poor guy.
+
+            @param poor_guy:
+                Who is the next poor guy?
+                The guy will use my result queue as its task queue.
+        '''
+        # The next is None?????
+        if not poor_guy:
+            self.err('How Can I Transfer The Result To NONE?????')
+            return self
+        if not self.r_q and not poor_guy.t_q:
+            # Generate a temprary queue
+            tmp_queue = Queue.Queue()
+            # Set the queue
+            self.set_result_queue(tmp_queue)
+            poor_guy.set_task_queue(tmp_queue)
+        elif self.r_q:
+            # Set target worker's task queue
+            poor_guy.set_task_queue(self.r_q)
+        else:
+            # Set my result queue
+            self.set_result_queue(poor_guy.t_q)
+        return poor_guy
+
+    def info(self, i):
+        '''
+            Output some information.
+
+            @param i:
+                The information which is going to be outputed.
+        '''
+        # If not in the debug mode
+        if not self.info_out:
+            return
+        util.info('[{0}]: {1}'.format(self.name, i))
+    
+    def err(self, e):
+        '''
+            Output some error information.
+
+            @param e:
+                The error information which is going to be outputed.
+        '''
+        util.err('[{0}]: {1}'.format(self.name, e))
+

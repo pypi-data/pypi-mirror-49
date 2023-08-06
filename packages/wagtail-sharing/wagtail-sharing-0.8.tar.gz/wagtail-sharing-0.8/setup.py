0b21e92c46f7e270de39e24378f947a3fda80150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:07 +0000

    Add package wagtail-sharing/0.8/wagtail-sharing-0.8.tar.gz

---
+++ packages/wagtail-sharing/wagtail-sharing-0.8.tar.gz/wagtail-sharing-0.8/setup.py
@@ -0,0 +1,53 @@
+from setuptools import find_packages, setup
+
+
+install_requires = [
+    'Django>=1.8,<2.1',
+    'wagtail>=1.6,<2.1',
+]
+
+
+testing_extras = [
+    'coverage>=3.7.0',
+    'flake8>=2.2.0',
+    'mock>=1.0.0',
+]
+
+
+short_description = 'Easier sharing of Wagtail drafts'
+
+
+setup(
+    name='wagtail-sharing',
+    url='https://github.com/cfpb/wagtail-sharing',
+    author='CFPB',
+    author_email='tech@cfpb.gov',
+    license='CCO',
+    version='0.8',
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=install_requires,
+    extras_require={
+        'testing': testing_extras,
+    },
+    description=short_description,
+    long_description=open('README.rst').read(),
+    classifiers=[
+        'Framework :: Django',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 2.0',
+        'Framework :: Wagtail',
+        'Framework :: Wagtail :: 1',
+        'Framework :: Wagtail :: 2',
+        'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
+        'License :: Public Domain',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ]
+)

00c439e5ad45f745c6f71c0379c3c9477225c20b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:59 +0000

    Add package realm-cli/0.0.9/realm_cli-0.0.9.tar.gz

---
+++ packages/realm-cli/realm_cli-0.0.9.tar.gz/realm_cli-0.0.9/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="realm_cli",
+    version="0.0.9",
+    entry_points={"console_scripts": ["realm-cli=realm_cli.main:main"]},
+    author="nilinswap",
+    author_email="nilinswap@gmail.com",
+    description="A command line utitily to provide services offered by realm( a rust-elm web framework).",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ackotech/realm/",
+    packages=find_packages(),
+    install_requires=["cookiecutter>=1.6.0"],
+)

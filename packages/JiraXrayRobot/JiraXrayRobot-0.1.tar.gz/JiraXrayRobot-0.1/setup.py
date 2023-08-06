22e5e7a26a6f6d64a8b25015005a4aa26c82c248
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:58 +0000

    Add package JiraXrayRobot/0.1/JiraXrayRobot-0.1.tar.gz

---
+++ packages/JiraXrayRobot/JiraXrayRobot-0.1.tar.gz/JiraXrayRobot-0.1/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'JiraXrayRobot',         
+  packages = ['JiraXrayRobot'],   
+  version = '0.1',      
+  license='GNU General Public License family',
+  description = 'Robot Framework Library to Interact with JIRA XRAY',
+  author = 'Santanu Ray',
+  author_email = 'santanukrray1982@gmail.com',
+  url = 'https://github.com/esauray/JiraXrayRobot',
+  download_url = 'https://github.com/esauray/JiraXrayRobot/archive/master.zip',
+  keywords = ['JIRA', 'XRAY', 'ROBOT FRAMEOWRK'],
+  install_requires=[            # I get to this in a second
+          'validators',
+          'beautifulsoup4',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)

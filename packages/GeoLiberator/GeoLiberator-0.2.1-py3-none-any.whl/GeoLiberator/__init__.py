9bbe104d0fa6719c20f826d737ef82d1b085fea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:02 +0000

    Add package GeoLiberator/0.2.1/GeoLiberator-0.2.1-py3-none-any.whl

---
+++ packages/GeoLiberator/GeoLiberator-0.2.1-py3-none-any.whl/GeoLiberator/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+'''
+Author: David J. Morfe
+Application Name: GeoLiberator
+Functionality Purpose: Instill data quality upon address data
+Version: Alpha 0.2.1
+'''
+
+name = "GeoLiberator"
+from .geoliberator import GeoLiberator, geoLiberate, autoGeoLiberate

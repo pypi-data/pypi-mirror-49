4c07c3d9df9c5000cc2e40b3b9c0f394f7873a2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:58 +0000

    Add package chainer/7.0.0b2/chainer-7.0.0b2.tar.gz

---
+++ packages/chainer/chainer-7.0.0b2.tar.gz/chainer-7.0.0b2/chainerx_cc/chainerx/routines/sorting.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <absl/types/optional.h>
+
+#include "chainerx/array.h"
+#include "chainerx/axes.h"
+
+namespace chainerx {
+
+Array ArgMax(const Array& a, const OptionalAxes& axis = absl::nullopt);
+
+Array ArgMin(const Array& a, const OptionalAxes& axis = absl::nullopt);
+
+}  // namespace chainerx

7840473152bfefd8ea9c9e7afea175b135ecd63c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:04:26 +0000

    Add package rspyutils/0.1.4/rspyutils-0.1.4.tar.gz

---
+++ packages/rspyutils/rspyutils-0.1.4.tar.gz/rspyutils-0.1.4/pyutils/yamls.py
@@ -0,0 +1,20 @@
+# !/usr/bin/python
+# -*- coding: UTF-8 -*-
+"""
+Created on MAY 21, 2018
+@author: zhanglanhui
+homepage recommender:ffm
+"""
+import os
+import yaml
+
+
+def read_from_yaml(file: str) -> dict:
+    conf_dic = yaml.load(open(file, 'r'),
+                         Loader=yaml.FullLoader)
+
+    return conf_dic
+
+
+if __name__ == "__main__":
+    print(read_from_yaml("config/dev/server_config.yaml"))

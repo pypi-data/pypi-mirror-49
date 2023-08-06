2f84a3579a29f077863e1b49e39789dbb6abd42b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:24 +0000

    Add package colouredlogs/10.0.1/colouredlogs-10.0.1.tar.gz

---
+++ packages/colouredlogs/colouredlogs-10.0.1.tar.gz/colouredlogs-10.0.1/docs/examples/custom-colors.sh
@@ -0,0 +1,5 @@
+export COLOUREDLOGS_LOG_FORMAT='%(asctime)s - %(message)s'
+export COLOUREDLOGS_DATE_FORMAT='%H:%M:%S'
+export COLOUREDLOGS_FIELD_STYLES='' # no styles
+export COLOUREDLOGS_LEVEL_STYLES='spam=22;debug=28;verbose=34;notice=220;warning=202;success=118,bold;error=124;critical=background=red'
+colouredlogs --demo

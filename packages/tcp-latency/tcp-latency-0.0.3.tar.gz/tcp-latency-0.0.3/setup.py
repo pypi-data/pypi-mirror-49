a25c44f4ec417820aa14edf710cf33f992d0a99b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:14 +0000

    Add package tcp-latency/0.0.3/tcp-latency-0.0.3.tar.gz

---
+++ packages/tcp-latency/tcp-latency-0.0.3.tar.gz/tcp-latency-0.0.3/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+import setuptools
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='tcp-latency',
+    version='0.0.3',
+    description='Meassure latency using TCP.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/dgzlopes/tcp-latency',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: System :: Networking',
+    ],
+    author='Daniel Gonzalez Lopes',
+    author_email='danielgonzalezlopes@gmail.com',
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'tcp-latency = tcp_latency.tcp_latency:_main',
+        ],
+    },
+)

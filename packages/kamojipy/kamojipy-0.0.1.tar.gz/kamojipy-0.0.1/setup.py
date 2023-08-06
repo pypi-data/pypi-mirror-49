99b299a10204feee198feb1084425940f1084fb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:15 +0000

    Add package kamojipy/0.0.1/kamojipy-0.0.1.tar.gz

---
+++ packages/kamojipy/kamojipy-0.0.1.tar.gz/kamojipy-0.0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='kamojipy',
+    version='0.0.1',
+    description='a pip-installable package for kamojis',
+    license='MIT',
+    author='Safyre Lyons',
+    author_email='lyons.safyre@gmail.com',
+    keywords=['kamoji'],
+    url='https://github.com/SafyreLyons/kamojipy'
+)

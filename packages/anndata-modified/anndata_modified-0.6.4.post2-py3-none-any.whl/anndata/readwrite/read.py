aaeb79c8dbc5e2e1e216189254a26389f1273913
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:12:36 +0000

    Add package anndata-modified/0.6.4.post2/anndata_modified-0.6.4.post2-py3-none-any.whl

---
+++ packages/anndata-modified/anndata_modified-0.6.4.post2-py3-none-any.whl/anndata/readwrite/read.py
@@ -0,0 +1,447 @@
+from pathlib import Path
+from typing import Union, Optional, Iterable, Generator, Iterator
+from collections import OrderedDict
+import gzip
+import bz2
+import numpy as np
+
+from ..base import AnnData
+from .. import h5py
+from .utils import *
+
+
+def read_csv(
+    filename: Union[Path, str, Iterator[str]],
+    delimiter: Optional[str]=',',
+    first_column_names: Optional[bool]=None,
+    dtype: str='float32',
+) -> AnnData:
+    """Read ``.csv`` file.
+
+    Same as :func:`~anndata.read_text` but with default delimiter ``','``.
+
+    Parameters
+    ----------
+    filename
+        Data file.
+    delimiter
+        Delimiter that separates data within text file. If `None`, will split at
+        arbitrary number of white spaces, which is different from enforcing
+        splitting at single white space ' '.
+    first_column_names
+        Assume the first column stores row names.
+    dtype
+        Numpy data type.
+    """
+    return read_text(filename, delimiter, first_column_names, dtype)
+
+
+def read_excel(
+    filename: Union[Path, str],
+    sheet: Union[str, int],
+) -> AnnData:
+    """Read ``.xlsx`` (Excel) file.
+
+    Assumes that the first columns stores the row names and the first row the
+    column names.
+
+    Parameters
+    ----------
+    filename
+        File name to read from.
+    sheet
+        Name of sheet in Excel file.
+    """
+    filename = str(filename)  # allow passing pathlib.Path objects
+    # rely on pandas for reading an excel file
+    from pandas import read_excel
+    df = read_excel(filename, sheet)
+    X = df.values[:, 1:].astype(float)
+    row = {'row_names': df.iloc[:, 0].values.astype(str)}
+    col = {'col_names': np.array(df.columns[1:], dtype=str)}
+    return AnnData(X, row, col)
+
+
+def read_umi_tools(filename: Union[Path, str]) -> AnnData:
+    """Read a gzipped condensed count matrix from umi_tools.
+
+    Parameters
+    ----------
+    filename
+        File name to read from.
+    """
+    filename = str(filename)  # allow passing pathlib.Path objects
+    # import pandas for conversion of a dict of dicts into a matrix
+    # import gzip to read a gzipped file :-)
+    import gzip
+    from pandas import DataFrame
+
+    dod = {}  # this will contain basically everything
+    fh = gzip.open(filename)
+    header = fh.readline()  # read the first line
+
+    for line in fh:
+        t = line.decode('ascii').split('\t')  # gzip read bytes, hence the decoding
+        try:
+            dod[t[1]].update({t[0]:int(t[2])})
+        except KeyError:
+            dod[t[1]] = {t[0]:int(t[2])}
+
+    df = DataFrame.from_dict(dod, orient='index')  # build the matrix
+    df.fillna(value=0., inplace=True)  # many NaN, replace with zeros
+    return AnnData(np.array(df), {'obs_names': df.index}, {'var_names': df.columns})
+
+
+def read_hdf(filename: Union[Path, str], key: str) -> AnnData:
+    """Read ``.h5`` (hdf5) file.
+
+    Note: Also looks for fields ``row_names`` and ``col_names``.
+
+    Parameters
+    ----------
+    filename
+        Filename of data file.
+    key
+        Name of dataset in the file.
+    """
+    filename = str(filename)  # allow passing pathlib.Path objects
+    with h5py.File(filename, 'r') as f:
+        # the following is necessary in Python 3, because only
+        # a view and not a list is returned
+        keys = [k for k in f.keys()]
+        if key == '':
+            raise ValueError('The file ' + filename +
+                             ' stores the following sheets:\n' + str(keys) +
+                             '\n Call read/read_hdf5 with one of them.')
+        # read array
+        X = f[key][()]
+        # try to find row and column names
+        rows_cols = [{}, {}]
+        for iname, name in enumerate(['row_names', 'col_names']):
+            if name in keys:
+                rows_cols[iname][name] = f[name][()]
+    adata = AnnData(X, rows_cols[0], rows_cols[1])
+    return adata
+
+
+def read_loom(filename: Union[Path, str], sparse: bool = False) -> AnnData:
+    """Read ``.loom``-formatted hdf5 file.
+
+    This reads the whole file into memory.
+
+    Beware that you have to explicitly state when you want to read the file as
+    sparse data.
+
+    Parameters
+    ----------
+    filename
+        The filename.
+    sparse
+        Whether to read the data matrix as sparse.
+    """
+    filename = str(filename)  # allow passing pathlib.Path objects
+    from loompy import connect
+    if sparse:
+        with connect(filename, 'r') as lc:
+            X = lc.sparse()
+    else:
+        with h5py.File(filename, 'r') as f:
+            X = f['matrix'][()]
+    with connect(filename, 'r') as lc:
+        adata = AnnData(
+            X.T,
+            obs=dict(lc.col_attrs),  # not ideal: make the generator a dict...
+            var=dict(lc.row_attrs))
+        lc.close()
+    return adata
+
+
+def read_mtx(filename: Union[Path, str], dtype: str='float32') -> AnnData:
+    """Read ``.mtx`` file.
+
+    Parameters
+    ----------
+    filename
+        The filename.
+    dtype
+        Numpy data type.
+    """
+    filename = str(filename)  # allow passing pathlib.Path objects
+    from scipy.io import mmread
+    # could be rewritten accounting for dtype to be more performant
+    X = mmread(filename).astype(dtype)
+    from scipy.sparse import csr_matrix
+    X = csr_matrix(X)
+    return AnnData(X)
+
+
+def read_text(
+    filename: Union[Path, str, Iterator[str]],
+    delimiter: Optional[str]=None,
+    first_column_names: Optional[bool]=None,
+    dtype: str='float32',
+) -> AnnData:
+    """Read ``.txt``, ``.tab``, ``.data`` (text) file.
+
+    Same as :func:`~anndata.read_csv` but with default delimiter ``None``.
+
+    Parameters
+    ----------
+    filename
+        Data file, filename or stream.
+    delimiter
+        Delimiter that separates data within text file. If `None`, will split at
+        arbitrary number of white spaces, which is different from enforcing
+        splitting at single white space ' '.
+    first_column_names
+        Assume the first column stores row names.
+    dtype
+        Numpy data type.
+    """
+    if isinstance(filename, (str, Path)):
+        if str(filename).endswith('.gz'):
+            with gzip.open(str(filename), mode='rt') as f:
+                return _read_text(f, delimiter, first_column_names, dtype)
+        elif str(filename).endswith('.bz2'):
+            with bz2.open(str(filename), mode='rt') as f:
+                return _read_text(f, delimiter, first_column_names, dtype)
+        else:
+            with Path(filename).open() as f:
+                return _read_text(f, delimiter, first_column_names, dtype)
+    else:
+        return _read_text(filename, delimiter, first_column_names, dtype)
+
+
+def iter_lines(file_like: Iterable[str]) -> Generator[str, None, None]:
+    """ Helper for iterating only nonempty lines without line breaks"""
+    for line in file_like:
+        line = line.rstrip('\r\n')
+        if line:
+            yield line
+
+
+def _read_text(
+    f: Iterator[str],
+    delimiter: Optional[str],
+    first_column_names: Optional[bool],
+    dtype: str,
+) -> AnnData:
+    comments = []
+    data = []
+    lines = iter_lines(f)
+    col_names = []
+    row_names = []
+    # read header and column names
+    for line in lines:
+        if line.startswith('#'):
+            comment = line.lstrip('# ')
+            if comment:
+                comments.append(comment)
+        else:
+            if delimiter is not None and delimiter not in line:
+                raise ValueError('Did not find delimiter "{}" in first line.'
+                                 .format(delimiter))
+            line_list = line.split(delimiter)
+            # the first column might be row names, so check the last
+            if not is_float(line_list[-1]):
+                col_names = line_list
+                # logg.msg('    assuming first line in file stores column names', v=4)
+            else:
+                if not is_float(line_list[0]) or first_column_names:
+                    first_column_names = True
+                    row_names.append(line_list[0])
+                    data.append(np.array(line_list[1:], dtype=dtype))
+                else:
+                    data.append(np.array(line_list, dtype=dtype))
+            break
+    if not col_names:
+        # try reading col_names from the last comment line
+        if len(comments) > 0:
+            # logg.msg('    assuming last comment line stores variable names', v=4)
+            col_names = np.array(comments[-1].split())
+        # just numbers as col_names
+        else:
+            # logg.msg('    did not find column names in file', v=4)
+            col_names = np.arange(len(data[0])).astype(str)
+    col_names = np.array(col_names, dtype=str)
+    # read another line to check if first column contains row names or not
+    if first_column_names is None: first_column_names = False
+    for line in lines:
+        line_list = line.split(delimiter)
+        if first_column_names or not is_float(line_list[0]):
+            # logg.msg('    assuming first column in file stores row names', v=4)
+            first_column_names = True
+            row_names.append(line_list[0])
+            data.append(np.array(line_list[1:], dtype=dtype))
+        else:
+            data.append(np.array(line_list, dtype=dtype))
+        break
+    # if row names are just integers
+    if len(data) > 1 and data[0].size != data[1].size:
+        # logg.msg('    assuming first row stores column names and '
+        #          'first column row names', v=4)
+        first_column_names = True
+        col_names = np.array(data[0]).astype(int).astype(str)
+        row_names.append(data[1][0].astype(int).astype(str))
+        data = [data[1][1:]]
+    # parse the file
+    for line in lines:
+        line_list = line.split(delimiter)
+        if first_column_names:
+            row_names.append(line_list[0])
+            data.append(np.array(line_list[1:], dtype=dtype))
+        else:
+            data.append(np.array(line_list, dtype=dtype))
+    # logg.msg('    read data into list of lists', t=True, v=4)
+    # transfrom to array, this takes a long time and a lot of memory
+    # but it's actually the same thing as np.genfromtxt does
+    # - we don't use the latter as it would involve another slicing step
+    #   in the end, to separate row_names from float data, slicing takes
+    #   a lot of memory and CPU time
+    if data[0].size != data[-1].size:
+        raise ValueError(
+            'length of first line ({}) is different from length of last line ({})'
+            .format(data[0].size, data[-1].size))
+    data = np.array(data, dtype=dtype)
+    # logg.msg('    constructed array from list of list', t=True, v=4)
+    # transform row_names
+    if not row_names:
+        row_names = np.arange(len(data)).astype(str)
+        # logg.msg('    did not find row names in file', v=4)
+    else:
+        row_names = np.array(row_names)
+        for iname, name in enumerate(row_names):
+            row_names[iname] = name.strip('"')
+    # adapt col_names if necessary
+    if col_names.size > data.shape[1]:
+        col_names = col_names[1:]
+    for iname, name in enumerate(col_names):
+        col_names[iname] = name.strip('"')
+    return AnnData(data,
+                   obs={'obs_names': row_names},
+                   var={'var_names': col_names})
+
+
+def read_h5ad(filename, backed: Union[bool, str] = False):
+    """Read ``.h5ad``-formatted hdf5 file.
+
+    Parameters
+    ----------
+    filename
+        File name of data file.
+    backed : {``False``, ``True``, ``'r'``, ``'r+'``}
+        Load :class:`~anndata.AnnData` in `backed` mode instead of fully
+        loading it into memory (`memory` mode). `True` and 'r' are
+        equivalent. If you want to modify backed attributes of the AnnData
+        object, you need to choose 'r+'.
+    """
+
+    if backed:
+        # open in backed-mode
+        return AnnData(filename=filename, filemode=backed)
+    else:
+        # load everything into memory
+        d = _read_h5ad(filename=filename)
+        return AnnData(d)
+
+
+def _read_h5ad(adata: AnnData = None, filename: Optional[Union[Path, str]] = None, mode: str = None):
+    """Return a dict with arrays for initializing AnnData.
+
+    Parameters
+    ----------
+    filename
+        Defaults to the objects filename if ``None``.
+    """
+    if filename is None and (adata is None or adata.filename is None):
+        raise ValueError('Need either a filename or an AnnData object with file backing')
+
+    # we need to be able to call the function without reference to self when
+    # not reading in backed mode
+    backed = False
+    if filename is None:
+        backed = True if mode is None else mode
+        filename = adata.filename
+
+    filename = str(filename)  # allow passing pathlib.Path objects
+    d = {}
+    if backed:
+        f = adata.file._file
+    else:
+        f = h5py.File(filename, 'r')
+    for key in f.keys():
+        if backed and key in AnnData._BACKED_ATTRS:
+            d[key] = None
+        else:
+            _read_key_value_from_h5(f, d, key)
+    # backwards compat: save X with the correct name
+    if 'X' not in d:
+        if backed == 'r+':
+            for key in AnnData._H5_ALIASES['X']:
+                if key in d:
+                    del f[key]
+                    f.create_dataset('X', data=d[key])
+                    break
+    # backwards compat: store sparse matrices properly
+    csr_keys = [key.replace('_csr_data', '')
+                for key in d if '_csr_data' in key]
+    for key in csr_keys:
+        d = load_sparse_csr(d, key=key)
+    if not backed:
+        f.close()
+    return d
+
+
+def _read_key_value_from_h5(f, d, key, key_write=None):
+    if key_write is None: key_write = key
+    if isinstance(f[key], h5py.Group):
+        d[key_write] = OrderedDict() if key == 'uns' else {} 
+        for k in f[key].keys():
+            _read_key_value_from_h5(f, d[key_write], key + '/' + k, k)
+        return
+    # the '()' means 'load everything into memory' (by contrast, ':'
+    # only works if not reading a scalar type)
+    value = f[key][()]
+
+    def postprocess_reading(key, value):
+        if value.ndim == 1 and len(value) == 1:
+            value = value[0]
+        if hasattr(value, 'dtype') and value.dtype.kind == 'S':
+            value = value.astype(str)
+            # backwards compat:
+            # recover a dictionary that has been stored as a string
+            if len(value) > 0:
+                if value[0] == '{' and value[-1] == '}': value = eval(value)
+        # transform byte strings in recarrays to unicode strings
+        # TODO: come up with a better way of solving this, see also below
+        if (key not in AnnData._H5_ALIASES['obs']
+            and key not in AnnData._H5_ALIASES['var']
+            and key != 'raw.var'
+            and not isinstance(value, dict) 
+            and hasattr(value, 'dtype') and value.dtype.names is not None):
+            new_dtype = [((dt[0], 'U{}'.format(int(int(dt[1][2:])/4)))
+                          if dt[1][1] == 'S' else dt) for dt in value.dtype.descr]
+            value = value.astype(new_dtype)
+        return key, value
+
+    key, value = postprocess_reading(key, value)
+    d[key_write] = value
+    return
+
+
+def load_sparse_csr(d, key='X'):
+    from scipy.sparse.csr import csr_matrix
+    key_csr = key + '_csr'
+    d[key] = csr_matrix((d[key_csr + '_data'],
+                         d[key_csr + '_indices'],
+                         d[key_csr + '_indptr']),
+                        shape=d[key_csr + '_shape'])
+    del_sparse_matrix_keys(d, key_csr)
+    return d
+
+
+def del_sparse_matrix_keys(mapping, key_csr):
+    del mapping[key_csr + '_data']
+    del mapping[key_csr + '_indices']
+    del mapping[key_csr + '_indptr']
+    del mapping[key_csr + '_shape']
---
+++ packages/anndata-modified/anndata_modified-0.6.4.post2-py3-none-any.whl/anndata/readwrite/write.py
@@ -0,0 +1,181 @@
+import os
+import warnings
+from collections import Mapping
+from pathlib import Path
+from typing import Union
+import pandas as pd
+import numpy as np
+from scipy.sparse import issparse
+import logging as logg
+
+from ..base import AnnData
+from .. import h5py
+
+
+def write_csvs(dirname: Union[Path, str], adata: AnnData, skip_data: bool = True, sep: str = ','):
+    """See :meth:`~anndata.AnnData.write_csvs`.
+    """
+    dirname = Path(dirname)
+    if dirname.suffix == '.csv':
+        dirname = dirname.with_suffix('')
+    # write the following at warning level, it's very important for the users
+    # logg.info('writing \'.csv\' files to', dirname)
+    if not dirname.is_dir():
+        dirname.mkdir(parents=True, exist_ok=True)
+    dir_uns = dirname / 'uns'
+    if not dir_uns.is_dir():
+        dir_uns.mkdir(parents=True, exist_ok=True)
+    d = dict(
+        obs=adata._obs,
+        var=adata._var,
+        obsm=adata._obsm.to_df(),
+        varm=adata._varm.to_df(),
+    )
+    if not skip_data:
+        d['X'] = pd.DataFrame(
+            adata._X.toarray() if issparse(adata._X) else adata._X)
+    d_write = {**d, **adata._uns}
+    not_yet_raised_sparse_warning = True
+    for key, value in d_write.items():
+        if issparse(value):
+            if not_yet_raised_sparse_warning:
+                warnings.warn('Omitting to write sparse annotation.')
+                not_yet_raised_sparse_warning = False
+            continue
+        filename = dirname
+        if key not in {'X', 'var', 'obs', 'obsm', 'varm'}:
+            filename = dir_uns
+        filename /= '{}.csv'.format(key)
+        df = value
+        if not isinstance(value, pd.DataFrame):
+            value = np.array(value)
+            if np.ndim(value) == 0:
+                value = value[None]
+            try:
+                df = pd.DataFrame(value)
+            except Exception as e:
+                warnings.warn('Omitting to write {!r}.'.format(key), type(e))
+                continue
+        df.to_csv(
+            filename, sep=sep,
+            header=True if key in {'obs', 'var', 'obsm', 'varm'} else False,
+            index=True if key in {'obs', 'var'} else False,
+        )
+
+
+def write_loom(filename: Union[Path, str], adata: AnnData):
+    filename = str(filename)  # allow passing Path object
+    row_attrs = adata.var.to_dict('list')
+    row_attrs['var_names'] = adata.var_names.values
+    col_attrs = adata.obs.to_dict('list')
+    col_attrs['obs_names'] = adata.obs_names.values
+    X = adata.X.T
+    if issparse(X):
+        logg.info(
+            '... writing to \'.loom\' file densifies sparse matrix')
+        X = X.tocoo()
+    from loompy import create
+    if os.path.exists(filename):
+        os.remove(filename)
+    create(filename, X, row_attrs=row_attrs, col_attrs=col_attrs)
+
+
+def _write_h5ad(filename: Union[Path, str], adata: AnnData, **kwargs):
+    filename = str(filename)  # allow passing pathlib.Path objects
+    if not filename.endswith(('.h5', '.h5ad')):
+        raise ValueError('Filename needs to end with \'.h5ad\'.')
+    if adata.isbacked:
+        # close so that we can reopen below
+        adata.file.close()
+    # create directory if it doesn't exist
+    dirname = os.path.dirname(filename)
+    if dirname != '' and not os.path.exists(dirname):
+        os.makedirs(dirname)
+    d = adata._to_dict_fixed_width_arrays()
+    # we're writing to a different location than the backing file
+    # - load the matrix into the memory...
+    if adata.isbacked and filename != adata.filename:
+        d['X'] = adata.X[:]
+    # need to use 'a' if backed, otherwise we loose the backed objects
+    with h5py.File(filename, 'a' if adata.isbacked else 'w') as f:
+        for key, value in d.items():
+            _write_key_value_to_h5(f, key, value, **kwargs)
+    if adata.isbacked:
+        adata.file.open(filename, 'r+')
+
+
+def _write_key_value_to_h5(f, key, value, **kwargs):
+    if isinstance(value, Mapping):
+        for k, v in value.items():
+            if not isinstance(k, str):
+                warnings.warn('dict key {} transformed to str upon writing to h5,'
+                              'using string keys is recommended'
+                              .format(k))
+            _write_key_value_to_h5(f, key + '/' + str(k), v, **kwargs)
+        return
+
+    def preprocess_writing(value):
+        if value is None:
+            return value
+        elif issparse(value):
+            return value
+        elif isinstance(value, dict):
+            # old hack for storing dicts, is never reached
+            # in the current implementation, can be removed in the future
+            value = np.array([str(value)])
+        else:
+            # make sure value is an array
+            value = np.array(value)
+            # hm, why that?
+            if value.ndim == 0: value = np.array([value])
+        # make sure string format is chosen correctly
+        if value.dtype.kind == 'U': value = value.astype(np.string_)
+        return value
+
+    value = preprocess_writing(value)
+
+    # for some reason, we need the following for writing string arrays
+    if key in f.keys() and value is not None: del f[key]
+
+    # ignore arrays with empty dtypes
+    if value is None or not value.dtype.descr:
+        return
+    try:
+        if key in set(f.keys()):
+            is_valid_group = isinstance(f[key], h5py.Group) \
+                and f[key].shape == value.shape \
+                and f[key].dtype == value.dtype \
+                and not isinstance(f[key], h5py.SparseDataset)
+            if not is_valid_group and not issparse(value):
+                f[key][()] = value
+                return
+            else:
+                del f[key]
+        f.create_dataset(key, data=value, **kwargs)
+    except TypeError:
+        # try writing as byte strings
+        try:
+            if value.dtype.names is None:
+                if key in set(f.keys()):
+                    if (f[key].shape == value.shape
+                            and f[key].dtype == value.dtype):
+                        f[key][()] = value.astype('S')
+                        return
+                    else:
+                        del f[key]
+                f.create_dataset(key, data=value.astype('S'), **kwargs)
+            else:
+                new_dtype = [(dt[0], 'S{}'.format(int(dt[1][2:])*4))
+                             for dt in value.dtype.descr]
+                if key in set(f.keys()):
+                    if (f[key].shape == value.shape
+                            and f[key].dtype == value.dtype):
+                        f[key][()] = value.astype(new_dtype)
+                        return
+                    else:
+                        del f[key]
+                f.create_dataset(
+                    key, data=value.astype(new_dtype), **kwargs)
+        except Exception as e:
+            warnings.warn('Could not save field with key = "{}" '
+                          'to hdf5 file.'.format(key))

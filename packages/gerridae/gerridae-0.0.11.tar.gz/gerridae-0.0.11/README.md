cd86608a22ebc1ff1d3584447dc3d9b3e1e1c197
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:05 +0000

    Add package gerridae/0.0.11/gerridae-0.0.11.tar.gz

---
+++ packages/gerridae/gerridae-0.0.11.tar.gz/gerridae-0.0.11/README.md
@@ -0,0 +1,13 @@
+# this is new spider framework gerridae
+
+> 学习之作, 参考[ruia](https://github.com/howie6879/ruia)
+
+## v0.1.0目标设计
+
+> 2019-07-06
+
+- [x] 完成基本`spider`, `item`设定
+- [ ] 满足`comet`基本爬取设定
+- [x] 支持`css selector`方式
+
+
---
+++ packages/gerridae/gerridae-0.0.11.tar.gz/gerridae-0.0.11/tests/test_spider.py
@@ -0,0 +1,21 @@
+"""
+test the integer field
+"""
+
+from gerridae import Item, TextField, Spider
+
+
+class BaiduItem(Item):
+    name = TextField(css_select='#su')
+
+
+class BaiduSpider(Spider):
+    start_urls = ['https://www.baidu.com']
+
+    def parse(self, response):
+        result = BaiduItem.get_item(html=response.text)
+        assert result
+
+
+def test_spider():
+    BaiduSpider.start()

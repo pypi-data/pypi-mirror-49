0e9cb2bfc67749ebfad941019b09c281371ec3f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:17 +0000

    Add package seamm-ff-util/0.2.1/seamm_ff_util-0.2.1.tar.gz

---
+++ packages/seamm-ff-util/seamm_ff_util-0.2.1.tar.gz/seamm_ff_util-0.2.1/seamm_ff_util/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""Top-level package for SEAMM_FF_Util."""
+
+__author__ = """Paul Saxe"""
+__email__ = 'psaxe@molssi.org'
+__version__ = '0.2.1'
+
+from seamm_ff_util.forcefield import Forcefield  # noqa: F401
+from seamm_ff_util.ff_assigner import FFAssigner  # noqa: F401

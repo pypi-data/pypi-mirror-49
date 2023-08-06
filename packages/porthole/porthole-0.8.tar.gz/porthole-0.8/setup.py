7ddf22ce9f86e71c755af933ba6a1ebfcf13b316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:36 +0000

    Add package porthole/0.8/porthole-0.8.tar.gz

---
+++ packages/porthole/porthole-0.8.tar.gz/porthole-0.8/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='porthole',
+      version='0.8',
+      description='An automated reporting package.',
+      author='Billy McMonagle',
+      author_email='speedyturkey@gmail.com',
+      url='https://github.com/speedyturkey/porthole',
+      packages=['porthole'],
+      python_requires='>3.6',
+      install_requires=[
+                        'pymysql',
+                        'psycopg2-binary',
+                        'xlsxwriter',
+                        'SQLAlchemy',
+                        'pytz'
+                    ],
+      extras_require={
+        'AWS': ["boto3"]
+      },
+      zip_safe=False)

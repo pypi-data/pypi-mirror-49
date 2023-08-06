0878836a7d63bb25508cf897bde723ff41282aa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:06 +0000

    Add package onegov.town/1.15.10/onegov.town-1.15.10-py3-none-any.whl

---
+++ packages/onegov.town/onegov.town-1.15.10-py3-none-any.whl/onegov/town/theme/town_theme.py
@@ -0,0 +1,27 @@
+from onegov.core.utils import module_path
+from onegov.org.theme import OrgTheme
+
+
+# options editable by the user
+user_options = {
+    'primary-color': '#006fba',
+}
+
+
+class TownTheme(OrgTheme):
+    name = 'onegov.town.foundation'
+
+    # don't touch this number, it's incremented using bumpversion, so every
+    # release will automatically trigger a rebuild of the theme
+    version = '1.15.10'
+
+    @property
+    def post_imports(self):
+        return super().post_imports + [
+            'town'
+        ]
+
+    @property
+    def extra_search_paths(self):
+        base_paths = super().extra_search_paths
+        return [module_path('onegov.town.theme', 'styles')] + base_paths

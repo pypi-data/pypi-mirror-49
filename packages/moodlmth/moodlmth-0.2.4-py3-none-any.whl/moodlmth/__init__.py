28954311bb094894949b7b5e814de57c1712faa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:49 +0000

    Add package moodlmth/0.2.4/moodlmth-0.2.4-py3-none-any.whl

---
+++ packages/moodlmth/moodlmth-0.2.4-py3-none-any.whl/moodlmth/__init__.py
@@ -0,0 +1,16 @@
+__author__ = "Arijit Basu"
+__email__ = "sayanarijit@gmail.com"
+__homepage__ = "https://github.com/sayanarijit/moodlmth"
+__description__ = "Convert raw HTML pages into python source code"
+__version__ = "v0.2.4"
+__license__ = "MIT"
+__all__ = [
+    "__author__",
+    "__email__",
+    "__homepage__",
+    "__description__",
+    "__version__",
+    "__license__",
+    "cli",
+    "converter",
+]

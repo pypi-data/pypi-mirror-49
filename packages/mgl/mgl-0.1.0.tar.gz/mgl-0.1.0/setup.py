7e9ca52b51f16ee1f9eeea573d355e325c78ad67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:04 +0000

    Add package mgl/0.1.0/mgl-0.1.0.tar.gz

---
+++ packages/mgl/mgl-0.1.0.tar.gz/mgl-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import Extension, setup
+
+# ext = Extension(
+#     name='mgl',
+#     sources=['mgl.cpp'],
+#     extra_compile_args=['-fpermissive'],
+# )
+
+setup(
+    name='mgl',
+    version='0.1.0',
+    # ext_modules=[ext],
+)

7b3e06357f2e3a21ecc521b67576c4755d623419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:18 +0000

    Add package pyro-ppl/0.3.4/pyro-ppl-0.3.4.tar.gz

---
+++ packages/pyro-ppl/pyro-ppl-0.3.4.tar.gz/pyro-ppl-0.3.4/pyro/params/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import, division, print_function
+
+from pyro.params.param_store import module_from_param_with_module_name, param_with_module_name, user_param_name
+
+__all__ = [
+    "module_from_param_with_module_name",
+    "param_with_module_name",
+    "user_param_name",
+]

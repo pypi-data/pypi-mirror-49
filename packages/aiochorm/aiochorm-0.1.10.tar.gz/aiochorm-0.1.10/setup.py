01b62b5d1fa1615872c527314d00adb57bb862c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:49 +0000

    Add package aiochorm/0.1.10/aiochorm-0.1.10.tar.gz

---
+++ packages/aiochorm/aiochorm-0.1.10.tar.gz/aiochorm-0.1.10/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+README = '''
+Async version of infi.clickhouse_orm.
+
+More information available on read the doc: https://github.com/qvp/aiochorm
+'''
+
+setup(
+    name='aiochorm',
+    version='0.1.10',
+    packages=['aiochorm'],
+    description='Async version of infi.clickhouse_orm.',
+    long_description=README,
+    author='Alexander Kuzmenko',
+    author_email='mail@alexanderkuzmenko.com',
+    url='https://github.com/qvp/aiochorm',
+    download_url='',
+    license='MIT',
+    install_requires=[]
+)

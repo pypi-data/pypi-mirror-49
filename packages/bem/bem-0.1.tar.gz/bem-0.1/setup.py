b5c77a39f1180cca0fac316d2b7e679e505837d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:15 +0000

    Add package bem/0.1/bem-0.1.tar.gz

---
+++ packages/bem/bem-0.1.tar.gz/bem-0.1/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='bem',
+      version='0.1',
+      description='',
+    #   long_description=open('README.rst').read(),
+      author='Solène Ulmer-Moll',
+      author_email='solene.ulmer-moll@astro.up.pt',
+      license='MIT',
+      url='https://github.com/soleneulmer/bem',
+      packages=['bem'],
+     )

4420163689a329990f065236427380cebed78fa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:08 +0000

    Add package cima.goes/1.1b50/cima.goes-1.1b50.tar.gz

---
+++ packages/cima.goes/cima.goes-1.1b50.tar.gz/cima.goes-1.1b50/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import find_namespace_packages
+from distutils.core import setup
+
+setup(
+    name='cima.goes',
+    version='1.1.beta50',
+    description='GOES-16 File Processing',
+    author='Fido Garcia',
+    author_email='garciafido@gmail.com',
+    package_dir={'': 'src'},
+    url='https://github.com/garciafido/cima-goes',
+    packages=find_namespace_packages(where='src'),
+    include_package_data=True,
+    license='MIT',
+    package_data={'': ['*.json', '*.cpt']},
+    data_files = [("", ["LICENSE"])],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

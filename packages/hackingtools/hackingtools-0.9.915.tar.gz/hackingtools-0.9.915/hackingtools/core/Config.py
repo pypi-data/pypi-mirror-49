b2a3b9c5f627429635c9a916ff924512e36fa503
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:18 +0000

    Add package hackingtools/0.9.915/hackingtools-0.9.915.tar.gz

---
+++ packages/hackingtools/hackingtools-0.9.915.tar.gz/hackingtools-0.9.915/hackingtools/core/Config.py
@@ -0,0 +1,103 @@
+import os
+import json
+
+global config
+config = {}
+
+def __readConfig__():
+    global config
+    config = {}
+    with open(os.path.join(os.path.dirname(__file__) , 'config.json')) as json_data_file:
+        config = json.load(json_data_file)
+
+def __save_config__(new_conf):
+    with open(os.path.join(os.path.dirname(__file__) , 'config.json'), 'w', encoding='utf8') as outfile:  
+        json.dump(new_conf, outfile, indent=4, ensure_ascii=False)
+
+def __createModuleTemplateConfig__(module_name):
+    global config
+    config_tmp = {}
+    with open(os.path.join(os.path.dirname(__file__) , 'config.json')) as json_data_file:
+        config_tmp = json.load(json_data_file)
+    new_config = {
+        "__gui_label__" : "_MODULE_GUI_LABEL_",
+        "_comment" : "Rename templates if have to use: (remove underscore) 'django_form_main_function' and 'django_form_module_function'",
+        "_django_form_main_function_" : {
+            "_HTML_FIELD_NAME_" : {
+                "__id__" : "_HTML_FIELD_NAME_",
+                "__type__" : "_HTML_INPUT_TYPE_",
+                "__className__" : "_HTML_INPUT_CLASS_",
+                "label_desc" : "_DESCRIPTION_LABEL_",
+                "required" : "_IF_REQUIRED_",
+                "value" : "_INPUT_VALUE_",
+                "loading_text" : "_INPUT_LOADING_TEXT_"
+            }
+        },
+        "_django_form_module_function_" : {
+            "_PUBLIC_FUNCTION_" : {
+                "__function__" : "_FUNCTION_CALLABLE_NAME_",
+                "__return__" : "_IF_RETURNS_LIKE_TEXT_",
+                "_PARAM_TO_USE_IN_VIEWS_PY_" : {
+                    "__id__" : "_HTML_FIELD_NAME_",
+                    "__type__" : "_HTML_INPUT_TYPE_",
+                    "__className__" : "_HTML_INPUT_CLASS_",
+                    "label_desc" : "_DESCRIPTION_LABEL_",
+                    "value" : "_INPUT_VALUE_",
+                    "required" : "_IF_REQUIRED_",
+                    "returnable_modules_functions" : {
+                        "_MODULE_CALL_FOR_" : [
+                            "_THAT_MODULES_FUNCTION"
+                        ]
+                    }
+                }
+            }
+        }
+    }
+    new_key_module = 'ht_{mod}'.format(mod=module_name)
+    config_tmp['modules'][new_key_module] = new_config
+    __save_config__(config_tmp)
+
+def __look_for_changes__():
+    import json
+
+    global config
+    config_tmp = {}
+    with open(os.path.join(os.path.dirname(__file__) , 'config.json')) as json_data_file:
+        config_tmp = json.load(json_data_file)
+
+    if not sorted(config.items()) == sorted(config_tmp.items()):
+        config = config_tmp
+        return True
+    return False
+
+def getConfig(parentKey, key, subkey=None, extrasubkey=None):
+    try:
+        if extrasubkey:
+            try:
+                if subkey:
+                    try:
+                        return config[parentKey][key][subkey][extrasubkey]
+                    except:
+                        return
+            except:
+                return
+        else:
+            try:
+                if subkey:
+                    try:
+                        return config[parentKey][key][subkey]
+                    except:
+                        return
+            except:
+                return
+        return config[parentKey][key]
+    except:
+        return
+
+def getApiKey(apiName):
+    try:
+        return config['core']['__API_KEY__'][apiName]
+    except:
+        return 'API {n} not found'.format(n=apiName)
+
+__readConfig__()

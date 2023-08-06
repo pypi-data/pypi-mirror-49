2b5148839ac86020379b7b78d98a2ceba6c84c4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:03:31 +0000

    Add package PushTool/1.4/PushTool-1.4.tar.gz

---
+++ packages/PushTool/PushTool-1.4.tar.gz/PushTool-1.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+setup(
+        name='PushTool',     # 包名字
+        version='1.4',   # 包版本
+        description='push tool can help your work',   # 简单描述
+        author='Mryan2005',  # 作者
+        author_email='A2564011261@163.com',  # 作者邮箱
+        url='https://github.com/SuperSystemStudio/push.bat.git',      # 包的主页
+        license='MIT License',
+        packages=find_packages(),
+        include_package_data=True,
+        zip_safe=True,
+        entry_points={
+            'console_scripts':[
+                'push = pushtool.push:main'
+            ]
+        },
+)

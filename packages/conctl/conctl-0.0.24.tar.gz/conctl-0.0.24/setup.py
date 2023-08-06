5f6a623031769536c8144c3836c9404a598ec14a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:57 +0000

    Add package conctl/0.0.24/conctl-0.0.24.tar.gz

---
+++ packages/conctl/conctl-0.0.24.tar.gz/conctl-0.0.24/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+setup(
+    name='conctl',
+    version='0.0.24',
+    url='https://github.com/joedborg/conctl',
+    license='Apache License 2.0',
+    author='Joe Borg',
+    author_email='joe@josephb.org',
+    description='Drive Containerd and Docker from one CLI',
+    packages=['conctl', 'conctl/bin'],
+    install_requires=[
+        'click',
+    ],
+    entry_points={
+        'console_scripts': [
+            'conctl=conctl.bin.conctl:cli',
+        ],
+    }
+)

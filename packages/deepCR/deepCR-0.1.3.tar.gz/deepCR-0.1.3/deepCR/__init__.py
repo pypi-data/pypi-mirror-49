d1179f41e8bdfae40e56d5ed0d505f29d495cb08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:30 +0000

    Add package deepCR/0.1.3/deepCR-0.1.3.tar.gz

---
+++ packages/deepCR/deepCR-0.1.3.tar.gz/deepCR-0.1.3/deepCR/__init__.py
@@ -0,0 +1,16 @@
+"""
+deepCR: Deep Learning Based Cosmic Ray Removal for Astronomical Images
+
+Apply a learned DL model to a 2d numpy array to remove
+cosmic rays.
+
+Code which accompanies the paper: Zhang & Bloom (2019)
+
+See https://github.com/profjsb/deepCR
+
+"""
+from deepCR.model import deepCR
+
+__all__ = model.__all__
+
+__version__ = '0.1.3'

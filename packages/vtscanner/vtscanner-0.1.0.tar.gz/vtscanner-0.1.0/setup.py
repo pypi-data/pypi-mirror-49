8ccabb6afc309b8bb10e016f55ad18f76f593ba4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:07 +0000

    Add package vtscanner/0.1.0/vtscanner-0.1.0.tar.gz

---
+++ packages/vtscanner/vtscanner-0.1.0.tar.gz/vtscanner-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+from distutils.core import setup
+
+setup (
+    name = 'vtscanner',
+    version = '0.1.0',
+    description = 'VT scanner',
+    author='Parth Patel',
+    author_email='parthpatel.pp802@gmail.com',
+    url="http://pypi.python.org/pypi/vtscanner",
+)

be60803467a41334ed2b756dfb644cddc6299496
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:42 +0000

    Add package guildai/0.6.5/guildai-0.6.5-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/guildai/guildai-0.6.5-cp36-cp36m-manylinux1_x86_64.whl/guildai-0.6.5.data/purelib/guild/log.py
@@ -0,0 +1,88 @@
+# Copyright 2017-2019 TensorHub, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import
+from __future__ import division
+
+import logging
+import logging.config
+
+__last_init_kw = None
+
+class ConsoleLogHandler(logging.StreamHandler):
+
+    DEFAULT_FORMATS = {
+        "_": "%(levelname)s: %(message)s",
+        "DEBUG": "%(levelname)s: [%(name)s] %(message)s",
+        "INFO": "%(message)s",
+    }
+
+    def __init__(self, formats=None):
+        super(ConsoleLogHandler, self).__init__()
+        formats = formats or self.DEFAULT_FORMATS
+        self._formatters = {
+            level: logging.Formatter(fmt)
+            for level, fmt in formats.items()
+        }
+
+    def format(self, record):
+        fmt = (self._formatters.get(record.levelname) or
+               self._formatters.get("_"))
+        if fmt:
+            return fmt.format(record)
+        else:
+            return super(ConsoleLogHandler, self).format(record)
+
+def init_logging(level=logging.INFO, formats=None):
+    _preempt_logging_mods()
+    console_handler = {
+        "class": "guild.log.ConsoleLogHandler",
+        "formats": formats,
+    }
+    logging.config.dictConfig({
+        "version": 1,
+        "handlers": {
+            "console": console_handler
+        },
+        "root": {
+            "level": level,
+            "handlers": ["console"]
+        },
+        "disable_existing_loggers": False,
+    })
+    globals()["__last_init_kw"] = dict(level=level, formats=formats)
+
+def _preempt_logging_mods():
+    """Preempt known logging mods.
+
+    Some modules modify logging without respecting previous config
+    (e.g. absl.logging). This function preempts those changes so that
+    our config is applied afterward.
+    """
+    try:
+        import absl.logging
+    except Exception:
+        pass
+
+def disable_noisy_loggers(level=logging.INFO):
+    if level <= logging.DEBUG:
+        _set_logger_level(["chardet"], logging.INFO)
+
+def _set_logger_level(pkgs, level):
+    for pkg in pkgs:
+        log = logging.getLogger(pkg)
+        log.setLevel(level)
+
+def current_settings():
+    return __last_init_kw

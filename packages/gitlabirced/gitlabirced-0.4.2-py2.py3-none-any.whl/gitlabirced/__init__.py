868076a2412fa1280524f9caf0520b53d815ab47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:03 +0000

    Add package gitlabirced/0.4.2/gitlabirced-0.4.2-py2.py3-none-any.whl

---
+++ packages/gitlabirced/gitlabirced-0.4.2-py2.py3-none-any.whl/gitlabirced/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for GitlabIRCed."""
+
+__author__ = """Pedro Alvarez Piedehierro"""
+__email__ = 'pedro@alvarezpiedehierro.com'
+__version__ = '0.4.2'

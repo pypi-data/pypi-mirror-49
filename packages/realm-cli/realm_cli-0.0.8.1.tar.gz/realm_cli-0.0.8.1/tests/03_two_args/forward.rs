adcb86581b6dd17fcf2a64b24bb206a3017745ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:58 +0000

    Add package realm-cli/0.0.8.1/realm_cli-0.0.8.1.tar.gz

---
+++ packages/realm-cli/realm_cli-0.0.8.1.tar.gz/realm_cli-0.0.8.1/tests/03_two_args/forward.rs
@@ -0,0 +1,11 @@
+pub fn magic(req: realm::Request) -> realm::Result {
+    let mut input = realm::request_config::RequestConfig::new(req)?;
+    match input.path.as_str() {
+        "/" => {
+            let i = input.get("i", false)?;
+            let s = input.get("s", false)?;
+            crate::routes::index::layout(&input.req, i, s)
+        },
+        _ => unimplemented!(),
+    }
+}
---
+++ packages/realm-cli/realm_cli-0.0.8.1.tar.gz/realm_cli-0.0.8.1/tests/04_order/reverse.rs
@@ -0,0 +1,23 @@
+use realm::utils::{Maybe, url2path};
+
+
+pub fn ab_c() -> String {
+    "/ab/c/".to_owned()
+}
+
+pub fn ab() -> String {
+    "/ab/".to_owned()
+}
+
+pub fn index() -> String {
+    "/".to_owned()
+}
+
+pub fn foo() -> String {
+    "/foo/".to_owned()
+}
+
+pub fn bar() -> String {
+    "/bar/".to_owned()
+}
+

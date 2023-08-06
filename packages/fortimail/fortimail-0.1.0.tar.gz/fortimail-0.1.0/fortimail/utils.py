d139b68a498d71aeb649e18c57e996170d53d596
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:21 +0000

    Add package fortimail/0.1.0/fortimail-0.1.0.tar.gz

---
+++ packages/fortimail/fortimail-0.1.0.tar.gz/fortimail-0.1.0/fortimail/utils.py
@@ -0,0 +1,26 @@
+from fortimail import exceptions
+
+
+def raise_for_error(status_code, message=None, errors=None):
+    '''
+    Helper method to raise exceptions based on the status code of a response
+    received back from Fortimail.
+
+    Arguments:
+        status_code {int} -- Status code received in a response from Fortimail
+
+    Keyword Arguments:
+        message {str} -- Optional message (default: {None})
+        errors {list | str} -- Optional errors (default: {None})
+
+    Raises:
+        exceptions.Forbidden
+        exceptions.UnexpectedError
+        exceptions.NotFound
+    '''
+    if status_code == 403:
+        raise exceptions.Forbidden(message, errors=errors)
+    elif status_code == 404:
+        raise exceptions.NotFound(message, errors=errors)
+    else:
+        raise exceptions.UnexpectedError(message)

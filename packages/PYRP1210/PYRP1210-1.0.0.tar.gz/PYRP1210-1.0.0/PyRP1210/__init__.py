974d55cade7511f6f5e1265965f95a3ce1185030
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:44 +0000

    Add package PYRP1210/1.0.0/PYRP1210-1.0.0.tar.gz

---
+++ packages/PYRP1210/PYRP1210-1.0.0.tar.gz/PYRP1210-1.0.0/PyRP1210/__init__.py
@@ -0,0 +1,12 @@
+
+from PyRP1210.RP1210 import *
+from PyRP1210.RP1210Functions import *
+from PyRP1210.RP1210Select import *
+from PyQt5 import *
+name = "PyRP1210"
+# from RP1210 import *
+# from RP1210Functions import *
+# from RP1210Select import *
+
+# name = "PyRP1210"
+

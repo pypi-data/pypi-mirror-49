c9df70130dd2804f407ccdc133dd8c36d903af4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:04 +0000

    Add package Yodine/0.1.2.1/Yodine-0.1.2.1.tar.gz

---
+++ packages/Yodine/Yodine-0.1.2.1.tar.gz/Yodine-0.1.2.1/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.rst").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="Yodine",
+    version="0.1.2.1",
+    description="",
+    license="MIT",
+    author="Gustavo6046",
+    packages=find_packages(),
+    install_requires=['yodine_data>=0.1.0'] + [r for r in open('requirements.txt').read().split('\n') if r],
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+    ]
+)

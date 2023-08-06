1064b8aa9c69f932fd5aa66b94b25aa572a5d632
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:21 +0000

    Add package bareasgi/2.0.2/bareasgi-2.0.2.tar.gz

---
+++ packages/bareasgi/bareasgi-2.0.2.tar.gz/bareasgi-2.0.2/src/bareasgi/application.py
@@ -0,0 +1,147 @@
+from typing import Mapping, List, Any, Optional, MutableMapping
+import logging
+from baretypes import (
+    Scope,
+    ASGIInstance,
+    HttpRouter,
+    WebSocketRouter,
+    StartupHandler,
+    ShutdownHandler,
+    HttpResponse,
+    HttpMiddlewareCallback
+)
+from .instance import Instance
+from .basic_router import BasicHttpRouter, BasicWebSocketRouter
+from bareutils.streaming import text_writer
+
+DEFAULT_NOT_FOUND_RESPONSE: HttpResponse = (404, [(b'content-type', b'text/plain')], text_writer('Not Found'))
+
+logger = logging.getLogger(__name__)
+
+
+class Application:
+    """A class to hold the application.
+
+    For example:
+
+    .. code-block:: python
+
+        from bareasgi import (
+            Application,
+            Scope,
+            Info,
+            RouteMatches,
+            Content,
+            Reply,
+            WebSocket,
+            text_reader,
+            text_writer
+        )
+
+        async def http_request_callback(
+            scope: Scope,
+            info: Info,
+            matches: RouteMatches,
+            content: Content,
+            reply: Reply
+        ) -> None:
+            text = await text_reader(content)
+            await reply(200, [(b'content-type', b'text/plain')], text_writer('This is not a test'))
+
+        import uvicorn
+
+        app = Application()
+        app.http_router.add({'GET', 'POST', 'PUT', 'DELETE'}, '/{path}', http_request_callback)
+
+        uvicorn.run(app, port=9009)
+    """
+
+
+    def __init__(
+            self,
+            *,
+            middlewares: Optional[List[HttpMiddlewareCallback]] = None,
+            http_router: Optional[HttpRouter] = None,
+            web_socket_router: Optional[WebSocketRouter] = None,
+            startup_handlers: Optional[List[StartupHandler]] = None,
+            shutdown_handlers: Optional[List[ShutdownHandler]] = None,
+            not_found_response: Optional[HttpResponse] = None,
+            info: Optional[MutableMapping[str, Any]] = None
+    ) -> None:
+        """Construct the application
+
+        :param middlewares: Optional list of middleware callbacks.
+        :type middlewares: Optional[List[HttpMiddlewareCallback]]
+        :param http_router: Optional router to for http routes.
+        :type http_router: Optional[HttpRouter]
+        :param web_socket_router: Optional router for web routes.
+        :type web_socket_router: Optional[WebSocketRouter]
+        :param startup_handlers: Optional handlers to run at startup.
+        :type startup_handlers: Optional[List[StartupHandler]]
+        :param shutdown_handlers: Optional handlers to run at shutdown.
+        :type shutdown_handlers: Optional[List[ShutdownHandler]]
+        :param info: Optional dictionary for user data.
+        :type info: Optional[MutableMapping[str, Any]]
+        """
+        self._context: Mapping[str, Any] = {
+            'info': dict() if info is None else info,
+            'lifespan': {
+                'lifespan.startup': startup_handlers or [],
+                'lifespan.shutdown': shutdown_handlers or []
+            },
+            'http': {
+                'router': http_router or BasicHttpRouter(not_found_response or DEFAULT_NOT_FOUND_RESPONSE),
+                'middlewares': middlewares or []
+            },
+            'websocket': web_socket_router or BasicWebSocketRouter()
+        }
+
+
+    @property
+    def info(self) -> MutableMapping[str, Any]:
+        """A place to sto application specific data.
+
+        :return: A dictionary.
+        """
+        return self._context['info']
+
+
+    @property
+    def middlewares(self) -> List[HttpMiddlewareCallback]:
+        """The middlewares.
+
+        :return: A list of the middleware to apply to every route.
+        """
+        return self._context['http']['middlewares']
+
+
+    @property
+    def http_router(self) -> HttpRouter:
+        """Router for http routes
+
+        :return: The configured router.
+        """
+        return self._context['http']['router']
+
+
+    @property
+    def ws_router(self) -> WebSocketRouter:
+        """Router for WebSocket routes"""
+        return self._context['websocket']
+
+
+    @property
+    def startup_handlers(self) -> List[StartupHandler]:
+        """Handlers run at startup"""
+        return self._context['lifespan']['lifespan.startup']
+
+
+    @property
+    def shutdown_handlers(self) -> List[ShutdownHandler]:
+        """Handlers run on shutdown"""
+        return self._context['lifespan']['lifespan.shutdown']
+
+
+    def __call__(self, scope: Scope) -> ASGIInstance:
+        logger.debug('Creating instance', extra=scope)
+        return Instance(self._context, scope)
---
+++ packages/bareasgi/bareasgi-2.0.2.tar.gz/bareasgi-2.0.2/src/bareasgi/websocket_instance.py
@@ -0,0 +1,90 @@
+from typing import Optional, Union
+import logging
+from baretypes import (
+    Scope,
+    Info,
+    Send,
+    Receive,
+    WebSocket,
+    WebSocketRouter,
+    WebSocketInternalError
+)
+
+logger = logging.getLogger(__name__)
+
+
+class WebSocketImpl(WebSocket):
+
+    def __init__(self, receive: Receive, send: Send):
+        self._receive = receive
+        self._send = send
+
+
+    async def accept(self, subprotocol: Optional[str] = None) -> None:
+        response = {'type': 'websocket.accept'}
+        if subprotocol:
+            response['subprotocol'] = subprotocol
+        logger.debug('Accepting', extra=response)
+        await self._send(response)
+
+
+    async def receive(self) -> Optional[Union[bytes, str]]:
+        request = await self._receive()
+        request_type = request['type']
+        logger.debug(f'Received {request_type}', extra=request)
+
+        if request_type == 'websocket.receive':
+            return request['bytes'] if 'bytes' in request and request['bytes'] else request['text']
+        elif request_type == 'websocket.disconnect':
+            return None
+
+        logger.error(f'Failed to understand request type "{request_type}', extra=request)
+        raise WebSocketInternalError(f'Unknown type: "{request_type}"')
+
+
+    async def send(self, content: Union[bytes, str]) -> None:
+        response = {'type': 'websocket.send'}
+
+        if isinstance(content, bytes):
+            response['bytes'] = content
+        elif isinstance(content, str):
+            response['text'] = content
+        else:
+            raise ValueError('Content must be bytes or str')
+
+        logger.debug(f'Sending {response["type"]}', extra=response)
+        await self._send(response)
+
+
+    async def close(self, code: int = 1000) -> None:
+        response = {'type': 'websocket.close', 'code': code}
+        logger.debug(f'Closing with code {code}', extra=response)
+        await self._send(response)
+
+
+class WebSocketInstance:
+
+    def __init__(self, scope: Scope, route_handler: WebSocketRouter, info: Info) -> None:
+        self.scope = scope
+        self.info = info
+        self.request_handler, self.matches = route_handler(scope)
+
+
+    async def __call__(self, receive: Receive, send: Send):
+
+        request = await receive()
+        request_type = request['type']
+        logger.debug(f'Received {request_type}', extra=request)
+
+        if request_type == 'websocket.connect':
+            await self.request_handler(
+                self.scope,
+                self.info,
+                self.matches,
+                WebSocketImpl(receive, send)
+            )
+        elif request_type == 'websocket.disconnect':
+            pass
+        else:
+            logger.error(f'Failed to understand request type "{request_type}', extra=request)
+            raise WebSocketInternalError(f'Unknown request type "{request_type}')

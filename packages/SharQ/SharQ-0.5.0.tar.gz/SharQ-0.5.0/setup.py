b99a0f5ea9404c9227b22ee918f404d75a059c9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:11 +0000

    Add package SharQ/0.5.0/SharQ-0.5.0.tar.gz

---
+++ packages/SharQ/SharQ-0.5.0.tar.gz/SharQ-0.5.0/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2014 Plivo Team. See LICENSE.txt for details.
+from setuptools import setup
+
+setup(
+    name='SharQ',
+    version='0.5.0',
+    url='https://github.com/plivo/sharq',
+    author='Plivo Team',
+    author_email='hello@plivo.com',
+    packages=['sharq'],
+    package_data={
+        'sharq': ['scripts/lua/*.lua']
+    },
+    license='The MIT License (MIT)',
+    description='An API queueing system built at Plivo.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'redis==2.10.1',
+        'msgpack-python==0.4.2',
+        'rediscluster>=0.5.3'
+    ],
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)

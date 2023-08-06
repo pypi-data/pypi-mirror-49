e9212e743d852ab3cb912296c356a3bf85fef43f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:10 +0000

    Add package mkdocs-file-selector/0.1.5/mkdocs-file-selector-0.1.5.tar.gz

---
+++ packages/mkdocs-file-selector/mkdocs-file-selector-0.1.5.tar.gz/mkdocs-file-selector-0.1.5/mkdocs_file_selector/plugin.py
@@ -0,0 +1,18 @@
+import os
+import sys
+from timeit import default_timer as timer
+from datetime import datetime, timedelta
+
+from mkdocs import utils as mkdocs_utils
+from mkdocs.config import config_options, Config
+from mkdocs.plugins import BasePlugin
+
+class FileSelector(BasePlugin):
+
+    config_scheme = (
+        ('param', config_options.Type(mkdocs_utils.string_types, default='')),
+    )
+
+    def __init__(self):
+        self.enabled = True
+        self.total_time = 0

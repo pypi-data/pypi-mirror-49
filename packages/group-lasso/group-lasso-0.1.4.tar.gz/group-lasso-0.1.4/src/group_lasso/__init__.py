fb424bd507b471d19c5375d9f0406c840accf734
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:10 +0000

    Add package group-lasso/0.1.4/group-lasso-0.1.4.tar.gz

---
+++ packages/group-lasso/group-lasso-0.1.4.tar.gz/group-lasso-0.1.4/src/group_lasso/__init__.py
@@ -0,0 +1,14 @@
+"""Group lasso regularised linear models.
+"""
+
+
+__version__ = "0.1.4"
+__author__ = "Yngve Mardal Moe"
+__email__ = "yngve.m.moe@gmail.com"
+
+
+from group_lasso._group_lasso import (
+    GroupLasso,
+    LogisticGroupLasso,
+    BaseGroupLasso,
+)

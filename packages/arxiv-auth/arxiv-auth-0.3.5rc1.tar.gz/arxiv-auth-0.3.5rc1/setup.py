6387c52325017917ff644488a741f81a6ab1cce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:05:57 +0000

    Add package arxiv-auth/0.3.5rc1/arxiv-auth-0.3.5rc1.tar.gz

---
+++ packages/arxiv-auth/arxiv-auth-0.3.5rc1.tar.gz/arxiv-auth-0.3.5rc1/setup.py
@@ -0,0 +1,23 @@
+"""Install arXiv auth package."""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='arxiv-auth',
+    version='0.3.5rc1',
+    packages=[f'arxiv.{package}' for package
+              in find_packages('./arxiv', exclude=['*test*'])],
+    scripts=['bin/generate-token'],
+    install_requires=[
+        "pycountry",
+        "sqlalchemy",
+        "mysqlclient",
+        "python-dateutil",
+        "arxiv-base",
+        "pyjwt",
+        "redis==2.10.6",
+        "redis-py-cluster==1.3.6",
+        "flask-sqlalchemy"
+    ],
+    zip_safe=False
+)

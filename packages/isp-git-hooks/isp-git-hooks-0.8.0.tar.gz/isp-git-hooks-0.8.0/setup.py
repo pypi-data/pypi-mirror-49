801597a38b790cefac7324ace0073816805a4658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:16 +0000

    Add package isp-git-hooks/0.8.0/isp-git-hooks-0.8.0.tar.gz

---
+++ packages/isp-git-hooks/isp-git-hooks-0.8.0.tar.gz/isp-git-hooks-0.8.0/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['isp_git_hooks']
+
+package_data = \
+{'': ['*'], 'isp_git_hooks': ['pre_commit_hooks/*']}
+
+install_requires = \
+['pre-commit>=1.17,<2.0']
+
+setup_kwargs = {
+    'name': 'isp-git-hooks',
+    'version': '0.8.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Marcel Hekking',
+    'author_email': 'm.hekking@isprojects.nl',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)

f497fb4c735131435c174c77071b7f6e13036ae4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:37 +0000

    Add package cmd-tools/0.0.8/cmd-tools-0.0.8.tar.gz

---
+++ packages/cmd-tools/cmd-tools-0.0.8.tar.gz/cmd-tools-0.0.8/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+
+def long_long_description():
+    with open('README.md', 'r') as f:
+        desc = f.read()
+    return desc
+
+
+setup(
+    name='cmd-tools',
+    version='0.0.8',
+    description='command tools',
+    long_description=long_long_description(),
+    author='buglan',
+    author_email='1831353087@qq.com',
+    scripts=['command.py'],
+    packages=find_packages(),
+    install_requires=['click'],
+    entry_points='''
+        [console_scripts]
+        hello=command:hello
+        cmd=command:cmd
+        define=command:define
+    ''',
+    setup_requires=[
+        'pytest-runner'
+    ],
+    tests_require=[
+        'pytest',
+    ]
+
+)

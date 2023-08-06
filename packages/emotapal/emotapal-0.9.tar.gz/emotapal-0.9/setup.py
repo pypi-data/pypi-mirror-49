674e140b7f3d445cfad03c85d537c88b4bf6f987
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:56 +0000

    Add package emotapal/0.9/emotapal-0.9.tar.gz

---
+++ packages/emotapal/emotapal-0.9.tar.gz/emotapal-0.9/setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+
+
+
+setup(
+  include_package_data = True,
+  name = 'emotapal',
+  packages = ['emotapal'],   
+  version = '0.9',      
+  license='gpl-3.0',        
+  description = 'Match colors and words',   
+  author = 'Josh Ashkinaze',                   
+  author_email = 'josh.ashkinaze@gmail.com',   
+  url = 'https://github.com/josh-ashkinaze/emotapal',  
+  download_url = 'https://github.com/josh-ashkinaze/emotapal/archive/v_09.tar.gz',    
+  keywords = ['colors', 'emotions', 'psychology', 'image processing'],  
+  install_requires=[           
+      "requests", "scikit-learn", "seaborn",
+      "matplotlib", "colorthief", "google_images_download", "afinn"
+      ],
+
+  package_data={'emotapal': ['clf.pkl']},
+
+  classifiers=[
+    'Development Status :: 3 - Alpha',     
+    'Intended Audience :: Developers',     
+    'Topic :: Software Development :: Build Tools',
+    'Programming Language :: Python :: 3',      #
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+  ],
+)
+
+
+

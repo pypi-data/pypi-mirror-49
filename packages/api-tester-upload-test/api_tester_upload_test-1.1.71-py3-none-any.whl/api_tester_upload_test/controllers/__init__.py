22a322b6860f71737ce273491e2f36aff6a10522
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:04:01 +0000

    Add package api-tester-upload-test/1.1.71/api_tester_upload_test-1.1.71-py3-none-any.whl

---
+++ packages/api-tester-upload-test/api_tester_upload_test-1.1.71-py3-none-any.whl/api_tester_upload_test/controllers/__init__.py
@@ -0,0 +1,12 @@
+__all__ = [
+    'base_controller',
+    'authentication_operations_controller',
+    'folders_operations_controller',
+    'trash_operations_controller',
+    'history_operations_controller',
+    'share_operations_controller',
+    'administrators_operations_controller',
+    'file_operations_controller',
+    'create_account_controller',
+    'user_operations_controller',
+]

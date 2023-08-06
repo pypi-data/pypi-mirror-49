706e14fa5f2fc3e62c94c1fe1b724af63acf457f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:50 +0000

    Add package OMEMO/0.10.5/OMEMO-0.10.5.tar.gz

---
+++ packages/OMEMO/OMEMO-0.10.5.tar.gz/OMEMO-0.10.5/omemo/__init__.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import
+
+from .version import __version__
+
+import sys
+
+from . import backends
+from . import implementations
+from . import promise
+from . import util
+from .defaultotpkpolicy import DefaultOTPKPolicy
+from .extendeddoubleratchet import make as make_ExtendedDoubleRatchet
+from .extendedpublicbundle import ExtendedPublicBundle
+from .otpkpolicy import OTPKPolicy
+from .sessionmanager import SessionManager
+from .state import make as make_State
+from .storage import Storage
+from .x3dhdoubleratchet import make as make_X3DHDoubleRatchet
+
+if sys.version_info[0] == 3:
+    from .sessionmanagerasyncio import SessionManagerAsyncIO

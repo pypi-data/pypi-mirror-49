f7fe32fc957f926462c366b5f962882d011c808f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:14 +0000

    Add package py139813/0.2dev/py139813-0.2dev.tar.gz

---
+++ packages/py139813/py139813-0.2dev.tar.gz/py139813-0.2dev/py139813/riazi2.py
@@ -0,0 +1,5 @@
+def multiply(x,y):
+    return x*y
+
+def power(x,y):
+    return x**y
---
+++ packages/py139813/py139813-0.2dev.tar.gz/py139813-0.2dev/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name = 'py139813',
+    version = '0.2dev',
+    packages = ['py139813',],
+    license = 'UT',
+    long_description = open("README.txt").read(),
+    )

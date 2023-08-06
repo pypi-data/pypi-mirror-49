6c0da29fe3188cdda5c012b04c8b53066d437e26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:24 +0000

    Add package rqmts/1.0.0/rqmts-1.0.0-py3-none-any.whl

---
+++ packages/rqmts/rqmts-1.0.0-py3-none-any.whl/rqmts/parsing.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import pkg_resources
+
+def _import(imports):
+ modules = {}
+ for module in imports:
+    try:
+        modules[module] = __import__(module)
+    except ImportError:
+        print ("Error importing", module)
+ return modules
+
+def fetch(package):
+ return pkg_resources.get_distribution(package).project_name, pkg_resources.get_distribution(package).version
+
+def parse(code):
+    modules = []
+    code = code.splitlines()
+    for item in code:
+        if item[:7] == "import " and ", " not in item:
+            if " as " in item:
+                modules.append(item[7:item.find(" as ")])
+            else:
+                modules.append(item[7:])
+        elif item[:5] == "from ":
+            if "." in item[5:item.find(" import ")]:
+                modules.append(item[5:item.find(" import ")].split(".")[0])
+            else:
+                modules.append(item[5:item.find(" import ")])
+        elif ", " in item:
+            item = item[7:].split(", ")
+            modules = modules+item
+        else:
+            print(item)
+    return list(set(modules))

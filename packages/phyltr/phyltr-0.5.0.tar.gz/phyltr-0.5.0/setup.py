a2541d565331401fc624c6c50cfe143822884891
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:18 +0000

    Add package phyltr/0.5.0/phyltr-0.5.0.tar.gz

---
+++ packages/phyltr/phyltr-0.5.0.tar.gz/phyltr-0.5.0/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='phyltr',
+    version='0.5.0',
+    description='Unix filters for manipulating and analysing (samples of) phylogenetic trees represented in the Newick format',
+    author='Luke Maurits',
+    author_email='luke@maurits.id.au',
+    url='https://github.com/lmaurits/phyltr',
+    license="GPL3",
+    classifiers=[
+        'Programming Language :: Python',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    entry_points = {
+        'console_scripts': ['phyltr=phyltr.__main__:run_command'],
+    },
+    install_requires=[
+        'ete3',
+    ],
+    extras_require={
+        'dev': ['flake8', 'wheel', 'twine'],
+        'test': [
+            'mock',
+            'pytest>=3.6',
+            'pytest-mock',
+            'pytest-cov',
+            'coverage>=4.2',
+        ],
+    },
+)

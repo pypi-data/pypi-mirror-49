a7e52ef1fd40032d028f63fdeffc945dad4320be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:33 +0000

    Add package bhpq/1.0.6/bhpq-1.0.6.tar.gz

---
+++ packages/bhpq/bhpq-1.0.6.tar.gz/bhpq-1.0.6/bhpq/__init__.py
@@ -0,0 +1,11 @@
+"""BHPQ - Binary Heap Priority Queue
+
+    >>> from bhpq import BinaryHeapPriorityQueue
+
+See https://github.com/aayushuppal/bhpq for more information
+"""
+
+# Version of bhrq package
+__version__ = "1.0.6"
+
+from .bhpq import BinaryHeapPriorityQueue

e7ae05eefe06ed3c0bea24afed8eb91fd170c884
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:12 +0000

    Add package hopla/1.0.5/hopla-1.0.5.tar.gz

---
+++ packages/hopla/hopla-1.0.5.tar.gz/hopla-1.0.5/hopla/__init__.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+##########################################################################
+# Hopla - Copyright (C) AGrigis, 2015
+# Distributed under the terms of the CeCILL-B license, as published by
+# the CEA-CNRS-INRIA. Refer to the LICENSE file or to
+# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+# for details.
+##########################################################################
+
+# Hopla globals
+from .info import DESCRIPTION, __version__  # pragma: no cover
+from .scheduler import scheduler  # pragma: no cover
+from .converter import hopla  # pragma: no cover
+
+__doc__ = DESCRIPTION  # pragma: no cover

8892d9064a1f3af21402e6c422b40139d98c2016
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:15 +0000

    Add package dominant_color/0.0.2/dominant_color-0.0.2-py3-none-any.whl

---
+++ packages/dominant_color/dominant_color-0.0.2-py3-none-any.whl/dominant_color/kmeans_palette.py
@@ -0,0 +1,33 @@
+from PIL import Image
+import numpy as np
+import scipy
+import scipy.misc
+import scipy.cluster
+
+
+def kmeans_palette(image, bins=5, workimage_width=150, workimage_height=150):
+    im = Image.open(image)
+    im = im.resize((workimage_width, workimage_height))
+    ar = np.asarray(im)
+    shape = ar.shape
+
+    # Flatten as a list of pixels
+    ar = ar.reshape(scipy.product(shape[:2]), shape[2]).astype(float)
+
+    # Finding clusters
+    codebook, _ = scipy.cluster.vq.kmeans(ar, bins)
+    # codebook contain 5 middle of bins, as [r, g, b]
+
+    code, _ = scipy.cluster.vq.vq(ar, codebook)
+    # code contains bin ids of each pixel
+
+    counts, _ = scipy.histogram(code, bins)  # Binning cluster ids to count them
+
+    palette = sorted(
+        [
+            (100 * counts[i] / (workimage_width * workimage_height), codebook[i])
+            for i in range(bins)
+        ],
+        reverse=True,
+    )
+    return palette

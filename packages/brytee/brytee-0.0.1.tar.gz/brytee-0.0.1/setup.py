7d59a0ddda7d368872403ebcb34eea1877b0d80d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:21 +0000

    Add package brytee/0.0.1/brytee-0.0.1.tar.gz

---
+++ packages/brytee/brytee-0.0.1.tar.gz/brytee-0.0.1/setup.py
@@ -0,0 +1,13 @@
+#!/user/bin/env python3 
+
+from setuptools import setup
+
+
+setup(name='brytee',
+      version='0.0.1',
+      description='Python Distribution Utilities',
+      author='Greg Ward',
+      author_email='gward@python.net',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=[],
+     )

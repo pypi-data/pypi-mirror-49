71148b7e22f9573fd869902ed368e040e6e9df71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:35 +0000

    Add package cnvrg/0.0.4.15/cnvrg-0.0.4.15.tar.gz

---
+++ packages/cnvrg/cnvrg-0.0.4.15.tar.gz/cnvrg-0.0.4.15/cnvrg/__init__.py
@@ -0,0 +1,9 @@
+from cnvrg.modules.library import Library
+from cnvrg.modules.experiment import Experiment
+from cnvrg.modules.endpoint import Endpoint
+from cnvrg.modules.data_connector.mysql_connector import MysqlConnector
+from cnvrg.version import VERSION
+
+
+
+__version__ = VERSION

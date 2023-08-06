1ff19d67945f40cdc5ce5e3ad042ca1bbfd34540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:08 +0000

    Add package idottie/0.0.2/idottie-0.0.2-py2.py3-none-any.whl

---
+++ packages/idottie/idottie-0.0.2-py2.py3-none-any.whl/idottie/Translate.py
@@ -0,0 +1,11 @@
+
+class Translate(object):
+
+    def hello(self):
+        print('hello')
+
+    def toEnglish(self, chinese_text):
+        print(chinese_text)
+
+    def toChinese(self, english_text):
+        print(english_text)

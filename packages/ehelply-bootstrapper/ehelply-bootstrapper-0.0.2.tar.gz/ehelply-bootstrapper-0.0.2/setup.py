ce07f86caca847b56da6ffdbfce323b32e7b11b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:13 +0000

    Add package ehelply-bootstrapper/0.0.2/ehelply-bootstrapper-0.0.2.tar.gz

---
+++ packages/ehelply-bootstrapper/ehelply-bootstrapper-0.0.2.tar.gz/ehelply-bootstrapper-0.0.2/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='ehelply-bootstrapper',
+    packages=find_packages(),  # this must be the same as the name above
+    version='0.0.2',
+    description='eHelply Bootstrapper',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Shawn Clake',
+    author_email='shawn.clake@gmail.com',
+    url='https://github.com/ehelply/Bootstrapper',
+    keywords=[],
+    include_package_data=True,
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=[
+        'pika',
+        'librabbitmq',
+        'kombu',
+        'pydantic',
+        'pymlconf',
+        'fastapi',
+        'pymongo',
+        'redis',
+        'python-socketio',
+        'email-validator',
+        'uvicorn',
+        'pprint',
+        'pyjwt',
+        'passlib[bcrypt]',
+        'pytest',
+        'python-multipart',
+        'requests',
+        'sentry-asgi',
+        'python_dateutil == 2.6.0',
+        'six',
+        'boto3',
+        'ehelply-logger>=0.0.3',
+    ],
+
+)

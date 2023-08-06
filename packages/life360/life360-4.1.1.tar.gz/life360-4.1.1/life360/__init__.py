572a36fb4a40eb7993f7f8de67d95e2e7afb59b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:27 +0000

    Add package life360/4.1.1/life360-4.1.1.tar.gz

---
+++ packages/life360/life360-4.1.1.tar.gz/life360-4.1.1/life360/__init__.py
@@ -0,0 +1,8 @@
+"""
+Python package for accessing Life360 REST API.
+"""
+
+
+from .exceptions import *
+from .life360 import Life360
+from .version import __version__
---
+++ packages/life360/life360-4.1.1.tar.gz/life360-4.1.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md") as fp:
+    long_description = fp.read()
+
+version = {}
+with open("life360/version.py") as fp:
+    exec(fp.read(), version)
+
+setuptools.setup(
+    name="life360",
+    version=version['__version__'],
+    author="Phil Bruckner",
+    author_email="pnbruckner@gmail.com",
+    description="Life360 Communications Module",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pnbruckner/life360",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "requests",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

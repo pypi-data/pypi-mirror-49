d7b18da4e15e95044bbb6a7ad98aaa47b38d7af7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:04:54 +0000

    Add package onetoken/0.2.190719/onetoken-0.2.190719.tar.gz

---
+++ packages/onetoken/onetoken-0.2.190719.tar.gz/onetoken-0.2.190719/onetoken/config.py
@@ -0,0 +1,15 @@
+class Config:
+    HOST_REST = 'https://1token.trade/api/v1'
+    TRADE_HOST = 'https://1token.trade/api/v1/trade'
+    TRADE_HOST_WS = 'wss://1token.trade/api/v1/ws/trade'
+    TICK_HOST_WS = 'wss://1token.trade/api/v1/ws/tick?gzip=true'
+    CANDLE_HOST_WS = 'wss://1token.trade/api/v1/ws/candle?gzip=true'
+
+    @classmethod
+    def change_host(cls, target='1token.trade/', match='1token.trade/', nossl=False):
+        for item in ['TRADE_HOST', 'TRADE_HOST_WS', 'TICK_HOST_WS', 'HOST_REST', 'CANDLE_HOST_WS']:
+            new = getattr(cls, item).replace(match, target)
+            if nossl:
+                new = new.replace('https://', 'http://')
+                new = new.replace('wss://', 'ws://')
+            setattr(cls, item, new)
---
+++ packages/onetoken/onetoken-0.2.190719.tar.gz/onetoken-0.2.190719/onetoken/logger.py
@@ -0,0 +1,56 @@
+import functools
+import sys
+import logging
+from pathlib import Path
+
+log = logging.getLogger('ots')
+
+
+def set_log():
+    # syslog.basicConfig()
+    # import logging as syslog
+    # syslog.basicConfig()
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.DEBUG)
+    ch.setFormatter(
+        logging.Formatter('%(levelname)-.7s [%(asctime)s][1token]%(message)s', '%H:%M:%S'))
+    log.addHandler(ch)
+    log.setLevel(logging.INFO)
+
+    def wrap(orig):
+        @functools.wraps(orig)
+        def new_func(*args, **kwargs):
+            left = ' '.join(str(x) for x in args)
+            right = ' '.join('{}={}'.format(k, v) for k, v in kwargs.items())
+            new = ' '.join(filter(None, [left, right]))
+            import inspect
+            r = inspect.stack()[1]
+            new = f'[{Path(r.filename).name}:{r.lineno}] {new}'
+            orig(new)
+
+        return new_func
+
+    log.debug = wrap(log.debug)
+    log.info = wrap(log.info)
+    log.warning = wrap(log.warning)
+    log.exception = wrap(log.exception)
+
+
+set_log()
+
+
+def log_level(level):
+    print('set log level to {}'.format(level))
+    log.setLevel(level)
+
+
+def main():
+    log_level(logging.DEBUG)
+    log.debug('debug log test')
+    log.info('info log test')
+    log.warning('warning log test')
+    log.exception('exception log test')
+
+
+if __name__ == '__main__':
+    main()

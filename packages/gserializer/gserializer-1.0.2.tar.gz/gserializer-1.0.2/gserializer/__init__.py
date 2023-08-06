80dfba1e08d3ed21a676b11d1cc3841967c0d621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:02 +0000

    Add package gserializer/1.0.2/gserializer-1.0.2.tar.gz

---
+++ packages/gserializer/gserializer-1.0.2.tar.gz/gserializer-1.0.2/gserializer/__init__.py
@@ -0,0 +1,15 @@
+__version__ = "1.0.0"
+from gserializer.Tools.Filter import Filter
+from gserializer.Tools.Reader import Reader
+
+def init():
+    print("Welcome to the GoogleSheetsSerializer v1.0 beta by Paul Shao")
+
+def Reader():
+    return Reader()
+
+def Filter():
+    return Filter()
+
+if __name__ == "__init__":
+    init()

431dee11f88614422a8c8d7c2a73af2cc9988a29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:35 +0000

    Add package modest/0.1a20/modest-0.1a20.tar.gz

---
+++ packages/modest/modest-0.1a20.tar.gz/modest-0.1a20/setup.py
@@ -0,0 +1,35 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+    
+setup(
+    name='modest',
+    version='0.1a20',
+    packages=find_packages(exclude=("tests",)),
+    install_requires=[
+        'pandas',
+        'pint',
+        'numpy',
+        'matplotlib',
+        'skyfield',
+        'scipy',
+        'pyquaternion',
+        'requests',
+        'astropy',
+        'pyyaml>=3.12',
+        'mpld3',
+        'jinja2'
+    ],
+    description='A modular estimation library',
+    long_description=long_description,
+    license='MIT',
+    author='Joel Runnels',
+    author_email='runne010@umn.edu'
+)

30545fa5852daeb865caab61741de82be821a6ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:45 +0000

    Add package spherical-geometry/1.2.10/spherical_geometry-1.2.10.tar.gz

---
+++ packages/spherical-geometry/spherical_geometry-1.2.10.tar.gz/spherical_geometry-1.2.10/spherical_geometry/__init__.py
@@ -0,0 +1,14 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, unicode_literals, print_function
+
+"""
+Package for managing polygons on the unit sphere.
+"""
+
+# Affiliated packages may add whatever they like to this file, but
+# should keep this content at the top.
+# ----------------------------------------------------------------------------
+from ._astropy_init import *
+# ----------------------------------------------------------------------------

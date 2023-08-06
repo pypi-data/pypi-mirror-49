6db3e238f211ddfc898971f8a26d1db95297bab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:56 +0000

    Add package deepgeo-ext-maskrcnn/0.0.1/deepgeo_ext_maskrcnn-0.0.1-py3-none-any.whl

---
+++ packages/deepgeo-ext-maskrcnn/deepgeo_ext_maskrcnn-0.0.1-py3-none-any.whl/deepgeo_ext_maskrcnn/__init__.py
@@ -0,0 +1,14 @@
+# __init__.py
+# Copyright (C) 2019 Info Lab. (gnyontu39@gmail.com) and contributors
+#
+# 20190725 : pip install deepgeo tensorflow-gpu==1.12.0 keras cython
+
+__version__ = '0.0.190725.1'
+
+try:
+    from .Model import Model
+    from .Dataset import Dataset
+    from .Config  import Config
+except ImportError as e:
+    print(e," 추가할 수 없습니다.")
+    exit(1)

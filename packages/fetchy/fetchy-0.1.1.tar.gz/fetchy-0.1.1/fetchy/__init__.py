437f7ecb14c0246a51a45566fd4814294067e40e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:41 +0000

    Add package fetchy/0.1.1/fetchy-0.1.1.tar.gz

---
+++ packages/fetchy/fetchy-0.1.1.tar.gz/fetchy-0.1.1/fetchy/__init__.py
@@ -0,0 +1,15 @@
+__version__ = "0.1.1"
+
+from .version import Version, version_from_string
+from .dependency import (
+    Dependency,
+    dependency_from_string,
+    relationship_from_string,
+    dependencies_from_string,
+)
+from .package import Package, package_from_dict
+from .downloader import Downloader
+from .repository import Repository
+from .parser import Parser
+from .utils import *
+from .cli import main as cli

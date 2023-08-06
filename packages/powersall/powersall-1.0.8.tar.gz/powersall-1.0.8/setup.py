e78ebbfea970ed66d2b9325a27c2f792f4a5dae7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:14 +0000

    Add package powersall/1.0.8/powersall-1.0.8.tar.gz

---
+++ packages/powersall/powersall-1.0.8.tar.gz/powersall-1.0.8/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import re
+
+version = re.search(
+    '^__version__\s*=\s*"(.*)"',
+    open('powersall/powersall.py').read(),
+    re.M
+).group(1)
+
+setup(
+    name='powersall',
+    author='James Campbell',
+    author_email='jc@normail.co',
+    version=version,
+    license='MIT',
+    description='file extension info',
+    packages=['powersall'],
+    py_modules=['powersall', 'functions'],
+    keywords=['powerball', 'lottery',
+              'random-numbers', 'lotto', 'megamillions'],
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    include_package_data=True,
+    install_requires=[
+        'beautifulsoup4',
+        'requests',
+        'lxml',
+        'pandas',
+        'argparse',
+        'pathlib',
+        'markovify'
+    ],
+    entry_points={
+        'console_scripts': [
+            'powersall=powersall.powersall:main',
+        ],
+    },
+    url='https://github.com/jamesacampbell/powersall',
+    download_url='https://github.com/jamesacampbell/powersall/archive/{}.tar.gz'.format(
+        version)
+)

4dc85886a1e22ca9eb7d706df438abbabdea6666
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:22 +0000

    Add package icv/0.0.1.5.1/icv-0.0.1.5.1.tar.gz

---
+++ packages/icv/icv-0.0.1.5.1.tar.gz/icv-0.0.1.5.1/icv/detector/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: UTF-8 -*-
+from .service.client import DetectorClient
+from .tf_obj import TfObjectDetector
+from .mb import MbDetector
+from .mmd import MmdetDetector
+
+# __all__ = ['DetectorClient', 'TfObjectDetector', 'MbDetector', 'MmdetDetector']

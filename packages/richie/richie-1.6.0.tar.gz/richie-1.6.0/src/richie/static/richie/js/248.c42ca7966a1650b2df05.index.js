b8e68923b36ac6b04d3bf8e65ce9e2e6b391a975
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:17 +0000

    Add package richie/1.6.0/richie-1.6.0.tar.gz

---
+++ packages/richie/richie-1.6.0.tar.gz/richie-1.6.0/src/richie/static/richie/js/248.c42ca7966a1650b2df05.index.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[248],{445:function(n,w){}}]);
+//# sourceMappingURL=248.c42ca7966a1650b2df05.index.js.map
---
+++ packages/richie/richie-1.6.0.tar.gz/richie-1.6.0/src/richie/static/richie/js/249.c42ca7966a1650b2df05.index.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[249],{446:function(n,w){}}]);
+//# sourceMappingURL=249.c42ca7966a1650b2df05.index.js.map

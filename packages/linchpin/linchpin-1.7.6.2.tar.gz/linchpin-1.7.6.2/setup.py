2cefaae3410f2f64c1d4587dbf39d468c2f3ea7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:02 +0000

    Add package linchpin/1.7.6.2/linchpin-1.7.6.2.tar.gz

---
+++ packages/linchpin/linchpin-1.7.6.2.tar.gz/linchpin-1.7.6.2/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+from __future__ import absolute_import
+import os
+import ast
+from setuptools import setup, find_packages
+
+with open('linchpin/version.py') as f:
+    for line in f:
+        if line.startswith('__version__'):
+            ver = ast.parse(line).body[0].value.s
+            break
+
+# reading requirements from requirements.txt
+dir_path = os.path.dirname(os.path.realpath(__file__))
+reqs_file = 'requirements.txt'.format(dir_path)
+with open(reqs_file) as f:
+    required = f.read().splitlines()
+
+setup_required = list(required)
+setup_required.append('pytest-runner')
+
+ignore_dir = ['.git']
+
+setup(
+    name='linchpin',
+    version=ver,
+    description='Ansible based multi cloud provisioner',
+    author='samvaran kashyap rallabandi',
+    author_email='linchpin@redhat.com',
+    url='http://linchpin.readthedocs.io/',
+    setup_requires=setup_required,
+    install_requires=required,
+    entry_points='''
+        [console_scripts]
+        linchpin=linchpin.shell:runcli
+    ''',
+    tests_require=["pytest<=4.4.0", "nose", "mock", "coverage", "flake8"],
+    extras_require={
+        'krbV': ["python-krbV"],
+        'beaker': ['beaker-client>=23.3', 'python-krbV'],
+        'docs': ["docutils", "sphinx", "sphinx_rtd_theme", "sphinx-automodapi"],
+        'tests': ["nose", "mock", "coverage", "flake8", "pytest"],
+        'libvirt': ["libvirt-python>=3.0.0", "lxml"],
+        'vmware': ["PyVmomi>=6.7.1"],
+        'docker': ["docker-py>=1.7.0"],
+    },
+    zip_safe=False,
+    packages=find_packages(),
+    include_package_data=True,
+    scripts=[
+        'scripts/install_libvirt_deps.sh',
+        'scripts/install_selinux_venv.sh'
+    ]
+)

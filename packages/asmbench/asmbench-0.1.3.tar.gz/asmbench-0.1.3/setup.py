f9c4b884a4c9af852447ba7175e1f9baace1dc7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:01 +0000

    Add package asmbench/0.1.3/asmbench-0.1.3.tar.gz

---
+++ packages/asmbench/asmbench-0.1.3.tar.gz/asmbench-0.1.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+setup(
+    name='asmbench',
+    version='0.1.3',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    url='https://github.com/RRZE-HPC/asmbench',
+    license='AGPLv3',
+    author='Julian Hammer',
+    author_email='julian.hammer@fau.de',
+    description='A Benchmark Toolkit for Assembly Instructions Using the LLVM JIT',
+    long_description=long_description,
+    install_requires=['llvmlite>=0.23.2', 'psutil'],
+    extras_require={
+        'sc18src': ['numpy', 'matplotlib'],
+        'iaca': ['kerncraft'],
+    },
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'asmbench=asmbench.__main__:main'
+        ]
+    }
+)

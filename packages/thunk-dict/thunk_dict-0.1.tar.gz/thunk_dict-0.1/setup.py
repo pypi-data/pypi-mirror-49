41033a19ab7868029db55f8b97b143b9f4624ee2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:34 +0000

    Add package thunk-dict/0.1/thunk_dict-0.1.tar.gz

---
+++ packages/thunk-dict/thunk_dict-0.1.tar.gz/thunk_dict-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+setup(
+    name="thunk_dict",
+    version="0.1",
+    description="Memoized dictionaries",
+    author="kevalii",
+    author_email="alexrankine@college.harvard.edu",
+    packages=["thunk_dict", ],
+    license="MIT",
+    url="https://github.com/kevalii/thunk-dict",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        'Intended Audience :: Developers',
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License'
+    ],
+
+)

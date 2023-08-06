d363e1fc917d5c436b5f2935e3e573ead7b61c23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:46 +0000

    Add package datastories/0.3.1/datastories-0.3.1.tar.gz

---
+++ packages/datastories/datastories-0.3.1.tar.gz/datastories-0.3.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+ 
+
+
+
+
+setup(name='datastories',
+      version='0.3.1',
+      url='https://github.com/MaciejJanowski/DataStoryPatternLibrary',
+      license='MIT',
+      author='Maciej Janowski',
+      author_email='maciej.janowski@insight-centre.org',
+      description='Data Story Pattern Analysis for LOSD',
+      packages=find_packages(exclude=['tests']),
+      install_requires=['SPARQLWrapper',
+                        'sparql_dataframe','pandas',
+                        'numpy','scipy'],
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
+      zip_safe=False)

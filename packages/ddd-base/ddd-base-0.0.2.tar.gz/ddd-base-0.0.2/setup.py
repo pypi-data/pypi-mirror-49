41b08a5349f88f83168fc3294eb36fec1ba39180
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:46 +0000

    Add package ddd-base/0.0.2/ddd-base-0.0.2.tar.gz

---
+++ packages/ddd-base/ddd-base-0.0.2.tar.gz/ddd-base-0.0.2/setup.py
@@ -0,0 +1,38 @@
+"""Setuptools entry point."""
+import codecs
+import os
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules'
+]
+
+dirname = os.path.dirname(__file__)
+
+long_description = (
+    codecs.open(os.path.join(dirname, 'README.rst'), encoding='utf-8').read() + '\n' +
+    codecs.open(os.path.join(dirname, 'CHANGES.rst'), encoding='utf-8').read()
+)
+
+setup(
+    name='ddd-base',
+    version='0.0.2',
+    description='DDD base framework for python',
+    long_description=long_description,
+    author='Sun Wei',
+    author_email='wayde.sun@gmail.com',
+    url='https://github.com/sunwei/ddd-base',
+    packages=['ddd_base'],
+    install_requires=[],
+    classifiers=CLASSIFIERS)

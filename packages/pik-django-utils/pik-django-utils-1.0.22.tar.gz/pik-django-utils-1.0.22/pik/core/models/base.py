800a069a7f38bddbc38bb36f412dfd83a7ac9429
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:33 +0000

    Add package pik-django-utils/1.0.22/pik-django-utils-1.0.22.tar.gz

---
+++ packages/pik-django-utils/pik-django-utils-1.0.22.tar.gz/pik-django-utils-1.0.22/pik/core/models/base.py
@@ -0,0 +1,14 @@
+from .uided import Uided, PUided
+from .dated import Dated
+from .versioned import Versioned
+from .historized import Historized
+
+
+class BaseHistorical(Uided, Dated, Versioned, Historized):  # type: ignore
+    class Meta:
+        abstract = True
+
+
+class BasePHistorical(PUided, Dated, Versioned, Historized):  # type: ignore
+    class Meta:
+        abstract = True

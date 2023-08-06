a325017ca6f00730393e25539d8697374bde14aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:47 +0000

    Add package nomeroff-net/0.3.2/nomeroff_net-0.3.2-py3-none-any.whl

---
+++ packages/nomeroff-net/nomeroff_net-0.3.2-py3-none-any.whl/NomeroffNet/__init__.py
@@ -0,0 +1,7 @@
+from .Detector import *
+from .filters import *
+from .RectDetector import *
+from .TextDetector import *
+from .TextPostprocessing import *
+from .OptionsDetector import *
+from .mcm import *

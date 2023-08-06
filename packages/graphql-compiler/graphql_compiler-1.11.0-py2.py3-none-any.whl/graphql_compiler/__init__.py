f50fd0ba3187499d8329cc53f2280575a5f82e6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:36 +0000

    Add package graphql-compiler/1.11.0/graphql_compiler-1.11.0-py2.py3-none-any.whl

---
+++ packages/graphql-compiler/graphql_compiler-1.11.0-py2.py3-none-any.whl/graphql_compiler/__init__.py
@@ -0,0 +1,244 @@
+# Copyright 2017-present Kensho Technologies, LLC.
+"""Commonly-used functions and data types from this package."""
+from .compiler import (  # noqa
+    CompilationResult,
+    OutputMetadata,
+    compile_graphql_to_gremlin,
+    compile_graphql_to_match,
+    compile_graphql_to_sql,
+    compile_graphql_to_cypher,
+)
+from .query_formatting import insert_arguments_into_query  # noqa
+from .query_formatting.graphql_formatting import pretty_print_graphql  # noqa
+from .exceptions import (  # noqa
+    GraphQLCompilationError, GraphQLError, GraphQLInvalidArgumentError, GraphQLParsingError,
+    GraphQLValidationError
+)
+from .schema import (  # noqa
+    DIRECTIVES, EXTENDED_META_FIELD_DEFINITIONS, GraphQLDate, GraphQLDateTime, GraphQLDecimal,
+    insert_meta_fields_into_existing_schema, is_meta_field
+)
+from .schema_generation.graphql_schema import get_graphql_schema_from_schema_graph
+from .schema_generation.orientdb.schema_graph_builder import (
+    get_orientdb_schema_graph
+)
+
+__package_name__ = 'graphql-compiler'
+__version__ = '1.11.0'
+
+
+def graphql_to_match(schema, graphql_query, parameters, type_equivalence_hints=None):
+    """Compile the GraphQL input using the schema into a MATCH query and associated metadata.
+
+    Args:
+        schema: GraphQL schema object describing the schema of the graph to be queried
+        graphql_query: the GraphQL query to compile to MATCH, as a string
+        parameters: dict, mapping argument name to its value, for every parameter the query expects.
+        type_equivalence_hints: optional dict of GraphQL interface or type -> GraphQL union.
+                                Used as a workaround for GraphQL's lack of support for
+                                inheritance across "types" (i.e. non-interfaces), as well as a
+                                workaround for Gremlin's total lack of inheritance-awareness.
+                                The key-value pairs in the dict specify that the "key" type
+                                is equivalent to the "value" type, i.e. that the GraphQL type or
+                                interface in the key is the most-derived common supertype
+                                of every GraphQL type in the "value" GraphQL union.
+                                Recursive expansion of type equivalence hints is not performed,
+                                and only type-level correctness of this argument is enforced.
+                                See README.md for more details on everything this parameter does.
+                                *****
+                                Be very careful with this option, as bad input here will
+                                lead to incorrect output queries being generated.
+                                *****
+
+    Returns:
+        a CompilationResult object, containing:
+            - query: string, the resulting compiled and parameterized query string
+            - language: string, specifying the language to which the query was compiled
+            - output_metadata: dict, output name -> OutputMetadata namedtuple object
+            - input_metadata: dict, name of input variables -> inferred GraphQL type, based on use
+    """
+    compilation_result = compile_graphql_to_match(
+        schema, graphql_query, type_equivalence_hints=type_equivalence_hints)
+    return compilation_result._replace(
+        query=insert_arguments_into_query(compilation_result, parameters))
+
+
+def graphql_to_sql(schema, graphql_query, parameters, compiler_metadata,
+                   type_equivalence_hints=None):
+    """Compile the GraphQL input using the schema into a SQL query and associated metadata.
+
+    Args:
+        schema: GraphQL schema object describing the schema of the graph to be queried
+        graphql_query: the GraphQL query to compile to SQL, as a string
+        parameters: dict, mapping argument name to its value, for every parameter the query expects.
+        compiler_metadata: SqlMetadata object, provides SQLAlchemy specific backend
+                           information
+        type_equivalence_hints: optional dict of GraphQL interface or type -> GraphQL union.
+                                Used as a workaround for GraphQL's lack of support for
+                                inheritance across "types" (i.e. non-interfaces), as well as a
+                                workaround for Gremlin's total lack of inheritance-awareness.
+                                The key-value pairs in the dict specify that the "key" type
+                                is equivalent to the "value" type, i.e. that the GraphQL type or
+                                interface in the key is the most-derived common supertype
+                                of every GraphQL type in the "value" GraphQL union.
+                                Recursive expansion of type equivalence hints is not performed,
+                                and only type-level correctness of this argument is enforced.
+                                See README.md for more details on everything this parameter does.
+                                *****
+                                Be very careful with this option, as bad input here will
+                                lead to incorrect output queries being generated.
+                                *****
+
+    Returns:
+        a CompilationResult object, containing:
+            - query: string, the resulting compiled and parameterized query string
+            - language: string, specifying the language to which the query was compiled
+            - output_metadata: dict, output name -> OutputMetadata namedtuple object
+            - input_metadata: dict, name of input variables -> inferred GraphQL type, based on use
+    """
+    compilation_result = compile_graphql_to_sql(
+        schema, graphql_query, compiler_metadata, type_equivalence_hints=type_equivalence_hints)
+    return compilation_result._replace(
+        query=insert_arguments_into_query(compilation_result, parameters))
+
+
+def graphql_to_gremlin(schema, graphql_query, parameters, type_equivalence_hints=None):
+    """Compile the GraphQL input using the schema into a Gremlin query and associated metadata.
+
+    Args:
+        schema: GraphQL schema object describing the schema of the graph to be queried
+        graphql_query: the GraphQL query to compile to Gremlin, as a string
+        parameters: dict, mapping argument name to its value, for every parameter the query expects.
+        type_equivalence_hints: optional dict of GraphQL interface or type -> GraphQL union.
+                                Used as a workaround for GraphQL's lack of support for
+                                inheritance across "types" (i.e. non-interfaces), as well as a
+                                workaround for Gremlin's total lack of inheritance-awareness.
+                                The key-value pairs in the dict specify that the "key" type
+                                is equivalent to the "value" type, i.e. that the GraphQL type or
+                                interface in the key is the most-derived common supertype
+                                of every GraphQL type in the "value" GraphQL union.
+                                Recursive expansion of type equivalence hints is not performed,
+                                and only type-level correctness of this argument is enforced.
+                                See README.md for more details on everything this parameter does.
+                                *****
+                                Be very careful with this option, as bad input here will
+                                lead to incorrect output queries being generated.
+                                *****
+
+    Returns:
+        a CompilationResult object, containing:
+            - query: string, the resulting compiled and parameterized query string
+            - language: string, specifying the language to which the query was compiled
+            - output_metadata: dict, output name -> OutputMetadata namedtuple object
+            - input_metadata: dict, name of input variables -> inferred GraphQL type, based on use
+    """
+    compilation_result = compile_graphql_to_gremlin(
+        schema, graphql_query, type_equivalence_hints=type_equivalence_hints)
+    return compilation_result._replace(
+        query=insert_arguments_into_query(compilation_result, parameters))
+
+
+def get_graphql_schema_from_orientdb_schema_data(schema_data, class_to_field_type_overrides=None,
+                                                 hidden_classes=None):
+    """Construct a GraphQL schema from an OrientDB schema.
+
+    Args:
+        schema_data: list of dicts describing the classes in the OrientDB schema. The following
+                     format is the way the data is structured in OrientDB 2. See
+                     the README.md file for an example of how to query this data.
+                     Each dict has the following string fields:
+                        - name: string, the name of the class.
+                        - superClasses (optional): list of strings, the name of the class's
+                                                   superclasses.
+                        - superClass (optional): string, the name of the class's superclass. May be
+                                                 used instead of superClasses if there is only one
+                                                 superClass. Used for backwards compatibility with
+                                                 OrientDB.
+                        - customFields (optional): dict, string -> string, data defined on the class
+                                                   instead of instances of the class.
+                        - abstract: bool, true if the class is abstract.
+                        - properties: list of dicts, describing the class's properties.
+                                      Each property dictionary has the following string fields:
+                                         - name: string, the name of the property.
+                                         - type: int, builtin OrientDB type ID of the property.
+                                                 See schema_properties.py for the mapping.
+                                         - linkedType (optional): int, if the property is a
+                                                                  collection of builtin OrientDB
+                                                                  objects, then it indicates their
+                                                                  type ID.
+                                         - linkedClass (optional): string, if the property is a
+                                                                   collection of class instances,
+                                                                   then it indicates the name of
+                                                                   the class. If class is an edge
+                                                                   class, and the field name is
+                                                                   either 'in' or 'out', then it
+                                                                   describes the name of an
+                                                                   endpoint of the edge.
+                                         - defaultValue: string, the textual representation of the
+                                                         default value for the property, as
+                                                         returned by OrientDB's schema
+                                                         introspection code, e.g., '{}' for
+                                                         the embedded set type. Note that if the
+                                                         property is a collection type, it must
+                                                         have a default value.
+        class_to_field_type_overrides: optional dict, class name -> {field name -> field type},
+                                       (string -> {string -> GraphQLType}). Used to override the
+                                       type of a field in the class where it's first defined and all
+                                       the class's subclasses.
+        hidden_classes: optional set of strings, classes to not include in the GraphQL schema.
+
+    Returns:
+        tuple of (GraphQL schema object, GraphQL type equivalence hints dict).
+        The tuple is of type (GraphQLSchema, {GraphQLObjectType -> GraphQLUnionType}).
+    """
+    if class_to_field_type_overrides is None:
+        class_to_field_type_overrides = dict()
+    if hidden_classes is None:
+        hidden_classes = set()
+
+    schema_graph = get_orientdb_schema_graph(schema_data, [])
+    return get_graphql_schema_from_schema_graph(schema_graph, class_to_field_type_overrides,
+                                                hidden_classes)
+
+
+def graphql_to_redisgraph_cypher(schema, graphql_query, parameters, type_equivalence_hints=None):
+    """Compile the GraphQL input into a RedisGraph Cypher query and associated metadata.
+
+    Note that the corresponding function that would convert GraphQL to Cypher for Neo4j does not
+    exist because Neo4j supports query parameters but RedisGraph doesn't. So, for Neo4j we will use
+    the Neo4j client's own query parameter handling method but for RedisGraph we'll manually
+    interpolate values in the query string.
+
+    See README.md for a more detailed explanation.
+
+    Args:
+        schema: GraphQL schema object describing the schema of the graph to be queried
+        graphql_query: the GraphQL query to compile to Cypher, as a string
+        parameters: dict, mapping argument name to its value, for every parameter the query expects.
+        type_equivalence_hints: optional dict of GraphQL interface or type -> GraphQL union.
+                                Used as a workaround for GraphQL's lack of support for
+                                inheritance across "types" (i.e. non-interfaces), as well as a
+                                workaround for Gremlin's total lack of inheritance-awareness.
+                                The key-value pairs in the dict specify that the "key" type
+                                is equivalent to the "value" type, i.e. that the GraphQL type or
+                                interface in the key is the most-derived common supertype
+                                of every GraphQL type in the "value" GraphQL union.
+                                Recursive expansion of type equivalence hints is not performed,
+                                and only type-level correctness of this argument is enforced.
+                                See README.md for more details on everything this parameter does.
+                                *****
+                                Be very careful with this option, as bad input here will
+                                lead to incorrect output queries being generated.
+                                *****
+
+    Returns:
+        a CompilationResult object, containing:
+            - query: string, the resulting compiled and parameterized query string
+            - language: string, specifying the language to which the query was compiled
+            - output_metadata: dict, output name -> OutputMetadata namedtuple object
+            - input_metadata: dict, name of input variables -> inferred GraphQL type, based on use
+    """
+    compilation_result = compile_graphql_to_cypher(
+        schema, graphql_query, type_equivalence_hints=type_equivalence_hints)
+    return compilation_result._replace(
+        query=insert_arguments_into_query(compilation_result, parameters))
---
+++ packages/graphql-compiler/graphql_compiler-1.11.0-py2.py3-none-any.whl/graphql_compiler/compiler/ir_lowering_match/__init__.py
@@ -0,0 +1,131 @@
+# Copyright 2018-present Kensho Technologies, LLC.
+import six
+
+from ..blocks import Filter
+from ..ir_lowering_common.common import (extract_optional_location_root_info,
+                                         extract_simple_optional_location_info,
+                                         lower_context_field_existence,
+                                         merge_consecutive_filter_clauses,
+                                         optimize_boolean_expression_comparisons,
+                                         remove_end_optionals)
+from .ir_lowering import (lower_backtrack_blocks,
+                          lower_folded_coerce_types_into_filter_blocks,
+                          lower_has_substring_binary_compositions,
+                          remove_backtrack_blocks_from_fold,
+                          rewrite_binary_composition_inside_ternary_conditional,
+                          truncate_repeated_single_step_traversals,
+                          truncate_repeated_single_step_traversals_in_sub_queries)
+from ..ir_sanity_checks import sanity_check_ir_blocks_from_frontend
+from .between_lowering import lower_comparisons_to_between
+from .optional_traversal import (collect_filters_to_first_location_occurrence,
+                                 convert_optional_traversals_to_compound_match_query,
+                                 lower_context_field_expressions, prune_non_existent_outputs)
+from ..match_query import convert_to_match_query
+from ..workarounds import (orientdb_class_with_while, orientdb_eval_scheduling,
+                           orientdb_query_execution)
+from .utils import construct_where_filter_predicate
+
+##############
+# Public API #
+##############
+
+
+def lower_ir(ir_blocks, query_metadata_table, type_equivalence_hints=None):
+    """Lower the IR into an IR form that can be represented in MATCH queries.
+
+    Args:
+        ir_blocks: list of IR blocks to lower into MATCH-compatible form
+        query_metadata_table: QueryMetadataTable object containing all metadata collected during
+                              query processing, including location metadata (e.g. which locations
+                              are folded or optional).
+        type_equivalence_hints: optional dict of GraphQL interface or type -> GraphQL union.
+                                Used as a workaround for GraphQL's lack of support for
+                                inheritance across "types" (i.e. non-interfaces), as well as a
+                                workaround for Gremlin's total lack of inheritance-awareness.
+                                The key-value pairs in the dict specify that the "key" type
+                                is equivalent to the "value" type, i.e. that the GraphQL type or
+                                interface in the key is the most-derived common supertype
+                                of every GraphQL type in the "value" GraphQL union.
+                                Recursive expansion of type equivalence hints is not performed,
+                                and only type-level correctness of this argument is enforced.
+                                See README.md for more details on everything this parameter does.
+                                *****
+                                Be very careful with this option, as bad input here will
+                                lead to incorrect output queries being generated.
+                                *****
+
+    Returns:
+        MatchQuery object containing the IR blocks organized in a MATCH-like structure
+    """
+    sanity_check_ir_blocks_from_frontend(ir_blocks, query_metadata_table)
+
+    # Construct the mapping of each location to its corresponding GraphQL type.
+    location_types = {
+        location: location_info.type
+        for location, location_info in query_metadata_table.registered_locations
+    }
+
+    # Compute the set of all locations that have associated type coercions.
+    coerced_locations = {
+        location
+        for location, location_info in query_metadata_table.registered_locations
+        if location_info.coerced_from_type is not None
+    }
+
+    # Extract information for both simple and complex @optional traverses
+    location_to_optional_results = extract_optional_location_root_info(ir_blocks)
+    complex_optional_roots, location_to_optional_roots = location_to_optional_results
+    simple_optional_root_info = extract_simple_optional_location_info(
+        ir_blocks, complex_optional_roots, location_to_optional_roots)
+    ir_blocks = remove_end_optionals(ir_blocks)
+
+    # Append global operation block(s) to filter out incorrect results
+    # from simple optional match traverses (using a WHERE statement)
+    if len(simple_optional_root_info) > 0:
+        where_filter_predicate = construct_where_filter_predicate(
+            query_metadata_table, simple_optional_root_info)
+        # The GlobalOperationsStart block should already exist at this point. It is inserted
+        # in the compiler_frontend, and this function asserts that at the beginning.
+        ir_blocks.insert(-1, Filter(where_filter_predicate))
+
+    # These lowering / optimization passes work on IR blocks.
+    ir_blocks = lower_context_field_existence(ir_blocks, query_metadata_table)
+    ir_blocks = optimize_boolean_expression_comparisons(ir_blocks)
+    ir_blocks = rewrite_binary_composition_inside_ternary_conditional(ir_blocks)
+    ir_blocks = merge_consecutive_filter_clauses(ir_blocks)
+    ir_blocks = lower_has_substring_binary_compositions(ir_blocks)
+    ir_blocks = orientdb_eval_scheduling.workaround_lowering_pass(ir_blocks, query_metadata_table)
+
+    # Here, we lower from raw IR blocks into a MatchQuery object.
+    # From this point on, the lowering / optimization passes work on the MatchQuery representation.
+    match_query = convert_to_match_query(ir_blocks)
+
+    match_query = lower_comparisons_to_between(match_query)
+
+    match_query = lower_backtrack_blocks(match_query, query_metadata_table)
+    match_query = truncate_repeated_single_step_traversals(match_query)
+    match_query = orientdb_class_with_while.workaround_type_coercions_in_recursions(match_query)
+
+    # Optimize and lower the IR blocks inside @fold scopes.
+    new_folds = {
+        key: merge_consecutive_filter_clauses(
+            remove_backtrack_blocks_from_fold(
+                lower_folded_coerce_types_into_filter_blocks(folded_ir_blocks)
+            )
+        )
+        for key, folded_ir_blocks in six.iteritems(match_query.folds)
+    }
+    match_query = match_query._replace(folds=new_folds)
+
+    compound_match_query = convert_optional_traversals_to_compound_match_query(
+        match_query, complex_optional_roots, location_to_optional_roots)
+    compound_match_query = prune_non_existent_outputs(compound_match_query)
+    compound_match_query = collect_filters_to_first_location_occurrence(compound_match_query)
+    compound_match_query = lower_context_field_expressions(compound_match_query)
+
+    compound_match_query = truncate_repeated_single_step_traversals_in_sub_queries(
+        compound_match_query)
+    compound_match_query = orientdb_query_execution.expose_ideal_query_execution_start_points(
+        compound_match_query, location_types, coerced_locations)
+
+    return compound_match_query

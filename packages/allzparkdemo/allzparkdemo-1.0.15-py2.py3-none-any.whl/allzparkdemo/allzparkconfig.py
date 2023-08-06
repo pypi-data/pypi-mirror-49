1c0a69879c046e53a41a342e34b65d09974c6944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:49 +0000

    Add package allzparkdemo/1.0.15/allzparkdemo-1.0.15-py2.py3-none-any.whl

---
+++ packages/allzparkdemo/allzparkdemo-1.0.15-py2.py3-none-any.whl/allzparkdemo/allzparkconfig.py
@@ -0,0 +1,36 @@
+# An example Allzpark config
+
+startup_profile = "alita"
+startup_application = "maya==2018.0.5"
+
+
+def profiles():
+    """This function can also be a variable
+
+    Example:
+        profiles = ["profile1", "profile2"]
+
+    """
+
+    import os
+
+    demo_profiles = [
+        "alita",
+    ]
+
+    user_profiles = os.getenv("MY_PROJECTS", "").split(",")
+
+    return user_profiles + demo_profiles
+
+
+# Applications are specified in the profile package,
+# but Allzpark has the option to "Show all applications"
+# which disregards those, and shows these instead, for any
+# given profile.
+applications = [
+    "maya",
+    "houdini",
+    "nuke",
+    "aftereffects",
+    "terminal",
+]

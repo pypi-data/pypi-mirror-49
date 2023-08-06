78328c7157d2181ca5fc530a30d816c78af75b39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:15 +0000

    Add package gitutils/1.4.0/gitutils-1.4.0.tar.gz

---
+++ packages/gitutils/gitutils-1.4.0.tar.gz/gitutils-1.4.0/tests/test_scripts.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from pathlib import Path
+import subprocess
+import pytest
+
+R = Path(__file__).parent
+
+
+@pytest.mark.parametrize('op', ['gitemail'])
+def test_git(op):
+    subprocess.check_call([op, '..'], cwd=R)
+
+
+if __name__ == '__main__':
+    pytest.main(['-x', __file__])

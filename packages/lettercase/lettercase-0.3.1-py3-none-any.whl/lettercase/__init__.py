6065b745d3c1a5d9bcb6231c87e4d9083408d325
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:07 +0000

    Add package lettercase/0.3.1/lettercase-0.3.1-py3-none-any.whl

---
+++ packages/lettercase/lettercase-0.3.1-py3-none-any.whl/lettercase/__init__.py
@@ -0,0 +1,10 @@
+"""Library for converting between letter cases."""
+
+from .detect import *
+from .letter_case import *
+from .tools import *
+
+# needs to come after all other imports
+from .converters import *
+
+__version__ = "0.3.1"

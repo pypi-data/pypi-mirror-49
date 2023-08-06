10d814762d811b897a63f6b7ec3683c5a56ed5c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:27 +0000

    Add package pycopy-serial/0.4.1/pycopy-serial-0.4.1.tar.gz

---
+++ packages/pycopy-serial/pycopy-serial-0.4.1.tar.gz/pycopy-serial-0.4.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='pycopy-serial',
+      version='0.4.1',
+      description="pySerial-like module for Pycopy unix port (https://github.com/pfalcon/pycopy).",
+      url='https://github.com/pfalcon/pycopy-serial',
+      author='Paul Sokolovsky',
+      author_email='pfalcon@users.sourceforge.net',
+      license='MIT',
+      py_modules=['serial'],
+      install_requires=['pycopy-os', 'pycopy-fcntl'])

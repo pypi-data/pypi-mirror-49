6e2e4b9794b4c6a24884209f48ef97e72516d3db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:02 +0000

    Add package crowdwiz/0.2.0/crowdwiz-0.2.0-py3-none-any.whl

---
+++ packages/crowdwiz/crowdwiz-0.2.0-py3-none-any.whl/crowdwizbase/chains.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+known_chains = {
+	"CWD": {
+		"chain_id": "4bb7d961ed2ed1b2b618711b1bd4d8d905abe061fcb9c79d1ba15468eadfa998",
+		"core_symbol": "CWD",
+		"prefix": "CWD",
+	}
+}

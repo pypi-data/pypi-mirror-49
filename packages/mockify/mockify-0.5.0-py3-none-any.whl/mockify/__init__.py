bb2c0b4499a3202414cd8bae993655b71e8bb58d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:31 +0000

    Add package mockify/0.5.0/mockify-0.5.0-py3-none-any.whl

---
+++ packages/mockify/mockify-0.5.0-py3-none-any.whl/mockify/__init__.py
@@ -0,0 +1,18 @@
+# ---------------------------------------------------------------------------
+# mockify/__init__.py
+#
+# Copyright (C) 2018 - 2019 Maciej Wiatrzyk
+#
+# This file is part of Mockify library and is released under the terms of the
+# MIT license: http://opensource.org/licenses/mit-license.php.
+#
+# See LICENSE for details.
+# ---------------------------------------------------------------------------
+
+"""Library core module."""
+
+from ._engine import assert_satisfied, Call, Expectation, Registry
+
+__all__ = ['Call', 'Expectation', 'Registry', 'assert_satisfied']
+
+version = (0, 5, 0)

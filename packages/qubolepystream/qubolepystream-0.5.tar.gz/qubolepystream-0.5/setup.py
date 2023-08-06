bb870b292d6a027e2dd902a3ee38e4af3c61cb81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:52 +0000

    Add package qubolepystream/0.5/qubolepystream-0.5.tar.gz

---
+++ packages/qubolepystream/qubolepystream-0.5.tar.gz/qubolepystream-0.5/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+from os import path
+
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(name='qubolepystream',
+      version='0.5',
+      description='Simple and easy to use Qubole connector',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/achilleasatha/PyQubole',
+      author='Achilleas Athanasiou Fragkoulis',
+      author_email='achilleasatha@gmail.com',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      zip_safe=False,
+      install_requires=[
+            'qds_sdk',
+            're',
+            'os',
+            'pandas',
+            'json',
+            'time',
+            'warnings',
+            'io'
+      ]
+      )

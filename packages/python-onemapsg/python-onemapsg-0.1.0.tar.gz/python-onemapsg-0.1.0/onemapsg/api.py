6c405aecf37d3227d1b3e374cbb85ad09c5f5b72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:30 +0000

    Add package python-onemapsg/0.1.0/python-onemapsg-0.1.0.tar.gz

---
+++ packages/python-onemapsg/python-onemapsg-0.1.0.tar.gz/python-onemapsg-0.1.0/onemapsg/api.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+"""
+onemapsg.api
+~~~~~~~~~~~~
+
+This module contains the static API endpoints of `OneMap SG`_.
+
+.. _OneMap SG:
+https://docs.onemap.sg/
+"""
+
+BASE_URL = 'https://developers.onemap.sg/'
+
+endpoints = dict(
+    auth='privateapi/auth/post/getToken',
+    search='commonapi/search',
+    route='privateapi/routingsvc/route',
+    reverse_geocode='privateapi/commonsvc/revgeocodexy',
+    reverse_geocode_wgs84='privateapi/commonsvc/revgeocodexy',
+    reverse_geocode_svy21='privateapi/commonsvc/revgeocodexy'
+)
+
+
+class APISingleton:
+    """
+    Maps endpoints into class attributes and prefixes the
+    base URL.
+    """
+
+    def __init__(self):
+        for k, v in endpoints.items():
+            setattr(self, k, f'{BASE_URL}{v}')
+
+
+API = APISingleton()

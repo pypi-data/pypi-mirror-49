539c7154ab7e6d55fdf9ca56b2aefba6175ab68d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:11 +0000

    Add package numstranslate/1.0.0/numstranslate-1.0.0.tar.gz

---
+++ packages/numstranslate/numstranslate-1.0.0.tar.gz/numstranslate-1.0.0/README.md
@@ -0,0 +1,9 @@
+Перевод чисел из одной системы счисления в другую.
+
+Пример
+```
+binary_string = dec_to_bin(23, bit_length=10)
+>>> '0000010111'
+decimal_number = bin_to_dec(binary_string)
+>>> 23
+```
---
+++ packages/numstranslate/numstranslate-1.0.0.tar.gz/numstranslate-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name='numstranslate',
+    version='1.0.0',
+    author='Xalion',
+    author_email='xaligineer@gmail.com',
+    url='https://github.com/Xalion67/py/blob/master/number_translator.py',
+    description='Dou API wrapper',
+    license='MIT',
+    packages=['numstranslate']
+)

d7924051cc4c93d724b8d250c9dd976b4e636946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:11 +0000

    Add package preved/0.1.5/preved-0.1.5.tar.gz

---
+++ packages/preved/preved-0.1.5.tar.gz/preved-0.1.5/README.txt
@@ -0,0 +1,7 @@
+hello package example
+
+```
+rm -rf dist
+python3 setup.py sdist
+twine upload dist/*
+```
---
+++ packages/preved/preved-0.1.5.tar.gz/preved-0.1.5/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="preved",
+    version="0.1.5",
+    py_modules=["helloworld"],
+    package_dir={"": "src"},
+)

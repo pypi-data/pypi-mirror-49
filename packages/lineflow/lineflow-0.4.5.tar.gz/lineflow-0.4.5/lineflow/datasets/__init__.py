d9daf0fa39b404b3ed969e9b682450adff5d20d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:23 +0000

    Add package lineflow/0.4.5/lineflow-0.4.5.tar.gz

---
+++ packages/lineflow/lineflow-0.4.5.tar.gz/lineflow-0.4.5/lineflow/datasets/__init__.py
@@ -0,0 +1,10 @@
+from .seq2seq import Seq2SeqDataset  # NOQA
+
+from .cnn_dailymail import CnnDailymail  # NOQA
+from .imdb import Imdb  # NOQA
+from .msr_paraphrase import MsrParaphrase  # NOQA
+from .penn_treebank import PennTreebank  # NOQA
+from .small_parallel_enja import SmallParallelEnJa  # NOQA
+from .squad import Squad  # NOQA
+from .wikitext103 import WikiText103  # NOQA
+from .wikitext2 import WikiText2  # NOQA

d8faac168ecdcbd8f947ee798fb5170ca5b75858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:46 +0000

    Add package python-domain-equations/0.0.11/python_domain_equations-0.0.11-py3-none-any.whl

---
+++ packages/python-domain-equations/python_domain_equations-0.0.11-py3-none-any.whl/domain_equations/__init__.py
@@ -0,0 +1,15 @@
+"""
+@copyright: 2018 by Pauli Rikula <pauli.rikula@gmail.com>
+
+@license: MIT <http://www.opensource.org/licenses/mit-license.php>
+
+
+Generate and represent domain model classes via category-like equations which
+can be simplified to get the optimal class structure for the modeled domain.
+
+"""
+
+from .property_graph import PropertyGraph
+
+
+__all__ = ['PropertyGraph']

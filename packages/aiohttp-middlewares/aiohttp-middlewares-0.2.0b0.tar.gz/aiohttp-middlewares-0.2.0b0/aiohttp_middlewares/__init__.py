0d07575e0c3c4bf12204fe03c99fb9bc4756adbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:17 +0000

    Add package aiohttp-middlewares/0.2.0b0/aiohttp-middlewares-0.2.0b0.tar.gz

---
+++ packages/aiohttp-middlewares/aiohttp-middlewares-0.2.0b0.tar.gz/aiohttp-middlewares-0.2.0b0/aiohttp_middlewares/__init__.py
@@ -0,0 +1,24 @@
+"""
+===========
+Middlewares
+===========
+
+Collection of useful middlewares for aiohttp applications.
+
+"""
+
+__version__ = "0.2.0b0"
+
+from .constants import IDEMPOTENT_METHODS, NON_IDEMPOTENT_METHODS
+from .https import https_middleware
+from .shield import shield_middleware
+from .timeout import timeout_middleware
+
+# Make flake8 happy
+(
+    https_middleware,
+    IDEMPOTENT_METHODS,
+    NON_IDEMPOTENT_METHODS,
+    shield_middleware,
+    timeout_middleware,
+)

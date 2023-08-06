d7c6c3bb218a47cf5e6efd690d7644d323e7bca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:35 +0000

    Add package picturebot/0.0.1/picturebot-0.0.1.tar.gz

---
+++ packages/picturebot/picturebot-0.0.1.tar.gz/picturebot-0.0.1/HISTORY.rst
@@ -0,0 +1,14 @@
+=======
+History
+=======
+
+0.0.1 (2019-07-29)
+------------------
+
+* First release on PyPI.
+* Create workplace
+* Rename files
+* Added a config file
+* Updated tox config files
+* Updated travis config files
+* Empty unit tests
---
+++ packages/picturebot/picturebot-0.0.1.tar.gz/picturebot-0.0.1/tests/test_picturebot.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `picturebot` package."""
+
+import os
+import random
+import unittest
+
+class TestGeneralUtils(unittest.TestCase):
+
+    def test_FullPath(self):
+        pass
+
+if __name__ == '__main__':
+	unittest.main()

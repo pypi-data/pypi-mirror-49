db1713f0890b4f22612b49f611d21d89ada5510e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:34 +0000

    Add package robojslib/0.1.7/robojslib-0.1.7.tar.gz

---
+++ packages/robojslib/robojslib-0.1.7.tar.gz/robojslib-0.1.7/robojslib/__init__.py
@@ -0,0 +1,8 @@
+from robojslib import robojslib
+
+__author__ = 'Andrea Gubellini'
+__email__ = 'agubellini@yahoo.com'
+
+class robojslib1:
+    ROBOT_LIBRARY_SCOPE = "GLOBAL"
+    ROBOT_LIBRARY_DOC_FORMAT = "ROBOT"

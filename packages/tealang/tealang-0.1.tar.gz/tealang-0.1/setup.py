51819b805a34b9c06ea47e1643dab39c53948a37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:31 +0000

    Add package tealang/0.1/tealang-0.1.tar.gz

---
+++ packages/tealang/tealang-0.1.tar.gz/tealang-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='tealang',
+      version='0.1',
+      description='Tea: A High-level Language and Runtime System to Automate Statistical Analysis',
+      url='https://github.com/emjun/tea-lang',
+      author='Eunice Jun',
+      author_email='emjun@cs.washington.edu',
+      license='Apache License 2.0',
+      packages=find_packages()
+)

f81719dfaa38ce2909dfc2e2cbdd2aedf7266bf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:00 +0000

    Add package bdata/3.1.1/bdata-3.1.1.tar.gz

---
+++ packages/bdata/bdata-3.1.1.tar.gz/bdata-3.1.1/bdata/__init__.py
@@ -0,0 +1,11 @@
+from bdata.bdata import bdata
+from bdata.bdata import life
+from bdata.bdata import bdict
+from bdata.bdata import bvar
+from bdata.bdata import bhist
+from bdata.bdata import bscaler
+from bdata import mudpy
+
+__all__ = ['bdata','mudpy']
+__version__ = '3.1.1'
+__author__ = 'Derek Fujimoto'

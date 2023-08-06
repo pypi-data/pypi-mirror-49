9c340c877b117f19c153b015f9b84ef3251a9f82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:57 +0000

    Add package async-udemy-dl/0.1.0/async-udemy-dl-0.1.0.tar.gz

---
+++ packages/async-udemy-dl/async-udemy-dl-0.1.0.tar.gz/async-udemy-dl-0.1.0/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# encoding: utf-8
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="async-udemy-dl",
+    version="0.1.0",
+    author="wq2",
+    author_email="smilingwang2@gmail.com",
+    description="simple script to asynchronously download udemy course",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Firkraag/async-udemy-dl",
+    packages=setuptools.find_packages(),
+    py_modules=['async_udemy_dl'],
+    python_requires='>=3.7',
+    install_requires=['requests', 'aiohttp'],
+    entry_points = {
+        'console_scripts': [
+            'async-udemy-dl = async_udemy_dl:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

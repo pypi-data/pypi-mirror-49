19c8f404e78dab3fc5f4401991a01537cd4dd6ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:50 +0000

    Add package snifhex/0.1.1/snifhex-0.1.1.tar.gz

---
+++ packages/snifhex/snifhex-0.1.1.tar.gz/snifhex-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="snifhex",
+    version="0.1.1",
+    description="collection of scripts made by snifhex.",
+    py_modules=["hello"],
+    package_dir={"": 'src'},
+)

fd88e9a7c2ee4749214c86b1692cc483ed46a500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:13 +0000

    Add package treedoc/0.4.2/treedoc-0.4.2.tar.gz

---
+++ packages/treedoc/treedoc-0.4.2.tar.gz/treedoc-0.4.2/treedoc/tests/treedoctestpackage/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 21 21:14:43 2019
+
+@author: tommy
+"""
+
+from treedoctestpackage.module import MyClass
+from treedoctestpackage.subpackage.subpackagemodule import func_subtraction
+
+MyClass = MyClass
+func_subtraction = func_subtraction

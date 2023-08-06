70af45fb2a8015b272414b3659eb13886bef2e91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:03:43 +0000

    Add package python-redis-cache/1.1.0/python-redis-cache-1.1.0.tar.gz

---
+++ packages/python-redis-cache/python-redis-cache-1.1.0.tar.gz/python-redis-cache-1.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='python-redis-cache',
+      version='1.1.0',
+      description='Basic Redis caching for functions',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='http://github.com/taylorhakes/python-redis-cache',
+      author='Taylor Hakes',
+      license='MIT',
+      packages=find_packages(),
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest', 'mock', 'fakeredis'],
+)

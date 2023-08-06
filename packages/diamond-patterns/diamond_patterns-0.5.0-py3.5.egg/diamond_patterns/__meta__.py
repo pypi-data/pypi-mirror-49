3265dcd18b6c038c29e45d0ffbf30bb849351f79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:25 +0000

    Add package diamond-patterns/0.5.0/diamond_patterns-0.5.0-py3.5.egg

---
+++ packages/diamond-patterns/diamond_patterns-0.5.0-py3.5.egg/diamond_patterns/__meta__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '0.5.0'
+__project__ = 'diamond-patterns'
+__author__ = 'Ian Dennis Miller'
+__email__ = 'iandennismiller@gmail.com'
+__url__ = 'http://diamond-patterns.readthedocs.io/'
+__copyright__ = '2019 Ian Dennis Miller'

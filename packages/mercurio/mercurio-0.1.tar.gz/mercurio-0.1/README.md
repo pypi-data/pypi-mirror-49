ded265e5dba5ff79f5e9f8c0df378493565a80cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:11 +0000

    Add package mercurio/0.1/mercurio-0.1.tar.gz

---
+++ packages/mercurio/mercurio-0.1.tar.gz/mercurio-0.1/README.md
@@ -0,0 +1,8 @@
+Mercurio
+=========
+
+Send email with python, my way
+
+A simple class to send email with python, managing
+smtp, attachments and mimetypes, multiple recipients,
+attachments with a specific content id (to html email templating)...
---
+++ packages/mercurio/mercurio-0.1.tar.gz/mercurio-0.1/main.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from mercurio import mercurio
+
+if __name__ == '__main__':
+
+    hg = mercurio(
+        host="smtp.test.com",
+        port=25,
+        smtp_user="myuser",
+        smtp_pwd="mypwd"
+    )
+
+    hg.send(
+        fromaddress="from@me.com",
+        recipients=["to@me.com"],
+        reply_to_address="reply@to.me",
+        text="a long text, very long.",
+        sbj="The subject, or the object?",
+        attachments=[],
+        cid_attachments={"content_id_01": "mylogo.png"}
+    )
+
+    # to debug purposes, you can create the message and save it on disk
+    # without send it thgrougth smtp.
+    message = hg.make_message(
+        fromaddress="from@me.com",
+        recipients=["to@me.com"],
+        reply_to_address="reply@to.me",
+        text="a long text, very long.",
+        sbj="The subject, or the object?",
+        attachments=[],
+        cid_attachments={"content_id_01": "mylogo.png"}
+    )
+
+    with open('test_email.eml', 'w') as o:
+        o.write(message.as_string())

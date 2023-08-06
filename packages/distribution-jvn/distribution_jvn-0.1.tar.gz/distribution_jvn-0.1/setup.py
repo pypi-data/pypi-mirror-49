3c21eddf12e8858c8c6b84e77070d78f5aa969f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:53 +0000

    Add package distribution-jvn/0.1/distribution_jvn-0.1.tar.gz

---
+++ packages/distribution-jvn/distribution_jvn-0.1.tar.gz/distribution_jvn-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distribution_jvn',
+      version='0.1',
+      description='Gaussian and Boolean distributions',
+      packages=['distribution_jvn'],
+      zip_safe=False)

c59f0b34338ddcb8510e42399a83aebe211d73a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:40:37 +0000

    Add package load-remote/1.0/load_remote-1.0.tar.gz

---
+++ packages/load-remote/load_remote-1.0.tar.gz/load_remote-1.0/load/load_remote.py
@@ -0,0 +1,18 @@
+import imp
+import sys
+import subprocess
+
+
+def urllib_model(host, path):
+    ssh = subprocess.Popen(['ssh', host, 'cat', path],
+                           stdout=subprocess.PIPE)
+    u = ssh.stdout.read()
+
+    source = u.decode('utf-8')
+    mod = sys.modules.setdefault(path, imp.new_module(path))
+    code = compile(source, path, 'exec')
+    mod.__file__ = path
+    mod.__package__ = ''
+    exec(code, mod.__dict__)
+    return mod
+

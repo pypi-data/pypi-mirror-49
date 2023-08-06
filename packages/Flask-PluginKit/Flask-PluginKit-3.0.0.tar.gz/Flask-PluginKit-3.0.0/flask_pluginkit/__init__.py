60f6374415a2d4f1d92b4239b00a3d3ce5c11e7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:10 +0000

    Add package Flask-PluginKit/3.0.0/Flask-PluginKit-3.0.0.tar.gz

---
+++ packages/Flask-PluginKit/Flask-PluginKit-3.0.0.tar.gz/Flask-PluginKit-3.0.0/flask_pluginkit/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+    flask_pluginkit
+    ~~~~~~~~~~~~~~~
+
+    Load and run plugins for your Flask application
+
+    :copyright: (c) 2019 by staugur.
+    :license: BSD 3-Clause, see LICENSE for more details.
+"""
+
+from .pluginkit import PluginManager
+from .utils import Flask, LocalStorage, RedisStorage
+
+__author__ = "staugur <staugur@saintic.com>"
+
+__version__ = "3.0.0"
+
+__all__ = [
+    "Flask",
+    "PluginManager",
+    "LocalStorage",
+    "RedisStorage",
+]
---
+++ packages/Flask-PluginKit/Flask-PluginKit-3.0.0.tar.gz/Flask-PluginKit-3.0.0/flask_pluginkit/exceptions.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+    flask_pluginkit.exceptions
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Exception Classes
+
+    :copyright: (c) 2019 by staugur.
+    :license: BSD 3-Clause, see LICENSE for more details.
+"""
+
+
+class PluginError(Exception):
+    pass
+
+
+class VersionError(PluginError):
+    pass
+
+
+class PEPError(PluginError):
+    pass
+
+
+class TemplateNotFound(PluginError):
+    pass

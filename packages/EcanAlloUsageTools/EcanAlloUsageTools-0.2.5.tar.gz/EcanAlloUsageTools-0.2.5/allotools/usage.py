02d00d018fee9b38bacf0bbdc3c447d4a9dd7d4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:25 +0000

    Add package EcanAlloUsageTools/0.2.5/EcanAlloUsageTools-0.2.5.tar.gz

---
+++ packages/EcanAlloUsageTools/EcanAlloUsageTools-0.2.5.tar.gz/EcanAlloUsageTools-0.2.5/allotools/usage.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 16 08:51:31 2019
+
+@author: michaelek
+"""
+import numpy as np
+import pandas as pd
+from allotools.filters import allo_filter
+
+#####################################
+### Functions

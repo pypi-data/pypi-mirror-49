51c50a3223d238a0542e79f421c6b6bea40a35fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:28 +0000

    Add package numdoclint/0.0.9/numdoclint-0.0.9.tar.gz

---
+++ packages/numdoclint/numdoclint-0.0.9.tar.gz/numdoclint-0.0.9/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+description = 'Numdoc Lint provides features such as '\
+              'NumPy style docstring code checking.'
+
+long_description = \
+    'Numdoc Lint provides features such as NumPy style '\
+    'docstring code checking.\n\nPlease see github for more detail.'\
+    '\nhttps://github.com/simon-ritchie/numdoclint'
+
+setup(
+    name='numdoclint',
+    version='0.0.9',
+    url='https://github.com/simon-ritchie/numdoclint',
+    author='simon-ritchie',
+    author_email='',
+    maintainer='simon-ritchie',
+    maintainer_email='',
+    description=description,
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['six'],
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+),

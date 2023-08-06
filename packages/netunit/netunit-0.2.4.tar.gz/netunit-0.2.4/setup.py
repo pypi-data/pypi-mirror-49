9d96585e7896d283d5e1fb9dbbaf23c7d5a8ca8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:26 +0000

    Add package netunit/0.2.4/netunit-0.2.4.tar.gz

---
+++ packages/netunit/netunit-0.2.4.tar.gz/netunit-0.2.4/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+try:
+    with open("README.md", "r") as fh:
+        long_description = fh.read()
+except FileNotFoundError:
+    long_description = ""
+
+setup(name="netunit",
+      version="0.2.4",
+      description="Network testing framework",
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author="Marco Hoyer",
+      author_email="marco_hoyer@gmx.de",
+      url="https://github.com/marco-hoyer/netunit",
+      packages=["netunit"],
+      )

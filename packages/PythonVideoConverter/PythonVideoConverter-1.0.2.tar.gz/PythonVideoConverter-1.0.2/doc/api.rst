7ac4b5ca9f99daa720c23faa30f9d9b589e2812c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:22 +0000

    Add package PythonVideoConverter/1.0.2/PythonVideoConverter-1.0.2.tar.gz

---
+++ packages/PythonVideoConverter/PythonVideoConverter-1.0.2.tar.gz/PythonVideoConverter-1.0.2/doc/api.rst
@@ -0,0 +1,35 @@
+API Reference
+=============
+
+Python Video Converter high-level API
+------------------------------
+
+.. automodule:: converter
+    :members:
+
+Container formats
+-----------------
+
+.. automodule:: converter.formats
+    :members:
+
+Audio and video codecs
+----------------------
+
+.. automodule:: converter.codecs
+    :members:
+
+.. automodule:: converter.codecs.audio
+    :members:
+
+.. automodule:: converter.codecs.subtitle
+    :members:
+
+.. automodule:: converter.codecs.video
+    :members:
+
+Low-level ffmpeg wrapper
+------------------------
+
+.. automodule:: converter.ffmpeg
+    :members:
---
+++ packages/PythonVideoConverter/PythonVideoConverter-1.0.2.tar.gz/PythonVideoConverter-1.0.2/setup.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, Command
+import os
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+class TestCommand(Command):
+    user_options = []
+
+    def initialize_options(self):
+        self._testdir = os.path.join(os.getcwd(), 'test')
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        os.chdir(self._testdir)
+        retval = os.system('python3.7 -m test')
+        if retval != 0:
+            raise Exception('tests failed')
+
+
+class DocCommand(Command):
+    user_options = []
+
+    def initialize_options(self):
+        self._docdir = os.path.join(os.getcwd(), 'doc')
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        os.chdir(self._docdir)
+        os.system('make html')
+
+
+setup(
+    name='PythonVideoConverter',
+    version='1.0.2',
+    description='Video Converter library',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://gitlab.com/jamieoglindsey0/python-video-converter/-/archive/1.0.2/python-video-converter-1.0.2.tar.gz',
+    author='Jamie Lindsey',
+    author_email='admin@materialwebdesign.online',
+    cmdclass={
+        'test': TestCommand,
+        'doc': DocCommand
+    },
+
+    packages=[
+        'converter',
+        'converter.codecs',
+    ],
+
+    setup_requires=[
+        'six',
+    ],
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)

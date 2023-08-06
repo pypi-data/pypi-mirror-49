abc2ddb5574cc6d4e7ee2a2dd2308af15f69f09b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:35 +0000

    Add package cloudera-director-python-client/6.3.0/cloudera-director-python-client-6.3.0.tar.gz

---
+++ packages/cloudera-director-python-client/cloudera-director-python-client-6.3.0.tar.gz/cloudera-director-python-client-6.3.0/cloudera/director/common/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from cloudera.director.common.versions_api import VersionsApi
+
+# import models into sdk package
+from cloudera.director.common.models.version import Version
+
---
+++ packages/cloudera-director-python-client/cloudera-director-python-client-6.3.0.tar.gz/cloudera-director-python-client-6.3.0/setup.py
@@ -0,0 +1,48 @@
+# coding: utf-8
+
+"""
+Licensed to Cloudera, Inc. under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  Cloudera, Inc. licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+
+from setuptools import setup, find_packages  # noqa: H301
+
+NAME = "cloudera-director-python-client"
+VERSION = "6.3.0"
+# To install the library, run the following
+#
+# python setup.py install
+#
+# prerequisite: setuptools
+# http://pypi.python.org/pypi/setuptools
+
+REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil"]
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="Cloudera Altus Director API",
+    author_email="",
+    url="",
+    keywords=["Swagger", "Cloudera Altus Director API"],
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    include_package_data=True,
+    long_description="""\
+    Software-defined EDH clusters on cloud infrastructure  # noqa: E501
+    """
+)

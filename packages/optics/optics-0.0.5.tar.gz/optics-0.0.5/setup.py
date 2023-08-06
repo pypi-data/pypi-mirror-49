e81516eb4f3a7df199dc2cecc0592df273a96cf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:56 +0000

    Add package optics/0.0.5/optics-0.0.5.tar.gz

---
+++ packages/optics/optics-0.0.5.tar.gz/optics-0.0.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='optics',
+      version='0.0.5',
+      description='Transfer matrix simulation of 1D optical multilayers',
+      url='http://github.com/remy1618/optics',
+      author='Remy Ko',
+      author_email='remy.ko@mail.utoronto.ca',
+      license='MIT',
+      packages=['optics'],
+      include_package_data=True,
+      python_requires='>=2.7, !=3.*, <4',
+      install_requires=[
+          'numpy>=1.7',
+          'matplotlib>=1.1'
+          'scikit-image>=0.14'
+      ])

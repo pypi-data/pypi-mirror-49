4f84632d42ee3add655c1fa8f9b0e0e5224655a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:01 +0000

    Add package bryte/0.1.1/bryte-0.1.1-py3-none-any.whl

---
+++ packages/bryte/bryte-0.1.1-py3-none-any.whl/bryte/__init__.py
@@ -0,0 +1,13 @@
+# __init__.py
+
+# Version of the bryte package
+name = "I can delete this, it's just for testing, naija!"
+
+__author__      = "Mike Shobes"
+__copyright__ = "Copyright 2019, The NextPhase Project"
+__credits__ = ["Mike Shobes"]
+__license__ = "MIT"
+__version__ = "0.0.9"
+__maintainer__ = "Mike Shobes"
+__email__ = "mikeshobes718@yahoo.com"
+__status__ = "Development"

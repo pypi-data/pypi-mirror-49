87134b69421f079b02c0a6d2a9da9eeb443aeedf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:37 +0000

    Add package indy-plenum/1.9.0.dev848/indy-plenum-1.9.0.dev848.tar.gz

---
+++ packages/indy-plenum/indy-plenum-1.9.0.dev848.tar.gz/indy-plenum-1.9.0.dev848/plenum/test/input_validation/test_client_request.py
@@ -0,0 +1,203 @@
+import pytest
+
+from plenum.common.constants import CURRENT_PROTOCOL_VERSION
+from plenum.common.messages.client_request import ClientMessageValidator
+from plenum.common.types import f, OPERATION
+
+
+@pytest.fixture(params=['operation_schema_is_strict', 'operation_schema_is_not_strict',
+                        'schema_is_strict', 'schema_is_not_strict'])
+def validator(request):
+    operation_schema_is_strict = request.param == 'operation_schema_is_strict'
+    schema_is_strict = request.param == 'schema_is_strict'
+    return ClientMessageValidator(operation_schema_is_strict=operation_schema_is_strict,
+                                  schema_is_strict=schema_is_strict)
+
+
+@pytest.fixture
+def request_dict(operation, taa_acceptance):
+    return {f.IDENTIFIER.nm: "1" * 16,
+            f.REQ_ID.nm: 1,
+            OPERATION: operation,
+            f.SIG.nm: "signature",
+            f.DIGEST.nm: "digest",
+            f.TAA_ACCEPTANCE.nm: taa_acceptance,
+            f.PROTOCOL_VERSION.nm: CURRENT_PROTOCOL_VERSION}
+
+
+def test_minimal_valid(validator, operation):
+    req_dict = {f.IDENTIFIER.nm: "1" * 16,
+                f.REQ_ID.nm: 1,
+                OPERATION: operation,
+                f.PROTOCOL_VERSION.nm: CURRENT_PROTOCOL_VERSION}
+    validator.validate(req_dict)
+
+
+def test_with_signature_valid(validator, operation):
+    req_dict = {f.IDENTIFIER.nm: "1" * 16,
+                f.REQ_ID.nm: 1,
+                OPERATION: operation,
+                f.SIG.nm: "signature",
+                f.PROTOCOL_VERSION.nm: CURRENT_PROTOCOL_VERSION}
+    validator.validate(req_dict)
+
+
+def test_with_digest_valid(validator, operation):
+    req_dict = {f.IDENTIFIER.nm: "1" * 16,
+                f.REQ_ID.nm: 1,
+                OPERATION: operation,
+                f.DIGEST.nm: "digest",
+                f.PROTOCOL_VERSION.nm: CURRENT_PROTOCOL_VERSION}
+    validator.validate(req_dict)
+
+
+def test_with_taa_acceptance_valid(validator, operation, taa_acceptance):
+    req_dict = {f.IDENTIFIER.nm: "1" * 16,
+                f.REQ_ID.nm: 1,
+                OPERATION: operation,
+                f.TAA_ACCEPTANCE.nm: taa_acceptance,
+                f.PROTOCOL_VERSION.nm: CURRENT_PROTOCOL_VERSION}
+    validator.validate(req_dict)
+
+
+def test_with_version_valid(validator, operation):
+    req_dict = {f.IDENTIFIER.nm: "1" * 16,
+                f.REQ_ID.nm: 1,
+                OPERATION: operation,
+                f.PROTOCOL_VERSION.nm: CURRENT_PROTOCOL_VERSION}
+    validator.validate(req_dict)
+
+
+def test_all_valid(validator, request_dict):
+    validator.validate(request_dict)
+
+
+def test_signature_version_valid(validator, operation):
+    req_dict = {f.IDENTIFIER.nm: "1" * 16,
+                f.REQ_ID.nm: 1,
+                OPERATION: operation,
+                f.SIG.nm: "signature",
+                f.PROTOCOL_VERSION.nm: CURRENT_PROTOCOL_VERSION}
+    validator.validate(req_dict)
+
+
+def test_all_identifier_invalid(validator, request_dict):
+    request_dict[f.IDENTIFIER.nm] = "1" * 5
+    with pytest.raises(TypeError) as ex_info:
+        validator.validate(request_dict)
+    ex_info.match(r'b58 decoded value length 5 should be one of \[16, 32\]')
+
+
+def test_all_reqid_invalid(validator, request_dict):
+    request_dict[f.REQ_ID.nm] = -500
+    with pytest.raises(TypeError) as ex_info:
+        validator.validate(request_dict)
+    ex_info.match('negative value')
+
+
+def test_all_operation_invalid(validator, operation_invalid, request_dict):
+    request_dict[OPERATION] = operation_invalid
+    with pytest.raises(TypeError) as ex_info:
+        validator.validate(request_dict)
+    ex_info.match(r'\[ClientNYMOperation\]: b58 decoded value length 1 should be one of \[16, 32\]')
+
+
+def test_less_than_minimal_valid(validator, operation):
+    req_dict = {f.IDENTIFIER.nm: "1" * 16,
+                f.REQ_ID.nm: 1,
+                f.PROTOCOL_VERSION.nm: CURRENT_PROTOCOL_VERSION}
+    with pytest.raises(TypeError) as ex_info:
+        validator.validate(req_dict)
+    ex_info.match('missed fields - operation')
+
+    req_dict = {f.IDENTIFIER.nm: "1" * 16,
+                OPERATION: operation,
+                f.PROTOCOL_VERSION.nm: CURRENT_PROTOCOL_VERSION}
+    with pytest.raises(TypeError) as ex_info:
+        validator.validate(req_dict)
+    ex_info.match('missed fields - reqId')
+
+    req_dict = {f.REQ_ID.nm: 1,
+                OPERATION: operation,
+                f.PROTOCOL_VERSION.nm: CURRENT_PROTOCOL_VERSION}
+    with pytest.raises(TypeError) as ex_info:
+        validator.validate(req_dict)
+    ex_info.match('Missing both signatures and identifier')
+
+
+def test_all_signature_invalid(validator, request_dict):
+    request_dict[f.SIG.nm] = ""
+    with pytest.raises(TypeError) as ex_info:
+        validator.validate(request_dict)
+    ex_info.match("signature can not be empty")
+
+
+def test_all_digest_invalid(validator, request_dict):
+    request_dict[f.DIGEST.nm] = ""
+    with pytest.raises(TypeError) as ex_info:
+        validator.validate(request_dict)
+    ex_info.match('empty string \(digest=\)')
+
+
+def test_all_taa_acceptance_invalid(
+        validator, request_dict, taa_acceptance_invalid):
+    request_dict[f.TAA_ACCEPTANCE.nm] = taa_acceptance_invalid
+    with pytest.raises(
+        TypeError, match=("should be greater than")
+    ):
+        validator.validate(request_dict)
+
+
+def test_all_version_invalid(validator, request_dict):
+    request_dict[f.PROTOCOL_VERSION.nm] = -5
+    with pytest.raises(TypeError) as ex_info:
+        validator.validate(request_dict)
+    ex_info.match('Unknown protocol version value')
+
+
+def test_no_sigs(validator, operation):
+    request_data = {
+        f.IDENTIFIER.nm: "1" * 16,
+        f.REQ_ID.nm: 1,
+        OPERATION: operation,
+        f.SIG.nm: "signature",
+        f.DIGEST.nm: "digest",
+        f.PROTOCOL_VERSION.nm: CURRENT_PROTOCOL_VERSION
+    }
+    validator.validate(request_data)
+
+
+def test_no_idr(validator, operation):
+    request_data = {
+        f.REQ_ID.nm: 1,
+        OPERATION: operation,
+        f.SIGS.nm: {
+            "1" * 16: "signature"
+        },
+        f.DIGEST.nm: "digest",
+        f.PROTOCOL_VERSION.nm: CURRENT_PROTOCOL_VERSION
+    }
+    validator.validate(request_data)
+
+
+def test_no_sigs_and_idr(validator, operation):
+    request_data = {
+        f.REQ_ID.nm: 1,
+        OPERATION: operation,
+        f.SIG.nm: "signature",
+        f.DIGEST.nm: "digest",
+        f.PROTOCOL_VERSION.nm: CURRENT_PROTOCOL_VERSION
+    }
+    with pytest.raises(TypeError) as ex_info:
+        validator.validate(request_data)
+    ex_info.match('Missing both signatures and identifier')
+
+    request_data = {
+        f.REQ_ID.nm: 1,
+        OPERATION: operation,
+        f.DIGEST.nm: "digest",
+        f.PROTOCOL_VERSION.nm: CURRENT_PROTOCOL_VERSION
+    }
+    with pytest.raises(TypeError) as ex_info:
+        validator.validate(request_data)
+    ex_info.match('Missing both signatures and identifier')

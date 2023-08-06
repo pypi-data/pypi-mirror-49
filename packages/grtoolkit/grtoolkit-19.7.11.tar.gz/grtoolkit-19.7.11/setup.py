d684bbe5d425235fef2dc16ba74d75be472b68a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:37 +0000

    Add package grtoolkit/19.7.11/grtoolkit-19.7.11.tar.gz

---
+++ packages/grtoolkit/grtoolkit-19.7.11.tar.gz/grtoolkit-19.7.11/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+import os, sys
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="grtoolkit",
+    version="19.07.11",
+    author="Gabriel Rosales",
+    author_email="gabriel.alejandro.rosales@gmail.com",
+    description="Common functions for quick python development",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ZenosParadox/grtoolkit",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    scripts=["cmdline\\helloworld.bat"]
+)

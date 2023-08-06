6404a6b14d346bfad9887450bff0dc3306252c37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:11 +0000

    Add package pypi-flow/19.7.53/pypi_flow-19.7.53.tar.gz

---
+++ packages/pypi-flow/pypi_flow-19.7.53.tar.gz/pypi_flow-19.7.53/pypi_flow/__init__.py
@@ -0,0 +1,23 @@
+# Required with every package
+# Initializes package upon import call from python
+#
+# ex. 
+# import package_name.other_python_file
+
+import re, os
+
+def delDotPrefix(string):
+    '''Delete dot prefix to file extension if present'''
+    return string[1:] if string.find(".") == 0 else string
+
+def filesInFolder(folder, fileType="*"):  
+    '''Returns list of files of specified file type'''
+    fileType = delDotPrefix(fileType)
+    file_regex = re.compile(rf".*\.{fileType}", re.IGNORECASE)  # Regular Expression; dot star means find everything
+    file_list = []
+    for dirpath, _, filenames in os.walk(folder,):  # for each folder
+        for file in filenames:
+            file_search = file_regex.findall(file)
+            if file_search:  # if file_search is not empty
+                file_list.append(dirpath + '\\' + file)
+    return file_list

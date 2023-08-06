da19230e759dc8078e54f9940e09cd6273e2a80b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:04 +0000

    Add package currency-cloud/4.1.0/currency_cloud-4.1.0-py2-none-any.whl

---
+++ packages/currency-cloud/currency_cloud-4.1.0-py2-none-any.whl/currencycloud/resources/reference.py
@@ -0,0 +1,29 @@
+from currencycloud.resources.resource import Resource
+
+
+class Currency(Resource):
+    pass
+
+
+class ConversionDates(Resource):
+    pass
+
+
+class SettlementAccount(Resource):
+    pass
+
+
+class BeneficiaryRequiredDetails(Resource):
+    pass
+
+
+class PayerRequiredDetails(Resource):
+    pass
+
+
+class PaymentPurposeCode(Resource):
+    pass
+
+
+class BankDetails(Resource):
+    pass

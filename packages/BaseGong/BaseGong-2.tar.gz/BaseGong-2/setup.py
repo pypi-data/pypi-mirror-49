8335be4e67357185be3550526773954d04ce74ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:55 +0000

    Add package BaseGong/2/BaseGong-2.tar.gz

---
+++ packages/BaseGong/BaseGong-2.tar.gz/BaseGong-2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='BaseGong',
+    version='2',
+    author='gong',
+    author_email='2976560783@qq.com',
+    description='A small example',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/2976560783/GongUtil',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)

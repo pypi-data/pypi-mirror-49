cf01f87921eb24286bd30c0f4a3463606c92f97f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:47 +0000

    Add package echopy-lib/0.1.28/echopy_lib-0.1.28-py3-none-any.whl

---
+++ packages/echopy-lib/echopy_lib-0.1.28-py3-none-any.whl/echopy/echobase/config/object_types.py
@@ -0,0 +1,26 @@
+#: object_types
+class ObjectTypes:
+    def __init__(self):
+        self.NULL = 0
+        self.BASE = 1
+        self.ACCOUNT = 2
+        self.ASSET = 3
+        self.FORCE_SETTLEMENT = 4
+        self.COMMITTEE_MEMBER = 5
+        self.LIMIT_ORDER = 6
+        self.CALL_ORDER = 7
+        self.CUSTOM = 8
+        self.PROPOSAL = 9
+        self.OPERATION_HISTORY = 10
+        self.WITHDRAW_PERMISSION = 11
+        self.VESTING_BALANCE = 12
+        self.BALANCE = 13
+        self.CONTRACT = 14
+        self.CONTRACT_RESULT = 15
+        self.BLOCK_RESULT = 16
+        self.ETH_ADDRESS = 17
+        self.DEPOSIT_ETH = 18
+        self.WITHDRAW_ETH = 19
+        self.ERC20_TOKEN = 20
+        self.DEPOSIT_ERC20_TOKEN = 21
+        self.WITHDRAW_ERC20_TOKEN = 22
---
+++ packages/echopy-lib/echopy_lib-0.1.28-py3-none-any.whl/echopy/echobase/objecttypes.py
@@ -0,0 +1,25 @@
+#: Object types for object ids
+object_type = {}
+object_type["null"] = 0
+object_type["base"] = 1
+object_type["account"] = 2
+object_type["asset"] = 3
+object_type["force_settlement"] = 4
+object_type["committee_member"] = 5
+object_type["limit_order"] = 6
+object_type["call_order"] = 7
+object_type["custom"] = 8
+object_type["proposal"] = 9
+object_type["operation_history"] = 10
+object_type["withdraw_permission"] = 11
+object_type["vesting_balance"] = 12
+object_type["balance"] = 13
+object_type["contract"] = 14
+object_type["contract_result"] = 15
+object_type["block_result"] = 16
+object_type["eth_address"] = 17
+object_type["deposit_eth"] = 18
+object_type["withdraw_eth"] = 19
+object_type["erc20_token"] = 20
+object_type["deposit_erc20_token"] = 21
+object_type["withdraw_erc20_token"] = 22

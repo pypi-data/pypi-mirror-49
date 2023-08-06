bf54d4db9caacaf7d83c52b70e933034c6af6ec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:46 +0000

    Add package niflow-manager/0.0.2/niflow_manager-0.0.2-py3-none-any.whl

---
+++ packages/niflow-manager/niflow_manager-0.0.2-py3-none-any.whl/niflow_manager/cli/main.py
@@ -0,0 +1,27 @@
+import click
+from .. import __version__
+from .init import init
+
+
+@click.group()
+@click.version_option(__version__)
+def main():
+    pass
+
+
+main.command()(init)
+
+
+@main.command()
+def install():
+    print("install command not yet implemented")
+
+
+@main.command()
+def test():
+    print("test command not yet implemented")
+
+
+@main.command()
+def build():
+    print("build command not yet implemented")

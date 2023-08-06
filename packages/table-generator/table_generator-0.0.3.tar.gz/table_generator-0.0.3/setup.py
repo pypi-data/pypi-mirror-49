8cf513ed6505d315fe7e5101d12c9fc9a91ce2cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:08 +0000

    Add package table-generator/0.0.3/table_generator-0.0.3.tar.gz

---
+++ packages/table-generator/table_generator-0.0.3.tar.gz/table_generator-0.0.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="table_generator",
+    version="0.0.3",
+    author="Arin Khare",
+    author_email="arinmkhare@gmail.com",
+    description="Generates code for html table from csv files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lol-cubes/table_generator",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+    ],
+    install_requires=[
+        'lxml',
+    ],
+)

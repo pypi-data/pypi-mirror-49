4f07585d3e90df0f688909dc96e6ad2bdb36b189
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:33 +0000

    Add package python-package-test/0.0.11/python-package-test-0.0.11.tar.gz

---
+++ packages/python-package-test/python-package-test-0.0.11.tar.gz/python-package-test-0.0.11/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python-package-test',
+    version='0.0.11',
+    maintainer='Jack',
+    py_modules=['index', 'fact'],
+    packages=find_packages(),
+    maintainer_email='Jack@email.com',
+    install_requires=[
+        'Click'
+    ],
+    python_requires='>=3.0.*',
+    entry_points={
+        'console_scripts': [
+            'test-cli=lib.index:cli'
+        ]
+    }
+)
+
+
+# setup(
+#     name='python-package-test',
+#     version='0.0.6',
+#     maintainer='Jack',
+#     # py_modules=['index', 'fact'],
+#     packages=find_packages(),
+#     maintainer_email='Jack@email.com',
+#     install_requires=[
+#         'Click'
+#     ],
+#     entry_points='''
+#         [console_scripts]
+#         test-cli=index:cli
+#     '''
+# )

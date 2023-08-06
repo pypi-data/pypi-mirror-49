9fa6e6c3aa91d0d645d14c880b33a5e32caa0f30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:45 +0000

    Add package pyqt5-Uart-Tool/0.1/pyqt5-Uart-Tool-0.1.tar.gz

---
+++ packages/pyqt5-Uart-Tool/pyqt5-Uart-Tool-0.1.tar.gz/pyqt5-Uart-Tool-0.1/README.md
@@ -0,0 +1,4 @@
+**Python PyQt5 widget to open uart serial**
+
+[![Latest PyPI version](https://img.shields.io/badge/pypi-v0.1-green.svg)](https://pypi.org/project/qt-ledwidget/)
+[![License: MIT](https://img.shields.io/dub/l/vibe-d.svg)](https://opensource.org/licenses/MIT)
---
+++ packages/pyqt5-Uart-Tool/pyqt5-Uart-Tool-0.1.tar.gz/pyqt5-Uart-Tool-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='pyqt5-Uart-Tool',
+      version='0.1',
+      packages=['pyqt5UartTool'],
+      install_requires=[
+          'PyQt5',
+      ],
+      include_package_data=True,
+      url='', license='MIT',
+      author='Agatha Kuan',
+      author_email='agathakuannew@gmail.com',
+      description='PyQt5 uart tool in CHN')

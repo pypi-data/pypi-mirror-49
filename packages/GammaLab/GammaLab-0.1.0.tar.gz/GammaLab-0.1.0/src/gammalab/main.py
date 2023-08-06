c2caa046d1287a588b6a31d853544c300c920dc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:45 +0000

    Add package GammaLab/0.1.0/GammaLab-0.1.0.tar.gz

---
+++ packages/GammaLab/GammaLab-0.1.0.tar.gz/GammaLab-0.1.0/src/gammalab/main.py
@@ -0,0 +1,43 @@
+from . import all_services
+
+from threading import Timer
+import time
+try:
+    input=raw_input
+except:
+    pass
+
+def startup():
+    print("startup")
+
+    for s in all_services:
+        s.start()
+
+def shutdown():
+    if all_services:
+        print("shutting down")
+
+    while all_services:
+        s=all_services.pop()
+        try:
+          s.stop()
+          s.close()
+        except Exception as ex:
+          print(ex)
+
+    exit(0)
+
+def main(timeout=None):
+
+    startup()
+
+    if timeout is not None:
+        timer=Timer(timeout, shutdown)
+        timer.start()
+        
+    print("running")
+    input()
+
+    shutdown()
+    
+    

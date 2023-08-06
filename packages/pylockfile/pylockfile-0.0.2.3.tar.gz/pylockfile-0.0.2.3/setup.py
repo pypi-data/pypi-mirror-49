81640be48417fdbf1893ebd54ff52ea4b2efbcab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:04:26 +0000

    Add package pylockfile/0.0.2.3/pylockfile-0.0.2.3.tar.gz

---
+++ packages/pylockfile/pylockfile-0.0.2.3.tar.gz/pylockfile-0.0.2.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+      name='pylockfile',
+      version='0.0.2.3',
+      packages=find_packages(),
+      author='Kusakin Anton @iiiparadoxxxiii',
+      author_email='iiiparadoxxxiii@gmail.com',
+      url='https://github.com/iiiparadoxxxiii/lockfile',
+      )

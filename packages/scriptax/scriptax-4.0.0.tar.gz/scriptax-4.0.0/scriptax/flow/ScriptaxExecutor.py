5c777e9a6e2707e6cf40b05d8e987a52e6bbbaf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:36 +0000

    Add package scriptax/4.0.0/scriptax-4.0.0.tar.gz

---
+++ packages/scriptax/scriptax-4.0.0.tar.gz/scriptax-4.0.0/scriptax/flow/ScriptaxExecutor.py
@@ -0,0 +1,31 @@
+# System import
+
+# Application import
+from scriptax.parser.utils.BoilerPlate import customizable_parser
+from apitaxcore.utilities.Files import getPath
+from apitaxcore.flow.LoadedDrivers import LoadedDrivers
+from apitaxcore.models.State import State
+
+
+# Script is used to automate the execution of many commands
+class Scriptax:
+    def __init__(self, parameters, options):
+        self.options = options
+        self.parameters = parameters
+        self.log = State.log
+
+    # Begins executing the script from top to bottom & handles nested scripts
+    def execute(self, filepath) -> tuple:
+        if self.options.debug:
+            self.log.log('>>> Opening Script: ' + filepath)
+            self.log.log('')
+            self.log.log('')
+
+        if not self.options.driver:
+            self.options.driver = LoadedDrivers.getDefaultDriver()
+
+        scriptax = self.options.driver.getDriverScript(filepath)
+
+        result = customizable_parser(scriptax, parameters=self.parameters, options=self.options, file=getPath(filepath))
+
+        return result

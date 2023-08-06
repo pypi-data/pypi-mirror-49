6dc454fa8888e490ff7355298e7622e5da2e6cbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:02:23 +0000

    Add package vtt-to-srt3/0.1.6/vtt_to_srt3-0.1.6.tar.gz

---
+++ packages/vtt-to-srt3/vtt_to_srt3-0.1.6.tar.gz/vtt_to_srt3-0.1.6/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+
+    long_description = fh.read()
+
+setuptools.setup(
+
+     name='vtt_to_srt3',  
+     version='0.1.6',
+     author="Jeison Cardoso",
+     author_email="cardoso.jeison@gmail.com",
+     description="vtt to srt subtitles converter package",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/0um/vtt-to-srt.py",
+     packages=['vtt_to_srt'],
+     classifiers=[
+         "Programming Language :: Python :: 3.7",
+         "Operating System :: OS Independent",
+     ],
+ )

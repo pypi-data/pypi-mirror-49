77feed635fee01c17afbadfa876a5eafa5022f75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:48 +0000

    Add package mgv/1.0.0/mgv-1.0.0.tar.gz

---
+++ packages/mgv/mgv-1.0.0.tar.gz/mgv-1.0.0/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(name='mgv',
+      version='1.0.0',
+      description='An open-source nodal pipeline manager',
+      url='https://gitlab.com/TheYardVFX/mangrove',
+      author='Bekri Djelloul',
+      author_email='mangrove@theyard-vfx.com',
+      license='GNU v3',
+      packages=setuptools.find_packages())

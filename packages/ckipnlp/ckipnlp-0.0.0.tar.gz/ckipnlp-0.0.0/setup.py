41c69b604f47ce7e1fe8ae673c160c659c3aaa95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:01 +0000

    Add package ckipnlp/0.0.0/ckipnlp-0.0.0.tar.gz

---
+++ packages/ckipnlp/ckipnlp-0.0.0.tar.gz/ckipnlp-0.0.0/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='ckipnlp',
+    version='0.0.0',
+    description='CKIP NLP Wrappers',
+    long_description='Use `pyckip <https://pypi.org/project/pyckip/>`_ instead.',
+    url='https://pypi.org/project/ckipnlp/',
+    python_requires='<3',
+    install_requires=['pyckip'],
+)

eabf9f61dfd8ef4811f5b0ac30f40aace1e2d909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:38 +0000

    Add package reliability/0.1.1/reliability-0.1.1.tar.gz

---
+++ packages/reliability/reliability-0.1.1.tar.gz/reliability-0.1.1/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+def readme():
+    with open("README.md") as f:
+        README = f.read()
+    return README
+
+setuptools.setup(
+    name="reliability",
+    version="0.1.1",
+    description="Reliability Engineering toolkit for Python",
+    author="Matthew Reid",
+    author_email="m.reid854@gmail.com",
+    license="MIT",
+    url="https://github.com/MatthewReid854/reliability",
+    keywords=["reliability","engineering","RAM","weibull","survival analysis","censored data","lifelines","probability distributions","probability","quality"],
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Programming Language :: Python :: 3",
+    ],
+    install_requires=["autograd>=1.2.0",
+                      "scipy>=1.2.1",
+                      "numpy>=1.16.2",
+                      "matplotlib>=3.0.3",
+                      "pandas>=0.23.4",
+                      "autograd-gamma>=0.4.1",
+    ],
+    packages=["reliability"],
+    include_package_data=True,
+)

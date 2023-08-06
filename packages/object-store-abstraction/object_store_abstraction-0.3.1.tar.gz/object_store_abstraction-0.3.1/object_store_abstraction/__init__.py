f6fc00ab10edacd0401c287f6844002ad61c2a76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:56 +0000

    Add package object-store-abstraction/0.3.1/object_store_abstraction-0.3.1.tar.gz

---
+++ packages/object-store-abstraction/object_store_abstraction-0.3.1.tar.gz/object_store_abstraction-0.3.1/object_store_abstraction/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+"""
+    object_store_abstraction
+    ~~~~~~~~
+    Object store abstraction
+    :copyright: (c) 2018 by Robert Metcalf.
+    :license: MIT, see LICENSE for more details.
+"""
+
+#Exception classes from base
+from .objectStores_base import WrongObjectVersionExceptionClass,  SuppliedObjectVersionWhenCreatingExceptionClass, ObjectStoreConfigError,  MissingTransactionContextExceptionClass, UnallowedMutationExceptionClass,  TriedToDeleteMissingObjectExceptionClass, TryingToCreateExistingObjectExceptionClass
+#Exception instances from base
+from .objectStores_base import WrongObjectVersionException, SuppliedObjectVersionWhenCreatingException, MissingTransactionContextException, UnallowedMutationException, TriedToDeleteMissingObjectException, TryingToCreateExistingObjectException
+
+from .objectStores import createObjectStoreInstance, ObjectStoreConfigNotDictObjectExceptionClass, InvalidObjectStoreConfigUnknownTypeClass, InvalidObjectStoreConfigMissingTypeException, InvalidObjectStoreConfigUnknownTypeException, InvalidObjectStoreConfigMissingTypeClass
+
+from .makeDictJSONSerializable import getRJMJSONSerializableDICT, getNormalDICTFromRJMJSONSerializableDICT
+
+#Allow direct testing of types
+from .objectStores_SQLAlchemy import ObjectStore_SQLAlchemy
+from .objectStores_Memory import ObjectStore_Memory
+from .objectStores_SimpleFileStore import ObjectStore_SimpleFileStore
+from .objectStores_DynamoDB import ObjectStore_DynamoDB
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions

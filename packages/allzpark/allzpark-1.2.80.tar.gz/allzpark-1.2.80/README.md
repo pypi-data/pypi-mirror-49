eda7549e78c327134af37a055b1178b61a5407fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:22 +0000

    Add package allzpark/1.2.80/allzpark-1.2.80.tar.gz

---
+++ packages/allzpark/allzpark-1.2.80.tar.gz/allzpark-1.2.80/README.md
@@ -0,0 +1,26 @@
+### Allzpark
+
+Application launcher and environment management tool for games and digital post-production, built on [Rez](https://github.com/mottosso/bleeding-rez).
+
+<br>
+
+### Usage
+
+> In early development. Stay tuned for a release.
+
+Press the **Watch** icon top-right for notifications.
+
+![image](https://user-images.githubusercontent.com/2152766/60902299-4dac9d00-a267-11e9-893d-b3801fa422e9.png)
+
+![ALLZPARK_1](https://user-images.githubusercontent.com/2152766/58943971-bee4c600-8778-11e9-8117-f50fe260cee0.gif)
+
+<br>
+
+### More Gifs
+
+> "launchapp2" was the internal working title during development at [Studio Anima](http://studioanima.co.jp)
+
+![ALLZPARK_2](https://user-images.githubusercontent.com/2152766/58943970-be4c2f80-8778-11e9-9344-66007ba5cb5b.gif)
+![ALLZPARK_3](https://user-images.githubusercontent.com/2152766/58943973-bee4c600-8778-11e9-809a-cf2aaf7c94c0.gif)
+![ALLZPARK_4](https://user-images.githubusercontent.com/2152766/58946617-3cf79b80-877e-11e9-8887-df9a92cb1851.gif)
+![ALLZPARK_8](https://user-images.githubusercontent.com/2152766/58959026-16485d80-879c-11e9-8964-e277490dbf5f.gif)
---
+++ packages/allzpark/allzpark-1.2.80.tar.gz/allzpark-1.2.80/allzpark/cli.py
@@ -0,0 +1,232 @@
+"""Command-line interface to allzpark"""
+
+import os
+import sys
+import time
+import signal
+import logging
+import argparse
+import contextlib
+
+from .version import version
+from . import allzparkconfig
+
+timing = {}
+
+
+def _load_userconfig(fname=None):
+    fname = fname or os.getenv(
+        "ALLZPARK_CONFIG_FILE",
+        os.path.expanduser("~/allzparkconfig.py")
+    )
+
+    mod = {
+        "__file__": fname,
+    }
+
+    with open(fname) as f:
+        exec(compile(f.read(), f.name, 'exec'), mod)
+
+    tell("- Loading custom allzparkconfig..")
+    for key in dir(allzparkconfig):
+        if key.startswith("__"):
+            continue
+
+        try:
+            value = mod[key]
+        except KeyError:
+            continue
+
+        tell("  - %s=%r" % (key, value))
+        setattr(allzparkconfig, key, value)
+
+
+@contextlib.contextmanager
+def timings(title, timing=True):
+    sys.stdout.write(title)
+    t0 = time.time()
+
+    try:
+        yield
+    except Exception:
+        sys.stdout.write("fail\n")
+        raise
+    else:
+        if timing:
+            sys.stdout.write("ok - %.2fs\n" % (time.time() - t0))
+        else:
+            sys.stdout.write("ok\n")
+
+
+def tell(msg):
+    sys.stdout.write("%s\n" % msg)
+
+
+def main():
+    parser = argparse.ArgumentParser("allzpark", description=(
+        "An application launcher built on Rez, "
+        "pass --help for details"
+    ))
+
+    parser.add_argument("-v", "--verbose", action="count", default=0, help=(
+        "Print additional information about Allzpark during operation"))
+    parser.add_argument("--version", action="store_true", help=(
+        "Print version and exit"))
+    parser.add_argument("--clear-settings", action="store_true", help=(
+        "Start fresh with user preferences"))
+    parser.add_argument("--config-file", type=str, help=(
+        "Absolute path to allzparkconfig.py, takes precedence "
+        "over ALLZPARK_CONFIG_FILE"))
+    parser.add_argument("--no-config", action="store_true", help=(
+        "Do not load custom allzparkconfig.py"))
+    parser.add_argument("--root", help=(
+        "Path to where projects live on disk, "
+        "defaults to allzparkconfig.projects_dir"
+    ))
+
+    opts = parser.parse_args()
+
+    if opts.version:
+        tell(version)
+        exit(0)
+
+    print("=" * 30)
+    print(" allzpark (%s)" % version)
+    print("=" * 30)
+
+    with timings("- Loading Rez.. "):
+        try:
+            from rez import __file__ as _rez_location
+            from rez.utils._version import _rez_version
+            from rez.config import config
+        except ImportError:
+            tell("ERROR: allzpark requires rez")
+            exit(1)
+
+    with timings("- Loading Qt.. "):
+        try:
+            from .vendor import Qt
+        except ImportError:
+            tell("ERROR: allzpark requires a Python binding for Qt,\n"
+                 "such as PySide, PySide2, PyQt4 or PyQt5.")
+            exit(1)
+
+        from .vendor import six
+        from .vendor.Qt import QtWidgets, QtCore
+
+    # Provide for vendor dependencies
+    sys.modules["Qt"] = Qt
+    sys.modules["six"] = six
+
+    with timings("- Loading allzpark.. "):
+        from . import view, control, resources, util
+
+    try:
+        _load_userconfig(opts.config_file)
+    except (IOError, OSError):
+        # That's OK
+        pass
+
+    logging.basicConfig(format=(
+        "%(levelname)-8s %(name)s %(message)s" if opts.verbose else
+        "%(message)s"
+    ))
+    logging.getLogger("allzpark.vendor").setLevel(logging.CRITICAL)
+    logging.getLogger(__name__).setLevel(logging.DEBUG
+                                         if opts.verbose >= 2
+                                         else logging.INFO
+                                         if opts.verbose == 1
+                                         else logging.WARNING)
+    log = logging.getLogger(__name__)
+    log.propagate = True
+
+    # Allow the application to die on CTRL+C
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+    config.catch_rex_errors = False
+
+    with timings("- Loading preferences.. "):
+        storage = QtCore.QSettings(QtCore.QSettings.IniFormat,
+                                   QtCore.QSettings.UserScope,
+                                   "Allzpark", "preferences")
+
+        if opts.clear_settings:
+            sys.stdout.write("(clean) ")
+            storage.clear()
+
+        defaults = {
+            "memcachedURI": os.getenv("REZ_MEMCACHED_URI", "None"),
+            "pythonExe": sys.executable,
+            "pythonVersion": ".".join(map(str, sys.version_info)),
+            "qtVersion": Qt.__binding_version__,
+            "qtBinding": Qt.__binding__,
+            "qtBindingVersion": Qt.__qt_version__,
+            "rezLocation": os.path.dirname(_rez_location),
+            "rezVersion": _rez_version,
+            "rezConfigFile": os.getenv("REZ_CONFIG_FILE", "None"),
+            "rezPackagesPath": config.packages_path,
+            "rezLocalPath": config.local_packages_path.split(os.pathsep),
+            "rezReleasePath": config.release_packages_path.split(os.pathsep),
+            "settingsPath": storage.fileName(),
+        }
+
+        for key, value in defaults.items():
+            storage.setValue(key, value)
+
+        if allzparkconfig.startup_project:
+            storage.setValue("startupProject", allzparkconfig.startup_project)
+
+        if allzparkconfig.startup_application:
+            storage.setValue("startupApp", allzparkconfig.startup_application)
+
+    try:
+        __import__("localz")
+        allzparkconfig._localz_enabled = True
+    except ImportError:
+        allzparkconfig._localz_enabled = False
+
+    tell("-" * 30)  # Add some space between boot messages, and upcoming log
+
+    app = QtWidgets.QApplication(sys.argv)
+    ctrl = control.Controller(storage)
+
+    def excepthook(type, value, traceback):
+        """Try handling these from within the controller"""
+
+        # Give handler a chance to remedy the situation
+        handled = ctrl.on_unhandled_exception(type, value, traceback)
+
+        if not handled:
+            sys.__excepthook__(type, value, traceback)
+
+    sys.excepthook = excepthook
+
+    window = view.Window(ctrl)
+    user_css = storage.value("userCss") or ""
+
+    with open(resources.find("style.css")) as f:
+        css = f.read()
+
+        # Store for CSS Editor
+        window._originalcss = css
+
+        window.setStyleSheet("\n".join([css, user_css]))
+
+    window.show()
+
+    def measure():
+        duration = time.time() - timing["beforeReset"]
+        print("- Resolving contexts.. ok %.2fs" % duration)
+
+    def init():
+        timing["beforeReset"] = time.time()
+        root = opts.root or allzparkconfig.projects
+        ctrl.reset(root, on_success=measure)
+
+    # Give the window a moment to appear before occupying it
+    QtCore.QTimer.singleShot(50, init)
+
+    if os.name == "nt":
+        util.windows_taskbar_compat()
+
+    app.exec_()

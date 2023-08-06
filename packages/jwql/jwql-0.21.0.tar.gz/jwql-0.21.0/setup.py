d68833fbc9197995845189e089e7c657a2a3f525
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:41 +0000

    Add package jwql/0.21.0/jwql-0.21.0.tar.gz

---
+++ packages/jwql/jwql-0.21.0.tar.gz/jwql-0.21.0/setup.py
@@ -0,0 +1,47 @@
+import numpy as np
+from setuptools import setup
+from setuptools import find_packages
+
+VERSION = '0.21.0'
+
+AUTHORS = 'Matthew Bourque, Lauren Chambers, Misty Cracraft, Joe Filippazzo, Bryan Hilbert, '
+AUTHORS += 'Graham Kanarek, Catherine Martlin, Johannes Sahlmann'
+
+DESCRIPTION = 'The James Webb Space Telescope Quicklook Project'
+
+REQUIRES = [
+    'astropy',
+    'astroquery>=0.3.9',
+    'authlib',
+    'bokeh>=1.0',
+    'django>=2.0',
+    'jinja2',
+    'jwedb',
+    'jwst',
+    'matplotlib',
+    'numpy',
+    'numpydoc',
+    'pandas',
+    'psycopg2',
+    'pysiaf',
+    'pytest',
+    'sphinx',
+    'sqlalchemy',
+    'stsci_rtd_theme'
+]
+
+setup(
+    name='jwql',
+    version=VERSION,
+    description=DESCRIPTION,
+    url='https://github.com/spacetelescope/jwql.git',
+    author=AUTHORS,
+    author_email='jwql@stsci.edu',
+    license='BSD',
+    keywords=['astronomy', 'python'],
+    classifiers=['Programming Language :: Python'],
+    packages=find_packages(),
+    install_requires=REQUIRES,
+    include_package_data=True,
+    include_dirs=[np.get_include()],
+)

e6d6a3f42f82d6eada23149f48f19224e80eed5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:02 +0000

    Add package budgetwise/0.1/budgetwise-0.1.tar.gz

---
+++ packages/budgetwise/budgetwise-0.1.tar.gz/budgetwise-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='budgetwise',
+      version='0.1',
+      description='Easily calculate budgets for your company',
+      url='https://github.com/kaarelr/budgetwise',
+      author='Cloudberry',
+      author_email='kaarel.reinvars@gmail.com',
+      license='MIT',
+      packages=['budgetwise'],
+      zip_safe=False)

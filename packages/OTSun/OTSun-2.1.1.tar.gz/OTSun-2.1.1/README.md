f5f4ef9e79e49bfb1ec4b550c31394d058d42220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:42 +0000

    Add package OTSun/2.1.1/OTSun-2.1.1.tar.gz

---
+++ packages/OTSun/OTSun-2.1.1.tar.gz/OTSun-2.1.1/README.md
@@ -0,0 +1,9 @@
+# OTSun
+
+OTSun is a python package that uses the Monte Carlo Forward Ray Tracing for the optical analysis of Solar Thermal Collectors and Solar Cells. 
+
+## Installation
+
+## Documentation
+
+The documentation of the module is available at http://otsun.readthedocs.io/
---
+++ packages/OTSun/OTSun-2.1.1.tar.gz/OTSun-2.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+import versioneer
+
+setup(
+    name='OTSun',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    packages=['otsun',],
+    url='https://github.com/bielcardona/OTSun',
+    install_requires=['numpy==1.12', 'enum34' , 'autologging'],
+    license='MIT',
+    author='Gabriel Cardona, Ramon Pujol',
+    author_email='gabriel.cardona@uib.es, ramon.pujol@uib.es',
+    description='Analizer of sun collectors',
+)
+

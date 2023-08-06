48141c783172d9e1347ef1680555a62c53881b53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:24 +0000

    Add package azure-kusto-notebooks/0.1.1/azure-kusto-notebooks-0.1.1.tar.gz

---
+++ packages/azure-kusto-notebooks/azure-kusto-notebooks-0.1.1.tar.gz/azure-kusto-notebooks-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+PACKAGE_NAME = "azure-kusto-notebooks"
+VERSION = '0.1.1'
+INSTALL_REQUIRES = [
+    'azure-kusto-data[pandas]'
+]
+
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    author="zachariah.cox@gmail.com",
+    author_email="zachariah.cox@gmail.com",
+    description="utilities for working with kusto from notebooks",
+    url="https://github.com/zachariahcox/azure-kusto-notebooks",
+    install_requires=INSTALL_REQUIRES,
+    packages=find_packages(exclude=['azure', 'tests']),
+    extras_require={":python_version<'3.0'": ["azure-nspkg"]}
+)

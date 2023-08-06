974022363b3a40d3150678886297c11a26b9e5bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:30 +0000

    Add package makecents-exceptions/0.0.4/makecents-exceptions-0.0.4.tar.gz

---
+++ packages/makecents-exceptions/makecents-exceptions-0.0.4.tar.gz/makecents-exceptions-0.0.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="makecents-exceptions",
+    version="0.0.4",
+    author="MakeCents",
+    author_email="yma@mymakecents.com",
+    description="exceptions package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MakeCents-NYC/makecents-exceptionst",
+    packages=["makecents_exceptions"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)

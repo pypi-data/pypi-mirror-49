b0e3cfcc77c061ba3b6afff39ec67c434c0c13c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:50 +0000

    Add package html-to-csv/0.0.1/html-to-csv-0.0.1.tar.gz

---
+++ packages/html-to-csv/html-to-csv-0.0.1.tar.gz/html-to-csv-0.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    long_description = readme_file.read()
+
+setup(
+    name='html-to-csv',
+    version='0.0.1',
+    description='A utility that extracts tables from html documents and converts them to csv format',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/hanwentao/html2csv',
+    author='Wentao Han',
+    author_email='wentao.han@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords='html table csv convert',
+    packages=find_packages(exclude=['tests']),
+    python_requires='>=3.7',
+    install_requires=['beautifulsoup4', 'lxml'],
+    entry_points={
+        'console_scripts': [
+            'html2csv=html2csv.__main__:main',
+        ],
+    },
+)

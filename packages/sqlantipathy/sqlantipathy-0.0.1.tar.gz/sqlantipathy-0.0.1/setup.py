0b9a0b626325009bd6605b8bc8874beda2b66cce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:19 +0000

    Add package sqlantipathy/0.0.1/sqlantipathy-0.0.1.tar.gz

---
+++ packages/sqlantipathy/sqlantipathy-0.0.1.tar.gz/sqlantipathy-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="sqlantipathy",
+    version="0.0.1",
+    url="https://github.com/pietrogiuffrida/sqlantipathy/",
+    author="Pietro Giuffrida",
+    author_email="pietro.giuffri@gmail.com",
+    license="MIT",
+    packages=["sqlantipathy"],
+    zip_safe=False,
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    description='Python library to work with SQL DB',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
+

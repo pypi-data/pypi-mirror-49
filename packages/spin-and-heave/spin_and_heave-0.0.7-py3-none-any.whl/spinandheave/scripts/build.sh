b7ccb41c7c128f3d8f2391de27cbb8084980d1b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:24 +0000

    Add package spin-and-heave/0.0.7/spin_and_heave-0.0.7-py3-none-any.whl

---
+++ packages/spin-and-heave/spin_and_heave-0.0.7-py3-none-any.whl/spinandheave/scripts/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Read variables from command line arguments
+FILENAME=$1
+RUNTIME=$2
+SOURCE_PATH=$3
+if [[ $2 == *"nodejs"* ]]; then
+    BUILD_CMD="npm install --production"
+else
+    BUILD_CMD="pip install --progress-bar off -r requirements.txt -t ."
+fi
+
+# Convert to absolute paths
+SOURCE_DIR=$(cd "$SOURCE_PATH" && pwd)
+ZIP_DIR=$(cd "$(dirname "$FILENAME")" && pwd)
+ZIP_NAME=$(basename "$FILENAME")
+
+# Install dependencies, using a Docker image to correctly build native extensions
+docker run --rm -t -v "$SOURCE_DIR:/src" -v "$ZIP_DIR:/out" lambci/lambda:build-$RUNTIME sh -c "
+    cp -r /src /build &&
+    cd /build &&
+    $BUILD_CMD &&
+    chmod -R 755 . &&
+    zip -r /out/$ZIP_NAME . &&
+    chown \$(stat -c '%u:%g' /out) /out/$ZIP_NAME
+"
+
+echo "Created $FILENAME from $SOURCE_PATH"

c7b2bbc383dd94d2921e01e0dd5fa22549fbb262
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:05 +0000

    Add package micom/0.9.9/micom-0.9.9.tar.gz

---
+++ packages/micom/micom-0.9.9.tar.gz/micom-0.9.9/micom/__init__.py
@@ -0,0 +1,33 @@
+"""Simple init file for mico."""
+
+from micom.community import Community, load_pickle
+from micom import (
+    algorithms,
+    problems,
+    util,
+    data,
+    duality,
+    media,
+    solution,
+    workflows,
+    representation,
+    workflow_examples,
+)
+
+
+__all__ = (
+    "Community",
+    "algorithms",
+    "problems",
+    "optcom",
+    "util",
+    "data",
+    "duality",
+    "media",
+    "solution",
+    "load_pickle",
+    "workflows",
+    "workflow_examples",
+)
+
+__version__ = "0.9.9"

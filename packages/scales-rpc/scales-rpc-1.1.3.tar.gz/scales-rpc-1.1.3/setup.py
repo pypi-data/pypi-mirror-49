8de6e410b85fe7a9d4cb5e856c1117cf50400741
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:28 +0000

    Add package scales-rpc/1.1.3/scales-rpc-1.1.3.tar.gz

---
+++ packages/scales-rpc/scales-rpc-1.1.3.tar.gz/scales-rpc-1.1.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+  name='scales-rpc',
+  version='1.1.3',
+  author='Steve Niemitz',
+  author_email='sniemitz@twitter.com',
+  url='https://www.github.com/steveniemitz/scales',
+  description='A python RPC client stack',
+  summary='A generic python RPC client framework.',
+  license='MIT License',
+  packages=['scales',
+            'scales.http',
+            'scales.kafka',
+            'scales.loadbalancer',
+            'scales.mux',
+            'scales.pool',
+            'scales.redis',
+            'scales.thrift',
+            'scales.thrifthttp',
+            'scales.thriftmux'],
+  install_requires=[
+      'gevent>=1.3.0',
+      'thrift>=0.5.0,<0.11.0',
+      'kazoo>=2.5.0',
+      'requests>=2.0.0']
+)

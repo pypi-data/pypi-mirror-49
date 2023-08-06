3e2adbe7682a670c6c649098990625df0b17a524
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:07 +0000

    Add package hideout/0.2.2/hideout-0.2.2.tar.gz

---
+++ packages/hideout/hideout-0.2.2.tar.gz/hideout-0.2.2/hideout/utils.py
@@ -0,0 +1,13 @@
+import pickle
+
+from hideout import env
+from hideout.log import logger
+
+
+def freeze(target_object, file_path):
+    if env.HIDEOUT_SUPPRESS_CACHE is True:
+        logger.info("skip saving to cache file ...")
+        return
+    logger.info("saving {}".format(file_path))
+    with open(file_path, mode='wb') as f:
+        return pickle.dump(target_object, f)

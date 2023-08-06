00302ff81ad5841ce7b295b8448c1e89d607ab87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:31 +0000

    Add package Flask-PluginKit/3.1.0/Flask_PluginKit-3.1.0-py2.py3-none-any.whl

---
+++ packages/Flask-PluginKit/Flask_PluginKit-3.1.0-py2.py3-none-any.whl/flask_pluginkit/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+    flask_pluginkit
+    ~~~~~~~~~~~~~~~
+
+    Load and run plugins for your Flask application
+
+    :copyright: (c) 2019 by staugur.
+    :license: BSD 3-Clause, see LICENSE for more details.
+"""
+
+from .pluginkit import PluginManager
+from .utils import Flask, LocalStorage, RedisStorage
+from ._installer import PluginInstaller
+
+__author__ = "staugur <staugur@saintic.com>"
+
+__version__ = "3.1.0"
+
+__all__ = [
+    "Flask",
+    "PluginManager",
+    "LocalStorage",
+    "RedisStorage",
+    "PluginInstaller",
+]

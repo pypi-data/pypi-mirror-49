a808406b69bda38c99d54edbc892ef01fa9e680f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:45 +0000

    Add package package-1398/0.1dev/package-1398-0.1dev.tar.gz

---
+++ packages/package-1398/package-1398-0.1dev.tar.gz/package-1398-0.1dev/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name = 'package-1398',
+    version = '0.1dev',
+    package = 'package-1398',
+    license = 'UT',
+    long_description = open("README.txt").read()
+    )

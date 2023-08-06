8f0020c890b82db62e56b882225ab31ab86e0a74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:21 +0000

    Add package netapp-ocum/0.1.5/netapp_ocum-0.1.5-py2.py3-none-any.whl

---
+++ packages/netapp-ocum/netapp_ocum-0.1.5-py2.py3-none-any.whl/netapp_ocum/client.py
@@ -0,0 +1,95 @@
+from netapp_ocum.http import NetApp_OCUM_HTTP
+from netapp_ocum.objects import NetApp_OCUM_Collection
+from netapp_ocum.settings import NetApp_OCUM_Settings
+
+class NetApp_OCUM_Client(object):
+    """
+    Class object for handling HTTP requests to the OCUM API.
+    """
+    def __init__(self, api_host, api_user, api_password,
+        api_port   = 443,
+        verify_ssl = True,
+        filters    = {}
+    ):
+
+        # Store connection settings
+        self.settings = NetApp_OCUM_Settings(
+            api_host     = api_host,
+            api_user     = api_user,
+            api_password = api_password,
+            api_port     = api_port,
+            verify_ssl   = verify_ssl,
+        )
+
+        # Any filters as defined by the filter() method
+        self.filters = filters
+
+        # Request handler
+        self.request = NetApp_OCUM_HTTP(self.settings)
+
+    def _set_params(self, params):
+        """
+        Set query parameters for the request, merge any filters defined at the
+        class level. Parameters passed to the method via `params` argument
+        take precedence over class level filters.
+        """
+        query_params = params
+        if self.filters:
+            for k,v in self.filters.items():
+                if not k in query_params:
+                    query_params[k] = v
+        return query_params
+
+    def get_clusters(self, params={}):
+        """
+        Return a list of clusters from the OCUM.
+        """
+        return NetApp_OCUM_Collection(self.request, 'clusters', self._set_params(params))
+
+    def get_svms(self, params={}):
+        """
+        Return a list of SVMs from the OCUM.
+        """
+        return NetApp_OCUM_Collection(self.request, 'svms', self._set_params(params))
+
+    def get_nodes(self, params={}):
+        """
+        Return a list of nodes from the OCUM.
+        """
+        return NetApp_OCUM_Collection(self.request, 'nodes', self._set_params(params))
+
+    def get_aggregates(self, params={}):
+        """
+        Return a list of aggregates from the OCUM.
+        """
+        return NetApp_OCUM_Collection(self.request, 'aggregates', self._set_params(params))
+
+    def get_volumes(self, params={}):
+        """
+        Return a list of volumes from the OCUM.
+        """
+        return NetApp_OCUM_Collection(self.request, 'volumes', self._set_params(params))
+
+    def get_ports(self, params={}):
+        """
+        Return a list of ports from the OCUM.
+        """
+        return NetApp_OCUM_Collection(self.request, 'ports', self._set_params(params))
+
+    def get_interfaces(self, params={}):
+        """
+        Return a list of networking interfaces from the OCUM.
+        """
+        return NetApp_OCUM_Collection(self.request, 'interfaces', self._set_params(params))
+
+    def get_luns(self, params={}):
+        """
+        Return a list of LUNs from the OCUM.
+        """
+        return NetApp_OCUM_Collection(self.request, 'luns', self._set_params(params))
+
+    def get_namespaces(self, params={}):
+        """
+        Return a list of namespaces from the OCUM.
+        """
+        return NetApp_OCUM_Collection(self.request, 'namespaces', self._set_params(params))

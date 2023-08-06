b359b6590577bf667b4b2e1f6c1af2fd3de2f482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:51 +0000

    Add package argunparse/0.1.2/argunparse-0.1.2-py3-none-any.whl

---
+++ packages/argunparse/argunparse-0.1.2-py3-none-any.whl/argunparse/__init__.py
@@ -0,0 +1,11 @@
+"""This is "__init__.py" file for "argunparse" package.
+
+classes: ArgumentUnparser
+"""
+
+from .argument_unparser import ArgumentUnparser
+from ._version import VERSION
+
+__version__ = VERSION
+
+__all__ = ['ArgumentUnparser']

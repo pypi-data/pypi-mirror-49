1161b333928bf515cf6c7e315c8b83ec5b05eb4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:26 +0000

    Add package dokuwiki/1.2.1/dokuwiki-1.2.1.tar.gz

---
+++ packages/dokuwiki/dokuwiki-1.2.1.tar.gz/dokuwiki-1.2.1/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+setuptools.setup(
+    name = 'dokuwiki',
+    version = '1.2.1',
+    author = 'François Ménabé',
+    author_email = 'francois.menabe@gmail.com',
+    url = 'http://python-dokuwiki.readthedocs.org/en/latest/',
+    download_url='https://github.com/fmenabe/python-dokuwiki',
+    license='MIT License',
+    description = 'Manage DokuWiki via XML-RPC API.',
+    long_description=open('README.rst').read(),
+    keywords=['xmlrpc', 'dokuwiki'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+    ],
+    py_modules = ['dokuwiki'],
+)

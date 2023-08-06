b1ab3b0215b8989bf12716c0cf1fe8080d7e185b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:13 +0000

    Add package yandex-checkout/1.2.0/yandex_checkout-1.2.0.tar.gz

---
+++ packages/yandex-checkout/yandex_checkout-1.2.0.tar.gz/yandex_checkout-1.2.0/yandex_checkout/domain/common/confirmation_type.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+
+class ConfirmationType:
+    """
+    Constants representing confirmation types. Available values are:
+
+    * yandex_checkout.domain.common.ConfirmationType.EXTERNAL
+    * yandex_checkout.domain.common.ConfirmationType.REDIRECT
+    """
+    EMBEDDED = 'embedded'
+    EXTERNAL = 'external'
+    REDIRECT = 'redirect'

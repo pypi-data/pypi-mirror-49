4364eb7364a2320decdb4da61568591ae8923aea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:34 +0000

    Add package Terminal-Apple-Season/1.1.1/Terminal_Apple_Season-1.1.1-py3-none-any.whl

---
+++ packages/Terminal-Apple-Season/Terminal_Apple_Season-1.1.1-py3-none-any.whl/apple_season/apple.py
@@ -0,0 +1,74 @@
+from os.path import join, dirname
+import sys
+import random
+
+cwd = dirname(dirname(__file__))
+
+if cwd not in sys.path:
+    sys.path.append(cwd)
+
+from apple_season.coords import Coords, Image, Char
+
+
+apple_string = """  /
+/ \\
+\_/"""
+
+apple_grid = [list(row) for row in apple_string.split('\n')]
+
+chars = []
+for r, row in enumerate(apple_grid):
+    for x, char in enumerate(row):
+        chars.append(Char(x, len(apple_grid) - 1 - r, char))
+
+apple_image = Image(chars)
+
+
+class Apple(Coords):
+
+    def __init__(self, canvas, basket):
+
+        self.basket = basket
+
+        y = canvas.height
+        x = random.randint(1, canvas.width - 21)
+
+        self.has_fallen = False
+        self.caught = False
+        self.frame = 0
+
+        Coords.__init__(self, x, y, apple_image, canvas)
+
+
+    def end(self):
+
+        new_grid = [[' ' for i in range(3)] for x in range(3)]
+        new_image_chars = []
+        for r, row in enumerate(new_grid):
+            for x, char in enumerate(row):
+                new_image_chars.append(Char(x, len(new_grid) - 1 - r, char))
+        self.image = Image(new_image_chars)
+        self.has_fallen = True
+
+    def fall(self):
+
+        self.previous_y = self.y
+        self.y -= 1
+
+        if self.y <= 0:
+            self.end()
+
+    def check_caught(self):
+        
+        # x values for basket and apple
+        basket_char_coords = set([char.x + self.basket.x for char in self.basket.image.chars])
+        self_char_coords = set([char.x + self.x for char in self.image.chars])
+
+        overlapping = bool(basket_char_coords & self_char_coords != set())
+
+        if overlapping:
+            self.caught = True
+            self.end()
+            return True
+        else:
+            return False
---
+++ packages/Terminal-Apple-Season/Terminal_Apple_Season-1.1.1-py3-none-any.whl/apple_season/coords.py
@@ -0,0 +1,84 @@
+class Canvas:
+    """Canvas for displaying coords objects"""
+
+    def replace(self, x, y, char):
+        """Replaces a char with other char at certain location on canvas"""
+
+        if len(char) != 1:
+            raise ValueError('can only replace one char at a time')
+
+        self.grid[self.height - 1 - y][x] = char
+
+    def __init__(self, width, height):
+
+        self.width = width
+        self.height = height
+        
+        self.grid = [[' ' for x in range(self.width)] for y in range(self.height)]
+
+        for i, char in enumerate(reversed('press "q" to quit'), 1):
+            self.replace(self.width - i, self.height - 1, char)
+
+        
+    @property
+    def display(self):
+        """Returns string representation of current state"""
+        return '\n'.join([''.join(i) for i in self.grid])
+
+
+class Char:
+    """A single character that is part of an Image"""
+
+    def __init__(self, x, y, char):
+
+        self.x = x
+        self.y = y
+        self.char = char  # string
+
+
+class Image:
+    """A collection of chars that create an object in the game"""
+
+    def __init__(self, chars):
+
+        self.chars = chars
+
+
+class Coords:
+    """An object with a location on the canvas"""
+
+    def __init__(self, x, y, image, canvas):  # x and y are represented by bottom-left most char in Coords object design
+        
+        self.canvas = canvas  # Canvas Object
+        self.image = image  # Image object
+        self.x = x
+        self.y = y
+        self.previous_x = self.x
+        self.previous_y = self.y
+
+    def render(self):
+        """Alter canvas to display coords object in its current position"""
+
+        # clear previous rendering on canvas
+        for char in self.image.chars:
+            
+            # location of previous char on canvas
+            canvas_x = char.x + self.previous_x
+            canvas_y = char.y + self.previous_y
+
+            try:
+                self.canvas.replace(canvas_x, canvas_y, ' ')
+            except IndexError:
+                pass
+        
+        # add new rendering on canvas
+        for char in self.image.chars:
+            
+            # location of char on canvas
+            canvas_x = char.x + self.x
+            canvas_y = char.y + self.y
+
+            try:
+                self.canvas.replace(canvas_x, canvas_y, char.char)
+            except IndexError:
+                pass

cff95527343b723332e888752a0803cfd25a8f49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:19 +0000

    Add package bamboolean/0.3.0/bamboolean-0.3.0.tar.gz

---
+++ packages/bamboolean/bamboolean-0.3.0.tar.gz/bamboolean-0.3.0/bamboolean/tokens.py
@@ -0,0 +1,22 @@
+# Types
+STRING = 'STRING'
+BOOL = 'BOOL'
+INTEGER = 'INTEGER'
+FLOAT = 'FLOAT'
+ID = 'ID'
+
+LPAREN, RPAREN, = ('(', ')')
+
+# Operators
+AND, OR = ('AND', 'OR')
+NE, EQ, LT, LTE, GT, GTE = ('NE', 'EQ', 'LT', 'LTE', 'GT', 'GTE')
+
+EOF = 'EOF'
+
+
+def is_rel_op(op: str) -> bool:
+    return op in (NE, EQ, LT, LTE, GT, GTE)
+
+
+def is_value(tok_type: str) -> bool:
+    return tok_type in (STRING, BOOL, INTEGER, FLOAT)

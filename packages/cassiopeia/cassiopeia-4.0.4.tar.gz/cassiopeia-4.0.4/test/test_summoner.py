f4c4e7419442c7888258853ecd17b94a0e143aae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:09 +0000

    Add package cassiopeia/4.0.4/cassiopeia-4.0.4.tar.gz

---
+++ packages/cassiopeia/cassiopeia-4.0.4.tar.gz/cassiopeia-4.0.4/test/test_summoner.py
@@ -0,0 +1,20 @@
+import cassiopeia
+import pytest
+
+from datapipelines.common import NotFoundError
+
+from .constants import SUMMONER_NAME, UNKNOWN_SUMMONER_NAME
+
+
+def test_unknown_summoner():
+    assert not cassiopeia.get_summoner(name=UNKNOWN_SUMMONER_NAME, region="NA").exists
+
+
+def test_equal_summoners():
+    from_name = cassiopeia.get_summoner(name=SUMMONER_NAME, region="NA")
+    from_id = cassiopeia.get_summoner(id=from_name.id, region="NA")
+
+    assert from_name.id == from_id.id
+    assert from_name.name == from_id.name
+    assert from_name == from_id
+    assert from_name.to_dict() == from_id.to_dict()

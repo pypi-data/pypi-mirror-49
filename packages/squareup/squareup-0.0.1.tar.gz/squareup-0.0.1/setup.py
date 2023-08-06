97690ba79a236d4f028aa8a3e7e2d1b820e8eb96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:42 +0000

    Add package squareup/0.0.1/squareup-0.0.1.tar.gz

---
+++ packages/squareup/squareup-0.0.1.tar.gz/squareup-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+
+setuptools.setup(
+    name="squareup",
+    version="0.0.1",
+    author="Square",
+    author_email="devplat-external-sdk-accounts+pypi@squareup.com",
+    description="Square",
+    long_description="Square",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[],
+)

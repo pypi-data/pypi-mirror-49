7461e576fabe9783bc5628cbadf6ea9eb6537d3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:45 +0000

    Add package tg-hammer/0.6.8/tg-hammer-0.6.8.tar.gz

---
+++ packages/tg-hammer/tg-hammer-0.6.8.tar.gz/tg-hammer-0.6.8/hammer/__init__.py
@@ -0,0 +1,7 @@
+hammer_name = 'tg-hammer'
+hammer_description = 'Helpers for fabric based deployments.'
+hammer_version = '0.6.8'
+
+__name__ = hammer_name
+__description__ = hammer_description
+__version__ = hammer_version
---
+++ packages/tg-hammer/tg-hammer-0.6.8.tar.gz/tg-hammer-0.6.8/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import uuid
+
+import hammer
+
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+requirements = [str(ir.req) for ir in parse_requirements('requirements/base.txt', session=uuid.uuid1())]
+test_requirements = [str(ir.req) for ir in parse_requirements('requirements/development.txt', session=uuid.uuid1())]
+
+setup(
+    name=hammer.__name__,
+    version=hammer.__version__,
+    description=hammer.__description__,
+    long_description=open('README.md').read(),
+    author='Thorgate',
+    author_email='hi@thorgate.eu',
+    url='https://github.com/thorgate/tg-hammer',
+    packages=[
+        'hammer',
+    ],
+    include_package_data=True,
+    install_requires=requirements,
+    test_suite='py.test',
+    tests_require=test_requirements,
+    license="BSD",
+    keywords='tg-hammer hammer fabric vcs git hg',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+    ],
+)

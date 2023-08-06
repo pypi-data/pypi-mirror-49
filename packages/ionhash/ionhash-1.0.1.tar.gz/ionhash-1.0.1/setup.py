843670da338384d295bdbb7df900e569f06e1b74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:18 +0000

    Add package ionhash/1.0.1/ionhash-1.0.1.tar.gz

---
+++ packages/ionhash/ionhash-1.0.1.tar.gz/ionhash-1.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ionhash',
+    version='1.0.1',
+    description='Python implementation of Amazon Ion Hash',
+    url='http://github.com/amzn/ion-hash-python',
+    author='Amazon Ion Team',
+    author_email='ion-team@amazon.com',
+    license='Apache License 2.0',
+
+    packages=find_packages(exclude=['tests*']),
+
+    install_requires=[
+        'six',
+        'amazon.ion==0.4.0',
+    ],
+
+    setup_requires=[
+        'pytest-runner',
+    ],
+
+    tests_require=[
+        'pytest',
+    ],
+)

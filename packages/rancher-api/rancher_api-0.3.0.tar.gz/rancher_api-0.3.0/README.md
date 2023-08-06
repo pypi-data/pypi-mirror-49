43eec4a39fa027c16b615af7b04a33800425fd32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:17 +0000

    Add package rancher-api/0.3.0/rancher_api-0.3.0.tar.gz

---
+++ packages/rancher-api/rancher_api-0.3.0.tar.gz/rancher_api-0.3.0/README.md
@@ -0,0 +1,28 @@
+# [Rancher API](http://rancher-api.hive.pt)
+
+The Python Rancher API client.
+
+Currently the client is compatible with both v2-beta and v3 of the Rancher API.
+
+## Reference
+
+The Rancher API projects takes most of its reference implementation from the [Official Rancher API Documentation](http://docs.rancher.com/rancher/api/).
+
+## Configuration
+
+| Name | Type | Description |
+| ----- | ----- | ----- |
+| **RANCHER_BASE_URL** | `str` | The base URL to the rancher server of target (eg: `http://rancher.domain.com:8080/v3/`) (defaults to `None`). |
+| **RANCHER_USERNAME** | `str` | The username (access key) to be used for API authentication (defaults to `None`). |
+| **RANCHER_PASSWORD** | `str` | The password (secret key) to be used for API authentication (defaults to `None`). |
+
+## License
+
+Rancher API is currently licensed under the [Apache License, Version 2.0](http://www.apache.org/licenses/).
+
+## Build Automation
+
+[![Build Status](https://travis-ci.org/hivesolutions/rancher_api.svg?branch=master)](https://travis-ci.org/hivesolutions/rancher_api)
+[![Coverage Status](https://coveralls.io/repos/hivesolutions/rancher_api/badge.svg?branch=master)](https://coveralls.io/r/hivesolutions/rancher_api?branch=master)
+[![PyPi Status](https://img.shields.io/pypi/v/rancher_api.svg)](https://pypi.python.org/pypi/rancher_api)
+[![License](https://img.shields.io/badge/license-Apache%202.0-blue.svg)](https://www.apache.org/licenses/)
---
+++ packages/rancher-api/rancher_api-0.3.0.tar.gz/rancher_api-0.3.0/src/rancher/service.py
@@ -0,0 +1,161 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Hive Rancher API
+# Copyright (c) 2008-2019 Hive Solutions Lda.
+#
+# This file is part of Hive Rancher API.
+#
+# Hive Rancher API is free software: you can redistribute it and/or modify
+# it under the terms of the Apache License as published by the Apache
+# Foundation, either version 2.0 of the License, or (at your option) any
+# later version.
+#
+# Hive Rancher API is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# Apache License for more details.
+#
+# You should have received a copy of the Apache License along with
+# Hive Rancher API. If not, see <http://www.apache.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__version__ = "1.0.0"
+""" The version of the module """
+
+__revision__ = "$LastChangedRevision$"
+""" The revision number of the module """
+
+__date__ = "$LastChangedDate$"
+""" The last change date of the module """
+
+__copyright__ = "Copyright (c) 2008-2019 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "Apache License, Version 2.0"
+""" The license for the module """
+
+import time
+
+import appier
+
+class ServiceAPI(object):
+    """
+    The service API endpoints used by the Rancher 1.x
+    infra-structure.
+    """
+
+    def list_services(self, *args, **kwargs):
+        url = self.base_url + "services"
+        contents = self.get(url, **kwargs)
+        data = contents["data"]
+        return data
+
+    def list_services_name(self, name, stack = None):
+        url = self.base_url + "services?name=%s" % name
+        if stack: url += "&stackId=%s" % stack
+        contents = self.get(url)
+        data = contents["data"]
+        return data
+
+    def get_service(self, id):
+        url = self.base_url + "services/%s" % id
+        contents = self.get(url)
+        return contents
+
+    def get_service_safe(self, id, stack = None):
+        contents = self.list_services_name(id, stack = stack)
+        if contents: return contents[0]
+        return self.get_service(id)
+
+    def update_service(self, id, payload = {}):
+        url = self.base_url + "services/%s" % id
+        contents = self.put(url, data_j = payload)
+        data = contents["data"]
+        return data
+
+    def upgrade_service(
+        self,
+        id,
+        batch_size = 1,
+        interval = 2000,
+        full_upgrade = True,
+        try_finish = True,
+        safe = True,
+        launch_config = None
+    ):
+        url = self.base_url + "services/%s?action=upgrade" % id
+        if try_finish: self._service_try_finish(id)
+        if launch_config == None: launch_config = self._service_launch_config(id)
+        if safe and not self._service_active(id):
+            raise appier.OperationalError(
+                message = "Service is currently not ready for upgrade"
+            )
+        contents = self.post(
+            url,
+            data_j = dict(
+                inServiceStrategy = dict(
+                    batchSize = batch_size,
+                    intervalMillis = interval,
+                    fullUpgrade = full_upgrade,
+                    launchConfig = launch_config,
+                    secondaryLaunchConfigs = []
+                )
+            )
+        )
+        data = contents["data"]
+        return data
+
+    def finish_upgrade_service(self, id):
+        url = self.base_url + "services/%s?action=finishupgrade" % id
+        contents = self.post(url)
+        data = contents["data"]
+        return data
+
+    def rollback_service(self, id):
+        url = self.base_url + "services/%s?action=rollback" % id
+        contents = self.post(url)
+        data = contents["data"]
+        return data
+
+    def restart_service(self, id, batch_size = 1, interval = 2000):
+        url = self.base_url + "services/%s?action=restart" % id
+        contents = self.post(
+            url,
+            data_j = dict(
+                rollingRestartStrategy = dict(
+                    batchSize = batch_size,
+                    intervalMillis = interval
+                )
+            )
+        )
+        data = contents["data"]
+        return data
+
+    def _service_active(self, id):
+        service = self.get_service(id)
+        return service["state"] == "active"
+
+    def _service_upgraded(self, id):
+        service = self.get_service(id)
+        return service["state"] == "upgraded"
+
+    def _service_try_finish(self, id, timeout = 10.0, force = False):
+        if not self._service_upgraded(id) and not force: return
+        try: self.finish_upgrade_service(id)
+        except Exception: pass
+        else: time.sleep(timeout)
+
+    def _service_launch_config(self, id):
+        service = self.get_service(id)
+        data = service.get("data", {})
+        fields = data.get("fields", {})
+        launch_config = fields.get("launchConfig", {})
+        return launch_config
+
+    def _service_image_uuid(self, id):
+        launch_config = self._service_launch_config(id)
+        image_uuid = launch_config.get("imageUuid", None)
+        return image_uuid

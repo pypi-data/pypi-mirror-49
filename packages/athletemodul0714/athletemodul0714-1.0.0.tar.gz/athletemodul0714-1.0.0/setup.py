41bd589d5e5c6be9ea4ba69ef49a8cd7d48b6786
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:30 +0000

    Add package athletemodul0714/1.0.0/athletemodul0714-1.0.0.tar.gz

---
+++ packages/athletemodul0714/athletemodul0714-1.0.0.tar.gz/athletemodul0714-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+    name         = 'athletemodul0714',
+    version      ='1.0.0',
+    py_modules   =['athletemodul0714'],
+    author       ='zrh-sq',
+    author_email ='1336746693@qq.com',
+    url          ='http://www.headfirstlabs.com',
+    description  ='ahtlete_times',
+    )
+    

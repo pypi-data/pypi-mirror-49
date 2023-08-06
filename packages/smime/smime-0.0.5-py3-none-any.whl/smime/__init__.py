af5881e4642a5f93cf16c663a893f2abfdcc995f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:06 +0000

    Add package smime/0.0.5/smime-0.0.5-py3-none-any.whl

---
+++ packages/smime/smime-0.0.5-py3-none-any.whl/smime/__init__.py
@@ -0,0 +1,9 @@
+"""Python S/MIME handler."""
+
+__author__ = 'G. B. Versiani'
+__license__ = 'Apache License (2.0)'
+__version__ = '0.0.5'
+
+__all__ = [__author__, __license__, __version__]
+
+from .encrypt import encrypt
---
+++ packages/smime/smime-0.0.5-py3-none-any.whl/smime/block.py
@@ -0,0 +1,81 @@
+# coding: utf-8
+
+from __future__ import unicode_literals
+
+import os
+
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+
+from abc import ABCMeta, abstractmethod
+
+
+class BlockCipher():
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def encrypt(self, data):
+        return NotImplemented
+
+    @property
+    def session_key(self):
+        return NotImplemented
+
+    @property
+    def parameters(self):
+        return NotImplemented
+
+
+class AES(BlockCipher):
+    algorithm = None
+    key_size = None
+    block_size = 16
+
+    def __init__(self, algorithm, mode, key_size):
+        self.algorithm = algorithm
+        self.mode = mode
+        self.key_size = key_size
+        self._session_key = os.urandom(self.key_size)
+        self._iv = os.urandom(self.block_size)
+        algorithm = algorithms.AES(self._session_key)
+        mode = mode(self._iv)
+        backend = default_backend()
+        self._encryptor = Cipher(algorithm, mode, backend=backend).encryptor()
+
+    @property
+    def session_key(self):
+        return self._session_key
+
+    def encrypt(self, data):
+        padded_data = self._pad(data, self.block_size)
+        encrypted_content = self._encryptor.update(padded_data.encode('utf-8')) + self._encryptor.finalize()
+        return {
+            'content_type': 'data',
+            'content_encryption_algorithm': {
+                'algorithm': self.algorithm,
+                'parameters': self._iv
+            },
+            'encrypted_content': encrypted_content
+        }
+
+    @staticmethod
+    def _pad(s, block_size):
+        n = block_size - len(s) % block_size
+        return s + n * chr(n)
+
+    @property
+    def parameters(self):
+        return self._iv
+
+
+def get_cipher(algorithm):
+    algorithms = {
+        'aes128_cbc': (AES, (modes.CBC, 16)),
+        'aes192_cbc': (AES, (modes.CBC, 24)),
+        'aes256_cbc': (AES, (modes.CBC, 32)),
+    }
+    if algorithm in algorithms:
+        cipher, parameters = algorithms[algorithm]
+        return cipher(algorithm, *parameters)
+    else:
+        return None

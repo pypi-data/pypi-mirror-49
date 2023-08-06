99afb4d0725e07ca07405df5cbb263475d0bb030
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:10 +0000

    Add package preved/0.1.2/preved-0.1.2.tar.gz

---
+++ packages/preved/preved-0.1.2.tar.gz/preved-0.1.2/hello/app.py
@@ -0,0 +1,7 @@
+
+def hello():
+    print("hello")
+
+
+def add(a, b):
+    return a + b
---
+++ packages/preved/preved-0.1.2.tar.gz/preved-0.1.2/setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(
+    name='preved',
+    version='0.1.2',
+    packages=['hello'],
+)

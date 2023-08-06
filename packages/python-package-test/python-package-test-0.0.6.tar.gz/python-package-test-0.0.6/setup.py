45922efc5f7d904670173998ff0ee0e90b1b5a85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:02 +0000

    Add package python-package-test/0.0.6/python-package-test-0.0.6.tar.gz

---
+++ packages/python-package-test/python-package-test-0.0.6.tar.gz/python-package-test-0.0.6/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python-package-test',
+    version='0.0.6',
+    maintainer='Jack',
+    py_modules=['index', 'fact'],
+    packages=find_packages(),
+    maintainer_email='Jack@email.com',
+    install_requires=[
+        'Click'
+    ],
+    entry_points='''
+        [console_scripts]
+        test-cli=index:cli
+    '''
+)

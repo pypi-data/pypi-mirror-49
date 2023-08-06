3f21797b0f64597837ba2b0db99b948aacb4bdb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:39 +0000

    Add package zhangda/1.0/zhangda-1.0.tar.gz

---
+++ packages/zhangda/zhangda-1.0.tar.gz/zhangda-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='zhangda',
+    version='1.0',
+    description='这是一个测试',
+    author='zhanghui',
+    author_email='739628252@qq.com',
+    py_modules=['jiangda.demo','jiangda.demo2']
+
+)

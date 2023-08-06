6f9ec325778fd5afc4281e4001a8d7913ec6a946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:28 +0000

    Add package mendelmd/1.2.2/mendelmd-1.2.2.tar.gz

---
+++ packages/mendelmd/mendelmd-1.2.2.tar.gz/mendelmd-1.2.2/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(name='mendelmd',
+      version='1.2.2',
+      packages=find_packages(),
+      scripts=['bin/mendelmd'],
+      include_package_data=True,
+      install_requires=[
+          'wheel',
+          'django',
+          'pysam',
+          'cython',
+          'wheel',
+          'Django',
+          'psycopg2-binary',
+          'django-allauth',
+          'django-debug-toolbar',
+          'django-crispy-forms',
+          'SOAPpy',
+          'Sphinx',
+          'Fabric3',
+          'pysam',
+          'pynnotator',
+          'pyvcf',
+          'celery',
+          'django-celery',
+          'django-celery-results',
+          'django-appconf',
+          'gunicorn',
+          'boto3',
+          'django-formtools',
+          'dj-database-url',
+          'django-gravatar2',
+          'pandas',
+      ],
+      package_data = {
+            'templates': ['*']
+        }
+      # entry_points={  # Optional
+      #         'console_scripts': [
+      #             'mendelmd=mendelmd.main:main',
+      #         ],
+      # },
+      )

91fedc3e28ac8de05f6f5abf28b59250bcb806b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:24 +0000

    Add package django-cache-toolbox/1.1.1/django-cache-toolbox-1.1.1.tar.gz

---
+++ packages/django-cache-toolbox/django-cache-toolbox-1.1.1.tar.gz/django-cache-toolbox-1.1.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='django-cache-toolbox',
+    description="Non-magical object caching for Django.",
+    version='1.1.1',
+    url='https://chris-lamb.co.uk/projects/django-cache-toolbox',
+
+    author='Chris Lamb',
+    author_email='chris@chris-lamb.co.uk',
+    license='BSD',
+
+    packages=find_packages(exclude=('tests',)),
+
+    install_requires=(
+        "Django>=1.9",
+    ),
+)

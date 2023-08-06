d74f937829cb6372b95d5b961f51cb1c1804fd17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:25 +0000

    Add package Flask-Ueditor/0.1.0/flask-ueditor-0.1.0.tar.gz

---
+++ packages/Flask-Ueditor/flask-ueditor-0.1.0.tar.gz/Flask-Ueditor-0.1.0/README.md
@@ -0,0 +1,27 @@
+# **Flask ueditor Backstage, designed by yy**
+
+## Installing and Getting started
+
+1. Install
+  
+  You can clone flask_ueditor from github.
+  
+    git clone git@github.com:guaidashu/ueditor-flask.git
+   
+2. Start
+
+  You should 
+
+## Usage
+
+None
+
+## FAQ
+
+None
+
+## Running Tests
+
+## Finally Thanks 
+
+Thanks for your support.
---
+++ packages/Flask-Ueditor/flask-ueditor-0.1.0.tar.gz/Flask-Ueditor-0.1.0/setup.py
@@ -0,0 +1,43 @@
+"""
+Create by yy on 2019-07-24
+"""
+import os
+import sys
+
+from setuptools import setup, find_packages
+
+about = {}
+
+with open('flask_ueditor/__about__.py') as f:
+    exec(f.read(), about)
+
+if sys.argv[-1] == 'test':
+    status = os.system('make check')
+    status >>= 8
+    sys.exit(status)
+
+setup(
+    name=about['__title__'],
+    version=about['__version__'],
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    description=about['__description__'],
+    long_description=__doc__,
+    long_description_content_type="text/markdown",
+    license=about['__license__'],
+    url=about['__url__'],
+    packages=find_packages(),
+    install_requires=[
+        "qiniu >= 7.2.6",
+        "requests >= 2.21.0",
+        "Flask >= 1.0.2"
+    ],
+    classifiers=[
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ]
+)

8dc61668bbee3d7e45b5b8cbce28613d6cdc151d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:34 +0000

    Add package pynasdaq/0.1.1/pynasdaq-0.1.1.tar.gz

---
+++ packages/pynasdaq/pynasdaq-0.1.1.tar.gz/pynasdaq-0.1.1/pynasdaq/common.py
@@ -0,0 +1,3 @@
+DIVIDEND_CALENDAR_URL = "https://www.nasdaq.com/dividend-stocks/dividend-calendar.aspx"
+HIGH_YIELD_DIVIDEND_URL = "https://www.nasdaq.com/dividend-stocks/?render=download"
+DIVIDEND_HISTORY_URL = "https://www.nasdaq.com/symbol/{}/dividend-history"

fc403fd3e94e7d4f6df6ea6693c113cfe8a174d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:30 +0000

    Add package django-embed-gist/2019.7.25/django-embed-gist-2019.7.25.tar.gz

---
+++ packages/django-embed-gist/django-embed-gist-2019.7.25.tar.gz/django-embed-gist-2019.7.25/django_embed_gist/templatetags/embed_gist.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from django import template
+from django.utils.safestring import mark_safe
+import re
+
+register = template.Library()
+
+"""
+{% load gist %}
+
+{{ post.body|embed_gist }}
+"""
+
+def repl(m):
+    if "https://gist.github.com/" not in m[0]:
+        return m[0]
+    a = re.compile('<a[^>]* href="([^"]*)"')
+    url = a.match(m[0]).group(1)
+    return """<script src="%s.js"></script>""" % url
+
+@register.filter
+def embed_gist(html):
+    return re.sub(r'<a.*?>(.+?)</a>', repl, html)

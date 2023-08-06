05465e90b9b3c4e34c78a157fd5b83fc9e3bf4df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:10 +0000

    Add package imgload/0.1/imgload-0.1.tar.gz

---
+++ packages/imgload/imgload-0.1.tar.gz/imgload-0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open('README.md', "r") as readme_file:
+    long_description = readme_file.read()
+
+setup(
+    name='imgload',
+    version='0.1',
+    description='An image downloading library',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    py_modules=["imgload"],
+    install_requires=['requests-html'],
+    python_requires='>=3.7',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    url="https://github.com/frenos/imgload",
+    author="frenos",
+)

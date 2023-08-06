a5542ef230caf217f7dac86e638f17867b31d0cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:44 +0000

    Add package mih-naive-bayes/0.2.0/mih-naive-bayes-0.2.0.tar.gz

---
+++ packages/mih-naive-bayes/mih-naive-bayes-0.2.0.tar.gz/mih-naive-bayes-0.2.0/naive_bayes/binary_naive_bayes.py
@@ -0,0 +1,24 @@
+# encoding: utf-8
+
+import numpy as np
+
+from naive_bayes import NaiveBayes
+
+
+class BinaryNaiveBayes(NaiveBayes):
+
+
+    def __init__(self):
+        super(BinaryNaiveBayes, self).__init__()
+
+
+    def fit(self, D, C):
+
+        D = [np.unique(d) for d in D]
+        super(BinaryNaiveBayes, self).fit(D, C)
+
+
+    def predict(self, d):
+
+        d = np.unique(d)
+        return super(BinaryNaiveBayes, self).predict(d)

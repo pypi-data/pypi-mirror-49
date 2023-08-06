183b59b55695681712ff5b5f437b8287bbca31e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:24 +0000

    Add package azmlclient/2.3.0/azmlclient-2.3.0-py3-none-any.whl

---
+++ packages/azmlclient/azmlclient-2.3.0-py3-none-any.whl/azmlclient/__init__.py
@@ -0,0 +1,28 @@
+from azmlclient.base_databinding import AzmlException
+# do not import since azure-storage package is optional
+# from azmlclient.base_databinding_blobs import BlobConverters, BlobCollectionConverters
+from azmlclient.base import execute_rr, execute_bes, IllegalJobStateException, JobExecutionException, \
+    create_session_for_proxy, create_session_for_proxy_from_strings, RequestResponseClient, BatchClient
+
+from azmlclient.clients_config import GlobalConfig, ServiceConfig, ClientConfig, ConfigTemplateSyntaxError
+from azmlclient.clients_callmodes import CallMode, RemoteCallMode, RequestResponse, Batch
+from azmlclient.clients import AzureMLClient, azureml_service, unpack_single_value_from_df, LocalCallModeNotAllowed
+
+__all__ = [
+    # submodules
+    'base_databinding', 'base', 'clients', 'clients_callmodes', 'clients_config',  # 'base_databinding_blobs'
+    # symbols imported above
+    # -- base_databinding
+    'AzmlException',
+    # -- base databinding blob:  do not import since azure-storage package is optional
+    #'BlobConverters', 'BlobCollectionConverters',
+    # -- base
+    'execute_rr', 'execute_bes', 'IllegalJobStateException', 'JobExecutionException', 'create_session_for_proxy',
+    'RequestResponseClient', 'BatchClient', 'create_session_for_proxy_from_strings',
+    # -- clients_config
+    'GlobalConfig', 'ServiceConfig', 'ClientConfig', 'ConfigTemplateSyntaxError',
+    # -- clients_callmodes
+    'CallMode', 'RemoteCallMode', 'RequestResponse', 'Batch',
+    # -- clients
+    'AzureMLClient', 'azureml_service', 'unpack_single_value_from_df', 'LocalCallModeNotAllowed'
+]

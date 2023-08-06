c13a9ba05174b6223daa020027175a2279dd99ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:46 +0000

    Add package fints/2.2.0/fints-2.2.0-py3-none-any.whl

---
+++ packages/fints/fints-2.2.0-py3-none-any.whl/fints/exceptions.py
@@ -0,0 +1,38 @@
+class FinTSError(Exception):
+    pass
+
+
+class FinTSClientError(FinTSError):
+    pass
+
+
+class FinTSClientPINError(FinTSClientError):
+    pass
+
+
+class FinTSClientTemporaryAuthError(FinTSClientError):
+    pass
+
+
+class FinTSDialogError(FinTSError):
+    pass
+
+
+class FinTSDialogStateError(FinTSDialogError):
+    pass
+
+
+class FinTSDialogInitError(FinTSDialogError):
+    pass
+
+
+class FinTSConnectionError(FinTSError):
+    pass
+
+
+class FinTSUnsupportedOperation(FinTSError):
+    pass
+
+
+class FinTSNoResponseError(FinTSError):
+    pass

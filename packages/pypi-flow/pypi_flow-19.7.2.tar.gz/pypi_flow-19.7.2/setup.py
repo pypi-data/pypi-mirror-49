b26e3adf275ca497a473193ab9a9aa9fe02b9d18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:27 +0000

    Add package pypi-flow/19.7.2/pypi_flow-19.7.2.tar.gz

---
+++ packages/pypi-flow/pypi_flow-19.7.2.tar.gz/pypi_flow-19.7.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pypi_flow",
+    version="19.07.2",
+    author="Gabriel Rosales",
+    author_email="gabriel.alejandro.rosales@gmail.com",
+    description="Project Management tools for quickly creating and maintaining PYPI packages.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ZenosParadox/pypi-sidekick",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['grtoolkit']
+)

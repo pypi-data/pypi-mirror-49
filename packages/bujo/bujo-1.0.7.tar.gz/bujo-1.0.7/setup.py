ebba030ad65b8d9c1ab1a082bef491e6d9d10350
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:36 +0000

    Add package bujo/1.0.7/bujo-1.0.7.tar.gz

---
+++ packages/bujo/bujo-1.0.7.tar.gz/bujo-1.0.7/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bujo',
+    version='1.0.7',
+    description='A CLI tool for tracking anything and everything',
+    keywords="cli note diary journal note-taking bullet_journal",
+    long_description=open('README.rst').read(),
+    long_description_content_type="text/markdown",
+    author="Oref",
+    author_email='orefdev@gmail.com',
+    url='https://github.com/oref/PyBujo.git',
+    install_requires=[
+        'Click',
+        'pyyaml',
+        'pick',
+    ],
+    packages=['bujo'],
+    entry_points={
+        'console_scripts': ['bujo = bujo:cli'],
+    },
+    classifiers=(
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
+    ),
+)

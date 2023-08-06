fc80952c334ec43aba4b0b30767975cf919ac866
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:50 +0000

    Add package DesktopScoreViewer/1.0.3/DesktopScoreViewer-1.0.3.tar.gz

---
+++ packages/DesktopScoreViewer/DesktopScoreViewer-1.0.3.tar.gz/DesktopScoreViewer-1.0.3/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+# README text
+with open("README.rst", "r") as f:
+    long_description = f.read()
+
+#requirements.txt text
+with open('requirements.txt', "r") as req:
+    required = req.read().splitlines()
+
+#calling setup()
+setuptools.setup(
+    name="DesktopScoreViewer",
+    version="1.0.3",
+    description="Gathers live MLB game scores and data and displays them on a mini GUI on the top right corner of the screen",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/JosephJ12/DesktopScoreViewer",
+    author="Joseph Jung",
+    author_email="josephjung12@gmail.com",
+    license="MIT",
+    classifiers = [
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=required,
+)

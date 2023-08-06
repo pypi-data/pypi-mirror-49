8cf80bc09c2e28787494ed554aaca7e234e15544
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:37 +0000

    Add package django-cryptapi/0.1.3/django-cryptapi-0.1.3.tar.gz

---
+++ packages/django-cryptapi/django-cryptapi-0.1.3.tar.gz/django-cryptapi-0.1.3/cryptapi/templatetags/cryptapi_helper.py
@@ -0,0 +1,42 @@
+from django import template
+from cryptapi.helpers import get_coin_multiplier
+
+register = template.Library()
+
+
+@register.simple_tag
+def convert_value(coin, value):
+
+    _rounded = 0
+    multiplier = get_coin_multiplier(coin, default=None)
+
+    if multiplier:
+        _rounded = value / multiplier
+
+    return '{}'.format(_rounded).rstrip('0')
+
+
+@register.filter
+def coin_protocol(coin):
+    coins = {
+        'btc': 'bitcoin',
+        'bch': 'bitcoincash',
+        'ltc': 'litecoin',
+        'eth': 'ethereum',
+        'iota': 'iota',
+    }
+
+    return coins.get(coin, '')
+
+
+@register.filter
+def coin_name(coin):
+    coins = {
+        'btc': 'BTC',
+        'bch': 'BCH',
+        'ltc': 'LTC',
+        'eth': 'ETH',
+        'iota': 'MIOTA',
+    }
+
+    return coins.get(coin, '')

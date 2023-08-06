5048490b75e3fe78608ba2eb6bf8bfcb38aa6902
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:14 +0000

    Add package aiidalab-widgets-base/1.0.0a5/aiidalab-widgets-base-1.0.0a5.tar.gz

---
+++ packages/aiidalab-widgets-base/aiidalab-widgets-base-1.0.0a5.tar.gz/aiidalab-widgets-base-1.0.0a5/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from __future__ import absolute_import
+from setuptools import setup, find_packages
+import json
+
+# pylint: disable=syntax-error
+if __name__ == '__main__':
+    # Provide static information in setup.json
+    # such that it can be discovered automatically
+    with open('metadata.json', 'r') as info:
+        metadata = json.load(info)
+
+    with open('setup.json', 'r') as info:
+        kwargs = json.load(info)
+
+    setup(packages=find_packages(),
+          long_description=open('README.md').read(),
+          long_description_content_type='text/markdown',
+          author=metadata['authors'],
+          description=metadata['description'],
+          version=metadata['version'],
+          **kwargs)

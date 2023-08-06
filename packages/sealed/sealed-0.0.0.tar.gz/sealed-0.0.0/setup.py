4d22f3cd6772d541b1b0bcb4bc5458df1e2e2a68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:39 +0000

    Add package sealed/0.0.0/sealed-0.0.0.tar.gz

---
+++ packages/sealed/sealed-0.0.0.tar.gz/sealed-0.0.0/setup.py
@@ -0,0 +1,12 @@
+import os
+from setuptools import setup
+setup(
+    name = "sealed",
+    version = "0.0.0",
+    author = "mibarg",
+    author_email = "mibarg@users.noreply.github.com",
+    description = ("An example"),
+    license = "MIT",
+    url = "http://packages.python.org/example",
+    packages=['src', 'tests'],
+)

8bfb70e7a3415872cbbf528a6178f8e90a9d245d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:49 +0000

    Add package django-countries-with-calling-codes/0.1.1/django-countries-with-calling-codes-0.1.1.tar.gz

---
+++ packages/django-countries-with-calling-codes/django-countries-with-calling-codes-0.1.1.tar.gz/django-countries-with-calling-codes-0.1.1/README.rst
@@ -0,0 +1,21 @@
+===================================
+Django Countries with Calling Codes
+===================================
+
+A django-countries fork with calling codes and geoip2 integration added.
+
+.. contents::
+    :local:
+    :backlinks: none
+
+
+Installation
+============
+
+1. ``pip install django-countries-with-calling-codes``
+2. Add ``django_countries`` to ``INSTALLED_APPS``
+
+For more accurate sorting of translated country names, install the optional
+pyuca_ package.
+
+.. _pyuca: https://pypi.python.org/pypi/pyuca/
---
+++ packages/django-countries-with-calling-codes/django-countries-with-calling-codes-0.1.1.tar.gz/django-countries-with-calling-codes-0.1.1/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+REQUIREMENTS = [
+    'geoip2>=2.9.0',
+]
+
+setup(
+    name='django-countries-with-calling-codes',
+    version='0.1.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT',
+    description='Django-countries fork with calling codes and geoip2 integration added',
+    long_description=README,
+    url='https://github.com/rondebu/django-countries-with-calling-codes',
+    author='Rondebu Software',
+    author_email='info@rondebu.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Framework :: Django',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    install_requires=REQUIREMENTS,
+)

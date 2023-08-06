9c4ef9a83e6d365db834c3a52763fae2c966af13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:02 +0000

    Add package pyscaffoldext-custom-extension/0.6a1/pyscaffoldext-custom-extension-0.6a1.tar.gz

---
+++ packages/pyscaffoldext-custom-extension/pyscaffoldext-custom-extension-0.6a1.tar.gz/pyscaffoldext-custom-extension-0.6a1/tests/test_add_install_requires.py
@@ -0,0 +1,22 @@
+from os.path import exists as path_exists
+
+from pyscaffold.api import create_project
+from pyscaffold.cli import parse_args, process_opts
+from pyscaffold.contrib.configupdater import ConfigUpdater
+
+
+def test_add_install_requires(tmpfolder):
+    args = ["--custom-extension", "pyscaffoldext-some_extension"]
+    opts = parse_args(args)
+    opts = process_opts(opts)
+    create_project(opts)
+    assert path_exists("pyscaffoldext-some_extension/setup.cfg")
+
+    config_updater = ConfigUpdater()
+
+    with open("pyscaffoldext-some_extension/setup.cfg") as f:
+        config_updater.read_file(f)
+
+    install_requires = config_updater.get("options", "install_requires").value
+
+    assert "pyscaffold" in install_requires

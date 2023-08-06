08bd725e819120d0136999dd98364d67ebf1de4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:16 +0000

    Add package Easpy/0.2/Easpy-0.2.tar.gz

---
+++ packages/Easpy/Easpy-0.2.tar.gz/Easpy-0.2/README.txt
@@ -0,0 +1,33 @@
+# Easpy
+
+Easpy is a python package that allows to new developers to learn python more easily.
+
+# Overview
+Easpy was programmed to make it easy to learn Python, so easy that even a child could do it using Easpy.
+"Make it easier" is our keyword, our goal.
+
+### Installation
+To install it open your Terminal/Shell and type:
+```sh
+pip install Easpy
+```
+### Usage
+To use Easpy, at the start of any Python file type:
+```sh
+import Easpy
+```
+It's so easy!
+
+# Upcoming updates
+-A shell
+-Code improvements
+
+# Website
+The official Easpy website is coming soon.
+
+# FAQ
+Q-Why you named this package Easpy?
+A-We named it Easpy because it's the fusion of Easy and Python
+
+Q-Can i help to improve it?
+A-Sure! Contact me if you want to help
---
+++ packages/Easpy/Easpy-0.2.tar.gz/Easpy-0.2/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'Easpy',         
+  packages = [''],   
+  version = '0.2',      # Start with a small number and increase it with every change you make
+  license='mit',        
+  description = 'Easpy helps new developers to start programming in python by simplifying it',   
+  author = '123Lory321',                   
+  author_email = 'lorenzooliva321@gmail.com',      
+  url = 'https://github.com/123Lory321/Easpy',   
+  download_url = 'https://github.com/123Lory321/Easpy/archive/v0.2.tar.gz',    
+  keywords = ['Easpy', 'python', 'easy', 'python3'],   
+  install_requires=[            
+
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Choose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',     
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)

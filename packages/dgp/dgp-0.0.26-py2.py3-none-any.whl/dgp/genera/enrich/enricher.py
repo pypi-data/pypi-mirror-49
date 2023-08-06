f6468a940d979f3b82ab8c71cf644cb4e8e0a71b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:16 +0000

    Add package dgp/0.0.26/dgp-0.0.26-py2.py3-none-any.whl

---
+++ packages/dgp/dgp-0.0.26-py2.py3-none-any.whl/dgp/genera/enrich/enricher.py
@@ -0,0 +1,32 @@
+from ...core import BaseDataGenusProcessor
+
+
+class EnricherDGP(BaseDataGenusProcessor):
+
+    def init(self):
+        self._flows = None
+        analyzers = self.context.taxonomy.analyzers(self.config, self.context)
+        if analyzers is not None:
+            self.steps = self.init_classes(
+                self.context.taxonomy.analyzers
+            )
+
+    @property
+    def flows(self):
+        if self._flows is None:
+            self._flows = self.context.taxonomy.flows(self.config, self.context)
+        return self._flows
+
+    def preflow(self):
+        if self.flows:
+            return self.flows[0]
+
+    def flow(self):
+        from dataflows import Flow
+        if self.flows:
+            return Flow(
+                self.flows[1],
+            )
+        else:
+            return Flow(
+            )

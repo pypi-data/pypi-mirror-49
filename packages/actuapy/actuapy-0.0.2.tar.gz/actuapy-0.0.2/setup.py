00d8c6ba817a1d453e93a058cd82db6f85ee562a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:10 +0000

    Add package actuapy/0.0.2/actuapy-0.0.2.tar.gz

---
+++ packages/actuapy/actuapy-0.0.2.tar.gz/actuapy-0.0.2/setup.py
@@ -0,0 +1,35 @@
+import os, sys
+from setuptools import setup, find_packages
+
+def read_requirements():
+    """Parse requirements from requirements.txt."""
+    reqs_path = os.path.join('.', 'requirements.txt')
+    with open(reqs_path, 'r') as f:
+        requirements = [line.rstrip() for line in f]
+    return requirements
+
+try:
+    with open('README.md') as f:
+        readme = f.read()
+except IOError:
+    readme = ''
+
+try:
+    with open('LICENSE') as f:
+        license = f.read()
+except IOError:
+    license = ''
+
+setup(
+    name='actuapy',
+    version='0.0.2',
+    description='useful snippets for actuaries',
+    long_description=readme,
+    author='Rei Mizuta',
+    author_email='ayanamizuta832@gmail.com',
+    install_requires=read_requirements(),
+    url='https://github.com/ayanamizuta/actuary',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    test_suite='tests'
+)

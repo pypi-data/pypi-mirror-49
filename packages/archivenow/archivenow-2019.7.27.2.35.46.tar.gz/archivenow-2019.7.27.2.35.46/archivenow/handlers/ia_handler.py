4d8734b2a334fbaf63cb1289945797039038190d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:10 +0000

    Add package archivenow/2019.7.27.2.35.46/archivenow-2019.7.27.2.35.46.tar.gz

---
+++ packages/archivenow/archivenow-2019.7.27.2.35.46.tar.gz/archivenow-2019.7.27.2.35.46/archivenow/handlers/ia_handler.py
@@ -0,0 +1,44 @@
+import requests
+
+class IA_handler(object):
+
+    def __init__(self):
+        self.enabled = True
+        self.name = 'The Internet Archive'
+        self.api_required = False
+
+    def push(self, uri_org, p_args=[]):
+        msg = ''
+        try:
+            uri = 'https://web.archive.org/save/' + uri_org
+            # push into the archive
+            r = requests.get(uri, timeout=120, allow_redirects=True)
+            r.raise_for_status()
+            # extract the link to the archived copy 
+            if (r != None):
+                if "Location" in r.headers:
+                    return r.headers["Location"]
+                elif "Content-Location" in r.headers:
+                    if (r.headers["Content-Location"]).startswith("/web/"):
+                        return "https://web.archive.org"+r.headers["Content-Location"]
+                    else:
+                        try:
+                            uri_from_content = "https://web.archive.org" + r.text.split('var redirUrl = "',1)[1].split('"',1)[0]
+                        except:
+                            uri_from_content = r.headers["Content-Location"]
+                            #pass;
+                        return uri_from_content
+                else:
+                    for r2 in r.history:
+                        if 'Location' in r2.headers:
+                            return r.url
+                            #return r2.headers['Location']
+                        if 'Content-Location' in r2.headers:
+                            return r.url
+                            #return r2.headers['Content-Location']
+            msg = "("+self.name+ "): No HTTP Location/Content-Location header is returned in the response"               
+        except Exception as e:
+            if msg == '':
+                msg = "Error (" + self.name+ "): " + str(e)
+            pass;      
+        return msg

f51e2b85f6dbc8cd091e8965fc358d0e6039a083
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:42 +0000

    Add package pycopy-collections/0.2/pycopy-collections-0.2.tar.gz

---
+++ packages/pycopy-collections/pycopy-collections-0.2.tar.gz/pycopy-collections-0.2/collections/__init__.py
@@ -0,0 +1,31 @@
+# Should be reimplemented for MicroPython
+# Reason:
+# CPython implementation brings in metaclasses and other bloat.
+# This is going to be just import-all for other modules in a namespace package
+import ucollections
+from ucollections import OrderedDict
+try:
+    from .defaultdict import defaultdict
+except ImportError:
+    pass
+try:
+    from .deque import deque
+except ImportError:
+    pass
+
+class Mapping:
+    pass
+
+class MutableMapping:
+    pass
+
+def namedtuple(name, fields):
+    _T = ucollections.namedtuple(name, fields)
+
+    @classmethod
+    def _make(cls, seq):
+        return cls(*seq)
+
+    t = type(name, (_T,), {"_make": _make})
+
+    return t

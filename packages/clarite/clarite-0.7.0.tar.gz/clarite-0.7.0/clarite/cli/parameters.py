c5dd7d7444cc928da6d72b62fa2c3679c74cea54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:20 +0000

    Add package clarite/0.7.0/clarite-0.7.0.tar.gz

---
+++ packages/clarite/clarite-0.7.0.tar.gz/clarite-0.7.0/clarite/cli/parameters.py
@@ -0,0 +1,31 @@
+import click
+
+from .custom_types import ClariteDataParamType, ClariteEwasResultParamType
+
+# File IO
+INPUT_FILE = click.Path(exists=True, file_okay=True, dir_okay=False, readable=True)
+OUTPUT_FILE = click.Path(file_okay=True, dir_okay=False, writable=True)
+
+# Frequently used output parameters
+arg_output = click.argument('output', type=OUTPUT_FILE)
+
+# Standard datatypes that use multiple files together
+CLARITE_DATA = ClariteDataParamType()  # Instantiate it to use as a type in arguments/options
+arg_data = click.argument('data', type=CLARITE_DATA)
+
+# Tuple of dataset name, ewas df
+EWAS_RESULT = ClariteEwasResultParamType()
+
+
+# Skip/Only handling
+def process_skip_only(ctx, param, value):
+    if len(value) == 0:
+        return None
+    else:
+        return list(value)
+
+
+option_skip = click.option('-s', '--skip', type=click.STRING, multiple=True, callback=process_skip_only,
+                           help="variables to skip")
+option_only = click.option('-o', '--only', type=click.STRING, multiple=True, callback=process_skip_only,
+                           help="variables to process, skipping all others")
---
+++ packages/clarite/clarite-0.7.0.tar.gz/clarite-0.7.0/docs/source/cli.rst
@@ -0,0 +1,40 @@
+=============
+CLI Reference
+=============
+
+Once CLARITE is installed, the command line interface can be run using the :code:`clarte-cli` command.
+
+The command line interface has command groups that are the same as the modules in the package (except for *survey*).
+
+The :code:`--help` option will show documentation when run with any command or command group:
+
+.. code-block:: bash
+
+   $ clarite-cli --help
+   Usage: clarite-cli [OPTIONS] COMMAND [ARGS]...
+
+   Options:
+   --help  Show this message and exit.
+   
+   Commands:
+     analyze
+     describe
+     load
+     modify
+     plot
+
+.. click:: clarite.cli:analyze_cli
+    :prog: clarite-cli analyze
+    :show-nested:
+.. click:: clarite.cli:describe_cli
+    :prog: clarite-cli describe
+    :show-nested:
+.. click:: clarite.cli:load_cli
+    :prog: clarite-cli load
+    :show-nested:
+.. click:: clarite.cli:modify_cli
+    :prog: clarite-cli modify
+    :show-nested:
+.. click:: clarite.cli:plot_cli
+    :prog: clarite-cli plot
+    :show-nested:

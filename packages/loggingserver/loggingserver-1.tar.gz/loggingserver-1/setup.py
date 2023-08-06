bdd380c13cbf35e45c5b10044245d3186ad9a772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:04 +0000

    Add package loggingserver/1/loggingserver-1.tar.gz

---
+++ packages/loggingserver/loggingserver-1.tar.gz/loggingserver-1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="loggingserver",
+    py_modules=["loggingserver"],
+    version="1",
+    author="Gleb Fedorov",
+    author_email="vdrhtc@gmail.com",
+    description="Multiprocessing-safe logging server",
+    long_description="Multiprocessing-safe logging server based on a singletone pattern and a multiprocessing queue",
+    long_description_content_type="text/markdown",
+    url="https://github.com/vdrhtc/LoggingServer",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
+

4d620c38b9f52b94cad69474bd912c11358b1a92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:06 +0000

    Add package elasticsearch-cli/1.0.2/elasticsearch-cli-1.0.2.tar.gz

---
+++ packages/elasticsearch-cli/elasticsearch-cli-1.0.2.tar.gz/elasticsearch-cli-1.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='elasticsearch-cli',
+    version='1.0.2',
+    license='MIT',
+    author="Wesly Allan",
+    author_email="weslyg22@gmail.com",
+    description="Elasticsearch cli",
+    url="https://github.com/WeslyG/elasticsearch-cli",
+    packages=find_packages(),
+    install_requires=[
+        'Click',
+        'Elasticsearch'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: Implementation",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points='''
+        [console_scripts]
+        es=main:cli
+    ''',
+)

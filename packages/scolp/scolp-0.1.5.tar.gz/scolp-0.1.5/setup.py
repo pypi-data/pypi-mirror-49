c0438cb71e9753624d11979a35ca9fdde2164134
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:20 +0000

    Add package scolp/0.1.5/scolp-0.1.5.tar.gz

---
+++ packages/scolp/scolp-0.1.5.tar.gz/scolp-0.1.5/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="scolp",
+    version="0.1.5",
+    author="David Ohana",
+    author_email="davidoha@gmail.com",
+    description="Streaming Column Printer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/davidohana/python-scolp",
+    packages=setuptools.find_packages(),
+    py_modules=['scolp'],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries",
+    ],
+)

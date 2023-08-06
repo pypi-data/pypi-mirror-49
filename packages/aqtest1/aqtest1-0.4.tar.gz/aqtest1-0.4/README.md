b0641ea4a77befca3e0ffb872cee96dfbfaa0f41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:32 +0000

    Add package aqtest1/0.4/aqtest1-0.4.tar.gz

---
+++ packages/aqtest1/aqtest1-0.4.tar.gz/aqtest1-0.4/README.md
@@ -0,0 +1,1 @@
+This is just a package upload test, please do not use.
---
+++ packages/aqtest1/aqtest1-0.4.tar.gz/aqtest1-0.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages, Extension
+
+setup(name='aqtest1',
+      version='0.4',
+      description='this is just a test',
+      author='HENRY TEST',
+      author_email='liuyichen@std.uestc.edu.cn',
+      packages=['aqtest1',],
+      install_requires=[
+      'pysam',
+      'numpy',
+      'scipy',],
+      dependency_links = [
+      'https://github.com/samtools/samtools/tarball/master.tar.gz#egg=samtools',
+      'https://github.com/lh3/minimap2/tarball/master.tar.gz#egg=minimap2',],
+      entry_points={'console_scripts':['step1=aqtest.Aquila_step1:main','step2=aqtest.Aquila_step2:main','assemble=aqtest.Aquila_assembly_based_variants_call:main','phasing=aqtest.Aquila_phasing_all_variants:main']},
+      zip_safe=False)

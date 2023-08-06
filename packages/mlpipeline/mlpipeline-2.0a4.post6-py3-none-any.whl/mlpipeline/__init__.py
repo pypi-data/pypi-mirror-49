674278b64c1fc781f8c3cd22ea826575b119faff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:42 +0000

    Add package mlpipeline/2.0a4.post6/mlpipeline-2.0a4.post6-py3-none-any.whl

---
+++ packages/mlpipeline/mlpipeline-2.0a4.post6-py3-none-any.whl/mlpipeline/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "2.0.a.4.post.6"
+
+from mlpipeline._api_interface import (mlpipeline_execute_exeperiment,
+                                       mlpipeline_execute_pipeline,
+                                       get_experiment)

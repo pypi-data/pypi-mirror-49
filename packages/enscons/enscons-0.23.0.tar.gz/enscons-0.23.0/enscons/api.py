b0e978019d7afd68df249373ace1b6ed4c4e584c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:48 +0000

    Add package enscons/0.23.0/enscons-0.23.0.tar.gz

---
+++ packages/enscons/enscons-0.23.0.tar.gz/enscons-0.23.0/enscons/api.py
@@ -0,0 +1,47 @@
+"""
+PEP 517 interface to enscons.
+
+To be invoked by a future version of pip.
+
+May not be possible to invoke more than one function without reloading Python.
+"""
+
+import os.path
+import sys
+import SCons.Script.Main
+
+
+def get_build_wheel_requires(settings):
+    return []
+
+
+def prepare_wheel_metadata(metadata_directory, settings):
+    return  # basename of wheel...
+
+
+def build_wheel(wheel_directory, settings, metadata_directory=None):
+    sys.argv[1:] = ["--wheel-dir=" + wheel_directory, "bdist_wheel"]
+    try:
+        SCons.Script.Main.main()
+    except SystemExit as e:
+        pass
+    for target in SCons.Script.DEFAULT_TARGETS:
+        target_name = str(target)
+        if target_name.endswith(".whl"):
+            return os.path.basename(target_name)
+
+
+def get_build_sdist_requires(settings):
+    return []
+
+
+def build_sdist(sdist_directory, settings):
+    sys.argv[1:] = ["--dist-dir=" + sdist_directory, "sdist"]
+    try:
+        SCons.Script.Main.main()
+    except SystemExit as e:
+        pass
+    for target in SCons.Script.DEFAULT_TARGETS:
+        target_name = str(target)
+        if target_name.endswith(".tar.gz"):
+            return os.path.basename(target_name)

26c02bf51d1c39161d2c90869ecd5e24ab148c83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:12 +0000

    Add package policypools/0.0.1/policypools-0.0.1-py3-none-any.whl

---
+++ packages/policypools/policypools-0.0.1-py3-none-any.whl/pools/DiscardNewestThreadPool.py
@@ -0,0 +1,23 @@
+from threading import Thread
+
+from pools.AbstractThreadPool import AbstractThreadPool
+
+
+class DiscardNewestThreadPool(AbstractThreadPool):
+
+    def __init__(self, max_q_size: int = 1, num_workers: int = 1):
+        """
+        Thread pool that will not put the new thread into in the thread queue if the thread queue is full
+        :param max_q_size: the maximum size for the thread queue
+        :param num_workers: the maximum number of concurrently running workers
+        """
+        super(DiscardNewestThreadPool, self).__init__(max_q_size, num_workers)
+
+    def submit(self, thread: Thread):
+        """
+        Submits a new thread, discarding the the given thread if the queue is full
+        :param thread: the given thread to run
+        :return: None
+        """
+        if not self.__thread_q.full():
+            self.__thread_q.put(thread)

07c510297008bf4591e92e7da3769e8502c102d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:39 +0000

    Add package webplatform-auth/1.0.3/webplatform-auth-1.0.3.tar.gz

---
+++ packages/webplatform-auth/webplatform-auth-1.0.3.tar.gz/webplatform-auth-1.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(name = "webplatform-auth",
+   version = "1.0.3",
+   description = "Authentication for webplatform",
+   author = "Matthew Owens",
+   author_email = "mowens@redhat.com",
+   url = "https://github.com/lost-osiris/webplatform-auth",
+   packages = find_packages(),
+   include_package_data = True,
+   install_requires = [
+      "webplatform-cli",
+      # "webplatform-backend",
+   ],
+   python_requires='>=3',
+   license='MIT',
+   long_description = """TODO""",
+   classifiers = [
+       "Programming Language :: Python :: 3",
+       "License :: OSI Approved :: MIT License",
+       "Operating System :: OS Independent",
+   ],
+   scripts = [
+      "webplatform_auth/webplatform-auth"
+   ]
+)

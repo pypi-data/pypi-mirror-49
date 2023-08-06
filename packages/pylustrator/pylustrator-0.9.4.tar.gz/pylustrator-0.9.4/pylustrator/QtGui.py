6d114c0290aab12da6b1ab89ef12a90aae8b5d86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:11 +0000

    Add package pylustrator/0.9.4/pylustrator-0.9.4.tar.gz

---
+++ packages/pylustrator/pylustrator-0.9.4.tar.gz/pylustrator-0.9.4/pylustrator/QtGui.py
@@ -0,0 +1,256 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# QtGui.py
+
+# Copyright (c) 2016-2019, Richard Gerum
+#
+# This file is part of Pylustrator.
+#
+# Pylustrator is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Pylustrator is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pylustrator. If not, see <http://www.gnu.org/licenses/>
+
+from __future__ import division, print_function
+from qtpy import QtCore, QtWidgets, QtGui
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qt4 import NavigationToolbar2QT as NavigationToolbar
+from .matplotlibwidget import MatplotlibWidget
+from matplotlib import _pylab_helpers
+import matplotlib as mpl
+import qtawesome as qta
+
+from .QtShortCuts import AddQColorChoose, QDragableColor
+
+import sys
+
+
+def my_excepthook(type, value, tback):
+    sys.__excepthook__(type, value, tback)
+
+
+sys.excepthook = my_excepthook
+
+""" Matplotlib overlaod """
+figures = {}
+app = None
+
+
+def initialize():
+    global app
+    if app is None:
+        app = QtWidgets.QApplication(sys.argv)
+    plt.show = show
+    plt.figure = figure
+
+
+def show():
+    global figures
+    # iterate over figures
+    for figure in figures:
+        # get the window
+        window = figures[figure].window
+        # update the colors
+        window.updateColors()
+        # and show it
+        window.show()
+    # execute the application
+    app.exec_()
+
+
+def figure(num=None, size=None, *args, **kwargs):
+    global figures
+    # if num is not defined create a new number
+    if num is None:
+        num = len(figures)
+    # if number is not defined
+    if num not in figures.keys():
+        # create a new window and store it
+        canvas = PlotWindow(num, *args, **kwargs).canvas
+        figures[num] = canvas
+    # get the canvas of the figure
+    canvas = figures[num]
+    # set the size if it is defined
+    if size is not None:
+        figures[num].window.setGeometry(100, 100, size[0] * 80, size[1] * 80)
+    # set the figure as the active figure
+    _pylab_helpers.Gcf.set_active(canvas.manager)
+    # return the figure
+    return canvas.figure
+
+""" Figure list functions """
+
+
+def addChildren(color_artists, parent):
+    for artist in parent.get_children():
+        # ignore empty texts
+        if isinstance(artist, mpl.text.Text) and artist.get_text() == "":
+            continue
+
+        # add the children of the item (not for text or ticks)
+        if not isinstance(artist, (mpl.text.Text, mpl.axis.XTick, mpl.axis.YTick)):
+            addChildren(color_artists, artist)
+
+        # iterate over the elements
+        for color_type_name in ["edgecolor", "facecolor", "color"]:
+            colors = getattr(artist, "get_" + color_type_name, lambda: None)()
+            # ignore colors that are not set
+            if colors is None:
+                continue
+            # test if it is a colormap
+            try:
+                cmap = colors.cmap
+                value = colors.value
+            except AttributeError:
+                cmap = None
+
+            # convert to array
+            if not (isinstance(colors, np.ndarray) and len(colors.shape) > 1):
+                colors = [colors]
+
+            # omit blacks and whites
+            if mpl.colors.to_hex(colors[0]) == "#000000" or mpl.colors.to_hex(colors[0]) == "#ffffff":
+                continue
+
+            # if we have a colormap
+            if cmap:
+                # iterate over the colors of the colormap
+                for index, color in enumerate(cmap.get_color()):
+                    # convert to hex
+                    color = mpl.colors.to_hex(color)
+                    # check if it is already in the dictionary
+                    if color not in color_artists:
+                        color_artists[color] = []
+                    # add the artist
+                    color_artists[color].append([color_type_name, artist, value, cmap, index])
+            else:
+                # iterate over the colors
+                for color in colors:
+                    # ignore transparent colors
+                    if mpl.colors.to_rgba(color)[3] == 0:
+                        continue
+                    # convert to hey
+                    color = mpl.colors.to_hex(color)
+                    # check if it is already in the dictionary
+                    if color not in color_artists:
+                        color_artists[color] = []
+                    # add the artist
+                    color_artists[color].append([color_type_name, artist, None, None, None])
+
+def figureListColors(figure):
+    figure.color_artists = {}
+    addChildren(figure.color_artists, figure)
+
+def figureSwapColor(figure, new_color, color_base):
+    if getattr(figure, "color_artists", None) is None:
+        figureListColors(figure)
+    for data in figure.color_artists[color_base]:
+        # get the data
+        color_type_name, artist, value, cmap, index = data
+        # if the color is part of a colormap, update the colormap
+        if cmap:
+            # update colormap
+            cmap.set_color(new_color, index)
+            # use the attributes setter method
+            getattr(artist, "set_" + color_type_name)(cmap(value))
+        else:
+            # use the attributes setter method
+            getattr(artist, "set_" + color_type_name)(new_color)
+
+""" Window """
+
+
+class PlotWindow(QtWidgets.QWidget):
+    def __init__(self, number, *args, **kwargs):
+        QtWidgets.QWidget.__init__(self)
+
+        # widget layout and elements
+        self.setWindowTitle("Figure %s" % number)
+        self.setWindowIcon(qta.icon("fa.bar-chart"))
+        self.layout_main = QtWidgets.QHBoxLayout(self)
+
+        # add plot layout
+        self.layout_plot = QtWidgets.QVBoxLayout(self)
+        self.layout_main.addLayout(self.layout_plot)
+
+        # add plot canvas
+        self.canvas = MatplotlibWidget(self)
+        self.canvas.window = self
+        self.layout_plot.addWidget(self.canvas)
+        _pylab_helpers.Gcf.set_active(self.canvas.manager)
+
+        # add toolbar
+        self.navi_toolbar = NavigationToolbar(self.canvas, self)
+        self.layout_plot.addWidget(self.navi_toolbar)
+        self.layout_plot.addStretch()
+
+        # initialize color artist dict
+        self.color_artists = {}
+
+        # add color chooser layout
+        self.layout_right = QtWidgets.QVBoxLayout()
+        self.layout_colors = QtWidgets.QVBoxLayout()
+        self.layout_right.addLayout(self.layout_colors)
+        self.layout_colors2 = QtWidgets.QVBoxLayout()
+        self.layout_right.addLayout(self.layout_colors2)
+        self.layout_main.addLayout(self.layout_right)
+
+
+
+    def addColorButton(self, color, basecolor=None):
+        button = QDragableColor(mpl.colors.to_hex(color))
+        self.layout_colors.addWidget(button)
+        button.color_changed.connect(lambda c, color_base=basecolor: self.color_selected(c, color_base))
+        if basecolor:
+            self.color_buttons[basecolor] = button
+        self.color_buttons_list.append(button)
+
+    def updateColors(self):
+        # add recursively all artists of the figure
+        figureListColors(self.canvas.figure)
+        self.color_artists = self.canvas.figure.color_artists
+
+        # iterate over all colors
+        self.color_buttons = {}
+        self.color_buttons_list = []
+
+        for color in self.color_artists:
+            self.addColorButton(color, color)
+
+        # add a text widget to allow easy copy and paste
+        self.colors_text_widget = QtWidgets.QTextEdit()
+        self.colors_text_widget.setAcceptRichText(False)
+        self.layout_colors2.addWidget(self.colors_text_widget)
+        self.colors_text_widget.setText("\n".join([mpl.colors.to_hex(color) for color in self.color_artists]))
+        self.colors_text_widget.textChanged.connect(self.colors_changed)
+
+        # update the canvas dimensions
+        self.canvas.updateGeometry()
+
+    def colors_changed(self):
+        # when the colors in the text edit changed
+        for index, color in enumerate(self.colors_text_widget.toPlainText().split("\n")):
+            try:
+                color = mpl.colors.to_hex(color.strip())
+            except ValueError:
+                continue
+            if len(self.color_buttons_list) <= index:
+                self.addColorButton(color)
+            self.color_buttons_list[index].setColor(color)
+
+    def color_selected(self, new_color, color_base):
+        if color_base is None:
+            return
+        figureSwapColor(self.canvas.figure, new_color, color_base)
+        # redraw the plot
+        self.canvas.draw()
---
+++ packages/pylustrator/pylustrator-0.9.4.tar.gz/pylustrator-0.9.4/pylustrator/QtShortCuts.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# QtShortCuts.py
+
+# Copyright (c) 2016-2019, Richard Gerum
+#
+# This file is part of Pylustrator.
+#
+# Pylustrator is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Pylustrator is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pylustrator. If not, see <http://www.gnu.org/licenses/>
+
+from qtpy import QtCore, QtGui, QtWidgets
+import numpy as np
+import matplotlib as mpl
+
+
+def getColorFromCoordinates(p):
+    desktop = QtWidgets.QApplication.desktop()
+    pixmap = QtWidgets.QGuiApplication.screens().at(desktop.screenNumber()).grabWindow(desktop.winId(),
+                                                                                         p.x(), p.y(), 1, 1)
+    i = pixmap.toImage()
+    return i.pixel(0, 0)
+
+
+""" Color Chooser """
+
+class QDragableColor(QtWidgets.QLineEdit):
+    color_changed = QtCore.Signal(str)
+
+    def __init__(self, value):
+        QtWidgets.QLineEdit.__init__(self, value)
+        self.setAcceptDrops(True)
+        self.setAlignment(QtCore.Qt.AlignHCenter)
+        self.setColor(value, True)
+
+    def setColor(self, value, no_signal=False):
+        # display and save the new color
+        self.setStyleSheet("text-align: center; background-color: %s; border: 2px solid black" % value)
+        self.color = value
+        self.setText(value)
+        self.color_changed.emit(value)
+
+    def getColor(self):
+        # return the color
+        return self.color
+
+    def mousePressEvent(self, event):
+        if event.button() == QtCore.Qt.LeftButton:
+            drag = QtGui.QDrag(self)
+            drag.setPixmap(self.grab())
+            mime = QtCore.QMimeData()
+            mime.setText(self.color)
+            drag.setMimeData(mime)
+            self.setStyleSheet("background-color: lightgray; border: 2px solid gray")
+            self.setDisabled(True)
+            self.setText("")
+            drag.exec()
+            self.setText(self.color)
+            self.setDisabled(False)
+            self.setStyleSheet("text-align: center; background-color: %s; border: 2px solid black" % self.color)
+        elif event.button() == QtCore.Qt.RightButton:
+            self.openDialog()
+
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasFormat("text/plain") and event.source() != self:
+            event.acceptProposedAction()
+            self.setStyleSheet("background-color: %s; border: 2px solid red" % self.color)
+
+    def dragLeaveEvent(self, event):
+        self.setStyleSheet("background-color: %s; border: 2px solid black" % self.color)
+
+    def dropEvent(self, event):
+        color = event.source().getColor()
+        event.source().setColor(self.getColor())
+        self.setColor(color)
+
+    def openDialog(self):
+        # get new color from color picker
+        qcolor = QtGui.QColor(*np.array(mpl.colors.to_rgb(self.getColor())) * 255)
+        color = QtWidgets.QColorDialog.getColor(qcolor)
+        # if a color is set, apply it
+        if color.isValid():
+            color = "#%02x%02x%02x" % color.getRgb()[:3]
+            self.setColor(color)
+
+
+def AddQColorChoose(layout, text, value=None, strech=False):
+    # add a layout
+    horizontal_layout = QtWidgets.QHBoxLayout()
+    layout.addLayout(horizontal_layout)
+    # add a text
+    # text = QtWidgets.QLabel(text)
+    button = QtWidgets.QPushButton("")
+
+    # button.label = text
+
+    def OpenDialog():
+        # get new color from color picker
+        qcolor = QtGui.QColor(*np.array(mpl.colors.to_rgb(button.getColor())) * 255)
+        color = QtWidgets.QColorDialog.getColor(qcolor)
+        # if a color is set, apply it
+        if color.isValid():
+            color = "#%02x%02x%02x" % color.getRgb()[:3]
+            button.setColor(color)
+            lineEdit.setText(color)
+            try:
+                button.color_changed(color)
+            except AttributeError:
+                pass
+
+    def setColor(value):
+        # display and save the new color
+        button.setStyleSheet("background-color: %s;" % value)
+        button.color = value
+        try:
+            button.color_changed(value)
+        except AttributeError:
+            pass
+
+    def getColor():
+        # return the color
+        return button.color
+
+    def editFinished():
+        new_color = lineEdit.text()
+        setColor(new_color)
+
+    # default value for the color
+    if value is None:
+        value = "#FF0000"
+
+    lineEdit = QtWidgets.QLineEdit()
+    lineEdit.setFixedWidth(50)
+    lineEdit.setText(value)
+    # lineEdit.label = text
+    lineEdit.editingFinished.connect(editFinished)
+
+    # add functions to button
+    button.pressed.connect(OpenDialog)
+    button.setColor = setColor
+    button.getColor = getColor
+    # set the color
+    button.setColor(value)
+    # add widgets to the layout
+    # horizontal_layout.addWidget(text)
+    horizontal_layout.addWidget(button)
+    horizontal_layout.addWidget(lineEdit)
+    # add a strech if requested
+    if strech:
+        horizontal_layout.addStretch()
+    return button

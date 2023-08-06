bb9655567de00b102a6b6d2131825b668b152b50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:04:17 +0000

    Add package todolist-v1/1.0.1/todolist_v1-1.0.1.tar.gz

---
+++ packages/todolist-v1/todolist_v1-1.0.1.tar.gz/todolist_v1-1.0.1/setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='todolist_v1',
+    version='1.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    description='A simple Django app to keep track of your todo list. It comes with a RESTful API, celery task queue, Oauth2 and is fully tested.',
+    long_description=open('README.md').read(),
+    author='Kenneth Mathenge',
+    author_email='mathenge@example.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    scripts=['bin/todo_manage'],
+)

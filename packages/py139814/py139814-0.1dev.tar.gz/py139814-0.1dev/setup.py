4a2d473a07cbec89686af892dabba7d01409e9d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:17 +0000

    Add package py139814/0.1dev/py139814-0.1dev.tar.gz

---
+++ packages/py139814/py139814-0.1dev.tar.gz/py139814-0.1dev/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name = 'py139814',
+    version = '0.1dev',
+    packages = ['py139814',],
+    license = 'UT',
+    long_description = open("README.txt").read(),
+    )

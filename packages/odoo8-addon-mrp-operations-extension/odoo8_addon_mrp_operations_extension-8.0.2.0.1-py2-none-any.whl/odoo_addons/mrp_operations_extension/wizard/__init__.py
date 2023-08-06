bf9295c48074d4a19a9bee27450436c2e8c5a8f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:40 +0000

    Add package odoo8-addon-mrp-operations-extension/8.0.2.0.1/odoo8_addon_mrp_operations_extension-8.0.2.0.1-py2-none-any.whl

---
+++ packages/odoo8-addon-mrp-operations-extension/odoo8_addon_mrp_operations_extension-8.0.2.0.1-py2-none-any.whl/odoo_addons/mrp_operations_extension/wizard/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+from . import change_production_qty
+from . import mrp_work_order_produce
+from . import workcenter_line_finish

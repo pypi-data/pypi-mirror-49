843db28b8d9d0ba009e5167d573d7fcdda4fd82c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:24 +0000

    Add package pyprojroot/0.1.2/pyprojroot-0.1.2-py3-none-any.whl

---
+++ packages/pyprojroot/pyprojroot-0.1.2-py3-none-any.whl/pyprojroot/pyprojroot.py
@@ -0,0 +1,23 @@
+import pathlib as pl
+import warnings
+
+
+def pyprojroot(p, proj_files):
+    for pf in proj_files:
+        found = list(p.glob(pf))
+        if len(found) > 0:
+            return(p)
+    return pyprojroot(p.parent, proj_files)
+
+
+def here(rel_proj_path='.',
+         proj_files=['.git', '.here', '*.Rproj', 'requirements.txt', 'setup.py']):
+    proj_path = pyprojroot(pl.Path('.').cwd(), proj_files)
+
+    pth = proj_path / rel_proj_path
+
+    if pth.exists():
+        return pth
+    else:
+        warnings.warn("Path doesn't exist: {}".format(pth))
+        return pth

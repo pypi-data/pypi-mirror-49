81ae7f8be873a3233b1e201eb739ec5565970a1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:20 +0000

    Add package jupyterlab-omnisci/0.11.0/jupyterlab-omnisci-0.11.0.tar.gz

---
+++ packages/jupyterlab-omnisci/jupyterlab-omnisci-0.11.0.tar.gz/jupyterlab-omnisci-0.11.0/setup.py
@@ -0,0 +1,31 @@
+"""A setuptools based setup module for omnisci renderers"""
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+from setuptools import find_packages, setup
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="jupyterlab-omnisci",  # Required
+    version="0.11.0",  # Required
+    description="Omnisci integration with JupyterLab",  # Required
+    long_description=long_description,  # Optional
+    long_description_content_type="text/markdown",  # Optional (see note above)
+    url="https://github.com/Quansight/jupyterlab-omnisci",  # Optional
+    packages=find_packages(),
+    install_requires=[
+        "jupyterlab>=1.0.0",
+        "ibis-framework>=1.1.0",
+        "altair>=3.0.1",
+        "ipywidgets",
+        "vdom",
+        "pyyaml",
+        "vega_datasets"
+    ]
+)

bc8786b84f8b5ee5b543b99d822ae6c0ac52fcd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:37 +0000

    Add package grtoolkit/19.7.11/grtoolkit-19.7.11-py3-none-any.whl

---
+++ packages/grtoolkit/grtoolkit-19.7.11-py3-none-any.whl/grtoolkit/__init__.py
@@ -0,0 +1,12 @@
+import grtoolkit.Decorators
+import grtoolkit.File
+import grtoolkit.Format
+import grtoolkit.Math
+import grtoolkit.PDF
+import grtoolkit.Storage
+import grtoolkit.Windows
+
+import os, sys
+
+def cwd():
+    return os.path.dirname(sys.argv[0])

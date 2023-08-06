86a58803ce14ca84a4f961abbe7932c99954bbc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:03 +0000

    Add package alphaj-naver-stock-cralwer/0.1.3/alphaj_naver_stock_cralwer-0.1.3-py3-none-any.whl

---
+++ packages/alphaj-naver-stock-cralwer/alphaj_naver_stock_cralwer-0.1.3-py3-none-any.whl/alphaj_naver_stock_info_crawler/__init__.py
@@ -0,0 +1,17 @@
+
+import alphaj_naver_stock_info_crawler.SheetHandler as sheet_handler
+
+import alphaj_naver_stock_info_crawler.ExcelHandler as excel_handler
+
+__all__ = ['ExcelHandler', 'SheetHandler']
+
+def get_stock_all_information_json(stock_code, stock_name):
+    return sheet_handler.get_stock_all_information(stock_code, stock_name)
+
+def save_stock_all_information_to_excel(stock_code, stock_name, file_name = None):
+    stock_object = sheet_handler.get_stock_all_information(stock_code, stock_name)
+    if file_name == None:
+        file_name = '{}_{}.xlsx'.format(stock_code, stock_name)
+    if not file_name.endswith('.xlsx'):
+        file_name = file_name + ".xlsx"
+    excel_handler.save_all_stock_information_to_excel(stock_object, file_name)

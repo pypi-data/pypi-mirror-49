82358ab79042a6f5cbff45dda74953657980e601
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:00:44 +0000

    Add package trw/0.0.1/trw-0.0.1-py2.py3-none-any.whl

---
+++ packages/trw/trw-0.0.1-py2.py3-none-any.whl/trw/__init__.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+
+from .metadata import (
+    __author__,
+    __description__,
+    __email__,
+    __license__,
+    __name__,
+    __url__,
+    __version__,
+)
---
+++ packages/trw/trw-0.0.1-py2.py3-none-any.whl/trw/metadata.py
@@ -0,0 +1,17 @@
+__name__ = "trw"
+__version__ = "0.0.1"
+__description__ = "Train, test, debug and optimize PyTorch models"
+__url__ = "https://github.com/civodlu/trw"
+__author__ = "Civodlu"
+__email__ = "civodlu@gmail.com"
+__license__ = "MIT"
+
+
+def sum(a, b):
+    """
+    Calculate the sum
+
+    >>> sum(1, 3)
+    4
+    """
+    return a + b

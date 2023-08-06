2315be3cfe116dc29248c98cec71d8863d7f9843
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:06 +0000

    Add package what3words/3.0.2/what3words-3.0.2.tar.gz

---
+++ packages/what3words/what3words-3.0.2.tar.gz/what3words-3.0.2/what3words/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# coding: utf8
+
+from .what3words import Geocoder
+from .what3words import Circle
+from .what3words import Coordinates
+from .what3words import BoundingBox
+
+import version
+
+__title__ = 'what3words'
+__author__ = 'what3words'
+__author_email__ = 'development@what3words.com'
+__version__ = version.__version__
+__license__ = 'MIT'
+__copyright__ = 'Copyright (c) 2017 what3words'

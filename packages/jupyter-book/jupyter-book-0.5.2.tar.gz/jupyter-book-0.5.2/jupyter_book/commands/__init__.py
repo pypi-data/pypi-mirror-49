af755b3613572104877bd73ff2499808b7bdda3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:57 +0000

    Add package jupyter-book/0.5.2/jupyter-book-0.5.2.tar.gz

---
+++ packages/jupyter-book/jupyter-book-0.5.2.tar.gz/jupyter-book-0.5.2/jupyter_book/commands/__init__.py
@@ -0,0 +1,6 @@
+"""Defines the commands that the CLI will use."""
+from .build import build
+from .create import create
+from .upgrade import upgrade
+from .run import run
+from .version import version

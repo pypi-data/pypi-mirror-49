ee633ba447115b8c8d9d92a03818d741d6d0befa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:31 +0000

    Add package md2xmind/1.0.2/md2xmind-1.0.2-py3.7.egg

---
+++ packages/md2xmind/md2xmind-1.0.2-py3.7.egg/example/Demo.py
@@ -0,0 +1,21 @@
+# !/usr/bin/python
+# -*- coding:utf-8 -*-
+"""
+@Author  : jingle1267
+@Time    : 2019-07-20 05:09
+@desc：  : 
+"""
+import os
+
+from md2xmind import Md2Xmind
+
+
+def main():
+	print('example main')
+	file_path = os.path.abspath(os.path.join(os.getcwd(), 'test2.md'))
+
+	Md2Xmind.main(file_path, 'test2')
+
+
+if __name__ == '__main__':
+	main()
---
+++ packages/md2xmind/md2xmind-1.0.2-py3.7.egg/md2xmind/__about__.py
@@ -0,0 +1,17 @@
+# !/usr/bin/python
+# -*- coding:utf-8 -*-
+"""
+@Author  : jingle1267
+@Time    : 2019-07-20 09:40
+@desc：  : 
+"""
+
+__title__ = 'md2xmind'
+__description__ = 'md2xmind'
+__keywords__ = 'md2xmind, md, xmind',
+__url__ = 'https://github.com/jingle1267/md2xmind'
+__author__ = 'jingle1267'
+__author_email__ = 'jingle1267@163.com'
+__version__ = '1.1.0'
+__license__ = 'Apache License Version 2.0'
+__cake__ = u'\u2728 \U0001f370 \u2728'

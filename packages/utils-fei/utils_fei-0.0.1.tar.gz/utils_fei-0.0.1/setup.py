3e0639dbeb8e45f920f4c6f2b92c16a258f3fc49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:09 +0000

    Add package utils-fei/0.0.1/utils_fei-0.0.1.tar.gz

---
+++ packages/utils-fei/utils_fei-0.0.1.tar.gz/utils_fei-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='utils_fei',
+      version='0.0.1',
+      description='python tools for gaofei',
+      url='https://github.com/BigDataFounder/string2date',
+      author='gaofei',
+      author_email='1010155688@qq.com',
+      license='MIT',
+      packages=['utils_fei'])

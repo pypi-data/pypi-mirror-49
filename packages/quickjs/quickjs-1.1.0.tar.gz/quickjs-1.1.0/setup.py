adb9c6d46ae305d14bf6c16ef9531489cff65de6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:33 +0000

    Add package quickjs/1.1.0/quickjs-1.1.0.tar.gz

---
+++ packages/quickjs/quickjs-1.1.0.tar.gz/quickjs-1.1.0/setup.py
@@ -0,0 +1,21 @@
+import glob
+from setuptools import setup, Extension
+
+_quickjs = Extension('_quickjs',
+                     define_macros=[('CONFIG_VERSION', '"2019-07-09"')],
+                     sources=['module.c'] + glob.glob("third-party/*.c"),
+                     headers=glob.glob("third-party/*.h"))
+
+long_description = """
+Thin Python wrapper around https://bellard.org/quickjs/ .
+"""
+
+setup(author="Petter Strandmark",
+      author_email="petter.strandmark@gmail.com",
+      name='quickjs',
+      url='https://github.com/PetterS/quickjs',
+      version='1.1.0',
+      description='Wrapping the quickjs C library.',
+      long_description=long_description,
+      packages=["quickjs"],
+      ext_modules=[_quickjs])

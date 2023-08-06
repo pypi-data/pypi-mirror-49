147654ab682890111644bd14c6c8c1cecd4ec53b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:56 +0000

    Add package opy-logger/1.1/opy-logger-1.1.tar.gz

---
+++ packages/opy-logger/opy-logger-1.1.tar.gz/opy-logger-1.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="opy-logger",
+    version="1.1",
+    author="Muhammed Çamsarı",
+    license='MIT',
+    author_email="Muhammedcamsari@icloud.com",
+    description="Used for simple log output.",
+    long_description=long_description,
+    url="https://github.com/muhammedcamsari/opy-logger",
+    long_description_content_type="text/markdown",
+    keywords=['log', 'logger', 'simple'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+    ],
+    py_modules=['opylogger'],
+    entry_points='''
+        [console_scripts]
+        opylogger=opylogger:opylogger
+    ''',
+)

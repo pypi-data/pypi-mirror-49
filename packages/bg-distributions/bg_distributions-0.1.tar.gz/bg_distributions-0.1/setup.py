960a2bf836123efc845966ae62e19ca121f249bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:37 +0000

    Add package bg-distributions/0.1/bg_distributions-0.1.tar.gz

---
+++ packages/bg-distributions/bg_distributions-0.1.tar.gz/bg_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='bg_distributions',
+      version='0.1',
+      description='Gaussian/Binomial distributions',
+      packages=['bg_distributions'],
+      author='Francois Ferreira',
+      zip_safe=False)

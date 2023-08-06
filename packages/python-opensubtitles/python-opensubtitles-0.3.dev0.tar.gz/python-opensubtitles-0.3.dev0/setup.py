cd46ef768b9786539514a87124be9bc41d10d5d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:16 +0000

    Add package python-opensubtitles/0.3.dev0/python-opensubtitles-0.3.dev0.tar.gz

---
+++ packages/python-opensubtitles/python-opensubtitles-0.3.dev0.tar.gz/python-opensubtitles-0.3.dev0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.3'
+
+setup(
+    name='python-opensubtitles',
+    version=version,
+    description="Wrapper to use the OpenSubtitles API easily",
+    keywords='opensubtitles python api',
+    author='\xc3\x81lex Gonz\xc3\xa1lez',
+    author_email='agonzalezro@gmail.com',
+    url='http://twitter.com/agonzalezro',
+    license='BSD',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+)

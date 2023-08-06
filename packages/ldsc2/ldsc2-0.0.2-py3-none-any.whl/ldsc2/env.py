2b5064185d1ed12789ba30074d4b8cfa647f2c39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:02 +0000

    Add package ldsc2/0.0.2/ldsc2-0.0.2-py3-none-any.whl

---
+++ packages/ldsc2/ldsc2-0.0.2-py3-none-any.whl/ldsc2/env.py
@@ -0,0 +1,35 @@
+#===============================================================================
+# env.py
+#===============================================================================
+
+# Imports ======================================================================
+
+import os
+import os.path
+
+
+
+
+# Constants ====================================================================
+
+DIR = os.environ.get(
+    'LDSC2_DIR',
+    os.path.join(os.path.dirname(__file__), 'LDSC')
+)
+ANACONDA_DIR = os.environ.get(
+    'LDSC2_ANACONDA_DIR',
+    os.path.join(os.path.dirname(__file__), 'anaconda2')
+)
+ANACONDA_PATH = os.path.join(ANACONDA_DIR, 'envs', 'ldsc', 'bin', 'python')
+PLINKFILES = os.environ.get(
+    'LDSC2_PLINKFILES',
+    os.path.join(DIR, '1000G_EUR_Phase3_plink')
+)
+PLINKFILES_EAS = os.environ.get(
+    'LDSC2_PLINKFILES_EAS',
+    os.path.join(DIR, '1000G_Phase3_EAS_plinkfiles')
+)
+HAPMAP3_SNPS = os.environ.get(
+    'LDSC2_HAPMAP3_SNPS',
+    os.path.join(DIR, 'hapmap2_snps')
+)

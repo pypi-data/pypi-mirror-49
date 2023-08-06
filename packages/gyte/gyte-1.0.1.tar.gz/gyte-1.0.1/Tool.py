dc573c10226191a8a3212b1dce85564288383b0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:41 +0000

    Add package gyte/1.0.1/gyte-1.0.1.tar.gz

---
+++ packages/gyte/gyte-1.0.1.tar.gz/gyte-1.0.1/Tool.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 27 09:59:23 2019
+
+@author: guoyi
+"""
+
+import gyte
+gyte.gyteone.run()
+def show():
+    print("This is Tool.py")
---
+++ packages/gyte/gyte-1.0.1.tar.gz/gyte-1.0.1/gyte/gyteone.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 27 10:01:04 2019
+
+@author: guoyi
+"""
+
+def run():
+    print("这是GuoYi测试项目！")

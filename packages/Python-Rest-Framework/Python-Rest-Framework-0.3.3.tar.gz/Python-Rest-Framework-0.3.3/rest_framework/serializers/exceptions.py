cfc366162102b326cf4f2518a86463794c1f1e15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:58 +0000

    Add package Python-Rest-Framework/0.3.3/Python-Rest-Framework-0.3.3.tar.gz

---
+++ packages/Python-Rest-Framework/Python-Rest-Framework-0.3.3.tar.gz/Python-Rest-Framework-0.3.3/rest_framework/serializers/exceptions.py
@@ -0,0 +1,23 @@
+"""
+Errors for serializers.
+
+"""
+from rest_framework.exceptions import ApiException
+
+
+class ValidationError(ApiException):
+    """
+    Validation error.
+
+    """
+    default_detail = 'Invalid input.'
+
+    def __init__(self, code=None, *args, **kwargs):
+        """
+        Validation error.
+
+        :param str code: Code, for check error type and get current error message.
+
+        """
+        super().__init__(*args, **kwargs)
+        self.code = code

9c0ec6adaac3b21720694f0efac3040cf35a1c62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:35 +0000

    Add package typed-astunparse/2.1.4/typed-astunparse-2.1.4.tar.gz

---
+++ packages/typed-astunparse/typed-astunparse-2.1.4.tar.gz/typed-astunparse-2.1.4/setup.py
@@ -0,0 +1,38 @@
+"""This is setup.py file for typed-astunparse."""
+
+import setup_boilerplate
+
+
+class Package(setup_boilerplate.Package):
+
+    """Package metadata."""
+
+    name = 'typed-astunparse'
+    description = 'typed-astunparse is to typed-ast as astunparse is to ast'
+    url = 'https://github.com/mbdevpl/typed-astunparse'
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Education',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software Development :: Code Generators',
+        'Topic :: Software Development :: Compilers',
+        'Topic :: Software Development :: Pre-processors',
+        'Topic :: Utilities']
+    keywords = ['ast', 'unparsing', 'pretty printing']
+
+
+if __name__ == '__main__':
+    Package.setup()

7397685cccc0ec75fc456ba77bb95b3af32dbfd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:12 +0000

    Add package yolov3/1.7/yolov3-1.7.tar.gz

---
+++ packages/yolov3/yolov3-1.7.tar.gz/yolov3-1.7/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="yolov3",
+    version="1.7",
+    author="qingtian",
+    author_email="",
+    description="Keras(TF backend) implementation of yolo v3 objects detection. ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=['numpy==1.14.4', 'tensorflow==1.14.0', 'Keras==2.1.3', 'opencv-python==4.1.0.25'],
+    entry_points={
+        'console_scripts': [
+            'demo=demo:main'
+        ],
+    },
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)

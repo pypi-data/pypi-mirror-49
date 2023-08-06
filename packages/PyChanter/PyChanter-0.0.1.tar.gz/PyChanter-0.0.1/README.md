496a97d9157223272a1a6bd4865210c70d9df416
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:20 +0000

    Add package PyChanter/0.0.1/PyChanter-0.0.1.tar.gz

---
+++ packages/PyChanter/PyChanter-0.0.1.tar.gz/PyChanter-0.0.1/README.md
@@ -0,0 +1,14 @@
+=========================
+Python Editor Application
+=========================
+
+A simple QScintilla based Python Editor
+
+Prerequiste:
+
+- Python
+- PyQt5
+- QtConsole
+
+Launch
+    Run **python launchApplication.py**
---
+++ packages/PyChanter/PyChanter-0.0.1.tar.gz/PyChanter-0.0.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="PyChanter",
+    version="0.0.1",
+    author="Hariharan",
+    author_email="hari.haran849@example.com",
+    description="A small Scintilla based python editor",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hariharan849/PyChanter",
+    packages=setuptools.find_packages('resources'),
+    include_package_data=True,
+    install_requires=['PyQt5', 'pyqtconsole'],
+    package_data={
+        'resources': ['*.qss', '*.png', '*.stylesheet', '*.ui'],
+        'icons': ['*.png']
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

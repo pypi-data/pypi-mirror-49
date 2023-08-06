168aeb4df36ab5780ebd30dc1b53bf5eba2622e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:54 +0000

    Add package scikit-deploy/2.1.2/scikit_deploy-2.1.2-py3-none-any.whl

---
+++ packages/scikit-deploy/scikit_deploy-2.1.2-py3-none-any.whl/scikit_deploy/scikit_deploy_server/server/prediction.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def predict(model, input_data, config):
+    sample = config.process_input(input_data)
+    vec = np.array(sample).reshape(1, -1)
+    res = model.predict(vec)
+    return config.process_output(res)

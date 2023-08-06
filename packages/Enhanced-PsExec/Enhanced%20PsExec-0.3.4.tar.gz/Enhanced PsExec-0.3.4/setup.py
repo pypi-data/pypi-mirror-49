acd2c0d3a5de003a1bc05d04603a1731c33652d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:09 +0000

    Add package Enhanced-PsExec/0.3.4/Enhanced%20PsExec-0.3.4.tar.gz

---
+++ packages/Enhanced-PsExec/Enhanced%20PsExec-0.3.4.tar.gz/Enhanced PsExec-0.3.4/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+import os
+with open("README.md",'r') as f:
+	long_description = f.read()
+os.system("python -m pip install colorama")
+setup(
+	name='Enhanced PsExec',
+	version='0.3.4',
+	description='Enhanced PSEXEC',
+	py_modules=["Epsexec"],
+	package_dir={'': 'src'},
+
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+
+	url="https://github.com/orishamir/",
+	author="Ori Shamir",
+	author_email="Epsexecnoreply@gmail.com",
+
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"Programming Language :: Python :: 3.7",
+		"Development Status :: 3 - Alpha",
+		"License :: OSI Approved :: MIT License",
+		"Natural Language :: English",
+		"Topic :: System :: Systems Administration",
+		"Operating System :: Microsoft :: Windows"
+	]
+
+	)

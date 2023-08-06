672df5d03001974f81404b80095bc36ed29eb9b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:15 +0000

    Add package architect.sdk/0.1.0/architect.sdk-0.1.0.tar.gz

---
+++ packages/architect.sdk/architect.sdk-0.1.0.tar.gz/architect.sdk-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(name='architect.sdk',
+      version='0.1.0',
+      description='Architect Python SDK',
+      url='https://github.com/architect-team/python-sdk',
+      author='architect.io',
+      author_email='packages@architect.io',
+      license='GPL-3.0',
+      packages=['architect.sdk'])

853021c0fbc264bcee269029294ac2fb12a0d8e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:03 +0000

    Add package apk-cheater/0.0.1/apk_cheater-0.0.1-py3.8.egg

---
+++ packages/apk-cheater/apk_cheater-0.0.1-py3.8.egg/scripts/loader.js
@@ -0,0 +1,73 @@
+'use strict';
+
+var scan_address = [1,2];
+rpc.exports = {
+	scan: function(pattern) {
+		console.log("[Scan] Starting.. ");
+		scan_address = []; // remove history
+		var ranges = Process.enumerateRangesSync({protection: 'r--', coalesce: true});
+		var range;
+		function processNext(){
+			range = ranges.pop();
+			if(!range){
+				return false;
+			}
+
+			Memory.scan(range.base, range.size, pattern, { // Memory.scanSync is so slow..
+				onMatch: function(address, size){
+					scan_address.push(address);
+				}, 
+				onError: function(reason){
+					// console.log('[!] There was an error scanning memory');
+				}, 
+				onComplete: function(){
+					if (!processNext()){
+						console.log("[Scan] All done");
+					}
+				}
+			});
+
+			return true;
+		}
+		processNext();
+	},
+	show: function() {
+		console.log("Size: " + scan_address.length);
+		if (scan_address.length <= 10) {
+			for(var i=0; i<scan_address.length; i++){
+				try{
+					console.log(scan_address[i] + ": " + scan_address[i].readInt());
+				} catch(err) {
+				}
+			}
+		}
+	},
+	next: function(x) {
+		var address;
+		var temp = [];
+		for (var idx in scan_address){
+			try{
+				address = scan_address[idx];
+				if (x == address.readInt()){
+					temp.push(address);
+				}
+			} catch(err) {
+				// console.log(err);
+			}
+		}
+
+		scan_address = temp; // replace
+	},
+	edit: function(x) {
+		var address;
+		for (var idx in scan_address){
+			try{
+				address = scan_address[idx];
+				address.writeInt(x);
+				console.log("Edit: " + address);
+			} catch(err) {
+				// console.log(err);
+			}
+		}
+	},
+};
---
+++ packages/apk-cheater/apk_cheater-0.0.1-py3.8.egg/scripts/main.py
@@ -0,0 +1,68 @@
+import os
+import re
+import frida
+import struct
+
+def find_device(type):
+    for device in frida.enumerate_devices():
+        if device.type == type:
+            return device
+    return None
+
+def run():
+    device = find_device("usb")
+    target_pid = device.get_process("Gadget").pid
+    session = device.attach(target_pid)
+
+    ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
+    with open(os.path.join(ROOT_DIR, 'loader.js')) as f:
+        data = f.read()
+    script = session.create_script(data)
+    script.load()
+
+
+    def int2hex(value):
+        hexed = list(struct.pack("<I", value).hex())
+
+        size = len(hexed)
+        for idx in range(size-2, 0, -2):
+            hexed.insert(idx, " ")
+        return "".join(hexed)
+
+    def get_value():
+        value = int(input("Enter the value: "))
+        return int2hex(value)
+
+    menus = [x for x in re.findall(r"\s*(.*):\s*function\(", data) if not x.startswith('on')]
+    while True:
+        show_help = ""
+        for idx, name in enumerate(menus):
+            show_help += "{:d}: {:s}\n".format(idx+1, name)
+        show_help += "Enter > " 
+
+        no = input(show_help)
+        if not no:
+            continue
+
+        no = int(no)
+        if not (0 < no and no <= len(menus)):
+            print("Wrong number")
+            continue
+
+        c = menus[no-1]
+        if c == 'scan':
+            script.exports.scan(get_value())
+        elif c == 'show':
+            script.exports.show()
+        elif c == 'next':
+            script.exports.next(int(input("Next value:")))
+        elif c == 'edit':
+            script.exports.edit(int(input("Edit value:")))
+        else:
+            print("Please insert '{:s}' wrapper code to main.py".format(c))
+            break
+
+    session.detach()
+
+if __name__ == '__main__':
+    run()

9b1d40afd9ee2d4a14077679f89031f763f191c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:34 +0000

    Add package Terminal-Apple-Season/1.1.1/Terminal-Apple-Season-1.1.1.tar.gz

---
+++ packages/Terminal-Apple-Season/Terminal-Apple-Season-1.1.1.tar.gz/Terminal-Apple-Season-1.1.1/apple_season/basket.py
@@ -0,0 +1,55 @@
+from os.path import join, dirname
+import sys
+
+cwd = dirname(dirname(__file__))
+
+if cwd not in sys.path:
+    sys.path.append(cwd)
+
+from apple_season.coords import Coords, Image, Char
+
+
+basket_string = """\------------------/
+ \                /
+  \              /
+   \____________/"""
+
+basket_grid = [list(row) for row in basket_string.split('\n')]
+
+chars = []
+for r, row in enumerate(basket_grid):
+    for x, char in enumerate(row):
+        chars.append(Char(x, len(basket_grid) - 1 - r, char))
+
+basket_image = Image(chars)
+
+
+class Basket(Coords):
+
+    def __init__(self, canvas):
+
+        Coords.__init__(self, int(canvas.width / 2), 1, basket_image, canvas)
+
+
+    def move(self, direction):
+
+        self.previous_x = self.x
+        self.previous_y = self.y
+
+        if direction == "right":
+
+            # quit function if off the right edge
+            for char in self.image.chars:
+                if char.x + self.x + 5 > self.canvas.width:
+                    return
+
+            self.x += 5
+
+        elif direction == "left":
+            
+            # quit function if off the left edge
+            for char in self.image.chars:
+                if char.x + self.x - 5 < 0:
+                    return
+
+            self.x -= 5

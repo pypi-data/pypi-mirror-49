0f36fac15c5b825401b852b83a99f99dc42f1c92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:34 +0000

    Add package hasasia/0.1.1/hasasia-0.1.1.tar.gz

---
+++ packages/hasasia/hasasia-0.1.1.tar.gz/hasasia-0.1.1/tests/test_hasasia.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `hasasia` package."""
+
+import pytest
+import numpy as np
+
+from hasasia import sensitivity as sens
+from hasasia import skymap
+from hasasia.sim import sim_pta
+
+phi = np.random.uniform(0, 2*np.pi,size=34)
+theta = np.random.uniform(0, np.pi,size=34)
+freqs = np.logspace(np.log10(5e-10),np.log10(5e-7),400)
+timespans=[11.4 for ii in range(10)]
+timespans.extend([3.0 for ii in range(24)])
+
+
+@pytest.fixture
+def pta_simple():
+    """
+    Sample set of pulsars for testing. All Tspan=11.4 yrs
+    """
+    return sim_pta(timespan=11.4, cad=23, sigma=1e-7,
+                   phi=phi, theta=theta, Npsrs=34)
+
+@pytest.fixture
+def pta_heter():
+    """
+    Sample set of pulsars for testing.
+    """
+    return sim_pta(timespan=timespans,cad=23,sigma=1e-7,
+                   phi=phi,theta=theta)
+
+def test_simple_pta(pta_simple):
+    """Sensitivity Tests"""
+    spectra = []
+    for p in pta_simple:
+        sp = sens.Spectrum(p, freqs=freqs)
+        sp.Tf
+        spectra.append(sp)

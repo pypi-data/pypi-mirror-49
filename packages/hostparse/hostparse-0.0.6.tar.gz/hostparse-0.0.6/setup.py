e3a9fc1123b6c91b21c282cda776f05cc83a206a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:34 +0000

    Add package hostparse/0.0.6/hostparse-0.0.6.tar.gz

---
+++ packages/hostparse/hostparse-0.0.6.tar.gz/hostparse-0.0.6/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+        name = 'hostparse',
+        version = "0.0.6",
+        author = 'Yonathan Klijnsma',
+        author_email = 'admin@0x3a.com',
+        url = 'https://github.com/0x3a/hostparse',
+        packages=find_packages(),
+        include_package_data=True,
+        description = 'A command-line client for URL and hostname swizzling ',
+        long_description=read('README.md'),
+        long_description_content_type="text/markdown",
+        install_requires=[
+            'tldextract',
+        ],
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        entry_points={
+            'console_scripts': [
+                'hostparse=hostparse:main',
+            ],
+        },
+     )

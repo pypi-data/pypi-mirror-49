cf1d7d6a78fe7029bf86b28cd2df07f534384271
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:27 +0000

    Add package drf-firebase-auth-cavoke/0.0.7/drf-firebase-auth-cavoke-0.0.7.tar.gz

---
+++ packages/drf-firebase-auth-cavoke/drf-firebase-auth-cavoke-0.0.7.tar.gz/drf-firebase-auth-cavoke-0.0.7/drf_firebase_auth_cavoke/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'drf-firebase-auth-cavoke'
+__version__ = '0.0.7'
+__author__ = 'Alex Kovrigin'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2019 Alex Kovrigin'
+
+# Version synonym
+VERSION = __version__

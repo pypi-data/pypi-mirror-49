b6d353882d345831e7e6436f3e764fb7e27cc124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:19 +0000

    Add package cloudns-api/0.5/cloudns_api-0.5-py3-none-any.whl

---
+++ packages/cloudns-api/cloudns_api-0.5-py3-none-any.whl/cloudns_api/config.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+#
+# name:             config.py
+# author:           Harold Bradley III | Prestix Studio, LLC.
+# email:            harold@prestix.studio
+# created on:       05/27/2019
+#
+
+"""
+cloudns_api.config
+~~~~~~~~~~~~~~~~~~
+
+This module contains the API config constants.
+"""
+
+from os import environ
+
+
+def _is_true(env_var):
+    if env_var is None:
+        return False
+    return env_var.lower() in [1, 'true', 'yes']
+
+
+CLOUDNS_API_TESTING = _is_true(environ.get('CLOUDNS_API_TESTING'))
+
+CLOUDNS_API_AUTH_ID = environ.get('CLOUDNS_API_AUTH_ID')
+if not CLOUDNS_API_AUTH_ID:
+    if CLOUDNS_API_TESTING:
+        CLOUDNS_API_AUTH_ID = 'test_id'
+    else:  # pragma: no cover
+        raise EnvironmentError(
+            'Environment variable "CLOUDNS_API_AUTH_ID" not set.'
+        )
+
+CLOUDNS_API_AUTH_PASSWORD = environ.get('CLOUDNS_API_AUTH_PASSWORD')
+if not CLOUDNS_API_AUTH_PASSWORD:
+    if CLOUDNS_API_TESTING:
+        CLOUDNS_API_AUTH_PASSWORD = 'test_password'
+    else:  # pragma: no cover
+        raise EnvironmentError(
+            'Environment variable "CLOUDNS_API_AUTH_PASSWORD" not set.'
+        )
+
+CLOUDNS_API_DEBUG = _is_true(environ.get('CLOUDNS_API_DEBUG'))

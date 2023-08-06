41a9c5bd044e907f01385e9a4208177d42794fec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:06 +0000

    Add package bert-for-tf2/0.3.2/bert-for-tf2-0.3.2.tar.gz

---
+++ packages/bert-for-tf2/bert-for-tf2-0.3.2.tar.gz/bert-for-tf2-0.3.2/bert/__init__.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+#
+# created by kpe on 15.Mar.2019 at 15:28
+#
+from __future__ import division, absolute_import, print_function
+
+from .attention import AttentionLayer
+from .layer import Layer
+from .model import BertModelLayer
+
+from .loader import StockBertConfig, load_stock_weights

bbcec2768fec7dade05f4ab62fd6919f09f30bde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:06 +0000

    Add package troposphere-mate/0.0.3/troposphere_mate-0.0.3-py2.py3-none-any.whl

---
+++ packages/troposphere-mate/troposphere_mate-0.0.3-py2.py3-none-any.whl/troposphere_mate/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+"""
+Package Description.
+"""
+
+from ._version import __version__
+
+__short_description__ = "Package short description."
+__license__ = "MIT"
+__author__ = "Sanhe Hu"
+__author_email__ = "husanhe@gmail.com"
+__github_username__ = "MacHu-GWU"
+
+try:
+    from troposphere import *
+    from troposphere import AWSObject as TroposphereAWSObject
+
+    from .core.associate import associate
+    from .core.mate import (
+        Template,
+        Parameter,
+        Output,
+    )
+    from troposphere_mate.core.canned import ConfigClass, MultiEnvBasicConfig, Constant, Derivable
+except ImportError as e:
+    pass

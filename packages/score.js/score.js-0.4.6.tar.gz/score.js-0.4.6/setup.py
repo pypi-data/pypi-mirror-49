d8898a58e865d365aa02c826b68704ade175820d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:58 +0000

    Add package score.js/0.4.6/score.js-0.4.6.tar.gz

---
+++ packages/score.js/score.js-0.4.6.tar.gz/score.js-0.4.6/setup.py
@@ -0,0 +1,38 @@
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.rst')) as f:
+    README = f.read()
+
+setup(
+    name='score.js',
+    version='0.4.6',
+    description='Helpers for managing javascript with The SCORE Framework',
+    long_description=README,
+    author='strg.at',
+    author_email='score@strg.at',
+    url='http://score-framework.org',
+    keywords='score framework web javascript',
+    packages=['score', 'score.js'],
+    namespace_packages=['score'],
+    zip_safe=False,
+    license='LGPL',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Framework :: Pyramid',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General '
+            'Public License v3 or later (LGPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
+    install_requires=[
+        'score.webassets >= 0.3.22',
+    ],
+)

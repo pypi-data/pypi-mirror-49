dff303009277ac5f0127ad497ae8d9ce06674c91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:44 +0000

    Add package pymsl/1.3/pymsl-1.3.tar.gz

---
+++ packages/pymsl/pymsl-1.3.tar.gz/pymsl-1.3/setup.py
@@ -0,0 +1,30 @@
+"""Setup module"""
+
+from setuptools import setup
+
+with open('README.md', 'r') as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setup(
+    name='pymsl',
+    version='1.3',
+    description='Python library for interacting with the Netflix MSL API',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    url='https://github.com/truedread/pymsl',
+    author='truedread',
+    author_email='truedread11@gmail.com',
+    license='GNU GPLv3',
+    packages=['pymsl'],
+    install_requires=['pycryptodomex', 'requests'],
+    classifiers=[
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Utilities'
+    ]
+)

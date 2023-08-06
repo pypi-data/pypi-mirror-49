9619aaf086a0542638526844a5c38028b8e9ab17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:30 +0000

    Add package ytu/0.2.0/ytu-0.2.0-py2.py3-none-any.whl

---
+++ packages/ytu/ytu-0.2.0-py2.py3-none-any.whl/ytu/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for ytu."""
+
+__author__ = """Ramiro Gómez"""
+__email__ = 'code@ramiro.org'
+__version__ = '0.2.0'
+
+
+from .ytu import video_id, is_youtube

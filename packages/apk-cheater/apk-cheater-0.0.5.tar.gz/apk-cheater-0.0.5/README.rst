45e50d8444ed3df9cfafeac5669addac28353ccb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:41 +0000

    Add package apk-cheater/0.0.5/apk-cheater-0.0.5.tar.gz

---
+++ packages/apk-cheater/apk-cheater-0.0.5.tar.gz/apk-cheater-0.0.5/README.rst
@@ -0,0 +1,17 @@
+apk-cheater
+============================================================
+| Memory editor with frida-gadget
+
+.. code:: sh
+
+  $ pip install frida-gadget 
+  $ pip install apk-cheater
+  $ frida-gadget {APK_PATH}
+  > Gadget APK: {GADGET_APK_PATH}
+  ... singing and running your usb device ..
+  $ apk-cheater
+  1: scan
+  2: show
+  3: next
+  4: edit
+  >  
---
+++ packages/apk-cheater/apk-cheater-0.0.5.tar.gz/apk-cheater-0.0.5/setup.py
@@ -0,0 +1,35 @@
+import sys
+import setuptools
+
+if sys.version_info < (3,):
+    print("Unfortunately, your python version is not supported!\n"
+          + "Please upgrade at least to Python 3!")
+    sys.exit(1)
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="apk-cheater",
+    python_requires='>=3',
+    version="0.0.5",
+    author="ksg97031",
+    author_email="ksg97031@gmail.com",
+    description="Easy to use frida gadget",
+    install_requires=['frida==12.6.10'],
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/ksg97031/apk-cheater",
+    packages=setuptools.find_packages(),
+    package_data={'scripts': ["loader.js"]},
+    entry_points={
+        'console_scripts': [
+            'apk-cheater = scripts.main:run'
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

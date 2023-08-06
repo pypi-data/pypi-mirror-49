da485b2e545d9a68f8dec008bdcaba7ece2c3eea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:14 +0000

    Add package krakensegment/0.3/krakensegment-0.3.tar.gz

---
+++ packages/krakensegment/krakensegment-0.3.tar.gz/krakensegment-0.3/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from __future__ import absolute_import, division, print_function
+
+from setuptools import setup
+
+setup(
+    packages=['krakensegment'],
+    version='0.3',
+)

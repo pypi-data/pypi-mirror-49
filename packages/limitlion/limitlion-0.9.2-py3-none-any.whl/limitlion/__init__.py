1aae52097d14cae54c3617c355bd7a932f8a0621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:03 +0000

    Add package limitlion/0.9.2/limitlion-0.9.2-py3-none-any.whl

---
+++ packages/limitlion/limitlion-0.9.2-py3-none-any.whl/limitlion/__init__.py
@@ -0,0 +1,12 @@
+"""LimitLion package."""
+
+from .throttle import *
+
+__all__ = ['throttle',
+           'throttle_configure',
+           'throttle_delete',
+           'throttle_get',
+           'throttle_reset',
+           'throttle_set',
+           'throttle_wait',
+           ]

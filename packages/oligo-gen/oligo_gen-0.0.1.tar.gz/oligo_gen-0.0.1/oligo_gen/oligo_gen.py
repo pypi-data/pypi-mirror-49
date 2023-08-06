c87dfc2682344768dcb77e17abe803ed88228389
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:40 +0000

    Add package oligo-gen/0.0.1/oligo_gen-0.0.1.tar.gz

---
+++ packages/oligo-gen/oligo_gen-0.0.1.tar.gz/oligo_gen-0.0.1/oligo_gen/oligo_gen.py
@@ -0,0 +1,19 @@
+import Bio
+from Bio import SeqIO
+
+def recordOligo(seq, leno, off):
+	s = len(seq)
+	for i in range(0,s-(s%off)-leno+1,off):
+		print(seq[i:i+leno])
+
+def genomeOligo(genome, olen, ooff):
+	for k in range(0,len(genome)):
+		recordOligo(str(genome[k].seq), olen, ooff)
+
+def oligo_gen(filename, length, offset):
+	records = list(SeqIO.parse(filename, "fasta"))
+	oligo_len = int(length)
+	oligo_offset = int(offset)
+	genomeOligo(records, oligo_len, oligo_offset)
+
+
---
+++ packages/oligo-gen/oligo_gen-0.0.1.tar.gz/oligo_gen-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+	name='oligo_gen',
+	version='0.0.1',
+	description='generate oligos from fasta',
+	py_modules=['oligo_gen'],
+	package_dir={'':'oligo_gen'},
+)

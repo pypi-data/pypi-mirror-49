a664aedbd2736136a7b1abd0fa8d2e16ce1766eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:33 +0000

    Add package crystal-ball/0.1.2/crystal-ball-0.1.2.tar.gz

---
+++ packages/crystal-ball/crystal-ball-0.1.2.tar.gz/crystal-ball-0.1.2/setup.py
@@ -0,0 +1,27 @@
+
+import setuptools
+
+setuptools.setup(
+    name='crystal-ball',
+    version='0.1.2',
+    author='Winn Y Cui',
+    author_email='winn.yc@berkeley.edu',
+    packages=setuptools.find_packages(),
+    scripts=[],
+    url='http://pypi.python.org/pypi/crystal-ball/',
+    license='LICENSE.txt',
+    description='Useful crystal-ball-related stuff.',
+    long_description=open('README.asc').read(),
+    long_description_content_type="text/markdown",
+    install_requires=[
+        # "Django >= 1.1.1",
+        # "caldav == 0.1.4",
+        "pandas",
+        "seaborn",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)

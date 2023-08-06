05f0f23a27f8e983f31d590201cc0fd112ef3373
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:03 +0000

    Add package ramda/0.5.3/ramda-0.5.3.tar.gz

---
+++ packages/ramda/ramda-0.5.3.tar.gz/ramda-0.5.3/ramda/keys_test.py
@@ -0,0 +1,9 @@
+from .keys import keys
+from ramda.private.asserts import assert_iterables_equal
+
+
+test_dict = {"a": 1, "b": 2, "c": 3}
+
+
+def keys_test():
+    assert_iterables_equal(keys(test_dict), set(["a", "c", "b"]))

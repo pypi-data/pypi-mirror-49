630c4be07e3b3767ef2c027b7fb03038d043278b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:30 +0000

    Add package mario-addons/0.1.12/mario_addons-0.1.12-py2.py3-none-any.whl

---
+++ packages/mario-addons/mario_addons-0.1.12-py2.py3-none-any.whl/mario_addons/plugins/read.py
@@ -0,0 +1,24 @@
+import csv
+import typing as t
+
+
+def read_csv_dicts(
+    file, header: bool, **kwargs
+) -> t.Iterable[t.Dict[t.Union[str, int], str]]:
+    "Read csv rows into an iterable of dicts."
+
+    rows = list(file)
+
+    first_row = next(csv.reader(rows))
+    if header:
+        fieldnames = first_row
+        reader = csv.DictReader(rows, fieldnames=fieldnames, **kwargs)
+        return list(reader)[1:]
+
+    fieldnames = range(len(first_row))
+    return csv.DictReader(rows, fieldnames=fieldnames, **kwargs)
+
+
+def read_csv_tuples(file, **kwargs) -> t.Iterable[t.Tuple]:
+    "Read csv rows into an iterable of tuples."
+    return (tuple(row) for row in csv.reader(file, **kwargs))

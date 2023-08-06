a8f62b594e184623d9f70ebd6bd903604ae49b88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:53 +0000

    Add package ctpbee/0.22/ctpbee-0.22.tar.gz

---
+++ packages/ctpbee/ctpbee-0.22.tar.gz/ctpbee-0.22/ctpbee/interface/__init__.py
@@ -0,0 +1,33 @@
+"""
+Notice : 神兽保佑 ，测试一次通过
+//      
+//      ┏┛ ┻━━━━━┛ ┻┓
+//      ┃　　　　　　 ┃
+//      ┃　　　━　　　┃
+//      ┃　┳┛　  ┗┳　┃
+//      ┃　　　　　　 ┃
+//      ┃　　　┻　　　┃
+//      ┃　　　　　　 ┃
+//      ┗━┓　　　┏━━━┛
+//        ┃　　　┃   Author: somewheve
+//        ┃　　　┃   Datetime: 2019/7/7 下午2:45  ---> 无知即是罪恶
+//        ┃　　　┗━━━━━━━━━┓
+//        ┃　　　　　　　    ┣┓
+//        ┃　　　　         ┏┛
+//        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+//          ┃ ┫ ┫   ┃ ┫ ┫
+//          ┗━┻━┛   ┗━┻━┛
+//
+"""
+from ctpbee.interface.ctp import BeeTdApi, BeeMdApi, BeeTdApiApp
+
+
+class Interface:
+    api_map = {
+        "ctp": (BeeMdApi, BeeTdApi),
+        "ctp_se": (BeeMdApi, BeeTdApiApp)
+    }
+
+    @classmethod
+    def get_interface(self, app):
+        return self.api_map.get(app.config.get("INTERFACE"), None)

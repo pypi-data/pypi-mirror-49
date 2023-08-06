6e08d9de6a56bd75220de6cf11fa8012b985f703
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:53 +0000

    Add package qgis-plugin-ci/1.0.7/qgis_plugin_ci-1.0.7-py3.7.egg

---
+++ packages/qgis-plugin-ci/qgis_plugin_ci-1.0.7-py3.7.egg/qgispluginci/exceptions.py
@@ -0,0 +1,21 @@
+
+
+class TranslationFailed(Exception):
+    pass
+
+
+class TransifexNoResource(Exception):
+    pass
+
+
+class TransifexManyResources(Warning):
+    pass
+
+
+class GithubReleaseNotFound(Exception):
+    pass
+
+
+class GithubReleaseCouldNotUploadAsset(Exception):
+    pass
+

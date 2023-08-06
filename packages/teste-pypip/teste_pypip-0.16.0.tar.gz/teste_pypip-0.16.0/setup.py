4315a5c1f678b30a6979e904e5e933fbda3a200b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:00 +0000

    Add package teste-pypip/0.16.0/teste_pypip-0.16.0.tar.gz

---
+++ packages/teste-pypip/teste_pypip-0.16.0.tar.gz/teste_pypip-0.16.0/setup.py
@@ -0,0 +1,14 @@
+﻿# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='teste_pypip',
+    version='0.16.0',
+    license='MIT License',
+    author='Thiago Rocha Ferreira',
+    author_email='thiagorocha503@gmail.com',
+    keywords='teste_pypip',
+    description='teste de pacote python',
+    packages=['calc'],
+    install_requires=[]
+)

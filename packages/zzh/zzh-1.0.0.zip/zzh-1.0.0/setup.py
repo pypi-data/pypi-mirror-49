b05b4b5d86b7db1d2e3b42bcdd6bf1d14b8da986
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:25 +0000

    Add package zzh/1.0.0/zzh-1.0.0.zip

---
+++ packages/zzh/zzh-1.0.0.zip/zzh-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+	name = "zzh",
+	version = "1.0.0",
+	description = "yeye",
+	author = "zzh",
+	author_email = "1905811702@qq.com",
+	url = "https://github.com/zzhwln/zzh",
+	requires=["aiohttp","lxml"],
+	long_description = """very log"""      
+ ) 

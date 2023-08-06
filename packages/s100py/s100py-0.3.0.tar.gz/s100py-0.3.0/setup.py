8587c090f490e674fdc3f24b2a56929dec31e8e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:12:02 +0000

    Add package s100py/0.3.0/s100py-0.3.0.tar.gz

---
+++ packages/s100py/s100py-0.3.0.tar.gz/s100py-0.3.0/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+import os
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# Get the long description from the README file
+with open(os.path.join(HERE, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='s100py',
+    version='0.3.0',
+    license='BSD-2-Clause',
+    author='Erin Nagel, Jason Greenlaw',
+    author_email='erin.nagel@noaa.gov, jason.greenlaw@noaa.gov',
+    description='This python package provides utilities for encoding hydrographic datasets in the International Hydrographic Organization (IHO) S-100 format',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/noaa-ocs-s100/s100py',
+    packages=setuptools.find_packages(),
+    install_requires=['thyme(>=0.3.3)', 'numpy', 'h5py'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+    ],
+)

26a7c389d047e79b6e4afc41d5488b34f3b90f17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:13 +0000

    Add package html-to-csv/0.0.2/html_to_csv-0.0.2-py3-none-any.whl

---
+++ packages/html-to-csv/html_to_csv-0.0.2-py3-none-any.whl/html2csv/__main__.py
@@ -0,0 +1,30 @@
+import argparse
+import sys
+
+from . import Converter
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Convert HTML table to CSV format.')
+    parser.add_argument('input_file',
+        help='input files (default: standard input)',
+        nargs='*',
+        type=argparse.FileType('r'),
+        default=[sys.stdin],
+    )
+    parser.add_argument('-o', '--output-file',
+        help='output file (default: standard output)',
+        nargs='?',
+        type=argparse.FileType('w'),
+        default=sys.stdout,
+    )
+    args = parser.parse_args()
+    converter = Converter()
+    for input_file in args.input_file:
+        output = converter.convert(input_file.read())
+        for csv_string, _ in output:
+            args.output_file.write(csv_string)
+
+
+if __name__ == '__main__':
+    sys.exit(main())

df7728bf5501a32f7111d40aa069e04aab5db4d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:06 +0000

    Add package onegov.user/0.22.3/onegov.user-0.22.3-py3-none-any.whl

---
+++ packages/onegov.user/onegov.user-0.22.3-py3-none-any.whl/onegov/user/tests/test_collections.py
@@ -0,0 +1,193 @@
+import pytest
+
+from onegov.core.crypto import RANDOM_TOKEN_LENGTH
+from onegov.user.collections import UserCollection
+from onegov.user.collections import UserGroupCollection
+from onegov.user.errors import (
+    AlreadyActivatedError,
+    ExistingUserError,
+    InsecurePasswordError,
+    InvalidActivationTokenError,
+    UnknownUserError,
+)
+
+
+def test_user_add(session):
+
+    users = UserCollection(session)
+    user = users.add('dmr@example.org', 'p@ssw0rd', 'root')
+
+    assert users.by_username(user.username).username == 'dmr@example.org'
+
+    groups = UserGroupCollection(session)
+    group = groups.add(name='group')
+
+    user = users.add('abc@example.org', 'p@ssw0rd', 'root', group=group)
+    assert users.by_username(user.username).group.name == 'group'
+
+
+def test_user_add_conflict(session):
+
+    users = UserCollection(session)
+    users.add('dmr@example.org', 'p@ssw0rd', 'root')
+
+    with pytest.raises(ExistingUserError):
+        users.add('dmr@example.org', 'p@ssw0rd', 'root')
+
+
+def test_user_exists(session):
+
+    users = UserCollection(session)
+
+    assert not users.exists('dmr@example.org')
+    users.add('dmr@example.org', 'p@ssw0rd', 'root')
+    assert users.exists('dmr@example.org')
+
+
+def test_user_by_yubikey(session):
+
+    users = UserCollection(session)
+    users.add('dmr@example.org', 'p@ssw0rd', 'root', second_factor={
+        'type': 'yubikey',
+        'data': 'cccccccdefgh'
+    })
+
+    assert users.by_yubikey('cccccccdefgh')
+
+
+def test_set_yubikey(session):
+    users = UserCollection(session)
+    user = users.add('dmr@example.org', 'p@ssw0rd', 'root')
+
+    with pytest.raises(AssertionError):
+        user.yubikey = 'xxx'
+
+
+def test_user_data(session):
+
+    users = UserCollection(session)
+
+    user = users.add('dmr@example.org', 'p@ssw0rd', 'root', data={
+        'firstname': 'Dennis',
+        'lastname': 'Ritchie'
+    })
+
+    assert user.data['firstname'] == 'Dennis'
+    assert user.data['lastname'] == 'Ritchie'
+
+
+def test_user_delete(session):
+
+    users = UserCollection(session)
+    users.add('dmr@example.org', 'p@ssw0rd', 'root')
+
+    assert users.exists('dmr@example.org')
+    users.delete('dmr@example.org')
+    assert not users.exists('dmr@example.org')
+
+    with pytest.raises(UnknownUserError):
+        users.delete('dmr@example.org')
+
+
+def test_user_password(session):
+
+    users = UserCollection(session)
+    user = users.add('AzureDiamond', 'hunter2', 'irc-user')
+    assert user.password.startswith('$bcrypt-sha256$2')
+    assert user.password_hash.startswith('$bcrypt-sha256$2')
+
+    def may_login(username, password):
+        return bool(users.by_username_and_password(username, password))
+
+    assert may_login('AzureDiamond', 'hunter2')
+    assert not may_login('AzureDiamond', 'hunter3')
+    assert not may_login('AzureDiamon', 'hunter2')
+
+    user.password = 'test'
+    assert user.password.startswith('$bcrypt-sha256$2')
+    assert user.password_hash.startswith('$bcrypt-sha256$2')
+    assert may_login('AzureDiamond', 'test')
+
+    # this is an exception, we allow for empty passwords, but we will not
+    # query for them by default, see usercollection class
+    user.password = ''
+    assert user.password.startswith('$bcrypt-sha256$2')
+    assert user.password_hash.startswith('$bcrypt-sha256$2')
+    assert not may_login('AzureDiamon', '')
+
+
+def test_register_user(session):
+
+    users = UserCollection(session)
+
+    class MockRequest(object):
+        client_addr = '127.0.0.1'
+
+    request = MockRequest()
+
+    with pytest.raises(AssertionError):
+        users.register(None, None, request)
+
+    with pytest.raises(InsecurePasswordError):
+        users.register('user', 'short', request)
+
+    user = users.register('user', 'p@ssw0rd', request)
+    token = user.data['activation_token']
+
+    assert len(token) == RANDOM_TOKEN_LENGTH
+    assert not user.active
+    assert user.role == 'member'
+
+    with pytest.raises(UnknownUserError):
+        users.activate_with_token('waldo', 'asdf')
+
+    with pytest.raises(InvalidActivationTokenError):
+        users.activate_with_token('user', 'asdf')
+
+    users.activate_with_token('user', token)
+    assert user.active
+    assert 'activation_token' not in user.data
+
+    with pytest.raises(ExistingUserError):
+        user = users.register('user', 'p@ssw0rd', request)
+
+    with pytest.raises(AlreadyActivatedError):
+        users.activate_with_token('user', token)
+
+
+def test_user_by_role(session):
+
+    users = UserCollection(session)
+
+    assert users.by_roles('admin', 'member').count() == 0
+
+    users.add('admin@example.org', 'p@ssw0rd', role='admin')
+    users.add('member@example.org', 'p@ssw0rd', role='member')
+
+    assert users.by_roles('admin', 'member').count() == 2
+    assert users.by_roles('admin').count() == 1
+    assert users.by_roles('member').count() == 1
+
+
+def test_user_lowercase(session):
+
+    users = UserCollection(session)
+    users.add('Admin@Foo.Bar', 'p@ssw0rd', role='admin')
+
+    assert users.by_username('Admin@Foo.Bar').username == 'admin@foo.bar'
+    assert users.by_username('admin@foo.bar').username == 'admin@foo.bar'
+
+    with pytest.raises(ExistingUserError) as e:
+        users.add('admin@foo.Bar', 'p@ssw0rd', role='admin')
+
+    assert e.value.message == 'admin@foo.Bar'
+
+
+def test_user_group(session):
+    groups = UserGroupCollection(session)
+
+    groups.add(name='group y')
+    groups.add(name='group x')
+    assert [group.name for group in groups.query().all()] == [
+        'group x', 'group y'
+    ]

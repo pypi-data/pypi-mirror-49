da8cf63fa1bde3966acb3c03123b89ad2b7d063a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:49 +0000

    Add package generoo/2019.7.24.5/generoo-2019.7.24.5-py3-none-any.whl

---
+++ packages/generoo/generoo-2019.7.24.5-py3-none-any.whl/generoo/archetypes/java/spring-boot/2.0/{{artifact_id}}-api/src/main/java/{{group_id_slashes}}/{{artifact_id_slashes}}/{{artifact_id_capitalized}}Application.java
@@ -0,0 +1,20 @@
+package {{group_id}}.{{artifact_id_periods}};
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication(scanBasePackages = {
+	"{{group_id}}.{{artifact_id_periods}}"
+})
+@EnableSwagger2
+{{#hibernate}}
+@EnableJpaRepositories(basePackages = "{{group_id}}.{{artifact_id_periods}}.repository")
+{{/hibernate}}
+public class {{artifact_id_capitalized}}Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run({{artifact_id_capitalized}}Application.class, args);
+	}
+}

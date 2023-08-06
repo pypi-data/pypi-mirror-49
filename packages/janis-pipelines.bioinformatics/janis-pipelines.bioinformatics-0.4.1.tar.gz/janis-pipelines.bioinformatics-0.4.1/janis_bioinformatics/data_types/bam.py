ae60e65a25280697c00261a61a2ee7f44d4b6129
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:19 +0000

    Add package janis-pipelines.bioinformatics/0.4.1/janis-pipelines.bioinformatics-0.4.1.tar.gz

---
+++ packages/janis-pipelines.bioinformatics/janis-pipelines.bioinformatics-0.4.1.tar.gz/janis-pipelines.bioinformatics-0.4.1/janis_bioinformatics/data_types/bam.py
@@ -0,0 +1,23 @@
+from janis_core import File
+
+
+class Bam(File):
+    @staticmethod
+    def name():
+        return "BAM"
+
+    def doc(self):
+        return "A binary version of a SAM file, http://software.broadinstitute.org/software/igv/bam"
+
+
+class BamBai(Bam):
+    @staticmethod
+    def name():
+        return "BamPair"
+
+    @staticmethod
+    def secondary_files():
+        return ["^.bai"]
+
+    def doc(self):
+        return "A Bam and bai as the secondary"

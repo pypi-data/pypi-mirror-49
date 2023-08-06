10144372ffc8541bc52e70c877cc67b38129441d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:35 +0000

    Add package cmd-tools/0.0.4/cmd_tools-0.0.4-py3-none-any.whl

---
+++ packages/cmd-tools/cmd_tools-0.0.4-py3-none-any.whl/cmd_tools-0.0.4.data/scripts/command.py
@@ -0,0 +1,36 @@
+import click
+from subprocess import PIPE, run
+from pathlib import Path
+import json
+
+
+@click.command()
+def hello():
+    click.echo('hello world', nl=False)
+
+
+@click.command()
+@click.argument('command', nargs=-1)
+def cmd(command):
+    command = " ".join(command)
+    result = run_command(command)
+    click.echo(result, nl=False)
+
+
+def run_command(command):
+    result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
+    output = result.stdout
+    return output if output else result.stderr
+
+
+def load_config(path=Path.home() / Path('.cmdrc.json')):
+    if not path.exists():
+        raise ValueError('~/.cmdrc.json not exist')
+    with open(path, 'r') as f:
+        config = f.read()
+
+    try:
+        config = json.loads(config)
+    except json.decoder.JSONDecodeError:
+        raise ValueError(f'{path} file format error')
+    return config

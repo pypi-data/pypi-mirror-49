57a66b11162be47f990df835ca859d0948e5c16a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:22 +0000

    Add package pycopy-cpython-utimeq/0.0/pycopy-cpython-utimeq-0.0.tar.gz

---
+++ packages/pycopy-cpython-utimeq/pycopy-cpython-utimeq-0.0.tar.gz/pycopy-cpython-utimeq-0.0/utimeq.py
@@ -0,0 +1,14 @@
+class utimeq:
+
+    def __init__(self, sz):
+        self.heap = []
+        self.max_sz = sz
+
+    def push(self, time, obj, userdata):
+
+    def pop(self, arr):
+
+    def peektime(self):
+        return self.heap[0][0]
+
+    def remove(self, id):

e6955936f49c3f0266f5a5ff56c84ffa3473fd0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:24 +0000

    Add package pypassword/0.1.2/pypassword-0.1.2-py2.py3-none-any.whl

---
+++ packages/pypassword/pypassword-0.1.2-py2.py3-none-any.whl/pypassword/pypassword.py
@@ -0,0 +1,32 @@
+import string
+import random
+import argparse
+
+
+def generate(size=12,
+             chars=string.ascii_letters + string.digits,
+             special=False):
+    """
+    Generate password
+    :param size: Size is number of chars
+    :param chars: Charset
+    :param special: Boolean, us special characters or not
+    :return: Random string
+    """
+    if special:
+        chars += string.punctuation
+    return ''.join(random.SystemRandom().choice(chars) for _ in range(size))
+
+
+if __name__ == "__main__":
+    size = 14
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--size", help="Random string size", type=int)
+    parser.add_argument("-x", help="Use special characters", action="store_true")
+
+    args = parser.parse_args()
+    if args.size:
+        size = args.size
+
+    print(generate(size=size,
+                   special=args.x))

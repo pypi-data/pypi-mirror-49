e44199fa077d952dfdce0ea8071dbe9e541ff092
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:34 +0000

    Add package ipa2/0.1.0/ipa2-0.1.0-py3.6.egg

---
+++ packages/ipa2/ipa2-0.1.0-py3.6.egg/ipa2/test.py
@@ -0,0 +1,13 @@
+import unittest
+from ipa2.main import *
+from ipa2 import IPAConverter
+
+class Test(unittest.TestCase):
+
+    def test_IPA(self):
+        ipa = IPAConverter('yue')
+        self.assertEqual(['nei˩˧ hou˧˥', 'lei˩˧ hou˧˥'], ipa.convert_sent("你好"))
+
+    def test_pip(self):
+        ipa = IPAConverter('yue')
+        self.assertEqual(['nei˩˧ hou˧˥', 'lei˩˧ hou˧˥'], ipa.convert_sent("你好"))

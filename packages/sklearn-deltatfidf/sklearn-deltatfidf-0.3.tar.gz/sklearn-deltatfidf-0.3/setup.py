04dd5b5dcd1748f820b2b563ed8f7c4a2ee8130f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:03:56 +0000

    Add package sklearn-deltatfidf/0.3/sklearn-deltatfidf-0.3.tar.gz

---
+++ packages/sklearn-deltatfidf/sklearn-deltatfidf-0.3.tar.gz/sklearn-deltatfidf-0.3/setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='sklearn-deltatfidf',
+    version=0.3,
+    packages=['sklearn_deltatfidf'],
+    description='DeltaTfidfVectorizer for scikit-learn',
+    url='https://github.com/r-m-n/sklearn-deltatfidf',
+    download_url='https://github.com/r-m-n/sklearn-deltatfidf/archive/0.3.tar.gz',
+    keywords=['sklearn', 'scikit-learn', 'tfidf', 'deltatfidf', 'delta tfidf'],
+    license='MIT',
+    install_requires=[
+        'numpy',
+        'scipy',
+        'scikit-learn',
+        'six'
+    ]
+)

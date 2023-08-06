b5a196af38c568a98a03e0524336d06a16de8f63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:58 +0000

    Add package GongUtil/2.3/GongUtil-2.3.tar.gz

---
+++ packages/GongUtil/GongUtil-2.3.tar.gz/GongUtil-2.3/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name='GongUtil',
+    license='MIT',
+    version='2.3',
+    author='gongzhengyang',
+    author_email='2976560783@qq.com',
+    description='A small example',
+    url='https://github.com/2976560783/GongUtil',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+
+)

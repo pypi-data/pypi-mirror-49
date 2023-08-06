a6a06bd45721c2d9c57fa11923289fecfb67b5ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:44 +0000

    Add package PLP-Output/0.0.3/PLP-Output-0.0.3.tar.gz

---
+++ packages/PLP-Output/PLP-Output-0.0.3.tar.gz/PLP-Output-0.0.3/PLP-Output/__init__.py
@@ -0,0 +1,2 @@
+from plpout import plpout
+from savetohive import saveToHive
---
+++ packages/PLP-Output/PLP-Output-0.0.3.tar.gz/PLP-Output-0.0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='PLP-Output',
+    version='0.0.3',
+    description='Libreria para tratamiento de salidas PLP',
+    packages=['PLP-Output'],
+    author='Javier De la Fuente Arce',
+    author_email='javier.delafuente18@outlook.com',
+    url='https://github.com/javierclb/PLP-Output',
+    download_url='https://github.com/javierclb/PLP-Output/tree/master/dist',
+    keywords=['PLP','energy','CDEC'],
+    licence='MIT',
+    install_requires=['pandas']
+)

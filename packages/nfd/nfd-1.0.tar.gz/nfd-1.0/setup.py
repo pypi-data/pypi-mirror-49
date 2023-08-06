e542e7e360f6ac04ea9061c23d803b7a54e2b8f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:16 +0000

    Add package nfd/1.0/nfd-1.0.tar.gz

---
+++ packages/nfd/nfd-1.0.tar.gz/nfd-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+name='nfd',
+version='1.0',
+author='Nasir Ali',
+author_email='nasiralis1731@gmail.com',
+packages=['nfd'],
+scripts=['bin/nfd'],
+install_requires=['requests', ],
+)

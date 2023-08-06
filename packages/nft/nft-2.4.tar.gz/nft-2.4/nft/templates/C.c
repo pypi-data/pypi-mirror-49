c0d4fc5a9c6bc10669b802e8096cffac188d7c9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:33 +0000

    Add package nft/2.4/nft-2.4.tar.gz

---
+++ packages/nft/nft-2.4.tar.gz/nft-2.4/nft/templates/C.c
@@ -0,0 +1,18 @@
+/*-------------------------------------
+<filename>.c
+<project>
+
+    by <name> (<email>)
+       on <date>
+--------------------------------------*/
+
+#include <string.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+
+int main(int argc, char ** argv) {
+	printf("New C file created from template\n")
+    return 0;
+}

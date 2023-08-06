e089386d0be75132e29f277cbe851cc224619a5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:01:00 +0000

    Add package zhangxulong/0.0.9/zhangxulong-0.0.9.tar.gz

---
+++ packages/zhangxulong/zhangxulong-0.0.9.tar.gz/zhangxulong-0.0.9/CHANGELOG.md
@@ -0,0 +1,5 @@
+* Ver: 0.0.9 CloudFlare api admin dns record
+* Ver: 0.0.8 encode vps subscribe
+* Ver: 0.0.7 wechat bot with tuling
+* Ver: 0.0.5 baseZhang review codes
+* Ver: 0.0.2 python 3.0 and init 
---
+++ packages/zhangxulong/zhangxulong-0.0.9.tar.gz/zhangxulong-0.0.9/zhangxulong/__init__.py
@@ -0,0 +1,7 @@
+from .plot_utils import *
+from .classification_evaluate import *
+from .basic_wav_io import *
+from .extract_mfcc import *
+from .wechat_bot import *
+from .get_pdf import *
+from .encode_vps import *

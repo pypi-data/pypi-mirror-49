c5567933874020fca1a44b7f86753aaf30f91e09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:32 +0000

    Add package signalfx-tracing/0.0.12/signalfx-tracing-0.0.12.tar.gz

---
+++ packages/signalfx-tracing/signalfx-tracing-0.0.12.tar.gz/signalfx-tracing-0.0.12/scripts/site_/sitecustomize.py
@@ -0,0 +1,31 @@
+# Copyright (C) 2019 SignalFx, Inc. All rights reserved.
+from __future__ import print_function
+
+import traceback
+import os.path
+import sys
+
+from signalfx_tracing import auto_instrument, create_tracer
+from signalfx_tracing.utils import get_module
+
+
+access_token = os.environ.get('SIGNALFX_ACCESS_TOKEN')
+
+try:
+    auto_instrument(create_tracer(access_token=access_token, set_global=True))
+except Exception:
+    print(traceback.format_exc())
+
+# Do not prevent existing sitecustomize module import. Done by
+# removing this module's package and attempting to import
+# sitecustomize module.
+
+# Removing references to this sitecustomize module
+# can trigger garbage collection and cause lookup failures in other modules.
+sys.modules['sfx_sitecustomize'] = sys.modules.pop('sitecustomize', None)
+sys.path.remove(os.path.abspath(os.path.dirname(__file__)))
+
+# Attempt to load any existing sitecustomize
+module = get_module('sitecustomize')
+if module is None:  # reset to our own if no preexisting
+    sys.modules['sitecustomize'] = sys.modules['sfx_sitecustomize']

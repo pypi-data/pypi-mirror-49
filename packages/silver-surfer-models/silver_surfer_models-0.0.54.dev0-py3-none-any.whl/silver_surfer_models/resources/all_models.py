b8857188210aa78e1d7048260b793eb2794a94ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:34 +0000

    Add package silver-surfer-models/0.0.54.dev0/silver_surfer_models-0.0.54.dev0-py3-none-any.whl

---
+++ packages/silver-surfer-models/silver_surfer_models-0.0.54.dev0-py3-none-any.whl/silver_surfer_models/resources/all_models.py
@@ -0,0 +1,63 @@
+# flake8: noqa
+
+from ...silver_surfer_models.resources.Claim.models import ClaimModel
+from ...silver_surfer_models.resources.ClaimChallenged.models import (
+    ClaimChallengedModel,
+)
+from ...silver_surfer_models.resources.ClaimDependency.models import (
+    ClaimDependencyModel,
+)
+from ...silver_surfer_models.resources.Company.models import CompanyModel
+from ...silver_surfer_models.resources.CrossRef.models import CrossRefModel
+from ...silver_surfer_models.resources.DocumentReference.models import (
+    DocumentReferenceModel,
+)
+from ...silver_surfer_models.resources.File.models import FileModel
+from ...silver_surfer_models.resources.InstitutionProbability.models import (
+    InstitutionProbabilityModel,
+)
+from ...silver_surfer_models.resources.OrangeBookPatent.models import (
+    OrangeBookPatentModel,
+)
+from ...silver_surfer_models.resources.OrangeBookProduct.models import (
+    OrangeBookProductModel,
+)
+from ...silver_surfer_models.resources.PacerCase.models import (
+    PacerCaseModel,
+)
+from ...silver_surfer_models.resources.Patent.models import (
+    PatentModel,
+)
+from ...silver_surfer_models.resources.PatentApplication.models import (
+    PatentApplicationModel,
+)
+from ...silver_surfer_models.resources.PatentCompound.models import (
+    PatentCompoundModel,
+)
+from ...silver_surfer_models.resources.PatentIdentifierSynonym.models import (
+    PatentIdentifierSynonymModel,
+)
+from ...silver_surfer_models.resources.PriorArt.models import (
+    PriorArtModel,
+)
+from ...silver_surfer_models.resources.PriorArtRefRelation.models import (
+    PriorArtRefRelationModel,
+)
+from ...silver_surfer_models.resources.PTAB2Decision.models import (
+    PTAB2DecisionModel,
+)
+from ...silver_surfer_models.resources.PTAB2Document.models import (
+    PTAB2DocumentModel,
+)
+from ...silver_surfer_models.resources.PTAB2Proceeding.models import (
+    PTAB2ProceedingModel,
+)
+from ...silver_surfer_models.resources.RelatedMatter.models import (
+    RelatedMatterModel,
+)
+from ...silver_surfer_models.resources.TrialStat.models import (
+    TrialStatModel,
+)
+from ...silver_surfer_models.resources.UserSearchHistoryItem.models import (
+    UserSearchHistoryItemModel,
+)

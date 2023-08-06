79f023dcaac1f9d6ef36103331a5c96b836a6fd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:46 +0000

    Add package refcliq/0.1.6/refcliq-0.1.6.tar.gz

---
+++ packages/refcliq/refcliq-0.1.6.tar.gz/refcliq-0.1.6/src/refcliq/template/precache-manifest.7a8e7a3cb2c885f53b9f0e7f6595dfcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abac4e2d0c7f542449fe6e743d9803f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92e07bd97f2579f426e9",
+    "url": "/static/css/2.6639d161.chunk.css"
+  },
+  {
+    "revision": "4007be4a0be63e18b4cb",
+    "url": "/static/css/main.53bdb8a5.chunk.css"
+  },
+  {
+    "revision": "92e07bd97f2579f426e9",
+    "url": "/static/js/2.88a63599.chunk.js"
+  },
+  {
+    "revision": "4007be4a0be63e18b4cb",
+    "url": "/static/js/main.4341f576.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);

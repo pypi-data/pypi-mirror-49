03d2a0f41ee0662a68d906ed47a81bb47b13378b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:12 +0000

    Add package invenio-base/1.1.0/invenio_base-1.1.0-py2.py3-none-any.whl

---
+++ packages/invenio-base/invenio_base-1.1.0-py2.py3-none-any.whl/invenio_base/wsgi.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2015-2018 CERN.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""WSGI application factory for Invenio."""
+
+from __future__ import absolute_import, print_function
+
+# They were moved in the same version so they can be in one try/except
+try:
+    from werkzeug.middleware.dispatcher import DispatcherMiddleware
+    from werkzeug.middleware.proxy_fix import ProxyFix
+    WERKZEUG_014 = False
+except ImportError:
+    from werkzeug.wsgi import DispatcherMiddleware
+    from werkzeug.contrib.fixers import ProxyFix
+    WERKZEUG_014 = True
+
+
+def create_wsgi_factory(mounts_factories):
+    """Create a WSGI application factory.
+
+    Usage example:
+
+    .. code-block:: python
+
+       wsgi_factory = create_wsgi_factory({'/api': create_api})
+
+    :param mounts_factories: Dictionary of mount points per application
+        factory.
+
+    .. versionadded:: 1.0.0
+    """
+    def create_wsgi(app, **kwargs):
+        mounts = {
+            mount: factory(**kwargs)
+            for mount, factory in mounts_factories.items()
+        }
+        return DispatcherMiddleware(app.wsgi_app, mounts)
+    return create_wsgi
+
+
+def wsgi_proxyfix(factory=None):
+    """Fix ``REMOTE_ADDR`` based on ``X-Forwarded-For`` headers.
+
+    .. note::
+
+       You must set ``WSGI_PROXIES`` to the correct number of proxies,
+       otherwise you application is susceptible to malicious attacks.
+
+    .. versionadded:: 1.0.0
+    """
+    def create_wsgi(app, **kwargs):
+        wsgi_app = factory(app, **kwargs) if factory else app.wsgi_app
+        num_proxies = app.config.get('WSGI_PROXIES')
+        if num_proxies:
+            if WERKZEUG_014:
+                return ProxyFix(wsgi_app, num_proxies=num_proxies)
+            else:
+                return ProxyFix(wsgi_app, x_for=num_proxies)
+        return wsgi_app
+    return create_wsgi

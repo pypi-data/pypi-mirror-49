c44ca01008c8672eb7396ac747f796bc846e2a4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:32 +0000

    Add package testpackagesanc/1.1.8/testpackagesanc-1.1.8.tar.gz

---
+++ packages/testpackagesanc/testpackagesanc-1.1.8.tar.gz/testpackagesanc-1.1.8/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="testpackagesanc",
+    version="1.1.8",
+    author="Jin Xin",
+    author_email="jin.xin@accenture.com",
+    description="testConfig",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=["testpackagesanc"],
+    scripts=['testpackagesanc/config.py'],
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    entry_points={
+        "console_scripts": ["config = testpackagesanc.config:get_config",]
+    },
+)

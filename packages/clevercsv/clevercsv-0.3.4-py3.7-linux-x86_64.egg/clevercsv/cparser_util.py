3bb0128addef76e34024111572ca802e351783f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:04 +0000

    Add package clevercsv/0.3.4/clevercsv-0.3.4-py3.7-linux-x86_64.egg

---
+++ packages/clevercsv/clevercsv-0.3.4-py3.7-linux-x86_64.egg/clevercsv/cparser_util.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+"""
+Python utility functions that wrap the C parser.
+
+"""
+
+import io
+
+from .cparser import Parser, Error as ParserError
+from .dialect import SimpleDialect
+from .exceptions import Error
+from .parser import field_size_limit
+
+
+def parse_data(
+    iterable, dialect=None, delimiter=None, quotechar=None, escapechar=None
+):
+    """
+    Parse iterable given dialect. Wraps the C parser.
+
+    """
+    if dialect is None:
+        dialect = SimpleDialect("", "", "")
+    if not delimiter is None:
+        dialect.delimiter = delimiter
+    if not quotechar is None:
+        dialect.quotechar = quotechar
+    if not escapechar is None:
+        dialect.escapechar = escapechar
+
+    parser = Parser(
+        iterable,
+        delimiter=dialect.delimiter,
+        quotechar=dialect.quotechar,
+        escapechar=dialect.escapechar,
+        field_limit=field_size_limit(),
+    )
+    try:
+        for row in parser:
+            yield row
+    except ParserError as e:
+        raise Error(str(e))
+
+
+def parse_string(data, *args, **kwargs):
+    """ Utility for when the CSV file is encoded as a single string """
+    return parse_data(io.StringIO(data, newline=""), *args, **kwargs)

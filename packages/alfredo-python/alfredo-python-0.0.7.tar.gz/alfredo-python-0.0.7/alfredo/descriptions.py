287be87bf9ec74d292725674e2bf2aa4e930a4a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:54 +0000

    Add package alfredo-python/0.0.7/alfredo-python-0.0.7.tar.gz

---
+++ packages/alfredo-python/alfredo-python-0.0.7.tar.gz/alfredo-python-0.0.7/alfredo/descriptions.py
@@ -0,0 +1,56 @@
+"""
+  Copyright (c) 2017-2018 Atrio, Inc.
+
+  All rights reserved.
+"""
+
+ruote = {
+    'users': {
+        'me': {},
+        ':id': {}
+    },
+    'sso': {
+        'token_by_email': {},
+        'providers': {},
+    },
+    'clusters': {
+        ':id': {}
+    },
+    'AWSclusters': {
+        ':id': {}
+    },
+    'queues': {
+        ':id': {}
+    },
+    'files': {
+        '__attrs_files': ['file'],
+        ':id': {}
+    },
+    'jobs': {
+        ':id': {
+            'stdout': {},
+            'stderr': {},
+            'log': {},
+            'perf': {},
+            'telemetry': {},
+        }
+    },
+    'jobs-by-uuid': {
+        ':uuid': {
+            'query': {},
+        }
+    },
+    'apps': {
+        ':id': {
+            'ranking': {},
+        }
+    }
+}
+virgo = {
+    'build': {
+        '__attrs_files': ['file'],
+        ':id': {
+            'log': {},
+        },
+    },
+}

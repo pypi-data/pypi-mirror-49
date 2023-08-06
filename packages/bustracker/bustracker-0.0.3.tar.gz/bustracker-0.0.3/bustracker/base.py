847bd6fa9103753b5bf4cfee2a1bf816f7867fae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:03 +0000

    Add package bustracker/0.0.3/bustracker-0.0.3.tar.gz

---
+++ packages/bustracker/bustracker-0.0.3.tar.gz/bustracker-0.0.3/bustracker/base.py
@@ -0,0 +1,27 @@
+from requests import get
+
+class Base(object):
+    def __init__(self, agency):        
+        self.agency = agency
+
+    def _prepare_request(self, stops):
+        """
+        Basic NextBus Request URL
+        """
+
+        stopsParams = ''
+        for stop in stops:            
+            stopsParams += '&stops=' + str(stop['routeTag']) + '|' + str(stop['stopTag'])
+        
+        url = 'http://webservices.nextbus.com/service/publicJSONFeed?command=predictionsForMultiStops&a={agency}&{stops}'.format(
+            agency = self.agency,
+            stops = stopsParams
+        )        
+
+        return url
+
+    def _request(self, stops):
+        url = self._prepare_request(stops)
+        data = get(url)
+
+        return data.json()

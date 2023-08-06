4ca64165918cf8310536704c2fa15ea11136f26a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:18 +0000

    Add package famn-opt/0.3.3/famn_opt-0.3.3.tar.gz

---
+++ packages/famn-opt/famn_opt-0.3.3.tar.gz/famn_opt-0.3.3/setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Setup and register package in PyPI
+"""
+
+from setuptools import setup
+
+# For testing:
+#
+# python setup.py sdist bdist_wheel
+# python -m pip install --user --upgrade twine
+# python -m twine upload --repository-url https://test.pypi.org/legacy/ dist/* -u hcolaux -p Ls2rpAn!
+# twine upload dist/* -u hcolaux -p Ls2rpAn!
+
+
+
+setup(name='famn_opt',
+      version='0.3.3',
+      description='Python code to optimise FAM-N pulses for MQMAS experiments',
+      long_description = open('Readme.rst').read(),
+      long_description_content_type='text/markdown',
+      url='https://github.com/hcolaux/famn_opt',
+      author='hcolaux',
+      author_email='henri.colaux@normalesup.org',
+      packages=['famn_opt'],
+      zip_safe=False,
+      license='GNU GPL',
+      classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",],
+      include_package_data=True,
+      install_requires=[ #'sys',
+                        #'os',
+                        #'subprocess',
+                        #'pickle',
+                        'numpy',
+                        'lxml',
+                      'matplotlib',
+                      'PyQt5',
+                      ],
+         extras_requires = ['pickle']
+      )
+

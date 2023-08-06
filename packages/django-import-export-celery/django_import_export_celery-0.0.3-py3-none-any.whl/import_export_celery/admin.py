e87555b1db06dacabccedfb042435777c90a0732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:15 +0000

    Add package django-import-export-celery/0.0.3/django_import_export_celery-0.0.3-py3-none-any.whl

---
+++ packages/django-import-export-celery/django_import_export_celery-0.0.3-py3-none-any.whl/import_export_celery/admin.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2019 o.s. Auto*Mat
+from django.contrib import admin
+
+from . import models, tasks
+
+@admin.register(models.ImportJob)
+class ImportJobAdmin(admin.ModelAdmin):
+    list_display = (
+        'model',
+        'file',
+        'change_summary',
+        'imported',
+        'author',
+        'updated_by',
+    )
+    readonly_fields = (
+        'change_summary',
+        'imported',
+        'errors',
+        'author',
+        'updated_by',
+    )
+
+    actions = (
+        tasks.run_import_job_action,
+    )
---
+++ packages/django-import-export-celery/django_import_export_celery-0.0.3-py3-none-any.whl/import_export_celery/tasks.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+# Author: Timothy Hobbs <timothy <at> hobbs.cz>
+from datetime import datetime
+import os
+
+from celery import task
+
+from django.conf import settings
+from django.core.files.base import ContentFile
+
+from django.utils.encoding import force_text
+from django.utils.translation import ugettext as _
+
+from import_export.formats.base_formats import DEFAULT_FORMATS
+
+from . import models
+from .model_config import ModelConfig
+
+from celery.utils.log import get_task_logger
+import logging
+
+logger = logging.getLogger(__name__)
+
+log = get_task_logger(__name__)
+
+
+importables = getattr(settings, 'IMPORT_EXPORT_CELERY_MODELS', {})
+
+
+@task(bind=False)
+def run_import_job(pk, dry_run=True):
+    log.info("Importing %s dry-run %s" % (pk, dry_run))
+    while True:
+        try:
+            import_job = models.ImportJob.objects.get(pk=pk)
+            break
+        except models.ImportJob.DoesNotExist:
+            pass
+    if dry_run:
+        import_job.errors = ""
+    model_config = ModelConfig(**importables[import_job.model])
+
+    for format in DEFAULT_FORMATS:
+        if import_job.format == format.CONTENT_TYPE:
+            input_format = format()
+            break
+    try:  # Copied from https://github.com/django-import-export/django-import-export/blob/3c082f98afe7996e79f936418fced3094f141c26/import_export/admin.py#L260 sorry
+        data = import_job.file.read()
+        if not input_format.is_binary():
+            data = force_text(data, 'utf8')
+        dataset = input_format.create_dataset(data)
+    except UnicodeDecodeError as e:
+        import_job.errors += _("Imported file has a wrong encoding: %s" % e) + "\n"
+        import_job.save()
+        return
+    except Exception as e:
+        import_job.errors += _("Error reading file: %s") % e + "\n"
+        import_job.save()
+        return
+    resource = model_config.resource()
+    result = resource.import_data(dataset, dry_run=dry_run)
+    for error in result.base_errors:
+        import_job.errors += "\n%s\n%s\n" % (error.error, error.traceback)
+    for line, errors in result.row_errors():
+        for error in errors:
+            import_job.errors += _("Line: %s - %s\n\t%s\n%s") % (line, error.error, ",".join(error.row.values()), error.traceback)
+
+    if dry_run:
+        summary = '<table  border="1">' # TODO refactor the existing template so we can use it for this
+        # https://github.com/django-import-export/django-import-export/blob/6575c3e1d89725701e918696fbc531aeb192a6f7/import_export/templates/admin/import_export/import.html
+        summary += '<tr><td>change_type</td><td>' + '</td><td>'.join([f.column_name for f in resource.get_user_visible_fields()]) + '</td></tr>'
+        cols = lambda row: '</td><td>'.join([field for field in row.diff])
+        summary += '<tr><td>' + '</td></tr><tr><td>'.join([row.import_type + '</td><td>' + cols(row) for row in result.valid_rows()]) + '</tr>'
+        summary += '</table>'
+        import_job.change_summary.save(os.path.split(import_job.file.name)[1]+".html", ContentFile(summary))
+    else:
+        import_job.imported =  datetime.now()
+    import_job.save()
+
+def run_import_job_action(modeladmin, request, queryset):
+    for instance in queryset:
+        logger.info("Importing %s dry-run: False" % (instance.pk))
+        run_import_job.delay(instance.pk, dry_run=False)
+
+run_import_job_action.short_description = _("Perform import")

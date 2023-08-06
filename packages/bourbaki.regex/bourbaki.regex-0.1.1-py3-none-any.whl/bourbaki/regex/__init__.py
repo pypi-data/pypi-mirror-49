ccbda5b846ee5ee00ad69278883eda140e3b7f2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:25 +0000

    Add package bourbaki.regex/0.1.1/bourbaki.regex-0.1.1-py3-none-any.whl

---
+++ packages/bourbaki.regex/bourbaki.regex-0.1.1-py3-none-any.whl/bourbaki/regex/__init__.py
@@ -0,0 +1,7 @@
+"""Awesomely readable regex construction in pure Python"""
+
+from .base import CharRange, CharSet, CharClass, C, L, Literal, If, Alternation
+from .base import START, END, ANYCHAR, StartString, EndString, Tab, Endline, BackSpace, CarriageReturn
+from .base import WordBoundary, WordInternal, WordChar, NonWordChar, Digit, NonDigit, Whitespace, NonWhitespace
+
+__version__ = "0.1.1"

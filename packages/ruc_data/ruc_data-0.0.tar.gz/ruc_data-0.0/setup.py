7e6a049cf16b2bf8680c3237e1d172650269f190
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:40 +0000

    Add package ruc_data/0.0/ruc_data-0.0.tar.gz

---
+++ packages/ruc_data/ruc_data-0.0.tar.gz/ruc_data-0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(name='ruc_data',
+      version='0.0',
+      description='test',
+      url='http://github.com/slowsnowball',
+      author='jiaohaibin',
+      author_email='jiaohaibin@ruc.edu.cn',
+      license='MIT',
+      packages=['ruc_data'],
+      install_requires=["pandas","requests"],
+      zip_safe=False)

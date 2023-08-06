5edc11067e60f34af34c4757794dec41593d0d2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:03 +0000

    Add package autopandas/0.2.2/autopandas-0.2.2-py3-none-any.whl

---
+++ packages/autopandas/autopandas-0.2.2-py3-none-any.whl/autopandas/generators/generators.py
@@ -0,0 +1,14 @@
+# Generators module
+
+# Import all generators
+from .artificial import Artificial
+from .copycat import Copycat
+from .vae import VAE
+from .anm import ANM
+
+# TODO
+# GAN, WGAN, medGAN
+# Copula
+# Gaussian multivariate
+# Parzen Windows
+# SAM

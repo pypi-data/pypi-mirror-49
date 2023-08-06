555c06315d82b6951d7634a67771ab3b79ace845
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:25 +0000

    Add package context-temp/0.11.0/context_temp-0.11.0.tar.gz

---
+++ packages/context-temp/context_temp-0.11.0.tar.gz/context_temp-0.11.0/README.rst
@@ -0,0 +1,25 @@
+context_temp
+************
+
+Context Manager wrappers for temp files and directories which remove the file
+or directory when when the manager goes out of scope.
+
+
+Installation
+============
+
+.. code-block:: bash
+
+    $ pip install context_temp
+
+Supports
+========
+
+context_temp has been tested with Python 2.7, 3.5, 3.6, 3.7
+
+Docs & Source
+=============
+
+Docs: http://context_temp.readthedocs.io/en/latest/
+
+Source: https://github.com/cltrudeau/context_temp
---
+++ packages/context-temp/context_temp-0.11.0.tar.gz/context_temp-0.11.0/context_temp.py
@@ -0,0 +1,58 @@
+__version__ = '0.11.0'
+
+import contextlib, os, shutil, tempfile
+
+# =============================================================================
+# Context Managers
+# =============================================================================
+
+@contextlib.contextmanager
+def temp_directory(path=None):
+    """This ``Context Manager`` creates a temporary directory and yields its
+    path.  Upon exit the directory is removed.
+
+    :param path: optional path for the creation of the directory, otherwise
+        defaults to operating system's default temp file location
+
+    Example:
+
+    .. code-block:: python
+
+        with temp_directory() as td:
+            filename = os.path.join(td, 'foo.txt')
+            with open(filename, 'w') as f:
+                f.write('stuff')
+
+        # got here? => td is now gone, foo.txt is gone too
+    """
+    tempdir = tempfile.mkdtemp(dir=path)
+    try:
+        yield tempdir
+    finally:
+        shutil.rmtree(tempdir)
+
+
+@contextlib.contextmanager
+def temp_file(path=None):
+    """This ``Context Manager`` creates a temporary file which is readable and
+    writable by the current user and yields its path.  Once the ``Context``
+    exits, the file is removed.
+
+    :param path: optional path for the creation of the file, otherwise
+        defaults to operating system's default temp file location
+
+    Example:
+
+    .. code-block:: python
+
+        with temp_file() as filename:
+            with open(filename, 'w') as f:
+                f.write('stuff')
+                
+        # got here? => file called "filename" is now gone
+    """
+    _, filename = tempfile.mkstemp(dir=path)
+    try:
+        yield filename
+    finally:
+        os.remove(filename)

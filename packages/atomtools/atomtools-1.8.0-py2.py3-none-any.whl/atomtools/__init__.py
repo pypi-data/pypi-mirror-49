ea52520710395ea15991ea2217a44cb2e368a9b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:24 +0000

    Add package atomtools/1.8.0/atomtools-1.8.0-py2.py3-none-any.whl

---
+++ packages/atomtools/atomtools-1.8.0-py2.py3-none-any.whl/atomtools/__init__.py
@@ -0,0 +1,16 @@
+"""
+independent chemical symbols
+"""
+
+
+__version__ = '1.8.0'
+def version():
+    return __version__
+
+
+
+# from . import name, unit, geo
+# from . import file, string, system
+# from . import types
+# from .filetype import filetype, list_supported_formats, support_multiframe
+# from .status import Status

7810fbd4861b2a199befee29df0460d98c987f4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:10 +0000

    Add package generoo/2019.7.23.1/generoo-2019.7.23.1.tar.gz

---
+++ packages/generoo/generoo-2019.7.23.1.tar.gz/generoo-2019.7.23.1/generoo/archetypes/java/spring-boot/2.0/{{artifact_id}}-api/src/main/java/{{group_id_slashes}}/{{artifact_id_slashes}}/service/{{artifact_id_capitalized}}Service.java
@@ -0,0 +1,18 @@
+package {{group_id}}.{{artifact_id_periods}}.service;
+
+import {{group_id}}.{{artifact_id_periods}}.repository.{{artifact_id_capitalized}}Repository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class {{artifact_id_capitalized}}Service {
+
+  {{#hibernate}}private final {{artifact_id_capitalized}}Repository {{artifact_id_camel}}Repository;
+
+  public {{artifact_id_capitalized}}Service({{artifact_id_capitalized}}Repository {{artifact_id_camel}}Repository) {
+    this.{{artifact_id_camel}}Repository = {{artifact_id_camel}}Repository;
+  }{{/hibernate}}
+  {{^hibernate}}
+  public {{artifact_id_capitalized}}Service() {
+  }
+  {{/hibernate}}
+}

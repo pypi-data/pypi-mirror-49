b35bf98f6318e36796a252607d9845487441b472
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:26 +0000

    Add package mosaik.ScenarioTools/0.1.0rc20190716091756/mosaik.ScenarioTools-0.1.0rc20190716091756-py3-none-any.whl

---
+++ packages/mosaik.ScenarioTools/mosaik.ScenarioTools-0.1.0rc20190716091756-py3-none-any.whl/mosaik_scenario_tools/scenario_tools/sink/sink_model.py
@@ -0,0 +1,23 @@
+# -*- coding: future_fstrings -*-
+
+
+class SinkModel(object):
+    def __init__(self, eid):
+        """
+        This is the simplest-thinkable source model
+
+        :param eid: The entity identifier this model shall carry
+        """
+
+        self._eid = eid
+        self._data = None
+
+    @property
+    def eid(self):
+        return self._eid
+
+    def get_data(self):
+        return self._data
+
+    def step(self):
+        self._data = None

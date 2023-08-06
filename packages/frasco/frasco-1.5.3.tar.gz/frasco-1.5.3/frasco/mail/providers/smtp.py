88216b864af3c5269d818aef3f3e7fffc6c1147c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:00 +0000

    Add package frasco/1.5.3/frasco-1.5.3.tar.gz

---
+++ packages/frasco/frasco-1.5.3.tar.gz/frasco-1.5.3/frasco/mail/providers/smtp.py
@@ -0,0 +1,17 @@
+from frasco.mail.provider import MailProvider
+from frasco.utils import inject_app_config
+
+
+class SMTPProvider(MailProvider):
+    def send(self, msg):
+        return self.state.mail.send(msg)
+
+    def start_bulk_connection(self):
+        connection = self.state.mail.connect()
+        # simulate entering a with context
+        # (flask-mail does not provide a way to connect otherwise)
+        connection.__enter__()
+        return connection
+
+    def stop_bulk_connection(self, conn):
+        conn.__exit__(None, None, None)

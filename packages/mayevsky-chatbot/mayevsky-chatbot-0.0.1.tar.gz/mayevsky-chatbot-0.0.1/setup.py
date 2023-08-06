1ab60f6a2e94d674dd31d010821c362ca5f85329
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:03 +0000

    Add package mayevsky-chatbot/0.0.1/mayevsky-chatbot-0.0.1.tar.gz

---
+++ packages/mayevsky-chatbot/mayevsky-chatbot-0.0.1.tar.gz/mayevsky-chatbot-0.0.1/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="mayevsky-chatbot",
+    version="0.0.1",
+    author="RedSnail",
+    author_email="",
+    description="The bot for TheBigMayevsky",
+    url="https://github.com/RedSnail/mayevsky-chatbot",
+    packages=setuptools.find_packages()
+)

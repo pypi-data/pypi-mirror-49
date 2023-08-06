2813ed773d300218fd3c9ddaeb7ee7b1fb944f41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:59 +0000

    Add package catalyst/19.7.2/catalyst-19.7.2-py2.py3-none-any.whl

---
+++ packages/catalyst/catalyst-19.7.2-py2.py3-none-any.whl/catalyst/rl/core/db.py
@@ -0,0 +1,45 @@
+from abc import abstractmethod, ABC
+
+
+class DBSpec(ABC):
+    @property
+    @abstractmethod
+    def epoch(self) -> int:
+        pass
+
+    @property
+    @abstractmethod
+    def num_trajectories(self) -> int:
+        pass
+
+    @abstractmethod
+    def set_sample_flag(self, sample: bool):
+        pass
+
+    @abstractmethod
+    def get_sample_flag(self) -> bool:
+        pass
+
+    @abstractmethod
+    def push_trajectory(self, trajectory, raw: bool):
+        pass
+
+    @abstractmethod
+    def get_trajectory(self, index=None):
+        pass
+
+    @abstractmethod
+    def clean_trajectories(self):
+        pass
+
+    @abstractmethod
+    def save_checkpoint(self, checkpoint, epoch):
+        pass
+
+    @abstractmethod
+    def load_checkpoint(self):
+        pass
+
+    @abstractmethod
+    def clean_checkpoint(self):
+        pass

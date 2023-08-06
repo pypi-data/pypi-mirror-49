a5c6bac44ae1464c6e53cd5af913be962873f591
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:22 +0000

    Add package Rackio/0.2.6/Rackio-0.2.6-py3-none-any.whl

---
+++ packages/Rackio/Rackio-0.2.6-py3-none-any.whl/rackio/core.py
@@ -0,0 +1,224 @@
+# -*- coding: utf-8 -*-
+"""rackio/core.py
+
+This module implements the core app class and methods for Rackio.
+"""
+
+import falcon
+import concurrent.futures
+
+from ._singleton import Singleton
+from .logger import LoggerEngine
+from .controls import ControlManager
+from .alarms import AlarmManager
+from .workers import LoggerWorker, ControlWorker, AlarmWorker, APIWorker, _ContinousWorker
+from .api import TagResource, TagCollectionResource, TagHistoryResource, TrendResource, AlarmResource, AlarmCollectionResource, EventCollectionResource
+
+
+class Rackio(Singleton):
+
+    """Rackio main application class.
+
+    This class is a singleton by inheritance, this makes
+    it available in each module of an end application or
+    code project
+
+    # Example
+    
+    ```python
+    >>> from rackio import Rackio
+    >>> app = Rackio()
+    ```
+
+    """
+
+    def __init__(self, context=None):
+
+        super(Rackio, self).__init__()
+        
+        self._context = context
+
+        self._worker_functions = list()
+        self._continous_functions = list()
+        self._custom_observer = None
+        self._control_manager = ControlManager()
+        self._alarm_manager = AlarmManager()
+        self._db_manager = None
+
+        self._api = falcon.API()
+
+        _tag = TagResource()
+        _tags = TagCollectionResource()
+        _tag_history = TagHistoryResource()
+        _tag_trend = TrendResource()
+        _alarm = AlarmResource()
+        _alarms = AlarmCollectionResource()
+        _events = EventCollectionResource()
+
+        self._api.add_route('/api/tags/{tag_id}', _tag)
+        self._api.add_route('/api/tags', _tags)
+
+        self._api.add_route('/api/tags/history/{tag_id}', _tag_history)
+        self._api.add_route('/api/tags/trends/{tag_id}', _tag_trend)
+
+        self._api.add_route('/api/alarms/{alarm_name}', _alarm)
+        self._api.add_route('/api/alarms', _alarms)
+
+        self._api.add_route('/api/events', _events)
+
+    def set_db(self, dbfile):
+        """Sets the database file.
+        
+        # Parameters
+        dbfile (str): a path to database file.
+        """
+
+        self._db_manager = LoggerEngine()
+        self._db_manager.set_db(dbfile)
+
+    def set_dbtags(self, tags, period=0.5):
+        """Sets the database tags for logging.
+        
+        # Parameters
+        tags (list): A list of the tags.
+        """
+
+        self._db_manager.set_period(period)
+        
+        for _tag in  tags:
+            self._db_manager.add_tag(_tag)
+
+    def append_rule(self, rule):
+        """Append a rule to the control manager.
+        
+        # Parameters
+        rule (Rule): a rule object.
+        """
+
+        self._control_manager.append_rule(rule)
+
+    def append_control(self, control):
+        """Append a control to the control manager.
+        
+        # Parameters
+        control (Control): a control object.
+        """
+
+        self._control_manager.append_control(control)
+
+    def append_alarm(self, alarm):
+        """Append an alarm to the alarm manager.
+        
+        # Parameters
+        alarm (Alarm): an alarm object.
+        """
+
+        self._alarm_manager.append_alarm(alarm)
+
+    def add_route(self, route, resource):
+        """Append a resource and route the api.
+        
+        # Parameters
+        route (str): The url route for this resource.
+        resource (object): a url resouce template class instance.
+        """
+
+        self._api.add_route(route, resource)
+
+    def rackit(self, period):
+        """Decorator method to register functions plugins.
+        
+        This method will register into the Rackio application
+        a new function to be executed by the Thread Pool Executor
+
+        # Example
+    
+        ```python
+        @app.rackit
+        def hello():
+            print("Hello!!!")
+        ```
+
+        # Parameters
+        period (float): Value of the default loop execution time.
+        """
+
+        def decorator(f):
+
+            _worker_function = (f, period)
+            self._worker_functions.append(_worker_function)
+            return f
+        
+        return decorator
+
+    def rackit_on(self, function=None, worker_name=None, period=0.5, pause_tag=None, stop_tag=None):
+        """Decorator method to register functions plugins with continous execution.
+        
+        This method will register into the Rackio application
+        a new function to be executed by the Thread Pool Executor
+
+        # Example
+    
+        ```python
+        @app.rackit_on(period=0.5)
+        def hello():
+            print("Hello!!!")
+
+        @app.rackit_on
+        def hello_world():
+            print("Hello World!!!")
+        ```
+
+        # Parameters
+        period (float): Value of the default loop execution time, if period is not defined 0.5 seconds is used.
+        """
+    
+        if function:
+            return _ContinousWorker(function)
+        else:
+            def wrapper(function):
+                return _ContinousWorker(function, worker_name, period, pause_tag, stop_tag)
+
+            return wrapper
+
+    def run(self, port=8000):
+
+        """Runs the main execution for the application to start serving.
+        
+        This will put all the components of the application at run
+
+        # Example
+    
+        ```python
+        >>> app.run()
+        ```
+        """
+
+        _control_worker = ControlWorker(self._control_manager)
+        _alarm_worker = AlarmWorker(self._alarm_manager)
+        _api_worker = APIWorker(self._api, port)
+
+        if self._db_manager:
+            self._db_manager._logger.set_db()
+            self._db_manager._logger.set_events()
+            _db_worker = LoggerWorker(self._db_manager)
+            _db_worker.start()
+        
+        _control_worker.start()
+        _alarm_worker.start()
+        _api_worker.start()
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
+            
+            for _f, period in self._worker_functions:
+
+                executor.submit(_f)
+
+            for _f in self._continous_functions:
+
+                executor.submit(_f)
+
+        _control_worker.join()
+        _alarm_worker.join()
+        _api_worker.join()
+            
---
+++ packages/Rackio/Rackio-0.2.6-py3-none-any.whl/rackio/logger.py
@@ -0,0 +1,395 @@
+# -*- coding: utf-8 -*-
+"""rackio/logger.py
+
+This module implements a sqlite database logger for the CVT instance, 
+will create a time-serie for each tag in a short memory sqlite data base.
+"""
+
+import threading
+import os
+import pickledb
+
+from datetime import datetime
+
+from .utils import process_waveform
+from ._singleton import Singleton
+
+EVENTS = "events"
+
+
+class TagLogger:
+
+    """CVT Tag Logger class.
+
+    This class is intended to be an API for tags 
+    settings and tags logged access.
+
+    # Example
+    
+    ```python
+    >>> from rackio.logger import TagLogger
+    >>> _logger = TagLogger()
+    ```
+    
+    """
+    
+    def __init__(self, dbfile="tags.db", memory=None):
+
+        self._dbfile = dbfile
+        self._memory = memory
+
+        self._period = None
+
+        if os.path.exists(self._dbfile):
+            os.remove(self._dbfile)
+        else:   
+            print("Can not delete the file as it doesn't exists")
+
+    def set_dbfile(self, dbfile):
+
+        self._dbfile = dbfile
+
+    def set_db(self):
+
+        self._db = pickledb.load(self._dbfile, False) 
+
+    def set_tag(self, tag):
+
+        # self._db.set(tag, list())
+        
+        waveform = dict()
+        waveform["values"] = list()
+        waveform["dt"] = self._period
+        waveform["t0"] = None
+
+        # self._db.set(tag, list())
+        self._db.set(tag, waveform)
+        self._db.dump()
+
+    def write_tag(self, tag, value):
+
+        # values = self._db.get(tag)
+        # values.append(value)
+        # self._db.set(tag, values)
+        # self._db.dump()
+
+        waveform = self._db.get(tag)
+
+        values = waveform["values"]
+
+        if not values:
+            
+            dt = self._period
+            t0 = datetime.now()
+            
+            waveform["dt"] = dt
+            waveform["t0"] = t0.strftime('%Y-%m-%d %H:%M:%S')
+
+        values.append(value)
+
+        waveform["values"] = values
+
+        self._db.set(tag, waveform)
+        self._db.dump()
+
+    def read_tag(self, tag):
+        
+        result = self._db.get(tag)
+        
+        return result
+
+    def set_events(self): 
+        
+        events = list()
+        self._db.set(EVENTS, events)
+        self._db.dump()
+
+    def add_event(self, event):
+
+        events = self._db.get(EVENTS)
+
+        try:
+            event = event._serialize()
+        except:
+            pass
+
+        events.append(event)
+
+        self._db.set(EVENTS, events)
+        self._db.dump()
+    
+    def get_events(self):
+
+        events = self._db.get(EVENTS)
+        events = events[:]
+
+        return events
+
+class LoggerEngine(Singleton):
+    """Logger Engine class for Tag thread-safe database logging.
+
+    This class is intended hold in memory tag based values and 
+    observers for those required tags, it is implemented as a singleton
+    so each sub-thread within the Rackio application can access tags
+    in a thread-safe mechanism.
+
+    # Example
+    
+    ```python
+    >>> from rackio.engine import CVTEngine
+    >>> tag_egine = CVTEngine()
+    >>> tag_engine.write_tag("TAG1", 40.43)
+    >>> value = tag_engine.read_tag("TAG1")
+    >>> print(value)
+    40.43
+    ```
+
+    """
+
+    def __init__(self, period=0.5):
+
+        super(LoggerEngine, self).__init__()
+
+        self._logger = TagLogger()
+        self._logging_tags = list()
+
+        self._logger._period = period
+
+        self._request_lock = threading.Lock()
+        self._response_lock = threading.Lock()
+
+        self._response = None
+
+        self._response_lock.acquire()
+
+    def set_db(self, dbfile):
+
+        self._logger.set_dbfile(dbfile)
+
+    def add_tag(self, tag):
+
+        self._logging_tags.append(tag)
+
+    def set_period(self, period):
+
+        self._logger._period = period
+
+    def get_period(self):
+
+        return self._logger._period
+
+    def write_tag(self, tag, value):
+
+        _query = dict()
+        _query["action"] = "write_tag"
+
+        _query["parameters"] = dict()
+        _query["parameters"]["tag"] = tag
+        _query["parameters"]["value"] = value
+
+        self.request(_query)
+        result = self.response()
+
+        return result
+
+    def read_tag(self, tag):
+
+        _query = dict()
+        _query["action"] = "read_tag"
+
+        _query["parameters"] = dict()
+        _query["parameters"]["tag"] = tag
+
+        self.request(_query)
+        result = self.response()
+
+        if result["result"]:
+            return result["response"]
+
+    def write_event(self, event):
+
+        _query = dict()
+        _query["action"] = "write_event"
+
+        _query["parameters"] = dict()
+        _query["parameters"]["event"] = event
+
+        self.request(_query)
+        result = self.response()
+
+        return result
+
+    def read_events(self):
+
+        _query = dict()
+        _query["action"] = "read_events"
+
+        self.request(_query)
+        result = self.response()
+
+        if result["result"]:
+            return result["response"]
+
+    def request(self, _query):
+
+        self._request_lock.acquire()
+
+        action = _query["action"]
+
+        if action == "write_tag":
+
+            try:
+                parameters = _query["parameters"]
+
+                tag = parameters["tag"]
+                value = parameters["value"]
+
+                self._logger.write_tag(tag, value)
+
+                self._response = {
+                    "result": True
+                }
+            except:
+                self._response = {
+                    "result": False
+                }
+        
+        elif action == "read_tag":
+
+            try:
+
+                parameters = _query["parameters"]
+
+                tag = parameters["tag"]
+
+                result = self._logger.read_tag(tag)
+                
+                self._response = {
+                    "result": True,
+                    "response": result
+                }
+            except:
+                self._response = {
+                    "result": False,
+                    "response": None
+                }
+
+        elif action == "write_event":
+
+            #try:
+                
+            parameters = _query["parameters"]
+            event = parameters["event"]
+
+            self._logger.add_event(event)
+
+            self._response = {
+                "result": True
+            }
+            #except:
+            #    self._response = {
+            #        "result": False
+            #    }
+
+        elif action == "read_events":
+
+            try:
+
+                result = self._logger.get_events()
+
+                self._response = {
+                    "result": True,
+                    "response": result
+                }
+            except:
+                self._response = {
+                    "result": False
+                }
+
+        self._response_lock.release()
+
+    def response(self):
+
+        self._response_lock.acquire()
+
+        result = self._response
+
+        self._request_lock.release()
+
+        return result
+
+
+class QueryLogger:
+
+    def __init__(self):
+
+        self._logger = LoggerEngine()
+
+    def get_history(self, tag):
+
+        return self._logger.read_tag(tag)
+
+    def get_waveform(self, tag):
+        
+        history = self._logger.read_tag(tag)
+        
+        result = dict()
+        result["dt"] = history["dt"]
+        result["t0"] = history["t0"]
+        result["values"] = history["values"][:]
+
+        return result
+
+    def query(self, tag, start, stop):
+
+        waveform = self.get_waveform(tag)
+
+        return process_waveform(waveform, start, stop)
+    
+    def query_last(self, tag, seconds=None, values=None):
+
+        if seconds:
+
+            stop = datetime.now()
+            start = stop - seconds
+
+            start = start.strftime('%Y-%m-%d %H:%M:%S')
+            stop = stop.strftime('%Y-%m-%d %H:%M:%S')
+
+            return self.query(tag, start, stop)
+
+        if values:
+            
+            values *= -1
+            waveform = self.get_waveform(tag)
+            
+            try:
+                waveform["values"] = waveform["values"][values:]
+            except:
+                waveform["values"] = waveform["values"][:]
+
+            return waveform
+
+    def query_first(self, tag, seconds=None, values=None):
+
+        history = self.get_history(tag)
+
+        if seconds:
+
+            start = history["t0"]
+            stop = start + seconds
+
+            start = start.strftime('%Y-%m-%d %H:%M:%S')
+            stop = stop.strftime('%Y-%m-%d %H:%M:%S')
+
+            return self.query(tag, start, stop)
+
+        if values:
+
+            waveform = self.get_waveform(tag)
+            try:
+                waveform["values"] = waveform["values"][:values]
+            except:
+                waveform["values"] = waveform["values"][:]
+
+            return waveform

1e3254cb63178174813bddcc1a2f363e69d2af57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:46 +0000

    Add package SeansPipTest2/0.0.1/SeansPipTest2-0.0.1.tar.gz

---
+++ packages/SeansPipTest2/SeansPipTest2-0.0.1.tar.gz/SeansPipTest2-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="SeansPipTest2",
+    version="0.0.1",
+    author="Sean",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SeanSRice/SeansPipTest2.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

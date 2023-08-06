a2a833607afd85fdc9b83dfa190c66fa62c31020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:58 +0000

    Add package cardbin/1.0.0/cardbin-1.0.0-py2.py3-none-any.whl

---
+++ packages/cardbin/cardbin-1.0.0-py2.py3-none-any.whl/cardbin/cardbin.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import json
+import os
+import pickle
+
+from stdnum import luhn
+from .bankbin import BANKBIN
+
+
+def valid(card):
+
+    if not luhn.is_valid(card):
+        return False
+
+    key = BANKBIN.get(card[:7]) if BANKBIN.get(
+        card[:7]) else BANKBIN.get(card[:6], None)
+
+    if key:
+        key = key.split('|')
+        return {'bank': key[0], 'type': key[1]}
+
+    return {'bank': '未知银行', 'type': None}

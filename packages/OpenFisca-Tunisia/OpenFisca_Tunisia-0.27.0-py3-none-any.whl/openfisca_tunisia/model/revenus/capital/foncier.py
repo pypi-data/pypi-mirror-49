a0ee56a5eb1641c19907f4b57bdc6605107f499a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:04 +0000

    Add package OpenFisca-Tunisia/0.27.0/OpenFisca_Tunisia-0.27.0-py3-none-any.whl

---
+++ packages/OpenFisca-Tunisia/OpenFisca_Tunisia-0.27.0-py3-none-any.whl/openfisca_tunisia/model/revenus/capital/foncier.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+from openfisca_tunisia.model.base import *
+
+
+# Revenus fonciers
+
+
+# Régime réel
+
+class foncier_reel_resultat_fiscal(Variable):
+    value_type = int
+    entity = Individu
+    label = u"Résultat fiscal (revenus fonciers, régime réel)"
+    definition_period = YEAR
+
+
+# Régime forfaitaire bâti
+
+class foncier_forfaitaire_batis_recettes(Variable):
+    value_type = int
+    entity = Individu
+    label = u"Recettes brutes réalisées TTC (revenus fonciers, base forfaitaire, immeubles bâtis)"
+    definition_period = YEAR
+
+
+class foncier_forfaitaire_batis_reliquat(Variable):
+    value_type = int
+    entity = Individu
+    label = u"Reliquat (revenus fonciers, base forfaitaire, immeubles bâtis)"
+    definition_period = YEAR
+
+
+class foncier_forfaitaire_batis_frais(Variable):
+    value_type = int
+    entity = Individu
+    label = u"Frais d’entretien et de réparation justifiés à déduire " \
+        u"(revenus fonciers, base forfaitaire, immeubles bâtis)"
+    definition_period = YEAR
+
+
+class foncier_forfaitaire_batis_taxe(Variable):
+    value_type = int
+    entity = Individu
+    label = u"Taxe effectivement payée à déduire (revenus fonciers, base forfaitaire, immeubles bâtis)"
+    definition_period = YEAR
+
+
+# Régime forfaitaire non bâti
+
+class foncier_forfaitaire_non_batis_recettes(Variable):
+    value_type = int
+    entity = Individu
+    label = u"Recettes brutes réalisées TTC (revenus fonciers, base forfaitaire, terrains non bâtis)"
+    definition_period = YEAR
+
+
+class foncier_forfaitaire_non_batis_depenses(Variable):
+    value_type = int
+    entity = Individu
+    label = u"Dépenses justifiées à déduire (revenus fonciers, base forfaitaire, terrains non bâtis)"
+    definition_period = YEAR
+
+
+class foncier_forfaitaire_non_batis_taxe(Variable):
+    value_type = int
+    entity = Individu
+    label = u"Taxe effectivement payée à déduire (revenus fonciers, base forfaitaire, terrains non bâtis)"
+    definition_period = YEAR
+
+
+class foncier_societes_personnes(Variable):
+    value_type = int
+    entity = Individu
+    label = u"Part dans le bénéfice ou dans la perte des sociétés de personnes et assimilées qui réalisent des revenus fonciers"
+    definition_period = YEAR

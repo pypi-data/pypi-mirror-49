6b0941b9da0265a7d49b408106e6834af05e8276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:29 +0000

    Add package sgvalidator/0.0.10/sgvalidator-0.0.10.tar.gz

---
+++ packages/sgvalidator/sgvalidator-0.0.10.tar.gz/sgvalidator-0.0.10/sgvalidator/fill_rate_checker.py
@@ -0,0 +1,30 @@
+import termcolor
+from .abstract_checker import AbstractChecker
+
+
+class FillRateChecker(AbstractChecker):
+    """
+    Checks the percentage of each columns which is null or blank and warns if it's greater than some cutoff.
+    To add a new fill rate check, just define a boolean mask and pass to `checkFillRateInner`
+    """
+    PERC_CUTOFF = 40
+
+    def __init__(self, data):
+        self.data = data
+
+    def check(self):
+        percNullDf = self.data.isna().mean() * 100
+        percBlankDf = self.data.apply(lambda x: x == "", axis=0).mean() * 100
+        FillRateChecker.checkInner(percNullDf, "null")
+        FillRateChecker.checkInner(percBlankDf, "blanks")
+
+    @staticmethod
+    def checkInner(mask, desc):
+        concerningColsWithPercentages = mask[mask > FillRateChecker.PERC_CUTOFF]
+        if len(concerningColsWithPercentages) > 0:
+            message = "WARNING: {} columns have a high percentage (> {}%) of {}. " \
+                      "Are you sure you scraped correctly?\n{}\n"\
+                .format(len(concerningColsWithPercentages), FillRateChecker.PERC_CUTOFF, desc,
+                        concerningColsWithPercentages)
+            print(termcolor.colored(message, "yellow"))
+        return concerningColsWithPercentages
---
+++ packages/sgvalidator/sgvalidator-0.0.10.tar.gz/sgvalidator-0.0.10/sgvalidator/trash_value_checker.py
@@ -0,0 +1,41 @@
+import termcolor
+from .validator_utils import ValidatorUtils
+from .abstract_checker import AbstractChecker
+
+class TrashValueChecker(AbstractChecker):
+    """
+    Checks for trash values in the data, like "null" or HTML tags, both when they're
+    standalone and also when they're mixed with other data, e.g. "Bob's Burgers null"
+
+    Filters to values that have some or all of BAD_TOKENS_INCLUDE but which do NOT include
+    anything from BAD_TOKENS_EXCLUDE
+    """
+    BAD_TOKENS_INCLUDE = ["null", "<", ">"]
+    BAD_TOKENS_EXCLUDE = ["<MISSING>", "<INACCESSIBLE>"]
+
+    def __init__(self, data, debug):
+        self.data = data
+        self.debug = debug
+
+    def check(self):
+        res = TrashValueChecker.findTrashValues(self.data)
+        debugExamples = res.head(10)
+        if len(res) > 0:
+            ValidatorUtils.fail("Found {} rows with trash data (e.g. nulls, HTML tags, etc.). Examples:\n{}\n"
+                                .format(len(res), debugExamples), self.debug)
+        else:
+            print(termcolor.colored("No trash values found...", "green"))
+
+    @staticmethod
+    def findTrashValues(data):
+        mask = data.apply(TrashValueChecker.findTrashValuesInner, axis=1)
+        return data[mask]
+
+    @staticmethod
+    def findTrashValuesInner(row):
+        for key, value, in row.items():
+            badTokensIncludeFilter = set(filter(lambda x: x in value, TrashValueChecker.BAD_TOKENS_INCLUDE))
+            badTokensExcludeFilter = set(filter(lambda x: x in value, TrashValueChecker.BAD_TOKENS_EXCLUDE))
+            if len(badTokensIncludeFilter) > 0 and len(badTokensExcludeFilter) == 0:
+                return True
+        return False

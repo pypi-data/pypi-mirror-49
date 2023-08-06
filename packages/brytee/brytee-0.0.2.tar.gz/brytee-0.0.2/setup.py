44b897a9462cb0965f3de0c74a80a7ff400c8053
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:00 +0000

    Add package brytee/0.0.2/brytee-0.0.2.tar.gz

---
+++ packages/brytee/brytee-0.0.2.tar.gz/brytee-0.0.2/setup.py
@@ -0,0 +1,12 @@
+#!/user/bin/env python3 
+
+from setuptools import setup
+
+setup(name='brytee',
+      version='0.0.2',
+      description="Python library of tools for enhancing your development, along with common shortcuts for speeding up your development",
+      author='Mike Shobes',
+      author_email="mikeshobes718@yahoo.com",
+      url="https://github.com/mikeshobes718/Python-Packages/tree/master/bryte",
+      packages=[],
+     )

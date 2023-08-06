00a88bd23155f4f005588bdc175f5dbb4213b8c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:58 +0000

    Add package pupy/2.19.1/pupy-2.19.1.tar.gz

---
+++ packages/pupy/pupy-2.19.1.tar.gz/pupy-2.19.1/pupy/utils.pyi
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# Pretty ~ Useful ~ Python
+
+from typing import Any
+from typing import Optional
+
+def timestamp(ts: Optional[float] = ...) -> str: ...
+def environ_dict(): ...
+
+class LinkedTmpDir:
+    dirpath: Any = ...
+    dirname: Any = ...
+    file_links: Any = ...
+    dir_links: Any = ...
+    def __init__(
+        self,
+        suffix: Optional[Any] = ...,
+        prefix: Optional[Any] = ...,
+        dir: Optional[Any] = ...,
+        mkdirs: Optional[Any] = ...,
+        lndirs: Optional[Any] = ...,
+        lnfiles: Optional[Any] = ...,
+        link_targets: Optional[Any] = ...,
+    ) -> None: ...
+    def __enter__(self): ...
+    def __exit__(self, exc: Any, value: Any, tb: Any) -> None: ...
+    def cleanup(self) -> None: ...
+
+def linked_tmp_dir(
+    suffix: Optional[Any] = ...,
+    prefix: Optional[Any] = ...,
+    dir: Optional[Any] = ...,
+    mkdirs: Any = ...,
+    lndirs: Any = ...,
+    lnfiles: Any = ...,
+) -> None: ...

53f5643e52bab81d4be9d2582eaa728c81e60750
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:43 +0000

    Add package iambic/0.1.1/iambic-0.1.1.tar.gz

---
+++ packages/iambic/iambic-0.1.1.tar.gz/iambic-0.1.1/iambic/__about__.py
@@ -0,0 +1,11 @@
+__title__ = "iambic"
+__package__ = "iambic"
+__description__ = (
+    "iambic: Data extraction and rendering library for Shakespearean text."
+)
+__url__ = "https://github.com/seandstewart/iambic"
+__version__ = "0.1.1"
+__author__ = "Sean Stewart"
+__author_email__ = "sean_stewart@me.com"
+__license__ = "MIT"
+__copyright__ = "Copyright 2019 Sean Stewart"

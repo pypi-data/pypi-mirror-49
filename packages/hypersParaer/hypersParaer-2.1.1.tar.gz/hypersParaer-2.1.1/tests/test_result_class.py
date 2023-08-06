688044934ebb4068d0231d2f52b8c9fc18c7ba64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:04 +0000

    Add package hypersParaer/2.1.1/hypersParaer-2.1.1.tar.gz

---
+++ packages/hypersParaer/hypersParaer-2.1.1.tar.gz/hypersParaer-2.1.1/tests/test_result_class.py
@@ -0,0 +1,22 @@
+from paraer.vendor import HTTPResult, HTTPPaginator
+from rest_framework.test import APIClient, APITestCase, APIRequestFactory
+
+
+class ResultClassTestCase(APITestCase):
+    def test_dict(self):
+        data = {"a": "b"}
+        request = APIRequestFactory().get('/api')
+        paginator = HTTPPaginator()
+        paginator.request = request
+
+        response = HTTPResult(paginator=paginator).response(data)
+        self.assertEqual(response.data, data)
+
+    def test_list(self):
+        data = [{"a": "b"}]
+        request = APIRequestFactory().get('/api')
+        paginator = HTTPPaginator()
+        paginator.request = request
+
+        response = HTTPResult(paginator=paginator).response(data)
+        self.assertEqual(response.data['records'], data)

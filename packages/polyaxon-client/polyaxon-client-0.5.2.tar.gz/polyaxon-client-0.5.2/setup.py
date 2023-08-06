0bd3c20527f8d335822cad8b9cafb23d4f806625
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:36 +0000

    Add package polyaxon-client/0.5.2/polyaxon-client-0.5.2.tar.gz

---
+++ packages/polyaxon-client/polyaxon-client-0.5.2.tar.gz/polyaxon-client-0.5.2/setup.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+import sys
+
+from setuptools import find_packages, setup
+from setuptools.command.test import test as TestCommand
+
+
+def read_readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
+
+
+setup(name='polyaxon-client',
+      version='0.5.2',
+      description='Python client to interact with Polyaxon API.',
+      long_description=read_readme(),
+      long_description_content_type="text/markdown",
+      maintainer='Mourad Mourafiq',
+      maintainer_email='mourad@polyaxon.com',
+      author='Mourad Mourafiq',
+      author_email='mourad@polyaxon.com',
+      url='https://github.com/polyaxon/polyaxon-client',
+      license='MIT',
+      platforms='any',
+      packages=find_packages(),
+      keywords=[
+          'polyaxon',
+          'aws',
+          's3',
+          'microsoft',
+          'azure',
+          'google cloud storage',
+          'gcs',
+          'tensorFlow',
+          'deep-learning',
+          'machine-learning',
+          'data-science',
+          'neural-networks',
+          'artificial-intelligence',
+          'ai',
+          'reinforcement-learning',
+          'kubernetes',
+      ],
+      install_requires=[
+          "polyaxon-schemas==0.5.2",
+          "polystores>=0.2.3",
+          "psutil==5.4.7",
+          "requests>=2.20.0",
+          "requests-toolbelt==0.8.0",
+          "websocket-client>=0.53.0",
+      ],
+      extras_require={
+          'gcs': ['google-cloud-storage'],
+          's3': ['boto3', 'botocore'],
+          'azure': ['azure-storage'],
+      },
+      classifiers=[
+          'Programming Language :: Python',
+          'Operating System :: OS Independent',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering :: Artificial Intelligence'
+      ],
+      tests_require=[
+          "pytest",
+          "httpretty==0.9.6",
+          "fake-factory==0.7.2"
+      ],
+      cmdclass={'test': PyTest})

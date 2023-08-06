61eb2b1c6371cf91dd7b46d428fa1d504a6ba168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:16 +0000

    Add package qrange/1.0.6/qrange-1.0.6.tar.gz

---
+++ packages/qrange/qrange-1.0.6.tar.gz/qrange-1.0.6/setup.py
@@ -0,0 +1,25 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="qrange",
+    version="1.0.6",
+    description="Working with ranges made simple",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Forfunckle/qrange",
+    author="Forfunckle",
+    author_email="cosminjames555@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages = ['qrange'],
+    include_package_data=True,
+)

6e73f1f0f81faf752d4d36bfeba98059adf23666
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:26 +0000

    Add package policypools/0.0.5/policypools-0.0.5-py3-none-any.whl

---
+++ packages/policypools/policypools-0.0.5-py3-none-any.whl/policypools/BoundedThreadPool.py
@@ -0,0 +1,23 @@
+from threading import Thread
+
+from policypools.AbstractThreadPool import AbstractThreadPool
+
+
+class BoundedThreadPool(AbstractThreadPool):
+
+    def __init__(self, max_q_size: int = 1, num_workers: int = 1):
+        """
+        Creates a thread pool bounded by the size and the num workers
+        since there is no policy this pool throws exceptions
+        :param max_q_size: the maximum size for the thread queue
+        :param num_workers: the maximum number of concurrently running workers
+        """
+        super(BoundedThreadPool, self).__init__(max_q_size, num_workers)
+
+    def submit(self, thread: Thread):
+        """
+        Submits a new thread, may throw queue.Full
+        :param thread: the given thread to run
+        :return: None
+        """
+        self._thread_q.put(thread, block=False)

5319b829bfb150d13405e366bf21246c9279dc3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:38 +0000

    Add package onegov.user/0.22.2/onegov.user-0.22.2.tar.gz

---
+++ packages/onegov.user/onegov.user-0.22.2.tar.gz/onegov.user-0.22.2/onegov/user/tests/test_cli.py
@@ -0,0 +1,141 @@
+import os.path
+import yaml
+
+from click.testing import CliRunner
+from onegov.user.cli import cli
+
+
+def test_cli(postgres_dsn, session_manager, temporary_directory, redis_url):
+
+    cfg = {
+        'applications': [
+            {
+                'path': '/foo/*',
+                'application': 'onegov.core.Framework',
+                'namespace': 'foo',
+                'configuration': {
+                    'dsn': postgres_dsn,
+                    'redis_url': redis_url
+                }
+            }
+        ]
+    }
+
+    session_manager.ensure_schema_exists('foo-bar')
+
+    cfg_path = os.path.join(temporary_directory, 'onegov.yml')
+
+    with open(cfg_path, 'w') as f:
+        f.write(yaml.dump(cfg))
+
+    runner = CliRunner()
+    result = runner.invoke(cli, [
+        '--config', cfg_path,
+        '--select', '/foo/bar',
+        'add', 'admin', 'admin@example.org',
+        '--password', 'hunter2',
+        '--no-prompt',
+    ])
+
+    assert result.exit_code == 0
+    assert 'Adding admin@example.org to foo/bar' in result.output
+    assert 'admin@example.org was added' in result.output
+
+    result = runner.invoke(cli, [
+        '--config', cfg_path,
+        '--select', '/foo/bar',
+        'list'
+    ])
+    assert result.exit_code == 0
+    assert 'admin@example.org' in result.output
+
+    result = runner.invoke(cli, [
+        '--config', cfg_path,
+        '--select', '/foo/bar',
+        'list-sessions'
+    ])
+    assert result.exit_code == 0
+    assert 'admin@example.org' not in result.output
+
+    result = runner.invoke(cli, [
+        '--config', cfg_path,
+        '--select', '/foo/bar',
+        'add', 'admin', 'admin@example.org',
+        '--password', 'hunter2',
+        '--no-prompt',
+    ])
+
+    assert result.exit_code == 1
+    assert 'admin@example.org already exists' in result.output
+
+    result = runner.invoke(cli, [
+        '--config', cfg_path,
+        '--select', '/foo/bar',
+        'exists', 'admin@example.org'
+    ])
+
+    assert result.exit_code == 0
+    assert 'admin@example.org exists' in result.output
+
+    result = runner.invoke(cli, [
+        '--config', cfg_path,
+        '--select', '/foo/bar',
+        'change-password', 'admin@example.org',
+        '--password', 'hunter2'
+    ])
+
+    assert result.exit_code == 0
+    assert "admin@example.org's password was changed" in result.output
+
+    result = runner.invoke(cli, [
+        '--config', cfg_path,
+        '--select', '/foo/bar',
+        'delete', 'admin@example.org'
+    ])
+
+    assert result.exit_code == 0
+    assert 'admin@example.org was deleted' in result.output
+
+    result = runner.invoke(cli, [
+        '--config', cfg_path,
+        '--select', '/foo/bar',
+        'list'
+    ])
+    assert result.exit_code == 0
+    assert 'admin@example.org' not in result.output
+
+    result = runner.invoke(cli, [
+        '--config', cfg_path,
+        '--select', '/foo/bar',
+        'list-sessions'
+    ])
+    assert result.exit_code == 0
+    assert 'admin@example.org' not in result.output
+
+    result = runner.invoke(cli, [
+        '--config', cfg_path,
+        '--select', '/foo/bar',
+        'delete', 'admin@example.org'
+    ])
+
+    assert result.exit_code == 1
+    assert 'admin@example.org does not exist' in result.output
+
+    result = runner.invoke(cli, [
+        '--config', cfg_path,
+        '--select', '/foo/bar',
+        'exists', 'admin@example.org'
+    ])
+
+    assert result.exit_code == 1
+    assert 'admin@example.org does not exist' in result.output
+
+    result = runner.invoke(cli, [
+        '--config', cfg_path,
+        '--select', '/foo/bar',
+        'change-password', 'admin@example.org',
+        '--password', 'hunter2'
+    ])
+
+    assert result.exit_code == 1
+    assert 'admin@example.org does not exist' in result.output

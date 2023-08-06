94d77b38a2fe14a91dff6a7c11fc59188d56bcc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:26 +0000

    Add package twitter-text-parser/1.0.0/twitter-text-parser-1.0.0.tar.gz

---
+++ packages/twitter-text-parser/twitter-text-parser-1.0.0.tar.gz/twitter-text-parser-1.0.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.rst", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="twitter-text-parser",
+    version="1.0.0",
+    author="swen128",
+    author_email="fujjisaaan@gmail.com",
+    description="A library to parse or validate Twitter texts properly",
+    long_description=long_description,
+    url="https://github.com/swen128/twitter-text-python",
+    packages=find_packages(),
+    install_requires=["attrs"],
+    python_requires='~=3.5',
+    classifiers=[
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Text Processing"
+    ],
+    package_data={
+        'twitter_text.regexp': ['*.txt']
+    }
+)

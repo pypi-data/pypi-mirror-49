9c38779b5f4da6d9d4772b8a5230945621d24cba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:54 +0000

    Add package raspi-servo/1.0.3/raspi_servo-1.0.3.tar.gz

---
+++ packages/raspi-servo/raspi_servo-1.0.3.tar.gz/raspi_servo-1.0.3/raspi_servo/servo.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+
+
+class Servo:
+	def __init__(self,num,angle)
+		GPIO.setup(num,GPIO.OUT)
+		p=GPIO.PWM(num,angle)
+		self.angle = angle
+		p.start(2.5)
+		
+		
+	def transfer(t):
+		f=10.0/180.0
+		t=t*f+2.5
+		t=int(t*10)/10.0
+		return t
+		
+		
+	def set(angle):
+		if angle<0 or angle>180:
+			p.stop()
+		else:
+			p.ChangeDutyCycle(transfer(angle))

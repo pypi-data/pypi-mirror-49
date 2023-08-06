da3688093152278e1e9356cea090b7807b39d5eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:12 +0000

    Add package entrancebar/0.0.10/entrancebar-0.0.10-py3-none-any.whl

---
+++ packages/entrancebar/entrancebar-0.0.10-py3-none-any.whl/entrancebar/__init__.py
@@ -0,0 +1,52 @@
+import argparse
+import importlib.util
+import os.path
+import sys
+import os
+import os.path
+from mako.template import Template
+
+alias = {}
+cache = {}
+
+if os.path.exists(os.path.join(os.getcwd(), "./entrancebar.config.json")):
+    try:
+        import ujson as json
+    except ModuleNotFoundError:
+        import json
+    context = json.load(open(os.path.join(os.getcwd(), "./entrancebar.config.json")))
+    alias = context.get("alias", {})
+
+def export(globaldict, package="default"):
+    globaldict['ENTRANCEBAR'] = {}
+    def __export__(deposit):
+        globaldict['ENTRANCEBAR'][package] = deposit
+    return __export__
+
+def entrance_file(filename: str, package="default"):
+    for i in alias.keys():
+        filename = filename.replace(i, alias[i])
+    filename = Template(filename).render(projectDir=os.getcwd())
+    path = os.path.join(os.path.dirname(os.path.abspath(sys._getframe(1).f_code.co_filename)), filename)
+    if not cache.get(path):
+        spec = importlib.util.spec_from_file_location(os.path.basename(path), path)
+        origin = importlib.util.module_from_spec(spec)
+        loaded = spec.loader.exec_module(origin)
+        cache[path] = origin
+    else:
+        origin = cache[path]
+    if not cache.get(os.path.dirname(os.path.abspath(sys._getframe(1).f_code.co_filename))):
+        local = os.path.dirname(os.path.abspath(sys._getframe(1).f_code.co_filename))
+        spec_local = importlib.util.spec_from_file_location(os.path.basename(local), local)
+        origin_local = importlib.util.module_from_spec(spec)
+        loaded_local = spec.loader.exec_module(origin)
+        cache[local] = origin_local
+    if 'ENTRANCEBAR' not in origin.__dict__:
+        return origin
+    return origin.__dict__["ENTRANCEBAR"][package]
+
+def path_render(path):
+    for i in alias.keys():
+        path = path.replace(i, alias[i])
+    path = Template(path).render(projectDir=os.getcwd())
+    return os.path.join(os.path.dirname(os.path.abspath(sys._getframe(1).f_code.co_filename)), path)

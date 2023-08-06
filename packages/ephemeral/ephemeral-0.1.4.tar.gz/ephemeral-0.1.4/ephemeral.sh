bd26af9a7a23d517ce55f0de827705812e9c05f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:41 +0000

    Add package ephemeral/0.1.4/ephemeral-0.1.4.tar.gz

---
+++ packages/ephemeral/ephemeral-0.1.4.tar.gz/ephemeral-0.1.4/ephemeral.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# Copyright (C) 2019 Sergej Alikov <sergej.alikov@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+set -eu
+
+BIND="${1:-0.0.0.0:8080}"
+
+num_cores=$(nproc)
+num_workers=$((2 * num_cores + 1))
+
+exec gunicorn --bind="$BIND" --workers="$num_workers" ephemeral.wsgi

e18d043a1ab2c8c7e6bf73f21debf8d5f08cf0f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:12 +0000

    Add package django-boolean-switch/0.2.9/django_boolean_switch-0.2.9-py3-none-any.whl

---
+++ packages/django-boolean-switch/django_boolean_switch-0.2.9-py3-none-any.whl/boolean_switch/urls.py
@@ -0,0 +1,19 @@
+from django import get_version
+from distutils.version import StrictVersion
+from django.conf.urls import include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+from boolean_switch.views import switch
+
+if StrictVersion(get_version()) > StrictVersion('1.8'):
+    urlpatterns = [
+        url(r'^(?P<url>.*)/switch/$', switch, name='switch'),
+    ]
+else:
+    from django.conf.urls import patterns
+
+    urlpatterns = patterns('',
+        url(r'^(?P<url>.*)/switch/$', switch, name='switch'),
+    )

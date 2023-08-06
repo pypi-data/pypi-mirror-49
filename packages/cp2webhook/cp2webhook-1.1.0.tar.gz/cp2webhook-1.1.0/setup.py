ad3363b33ca3bc03e85efa2f8a5c90e0787d2223
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:29 +0000

    Add package cp2webhook/1.1.0/cp2webhook-1.1.0.tar.gz

---
+++ packages/cp2webhook/cp2webhook-1.1.0.tar.gz/cp2webhook-1.1.0/setup.py
@@ -0,0 +1,41 @@
+import pip
+import setuptools
+
+with open('requirements.txt', 'r') as f:
+    requirements = f.read()
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+name = 'cp2webhook'
+
+setuptools.setup(
+    name=name,
+    version='1.1.0',
+    author='HARDWARIO s.r.o.',
+    author_email='ask@hardwario.com',
+    description='COOPER to Webhook',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/hardwario/cp2webhook',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities'
+    ],
+    keywords='cooper influxdb iot',
+    platforms='any',
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            '%s=%s:main' % (name, name)
+        ]
+    }
+)

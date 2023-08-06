db7e5faa124fd35480a25c474f8dffd79a71aef6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:22 +0000

    Add package django-mp-reviews/3.0/django-mp-reviews-3.0.tar.gz

---
+++ packages/django-mp-reviews/django-mp-reviews-3.0.tar.gz/django-mp-reviews-3.0/reviews/admin.py
@@ -0,0 +1,19 @@
+
+from django.contrib import admin
+
+from reviews.models import Review
+
+
+class ReviewAdmin(admin.ModelAdmin):
+
+    list_filter = ['is_active']
+
+    list_display = [
+        'name', 'email', 'date_created', 'user', 'rating', 'is_active']
+
+    list_editable = ['is_active']
+
+    search_fields = ['id', 'name', 'email', 'text']
+
+
+admin.site.register(Review, ReviewAdmin)

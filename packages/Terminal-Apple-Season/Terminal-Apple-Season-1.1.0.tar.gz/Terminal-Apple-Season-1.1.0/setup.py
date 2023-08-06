4a5ed9df83726d741b556d25dc09270dfa6c1a84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:24 +0000

    Add package Terminal-Apple-Season/1.1.0/Terminal-Apple-Season-1.1.0.tar.gz

---
+++ packages/Terminal-Apple-Season/Terminal-Apple-Season-1.1.0.tar.gz/Terminal-Apple-Season-1.1.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Terminal-Apple-Season",
+    version="1.1.0",
+    author="Arin Khare",
+    author_email="arinmkhare@gmail.com",
+    description="Apple season game for terminal",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lol-cubes/Terminal-Apple-Season",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+    ],
+    include_package_data=True
+)

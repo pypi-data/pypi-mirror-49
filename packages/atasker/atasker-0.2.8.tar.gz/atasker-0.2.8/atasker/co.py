ad1c245602b93b79c7c747018f5f7e4fad7a399e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:12 +0000

    Add package atasker/0.2.8/atasker-0.2.8.tar.gz

---
+++ packages/atasker/atasker-0.2.8.tar.gz/atasker-0.2.8/atasker/co.py
@@ -0,0 +1,46 @@
+from atasker import task_supervisor
+
+from atasker import TASK_NORMAL, TT_MP
+
+import uuid
+import asyncio
+
+
+async def co_mp_apply(f,
+                      args=(),
+                      kwargs={},
+                      priority=TASK_NORMAL,
+                      delay=None,
+                      supervisor=None):
+
+    class CO:
+
+        async def run(self, *args, **kwargs):
+            self._event = asyncio.Event()
+            task = (self.task_id, self.func, args, kwargs, self.callback)
+            return self.supervisor.put_task(
+                task, self.priority, self.delay, tt=TT_MP)
+
+        async def _set_event(self):
+            self._event.set()
+
+        def callback(self, result):
+            self.supervisor.mark_task_completed(self.task_id)
+            self._result = result
+            asyncio.run_coroutine_threadsafe(self._set_event(), loop=self._loop)
+
+        async def get_result(self):
+            await self._event.wait()
+            self._event.clear()
+            return self._result
+
+    co = CO()
+    co.task_id = str(uuid.uuid4())
+    co.priority = priority
+    co.delay = delay
+    co.supervisor = supervisor if supervisor else task_supervisor
+    co.func = f
+    co._loop = asyncio.get_event_loop()
+    if not await co.run(args, kwargs):
+        return None
+    return await co.get_result()

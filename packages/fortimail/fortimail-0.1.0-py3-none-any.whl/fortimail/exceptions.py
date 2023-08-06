489d57a2883c7eaff068f6fca5c05b4b96ccac96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:21 +0000

    Add package fortimail/0.1.0/fortimail-0.1.0-py3-none-any.whl

---
+++ packages/fortimail/fortimail-0.1.0-py3-none-any.whl/fortimail/exceptions.py
@@ -0,0 +1,50 @@
+'''
+Errors for the Fortimail client
+
+Root exception lets consumers of your API catch exceptions you raise on purpose
+'''
+
+
+class FortiMailError(Exception):
+    '''
+    Base-class for all exceptions raised by this module.
+    '''
+    def __init__(self, message=None, errors=None):
+        if errors:
+            message = ', '.join(errors)
+
+        self.errors = errors
+
+        super().__init__(message)
+
+
+class IllegalArgumentError(FortiMailError):
+    '''
+    Error raised when a function gets an illegal argument
+    '''
+    pass
+
+
+class ClientInitialisationError(FortiMailError):
+    '''
+    Error raised when the client is not initialised correctly
+    '''
+    pass
+
+
+class Forbidden(FortiMailError):
+    '''
+    Error raised when the client is forbidden
+    '''
+    pass
+
+
+class NotFound(FortiMailError):
+    '''
+    Error raised when the resource is not found or invalid path
+    '''
+    pass
+
+
+class UnexpectedError(FortiMailError):
+    pass

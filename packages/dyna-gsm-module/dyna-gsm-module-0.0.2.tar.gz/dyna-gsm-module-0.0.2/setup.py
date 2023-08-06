e8a0853c1bceb02ead089d76579fe6d92b1e9b27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:35 +0000

    Add package dyna-gsm-module/0.0.2/dyna-gsm-module-0.0.2.tar.gz

---
+++ packages/dyna-gsm-module/dyna-gsm-module-0.0.2.tar.gz/dyna-gsm-module-0.0.2/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+def readme():
+    with open("README.md", "r") as fh:
+        long_description = fh.read()
+    return long_description
+
+setuptools.setup(
+    name="dyna-gsm-module",
+    version="0.0.2",
+    author="John D. Geliberte",
+    author_email="gelibertejohn@gmail.com",
+    description="GSM modules required for dynaslope 3 project",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/jgeliberte/dyna-gsm-module",
+    packages=setuptools.find_packages(),
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3.5",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    install_requires=["serial","RPi.GPIO","python-gsmmodem-new","configparser"],
+    entry_points={
+        "console_scripts": [
+            "dyna-gsm-module=dyna_gsm_module.runner:main"
+        ]
+    }
+)

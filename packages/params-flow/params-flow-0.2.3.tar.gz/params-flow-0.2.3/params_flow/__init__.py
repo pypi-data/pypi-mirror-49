18c5505694f52a1753d3ddd141f3e66ced0909ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:37 +0000

    Add package params-flow/0.2.3/params-flow-0.2.3.tar.gz

---
+++ packages/params-flow/params-flow-0.2.3.tar.gz/params-flow-0.2.3/params_flow/__init__.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+#
+# created by kpe on 08.04.2019 at 8:52 PM
+#
+from __future__ import division, absolute_import, print_function
+
+from params_flow.layer import Layer
+from params_flow.model import Model
+from params_flow.normalization import LayerNormalization
+from params_flow.activations import gelu, gelu_exact

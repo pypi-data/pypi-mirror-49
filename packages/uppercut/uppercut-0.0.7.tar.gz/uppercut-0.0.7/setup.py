878f2d5db60de05f1e39fc524f6aa03daa4bec7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:05 +0000

    Add package uppercut/0.0.7/uppercut-0.0.7.tar.gz

---
+++ packages/uppercut/uppercut-0.0.7.tar.gz/uppercut-0.0.7/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup,find_packages
+print('A')
+setup(
+    name='uppercut',
+    url='https://github.com/StephanAm/uppercut.python.tools',
+    author='Stephan Marais',
+    maintainer='Stephan Marais',
+    version='0.0.7',
+    description='A library to handle common tasks for creating an Uppercut module in python.',
+    py_modules=['test'],
+    package_dir={'':'src'},
+    packages=find_packages('src'),
+    classifiers=[
+        "Programming Language :: Python :: 3.7"
+    ],
+)

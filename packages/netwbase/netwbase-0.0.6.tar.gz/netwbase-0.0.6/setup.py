4142355b733c7b8f239d1f7837ac43b13e1ddf62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:52 +0000

    Add package netwbase/0.0.6/netwbase-0.0.6.tar.gz

---
+++ packages/netwbase/netwbase-0.0.6.tar.gz/netwbase-0.0.6/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+
+setuptools.setup(
+    name="netwbase",
+    version="0.0.6",
+    author="n0nliner",
+    author_email="vkusnisaharok@gmail.com",
+    description="A small network DBMS",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/n0nliner/netwbase",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

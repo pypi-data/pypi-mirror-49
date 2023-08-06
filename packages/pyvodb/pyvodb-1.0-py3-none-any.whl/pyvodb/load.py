6c98ab2481f20891ca21a8a7da950203ffc0d6d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:41 +0000

    Add package pyvodb/1.0/pyvodb-1.0-py3-none-any.whl

---
+++ packages/pyvodb/pyvodb-1.0-py3-none-any.whl/pyvodb/load.py
@@ -0,0 +1,273 @@
+import os
+import sys
+import json
+import datetime
+import contextlib
+import collections
+
+import yaml
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql.expression import select
+from dateutil import rrule
+
+from . import tables
+
+try:
+    YAML_SAFE_LOADER = yaml.CSafeLoader
+except AttributeError:
+    YAML_SAFE_LOADER = yaml.SafeLoader
+
+
+def get_db(directory, engine=None):
+    """Get a database
+
+    :param directory: The root data directory
+    :param engine: a pre-created SQLAlchemy engine (default: in-memory SQLite)
+    """
+    if engine is None:
+        engine = create_engine('sqlite://')
+    tables.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    db = Session()
+    if directory is not None:
+        load_from_directory(db, directory)
+    return db
+
+
+def dict_from_directory(directory, root, ignored_files=()):
+    data = {}
+    for filename in os.listdir(os.path.join(root, directory)):
+        fullname = os.path.join(directory, filename)
+        absname = os.path.join(root, fullname)
+        if filename in ignored_files or filename.startswith('.'):
+            pass
+        elif filename.endswith('.yaml'):
+            info = load_yaml_file(absname)
+            info['_source'] = fullname
+            data[filename[:-5]] = info
+        elif os.path.isdir(absname):
+            data[filename] = dict_from_directory(fullname, root)
+        else:
+            raise ValueError('Unexpected file: ' + fullname)
+    return data
+
+
+def load_yaml_file(filename):
+    try:
+        with open(filename) as f:
+            return yaml.load(f, Loader=YAML_SAFE_LOADER)
+    except Exception as e:
+        raise Exception('Failed to load file {}: {}'.format(filename, e)) from e
+
+
+def load_from_directory(db, directory):
+    metadata = load_yaml_file(os.path.join(directory, 'meta.yaml'))
+    data = dict_from_directory(
+        '.', directory,
+        ignored_files=metadata.get('ignored_files',
+                                   ['.git', 'README', 'tests']))
+    load_from_dict(db, data, metadata)
+
+
+def load_from_dict(db, data, metadata):
+    """Load data from a dict (as loaded from directory of YAMLs) into database
+    """
+    # The ORM overhead is too high for this kind of bulk load,
+    # so drop down to SQLAlchemy Core.
+    # This tries to do minimize the number of SQL commands by loading entire
+    # tables at once
+
+    if metadata['version'] != 2:
+        raise ValueError('Can only load version 2')
+
+    try:
+        bind = db.get_bind(tables.Event)
+    except (ValueError, AttributeError):
+        pass
+    else:
+        if bind.dialect.name == 'sqlite':
+            db.execute('PRAGMA foreign_keys = ON')
+
+    with bulk_inserter(db) as insert:
+
+        # Load speakers
+
+        speaker_slugs = set()
+
+        for series_slug, series in data['series'].items():
+            for event_slug, event in series['events'].items():
+                for talk in event.get('talks'):
+                    for speaker in talk.get('speakers', ()):
+                        if speaker not in speaker_slugs:
+                            speaker_slugs.add(speaker)
+                            insert(tables.Speaker, {
+                                'slug': speaker,
+                                'name': speaker,
+                            })
+
+        venue_ids = {}
+
+        # Load cities, and their venues
+
+        for city_slug, city in data['cities'].items():
+            city_data = city['city']
+            insert(tables.City, {
+                'slug': city_slug,
+                'name': city_data['name'],
+                'latitude': city_data['location']['latitude'],
+                'longitude': city_data['location']['longitude'],
+                '_source': city_data['_source'],
+            })
+
+            for venue_slug, venue in city.get('venues', {}).items():
+                venue_ids[city_slug, venue_slug] = insert(tables.Venue, {
+                    'city_slug': city_slug,
+                    'slug': venue_slug,
+                    'name': venue['name'],
+                    'address': venue.get('address'),
+                    'latitude': venue['location']['latitude'],
+                    'longitude': venue['location']['longitude'],
+                    'notes': venue.get('notes'),
+                })
+
+
+        # Load series, their events, and everything underneath
+
+        for series_slug, series_dir in data['series'].items():
+
+            series = series_dir['series']
+            recurrence = series.get('recurrence')
+            if recurrence:
+                rrule_str = recurrence['rrule']
+                rrule.rrulestr(rrule_str)  # check rrule syntax
+                recurrence_attrs = {
+                    'recurrence_rule': rrule_str,
+                    'recurrence_scheme': recurrence['scheme'],
+                    'recurrence_description_cs': recurrence['description']['cs'],
+                    'recurrence_description_en': recurrence['description']['en'],
+                }
+            else:
+                recurrence_attrs = {
+                    'recurrence_rule': None,
+                    'recurrence_scheme': None,
+                    'recurrence_description_cs': None,
+                    'recurrence_description_en': None,
+                }
+            insert(tables.Series, {
+                'slug': series_slug,
+                'name': series['name'],
+                'home_city_slug': series.get('city'),
+                'description_cs': series['description']['cs'],
+                'description_en': series['description']['en'],
+                'organizer_info': json.dumps(series.get('organizer-info', ())),
+                **recurrence_attrs,
+            })
+
+            for event_slug, event in series_dir['events'].items():
+                venue_slug = event.get('venue')
+                city_slug = event['city']
+                if venue_slug:
+                    venue_id = venue_ids[city_slug, venue_slug]
+                else:
+                    venue_id = None
+
+                start = make_full_datetime(event['start'])
+                end = event.get('end')
+                if end is None:
+                    end = start.replace(hour=23, minute=59, second=59)
+                event_id = insert(tables.Event, {
+                    'name': event['name'],
+                    'number': event.get('number'),
+                    'topic': event.get('topic'),
+                    'description': event.get('description'),
+                    'date': start.date(),
+                    'start_time': start.time(),
+                    'end': end,
+                    'all_day': event.get('all_day', False),
+                    'series_slug': series_slug,
+                    'city_slug': city_slug,
+                    'venue_id': venue_id,
+                    '_source': event['_source']
+                })
+
+                for i, talk in enumerate(event.get('talks', ())):
+                    talk_id = insert(tables.Talk, {
+                        'event_id': event_id,
+                        'index': i,
+                        'title': talk['title'],
+                        'description': talk.get('description'),
+                        'is_lightning': talk.get('lightning', False),
+                    })
+
+                    for i, speaker in enumerate(talk.get('speakers', ())):
+                        assert speaker in speaker_slugs
+                        insert(tables.TalkSpeaker, {
+                            'talk_id': talk_id,
+                            'index': i,
+                            'speaker_slug': speaker,
+                        })
+
+                    for i, link in enumerate([
+                            *({'talk': u} for u in talk.get('urls', ())),
+                            *talk.get('coverage', {})]):
+                        for kind, url in link.items():
+                            insert(tables.TalkLink, {
+                                'talk_id': talk_id,
+                                'index': i,
+                                'url': url,
+                                'kind': kind,
+                            })
+
+                for i, url in enumerate(event.get('urls', ())):
+                    insert(tables.EventLink, {
+                        'event_id': event_id,
+                        'index': i,
+                        'url': url,
+                    })
+
+
+def make_full_datetime(value):
+    if hasattr(value, 'time'):
+        date = value.date()
+        time = value.time()
+    else:
+        date = value
+        time = datetime.time(hour=19)
+    return datetime.datetime.combine(date, time)
+
+
+@contextlib.contextmanager
+def bulk_inserter(db):
+    next_id = {}
+    table_columns = {}
+    table_rows = collections.OrderedDict()
+    need_id = {}
+
+    def insert(orm_class, row):
+        table = orm_class.__table__
+        if table in table_columns:
+            if set(row) != table_columns[table]:
+                raise ValueError('uneven table row')
+        else:
+            next_id[table] = 0
+            table_columns[table] = set(row)
+            table_rows[table] = []
+            need_id[table] = ('id' in table.c and table.c['id'].autoincrement)
+
+        row = dict(row)
+        if need_id[table]:
+            row['id'] = the_id = next_id[table]
+            next_id[table] += 1
+        else:
+            the_id = None
+
+        table_rows[table].append(row)
+
+        return the_id
+
+    yield insert
+
+    for table, rows in table_rows.items():
+        db.execute(table.delete())
+        db.execute(table.insert(), rows)
---
+++ packages/pyvodb/pyvodb-1.0-py3-none-any.whl/pyvodb/tables.py
@@ -0,0 +1,395 @@
+import re
+from urllib.parse import urlparse
+import datetime
+import collections
+import itertools
+
+from sqlalchemy import Column, ForeignKey, MetaData, extract, desc
+from sqlalchemy import UniqueConstraint
+from sqlalchemy.types import Boolean, Integer, Unicode, UnicodeText, Date, Time
+from sqlalchemy.types import Enum, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import backref, relationship
+from sqlalchemy.orm.session import Session
+from sqlalchemy.ext.orderinglist import ordering_list
+from sqlalchemy.ext.associationproxy import association_proxy
+from dateutil import tz, rrule, relativedelta
+
+metadata = MetaData()
+TableBase = declarative_base(metadata=metadata)
+
+CET = tz.gettz('Europe/Prague')
+
+YOUTUBE_RE = re.compile(r'''(?x)https?://
+                        (?:
+                            (?:www\.youtube\.com/watch\?v=)|
+                            (?:youtu\.be/)
+                        )
+                        ([-0-9a-zA-Z_]+)''')
+
+
+def date_property(name):
+    @hybrid_property
+    def _func(self):
+        return getattr(self.date, name)
+    @_func.expression
+    def _func(self):
+        return extract(name, self.date)
+    return _func
+
+
+class Event(TableBase):
+    u"""An event."""
+    __tablename__ = 'events'
+    __table_args__ = (UniqueConstraint('city_slug', 'date', 'start_time'),)
+    id = Column(
+        Integer, primary_key=True, nullable=False,
+        doc=u"An internal numeric ID")
+    series_slug = Column(
+        ForeignKey('series.slug'), nullable=False,
+        doc=u"The series this event belongs to")
+    name = Column(
+        Unicode(), nullable=False,
+        doc=u"General name of the event – often, this is the same as the "
+            u"name of the series")
+    number = Column(
+        Integer(), nullable=True,
+        doc=u"Serial number of the event (if kept track of)")
+    topic = Column(
+        Unicode(), nullable=True,
+        doc=u"Topic of the event")
+    description = Column(
+        UnicodeText(), nullable=True,
+        doc=u"Description of the event")
+    date = Column(
+        Date(),
+        doc=u"Day the event takes place")
+    start_time = Column(
+        Time(),
+        doc=u"The start time")
+    all_day = Column(
+        Boolean(), nullable=False, default=False,
+        doc=u"True if the event lasts all day")
+    end = Column(
+        DateTime(),
+        doc=u"The end date and time")
+    _source = Column(
+        Unicode(), nullable=True,
+        doc=u"File from which the entry was loaded")
+    city_slug = Column(ForeignKey('cities.slug'), nullable=False)
+    city = relationship('City', backref=backref('events',
+                                                order_by=desc('date')))
+    series = relationship('Series', backref=backref('events',
+                                                    order_by=desc('date')))
+    venue_id = Column(ForeignKey('venues.id'), nullable=True)
+    venue = relationship('Venue', backref=backref('events'))
+    talks = relationship('Talk', collection_class=ordering_list('index'),
+                         order_by='Talk.index',
+                         backref=backref('event'))
+
+    @property
+    def title(self):
+        parts = [self.name]
+        if self.number is not None:
+            parts.append('#{}'.format(self.number))
+        elif self.topic:
+            parts.append('–')
+        if self.topic:
+            parts.append(self.topic)
+        return ' '.join(parts)
+
+    @property
+    def start(self):
+        """The event's start time, as a timezone-aware datetime object"""
+        if self.start_time is None:
+            time = datetime.time(hour=19, tzinfo=CET)
+        else:
+            time = self.start_time.replace(tzinfo=CET)
+        return datetime.datetime.combine(self.date, time)
+
+    @property
+    def slug(self):
+        """Identifier for use in URLs. Unique within the series"""
+        return self.date.strftime('%Y-%m')
+
+    year = date_property('year')
+    month = date_property('month')
+    day = date_property('day')
+
+    @property
+    def one_day(self):
+        """Is true if the event only spans one day"""
+        return self.end.date() == self.date
+
+    def as_dict(self):
+        talks = []
+        for talk in self.talks:
+            talk_info = collections.OrderedDict()
+            talks.append(talk_info)
+            talk_info['title'] = talk.title
+            if talk.is_lightning:
+                talk_info['lightning'] = True
+            talk_info['speakers'] = [s.name for s in talk.speakers]
+            talk_info['urls'] = [l.url for l in talk.links if l.kind == 'talk']
+            talk_info['coverage'] = [{l.kind: l.url} for l in talk.links if l.kind != 'talk']
+            if talk.description:
+                talk_info['description'] = talk.description
+
+        result = collections.OrderedDict()
+        result['city'] = self.city.slug
+        result['start'] = datetime.datetime.combine(self.date, self.start_time)
+        result['name'] = self.name
+        if self.number is not None:
+            result['number'] = self.number
+        if self.topic is not None:
+            result['topic'] = self.topic
+        if self.description is not None:
+            result['description'] = self.description
+        if self.venue:
+            result['venue'] = self.venue.slug
+        result['talks'] = talks
+        result['urls'] = [l.url for l in self.links]
+        return result
+
+
+class City(TableBase):
+    u"""A city that holds events"""
+    __tablename__ = 'cities'
+    slug = Column(
+        Unicode(), primary_key=True,
+        doc=u"Unique identifier for use in URLs")
+    name = Column(
+        Unicode(), nullable=False,
+        doc=u"Name of the city")
+    longitude = Column(
+        Unicode(), nullable=False,
+        doc=u"Longitude of the location")
+    latitude = Column(
+        Unicode(), nullable=False,
+        doc=u"Latitude of the location")
+    _source = Column(
+        Unicode(), nullable=True,
+        doc=u"File from which the entry was loaded")
+
+
+class Series(TableBase):
+    u"""A series events"""
+    __tablename__ = 'series'
+    slug = Column(
+        Unicode(), primary_key=True,
+        doc=u"Unique identifier for use in URLs")
+    name = Column(
+        Unicode(), nullable=False,
+        doc=u"Name of the series")
+    home_city_slug = Column(
+        ForeignKey(City.slug), nullable=True,
+        doc=u"City this series usually takes place at, if any")
+    description_cs = Column(
+        Unicode(), nullable=True,
+        doc=u"Czech description")
+    description_en = Column(
+        Unicode(), nullable=True,
+        doc=u"English description")
+
+    recurrence_scheme = Column(
+        Unicode(), nullable=True,
+        doc=u"Basic type of the series' recurrence scheme")
+    recurrence_rule = Column(
+        Unicode(), nullable=True,
+        doc=u"RFC 2445 recurrence rule for regular event dates")
+    recurrence_description_cs = Column(
+        Unicode(), nullable=True,
+        doc=u"Czech description of the recurrence rule")
+    recurrence_description_en = Column(
+        Unicode(), nullable=True,
+        doc=u"English description of the recurrence rule")
+
+    # XXX: Remove this:
+    organizer_info = Column(
+        Unicode(), nullable=True,
+        doc=u"Info about organizers, as JSON.")
+
+    home_city = relationship('City', backref=backref('series'))
+
+    def next_occurrences(self, n=None, since=None):
+        """Yield the next planned occurrences after the date "since"
+
+        The `since` argument can be either a date or datetime onject.
+        If not given, it defaults to the date of the last event that's
+        already planned.
+
+        If `n` is given, the result is limited to that many dates;
+        otherwise, infinite results may be generated.
+        Note that less than `n` results may be yielded.
+        """
+        scheme = self.recurrence_scheme
+        if scheme is None:
+            return ()
+
+        db = Session.object_session(self)
+        query = db.query(Event)
+        query = query.filter(Event.series_slug == self.slug)
+        query = query.order_by(desc(Event.date))
+        query = query.limit(1)
+        last_planned_event = query.one_or_none()
+
+        if since is None:
+            last_planned_event = query.one()
+            since = last_planned_event.date
+        elif since < last_planned_event.date:
+            since = last_planned_event.date
+
+        start = getattr(since, 'date', since)
+
+        start += relativedelta.relativedelta(days=+1)
+
+        if (scheme == 'monthly'
+                and last_planned_event
+                and last_planned_event.date.year == start.year
+                and last_planned_event.date.month == start.month):
+            # Monthly events try to have one event per month, so exclude
+            # the current month if there was already a meetup
+            start += relativedelta.relativedelta(months=+1)
+            start = start.replace(day=1)
+
+        start = datetime.datetime.combine(start, datetime.time(tzinfo=CET))
+        result = rrule.rrulestr(self.recurrence_rule, dtstart=start)
+        if n is not None:
+            result = itertools.islice(result, n)
+        return result
+
+
+class Venue(TableBase):
+    u"""A venue to old events in"""
+    __tablename__ = 'venues'
+    __table_args__ = (UniqueConstraint('city_slug', 'slug'),)
+    id = Column(
+        Integer, primary_key=True, nullable=False,
+        doc=u"An internal numeric ID")
+    name = Column(
+        Unicode(), nullable=False,
+        doc=u"Name of the venue")
+    city_slug = Column(
+        ForeignKey('cities.slug'), nullable=False,
+        doc=u"City of the venue")
+    address = Column(
+        Unicode(), nullable=True,
+        doc=u"Address of the venue")
+    longitude = Column(
+        Unicode(), nullable=False,
+        doc=u"Longitude of the location")
+    latitude = Column(
+        Unicode(), nullable=False,
+        doc=u"Latitude of the location")
+    slug = Column(
+        Unicode(), nullable=False,
+        doc=u"Identifier for use in URLs")
+    notes = Column(
+        Unicode(), nullable=True,
+        doc=u"Notes about the venue, e.g. directions to get there")
+
+    city = relationship('City', backref=backref('venues'))
+
+    @property
+    def short_address(self):
+        if self.address is not None:
+            return self.address.splitlines()[0]
+
+
+class EventLink(TableBase):
+    __tablename__ = 'event_links'
+    event_id = Column(ForeignKey('events.id'), primary_key=True, nullable=False)
+    event = relationship('Event', backref=backref('links', cascade='delete'))
+    url = Column(Unicode(), primary_key=True, nullable=False)
+    index = Column(
+        Integer(),
+        doc=u"Index in order of an event's links")
+
+
+class Talk(TableBase):
+    u"""A talk"""
+    __tablename__ = 'talks'
+    id = Column(
+        Integer, primary_key=True, nullable=False,
+        doc=u"An internal numeric ID")
+    title = Column(
+        Unicode(), nullable=False,
+        doc=u"Talk title")
+    index = Column(
+        Integer(), nullable=False,
+        doc=u"Index in order of talks within an event")
+    description = Column(
+        UnicodeText(), nullable=True,
+        doc=u"Description of the talk")
+    is_lightning = Column(
+        Boolean(), nullable=False, default=False,
+        doc=u"True if this is a lightning talk")
+    event_id = Column(ForeignKey('events.id'), nullable=True)
+    talk_speakers = relationship('TalkSpeaker',
+                                 collection_class=ordering_list('index'),
+                                 order_by='TalkSpeaker.index',
+                                 backref=backref('talk'))
+    speakers = association_proxy('talk_speakers', 'speaker',
+                                 creator=lambda s: TalkSpeaker(speaker=s))
+    links = relationship('TalkLink',
+                         collection_class=ordering_list('index'),
+                         order_by='TalkLink.index',
+                         backref=backref('talk'))
+
+    @property
+    def youtube_id(self):
+        for link in self.links:
+            yid = link.youtube_id
+            if yid:
+                return yid
+
+
+class Speaker(TableBase):
+    __tablename__ = 'speakers'
+    slug = Column(
+        Unicode(), primary_key=True,
+        doc=u"Slug to be used in URLs")
+    name = Column(
+        Unicode(), nullable=False,
+        doc=u"Name of the speaker")
+    talk_speakers = relationship('TalkSpeaker', backref=backref('speaker'))
+    talks = association_proxy('talk_speakers', 'talk',
+                              creator=lambda t: TalkSpeaker(talk=t))
+
+
+class TalkSpeaker(TableBase):
+    __tablename__ = 'talk_speakers'
+    talk_id = Column(
+        ForeignKey('talks.id'),
+        primary_key=True, nullable=False)
+    speaker_slug = Column(
+        ForeignKey('speakers.slug'),
+        primary_key=True, nullable=False)
+    index = Column(
+        Integer(), nullable=True,
+        doc=u"Index in order of a talk's speakers")
+
+
+class TalkLink(TableBase):
+    __tablename__ = 'talk_links'
+    talk_id = Column(
+        ForeignKey('talks.id'), primary_key=True, nullable=False)
+    url = Column(Unicode(), primary_key=True, nullable=False)
+    index = Column(
+        Integer(),
+        doc=u"Index in order of a talk's links")
+    kind = Column(
+        Enum('slides', 'video', 'link', 'writeup', 'notes', 'talk'),
+        doc="Kind of the link. 'talk' is a link to the talk itself; "
+            "the rest is for suporting material")
+
+    @property
+    def hostname(self):
+        return urlparse(self.url).hostname
+
+    @property
+    def youtube_id(self):
+        match = YOUTUBE_RE.match(self.url)
+        if match:
+            return match.group(1)

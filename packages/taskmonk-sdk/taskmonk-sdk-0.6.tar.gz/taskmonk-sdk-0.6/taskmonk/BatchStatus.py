93ac1787e50a03dc6a434febdee0d76017769444
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:10 +0000

    Add package taskmonk-sdk/0.6/taskmonk-sdk-0.6.tar.gz

---
+++ packages/taskmonk-sdk/taskmonk-sdk-0.6.tar.gz/taskmonk-sdk-0.6/taskmonk/BatchStatus.py
@@ -0,0 +1,18 @@
+
+class BatchStatus:
+    newCount = 0
+    completed = 0
+    total = 0
+    inProgress = 0
+
+    def __init__(self,new_count,in_progress,completed,total):
+        self.completed = completed
+        self.newCount = new_count
+        self.inProgress = in_progress
+        self.total = total
+
+    def get_completed(self):
+        return completed
+    
+    def get_total(self):
+        return total

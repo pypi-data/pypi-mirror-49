2c139a68d4c5f16ea42c9b034260194ef45d2a46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:14 +0000

    Add package safescope/0.2.5/safescope-0.2.5.tar.gz

---
+++ packages/safescope/safescope-0.2.5.tar.gz/safescope-0.2.5/setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as readme_file:
+    readme = readme_file.read()
+
+# with open('HISTORY.rst') as history_file:
+#     history = history_file.read()
+
+requirements = [
+    # 're',
+    # 'inspect',
+]
+
+setup_requirements = [
+    'pytest-runner',
+    # TODO(havakv): put setup requirements (distutils extensions, etc.) here
+]
+
+test_requirements = [
+    'pytest',
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='safescope',
+    version='0.2.5',
+    description="Python package for preventing use of variables from global scope.",
+    # long_description=readme + '\n\n' + history,
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author="Haavard Kvamme",
+    author_email='haavard.kvamme@gmail.com',
+    url='https://github.com/havakv/safescope',
+    packages=find_packages(include=['safescope']),
+    include_package_data=True,
+    install_requires=requirements,
+    license="BSD license",
+    zip_safe=False,
+    keywords='safescope',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    setup_requires=setup_requirements,
+    python_requires='>=3.5',
+)

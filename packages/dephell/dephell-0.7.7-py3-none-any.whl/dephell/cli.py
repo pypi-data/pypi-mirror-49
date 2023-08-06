baa816c98007c0c1266af5c115a2d4409fbf6e98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:20 +0000

    Add package dephell/0.7.7/dephell-0.7.7-py3-none-any.whl

---
+++ packages/dephell/dephell-0.7.7-py3-none-any.whl/dephell/cli.py
@@ -0,0 +1,97 @@
+# built-in
+from argparse import Action, ArgumentParser
+from logging import getLogger
+from pdb import post_mortem
+from sys import argv
+from typing import List
+
+# app
+from .commands import COMMANDS
+from .constants import ReturnCodes
+from .exceptions import ExtraException
+
+
+logger = getLogger('dephell.cli')
+
+
+class PatchedParser(ArgumentParser):
+    def format_help(self):
+        formatter = self._get_formatter()
+        formatter.add_usage(self.usage, self._actions, self._mutually_exclusive_groups)
+        formatter.add_text(self.description)
+
+        for action_group in self._action_groups:
+            formatter.start_section(action_group.title)
+            formatter.add_text(action_group.description)
+            formatter.add_arguments(action_group._group_actions)
+            formatter.end_section()
+        formatter.add_text(self.epilog)
+
+        formatter.start_section('commands')
+        for name, command in sorted(COMMANDS.items()):
+            descr = command.get_parser().description.split('\n')[0]
+            formatter.add_argument(Action([name], '', help=descr))
+        formatter.end_section()
+
+        return formatter.format_help()
+
+
+parser = PatchedParser(
+    description='Manage dependencies, projects, virtual environments.',
+)
+parser.add_argument('command', choices=COMMANDS.keys(), nargs='?', help='command to execute')
+
+
+def main(argv: List[str]) -> int:
+    # get command name
+    for size, direction in ((1, 1), (2, 1), (2, -1)):
+        command_name = ' '.join(argv[:size][::direction])
+        command_args = argv[size:]
+        if command_name in COMMANDS:
+            break
+    else:
+        args = parser.parse_args(argv[:1])
+        if args.command is None:
+            parser.parse_args(['--help'])
+        command_name = args.command
+        command_args = argv[1:]
+
+    # get and init command object
+    command = COMMANDS[command_name]
+    try:
+        task = command(command_args)
+    except KeyError as e:  # env not found
+        logger.exception(e.args[0])
+        return ReturnCodes.INVALID_CONFIG.value
+    except Exception as e:
+        logger.exception('{}: {}'.format(type(e).__name__, e))
+        if '--traceback' in argv:
+            raise
+        return ReturnCodes.UNKNOWN_EXCEPTION.value
+
+    # validate config
+    is_valid = task.validate()
+    if not is_valid:
+        return ReturnCodes.INVALID_CONFIG.value
+
+    # execute command
+    try:
+        result = task()
+    except Exception as exc:
+        if isinstance(exc, ExtraException):
+            logger.exception(str(exc), extra=exc.extra)
+        else:
+            logger.exception('{}: {}'.format(type(exc).__name__, exc))
+        if hasattr(task, 'config') and task.config.get('pdb', False):
+            post_mortem()
+        return ReturnCodes.UNKNOWN_EXCEPTION.value
+    except KeyboardInterrupt:
+        logger.exception('stopped by user')
+        return ReturnCodes.UNKNOWN_EXCEPTION.value
+    if not result:
+        return ReturnCodes.COMMAND_ERROR.value
+    return ReturnCodes.OK.value
+
+
+def entrypoint():
+    exit(main(argv[1:]))
---
+++ packages/dephell/dephell-0.7.7-py3-none-any.whl/dephell/models/release.py
@@ -0,0 +1,54 @@
+# built-in
+from datetime import datetime
+from typing import Optional
+
+# external
+import attr
+from dephell_specifier import RangeSpecifier
+from packaging.utils import canonicalize_name
+from packaging.version import parse
+
+# app
+from ..cached_property import cached_property
+
+
+@attr.s(hash=False, cmp=True)
+class Release:
+    dependencies = None  # type: tuple
+
+    raw_name = attr.ib(type=str, cmp=False)
+    version = attr.ib(converter=parse, cmp=True)  # typing: ignore
+    time = attr.ib(repr=False, hash=False)                      # upload_time
+    python = attr.ib(default=None, repr=False, cmp=False)       # requires_python
+    hashes = attr.ib(factory=tuple, repr=False, cmp=False)      # digests/sha256
+
+    extra = attr.ib(type=Optional[str], default=None)
+
+    def __attrs_post_init__(self):
+        assert '[' not in self.raw_name, self.raw_name
+
+    @classmethod
+    def from_response(cls, name, version, info, extra=None):
+        latest = info[-1]
+        python = latest['requires_python']
+        if python is not None:
+            python = RangeSpecifier(python)
+
+        return cls(
+            raw_name=name,
+            version=version,
+            time=datetime.strptime(latest['upload_time'], '%Y-%m-%dT%H:%M:%S'),
+            python=python,
+            hashes=tuple(rel['digests']['sha256'] for rel in info),
+            extra=extra,
+        )
+
+    @cached_property
+    def name(self) -> str:
+        return canonicalize_name(self.raw_name)
+
+    def __str__(self):
+        return '{name}=={version}'.format(name=self.raw_name, version=self.version)
+
+    def hash(self):
+        return hash((self.name, self.version))

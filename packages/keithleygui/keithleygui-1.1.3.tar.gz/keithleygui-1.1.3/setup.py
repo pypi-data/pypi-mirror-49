09e20cbc9a95483b1c73d8dea37aef91c2ed70c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:16 +0000

    Add package keithleygui/1.1.3/keithleygui-1.1.3.tar.gz

---
+++ packages/keithleygui/keithleygui-1.1.3.tar.gz/keithleygui-1.1.3/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='keithleygui',
+    version='1.1.3',
+    description="",
+    author='Sam Schott',
+    author_email='ss2151@cam.ac.uk',
+    url='https://github.com/oe-fet/keithleygui.git',
+    license='MIT',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    package_data={
+        'keithleygui': ['*.ui'],
+    },
+    entry_points={
+        'console_scripts': [
+            'keithleygui=keithleygui.main:run'
+        ],
+        'gui_scripts': [
+            'keithleygui_gui=keithleygui.main:run'
+        ]
+    },
+    install_requires=[
+        'keithley2600>=1.2.1',
+        'numpy',
+        'pyvisa',
+        'pyqtgraph_cx>=0.12',
+        'qtpy',
+        'repr',
+        'setuptools',
+    ],
+    zip_safe=False,
+    keywords='keithleygui',
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='tests',
+    tests_require=[
+    ]
+)

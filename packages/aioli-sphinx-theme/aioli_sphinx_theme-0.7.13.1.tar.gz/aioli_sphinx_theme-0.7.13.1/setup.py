00401200a651733dfcd6de48f80ed741af58cb79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:04 +0000

    Add package aioli-sphinx-theme/0.7.13.1/aioli_sphinx_theme-0.7.13.1.tar.gz

---
+++ packages/aioli-sphinx-theme/aioli_sphinx_theme-0.7.13.1.tar.gz/aioli_sphinx_theme-0.7.13.1/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import codecs
+from setuptools import setup
+
+# Version info -- read without importing
+_locals = {}
+with open("alabaster/_version.py") as fp:
+    exec(fp.read(), None, _locals)
+version = _locals["__version__"]
+
+# README into long description
+with codecs.open("README.rst", encoding="utf-8") as f:
+    readme = f.read()
+
+setup(
+    name="aioli_sphinx_theme",
+    version=version,
+    description="Aioli Sphinx theme, based on Alabaster",
+    long_description=readme,
+    author="Robert Wikman",
+    author_email="rbw@vault13.org",
+    url="https://alabaster.readthedocs.io",
+    packages=["alabaster"],
+    modules=["aioli_sphinx_theme"],
+    include_package_data=True,
+    entry_points={"sphinx.html_themes": ["aioli_sphinx_theme = aioli_sphinx_theme"]},
+)

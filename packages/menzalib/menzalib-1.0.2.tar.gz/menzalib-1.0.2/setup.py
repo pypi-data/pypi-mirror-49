4f4096de2c78a2e3d7dada113b4991f9d2fccf53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:00 +0000

    Add package menzalib/1.0.2/menzalib-1.0.2.tar.gz

---
+++ packages/menzalib/menzalib-1.0.2.tar.gz/menzalib-1.0.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="latin1") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="menzalib",
+    version="1.0.2",
+    author=["Lorenzo Cavuoti", "Francesco Sacco"],
+    author_email="lorenzocav97@gmail.com",
+    description="Funzioni utili per lab3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LetteraUnica/menzalib",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'numdifftools',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ]
+)

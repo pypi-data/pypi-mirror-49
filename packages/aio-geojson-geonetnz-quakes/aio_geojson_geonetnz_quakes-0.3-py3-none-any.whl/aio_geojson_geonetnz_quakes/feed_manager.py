9f3774080abf9865eaa5fb39162528aa132d418c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:03 +0000

    Add package aio-geojson-geonetnz-quakes/0.3/aio_geojson_geonetnz_quakes-0.3-py3-none-any.whl

---
+++ packages/aio-geojson-geonetnz-quakes/aio_geojson_geonetnz_quakes-0.3-py3-none-any.whl/aio_geojson_geonetnz_quakes/feed_manager.py
@@ -0,0 +1,23 @@
+"""Feed Manager for GeoNet NZ Quakes feed."""
+from aio_geojson_client.feed_manager import FeedManagerBase
+from aiohttp import ClientSession
+
+from .feed import GeonetnzQuakesFeed
+
+
+class GeonetnzQuakesFeedManager(FeedManagerBase):
+    """Feed Manager for GeoNet NZ Quakes feed."""
+
+    def __init__(self, websession: ClientSession, generate_callback,
+                 update_callback, remove_callback,
+                 home_coordinates, mmi=-1, filter_radius=None,
+                 filter_minimum_magnitude=None):
+        """Initialize the GeoNet NZ Quakes Feed Manager."""
+        feed = GeonetnzQuakesFeed(
+            websession,
+            home_coordinates,
+            mmi=mmi,
+            filter_radius=filter_radius,
+            filter_minimum_magnitude=filter_minimum_magnitude)
+        super().__init__(feed, generate_callback, update_callback,
+                         remove_callback)

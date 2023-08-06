215e82595c431969d838ddaf4b31a0b5d6f66ed6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:05 +0000

    Add package datatable/0.9.0/datatable-0.9.0-cp35-cp35m-manylinux2010_x86_64.whl

---
+++ packages/datatable/datatable-0.9.0-cp35-cp35m-manylinux2010_x86_64.whl/datatable/__git__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# © H2O.ai 2018; -*- encoding: utf-8 -*-
+#   This Source Code Form is subject to the terms of the
+#   Mozilla Public License, v2.0. If a copy of the MPL was
+#   not distributed with this file, You can obtain one at
+#   http://mozilla.org/MPL/2.0/.
+# ----------------------------------------------------------
+# This file was auto-generated from ci/setup_utils.py
+
+__git_revision__ = 'f030c69175c4da1268b342bc2189c666b3637313'

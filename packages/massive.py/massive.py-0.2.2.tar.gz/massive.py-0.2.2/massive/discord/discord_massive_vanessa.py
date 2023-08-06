3da86955d989bb1938116ed3253e3b0ef18ff617
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:27 +0000

    Add package massive.py/0.2.2/massive.py-0.2.2.tar.gz

---
+++ packages/massive.py/massive.py-0.2.2.tar.gz/massive.py-0.2.2/massive/discord/discord_massive_vanessa.py
@@ -0,0 +1,30 @@
+import random
+
+from massive.discord import discord_massive
+
+
+class MassiveVanessa(discord_massive.Massive):
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
+
+	def convert(self, c):
+		self.__ends_with_emoji = False
+
+		if c == ' ':
+			return "  "
+
+		lower = random.choice([True, False])
+
+		if 'a' <= c.lower() <= 'z':
+			if lower:
+				return c.lower()
+
+			emoji = self.map_to_emoji(c)
+
+			if emoji:
+				self.__ends_with_emoji = True
+				return ":" + emoji + ": "
+
+			return c.upper()
+
+		return c

8eeec6234eed5b3df94a1248b36bb79e9466ee72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:30 +0000

    Add package soma/0.1.2/soma-0.1.2-py3-none-any.whl

---
+++ packages/soma/soma-0.1.2-py3-none-any.whl/soma/estilo/__init__.py
@@ -0,0 +1,7 @@
+from .utils import connect_and_query
+from .utils import format_produto_animale
+from .utils import calculate_giro
+
+from .modelling import correlation_matrix
+from .modelling import distance_matrix
+from .modelling import distance_orderner

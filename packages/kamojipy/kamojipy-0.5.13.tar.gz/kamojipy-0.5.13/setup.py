0f7950de38b0cbf59202f464280fd8ba8b556a6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:02 +0000

    Add package kamojipy/0.5.13/kamojipy-0.5.13.tar.gz

---
+++ packages/kamojipy/kamojipy-0.5.13.tar.gz/kamojipy-0.5.13/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name='kamojipy',
+    version="0.5.13",
+    author='Safyre Lyons',
+    author_email='lyons.safyre@gmail.com',
+    description="Kamoji string in python",
+    url='https://github.com/SafyreLyons/kamojipy',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    scripts=['kamojipy.py']
+)

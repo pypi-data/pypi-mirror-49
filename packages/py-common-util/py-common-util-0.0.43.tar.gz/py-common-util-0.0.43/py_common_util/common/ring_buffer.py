cdcb02ae2fad5152c1f930ff4c02f09d5e7f7e1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:48 +0000

    Add package py-common-util/0.0.43/py-common-util-0.0.43.tar.gz

---
+++ packages/py-common-util/py-common-util-0.0.43.tar.gz/py-common-util-0.0.43/py_common_util/common/ring_buffer.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from collections import deque
+
+
+class RingBuffer(deque):
+    """
+    python的ring buffer高性能封装
+    为性能考虑，应注释一切代理的方法(i.e. append, get)，程序中应直接使用deque原始支持的方法
+    # def append(self, x):
+        # super().append(x)
+    """
+    def __init__(self, maxlen):
+        super().__init__(maxlen=maxlen)
+
+
+if __name__ == '__main__':
+    import time
+    rb = RingBuffer(1000)
+    tm1 = time.time()
+    for i in range(int(1e6)):
+        rb.append(i)
+    print(list(rb)[:10])
+    rb.clear()
+    tm2 = time.time()
+    print("使用封装的ringbuffer执行时间；{:.2f}seconds".format(tm2 - tm1))
+
+    import time
+    import collections
+    d = collections.deque(maxlen=1000)
+    tm1 = time.time()
+    for i in range(int(1e6)):
+        d.append(i)
+    print(list(d)[:10])
+    d.clear()
+    tm2 = time.time()
+    print("使用原始的deque执行时间；{:.2f}seconds".format(tm2 - tm1))

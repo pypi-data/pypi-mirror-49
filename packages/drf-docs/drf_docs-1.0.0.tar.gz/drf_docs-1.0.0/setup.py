605e13a12f678085a0e4d5745f6ee9f07cb08fc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:06 +0000

    Add package drf-docs/1.0.0/drf_docs-1.0.0.tar.gz

---
+++ packages/drf-docs/drf_docs-1.0.0.tar.gz/drf_docs-1.0.0/setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+ @Time    : 19/7/29 11:15
+ @Author  : CyanZoy
+ @File    : setup.py.py
+ @Software: PyCharm
+ @Describe: 
+ """
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: mage
+# Mail: mage@woodcol.com
+# Created Time: 2018-1-23 19:17:34
+#############################################
+
+
+from setuptools import setup, find_packages
+
+import drf_docs
+
+setup(
+    name="drf_docs",
+    version=drf_docs.__version__,
+    keywords=["pip", "api", "docs"],
+    description="api docs",
+    long_description="django restframework api docs",
+    license="MIT Licence",
+
+    url="https://github.com/MAOA-L/docs_drf/tree/master/drf_docs",
+    author="MAOA-L",
+    author_email="chanzhouyun@gmail.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=['rest_framework']
+)

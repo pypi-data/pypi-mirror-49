e6ae11047ba3fab74f272269ab7f29680f101233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:52 +0000

    Add package django-contact-recaptcha-v3/1.0.1/django-contact-recaptcha-v3-1.0.1.tar.gz

---
+++ packages/django-contact-recaptcha-v3/django-contact-recaptcha-v3-1.0.1.tar.gz/django-contact-recaptcha-v3-1.0.1/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-contact-recaptcha-v3',
+    version='1.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',
+    description='Django Contact Form with Google Recaptcha v3 - integration.',
+    long_description=README,
+    url='https://www.blessond.com/contact',
+    author='Lukas Schönsgibl (aka hybridpicker)',
+    author_email='lukas@schoensgibl.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django :: 2.2',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+)

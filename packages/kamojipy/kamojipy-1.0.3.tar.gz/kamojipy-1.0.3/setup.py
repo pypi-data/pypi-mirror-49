9248a0c7e4cc0d150d0fc9fb4a762845a4d513c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:09 +0000

    Add package kamojipy/1.0.3/kamojipy-1.0.3.tar.gz

---
+++ packages/kamojipy/kamojipy-1.0.3.tar.gz/kamojipy-1.0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='kamojipy',
+      version='1.0.3',
+      description='Add kamoji to your python file github.com/SafyreLyons/kamojipy',
+      url='https://github.com/SafyreLyons/kamojipy',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author='Safyre Lyons',
+      author_email='lyons.safyre@gmail.com',
+      license='MIT',
+      py_modules=['kamojipy'],
+      package_dir={'': 'src'},
+      zip_safe=False)

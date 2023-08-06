5f101c98dc876433b3cc11984f774a1d4590c1a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:06:28 +0000

    Add package vkapi/1.1/vkapi-1.1.tar.gz

---
+++ packages/vkapi/vkapi-1.1.tar.gz/vkapi-1.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="vkapi",
+    version="1.1",
+    author="VerZsuT",
+    author_email="sacha3032104@gmail.com",
+    description="Python library for working with the vk.com API.",
+    install_requires="requests",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/VerZsuT/vkapi",
+    packages=['vkapi', 'vkapi/modules'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

7074fb91a10fde6c25c904717731f26002ccd073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:56 +0000

    Add package textacy/0.8.0/textacy-0.8.0-py2.py3-none-any.whl

---
+++ packages/textacy/textacy-0.8.0-py2.py3-none-any.whl/textacy/__init__.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import logging
+
+from textacy.about import __version__
+from textacy.constants import DEFAULT_DATA_DIR
+from textacy.cache import load_spacy_lang
+from textacy.doc import make_spacy_doc
+from textacy.corpus import Corpus
+from textacy.text_stats import TextStats
+from textacy.spacier.doc_extensions import set_doc_extensions
+# keep these out of the main namespace
+# they're somewhat niche, and slow to import bc of heavy dependencies
+# from textacy.tm import TopicModel
+# from textacy.vsm import Vectorizer
+
+set_doc_extensions()
+
+logger = logging.getLogger("textacy")
+# ensure reload() doesn't add another handler
+if len(logger.handlers) == 0:
+    logger.addHandler(logging.NullHandler())

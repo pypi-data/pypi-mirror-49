0132c3d2183549367334c51161f4ed63ae5d9a97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:39 +0000

    Add package gyte/1.0.0/gyte-1.0.0.zip

---
+++ packages/gyte/gyte-1.0.0.zip/gyte-1.0.0/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 27 09:59:41 2019
+
+@author: guoyi
+"""
+
+from setuptools import setup
+
+def readme_file():
+    with open("README.rst",encoding="utf-8") as rf:
+        return rf.read()
+
+setup(name="gyte",version="1.0.0",description="这是一个测试库！",packages=["gyte"],py_modules=["Tool"],author="GuoYi",author_eamil="guoyigoo@gmail.com",long_description="readme_file()",url="https://github.com/")

05bef3033fe3c5a2208d39918cc78059ab620680
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:33 +0000

    Add package pytorchfi/0.1.9/pytorchfi-0.1.9.tar.gz

---
+++ packages/pytorchfi/pytorchfi-0.1.9.tar.gz/pytorchfi-0.1.9/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pytorchfi",
+    version="0.1.9",
+    author="UIUC RSim",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    project_urls={
+        "Documentation": "https://pytorchfi.github.io/docs/",
+        "Source Code": "https://github.com/pytorchfi/pytorchfi"
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ]
+)

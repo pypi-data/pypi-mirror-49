4fd89d6aca1e3e47ab7eb1149d03f74aa7b65dff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:36 +0000

    Add package galibrate/0.4.0/galibrate-0.4.0.tar.gz

---
+++ packages/galibrate/galibrate-0.4.0.tar.gz/galibrate-0.4.0/setup_pypi.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="galibrate",
+    version="0.4.0",
+    author="Blake A. Wilson",
+    author_email="blakeaw1102@gmail.com",
+    description="Python toolkit for continuous Genetic Algorithm optimization.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/blakeaw/GAlibrate",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

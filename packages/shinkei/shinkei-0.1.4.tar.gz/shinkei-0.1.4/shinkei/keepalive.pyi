8cd99ee10a552a06f5b23cf5f476aab4bc7b818f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:49 +0000

    Add package shinkei/0.1.4/shinkei-0.1.4.tar.gz

---
+++ packages/shinkei/shinkei-0.1.4.tar.gz/shinkei-0.1.4/shinkei/keepalive.pyi
@@ -0,0 +1,20 @@
+import threading
+
+from .gateway import WSClient
+
+class KeepAlivePls(threading.Thread):
+    # noinspection PyMissingConstructor
+    def __init__(self, *args, ws: WSClient, **kwargs) -> None: ...
+
+    ws: WSClient
+    interval: float
+    stop_event: threading.Event
+    _last_ack: float
+    _last_send: float
+    latency: float
+
+    def run(self) -> None: ...
+
+    def stop(self) -> None: ...
+
+    def ack(self) -> None: ...

d6fb29dfc633daf312638addd041db4c35d4fe64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:02 +0000

    Add package bfit/2.7.4/bfit-2.7.4.tar.gz

---
+++ packages/bfit/bfit-2.7.4.tar.gz/bfit-2.7.4/bfit/backend/raise_window.py
@@ -0,0 +1,12 @@
+# move the working plot to the top and focus on it
+# Derek Fujimoto
+# July 2019 
+
+import matplotlib.pyplot as plt
+
+# bring window to front
+def raise_window():
+    wm = plt.get_current_fig_manager()    
+    wm.window.attributes('-topmost', True)
+    wm.window.attributes('-topmost', False)
+    wm.canvas.get_tk_widget().focus_force()

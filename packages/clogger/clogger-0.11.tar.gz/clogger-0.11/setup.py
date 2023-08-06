d0798da18ea0270fed54b4d515e8aa73b75a5448
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:00 +0000

    Add package clogger/0.11/clogger-0.11.tar.gz

---
+++ packages/clogger/clogger-0.11.tar.gz/clogger-0.11/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+      name='clogger',
+      version='0.11',
+      description='My way python logger configuration',
+      url='https://github.com/pietrogiuffrida/customlogger/',
+      author='Pietro Giuffrida',
+      author_email='pietro.giuffri@gmail.com',
+      license='MIT',
+      packages=['clogger'],
+      zip_safe=False,
+      install_requires=[],
+)

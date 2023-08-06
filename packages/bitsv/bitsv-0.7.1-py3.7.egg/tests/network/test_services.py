8ce205c17938358e42b4c96731ca91b62f812992
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:06 +0000

    Add package bitsv/0.7.1/bitsv-0.7.1-py3.7.egg

---
+++ packages/bitsv/bitsv-0.7.1-py3.7.egg/tests/network/test_services.py
@@ -0,0 +1,75 @@
+import pytest
+
+import bitsv
+from bitsv.network.services import (NetworkAPI, set_service_timeout)
+from tests.utils import (
+    catch_errors_raise_warnings, decorate_methods, raise_connection_error
+)
+
+MAIN_ADDRESS_USED1 = '1L2JsXHPMYuAa9ugvHGLwkdstCPUDemNCf'
+MAIN_ADDRESS_USED2 = '17SkEw2md5avVNyYgj6RiXuQKNwkXaxFyQ'
+MAIN_ADDRESS_UNUSED = '1DvnoW4vsXA1H9KDgNiMqY7iNkzC187ve1'
+TEST_ADDRESS_USED1 = 'n2eMqTT929pb1RDNuqEnxdaLau1rxy3efi'
+TEST_ADDRESS_USED2 = 'mmvP3mTe53qxHdPqXEvdu8WdC7GfQ2vmx5'
+TEST_ADDRESS_USED3 = 'mpnrLMH4m4e6dS8Go84P1r2hWwTiFTXmtW'
+TEST_ADDRESS_UNUSED = 'mp1xDKvvZ4yd8h9mLC4P76syUirmxpXhuk'
+
+
+def all_items_common(seq):
+    initial_set = set(seq[0])
+    intersection_lengths = [len(set(s) & initial_set) for s in seq]
+    return all_items_equal(intersection_lengths)
+
+
+def all_items_equal(seq):
+    initial_item = seq[0]
+    return all(item == initial_item for item in seq if item is not None)
+
+
+def test_set_service_timeout():
+    original = bitsv.network.services.DEFAULT_TIMEOUT
+    set_service_timeout(3)
+    updated = bitsv.network.services.DEFAULT_TIMEOUT
+
+    assert original != updated
+    assert updated == 3
+
+    set_service_timeout(original)
+
+
+class MockBackend(NetworkAPI):
+    IGNORED_ERRORS = NetworkAPI.IGNORED_ERRORS
+    GET_BALANCE_MAIN = [raise_connection_error]
+    GET_TRANSACTIONS_MAIN = [raise_connection_error]
+    GET_UNSPENT_MAIN = [raise_connection_error]
+    GET_BALANCE_TEST = [raise_connection_error]
+    GET_TRANSACTIONS_TEST = [raise_connection_error]
+    GET_UNSPENT_TEST = [raise_connection_error]
+
+
+class TestNetworkAPI:
+    def test_get_balance_main_equal(self):
+        results = [call(MAIN_ADDRESS_USED2) for call in NetworkAPI.GET_BALANCE_MAIN]
+        assert all(result == results[0] for result in results)
+
+    def test_get_balance_main_failure(self):
+        with pytest.raises(ConnectionError):
+            MockBackend.get_balance(MAIN_ADDRESS_USED2)
+
+    def test_get_transactions_main_equal(self):
+        """ FIXME this is only setup to list the transactions from BitIndex at this time. This will probably break
+        when adding in another api to NetworkAPI due to different return formats. Will fix it at that time."""
+        results = [call(MAIN_ADDRESS_USED1) for call in NetworkAPI.GET_TRANSACTIONS_MAIN]
+        assert all_items_common(results[:100])
+
+    def test_get_transactions_main_failure(self):
+        with pytest.raises(ConnectionError):
+            MockBackend.get_transactions(MAIN_ADDRESS_USED1)
+
+    def test_get_unspent_main_equal(self):
+        results = [call(MAIN_ADDRESS_USED2) for call in NetworkAPI.GET_UNSPENT_MAIN]
+        assert all_items_equal(results)
+
+    def test_get_unspent_main_failure(self):
+        with pytest.raises(ConnectionError):
+            MockBackend.get_unspent(MAIN_ADDRESS_USED1)

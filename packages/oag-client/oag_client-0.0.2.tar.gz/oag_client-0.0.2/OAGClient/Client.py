f5ca2e45e02432ba40cff078f36b84935a1a79f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:52 +0000

    Add package oag-client/0.0.2/oag_client-0.0.2.tar.gz

---
+++ packages/oag-client/oag_client-0.0.2.tar.gz/oag_client-0.0.2/OAGClient/Client.py
@@ -0,0 +1,23 @@
+import requests
+import xmltodict
+import json
+
+class Client:
+    def __init__(self, username, password):
+        self.URL = "http://xml.flightview.com/fvDemoConsOOOI/fvxml.exe?"
+        self.username = username
+        self.password = password
+
+    def make_request(self, oag_request_object):
+        username = "" if self.username == "" else "a={}".format(self.username)
+        password = "" if self.password == "" else "b={}".format(self.password)
+
+        request_string = "{}&{}&{}".format(username, password, oag_request_object)
+        response = requests.get(self.URL+request_string)
+        if response.status_code == 200:
+            json_response = json.dumps(xmltodict.parse(response.content))
+            return json_response
+        else:
+            return {"error": response.status_code, "content": response.content}
+
+

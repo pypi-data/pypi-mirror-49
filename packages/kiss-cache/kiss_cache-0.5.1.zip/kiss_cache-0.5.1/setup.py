48b6f5e90087b35e69aa6fe6e58aa1c9d678bc86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:31 +0000

    Add package kiss-cache/0.5.1/kiss_cache-0.5.1.zip

---
+++ packages/kiss-cache/kiss_cache-0.5.1.zip/kiss_cache-0.5.1/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name='kiss_cache',
+    packages=['kiss_cache'],
+    version='0.5.1',
+    license='MIT',
+    description='"Keep It Simple Stupid" Cache',
+    author='Bergutov Ruslan',
+    author_email='ruslanbergutov@gmail.com',
+    url='https://github.com/HiveTraum/kiss_cache',
+    download_url='https://github.com/HiveTraum/kiss_cache/archive/0.5.tar.gz',
+    keywords=['cache', 'memoize', 'kiss'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)

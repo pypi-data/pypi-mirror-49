8ea8c4308eee8f9c50303ecbab9963c9b21ce502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:12 +0000

    Add package autopandas/0.2.5/autopandas-0.2.5-py3-none-any.whl

---
+++ packages/autopandas/autopandas-0.2.5-py3-none-any.whl/autopandas/generators/copycat.py
@@ -0,0 +1,16 @@
+# Example generator. Copycat simply copy training data.
+
+class Copycat():
+    """ Baseline generator: simply copy training data.
+    """
+    def __init__(self):
+        self.data = None
+
+    def fit(self, data):
+        self.data = data
+
+    def sample(self, n=1, replace=False):
+        if self.data is None:
+            raise Exception('You firstly need to train the Copycat before sampling. Please use fit method.')
+        else:
+            return self.data.sample(n, replace=replace)

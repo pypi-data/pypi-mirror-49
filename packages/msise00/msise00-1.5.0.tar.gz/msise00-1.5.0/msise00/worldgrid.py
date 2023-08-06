c0213eed34d71e2b5075c7e829617a7b9f777b7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:38 +0000

    Add package msise00/1.5.0/msise00-1.5.0.tar.gz

---
+++ packages/msise00/msise00-1.5.0.tar.gz/msise00-1.5.0/msise00/worldgrid.py
@@ -0,0 +1,11 @@
+import numpy as np
+from typing import Tuple, Union
+
+
+def latlonworldgrid(latstep: Union[float, int] = 5,
+                    lonstep: Union[float, int] = 5) -> Tuple[np.ndarray, np.ndarray]:
+    lat = np.arange(-90., 90+latstep, latstep)
+    lon = np.arange(-180., 180+lonstep, lonstep)
+    glon, glat = np.meshgrid(lon, lat)
+
+    return glat, glon

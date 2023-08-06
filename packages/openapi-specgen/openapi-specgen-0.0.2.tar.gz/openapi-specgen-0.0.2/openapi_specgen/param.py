e7fc0f780d01f95aeb691980991493961782490f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:20 +0000

    Add package openapi-specgen/0.0.2/openapi-specgen-0.0.2.tar.gz

---
+++ packages/openapi-specgen/openapi-specgen-0.0.2.tar.gz/openapi-specgen-0.0.2/openapi_specgen/param.py
@@ -0,0 +1,34 @@
+from .schema import get_openapi_schema
+
+
+class OpenApiParam():
+
+    def __init__(self,
+                 name: str,
+                 location: str,
+                 data_type: type = None,
+                 default=None,
+                 required: bool = True):
+        self.name = name
+        self.location = location
+        self.data_type = data_type
+        self.default = default
+        self.required = required
+
+    def as_dict(self):
+        openapi_dict = {
+            'required': self.required,
+            'name': self.name,
+            'in': self.location
+        }
+
+        schema = {}
+
+        if self.data_type is not None:
+            schema = get_openapi_schema(self.data_type)
+        if self.default is not None:
+            schema['default'] = self.default
+
+        schema['title'] = self.name.title()
+        openapi_dict['schema'] = schema
+        return openapi_dict

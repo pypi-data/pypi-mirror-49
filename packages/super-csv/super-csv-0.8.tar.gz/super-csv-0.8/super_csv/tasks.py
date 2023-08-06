cebf654089a625390f85f8b5f06bfca6570327f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:28 +0000

    Add package super-csv/0.8/super-csv-0.8.tar.gz

---
+++ packages/super-csv/super-csv-0.8.tar.gz/super-csv-0.8/super_csv/tasks.py
@@ -0,0 +1,7 @@
+"""
+Tasks for async processing of csv files.
+"""
+from __future__ import absolute_import, unicode_literals
+
+# pylint: disable=unused-import
+from .mixins import do_deferred_commit

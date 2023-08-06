badf614f3e45c9f2ac9a90ea863d007cdc33b9c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:57 +0000

    Add package teste-pypip/0.17.0/teste_pypip-0.17.0.tar.gz

---
+++ packages/teste-pypip/teste_pypip-0.17.0.tar.gz/teste_pypip-0.17.0/setup.py
@@ -0,0 +1,20 @@
+﻿# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+   	name='teste_pypip',
+    	version='0.17.0',
+    	license='MIT License',
+    	author='Thiago Rocha Ferreira',
+    	author_email='thiagorocha503@gmail.com',
+    	keywords='teste_pypip',
+    	description='teste de pacote python',
+ 	long_description = 'Um teste de upload de pacote python',
+    	packages=['calc'],
+    	install_requires=[],
+ 	classifiers = [
+ 		'Natural Language :: Portuguese (Brazilian)',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python :: 3.8'
+	]  
+)

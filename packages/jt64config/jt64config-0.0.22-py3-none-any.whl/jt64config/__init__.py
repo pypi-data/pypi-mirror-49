80d9c2f402b546eda7f8f2b2dda21e45d66eed72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:48 +0000

    Add package jt64config/0.0.22/jt64config-0.0.22-py3-none-any.whl

---
+++ packages/jt64config/jt64config-0.0.22-py3-none-any.whl/jt64config/__init__.py
@@ -0,0 +1,8 @@
+version_info = (0, 0, 22)
+__title__ = "jt64config"
+__version__ = '.'.join(str(c) for c in version_info)
+__name__ = "jt64config"
+__author__ = "Greg Beam, KI7MT"
+__license__ = "GPLv3"
+__email__ = "ki7mt@yahoo.com"
+__summary__ = "JTSDK64 Tools Configuration"

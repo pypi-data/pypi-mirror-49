d322ccd7489d85c57283dc98ca20851d8e0cc342
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:03 +0000

    Add package alphaj-naver-stock-cralwer/0.1.2/alphaj_naver_stock_cralwer-0.1.2.tar.gz

---
+++ packages/alphaj-naver-stock-cralwer/alphaj_naver_stock_cralwer-0.1.2.tar.gz/alphaj_naver_stock_cralwer-0.1.2/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+     name='alphaj_naver_stock_cralwer',
+     version='0.1.2',
+     author="Alpha J",
+     author_email="jaeminyx@gmail.com",
+     description="A Naver Stock Cralwer",
+     long_description="A Naver Stock Cralwer",
+     long_description_content_type="text/markdown",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )

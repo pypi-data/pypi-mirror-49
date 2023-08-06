a92f429888673e889cf461a801ffe5bc25638360
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:07 +0000

    Add package ipa2/0.7.0/ipa2-0.7.0.tar.gz

---
+++ packages/ipa2/ipa2-0.7.0.tar.gz/ipa2-0.7.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ipa2',
+    version='0.7.0',
+    package_dir={'ipa2': 'ipa2'},
+    package_data={'ipa2': ['data/*']},
+    description='Tools for convert Text to IPA in python',
+    long_description="Github : https://github.com/voidful/ipa2",
+    url='https://github.com/voidful/ipa2',
+    author='voidful',
+    author_email='voidful.stack@gmail.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='nlp file io string text mining ipa voice',
+    zip_safe=True,
+    packages=find_packages()
+)

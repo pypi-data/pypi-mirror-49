818324e09a8deb5937392b4828cdad4f33ddddee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:06 +0000

    Add package pymars/0.1.5/pymars-0.1.5.tar.gz

---
+++ packages/pymars/pymars-0.1.5.tar.gz/pymars-0.1.5/mars/tensor/expressions/arithmetic/setreal.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright 1999-2018 Alibaba Group Holding Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .... import operands
+from .core import TensorBinOp, TensorConstant
+from .utils import arithmetic_operand
+
+
+@arithmetic_operand(sparse_mode='binary_and')
+class TensorSetReal(operands.SetReal, TensorBinOp):
+    @classmethod
+    def constant_cls(cls):
+        return TensorSetRealConstant
+
+
+@arithmetic_operand(sparse_mode='binary_and_const')
+class TensorSetRealConstant(operands.SetRealConstant, TensorConstant):
+    pass
+
+
+def set_real(val, real):
+    op = TensorSetReal(dtype=val.dtype)
+    return op(val, real)

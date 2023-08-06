d5679b4a9168c1edf7490db05ba28a23fb5d648f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:19 +0000

    Add package nda-tools/0.1.20/nda_tools-0.1.20.tar.gz

---
+++ packages/nda-tools/nda_tools-0.1.20.tar.gz/nda_tools-0.1.20/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+import NDATools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+        name='nda_tools',
+        description="NIMH Data Archive Python Client",
+        install_requires=['boto3', 'botocore', 'tqdm','requests'],
+        version= NDATools.__version__,
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        author='NDA',
+        author_email='NDAHelp@mail.nih.gov',
+        url="https://github.com/NDAR/nda-tools/tree/master/NDATools",
+        license='MIT',
+        packages=find_packages(),
+        include_package_data=True,
+        data_files=[('config', ['NDATools/clientscripts/config/settings.cfg'])],
+        entry_points={
+            'console_scripts': [
+                'vtcmd = NDATools.clientscripts.vtcmd:main',
+                'downloadcmd = NDATools.clientscripts.downloadcmd:main']
+        }
+    )

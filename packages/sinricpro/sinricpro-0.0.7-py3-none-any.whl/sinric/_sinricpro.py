5f394e1816e5e118bb660f8a933b3807328647a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:16 +0000

    Add package sinricpro/0.0.7/sinricpro-0.0.7-py3-none-any.whl

---
+++ packages/sinricpro/sinricpro-0.0.7-py3-none-any.whl/sinric/_sinricpro.py
@@ -0,0 +1,37 @@
+import asyncio
+from sinric._sinricprosocket import SinricProSocket
+from threading import Thread
+from sinric._events import Events
+
+
+class SinricPro:
+    def __init__(self, api, deviceid, request_callbacks,event_callbacks, enable_trace=False):
+        self.apiKey = api
+        self.deviceid = deviceid
+        self.request_callbacks = request_callbacks
+        self.socket = SinricProSocket(self.apiKey, self.deviceid, self.request_callbacks, enable_trace)
+        self.connection = asyncio.get_event_loop().run_until_complete(self.socket.connect())
+        self.event_callbacks = event_callbacks
+        self.event_handler = Events(self.connection)
+
+    def handle(self):
+        tasks = [
+            asyncio.ensure_future(self.socket.receiveMessage(self.connection)),
+        ]
+
+        asyncio.get_event_loop().run_until_complete(asyncio.wait(tasks))
+
+    def handle_clients(self, handle, udp_client):
+        asyncio.new_event_loop().run_until_complete(handle(udp_client))
+
+    def handle_all(self, udp_client):
+        t1 = Thread(target=self.handle_clients, args=(self.socket.handle, udp_client))
+        t2 = Thread(target=udp_client.listen)
+        t3 = Thread(target=self.event_callbacks['door_bell_event'])
+        t3.setDaemon(True)
+        t1.setDaemon(True)
+        t2.setDaemon(True)
+        t1.start()
+        t2.start()
+        t3.start()
+        self.handle()

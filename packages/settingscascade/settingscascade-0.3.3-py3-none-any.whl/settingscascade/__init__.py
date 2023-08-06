58d9b8768670afed3e36db3463363cf8cfe578ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:36 +0000

    Add package settingscascade/0.3.3/settingscascade-0.3.3-py3-none-any.whl

---
+++ packages/settingscascade/settingscascade-0.3.3-py3-none-any.whl/settingscascade/__init__.py
@@ -0,0 +1,14 @@
+from settingscascade.manager import SettingsManager
+from settingscascade.schema import ElementSchema
+
+__all__ = ("SettingsManager", "ElementSchema")
+
+try:  # pragma: no cover
+    import pkg_resources
+
+    try:
+        __version__ = pkg_resources.get_distribution("settingscascade").version
+    except pkg_resources.DistributionNotFound:
+        __version__ = "0.0.0"
+except (ImportError, AttributeError):  # pragma: no cover
+    __version__ = "0.0.0"

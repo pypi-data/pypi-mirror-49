780a2caaee8f4fad1466ca1193997fd0cf473da3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:38 +0000

    Add package bos-sync/0.4.2/bos_sync-0.4.2-py3-none-any.whl

---
+++ packages/bos-sync/bos_sync-0.4.2-py3-none-any.whl/bookied_sync/bettingmarket.py
@@ -0,0 +1,164 @@
+from .lookup import Lookup
+from peerplays.bettingmarket import BettingMarket, BettingMarkets
+from . import comparators
+from .exceptions import CannotCreateWithParentInProposal
+from . import log
+
+class LookupBettingMarket(Lookup, dict):
+    """ Lookup Class for Betting Market
+
+        :param dict description: list of internationalized descriptions
+        :param LookupBettingMarketGroup bmg: Parent element
+        :param dict extra_data: Optionally provide additional data that is
+               stored in the same dictionary
+
+    """
+
+    operation_update = "betting_market_update"
+    operation_create = "betting_market_create"
+
+    def __init__(self, description, bmg, extra_data={}):
+        Lookup.__init__(self)
+        self.identifier = "{}/{}".format(bmg["description"]["en"], description["en"])
+        self.bmg = bmg
+        self.parent = bmg
+        dict.__init__(self, extra_data)
+        dict.update(self, {"description": description})
+
+    @property
+    def event(self):
+        """ Return parent Event
+        """
+        return self.parent.event
+
+    @property
+    def group(self):
+        """ Return parent BMG
+        """
+        return self.parent
+
+    def test_operation_equal(self, bm, **kwargs):
+        """ This method checks if an object or operation on the blockchain
+            has the same content as an object in the  lookup
+        """
+        test_operation_equal_search = kwargs.get(
+            "test_operation_equal_search",
+            [
+                comparators.cmp_required_keys(
+                    ["new_group_id", "new_description", "betting_market_id"],
+                    ["group_id", "description", "betting_market_id"],
+                ),
+                comparators.cmp_status(),
+                comparators.cmp_group(),
+                comparators.cmp_asset(),
+                comparators.cmp_all_description(),
+            ],
+        )
+
+        """ We need to properly deal with the fact that betting markets
+            cannot be distinguished alone from the payload if they are bundled
+            in a proposal and refer to betting_market_group_id 0.0.x
+        """
+        group_id = bm.get("group_id", bm.get("new_group_id"))
+        test_group = self.valid_object_id(group_id)
+        if group_id and not test_group and group_id[0] == "0" and "proposal" in kwargs:
+            full_proposal = kwargs.get("proposal")
+            if full_proposal:
+                operation_id = int(group_id.split(".")[2])
+                parent_op = dict(full_proposal)["proposed_transaction"]["operations"][
+                    operation_id
+                ]
+                if not self.parent.test_operation_equal(
+                    parent_op[1], proposal=full_proposal
+                ):
+                    return False
+
+        if all(
+            [
+                # compare by using 'all' the funcs in find_id_search
+                func(self, bm)
+                for func in test_operation_equal_search
+            ]
+        ):
+            return True
+        return False
+
+    def find_id(self, **kwargs):
+        """ Try to find an id for the object of the  lookup on the
+            blockchain
+
+            .. note:: This only checks if a sport exists with the
+                        same description in **ENGLISH**!
+        """
+        # In case the parent is a proposal, we won't
+        # be able to find an id for a child
+        parent_id = self.parent.get_id(skip_proposals=False)
+        if not self.valid_object_id(parent_id):
+            return
+
+        bms = BettingMarkets(self.parent_id, peerplays_instance=self.peerplays)
+
+        find_id_search = kwargs.get(
+            "find_id_search",
+            [
+                # We compare only the 'eng' content by default
+                comparators.cmp_description("en"),
+                comparators.cmp_asset()
+            ],
+        )
+
+        for bm in bms:
+            if all(
+                [
+                    # compare by using 'all' the funcs in find_id_search
+                    func(self, bm)
+                    for func in find_id_search
+                ]
+            ):
+                return bm["id"]
+
+    def is_synced(self):
+        """ Test if data on chain matches lookup
+        """
+        if "id" in self and self["id"]:
+            bmg = BettingMarket(self["id"])
+            if self.test_operation_equal(bmg):
+                return True
+        return False
+
+    def propose_new(self):
+        """ Propose operation to create this object
+        """
+        if self.parent_id[:5] == "1.10.":
+            raise CannotCreateWithParentInProposal(
+                "Cannot propose with parent pending in proposal"
+            )
+        return self.peerplays.betting_market_create(
+            description=self.description,
+            payout_condition=[],
+            group_id=self.parent_id,
+            account=self.proposing_account,
+            append_to=Lookup.proposal_buffer,
+        )
+
+    def propose_update(self):
+        """ Propose to update this object to match  lookup
+        """
+        return self.peerplays.betting_market_update(
+            self.id,
+            payout_condition=[],
+            description=self.description,
+            group_id=self.parent_id,
+            account=self.proposing_account,
+            append_to=Lookup.proposal_buffer,
+        )
+
+    @property
+    def description(self):
+        """ This method ensures that the description has the proper format as
+            well as the proper string replacements for teams
+        """
+        return [
+            [k, v.format(**self)]  # replace variables
+            for k, v in self["description"].items()
+        ]
---
+++ packages/bos-sync/bos_sync-0.4.2-py3-none-any.whl/bookied_sync/bettingmarketgroupresolve.py
@@ -0,0 +1,205 @@
+from .lookup import Lookup
+from .rule import LookupRules
+from peerplays.bettingmarketgroup import BettingMarketGroup
+from bookied_sync.utils import dList2Dict
+from peerplays.rule import Rule
+from .substitutions import substitute_metric
+
+
+class LookupBettingMarketGroupResolve(Lookup, dict):
+    """ Lookup Class for Resolving BettingMarketGroups
+
+        .. note:: If ``result`` is a dictionary, then first element is
+            ``homeTeam`` and second is ``awayTeam``.
+    """
+
+    operation_update = None
+    operation_create = "betting_market_group_resolve"
+
+    def __init__(self, bmg, result, handicaps=None, overunder=None, extra_data={}):
+        Lookup.__init__(self)
+        self.identifier = "{}::resolution".format(dList2Dict(bmg.description)["en"])
+        self.parent = bmg
+        dict.__init__(self, extra_data)
+        dict.update(self, bmg)
+
+        assert (
+            isinstance(result, list) and len(result) == 2
+        ), "Result must be a list of length 2."
+        handicaps = handicaps or [0, 0]
+        overunder = overunder or 0
+        dict.update(self, dict(result=result, handicaps=handicaps, overunder=overunder))
+
+        # We here direct the handicaps and overunders through BettingMarket
+        # Group of the parent and load it from there again to allow
+        # modifications according to set_*()
+        if overunder:
+            self.parent.set_overunder(overunder)
+            self["overunder"] = self.parent["overunder"]
+
+        if any(handicaps):
+            self.parent.set_handicaps(home=handicaps[0], away=handicaps[1])
+            self["handicaps"] = self.parent["handicaps"]
+
+    @property
+    def overunder(self):
+        return self.parent["overunder"]
+
+    @property
+    def handicaps(self):
+        return self.parent["handicaps"]
+
+    @property
+    def bmg(self):
+        """ The BMG is the parent
+        """
+        return self.parent
+
+    @property
+    def markets(self):
+        """ The BMG is the parent
+        """
+        return self.parent.bettingmarkets
+
+    @property
+    def sport(self):
+        """ Return the sport for this BMG
+        """
+        return self.parent.sport
+
+    @property
+    def rules(self):
+        """ Return instance of LookupRules for this BMG
+        """
+        assert self["rules"] in self.sport["rules"]
+        return LookupRules(self.sport["identifier"], self["rules"])
+
+    @property
+    def grading(self):
+        # We take the actual grading from the blockchain and not from
+        # the lookup!!
+        rule = Rule(self.rules.id, peerplays_instance=self.peerplays)
+        return rule.grading
+
+    @property
+    def _metric(self):
+        return substitute_metric(
+            self.grading.get("metric", ""),
+            result=self["result"],
+            handicaps=self["handicaps"],
+            overunder=self["overunder"],
+            handicap_allow_float=self.parent.allow_float,
+        )
+
+    def _equation(self, eq):
+        return substitute_metric(
+            eq,
+            metric=self.metric,
+            result=self["result"],
+            handicaps=self["handicaps"],
+            overunder=self["overunder"],
+            handicap_allow_float=self.parent.allow_float,
+        )
+
+    @property
+    def metric(self):
+        s = self._metric
+        if not isinstance(s, str):
+            raise ValueError("metric must be string, was {}".format(type(s)))
+        try:
+            metric = eval(s)
+        except Exception:
+            raise Exception("Cannot evaluate metric '{}'".format(s))
+        return metric
+
+    def evaluate_metric(self, equation):
+        # Define variables we want to use when grading
+        if not isinstance(equation, str):
+            raise ValueError("equation must be string, was {}".format(type(equation)))
+        equation = self._equation(equation)
+        try:
+            metric = eval(equation)
+        except Exception:
+            raise Exception("Cannot evaluate metric '{}'".format(equation))
+        return metric
+
+    @property
+    def resolutions(self):
+        """ This property constructs the resultions array to be used in the
+            transactions. It takes the following form
+
+            .. code-block:: js
+
+                [
+                    ["1.21.257", "win"],
+                    ["1.21.258", "not_win"],
+                    ["1.21.259", "cancel"],
+                ]
+
+        """
+        bettingmarkets = self.markets
+        ret = []
+        for market in self.grading.get("resolutions", []):
+            bettingmarket = next(bettingmarkets)
+
+            resolved = {
+                key: self.evaluate_metric(equation) for key, equation in market.items()
+            }
+            # The resolved dictionary looks like this
+            # {'win': False, 'not_win': True, 'void': False}
+            # we now need to ensure that only one of those options is 'true'
+            assert (
+                sum(resolved.values()) == 1
+            ), "Multiple or no options resolved to 'True': {}".format(str(resolved))
+
+            ret.extend(
+                [[bettingmarket.id, key] for key, value in resolved.items() if value]
+            )
+
+        return ret
+
+    def test_operation_equal(self, resolve, **kwargs):
+        """ This method checks if an object or operation on the blockchain
+            has the same content as an object in the  lookup
+        """
+
+        lookupresults = self.resolutions
+        chainsresults = resolve["resolutions"]
+        bmg_id = resolve["betting_market_group_id"]
+
+        # Test if BMG exists
+        test_bmg = self.valid_object_id(bmg_id, BettingMarketGroup)
+
+        if (
+            all([a in chainsresults for a in lookupresults])
+            and all([b in lookupresults for b in chainsresults])
+            and (not test_bmg or bmg_id == self.parent_id)
+        ):
+            return True
+        return False
+
+    def find_id(self):
+        """ Market resolve operations do not have their own ids
+        """
+        pass
+
+    def is_synced(self):
+        """ Here, we need to figure out if the market has already been resolved
+        """
+        # FIXME  / TODO
+        return False
+
+    def propose_new(self):
+        """ This call proposes the resolution of the betting market group
+        """
+        return self.peerplays.betting_market_resolve(
+            self.parent_id,
+            self.resolutions,
+            account=self.proposing_account,
+            append_to=Lookup.proposal_buffer,
+        )
+
+    def propose_update(self):
+        """ There is no such thing as an updated resolution
+        """
+        pass

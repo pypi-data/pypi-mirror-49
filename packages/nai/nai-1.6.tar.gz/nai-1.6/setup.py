6d91a6b5f1a644304eca90c0f29eeb78c6bdcd23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:29 +0000

    Add package nai/1.6/nai-1.6.tar.gz

---
+++ packages/nai/nai-1.6.tar.gz/nai-1.6/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+name='nai',
+version='1.6',
+author='Nasir Ali',
+author_email='nasiralis1731@gmail.com',
+author_url='https://facebook.com/nasir.xo',
+packages=['nai'],
+scripts=['bin/phone'],
+install_requires=['bs4','requests', ],
+)

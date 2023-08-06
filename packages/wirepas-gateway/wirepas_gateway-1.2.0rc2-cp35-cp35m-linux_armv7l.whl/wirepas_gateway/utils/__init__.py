7d8c4d356cdd4ae51a82653fa80fd18a24430387
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:26 +0000

    Add package wirepas-gateway/1.2.0rc2/wirepas_gateway-1.2.0rc2-cp35-cp35m-linux_armv7l.whl

---
+++ packages/wirepas-gateway/wirepas_gateway-1.2.0rc2-cp35-cp35m-linux_armv7l.whl/wirepas_gateway/utils/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2019 Wirepas Ltd licensed under Apache License, Version 2.0
+#
+# See file LICENSE for full license details.
+
+from .log_tools import *
+from .serialization_tools import *
+from .argument_tools import *

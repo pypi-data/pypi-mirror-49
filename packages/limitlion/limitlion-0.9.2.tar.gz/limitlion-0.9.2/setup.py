e6cccde9efeb925c70ed2ce224d990fa2c1f4645
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:03 +0000

    Add package limitlion/0.9.2/limitlion-0.9.2.tar.gz

---
+++ packages/limitlion/limitlion-0.9.2.tar.gz/limitlion-0.9.2/setup.py
@@ -0,0 +1,40 @@
+"""LimitLion setup."""
+
+from setuptools import setup
+
+install_requires = [
+    'redis==2.10.6'
+]
+
+tests_require = install_requires + [
+    'pytest', 'pytest-cov'
+]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='limitlion',
+    version='0.9.2',
+    url='http://github.com/closeio/limitlion',
+    description='Close LimitLion',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    platforms='any',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    packages=[
+        'limitlion',
+    ],
+    package_data={'limitlion': ['*.lua']},
+    install_requires=install_requires,
+    tests_require=tests_require,
+)

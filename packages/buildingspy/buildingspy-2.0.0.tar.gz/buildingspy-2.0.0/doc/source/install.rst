986caf13e182e1bb143ac52bb0e139076c2cd7b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:46 +0000

    Add package buildingspy/2.0.0/buildingspy-2.0.0.tar.gz

---
+++ packages/buildingspy/buildingspy-2.0.0.tar.gz/buildingspy-2.0.0/doc/source/install.rst
@@ -0,0 +1,39 @@
+Install and Uninstall BuildingsPy
+=================================
+
+To install the *BuildingsPy* module, install
+`pip <https://pip.pypa.io/en/latest/>`_ and run from a console
+
+.. parsed-literal::
+
+   pip install buildingspy
+
+On Linux or Mac OS X, run
+
+.. parsed-literal::
+
+   sudo pip install buildingspy
+
+To uninstall, run
+
+.. parsed-literal::
+
+   pip uninstall buildingspy
+
+On Linux or Mac OS X, run
+
+.. parsed-literal::
+
+   sudo pip uninstall buildingspy
+
+To run regression tests with *BuildingsPy*,
+*PyTidyLib* needs to be installed. On Ubuntu, this can be done using
+
+.. parsed-literal::
+
+   sudo pip install pytidylib
+   sudo apt-get install tidy
+
+For other operating systems, see the instructions at the
+`PyTidyLib <https://pythonhosted.org/pytidylib/>`_
+site.
---
+++ packages/buildingspy/buildingspy-2.0.0.tar.gz/buildingspy-2.0.0/setup.py
@@ -0,0 +1,49 @@
+import os
+from setuptools import setup
+# Python setup file.
+# See http://packages.python.org/an_example_pypi_project/setuptools.html
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="buildingspy",
+    version="2.0.0",
+    author="Michael Wetter",
+    author_email="mwetter@lbl.gov",
+    description=("Package for simulating and testing models from the Modelica Buildings and IBPSA libraries"),
+    long_description=read('buildingspy/README.rst'),
+    license="3-clause BSD",
+    keywords="modelica dymola openmodelica mat",
+    url="http://simulationresearch.lbl.gov/modelica/",
+    install_requires=['future',
+                        'pytidylib',
+                        'jinja2',
+                        'gitpython'],
+    packages=['buildingspy',
+                'buildingspy/development',
+                'buildingspy/examples',
+                'buildingspy/fmi',
+                'buildingspy/io',
+                'buildingspy/simulate',
+                'buildingspy/thirdParty',
+                'buildingspy/thirdParty.dymat',
+                'buildingspy/thirdParty.dymat.DyMat',
+                'buildingspy/thirdParty.dymat.DyMat.Export'],
+    package_data={'': ['*.template']},
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Utilities"
+    ],
+)

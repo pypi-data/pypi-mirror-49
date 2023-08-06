8f580614e21aca675e0f48139f8f91ddfaa61e9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:27 +0000

    Add package ipyui/0.0.0/ipyui-0.0.0.tar.gz

---
+++ packages/ipyui/ipyui-0.0.0.tar.gz/ipyui-0.0.0/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(
+  name = 'ipyui',
+  packages = ['ipyui'],
+  version = '0.0.0',
+  license='',
+  description = 'Exploratory Data Analysis and Visualization',
+  author = 'Tobias Krabel, Florian Wetschoreck',
+  author_email = 'tobiaskrabel@gmail.com, florian.wetschoreck@gmail.com',
+  url = 'https://github.com/tkrabel/ipyui',
+  download_url = 'https://github.com/tkrabel/ipyui/archive/0.0.0.tar.gz',
+  keywords = ['data preparation', 'data exploration', 'data analysis', 'data transformation'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Programming Language :: Python :: 3.6',
+  ],
+)

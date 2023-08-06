23d243ee7c2bb092eb7e686cbce9c103f67c9736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:54 +0000

    Add package dbt-sqlserver/0.1.3/dbt-sqlserver-0.1.3.tar.gz

---
+++ packages/dbt-sqlserver/dbt-sqlserver-0.1.3.tar.gz/dbt-sqlserver-0.1.3/dbt/adapters/sqlserver/impl.py
@@ -0,0 +1,23 @@
+from dbt.adapters.sql import SQLAdapter
+from dbt.adapters.sqlserver import SQLServerConnectionManager
+
+
+class SQLServerAdapter(SQLAdapter):
+    ConnectionManager = SQLServerConnectionManager
+
+    @classmethod
+    def date_function(cls):
+        return 'getdate()'
+
+    @classmethod
+    def convert_text_type(cls, agate_table, col_idx):
+        column = agate_table.columns[col_idx]
+        lens = (len(d.encode("utf-8")) for d in column.values_without_nulls())
+        max_len = max(lens) if lens else 64
+        return "varchar({})".format(max_len)
+
+    @classmethod
+    def convert_datetime_type(cls, agate_table, col_idx):
+        return "datetime"
+
+

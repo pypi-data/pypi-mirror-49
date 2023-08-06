c8314868bae5bb1d3fcbacd6cf232d57793af091
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:30 +0000

    Add package flowws/0.1.0/flowws-0.1.0.tar.gz

---
+++ packages/flowws/flowws-0.1.0.tar.gz/flowws-0.1.0/README.md
@@ -0,0 +1,32 @@
+
+## Introduction
+
+`flowws` is an in-development framework for building modular, reusable
+task workflows. The core library contains tools to abstract over
+storage locations and parse arguments in a uniform way for both python
+scripts and command-line-based execution.
+
+`flowws` is being developed in conjunction with
+[hoomd-flowws](https://github.com/klarh/hoomd_flowws), which
+implements a set of flowws modules for use with
+[hoomd-blue](https://github.com/glotzerlab/hoomd-blue).
+
+## Installation
+
+Install flowws from source using `pip`:
+
+```
+pip install git+https://github.com/klarh/flowws.git#egg=flowws
+```
+
+## Documentation
+
+Browse more detailed documentation
+[online](https://flowws.readthedocs.io) or build the sphinx
+documentation from source:
+
+```
+git clone https://github.com/klarh/flowws
+cd flowws/doc
+make html
+```
---
+++ packages/flowws/flowws-0.1.0.tar.gz/flowws-0.1.0/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+
+with open('flowws/version.py') as version_file:
+    exec(version_file.read())
+
+setup(name='flowws',
+      author='Matthew Spellings',
+      author_email='matthew.p.spellings@gmail.com',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3',
+      ],
+      description='Library for development of stage-based scientific workflows',
+      entry_points={
+          'console_scripts': [
+              'flowws_run = flowws.run:main',
+              'flowws_freeze = flowws.freeze:main',
+          ],
+      },
+      extras_require={},
+      install_requires=[],
+      license='MIT',
+      packages=[
+          'flowws',
+      ],
+      python_requires='>=3',
+      version=__version__
+      )

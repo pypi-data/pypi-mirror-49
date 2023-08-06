9ea445da183a68c4d5ded81972e84359ccdafa94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:20 +0000

    Add package beautysh/5.0.1/beautysh-5.0.1.tar.gz

---
+++ packages/beautysh/beautysh-5.0.1.tar.gz/beautysh-5.0.1/tests/noformatter_test1_raw.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function mytest()
+{
+	echo "Commands in this part will be formatted and indented normally as usual"
+	
+	# @formatter:off
+	echo \
+	    --option1 \
+	        --option2 \
+	            --option3 \
+	# @formatter:on
+}
+
+# @formatter:off
+echo \
+    --option1 \
+        --option2 \
+            --option3 \
+# @formatter:on

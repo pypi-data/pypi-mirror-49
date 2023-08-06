7dc4e1b76d9bea50e719b8d4ee682eab667b88c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:19 +0000

    Add package Inter-IUT-3-Blackdoor/6.6.6/Inter%20IUT%203%20-%20Blackdoor-6.6.6.tar.gz

---
+++ packages/Inter-IUT-3-Blackdoor/Inter%20IUT%203%20-%20Blackdoor-6.6.6.tar.gz/Inter IUT 3 - Blackdoor-6.6.6/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="Inter IUT 3 - Blackdoor",
+	version="6.6.6",
+	packages=find_packages(), # permet de récupérer tout les fichiers
+	description="Just backdooring some computer ...",
+	url="https://0xswitch.fr",
+	author="switch",
+	license="WTFPL",
+	python_requires=">=3"
+	)

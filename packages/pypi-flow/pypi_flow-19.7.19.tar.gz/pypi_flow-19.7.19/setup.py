c9483095cb3e943a389819844da61ef528f35cc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:24 +0000

    Add package pypi-flow/19.7.19/pypi_flow-19.7.19.tar.gz

---
+++ packages/pypi-flow/pypi_flow-19.7.19.tar.gz/pypi_flow-19.7.19/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# files = ["packageTemplate/*"]
+import grtoolkit as grt 
+folder = grt.cwd() + '\\cmdline'
+cmdLineScripts = [f'cmdline\\{file}' for file in grt.File.filesInFolder(folder,"*")]
+
+setuptools.setup(
+    name="pypi_flow",
+    version="19.07.19",
+    author="Gabriel Rosales",
+    author_email="gabriel.alejandro.rosales@gmail.com",
+    description="Project Management tools for quickly creating and maintaining PYPI packages.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ZenosParadox/pypi_flow",
+    packages=setuptools.find_packages(),
+    # package_data = {'package':files},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['grtoolkit'],
+    include_package_data=True,
+    scripts=cmdLineScripts, #commandline scripts to include
+)

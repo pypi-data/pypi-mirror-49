9214d6d780079450577a92f3a395b602b8baf9e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:02 +0000

    Add package pyscaffoldext-custom-extension/0.6a1/pyscaffoldext_custom_extension-0.6a1-py2.py3-none-any.whl

---
+++ packages/pyscaffoldext-custom-extension/pyscaffoldext_custom_extension-0.6a1-py2.py3-none-any.whl/pyscaffoldext/custom_extension/templates/__init__.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+import string
+
+from pkg_resources import resource_string
+
+
+def get_template(name):
+    """Retrieve the template by name
+
+    Args:
+        name: name of template
+
+    Returns:
+        :obj:`string.Template`: template
+    """
+    file_name = "{name}.template".format(name=name)
+    data = resource_string("pyscaffoldext.custom_extension.templates",
+                           file_name)
+    return string.Template(data.decode("UTF-8"))
+
+
+def get_class_name_from_pkg_name(opts):
+    """Generate a class name from package name
+
+    Args:
+        opts (dict): given options, see :obj:`create_project` for
+            an extensive list.
+
+    Returns:
+        str: class name of extension
+    """
+    pkg_name = opts["package"]
+    return "".join(map(str.capitalize, pkg_name.split("_")))
+
+
+def extension(opts):
+    """Template of extension.py
+
+    Args:
+        opts: mapping parameters as dictionary
+
+    Returns:
+        str: file content as string
+    """
+    template = get_template("extension")
+    opts["extension_class_name"] = get_class_name_from_pkg_name(opts)
+    return template.safe_substitute(opts)
+
+
+def readme(opts):
+    """Template of README.rst
+
+    Args:
+        opts: mapping parameters as dictionary
+
+    Returns:
+        str: file content as string
+    """
+    template = get_template("readme")
+    return template.safe_substitute(opts)
+
+
+def test_custom_extension(opts):
+    """Template of test_custom_extension.py
+
+    Args:
+        opts: mapping parameters as dictionary
+
+    Returns:
+        str: file content as string
+    """
+    template = get_template("test_custom_extension")
+    return template.safe_substitute(opts)
+
+
+def conftest(opts):
+    """Template of improved conftest.py
+
+    Args:
+        opts: mapping parameters as dictionary
+
+    Returns:
+        str: file content as string
+    """
+    template = get_template("conftest")
+    return template.safe_substitute(opts)

2c98241c09fa884037db6f28844d5688e69d8637
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:18 +0000

    Add package segregation/1.1.1/segregation-1.1.1-py3-none-any.whl

---
+++ packages/segregation/segregation-1.1.1-py3-none-any.whl/segregation/tests/test_modified_dissimilarity.py
@@ -0,0 +1,18 @@
+import unittest
+import libpysal
+import geopandas as gpd
+import numpy as np
+from segregation.aspatial import ModifiedDissim
+
+
+class Modified_Dissim_Tester(unittest.TestCase):
+    def test_Modified_Dissim(self):
+        s_map = gpd.read_file(libpysal.examples.get_path("sacramentot2.shp"))
+        df = s_map[['geometry', 'HISP_', 'TOT_POP']]
+        np.random.seed(1234)
+        index = ModifiedDissim(df, 'HISP_', 'TOT_POP')
+        np.testing.assert_almost_equal(index.statistic, 0.31075891224250635)
+
+
+if __name__ == '__main__':
+    unittest.main()

def2b281c73781354d285024e00406dfeba8ff2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:18 +0000

    Add package pytorch-lightning/0.2.5.1/pytorch-lightning-0.2.5.1.tar.gz

---
+++ packages/pytorch-lightning/pytorch-lightning-0.2.5.1.tar.gz/pytorch-lightning-0.2.5.1/pytorch_lightning/root_module/hooks.py
@@ -0,0 +1,24 @@
+import torch
+
+class ModelHooks(torch.nn.Module):
+    def on_batch_start(self, data_batch):
+        pass
+
+    def on_batch_end(self):
+        pass
+
+    def on_epoch_start(self):
+        pass
+
+    def on_epoch_end(self):
+        pass
+
+    def on_pre_performance_check(self):
+        pass
+
+    def on_post_performance_check(self):
+        pass
+
+    def on_tng_metrics(self, metrics):
+        pass
+

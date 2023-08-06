a9615a958ebad27a4dee8d4cb7bdba454f5c1203
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:36 +0000

    Add package pycopy-ast/0.4/pycopy-ast-0.4.tar.gz

---
+++ packages/pycopy-ast/pycopy-ast-0.4.tar.gz/pycopy-ast-0.4/ast/__init__.py
@@ -0,0 +1,36 @@
+from .types import *
+
+
+def dump(t):
+    if isinstance(t, AST):
+        res = type(t).__name__
+        res += "("
+        comma = False
+        for k in dir(t):
+            if k.startswith("__"):
+                continue
+            if comma:
+                res += ", "
+            res += k + "=" + dump(getattr(t, k))
+            comma = True
+        res += ")"
+        return res
+    elif isinstance(t, list):
+        res = "["
+        comma = False
+        for v in t:
+            if comma:
+                res += ", "
+            res += dump(v)
+            comma = True
+        res += "]"
+        return res
+    else:
+        return repr(t)
+
+
+def iter_fields(t):
+    for k in t._fields:
+        if k.startswith("__"):
+            continue
+        yield (k, getattr(t, k, None))

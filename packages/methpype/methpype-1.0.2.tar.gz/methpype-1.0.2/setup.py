fb4e17f5838d820c3bbc522d475d405d1fc8726c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:56 +0000

    Add package methpype/1.0.2/methpype-1.0.2.tar.gz

---
+++ packages/methpype/methpype-1.0.2.tar.gz/methpype-1.0.2/setup.py
@@ -0,0 +1,28 @@
+# Lib
+from setuptools import setup, find_packages
+
+setup(
+    name='methpype',
+    version='1.0.2',
+    description='Python-based Illumina methylation array preprocessing software',
+    long_description='Python-based Illumina methylation array preprocessing software',
+    url='https://github.com/LifeEGX/methpype',
+    license='MIT',
+    author='Life Epigenetics',
+    author_email='info@lifeegx.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scipy',
+        'statsmodels',
+        'tqdm'
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    entry_points='''
+        [console_scripts]
+        methpype-cli=methpype.cli:app
+    ''',
+)

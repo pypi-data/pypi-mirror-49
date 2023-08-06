5a1473b508d0e018452b3f0ff9a52d94d27fa949
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:16 +0000

    Add package kapuccino/0.1.0.6/kapuccino-0.1.0.6.tar.gz

---
+++ packages/kapuccino/kapuccino-0.1.0.6.tar.gz/kapuccino-0.1.0.6/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='kapuccino',
+    version='0.1.0.6',
+    author='Enrique Coronado',
+    author_email='enriquecoronadozu@gmail.mx',
+    url='http://enriquecoronadozu.github.io',
+    description='An Authoring Artificial Intelligence Modelling library',
+    packages=["kapuccino"],
+    install_requires=[
+        'nep', 'transitions'
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Software Development"
+    ]
+)

17de51e2d81ae6b3922f433cba090d8f35beac09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:35 +0000

    Add package bilispider/0.1.2/BiliSpider-0.1.2.1.tar.gz

---
+++ packages/bilispider/BiliSpider-0.1.2.1.tar.gz/BiliSpider-0.1.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('README.md','r',encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = 'BiliSpider',
+    version = '0.1.2',
+    author = 'pangbo',
+    author_email = '373108669@qq.com',
+    description = 'A spider of Bilibili',
+    long_description = long_description ,
+    long_description_content_type = 'text/markdown',
+    url = r'https://github.com/pangbo13/BiliSpider/',
+    packages = setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        ],
+    entry_points = {'console_scripts': [
+        'BiliSpider = BiliSpider.__init__:main',
+        ],},
+    )

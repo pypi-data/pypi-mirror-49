5eb342d4987872208d7299369ce8a46bafbb49c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:33 +0000

    Add package riceprint/1.5.4/riceprint-1.5.4.tar.gz

---
+++ packages/riceprint/riceprint-1.5.4.tar.gz/riceprint-1.5.4/setup.py
@@ -0,0 +1,41 @@
+import os
+from setuptools import setup
+
+cwd = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(cwd, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# Install colorama if using Windows
+if os.name == 'nt':
+   dependencies = ['colorama']
+else:
+   dependencies = []
+
+setup(
+   name='riceprint',
+   version='1.5.4',
+   description='OS-agnostic colored & custom Python console print() functions.',
+   long_description=long_description,
+   author='Kevin Sacca',
+   author_email='ssriceboat@gmail.com',
+   url='https://github.com/ssriceboat/riceprint',
+   license='MIT',
+   classifiers=[
+      'Development Status :: 4 - Beta',
+
+      'Intended Audience :: Developers',
+      'Topic :: Software Development :: Build Tools',
+
+      'License :: OSI Approved :: MIT License',
+
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: 3.5',
+      'Programming Language :: Python :: 3.6',
+      'Programming Language :: Python :: 3.7',
+   ],
+   packages=['riceprint'],
+   package_dir={'riceprint': 'src/riceprint'},
+   install_requires=dependencies,
+   keywords='print console terminal shell python pprint color progress bar'
+)

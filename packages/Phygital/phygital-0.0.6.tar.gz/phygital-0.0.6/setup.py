c51435700ef8bfb4319e660d29f7c4402d413f03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:28 +0000

    Add package Phygital/0.0.6/phygital-0.0.6.tar.gz

---
+++ packages/Phygital/phygital-0.0.6.tar.gz/phygital-0.0.6/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 29 11:07:52 2019
+
+@author: Innotechway
+"""
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="phygital",
+    version="0.0.6",
+    author="Renuka Angole",
+    author_email="renuka.angole@gmail.com",
+    description="A powerful package to work on IoT projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    include_pacage = "*.py",
+    install_requires=["pyserial","requests"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+   
+   
+)

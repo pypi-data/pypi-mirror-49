363fccc13c35b4d361099181942cfa38bf966f5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:46 +0000

    Add package qtft-qopt/0.0.1.dev1/qtft-qopt-0.0.1.dev1.tar.gz

---
+++ packages/qtft-qopt/qtft-qopt-0.0.1.dev1.tar.gz/qtft-qopt-0.0.1.dev1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+NAME = "qtft-qopt"
+VERSION = "0.0.1.dev1"
+DESCRIPTION = "Quantum-inspired optimization algorithms"
+AUTHOR = "Quantum Technology Foundation of Thailand"
+EMAIL = "contact@qtft.org"
+HOMEPAGE_URL = "https://qtft.org/"
+LICENSE = "LGPL-2.1"
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=HOMEPAGE_URL,
+    license=LICENSE,
+    classifiers=[
+        'Development Status :: 1 - Planning'
+    ],
+    packages=[],
+)

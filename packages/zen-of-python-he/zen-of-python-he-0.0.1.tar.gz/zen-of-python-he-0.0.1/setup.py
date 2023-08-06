4b589eacf074bfd9cdba432a9e9f2d1535024a9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:40 +0000

    Add package zen-of-python-he/0.0.1/zen-of-python-he-0.0.1.tar.gz

---
+++ packages/zen-of-python-he/zen-of-python-he-0.0.1.tar.gz/zen-of-python-he-0.0.1/setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='zen-of-python-he',
+    version='0.0.1',
+    packages=find_packages(),
+    license='MIT',
+    description='Ultimite guide to life, python and all.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/tvruikuzui/zen_he',
+    author='Aharon Luzon',
+    author_email='ahron901@gmail.com'
+)

b534126c5bd329717c6ccdd003413d71ac2d1292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:57 +0000

    Add package hypersParaer/2.1.3/hypersParaer-2.1.3.tar.gz

---
+++ packages/hypersParaer/hypersParaer-2.1.3.tar.gz/hypersParaer-2.1.3/tests/test_result_class.py
@@ -0,0 +1,34 @@
+from paraer.vendor import HTTPResult, HTTPPaginator
+from rest_framework.test import APIClient, APITestCase, APIRequestFactory
+
+
+class ResultClassTestCase(APITestCase):
+    def test_dict(self):
+        data = {"a": "b"}
+        request = APIRequestFactory().get('/api')
+        paginator = HTTPPaginator()
+        paginator.request = request
+
+        response = HTTPResult(paginator=paginator).response(data)
+        self.assertEqual(response.data, data)
+
+    def test_list(self):
+        data = [{"a": "b"}]
+        request = APIRequestFactory().get('/api')
+        paginator = HTTPPaginator()
+        paginator.request = request
+
+        response = HTTPResult(request=request, paginator=paginator).response(data)
+        self.assertEqual(response.data['records'], data)
+
+
+    def test_csv(self):
+        data = [{"a": "b"}]
+        request = APIRequestFactory().get('/api', content_type='text/csv')
+        paginator = HTTPPaginator()
+        paginator.request = request
+
+        response = HTTPResult(request=request, paginator=paginator).response(data, filename='test.csv')
+        self.assertEqual(response.get('content-type'), 'text/csv')
+        self.assertEqual(response.get('content-type'), 'text/csv')
+        self.assertEqual(response.get('Content-Disposition'), 'attachment; filename=test.csv')

d3d6af61ea4b0e4e9cf1d3729538e10733f9f0b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:25 +0000

    Add package maskrcnn-utils/0.1.6/maskrcnn_utils-0.1.6.tar.gz

---
+++ packages/maskrcnn-utils/maskrcnn_utils-0.1.6.tar.gz/maskrcnn_utils-0.1.6/maskrcnn_utils/maskrcnn_utils.py
@@ -0,0 +1,133 @@
+import torch
+from . import _C
+from torch import nn
+from torch.autograd import Function
+from torch.autograd.function import once_differentiable
+from torch.nn.modules.utils import _pair
+
+def nms(dets, scores, threshold, centers=None):
+    '''
+    dets: tensor
+    scores: tensor
+    threshold: float
+    centers: tensor, coresponding mask center
+    '''
+    if centers is not None:
+        print("center nms ing...")
+        return _C.center_nms(dets, scores, threshold, centers)
+    else:
+        return _C.nms(dets, scores, threshold)
+
+# ROIAlign
+class _ROIAlign(Function):
+    @staticmethod
+    def forward(ctx, input, roi, output_size, spatial_scale, sampling_ratio):
+        ctx.save_for_backward(roi)
+        ctx.output_size = _pair(output_size)
+        ctx.spatial_scale = spatial_scale
+        ctx.sampling_ratio = sampling_ratio
+        ctx.input_shape = input.size()
+        output = _C.roi_align_forward(
+            input, roi, spatial_scale, output_size[0], output_size[1], sampling_ratio
+        )
+        return output
+
+    @staticmethod
+    @once_differentiable
+    def backward(ctx, grad_output):
+        rois, = ctx.saved_tensors
+        output_size = ctx.output_size
+        spatial_scale = ctx.spatial_scale
+        sampling_ratio = ctx.sampling_ratio
+        bs, ch, h, w = ctx.input_shape
+        grad_input = _C.roi_align_backward(
+            grad_output,
+            rois,
+            spatial_scale,
+            output_size[0],
+            output_size[1],
+            bs,
+            ch,
+            h,
+            w,
+            sampling_ratio,
+        )
+        return grad_input, None, None, None, None
+
+roi_align = _ROIAlign.apply
+
+class ROIAlign(nn.Module):
+    def __init__(self, output_size, spatial_scale, sampling_ratio):
+        super(ROIAlign, self).__init__()
+        self.output_size = output_size
+        self.spatial_scale = spatial_scale
+        self.sampling_ratio = sampling_ratio
+
+    def forward(self, input, rois):
+        return roi_align(
+            input, rois, self.output_size, self.spatial_scale, self.sampling_ratio
+        )
+
+    def __repr__(self):
+        tmpstr = self.__class__.__name__ + "("
+        tmpstr += "output_size=" + str(self.output_size)
+        tmpstr += ", spatial_scale=" + str(self.spatial_scale)
+        tmpstr += ", sampling_ratio=" + str(self.sampling_ratio)
+        tmpstr += ")"
+        return tmpstr
+
+# ROIPooling
+
+class _ROIPool(Function):
+    @staticmethod
+    def forward(ctx, input, roi, output_size, spatial_scale):
+        ctx.output_size = _pair(output_size)
+        ctx.spatial_scale = spatial_scale
+        ctx.input_shape = input.size()
+        output, argmax = _C.roi_pool_forward(
+            input, roi, spatial_scale, output_size[0], output_size[1]
+        )
+        ctx.save_for_backward(input, roi, argmax)
+        return output
+
+    @staticmethod
+    @once_differentiable
+    def backward(ctx, grad_output):
+        input, rois, argmax = ctx.saved_tensors
+        output_size = ctx.output_size
+        spatial_scale = ctx.spatial_scale
+        bs, ch, h, w = ctx.input_shape
+        grad_input = _C.roi_pool_backward(
+            grad_output,
+            input,
+            rois,
+            argmax,
+            spatial_scale,
+            output_size[0],
+            output_size[1],
+            bs,
+            ch,
+            h,
+            w,
+        )
+        return grad_input, None, None, None
+
+
+roi_pool = _ROIPool.apply
+
+
+class ROIPool(nn.Module):
+    def __init__(self, output_size, spatial_scale):
+        super(ROIPool, self).__init__()
+        self.output_size = output_size
+        self.spatial_scale = spatial_scale
+
+    def forward(self, input, rois):
+        return roi_pool(input, rois, self.output_size, self.spatial_scale)
+
+    def __repr__(self):
+        tmpstr = self.__class__.__name__ + "("
+        tmpstr += "output_size=" + str(self.output_size)
+        tmpstr += ", spatial_scale=" + str(self.spatial_scale)
+        tmpstr += ")"
+        return tmpstr
---
+++ packages/maskrcnn-utils/maskrcnn_utils-0.1.6.tar.gz/maskrcnn_utils-0.1.6/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from torch.utils.cpp_extension import CppExtension, CUDAExtension, BuildExtension
+import os
+import torch
+os.environ['CUDA_VISIBLE_DEVICES']='0,1,2,3'
+
+ext_modules=[
+        CUDAExtension('maskrcnn_utils._C',
+            [
+                'csrc/vision.cpp',
+                'csrc/cpu/ROIAlign_cpu.cpp',
+                'csrc/cpu/nms_cpu.cpp',
+                'csrc/cuda/ROIAlign_cuda.cu',
+                'csrc/cuda/ROIPool_cuda.cu',
+                'csrc/cuda/SigmoidFocalLoss_cuda.cu',
+                'csrc/cuda/nms.cu',
+                ],
+            extra_compile_args=['-DWITH_CUDA', '-O2'],
+            )
+        ]
+
+setup(
+        name='maskrcnn_utils',
+        version='0.1.6',
+        description='this is a tool package for maskrcnn',
+        author='Kuuuurt',
+        author_email='niu1187203155@gmail.com',
+        url='https://github.com/iHateTa11B0y/CROIUtils',
+        packages=['maskrcnn_utils'],
+        ext_modules=ext_modules,
+        cmdclass={'build_ext': BuildExtension},
+)

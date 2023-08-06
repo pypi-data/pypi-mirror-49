0d6938c1795f9d41e7c71fd06d848a7bd99f45d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:17 +0000

    Add package kodijsonrpc/1.1/kodijsonrpc-1.1.tar.gz

---
+++ packages/kodijsonrpc/kodijsonrpc-1.1.tar.gz/kodijsonrpc-1.1/setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup
+
+path = os.path.abspath(os.path.dirname(__file__))
+
+# Get the long description from the README file
+with open(os.path.join(path, 'README.rst'), encoding='utf-8') as f:
+  long_description = f.read()
+
+# Get requirements from file
+with open(os.path.join(path, 'requirements.txt')) as f:
+  requirements = f.read().splitlines()
+
+setup(name='kodijsonrpc',
+      version='1.1',
+      description='Kodi JSON-RPC client',
+      long_description=long_description,
+      url='http://github.com/davgeo/kodijsonrpc',
+      author='David George',
+      author_email='dcg.git@gmail.com',
+      license='MIT',
+      packages=["kodijsonrpc"],
+      install_requires=requirements)

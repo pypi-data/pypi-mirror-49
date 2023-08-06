61c1c9294e182079bf7e4011f44e026695fb6cab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:32 +0000

    Add package refnx/0.1.8/refnx-0.1.8-cp36-cp36m-manylinux2010_x86_64.whl

---
+++ packages/refnx/refnx-0.1.8-cp36-cp36m-manylinux2010_x86_64.whl/refnx/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM REFNX SETUP.PY
+short_version = '0.1.8'
+version = '0.1.8'
+full_version = '0.1.8'
+git_revision = '06bf2694ba1e8fad19f0d091af1e24c84f11c735'
+release = True
+if not release:
+    version = full_version

43feef8e5c0383902d173fdfc372b560671039ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:07 +0000

    Add package CrudeBHT/0.0.1/CrudeBHT-0.0.1.tar.gz

---
+++ packages/CrudeBHT/CrudeBHT-0.0.1.tar.gz/CrudeBHT-0.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "CrudeBHT",
+    version = "0.0.1",
+    author = "Naimish Mani B",
+    author_email = "naimish240@gmail.com",
+    description = "A simple implementation of the Barnes-Hut algorithm for N-body simulations in python 3.7 ",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    entry_points = {'console_scripts' : ['crudebht = code.simulation:__main__']},
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

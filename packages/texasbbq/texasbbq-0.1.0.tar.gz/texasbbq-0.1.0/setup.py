638c49a6b93302141ece2d75c6857e200b73278c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:38 +0000

    Add package texasbbq/0.1.0/texasbbq-0.1.0.tar.gz

---
+++ packages/texasbbq/texasbbq-0.1.0.tar.gz/texasbbq-0.1.0/setup.py
@@ -0,0 +1,39 @@
+""" texasbbq """
+
+from setuptools import setup
+from os import path
+# io.open is needed for projects that support Python 2.7
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='texasbbq',
+    version='0.1.0',
+    description='Smoke out the bugs that break dependent projects.',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    url='https://github.com/numba/texasbbq',
+    author='Valentin Haenel',
+    author_email='valentin@haenel.co',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords='testing development',
+    py_modules=["texasbbq"],
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
+)

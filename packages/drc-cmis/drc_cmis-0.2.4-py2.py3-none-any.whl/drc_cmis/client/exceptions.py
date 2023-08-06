af197aec95278bd3db6dc8b285a77dbac988608b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:01 +0000

    Add package drc-cmis/0.2.4/drc_cmis-0.2.4-py2.py3-none-any.whl

---
+++ packages/drc-cmis/drc_cmis-0.2.4-py2.py3-none-any.whl/drc_cmis/client/exceptions.py
@@ -0,0 +1,19 @@
+class DMSException(Exception):
+    def __init__(self, message, *args, **kwargs):
+        self.message = message
+
+
+class DocumentExistsError(DMSException):
+    pass
+
+
+class DocumentDoesNotExistError(DMSException):
+    pass
+
+
+class DocumentConflictException(DMSException):
+    pass
+
+
+class DocumentLockedException(DMSException):
+    pass

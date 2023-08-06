c8161f4c4a948c3e61d888d1343321e37413bab2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:26 +0000

    Add package ssutils/0.1.1/ssutils-0.1.1.tar.gz

---
+++ packages/ssutils/ssutils-0.1.1.tar.gz/ssutils-0.1.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name='ssutils',
+	version='0.1.1',
+	author="S Satapathy",
+	author_email="shubhakant.satapathy@gmail.com",
+	description="collection of useful python functions",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/satapathy/pypi-ssutils",
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+ )

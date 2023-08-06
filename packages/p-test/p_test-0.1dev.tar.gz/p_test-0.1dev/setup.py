8dc201c304bb2ce0f423d91ce1a2446eb3b12d49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:39 +0000

    Add package p-test/0.1dev/p_test-0.1dev.tar.gz

---
+++ packages/p-test/p_test-0.1dev.tar.gz/p_test-0.1dev/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name = 'p_test',
+    version = '0.1dev',
+    package = 'p_test',
+    license = 'UT',
+    long_description = open("README.txt").read(),
+    )

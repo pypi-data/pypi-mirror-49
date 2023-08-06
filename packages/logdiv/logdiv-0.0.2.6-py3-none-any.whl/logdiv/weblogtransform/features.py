411c0ce20c634c087ddf5e20f3ef66f698eafb08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:44 +0000

    Add package logdiv/0.0.2.6/logdiv-0.0.2.6-py3-none-any.whl

---
+++ packages/logdiv/logdiv-0.0.2.6-py3-none-any.whl/logdiv/weblogtransform/features.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+def assign_page_classification(weblog,pages,classification_columns,weblog_columns_dict,pages_columns_dict):
+    """
+    Associate each category and topic of requested and referrer pages of weblog using pages.csv file
+        
+    Parameters
+    ----------
+        weblog: pandas dataframe of requests
+
+        pages: pandas dataframe of pages
+            
+        classification_columns: list of string, associate each classification wanted to each page in weblog file
+
+        weblog_columns_dict: dict
+
+        pages_columns_dict: dict
+
+    Returns
+    -------
+        Pandas Dataframe
+    """
+    for classification_column in classification_columns:
+        
+        weblog['requested_'+classification_column]=weblog[weblog_columns_dict['requested_page_column']].map(pd.Series(index=pages\
+                          [pages_columns_dict['item_id_column']].values, data=pages[classification_column].values))
+        weblog['referrer_'+classification_column]=weblog[weblog_columns_dict['referrer_page_column']].map(pd.Series(index=pages\
+                          [pages_columns_dict['item_id_column']].values,data=pages[classification_column].values))
+    return weblog;

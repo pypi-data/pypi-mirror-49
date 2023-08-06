519dece8cd534ec53333d16497ff3ae8fe99bce2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:52 +0000

    Add package cubestories/0.2.6/cubestories-0.2.6.tar.gz

---
+++ packages/cubestories/cubestories-0.2.6.tar.gz/cubestories-0.2.6/CubeStories/storyTeller/storyTeller.py
@@ -0,0 +1,69 @@
+from ..storyPattern import *
+from ..cubeParameters import cubeParameters
+from ..metaData import metaData 
+#from ..storyPattern import *
+
+
+class storyTeller(object):
+
+    def __init__(self, metadataparameters,cubeparameters,patternparameters):
+        self.meta=metaData(**metadataparameters)
+        self.cubeparams=cubeParameters(**cubeparameters)
+        self.storytelling=None
+        self.analysispipeline=patternparameters
+    
+
+    def tellStory(self):
+        analysisList=[]
+        self.storytelling=self.meta+self.cubeparams
+        try:
+            for patanalysis in self.analysispipeline:
+                if(patanalysis=="MeasCount"):
+                    pattern=measCount(**self.analysispipeline[patanalysis])
+                    analysisList.append(pattern)
+                elif(patanalysis=="LeagueTab"):
+                    pattern=leagueTable(**self.analysispipeline[patanalysis])
+                    analysisList.append(pattern)
+                elif(patanalysis=="DissFact"):
+                    pattern=dissFactors(**self.analysispipeline[patanalysis])
+                    analysisList.append(pattern)
+                elif(patanalysis=="ExpInt"):
+                    pattern=exploreIntersection(**self.analysispipeline[patanalysis])
+                    analysisList.append(pattern)
+                elif(patanalysis=="ExtComp"):
+                    pattern=extComparison(**self.analysispipeline[patanalysis])
+                    analysisList.append(pattern)
+                elif(patanalysis=="HighCont"):
+                    pattern=highContrast(**self.analysispipeline[patanalysis])
+                    analysisList.append(pattern)
+                elif(patanalysis=="IntComp"):
+                    pattern=intComparison(**self.analysispipeline[patanalysis])
+                    analysisList.append(pattern)
+                elif(patanalysis=="ProfOut"):
+                    pattern=profOutliers(**self.analysispipeline[patanalysis])
+                    analysisList.append(pattern)
+                elif(patanalysis=="StBigDrillDown"):
+                    pattern=startBigDrillDown(**self.analysispipeline[patanalysis])
+                    analysisList.append(pattern)
+                elif(patanalysis=="StSmallZoomOut"):
+                    pattern=startSmallZoomOut(**self.analysispipeline[patanalysis])
+                    analysisList.append(pattern)
+                elif(patanalysis=="NarrChangeOT"):
+                    pattern=narrChangeOT(**self.analysispipeline[patanalysis])
+                    analysisList.append(pattern)
+                elif(patanalysis=="AByCategory"):
+                    pattern=analysisByCategory(**self.analysispipeline[patanalysis])
+                    analysisList.append(pattern)
+                else:
+                    raise Exception("Wrong Type of Story Pattern Selected")
+        
+            for pattern in analysisList:
+                self.storytelling+=pattern
+        except Exception as e:
+            print("Storytelling failed: "+repr(e))
+    
+    def showStory(self):
+        return self.storytelling.data
+            
+
+

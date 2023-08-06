e0d2f0bcd8ad815f49b05a63cde0a4d8adb8f4eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:15 +0000

    Add package nbsvm-sklearn/0.0.6/nbsvm-sklearn-0.0.6.tar.gz

---
+++ packages/nbsvm-sklearn/nbsvm-sklearn-0.0.6.tar.gz/nbsvm-sklearn-0.0.6/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+setup(
+    name='nbsvm-sklearn',
+    version='0.0.6',
+    description='sklearn wrapper for NB-SVM algorithm',
+    url='https://github.com/Pritishsingla/nbsvm_sklearn.git',
+    author='Pritish Singla',
+    author_email='pritishs901@gmail.com',
+    license='MIT',
+    packages=['nbsvm'],
+    install_requires=[
+        'numpy>=1.16.4',
+        'scikit-learn>=0.21.0',
+    ],
+    zip_safe=False
+  )

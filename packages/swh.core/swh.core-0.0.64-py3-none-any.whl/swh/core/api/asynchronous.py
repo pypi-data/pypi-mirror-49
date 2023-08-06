5565fa48160f541d2140c2383b74b9818409f129
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:17 +0000

    Add package swh.core/0.0.64/swh.core-0.0.64-py3-none-any.whl

---
+++ packages/swh.core/swh.core-0.0.64-py3-none-any.whl/swh/core/api/asynchronous.py
@@ -0,0 +1,61 @@
+import json
+import logging
+import pickle
+import sys
+import traceback
+
+import aiohttp.web
+from deprecated import deprecated
+import multidict
+
+from .serializers import msgpack_dumps, msgpack_loads, SWHJSONDecoder
+
+
+def encode_data_server(data, **kwargs):
+    return aiohttp.web.Response(
+        body=msgpack_dumps(data),
+        headers=multidict.MultiDict({'Content-Type': 'application/x-msgpack'}),
+        **kwargs
+    )
+
+
+async def decode_request(request):
+    content_type = request.headers.get('Content-Type')
+    data = await request.read()
+    if not data:
+        return {}
+    if content_type == 'application/x-msgpack':
+        r = msgpack_loads(data)
+    elif content_type == 'application/json':
+        r = json.loads(data.decode(), cls=SWHJSONDecoder)
+    else:
+        raise ValueError('Wrong content type `%s` for API request'
+                         % content_type)
+    return r
+
+
+async def error_middleware(app, handler):
+    async def middleware_handler(request):
+        try:
+            return (await handler(request))
+        except Exception as e:
+            if isinstance(e, aiohttp.web.HTTPException):
+                raise
+            logging.exception(e)
+            exception = traceback.format_exception(*sys.exc_info())
+            res = {'exception': exception,
+                   'exception_pickled': pickle.dumps(e)}
+            return encode_data_server(res, status=500)
+    return middleware_handler
+
+
+class RPCServerApp(aiohttp.web.Application):
+    def __init__(self, *args, middlewares=(), **kwargs):
+        middlewares = (error_middleware,) + middlewares
+        super().__init__(*args, middlewares=middlewares, **kwargs)
+
+
+@deprecated(version='0.0.64',
+            reason='Use the RPCServerApp instead')
+class SWHRemoteAPI(RPCServerApp):
+    pass

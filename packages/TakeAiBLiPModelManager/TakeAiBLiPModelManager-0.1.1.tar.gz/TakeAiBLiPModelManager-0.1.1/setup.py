d2249453afd0a0098babf533baa6fd4eeaf95785
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:08 +0000

    Add package TakeAiBLiPModelManager/0.1.1/TakeAiBLiPModelManager-0.1.1.tar.gz

---
+++ packages/TakeAiBLiPModelManager/TakeAiBLiPModelManager-0.1.1.tar.gz/TakeAiBLiPModelManager-0.1.1/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='TakeAiBLiPModelManager',
+    version='0.1.1',
+    url='',
+    license='MIT License',
+    author='Renan Santos',
+    author_email='renans@take.net',
+    keywords='BLiP NLP Ai ML Test',
+    description=u'Package to automate the intents creation, examples addition, model training, publishin and analysis in the BLiP platform',
+    packages=find_packages(),
+    install_requires=[],
+)

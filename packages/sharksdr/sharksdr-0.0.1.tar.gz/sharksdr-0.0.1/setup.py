c6d939ea3f1f1401fe9067e912de450588dbf042
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:40 +0000

    Add package sharksdr/0.0.1/sharksdr-0.0.1.tar.gz

---
+++ packages/sharksdr/sharksdr-0.0.1.tar.gz/sharksdr-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+setup(name='sharksdr',
+      version='0.0.1',
+      description='SHaRK',
+      long_description='',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Intended Audience :: Developers',
+      ],
+      url='http://github.com/storborg/shark',
+      keywords='',
+      author='Scott Torborg',
+      author_email='storborg@gmail.com',
+      license='',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+     )

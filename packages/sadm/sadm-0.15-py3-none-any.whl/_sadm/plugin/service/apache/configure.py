6b1cad9c39b89f82d82f5331c8662852ef1f541c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:44 +0000

    Add package sadm/0.15/sadm-0.15-py3-none-any.whl

---
+++ packages/sadm/sadm-0.15-py3-none-any.whl/_sadm/plugin/service/apache/configure.py
@@ -0,0 +1,14 @@
+# Copyright (c) Jeremías Casteglione <jrmsdev@gmail.com>
+# See LICENSE file.
+
+__all__ = ['configure']
+
+def configure(env, cfg):
+	env.settings.merge(cfg, 'service.apache', (
+		'conf.disable',
+		'mod.disable',
+		'site.disable',
+		'conf.enable',
+		'mod.enable',
+		'site.enable',
+	))

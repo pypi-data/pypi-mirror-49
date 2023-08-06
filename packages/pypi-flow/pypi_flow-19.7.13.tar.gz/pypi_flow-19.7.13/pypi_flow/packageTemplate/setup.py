861e8338acb562dc10900a5fc2e750fb3dce413f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:48 +0000

    Add package pypi-flow/19.7.13/pypi_flow-19.7.13.tar.gz

---
+++ packages/pypi-flow/pypi_flow-19.7.13.tar.gz/pypi_flow-19.7.13/pypi_flow/packageTemplate/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pypi-sidekick",
+    version="19.07.0",
+    author="Gabriel Rosales",
+    author_email="gabriel.alejandro.rosales@gmail.com",
+    description="Common functions for quick python development",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ZenosParadox/pypi-sidekick",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+)

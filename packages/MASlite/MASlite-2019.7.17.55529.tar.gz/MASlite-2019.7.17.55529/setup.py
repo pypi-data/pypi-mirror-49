e1578be689e2476ce8370154b70b995d464325c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:54 +0000

    Add package MASlite/2019.7.17.55529/MASlite-2019.7.17.55529.tar.gz

---
+++ packages/MASlite/MASlite-2019.7.17.55529.tar.gz/MASlite-2019.7.17.55529/setup.py
@@ -0,0 +1,40 @@
+# Maslite setup script created by package.py
+build_tag = "15b88d2b1a34e7b447f7a99772704042b1e77a3877b1c46db47f4b23426623d7"
+from pathlib import Path
+from setuptools import setup
+
+
+folder = Path(__file__).parent
+filename = "readme.md"
+readme = folder / filename
+assert isinstance(readme, Path)
+assert readme.exists(), readme
+with open(str(readme), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="MASlite",
+    version="2019.7.17.55529",
+    url="https://github.com/root-11/maslite",
+    license="MIT",
+    author="Bjorn Madsen",
+    author_email="bjorn.madsen@operationsresearchgroup.com",
+    description="A lightweight multi-agent system",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords="multi agent system MAS",
+    packages=["maslite"],
+    include_package_data=True,
+    data_files=[(".", ["license.md", "readme.md"])],
+    platforms="any",
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)

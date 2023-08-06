3e9f098ce4052ac50aea8f6ba3997e46735546df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:29 +0000

    Add package PipTUI/0.1.4/PipTUI-0.1.4.tar.gz

---
+++ packages/PipTUI/PipTUI-0.1.4.tar.gz/PipTUI-0.1.4/README.md
@@ -0,0 +1,32 @@
+# PipTUI
+Pip GUI in your terminal!
+
+![main](https://user-images.githubusercontent.com/41646249/61582508-8c2e3b80-ab34-11e9-8e53-a8439c0f2e04.png)
+![search](https://user-images.githubusercontent.com/41646249/61582525-a5cf8300-ab34-11e9-8724-fe09d8471e12.png)
+
+
+### Installation
+
+```bash
+pip3 install PipTUI
+```
+
+
+### Running it:
+```bash
+pip_tui
+```
+
+### Shortcuts
+
+* Install package: `Ctrl-A`
+* Uninstall package: `Ctrl-R`
+* Update package: `Ctrl-U`
+
+
+
+### DISCLAIMER
+
+This app only works if `pip` is installed and it's not a pip alternative, it's just GUI wrapper for the pip commands! 
+
+If you're using other ways to install python packages some changes should be made in your end. A little example is to set a pip alias if you're using apps like `pipx` to install python packages!
---
+++ packages/PipTUI/PipTUI-0.1.4.tar.gz/PipTUI-0.1.4/piptui/mainForm.py
@@ -0,0 +1,56 @@
+import curses
+
+from .custom.customForm import FormBaseNewHinted
+
+from .infoBox import InfoBox
+from .logBox import LogBox
+from .pkgBox import PkgBox
+from .searchBox import QueryPypi, SearchBox
+
+
+class MainForm(FormBaseNewHinted):
+    def create(self):
+        exit_handlers = {'^Q': lambda x: exit(0),
+                         155: lambda x: exit(0),
+                         curses.ascii.ESC: lambda x: exit(0)}
+        action_handlers = {
+            '^A': lambda x: self.parentApp.switchForm("INSTALL"),
+            '^R': lambda x: self.parentApp.switchForm("UNINSTALL"),
+            '^U': lambda x: self.parentApp.switchForm("UPDATE")}
+
+        self.add_handlers(exit_handlers)
+        self.add_handlers(action_handlers)
+        self.add_event_hander(
+            'event_package_select',
+            self.event_package_select)
+
+        y, x = self.useable_space()
+
+        self.PkgBoxObj = self.add(PkgBox,
+                                  name="Packages",
+                                  value=0, relx=1, max_width=x // 5, rely=2,
+                                  max_height=-5)
+
+        self.InfoBoxObj = self.add(
+            InfoBox, name='Description', rely=2, relx=(
+                x // 5) + 1, max_height=-5)
+        self.SearchBoxObj = self.add(
+            SearchBox,
+            name="Search",
+            value=0,
+            relx=1,
+            max_width=x // 5,
+            max_height=-5,
+        )
+
+        self.LogBoxObj = self.add(LogBox, name='Pip Log', relx=(
+            x // 5) + 1, rely=-7, max_height=-5)
+
+        self.action_controller = QueryPypi
+
+        self.display()
+        self.PkgBoxObj.create()
+        self.InfoBoxObj.create()
+
+    def event_package_select(self, *args, **kwargs):
+        self.InfoBoxObj.display_info()

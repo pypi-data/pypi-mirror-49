138293776f7ed9498b1e7f6d63257bef39ae3464
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:59 +0000

    Add package daauto/0.2/daauto-0.2.tar.gz

---
+++ packages/daauto/daauto-0.2.tar.gz/daauto-0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='daauto',
+      version='0.2',
+      description='autodata',
+      url='https://github.com/AronWater/daauto',
+      author='AronWater2',
+      author_email='kclukac@connect.ust.hk',
+      license='MIT',
+      packages=['daauto'],
+       install_requires=[
+          'markdown',
+      ],
+      zip_safe=False)

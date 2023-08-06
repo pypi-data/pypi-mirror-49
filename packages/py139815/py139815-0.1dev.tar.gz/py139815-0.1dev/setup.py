80592b2f32303c7fe3495668373681958a8d1545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:22 +0000

    Add package py139815/0.1dev/py139815-0.1dev.tar.gz

---
+++ packages/py139815/py139815-0.1dev.tar.gz/py139815-0.1dev/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name = 'py139815',
+    version = '0.1dev',
+    packages = ['py139815',],
+    license = 'UT',
+    long_description = open("README.txt").read()
+    )

b19dcceff8b68172f443a4f483f44cf99ed454d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:59 +0000

    Add package grtoolkit/19.7.22/grtoolkit-19.7.22.tar.gz

---
+++ packages/grtoolkit/grtoolkit-19.7.22.tar.gz/grtoolkit-19.7.22/grtoolkit/__init__.py
@@ -0,0 +1,13 @@
+import grtoolkit.Decorators
+import grtoolkit.File
+import grtoolkit.Format
+import grtoolkit.Math
+import grtoolkit.PDF
+import grtoolkit.Python
+import grtoolkit.Storage
+import grtoolkit.Windows
+
+import os, sys
+
+def cwd():
+    return os.path.dirname(sys.argv[0])

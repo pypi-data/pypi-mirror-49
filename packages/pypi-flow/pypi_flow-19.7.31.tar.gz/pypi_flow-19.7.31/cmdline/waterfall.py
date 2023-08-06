f6626c2841c05fae10cc5aaecd2e13085dab4b58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:15:37 +0000

    Add package pypi-flow/19.7.31/pypi_flow-19.7.31.tar.gz

---
+++ packages/pypi-flow/pypi_flow-19.7.31.tar.gz/pypi_flow-19.7.31/cmdline/waterfall.py
@@ -0,0 +1,20 @@
+import datetime
+import grtoolkit as grt
+from grtoolkit.Python import packageDir
+
+#COLLECT PROJECT INPUT FROM USER
+print("Welcome to the pypi_flow project creator!\nA project will be created in this directory based on the following information:\n")
+packageName = input("package name: ")
+author = input("author: ")
+description = input("project description: ")
+email = input("contact email: ")
+url = input("project website: ")
+year = datetime.datetime.now().year
+
+projectFolder = grt.cwd()
+templateSource = packageDir("pypi_flow") + "\\packageTemplate"
+
+print(projectFolder)
+
+import sys
+print(sys.argv[1])

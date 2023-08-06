34c24641f2b52d780462b01057c1b10d0d343051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:37 +0000

    Add package webplatform-backend/1.0.4/webplatform-backend-1.0.4.tar.gz

---
+++ packages/webplatform-backend/webplatform-backend-1.0.4.tar.gz/webplatform-backend-1.0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(name = "webplatform-backend",
+   version = "1.0.4",
+   description = "Backend Flask instance used for a webplatform",
+   author = "Matthew Owens",
+   author_email = "mowens@redhat.com",
+   url = "https://github.com/lost-osiris/webplatform-backend",
+   packages = find_packages(),
+   include_package_data = True,
+   install_requires = [
+      "webplatform-cli",
+      # "webplatform-auth",
+   ],
+   python_requires='>=3',
+   license='MIT',
+   long_description = """TODO""",
+   classifiers = [
+       "Programming Language :: Python :: 3",
+       "License :: OSI Approved :: MIT License",
+       "Operating System :: OS Independent",
+   ],
+   scripts = [
+      "webplatform_backend/webplatform-backend"
+   ]
+)

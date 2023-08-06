d3a7806e964f2a367d5e9abcf43b408681c23c93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:32 +0000

    Add package imagestoanimation/0.1.4/imagestoanimation-0.1.4.tar.gz

---
+++ packages/imagestoanimation/imagestoanimation-0.1.4.tar.gz/imagesToAnimation-0.1.4/imagestoanimation/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+from os import path
+from io import open
+here = path.abspath(path.dirname(__file__))
+
+with open("../README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='imagestoanimation',
+    version='0.1.4',
+    description="Converts a series of images to an animation using OpenCV",
+
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    url="https://github.com/jpsmithnl/imagesToAnimation",
+
+    # For a list of valid classifiers, see https://pypi.org/classifiers/
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6'
+    ],
+
+    packages=find_packages(),
+
+    install_requires=[
+        'opencv-python (>=4.1,<5.0)',
+        'configargparse (>=0.14.0,<0.15.0)',
+    ],
+
+    include_package_data=True,
+)
---
+++ packages/imagestoanimation/imagestoanimation-0.1.4.tar.gz/imagesToAnimation-0.1.4/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+packages = \
+['imagestoanimation']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['configargparse>=0.14.0,<0.15.0', 'opencv-python>=4.1,<5.0']
+
+entry_points = \
+{'console_scripts': ['imgvid = imagesToAnimation:run']}
+
+setup_kwargs = {
+    'name': 'imagestoanimation',
+    'version': '0.1.4',
+    'description': 'Converts a series of images to an animation using OpenCV',
+    'long_description': None,
+    'author': 'jpsmithnl',
+    'author_email': 'jp.smith@mun.ca',
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)

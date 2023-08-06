3c956a65fcb1bf006ca4bfb4f8b3ec232d53e7c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:21 +0000

    Add package testaid/0.3/testaid-0.3.tar.gz

---
+++ packages/testaid/testaid-0.3.tar.gz/testaid-0.3/CHANGELOG.rst
@@ -0,0 +1,26 @@
+=========
+Changelog
+=========
+
+0.1
+===
+
+* Initial version providing testpass and testvars fixtures
+
+0.2
+===
+
+* Convert testaid into a pytest plugin
+* Add tox configuration
+* Add molecule test project for Debian GNU/Linux
+* Add boilerplate function for testinfra invocation
+
+0.3
+===
+
+* Add example role: anarchism
+* Add resolve templates tests
+* Add escape special chars tests
+* Add resolve lookup plugin test
+* Add documentation to README file
+* Add makefile
---
+++ packages/testaid/testaid-0.3.tar.gz/testaid-0.3/setup.py
@@ -0,0 +1,38 @@
+import os
+import setuptools
+
+if __name__ == '__main__':
+    setuptools.setup(
+        name='testaid',
+        version='0.3',
+        author='RebelCodeBase',
+        author_email='rebelcodebase@riseup.net',
+        description='Fixtures for testinfra and molecule',
+        long_description=open(os.path.join(
+            os.path.dirname(__file__), 'README.rst')).read(),
+        url='https://github.com/RebelCodeBase/testaid',
+        license='Apache-2.0',
+        packages=setuptools.find_packages(),
+        package_data={
+            '': ['*.rst'],
+        },
+        entry_points={
+            'pytest11': ['testaid=testaid.plugin']
+        },
+        platforms='any',
+        zip_safe=False,
+        classifiers=[
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3',
+            'Intended Audience :: Information Technology',
+            'Intended Audience :: Developers',
+            'Intended Audience :: System Administrators',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: POSIX',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'Topic :: Software Development :: Testing',
+            'Topic :: System :: Systems Administration',
+            'Framework :: Pytest',
+
+        ],
+    )

4ade1688fe44f4a818d2fa82316692d671475f86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:40:37 +0000

    Add package viridian/0.0.1/viridian-0.0.1.tar.gz

---
+++ packages/viridian/viridian-0.0.1.tar.gz/viridian-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='viridian',
+    version='0.0.1',
+    author='Matthew Dowdell',
+    author_email='matthew.dowdell@hpe.com',
+    url='https://example.org',
+    description='A small package',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
---
+++ packages/viridian/viridian-0.0.1.tar.gz/viridian-0.0.1/viridian/__init__.py
@@ -0,0 +1,1 @@
+"""This is a placeholder for a package of the same name."""

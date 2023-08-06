0f7bf5a9c6f6d1690811aabf2c66e604c19adcea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:44 +0000

    Add package mysqltools-python/2.19.07.27/mysqltools-python-2.19.07.27.tar.gz

---
+++ packages/mysqltools-python/mysqltools-python-2.19.07.27.tar.gz/mysqltools-python-2.19.07.27/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+olds_scripts = ['bin/mtlsmonitor','bin/mtlsbackup','bin/mtlslog','bin/mtlsanalysis',
+               'bin/mtlshttp','bin/mtlsbigfiles','bin/mtlsdeleterows']
+
+news_scripts = ['bin/mtls-big-files','bin/mtls-delete-rows','bin/mtls-file-truncate','bin/mtls-http','bin/mtls-log']
+
+scripts = olds_scripts + news_scripts
+
+setup(name='mysqltools-python',
+      version='2.19.07.27',
+      scripts=scripts,
+      packages=['mtls','mtls.kits'],
+      maintainer='Neeky',
+      maintainer_email='neeky@live.com',
+      url='https://github.com/Neeky/mysqltools-python',
+      install_requires=['mysql-connector-python>=8.0.12'],
+      python_requires='>=3.1.*,'
+      )
+
+
+      

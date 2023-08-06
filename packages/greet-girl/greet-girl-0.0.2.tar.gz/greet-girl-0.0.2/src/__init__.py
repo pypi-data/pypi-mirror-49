93474ccd1a0a7225235c5cdf607f1e7761019310
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:06 +0000

    Add package greet-girl/0.0.2/greet-girl-0.0.2.tar.gz

---
+++ packages/greet-girl/greet-girl-0.0.2.tar.gz/greet-girl-0.0.2/src/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Author  : itswcg
+# @File    : __init__.py
+# @Time    : 19-7-15 下午5:43
+# @Blog    : https://blog.itswcg.com
+# @github  : https://github.com/itswcg
+
+name = "greet-girl"
